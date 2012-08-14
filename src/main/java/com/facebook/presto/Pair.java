e76b9e67af0bc766355d0288c16485a6b1adb070
Dain Sundstrom <dain@iq80.com>
2012-08-13 17:04:30 -0700

    Add equals and hashCode

--- src/main/java/com/facebook/presto/Pair.java
+++ src/main/java/com/facebook/presto/Pair.java
@@ -47,6 +47,35 @@ public class Pair
         };
     }
 
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Pair pair = (Pair) o;
+
+        if (position != pair.position) {
+            return false;
+        }
+        if (value != null ? !value.equals(pair.value) : pair.value != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = (int) (position ^ (position >>> 32));
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
 
     @Override
     public String toString()
