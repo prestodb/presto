13228204f45c59572a044d03a519cbb42af80946
Martin Traverso <martint@fb.com>
2012-08-21 11:32:45 -0700

    introduce tuple builder

--- src/test/java/com/facebook/presto/TestTupleInfo.java
+++ src/test/java/com/facebook/presto/TestTupleInfo.java
@@ -2,7 +2,6 @@ package com.facebook.presto;
 
 import org.testng.annotations.Test;
 
-import static com.facebook.presto.SizeOf.SIZE_OF_LONG;
 import static com.facebook.presto.SizeOf.SIZE_OF_SHORT;
 import static com.facebook.presto.TupleInfo.Type.FIXED_INT_64;
 import static com.facebook.presto.TupleInfo.Type.VARIABLE_BINARY;
@@ -11,26 +10,77 @@ import static org.testng.Assert.assertEquals;
 public class TestTupleInfo
 {
     @Test
-    public void testBasic()
+    public void testOnlyFixedLength()
     {
-        Slice slice = new Slice(SIZE_OF_LONG + SIZE_OF_LONG + SIZE_OF_SHORT + SIZE_OF_SHORT + 10 + 15);
+        TupleInfo info = new TupleInfo(FIXED_INT_64, FIXED_INT_64);
+
+        Tuple tuple = info.builder()
+                .append(42)
+                .append(67)
+                .build();
+
+        assertEquals(tuple.getLong(0), 42L);
+        assertEquals(tuple.getLong(1), 67L);
+        assertEquals(tuple.size(), 16);
+    }
+
+    @Test
+    public void testSingleVariableLength()
+    {
+        TupleInfo info = new TupleInfo(VARIABLE_BINARY);
+
+        Slice binary = Slices.wrappedBuffer(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+        Tuple tuple = info.builder()
+                .append(binary)
+                .build();
+
+        assertEquals(tuple.getSlice(0), binary);
+        assertEquals(tuple.size(), binary.length() + SIZE_OF_SHORT);
+    }
+
+    @Test
+    public void testMultipleVariableLength()
+    {
+        TupleInfo info = new TupleInfo(VARIABLE_BINARY, VARIABLE_BINARY);
 
         Slice binary1 = Slices.wrappedBuffer(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
         Slice binary2 = Slices.wrappedBuffer(new byte[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 });
 
-        slice.output()
-                .appendLong(42) // value of first long field
-                .appendLong(67) // value of second long field
-                .appendShort(30) // offset of second binary field
-                .appendShort(45) // tuple size
-                .appendBytes(binary1)
-                .appendBytes(binary2);
+        Tuple tuple = info.builder()
+                .append(binary1)
+                .append(binary2)
+                .build();
+
+        assertEquals(tuple.getSlice(0), binary1);
+        assertEquals(tuple.getSlice(1), binary2);
+        assertEquals(tuple.size(), binary1.length() + binary2.length() + SIZE_OF_SHORT + SIZE_OF_SHORT);
+    }
 
 
-        TupleInfo info = new TupleInfo(FIXED_INT_64, VARIABLE_BINARY, FIXED_INT_64, VARIABLE_BINARY);
-        assertEquals(info.getLong(slice, 0), 42L);
-        assertEquals(info.getSlice(slice, 1), binary1);
-        assertEquals(info.getLong(slice, 2), 67L);
-        assertEquals(info.getSlice(slice, 3), binary2);
+    @Test
+    public void testMixed()
+    {
+        TupleInfo info = new TupleInfo(FIXED_INT_64, VARIABLE_BINARY, FIXED_INT_64, VARIABLE_BINARY, FIXED_INT_64, VARIABLE_BINARY);
+
+        Slice binary1 = Slices.wrappedBuffer(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        Slice binary2 = Slices.wrappedBuffer(new byte[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 });
+        Slice binary3 = Slices.wrappedBuffer(new byte[] { 30, 31, 32, 33, 34, 35 });
+
+        Tuple tuple = info.builder()
+                .append(42)
+                .append(binary1)
+                .append(67)
+                .append(binary2)
+                .append(90)
+                .append(binary3)
+                .build();
+
+        assertEquals(tuple.getLong(0), 42L);
+        assertEquals(tuple.getSlice(1), binary1);
+        assertEquals(tuple.getLong(2), 67L);
+        assertEquals(tuple.getSlice(3), binary2);
+        assertEquals(tuple.getLong(4), 90L);
+        assertEquals(tuple.getSlice(5), binary3);
     }
 }
