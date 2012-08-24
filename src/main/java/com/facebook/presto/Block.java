8dc19c33c77168aba043dd61bb8b163882d82d09
Martin Traverso <martint@fb.com>
2012-08-24 12:58:39 -0700

    Add clarifying docs for Block interface

--- src/main/java/com/facebook/presto/Block.java
+++ src/main/java/com/facebook/presto/Block.java
@@ -6,8 +6,19 @@ public interface Block
 {
     int getCount();
 
+    /**
+     * Whether the values in this block are sorted
+     */
     boolean isSorted();
+
+    /**
+     * Whether the block contains a single value (with one or more positions for that value)
+     */
     boolean isSingleValue();
+
+    /**
+     * Whether the block contains gaps in its positions
+     */
     boolean isPositionsContiguous();
 
     Iterable<Long> getPositions();
