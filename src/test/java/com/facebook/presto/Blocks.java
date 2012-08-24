f0dda29056073fefd7ac90f885190a3bd2957dfd
Martin Traverso <martint@fb.com>
2012-08-24 11:03:23 -0700

    Extract createBlock into a separate util class

---
+++ src/test/java/com/facebook/presto/Blocks.java
@@ -0,0 +1,30 @@
+package com.facebook.presto;
+
+import static com.facebook.presto.TupleInfo.Type.FIXED_INT_64;
+import static com.facebook.presto.TupleInfo.Type.VARIABLE_BINARY;
+import static com.google.common.base.Charsets.UTF_8;
+
+public class Blocks
+{
+    public static ValueBlock createBlock(int position, String... values)
+    {
+        BlockBuilder builder = new BlockBuilder(position, new TupleInfo(VARIABLE_BINARY));
+
+        for (String value : values) {
+            builder.append(value.getBytes(UTF_8));
+        }
+
+        return builder.build();
+    }
+
+    public static ValueBlock createBlock(long position, long... values)
+    {
+        BlockBuilder builder = new BlockBuilder(position, new TupleInfo(FIXED_INT_64));
+
+        for (long value : values) {
+            builder.append(value);
+        }
+
+        return builder.build();
+    }
+}
