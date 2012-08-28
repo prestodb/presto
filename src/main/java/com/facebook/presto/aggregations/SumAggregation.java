8463b20e46138ce55e08e0656c5957b4af2af190
Dain Sundstrom <dain@iq80.com>
2012-08-27 23:18:26 -0700

    Add PipelinedAggregationBlockStream and benchmark

--- src/main/java/com/facebook/presto/aggregations/SumAggregation.java
+++ src/main/java/com/facebook/presto/aggregations/SumAggregation.java
@@ -1,14 +1,26 @@
 package com.facebook.presto.aggregations;
 
+import com.facebook.presto.Cursor;
 import com.facebook.presto.PositionBlock;
+import com.facebook.presto.Range;
 import com.facebook.presto.Tuple;
 import com.facebook.presto.TupleInfo;
 import com.facebook.presto.ValueBlock;
 import com.google.common.base.Optional;
 
+import javax.inject.Provider;
+
 public class SumAggregation
         implements AggregationFunction
 {
+    public static Provider<AggregationFunction> PROVIDER = new Provider<AggregationFunction>() {
+        @Override
+        public SumAggregation get()
+        {
+            return new SumAggregation();
+        }
+    };
+
     private long sum;
 
     @Override
@@ -26,6 +38,23 @@ public class SumAggregation
             for (Tuple value : filtered.get()) {
                 sum += value.getLong(0);
             }
+        }
+    }
+
+    @Override
+    public void add(Cursor cursor, Range relevantRange)
+    {
+        // todo if cursor is not "valid", advance to first position
+
+        // advance to start of range
+        // todo add seek method to cursor
+        while (cursor.getPosition() < relevantRange.getStart()) {
+            cursor.advanceNextPosition();
+        }
+
+        while (relevantRange.contains(cursor.getPosition())) {
+            sum += cursor.getLong(0);
+            cursor.advanceNextPosition();
         }
     }
 
