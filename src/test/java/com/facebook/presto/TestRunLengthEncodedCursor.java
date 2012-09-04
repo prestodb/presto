6c62c6a7712af2e059becad3e8aec57496fc496d
Martin Traverso <martint@fb.com>
2012-09-04 15:49:13 -0700

    Implement hasNextPosition/advanceNextPosition for RLE cursor

---
+++ src/test/java/com/facebook/presto/TestRunLengthEncodedCursor.java
@@ -0,0 +1,140 @@
+package com.facebook.presto;
+
+import com.facebook.presto.slice.Slices;
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableList;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class TestRunLengthEncodedCursor
+{
+    @Test
+    public void testFirstValue()
+            throws Exception
+    {
+        RunLengthEncodedCursor cursor = createCursor();
+        assertNextValue(cursor, 0, "apple");
+    }
+
+    @Test
+    public void testFirstPosition()
+            throws Exception
+    {
+        RunLengthEncodedCursor cursor = createCursor();
+        assertNextPosition(cursor, 0, "apple");
+    }
+
+
+    @Test
+    public void testAdvanceNextValue()
+            throws Exception
+    {
+        RunLengthEncodedCursor cursor = createCursor();
+
+        assertNextValue(cursor, 0, "apple");
+        assertNextValue(cursor, 5, "banana");
+        assertNextValue(cursor, 20, "cherry");
+        assertNextValue(cursor, 30, "date");
+
+        assertFalse(cursor.hasNextValue());
+    }
+
+    @Test
+    public void testAdvanceNextPosition()
+    {
+        RunLengthEncodedCursor cursor = createCursor();
+
+        assertNextPosition(cursor, 0, "apple");
+        assertNextPosition(cursor, 1, "apple");
+        assertNextPosition(cursor, 2, "apple");
+        assertNextPosition(cursor, 3, "apple");
+        assertNextPosition(cursor, 4, "apple");
+        assertNextPosition(cursor, 5, "banana");
+        assertNextPosition(cursor, 6, "banana");
+        assertNextPosition(cursor, 7, "banana");
+        assertNextPosition(cursor, 20, "cherry");
+        assertNextPosition(cursor, 21, "cherry");
+        assertNextPosition(cursor, 30, "date");
+
+        assertFalse(cursor.hasNextPosition());
+    }
+
+    @Test
+    public void testAdvanceToNextValueAdvancesPosition()
+            throws Exception
+    {
+        RunLengthEncodedCursor cursor = createCursor();
+
+        // first, skip to middle of a block
+        assertNextValue(cursor, 0, "apple");
+        assertNextPosition(cursor, 1, "apple");
+
+        // force jump to next block
+        assertNextValue(cursor, 5, "banana");
+    }
+
+    @Test
+    public void testAdvanceToNextPositionAdvancesValue()
+    {
+        RunLengthEncodedCursor cursor = createCursor();
+
+        // first, advance to end of first block
+        assertNextPosition(cursor, 0, "apple");
+        assertNextPosition(cursor, 1, "apple");
+        assertNextPosition(cursor, 2, "apple");
+        assertNextPosition(cursor, 3, "apple");
+        assertNextPosition(cursor, 4, "apple");
+
+        // force jump to next block
+        assertNextPosition(cursor, 5, "banana");
+    }
+
+    private RunLengthEncodedCursor createCursor()
+    {
+        TupleInfo info = new TupleInfo(TupleInfo.Type.VARIABLE_BINARY);
+
+        List<RunLengthEncodedBlock> blocks = ImmutableList.of(
+                new RunLengthEncodedBlock(Tuples.createTuple("apple"), Range.create(0, 4)),
+                new RunLengthEncodedBlock(Tuples.createTuple("banana"), Range.create(5, 7)),
+                new RunLengthEncodedBlock(Tuples.createTuple("cherry"), Range.create(20, 21)),
+                new RunLengthEncodedBlock(Tuples.createTuple("date"), Range.create(30, 30)));
+
+        return new RunLengthEncodedCursor(info, blocks.iterator());
+    }
+
+    private static void assertNextValue(Cursor cursor, long position, String value)
+    {
+        TupleInfo info = new TupleInfo(TupleInfo.Type.VARIABLE_BINARY);
+
+        Tuple tuple = info.builder()
+                .append(Slices.wrappedBuffer(value.getBytes(Charsets.UTF_8)))
+                .build();
+
+        assertTrue(cursor.hasNextValue());
+        cursor.advanceNextValue();
+
+        assertEquals(cursor.getTuple(), tuple);
+        assertEquals(cursor.getPosition(), position);
+    }
+
+    private static void assertNextPosition(Cursor cursor, long position, String value)
+    {
+        TupleInfo info = new TupleInfo(TupleInfo.Type.VARIABLE_BINARY);
+
+        Tuple tuple = info.builder()
+                .append(Slices.wrappedBuffer(value.getBytes(Charsets.UTF_8)))
+                .build();
+
+        assertTrue(cursor.hasNextPosition());
+        cursor.advanceNextPosition();
+
+        assertEquals(cursor.getTuple(), tuple);
+        assertEquals(cursor.getPosition(), position);
+    }
+
+}
