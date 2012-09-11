da283fa918620996d58ca697e4f18c4a87e0f510
Dain Sundstrom <dain@iq80.com>
2012-09-10 18:08:51 -0700

    Add position blocks, masked value block and data scan 3

--- src/test/java/com/facebook/presto/TestExample.java
+++ src/test/java/com/facebook/presto/TestExample.java
@@ -1,41 +1,54 @@
 package com.facebook.presto;
 
+import com.facebook.presto.operators.DataScan3;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+import static com.facebook.presto.TupleInfo.SINGLE_LONG_TUPLEINFO;
+import static com.google.common.base.Charsets.UTF_8;
+
 public class TestExample
 {
-//    public static void main(String[] args)
-//    {
-//        DataScan3 scan = newScan();
+    public static void main(String[] args)
+    {
+        DataScan3 scan = newScan();
 //        DataScan3 scan2 = newScan();
-//
+
 //        Merge merge = new Merge(ImmutableList.of(scan, scan2), new TupleInfo(FIXED_INT_64, FIXED_INT_64));
-//
-//        while (merge.hasNext()) {
-//            ValueBlock block = merge.next();
-//            for (Object value : block) {
-//                System.out.println(value);
-//            }
-//            System.out.println();
-//        }
-//    }
-//
-//    private static DataScan3 newScan()
-//    {
-//        Iterator<ValueBlock> values = ImmutableList.<ValueBlock>builder()
-//                .add(new UncompressedValueBlock(Range.create(0L, 5L), new TupleInfo(FIXED_INT_64), Slices.wrappedBuffer(new byte[] { 'a', 'b', 'c', 'd', 'e', 'f' })))
-//                .add(new UncompressedValueBlock(Range.create(20L, 25L), new TupleInfo(FIXED_INT_64), Slices.wrappedBuffer(new byte[]{'h', 'i', 'j', 'k', 'l', 'm'})))
-//                .add(new UncompressedValueBlock(Range.create(30L, 35L), new TupleInfo(FIXED_INT_64), Slices.wrappedBuffer(new byte[]{'n', 'o', 'p', 'q', 'r', 's'})))
-//                .build()
-//                .iterator();
-//
-//        Iterator<PositionBlock> positions = ImmutableList.<PositionBlock>builder()
-//                .add(new UncompressedPositionBlock(asList(2L, 4L, 6L, 8L)))
-//                .add(new UncompressedPositionBlock(asList(10L, 11L, 12L, 13L)))
-//                .add(new UncompressedPositionBlock(asList(18L, 19L, 20L, 21L, 22L)))
-//                .add(new UncompressedPositionBlock(asList(31L, 33L, 35L)))
-//                .add(new UncompressedPositionBlock(asList(40L, 41L, 42L)))
-//                .build()
-//                .iterator();
-//
-//        return new DataScan3(values, positions);
-//    }
+
+        // 2 c
+        //  4 e
+        // 20 h
+        // 21 i
+        // 22 j
+        // 31 o
+        // 33 q
+        // 35 s
+
+        Cursor cursor = scan.cursor();
+        while (cursor.hasNextPosition()) {
+            cursor.advanceNextPosition();
+            System.out.printf("%d %s\n", cursor.getPosition(), cursor.getSlice(0).toString(UTF_8));
+        }
+    }
+
+    private static DataScan3 newScan()
+    {
+        List<UncompressedValueBlock> values = ImmutableList.<UncompressedValueBlock>builder()
+                .add(Blocks.createBlock(0, "a", "b", "c", "d", "e", "f"))
+                .add(Blocks.createBlock(20, "h", "i", "j", "k", "l", "m"))
+                .add(Blocks.createBlock(30, "n", "o", "p", "q", "r", "s"))
+                .build();
+
+        List<UncompressedPositionBlock> positions = ImmutableList.<UncompressedPositionBlock>builder()
+                .add(new UncompressedPositionBlock(2L, 4L, 6L, 8L))
+                .add(new UncompressedPositionBlock(10L, 11L, 12L, 13L))
+                .add(new UncompressedPositionBlock(18L, 19L, 20L, 21L, 22L))
+                .add(new UncompressedPositionBlock(31L, 33L, 35L))
+                .add(new UncompressedPositionBlock(40L, 41L, 42L))
+                .build();
+
+        return new DataScan3(new UncompressedBlockStream(SINGLE_LONG_TUPLEINFO, values), new ValueBlockStream<>(new TupleInfo(), positions));
+    }
 }
