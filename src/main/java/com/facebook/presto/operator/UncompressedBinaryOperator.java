c3f0ad156405644d4010805b9a1ac0d5b4595491
Martin Traverso <martint@fb.com>
2012-09-14 11:17:54 -0700

    Initial take on binary operator

---
+++ src/main/java/com/facebook/presto/operator/UncompressedBinaryOperator.java
@@ -0,0 +1,86 @@
+package com.facebook.presto.operator;
+
+import com.facebook.presto.TupleInfo;
+import com.facebook.presto.block.BlockBuilder;
+import com.facebook.presto.block.BlockStream;
+import com.facebook.presto.block.Cursor;
+import com.facebook.presto.block.uncompressed.UncompressedBlock;
+import com.facebook.presto.block.uncompressed.UncompressedCursor;
+import com.facebook.presto.operation.BinaryOperation;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.AbstractIterator;
+
+import java.util.Iterator;
+
+/**
+ * A binary operator that produces uncompressed blocks
+ */
+public class UncompressedBinaryOperator
+        implements BlockStream, Iterable<UncompressedBlock>
+{
+    private final BlockStream leftOperandSource;
+    private final BlockStream rightOperandSource;
+    private final BinaryOperation operation;
+
+    public UncompressedBinaryOperator(BlockStream leftOperandSource, BlockStream rightOperandSource, BinaryOperation operation)
+    {
+        this.leftOperandSource = leftOperandSource;
+        this.rightOperandSource = rightOperandSource;
+        this.operation = operation;
+    }
+
+    @Override
+    public TupleInfo getTupleInfo()
+    {
+        return operation.getTupleInfo();
+    }
+
+    @Override
+    public Cursor cursor()
+    {
+        return new UncompressedCursor(getTupleInfo(), iterator());
+    }
+
+    @Override
+    public Iterator<UncompressedBlock> iterator()
+    {
+        final Cursor left = leftOperandSource.cursor();
+        final Cursor right = rightOperandSource.cursor();
+
+        boolean advancedLeft = left.advanceNextPosition();
+        boolean advancedRight = right.advanceNextPosition();
+
+        Preconditions.checkState(advancedLeft && advancedRight, "Empty source cursor"); // TODO: we should be able to support this scenario
+
+        return new AbstractIterator<UncompressedBlock>()
+        {
+            private boolean done = false;
+
+            @Override
+            protected UncompressedBlock computeNext()
+            {
+                if (done) {
+                    endOfData();
+                    return null;
+                }
+
+                BlockBuilder builder = new BlockBuilder(left.getPosition(), getTupleInfo());
+                do {
+                    long endPosition = Math.min(left.getCurrentValueEndPosition(), right.getCurrentValueEndPosition());
+
+                    // TODO: arbitrary types
+                    long value = operation.evaluateAsLong(left, right);
+
+                    while (!done && left.getPosition() <= endPosition && !builder.isFull()) {
+                        builder.append(value);
+
+                        done = !left.advanceNextPosition() || !right.advanceNextPosition();
+                    }
+                }
+                while (!done && !builder.isFull());
+
+                return builder.build();
+            }
+        };
+    }
+}
