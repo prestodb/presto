34a9762745f1fa9beb3dab54a5a6247c93992d17
Martin Traverso <martint@fb.com>
2012-09-17 12:12:07 -0700

    Fix query so that all predicates affect the output

--- src/test/java/com/facebook/presto/TestQueries.java
+++ src/test/java/com/facebook/presto/TestQueries.java
@@ -241,7 +241,7 @@ public class TestQueries
     @Test
     public void testCountWithAndPredicate()
     {
-        List<Tuple> expected = computeExpected("SELECT COUNT(*) FROM lineitem WHERE tax < discount AND tax > 0.01 AND discount < 10.0", FIXED_INT_64);
+        List<Tuple> expected = computeExpected("SELECT COUNT(*) FROM lineitem WHERE tax < discount AND tax > 0.01 AND discount < 0.05", FIXED_INT_64);
 
         TupleStream discount = createTupleStream(lineitemData, Column.LINEITEM_DISCOUNT, DOUBLE);
         TupleStream filteredDiscount = new ApplyPredicateOperator(discount, new Predicate<Cursor>()
@@ -249,7 +249,7 @@ public class TestQueries
             @Override
             public boolean apply(Cursor input)
             {
-                return input.getDouble(0) < 10.0;
+                return input.getDouble(0) < 0.05;
             }
         });
 
