a35c7a264c535b300393c1fe10d13aac806429e0
Dain Sundstrom <dain@iq80.com>
2012-09-18 12:34:37 -0700

    Update benchmarks to new serde code

--- src/test/java/com/facebook/presto/benchmark/BenchmarkGroupBy.java
+++ src/test/java/com/facebook/presto/benchmark/BenchmarkGroupBy.java
@@ -1,8 +1,10 @@
 package com.facebook.presto.benchmark;
 
-import com.facebook.presto.block.TupleStream;
 import com.facebook.presto.block.Cursor;
-import com.facebook.presto.block.uncompressed.UncompressedSerde;
+import com.facebook.presto.block.TupleStream;
+import com.facebook.presto.block.TupleStreamSerde;
+import com.facebook.presto.block.dictionary.DictionarySerde;
+import com.facebook.presto.block.rle.RunLengthEncodedSerde;
 import com.facebook.presto.operator.GroupByOperator;
 import com.facebook.presto.slice.Slice;
 import com.facebook.presto.slice.Slices;
@@ -18,19 +20,36 @@ public class BenchmarkGroupBy
     public static void main(String[] args)
             throws IOException, InterruptedException
     {
-        File file = new File("data/column5/column0.data");  // sorted
-//        File file = new File("data/columns/column5.data");  // not-sorted
+//        // raw not-sorted
+//        File groupByFile = new File("data/raw/column5.string_raw.data");
+//        TupleStreamSerde groupBySerde = UncompressedSerde.INSTANCE;
 
-        Slice pageTypeColumnSlice = Slices.mapFileReadOnly(file);
+//        // dictionary-raw not-sorted
+//        File groupByFile = new File("data/dic-raw/column5.string_dic-raw.data");
+//        TupleStreamSerde groupBySerde = new DictionarySerde(UncompressedSerde.INSTANCE);
+
+//        // raw sorted
+//        File groupByFile = new File("data/sorted/column5.string_raw.data");
+//        TupleStreamSerde groupBySerde = UncompressedSerde.INSTANCE;
+
+//        // rle
+//        File groupByFile = new File("data/rle/column5.string_rle.data");
+//        TupleStreamSerde groupBySerde = new RunLengthEncodedSerde();
+
+        // dictionary-rle
+        File groupByFile = new File("data/dic-rle/column5.string_dic-rle.data");
+        TupleStreamSerde groupBySerde = new DictionarySerde(new RunLengthEncodedSerde());
+
+        Slice pageTypeColumnSlice = Slices.mapFileReadOnly(groupByFile);
         for (int i = 0; i < 100000; ++i) {
-            TupleStream pageTypeColumn = UncompressedSerde.readAsStream(pageTypeColumnSlice);
+            TupleStream pageTypeColumn = groupBySerde.deserialize(pageTypeColumnSlice);
             GroupByOperator groupBy = new GroupByOperator(pageTypeColumn) ;
 
             Result result = doIt(groupBy);
             long count = result.count;
             Duration duration = result.duration;
 
-            DataSize fileSize = new DataSize(file.length(), DataSize.Unit.BYTE);
+            DataSize fileSize = new DataSize(groupByFile.length(), DataSize.Unit.BYTE);
 
             System.out.println(String.format("%s, %s, %.2f/s, %2.2f MB/s", duration, count, count / duration.toMillis() * 1000, fileSize.getValue(DataSize.Unit.MEGABYTE) / duration.convertTo(TimeUnit.SECONDS)));
         }
