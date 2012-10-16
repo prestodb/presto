288ffe385c0ecc26deb50d8b9adf016d60557fef
Martin Traverso <martint@fb.com>
2012-10-16 16:46:31 -0700

    AST rewriter framework

---
+++ src/main/java/com/facebook/presto/sql/compiler/IterableUtils.java
@@ -0,0 +1,27 @@
+package com.facebook.presto.sql.compiler;
+
+import com.google.common.collect.Iterables;
+
+import java.util.Iterator;
+
+public class IterableUtils
+{
+    public static <T> boolean sameElements(Iterable<? extends T> a, Iterable<? extends T> b)
+    {
+        if (Iterables.size(a) != Iterables.size(b)) {
+            return false;
+        }
+
+        Iterator<? extends T> first = a.iterator();
+        Iterator<? extends T> second = b.iterator();
+
+        while (first.hasNext() && second.hasNext()) {
+            if (first.next() != second.next()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
