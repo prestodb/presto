0048c1373672be41fe475ca19bb04d94fc1a0d3a
ehwang <ehwang@fb.com>
2012-10-16 11:24:34 -0700

    Improve MergeOperator to not materialize data
    Summary:
    Without a materialization layer, this instantly becomes superscalar.

--- presto-main/src/main/java/com/facebook/presto/block/ColumnMappingTupleStream.java
+++ presto-main/src/main/java/com/facebook/presto/block/ColumnMappingTupleStream.java
@@ -1,10 +1,7 @@
 package com.facebook.presto.block;
 
 import com.facebook.presto.Range;
-import com.facebook.presto.Tuple;
 import com.facebook.presto.TupleInfo;
-import com.facebook.presto.slice.Slice;
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.primitives.Ints;
 
@@ -46,6 +43,8 @@ public class ColumnMappingTupleStream
 
     public static ColumnMappingTupleStream map(TupleStream blockStream, Integer... columns)
     {
+        checkNotNull(blockStream, "blockStream is null");
+        checkNotNull(columns, "columns is null");
         return new ColumnMappingTupleStream(blockStream, Arrays.asList(columns));
     }
 
@@ -64,93 +63,7 @@ public class ColumnMappingTupleStream
     @Override
     public Cursor cursor(QuerySession session)
     {
-        Preconditions.checkNotNull(session, "session is null");
-        return new ForwardingCursor(delegate.cursor(session))
-        {
-            @Override
-            public TupleInfo getTupleInfo()
-            {
-                return tupleInfo;
-            }
-
-            @Override
-            public Tuple getTuple()
-            {
-                // Materializes the column mapped tuple... SLOW!
-                Tuple originalTuple = getDelegate().getTuple();
-                TupleInfo.Builder builder = tupleInfo.builder();
-                for (int column : selectedColumns) {
-                    switch (originalTuple.getTupleInfo().getTypes().get(column)) {
-                        case FIXED_INT_64:
-                            builder.append(originalTuple.getLong(column));
-                            break;
-                        case DOUBLE:
-                            builder.append(originalTuple.getDouble(column));
-                            break;
-                        case VARIABLE_BINARY:
-                            builder.append(originalTuple.getSlice(column));
-                            break;
-                        default:
-                            throw new IllegalArgumentException("Unknown type " + tupleInfo.getTypes().get(column));
-                    }
-                }
-                return builder.build();
-            }
-
-            @Override
-            public long getLong(int field)
-            {
-                return getDelegate().getLong(remapColumnIndex(field));
-            }
-
-            @Override
-            public double getDouble(int field)
-            {
-                return getDelegate().getDouble(remapColumnIndex(field));
-            }
-
-            @Override
-            public Slice getSlice(int field)
-            {
-                return getDelegate().getSlice(remapColumnIndex(field));
-            }
-
-            @Override
-            public boolean currentTupleEquals(Tuple value)
-            {
-                checkNotNull(value, "value is null");
-                if (!tupleInfo.equals(value.getTupleInfo())) {
-                    return false;
-                }
-                for (int index = 0; index < tupleInfo.getFieldCount(); index++) {
-                    switch (tupleInfo.getTypes().get(index)) {
-                        case FIXED_INT_64:
-                            if (getDelegate().getLong(remapColumnIndex(index)) != value.getLong(index)) {
-                                return false;
-                            }
-                            break;
-                        case DOUBLE:
-                            if (getDelegate().getDouble(remapColumnIndex(index)) != value.getDouble(index)) {
-                                return false;
-                            }
-                            break;
-                        case VARIABLE_BINARY:
-                            if (!getDelegate().getSlice(remapColumnIndex(index) ).equals(value.getSlice(index))) {
-                                return false;
-                            }
-                            break;
-                        default:
-                            throw new IllegalArgumentException("Unknown type " + tupleInfo.getTypes().get(index));
-                    }
-                }
-                return true;
-            }
-
-            private int remapColumnIndex(int column)
-            {
-                checkPositionIndex(column, selectedColumns.length, "invalid column index");
-                return selectedColumns[column];
-            }
-        };
-}
+        checkNotNull(session, "session is null");
+        return new ColumnMappingCursor(delegate.cursor(session), selectedColumns);
+    }
 }
