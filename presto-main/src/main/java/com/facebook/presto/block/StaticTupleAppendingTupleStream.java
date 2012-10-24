7f4a58a8f933adfef1694f39dcf0c49be28abd93
Eric Hwang <ehwang@fb.com>
2012-10-23 18:40:13 -0700

    Add StaticTupleAppendingTupleStream

---
+++ presto-main/src/main/java/com/facebook/presto/block/StaticTupleAppendingTupleStream.java
@@ -0,0 +1,126 @@
+package com.facebook.presto.block;
+
+import com.facebook.presto.Range;
+import com.facebook.presto.Tuple;
+import com.facebook.presto.TupleInfo;
+import com.facebook.presto.slice.Slice;
+import com.google.common.collect.ImmutableList;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Appends a statically defined Tuple to the end of every Tuple produced by the underlying TupleStream
+ */
+public class StaticTupleAppendingTupleStream
+        implements TupleStream
+{
+    private final TupleStream delegate;
+    private final Tuple tuple;
+    private final TupleInfo tupleInfo;
+
+    public StaticTupleAppendingTupleStream(TupleStream delegate, Tuple tuple)
+    {
+        this.delegate = checkNotNull(delegate, "delegate is null");
+        this.tuple = checkNotNull(tuple, "tuple is null");
+        tupleInfo = new TupleInfo(
+                ImmutableList.<TupleInfo.Type>builder()
+                        .addAll(delegate.getTupleInfo().getTypes())
+                        .addAll(tuple.getTupleInfo().getTypes())
+                        .build()
+        );
+    }
+
+    @Override
+    public TupleInfo getTupleInfo()
+    {
+        return tupleInfo;
+    }
+
+    @Override
+    public Range getRange()
+    {
+        return delegate.getRange();
+    }
+
+    @Override
+    public Cursor cursor(QuerySession session)
+    {
+        checkNotNull(session, "session is null");
+        return new StaticTupleAppendingCursor(delegate.cursor(session), tuple);
+    }
+
+    private static class StaticTupleAppendingCursor
+            extends ForwardingCursor
+    {
+        private final TupleInfo tupleInfo;
+        private final Tuple tuple;
+
+        private StaticTupleAppendingCursor(Cursor cursor, Tuple tuple)
+        {
+            super(checkNotNull(cursor, "cursor is null"));
+            this.tuple = checkNotNull(tuple, "tuple is null");
+            tupleInfo = new TupleInfo(
+                    ImmutableList.<TupleInfo.Type>builder()
+                            .addAll(cursor.getTupleInfo().getTypes())
+                            .addAll(tuple.getTupleInfo().getTypes())
+                            .build()
+            );
+        }
+
+        @Override
+        public TupleInfo getTupleInfo()
+        {
+            return tupleInfo;
+        }
+
+        @Override
+        public Tuple getTuple()
+        {
+            Cursors.checkReadablePosition(this);
+            TupleInfo.Builder builder = tupleInfo.builder();
+            Cursors.appendCurrentTupleToTupleBuilder(this, builder);
+            return builder.build();
+        }
+
+        @Override
+        public long getLong(int field)
+        {
+            Cursors.checkReadablePosition(this);
+            int delegateFieldCount = getDelegate().getTupleInfo().getFieldCount();
+            if (field < delegateFieldCount) {
+                return getDelegate().getLong(field);
+            }
+            return tuple.getLong(field - delegateFieldCount);
+        }
+
+        @Override
+        public double getDouble(int field)
+        {
+            Cursors.checkReadablePosition(this);
+            int delegateFieldCount = getDelegate().getTupleInfo().getFieldCount();
+            if (field < delegateFieldCount) {
+                return getDelegate().getDouble(field);
+            }
+            return tuple.getDouble(field - delegateFieldCount);
+        }
+
+        @Override
+        public Slice getSlice(int field)
+        {
+            Cursors.checkReadablePosition(this);
+            int delegateFieldCount = getDelegate().getTupleInfo().getFieldCount();
+            if (field < delegateFieldCount) {
+                return getDelegate().getSlice(field);
+            }
+            return tuple.getSlice(field - delegateFieldCount);
+        }
+
+        @Override
+        public boolean currentTupleEquals(Tuple value)
+        {
+            checkNotNull(value, "value is null");
+            Cursors.checkReadablePosition(this);
+            return Cursors.currentTupleEquals(this, value);
+        }
+    }
+}
