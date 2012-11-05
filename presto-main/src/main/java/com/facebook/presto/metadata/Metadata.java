d5e20a88407c5b99fba4377db4d6d5dfb1347490
Martin Traverso <martint@fb.com>
2012-11-05 13:24:23 -0800

    Match signature when looking up functions

--- presto-main/src/main/java/com/facebook/presto/metadata/Metadata.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/Metadata.java
@@ -1,8 +1,13 @@
 package com.facebook.presto.metadata;
 
+import com.facebook.presto.sql.tree.QualifiedName;
+import com.facebook.presto.tuple.TupleInfo;
+
+import java.util.List;
+
 public interface Metadata
 {
-    FunctionInfo getFunction(String name);
+    FunctionInfo getFunction(QualifiedName name, List<TupleInfo.Type> parameterTypes);
 
     TableMetadata getTable(String catalogName, String schemaName, String tableName);
 
