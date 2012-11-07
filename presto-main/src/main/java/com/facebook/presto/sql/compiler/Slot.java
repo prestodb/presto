8b3daa4541ad9028173ead232acba54b12ff0b3a
Martin Traverso <martint@fb.com>
2012-11-06 16:01:19 -0800

    Implement equals and hashCode for Slot and add preconditions

--- presto-main/src/main/java/com/facebook/presto/sql/compiler/Slot.java
+++ presto-main/src/main/java/com/facebook/presto/sql/compiler/Slot.java
@@ -1,5 +1,10 @@
 package com.facebook.presto.sql.compiler;
 
+import com.google.common.base.Preconditions;
+
+/**
+ * A slot is a unique identifier for a source of data. E.g., a column in a table or an attribute in a subquery
+ */
 public class Slot
 {
     private final int id;
@@ -7,6 +12,9 @@ public class Slot
 
     public Slot(int id, Type type)
     {
+        Preconditions.checkArgument(id >= 0, "Id must be positive");
+        Preconditions.checkNotNull(type, "type is null");
+
         this.id = id;
         this.type = type;
     }
@@ -25,5 +33,35 @@ public class Slot
     public String toString()
     {
         return getName();
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Slot slot = (Slot) o;
+
+        if (id != slot.id) {
+            return false;
+        }
+        if (type != slot.type) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = id;
+        result = 31 * result + type.hashCode();
+        return result;
     }
 }
