5deeca7a3d4359f141ab1ee572ddfda9f0238c24
Dain Sundstrom <dain@iq80.com>
2012-11-10 09:19:33 -0800

    New HashJoinOperator implementation using flat data structures

---
+++ presto-main/src/test/java/com/facebook/presto/operator/TestHashJoinOperator.java
@@ -0,0 +1,37 @@
+package com.facebook.presto.operator;
+
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.block.BlockAssertions.createLongSequenceBlock;
+import static com.facebook.presto.block.BlockAssertions.createStringSequenceBlock;
+import static com.facebook.presto.operator.OperatorAssertions.assertOperatorEquals;
+import static com.facebook.presto.operator.OperatorAssertions.createOperator;
+
+public class TestHashJoinOperator
+{
+    @Test
+    public void testJoin()
+            throws Exception
+    {
+        Operator buildSource = createOperator(new Page(
+                createStringSequenceBlock(20, 30),
+                createLongSequenceBlock(30, 40),
+                createLongSequenceBlock(40, 50)));
+
+        Operator probeSource = createOperator(new Page(
+                createStringSequenceBlock(0, 1000),
+                createLongSequenceBlock(1000, 2000),
+                createLongSequenceBlock(2000, 3000)));
+
+        HashJoinOperator joinOperator = new HashJoinOperator(buildSource, 0, probeSource, 0);
+
+        Operator expected = createOperator(new Page(
+                createStringSequenceBlock(20, 30),
+                createLongSequenceBlock(1020, 1030),
+                createLongSequenceBlock(2020, 2030),
+                createLongSequenceBlock(30, 40),
+                createLongSequenceBlock(40, 50)));
+
+        assertOperatorEquals(joinOperator, expected);
+    }
+}
