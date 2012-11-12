39bf197b9a80ec6e9fd1c4589bbb3fa1359006ea
David Phillips <david@acz.org>
2012-11-11 23:34:17 -0800

    Remove unused function

--- presto-main/src/main/java/com/facebook/presto/cli/Utils.java
+++ presto-main/src/main/java/com/facebook/presto/cli/Utils.java
@@ -1,30 +1,11 @@
 package com.facebook.presto.cli;
 
-import com.facebook.presto.block.BlockIterable;
-import com.facebook.presto.metadata.ColumnMetadata;
-import com.facebook.presto.metadata.Metadata;
-import com.facebook.presto.metadata.StorageManager;
-import com.facebook.presto.metadata.TableMetadata;
 import com.facebook.presto.operator.ConsolePrinter;
 import com.facebook.presto.operator.Operator;
 import io.airlift.units.Duration;
 
 public class Utils
 {
-    public static BlockIterable getColumn(StorageManager storageManager, Metadata metadata, final String tableName, String columnName)
-    {
-        TableMetadata tableMetadata = metadata.getTable("default", "default", tableName);
-        int index = 0;
-        for (ColumnMetadata columnMetadata : tableMetadata.getColumns()) {
-            if (columnName.equals(columnMetadata.getName())) {
-                break;
-            }
-            ++index;
-        }
-        final int columnIndex = index;
-        return storageManager.getBlocks("default", tableName, columnIndex);
-    }
-
     public static void printResults(long start, Operator aggregation)
     {
         long rows = ConsolePrinter.print(aggregation);
