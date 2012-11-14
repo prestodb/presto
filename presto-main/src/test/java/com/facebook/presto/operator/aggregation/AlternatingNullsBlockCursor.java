c3de563cec0cc471db49c19d14a897467db85d0e
Dain Sundstrom <dain@iq80.com>
2012-11-14 14:50:57 -0800

    Convert HashJoin to use PageBuilder
    Add cursor.appendTupleTo(BlockBuilder)

--- presto-main/src/test/java/com/facebook/presto/operator/aggregation/AlternatingNullsBlockCursor.java
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/AlternatingNullsBlockCursor.java
@@ -1,6 +1,7 @@
 package com.facebook.presto.operator.aggregation;
 
 import com.facebook.presto.block.Block;
+import com.facebook.presto.block.BlockBuilder;
 import com.facebook.presto.block.BlockCursor;
 import com.facebook.presto.slice.Slice;
 import com.facebook.presto.slice.Slices;
@@ -133,5 +134,14 @@ public class AlternatingNullsBlockCursor
     public int getRawOffset()
     {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void appendTupleTo(BlockBuilder blockBuilder)
+    {
+        if (isNullPosition()) {
+            blockBuilder.append(nullTuple);
+        }
+        delegate.appendTupleTo(blockBuilder);
     }
 }
