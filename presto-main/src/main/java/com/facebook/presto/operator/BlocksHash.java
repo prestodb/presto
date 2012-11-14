41e2a1ac07ae46110e5a1ec5efab4b785f33a38a
Dain Sundstrom <dain@iq80.com>
2012-11-14 15:13:48 -0800

    Extract hash from HashJoinOperator into BlocksHash

---
+++ presto-main/src/main/java/com/facebook/presto/operator/BlocksHash.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ */
+package com.facebook.presto.operator;
+
+import com.facebook.presto.block.BlockCursor;
+import com.facebook.presto.slice.Slice;
+import com.facebook.presto.tuple.TupleInfo;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.longs.Long2IntOpenCustomHashMap;
+import it.unimi.dsi.fastutil.longs.LongHash.Strategy;
+
+import java.util.Arrays;
+
+public class BlocksHash
+{
+    private final SliceHashStrategy hashStrategy;
+    private final Long2IntOpenCustomHashMap joinChannelHash;
+
+    private final IntArrayList positionLinks;
+
+    public BlocksHash(BlocksIndex joinChannelIndex)
+    {
+        hashStrategy = new SliceHashStrategy(joinChannelIndex.getTupleInfo(), joinChannelIndex.getSlices().elements());
+        joinChannelHash = new Long2IntOpenCustomHashMap(joinChannelIndex.getPositionCount(), hashStrategy);
+        joinChannelHash.defaultReturnValue(-1);
+        positionLinks = new IntArrayList(new int[joinChannelIndex.getOffsets().size()]);
+        Arrays.fill(positionLinks.elements(), -1);
+        for (int position = 0; position < joinChannelIndex.getOffsets().size(); position++) {
+            long sliceAddress = joinChannelIndex.getOffsets().elements()[position];
+            int oldPosition = joinChannelHash.put(sliceAddress, position);
+            if (oldPosition >= 0) {
+                // link the new position to the old position
+                positionLinks.set(position, oldPosition);
+            }
+        }
+    }
+
+    public void setProbeSlice(Slice slice)
+    {
+        hashStrategy.setProbeSlice(slice);
+    }
+
+    public int get(BlockCursor cursor)
+    {
+        int joinPosition = joinChannelHash.get(0xFF_FF_FF_FF_00_00_00_00L | cursor.getRawOffset());
+        return joinPosition;
+    }
+
+    public int getNextPosition(int currentPosition)
+    {
+        return positionLinks.getInt(currentPosition);
+    }
+
+    public static class SliceHashStrategy
+            implements Strategy
+    {
+        private final TupleInfo tupleInfo;
+        private final Slice[] slices;
+        private Slice probeSlice;
+
+        public SliceHashStrategy(TupleInfo tupleInfo, Slice[] slices)
+        {
+            this.tupleInfo = tupleInfo;
+            this.slices = slices;
+        }
+
+        public void setProbeSlice(Slice probeSlice)
+        {
+            this.probeSlice = probeSlice;
+        }
+
+        @Override
+        public int hashCode(long sliceAddress)
+        {
+            Slice slice = getSlice(sliceAddress);
+            int offset = (int) sliceAddress;
+            int length = tupleInfo.size(slice, offset);
+            int hashCode = slice.hashCode(offset, length);
+            return hashCode;
+        }
+
+        @Override
+        public boolean equals(long leftSliceAddress, long rightSliceAddress)
+        {
+            Slice leftSlice = getSlice(leftSliceAddress);
+            int leftOffset = (int) leftSliceAddress;
+            int leftLength = tupleInfo.size(leftSlice, leftOffset);
+
+            Slice rightSlice = getSlice(rightSliceAddress);
+            int rightOffset = (int) rightSliceAddress;
+            int rightLength = tupleInfo.size(rightSlice, rightOffset);
+
+            return leftSlice.equals(leftOffset, leftLength, rightSlice, rightOffset, rightLength);
+
+        }
+
+        private Slice getSlice(long sliceAddress)
+        {
+            int sliceIndex = (int) (sliceAddress >> 32);
+            Slice slice;
+            if (sliceIndex == 0xFF_FF_FF_FF) {
+                slice = probeSlice;
+            }
+            else {
+                slice = slices[sliceIndex];
+            }
+            return slice;
+        }
+    }
+}
--- presto-main/src/main/java/com/facebook/presto/operator/HashJoinOperator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/HashJoinOperator.java
@@ -3,16 +3,11 @@ package com.facebook.presto.operator;
 import com.facebook.presto.block.Block;
 import com.facebook.presto.block.BlockCursor;
 import com.facebook.presto.block.uncompressed.UncompressedBlock;
-import com.facebook.presto.slice.Slice;
 import com.facebook.presto.tuple.TupleInfo;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.ImmutableList;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.longs.Long2IntOpenCustomHashMap;
-import it.unimi.dsi.fastutil.longs.LongHash.Strategy;
 
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -78,11 +73,9 @@ public class HashJoinOperator
         private final int probeJoinChannel;
 
         private final PagesIndex buildIndex;
-        private final SliceHashStrategy hashStrategy;
-        private final Long2IntOpenCustomHashMap joinChannelHash;
+        private final BlocksHash hash;
 
         private final int buildJoinChannel;
-        private final IntArrayList positionLinks;
 
         private HashJoinIterator(List<TupleInfo> tupleInfos, Operator buildSource, int buildJoinChannel, Operator probeSource, int probeJoinChannel, int expectedPositions)
         {
@@ -93,22 +86,7 @@ public class HashJoinOperator
 
             // index build channel
             buildIndex = new PagesIndex(buildSource, expectedPositions);
-
-            // build hash over build join channel
-            BlocksIndex joinChannelIndex = buildIndex.getIndex(buildJoinChannel);
-            hashStrategy = new SliceHashStrategy(joinChannelIndex.getTupleInfo(), joinChannelIndex.getSlices().elements());
-            joinChannelHash = new Long2IntOpenCustomHashMap(expectedPositions, hashStrategy);
-            joinChannelHash.defaultReturnValue(-1);
-            positionLinks = new IntArrayList(new int[joinChannelIndex.getOffsets().size()]);
-            Arrays.fill(positionLinks.elements(), -1);
-            for (int position = 0; position < joinChannelIndex.getOffsets().size(); position++) {
-                long sliceAddress = joinChannelIndex.getOffsets().elements()[position];
-                int oldPosition = joinChannelHash.put(sliceAddress, position);
-                if (oldPosition >= 0) {
-                    // link the new position to the old position
-                    positionLinks.set(position, oldPosition);
-                }
-            }
+            hash = new BlocksHash(buildIndex.getIndex(buildJoinChannel));
         }
 
         protected Page computeNext()
@@ -136,17 +114,16 @@ public class HashJoinOperator
 
         private void join(Page page, PageBuilder pageBuilder)
         {
-            Block[] blocks = page.getBlocks();
-
             // open cursors
             BlockCursor[] cursors = new BlockCursor[page.getChannelCount()];
+            Block[] blocks = page.getBlocks();
             for (int i = 0; i < page.getChannelCount(); i++) {
                 cursors[i] = blocks[i].cursor();
             }
 
             // set hashing strategy to use probe block
             UncompressedBlock probeJoinBlock = (UncompressedBlock) page.getBlock(probeJoinChannel);
-            hashStrategy.setProbeSlice(probeJoinBlock.getSlice());
+            hash.setProbeSlice(probeJoinBlock.getSlice());
 
             int rows = page.getPositionCount();
             for (int position = 0; position < rows; position++) {
@@ -156,7 +133,7 @@ public class HashJoinOperator
                 }
 
                 // lookup the position of the "first" joined row
-                int joinPosition = joinChannelHash.get(0xFF_FF_FF_FF_00_00_00_00L | cursors[probeJoinChannel].getRawOffset());
+                int joinPosition = hash.get(cursors[probeJoinChannel]);
                 // while we have a position to join against...
                 while (joinPosition >= 0) {
                     for (int probeChannel = 0; probeChannel < cursors.length; probeChannel++) {
@@ -170,70 +147,13 @@ public class HashJoinOperator
                             outputIndex++;
                         }
                     }
-                    joinPosition = positionLinks.getInt(joinPosition);
+                    joinPosition = hash.getNextPosition(joinPosition);
                 }
             }
 
             for (BlockCursor cursor : cursors) {
                 checkState(!cursor.advanceNextPosition());
             }
-        }
-    }
-
-    public static class SliceHashStrategy
-            implements Strategy
-    {
-        private final TupleInfo tupleInfo;
-        private final Slice[] slices;
-        private Slice probeSlice;
-
-        public SliceHashStrategy(TupleInfo tupleInfo, Slice[] slices)
-        {
-            this.tupleInfo = tupleInfo;
-            this.slices = slices;
-        }
-
-        public void setProbeSlice(Slice probeSlice)
-        {
-            this.probeSlice = probeSlice;
-        }
-
-        @Override
-        public int hashCode(long sliceAddress)
-        {
-            Slice slice = getSlice(sliceAddress);
-            int offset = (int) sliceAddress;
-            int length = tupleInfo.size(slice, offset);
-            int hashCode = slice.hashCode(offset, length);
-            return hashCode;
-        }
-
-        @Override
-        public boolean equals(long leftSliceAddress, long rightSliceAddress)
-        {
-            Slice leftSlice = getSlice(leftSliceAddress);
-            int leftOffset = (int) leftSliceAddress;
-            int leftLength = tupleInfo.size(leftSlice, leftOffset);
-
-            Slice rightSlice = getSlice(rightSliceAddress);
-            int rightOffset = (int) rightSliceAddress;
-            int rightLength = tupleInfo.size(rightSlice, rightOffset);
-
-            return leftSlice.equals(leftOffset, leftLength, rightSlice, rightOffset, rightLength);
-
-        }
-
-        private Slice getSlice(long sliceAddress)
-        {
-            int sliceIndex = (int) (sliceAddress >> 32);
-            Slice slice;
-            if (sliceIndex == 0xFF_FF_FF_FF) {
-                slice = probeSlice;
-            }
-            else {
-                slice = slices[sliceIndex];
-            }
-            return slice;
         }
     }
 }
