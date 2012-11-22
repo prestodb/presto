8bd59d614147a01969e3ef2bd883c8731310a387
Dain Sundstrom <dain@iq80.com>
2012-11-22 11:11:01 -0800

    Initial distributed hash join implementation

--- presto-main/src/test/java/com/facebook/presto/benchmark/HashJoinBenchmark.java
+++ presto-main/src/test/java/com/facebook/presto/benchmark/HashJoinBenchmark.java
@@ -4,6 +4,7 @@ import com.facebook.presto.block.BlockIterable;
 import com.facebook.presto.operator.AlignmentOperator;
 import com.facebook.presto.operator.HashJoinOperator;
 import com.facebook.presto.operator.Operator;
+import com.facebook.presto.operator.SourceHashProvider;
 import com.facebook.presto.serde.BlocksFileEncoding;
 import com.facebook.presto.tpch.TpchBlocksProvider;
 
@@ -12,7 +13,7 @@ public class HashJoinBenchmark
 {
     public HashJoinBenchmark()
     {
-        super("hash_join", 1, 5);
+        super("hash_join", 4, 5);
     }
 
     /*
@@ -27,6 +28,7 @@ public class HashJoinBenchmark
         AlignmentOperator ordersTableScan = new AlignmentOperator(orderOrderKey, totalPrice);
 //        AlignmentOperator ordersTableScan = new AlignmentOperator(concat(nCopies(100, orderOrderKey)), concat(nCopies(100, totalPrice)));
 //        LimitOperator ordersLimit = new LimitOperator(ordersTableScan, 1_500_000);
+        SourceHashProvider sourceHashProvider = new SourceHashProvider(ordersTableScan, 0, 1_500_000);
 
         BlockIterable lineItemOrderKey = getBlockIterable(blocksProvider, "lineitem", "orderkey", BlocksFileEncoding.RAW);
         BlockIterable lineNumber = getBlockIterable(blocksProvider, "lineitem", "quantity", BlocksFileEncoding.RAW);
@@ -34,7 +36,7 @@ public class HashJoinBenchmark
 //        AlignmentOperator lineItemTableScan = new AlignmentOperator(concat(nCopies(100, lineItemOrderKey)), concat(nCopies(100, lineNumber)));
 //        LimitOperator lineItemLimit = new LimitOperator(lineItemTableScan, 10_000_000);
 
-        return new HashJoinOperator(ordersTableScan, 0, lineItemTableScan, 0);
+        return new HashJoinOperator(sourceHashProvider, lineItemTableScan, 0);
     }
 
     public static void main(String[] args)
