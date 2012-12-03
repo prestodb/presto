b8844ce43f8855de779bf234bb0d0db7af2d0449
David Phillips <david@acz.org>
2012-12-03 13:17:06 -0800

    Store history persistently

--- presto-main/src/main/java/com/facebook/presto/cli/Console.java
+++ presto-main/src/main/java/com/facebook/presto/cli/Console.java
@@ -3,11 +3,17 @@ package com.facebook.presto.cli;
 import io.airlift.command.Command;
 import io.airlift.command.Option;
 import jline.console.ConsoleReader;
+import jline.console.history.FileHistory;
+import jline.console.history.History;
 import jline.console.history.MemoryHistory;
 
 import java.io.Closeable;
+import java.io.File;
+import java.io.Flushable;
 import java.io.IOException;
 import java.net.URI;
+
+import static jline.internal.Configuration.getUserHome;
 
 @Command(name = "console", description = "Interactive console")
 public class Console
@@ -19,10 +25,7 @@ public class Console
     @Override
     public void run()
     {
-        MemoryHistory history = new MemoryHistory();
-        try (LineReader reader = new LineReader()) {
-            reader.setHistory(history);
-
+        try (LineReader reader = new LineReader(getHistory())) {
             while (true) {
                 String line = reader.readLine("presto> ");
                 if (line == null) {
@@ -71,15 +74,44 @@ public class Console
         return true;
     }
 
+    private static MemoryHistory getHistory()
+    {
+        MemoryHistory history;
+        File historyFile = new File(getUserHome(), ".presto_history");
+        try {
+            history = new FileHistory(historyFile);
+        }
+        catch (IOException e) {
+            System.err.printf("WARNING: Failed to load history file (%s): %s. " +
+                    "History will not be available during this session.%n",
+                    historyFile, e.getMessage());
+            history = new MemoryHistory();
+        }
+        history.setAutoTrim(true);
+        return history;
+    }
+
     private static class LineReader
             extends ConsoleReader
             implements Closeable
     {
-        private LineReader()
+        private LineReader(History history)
                 throws IOException
         {
             super();
             setExpandEvents(false);
+            setHistory(history);
+        }
+
+        @Override
+        public String readLine(String prompt, Character mask)
+                throws IOException
+        {
+            String line = super.readLine(prompt, mask);
+            if (getHistory() instanceof Flushable) {
+                ((Flushable) getHistory()).flush();
+            }
+            return line;
         }
 
         @Override
