62ec7d42fde3f77178ab24719458dab60f6a63a9
David Phillips <david@acz.org>
2012-12-03 15:30:25 -0800

    Move DelimitedTuplePrinter to upper level

---
+++ presto-main/src/main/java/com/facebook/presto/cli/DelimitedTuplePrinter.java
@@ -0,0 +1,46 @@
+package com.facebook.presto.cli;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.base.Throwables;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.List;
+
+import static com.facebook.presto.operator.OutputProcessor.OutputHandler;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class DelimitedTuplePrinter
+        implements OutputHandler
+{
+    private final Writer writer;
+    private final String delimiter;
+
+    public DelimitedTuplePrinter()
+    {
+        this(new OutputStreamWriter(System.out, Charsets.UTF_8), "\t");
+    }
+
+    public DelimitedTuplePrinter(Writer writer, String delimiter)
+    {
+        this.writer = checkNotNull(writer, "writer is null");
+        this.delimiter = checkNotNull(delimiter, "delimiter is null");
+    }
+
+    @Override
+    public void process(List<Object> values)
+    {
+        try {
+            Joiner.on(delimiter)
+                    .useForNull("NULL")
+                    .appendTo(writer, values);
+            writer.write('\n');
+            writer.flush();
+        }
+        catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+}
