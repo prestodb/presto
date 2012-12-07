ed08077eec032f9386c63b7dd2e24500fbe7f48c
David Phillips <david@acz.org>
2012-12-06 17:35:31 -0800

    Support listing tables for import data sources

--- presto-main/src/main/java/com/facebook/presto/metadata/ImportMetadata.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/ImportMetadata.java
@@ -6,12 +6,14 @@ import com.facebook.presto.spi.SchemaField;
 import com.facebook.presto.split.ImportClientFactory;
 import com.facebook.presto.tuple.TupleInfo;
 import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 import java.util.List;
 import java.util.concurrent.Callable;
 
+import static com.facebook.presto.metadata.MetadataUtil.checkCatalogName;
 import static com.facebook.presto.metadata.MetadataUtil.checkTableName;
 import static com.facebook.presto.util.RetryDriver.runWithRetryUnchecked;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -48,6 +50,52 @@ public class ImportMetadata
         List<ColumnMetadata> columns = convertToMetadata(catalogName, tableSchema);
 
         return new TableMetadata(catalogName, schemaName, tableName, columns, importTableHandle);
+    }
+
+    @Override
+    public List<QualifiedTableName> listTables(String catalogName)
+    {
+        checkCatalogName(catalogName);
+
+        ImportClient client = importClientFactory.getClient(catalogName);
+
+        ImmutableList.Builder<QualifiedTableName> list = ImmutableList.builder();
+
+        List<String> schemas = getDatabaseNames(client);
+        for (String schema : schemas) {
+            List<String> tables = getTableNames(client, schema);
+            for (String table : tables) {
+                list.add(new QualifiedTableName(catalogName, schema, table));
+            }
+        }
+
+        return list.build();
+    }
+
+    private static List<String> getTableNames(final ImportClient client, final String database)
+    {
+        return runWithRetryUnchecked(new Callable<List<String>>()
+        {
+            @Override
+            public List<String> call()
+                    throws Exception
+            {
+                return client.getTableNames(database);
+            }
+        });
+    }
+
+    private static List<String> getDatabaseNames(final ImportClient client)
+    {
+        return runWithRetryUnchecked(new Callable<List<String>>()
+        {
+            @Override
+            public List<String> call()
+                    throws Exception
+            {
+                return client.getDatabaseNames();
+            }
+        });
     }
 
     private List<ColumnMetadata> convertToMetadata(final String sourceName, List<SchemaField> schemaFields)
