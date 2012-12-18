77876dd8f317ac2c577d76d8e41f9889d1194403
Dain Sundstrom <dain@iq80.com>
2012-12-18 00:10:34 -0800

    Add min and max functions

---
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/DoubleMaxFixedWidthAggregation.java
@@ -0,0 +1,72 @@
+package com.facebook.presto.operator.aggregation;
+
+import com.facebook.presto.block.BlockBuilder;
+import com.facebook.presto.block.BlockCursor;
+import com.facebook.presto.slice.Slice;
+import com.facebook.presto.tuple.TupleInfo;
+
+import static com.facebook.presto.tuple.TupleInfo.SINGLE_DOUBLE;
+
+public class DoubleMaxFixedWidthAggregation
+        implements FixedWidthAggregationFunction
+{
+    public static final DoubleMaxFixedWidthAggregation DOUBLE_MAX = new DoubleMaxFixedWidthAggregation();
+
+    @Override
+    public TupleInfo getFinalTupleInfo()
+    {
+        return SINGLE_DOUBLE;
+    }
+
+    @Override
+    public TupleInfo getIntermediateTupleInfo()
+    {
+        return SINGLE_DOUBLE;
+    }
+
+    @Override
+    public void initialize(Slice valueSlice, int valueOffset)
+    {
+        // mark value null
+        SINGLE_DOUBLE.setNull(valueSlice, valueOffset, 0);
+    }
+
+    @Override
+    public void addInput(BlockCursor cursor, Slice valueSlice, int valueOffset)
+    {
+        if (cursor.isNull(0)) {
+            return;
+        }
+
+        // mark value not null
+        SINGLE_DOUBLE.setNotNull(valueSlice, valueOffset, 0);
+
+        // update current value
+        double currentValue = SINGLE_DOUBLE.getDouble(valueSlice, valueOffset, 0);
+        double newValue = cursor.getDouble(0);
+        SINGLE_DOUBLE.setDouble(valueSlice, valueOffset, 0, Math.max(currentValue, newValue));
+    }
+
+    @Override
+    public void addIntermediate(BlockCursor cursor, Slice valueSlice, int valueOffset)
+    {
+        addInput(cursor, valueSlice, valueOffset);
+    }
+
+    @Override
+    public void evaluateIntermediate(Slice valueSlice, int valueOffset, BlockBuilder output)
+    {
+        evaluateFinal(valueSlice, valueOffset, output);
+    }
+
+    @Override
+    public void evaluateFinal(Slice valueSlice, int valueOffset, BlockBuilder output)
+    {
+        if (!SINGLE_DOUBLE.isNull(valueSlice, valueOffset, 0)) {
+            double currentValue = SINGLE_DOUBLE.getDouble(valueSlice, valueOffset, 0);
+            output.append(currentValue);
+        } else {
+            output.appendNull();
+        }
+    }
+}
---
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/LongMaxFixedWidthAggregation.java
@@ -0,0 +1,74 @@
+package com.facebook.presto.operator.aggregation;
+
+import com.facebook.presto.block.BlockBuilder;
+import com.facebook.presto.block.BlockCursor;
+import com.facebook.presto.slice.Slice;
+import com.facebook.presto.tuple.TupleInfo;
+
+import java.lang.Math;
+
+import static com.facebook.presto.tuple.TupleInfo.SINGLE_LONG;
+
+public class LongMaxFixedWidthAggregation
+        implements FixedWidthAggregationFunction
+{
+    public static final LongMaxFixedWidthAggregation LONG_MAX = new LongMaxFixedWidthAggregation();
+
+    @Override
+    public TupleInfo getFinalTupleInfo()
+    {
+        return SINGLE_LONG;
+    }
+
+    @Override
+    public TupleInfo getIntermediateTupleInfo()
+    {
+        return SINGLE_LONG;
+    }
+
+    @Override
+    public void initialize(Slice valueSlice, int valueOffset)
+    {
+        // mark value null
+        SINGLE_LONG.setNull(valueSlice, valueOffset, 0);
+    }
+
+    @Override
+    public void addInput(BlockCursor cursor, Slice valueSlice, int valueOffset)
+    {
+        if (cursor.isNull(0)) {
+            return;
+        }
+
+        // mark value not null
+        SINGLE_LONG.setNotNull(valueSlice, valueOffset, 0);
+
+        // update current value
+        long currentValue = SINGLE_LONG.getLong(valueSlice, valueOffset, 0);
+        long newValue = cursor.getLong(0);
+        SINGLE_LONG.setLong(valueSlice, valueOffset, 0, Math.max(currentValue, newValue));
+    }
+
+    @Override
+    public void addIntermediate(BlockCursor cursor, Slice valueSlice, int valueOffset)
+    {
+        addInput(cursor, valueSlice, valueOffset);
+    }
+
+    @Override
+    public void evaluateIntermediate(Slice valueSlice, int valueOffset, BlockBuilder output)
+    {
+        evaluateFinal(valueSlice, valueOffset, output);
+    }
+
+    @Override
+    public void evaluateFinal(Slice valueSlice, int valueOffset, BlockBuilder output)
+    {
+        if (!SINGLE_LONG.isNull(valueSlice, valueOffset, 0)) {
+            long currentValue = SINGLE_LONG.getLong(valueSlice, valueOffset, 0);
+            output.append(currentValue);
+        } else {
+            output.appendNull();
+        }
+    }
+}
