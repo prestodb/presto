2d677b4e56cae8b0ed456a45acc8bb28631e9dc7
David Phillips <david@acz.org>
2012-12-21 16:37:52 -0800

    Add StatementSplitter class

---
+++ presto-main/src/test/java/com/facebook/presto/sql/parser/TestStatementSplitter.java
@@ -0,0 +1,49 @@
+package com.facebook.presto.sql.parser;
+
+import com.google.common.collect.ImmutableList;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestStatementSplitter
+{
+    @Test
+    public void testSplitterIncomplete()
+    {
+        StatementSplitter splitter = new StatementSplitter(" select * FROM foo  ");
+        assertEquals(splitter.getCompleteStatements(), ImmutableList.of());
+        assertEquals(splitter.getPartialStatement(), "select * FROM foo");
+    }
+
+    @Test
+    public void testSplitterSingle()
+    {
+        StatementSplitter splitter = new StatementSplitter("select * from foo;");
+        assertEquals(splitter.getCompleteStatements(), ImmutableList.of("select * from foo"));
+        assertEquals(splitter.getPartialStatement(), "");
+    }
+
+    @Test
+    public void testSplitterMultiple()
+    {
+        StatementSplitter splitter = new StatementSplitter(" select * from  foo ; select * from t; select * from ");
+        assertEquals(splitter.getCompleteStatements(), ImmutableList.of("select * from  foo", "select * from t"));
+        assertEquals(splitter.getPartialStatement(), "select * from");
+    }
+
+    @Test
+    public void testSplitterErrorBeforeComplete()
+    {
+        StatementSplitter splitter = new StatementSplitter(" select * from foo @ ; select ");
+        assertEquals(splitter.getCompleteStatements(), ImmutableList.of());
+        assertEquals(splitter.getPartialStatement(), "select * from foo @ ; select");
+    }
+
+    @Test
+    public void testSplitterErrorAfterComplete()
+    {
+        StatementSplitter splitter = new StatementSplitter("select * from foo; select z@ oops ");
+        assertEquals(splitter.getCompleteStatements(), ImmutableList.of("select * from foo"));
+        assertEquals(splitter.getPartialStatement(), "select z@ oops");
+    }
+}
