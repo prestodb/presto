04f555d1f86ae21463c15086df1ef0e29af8fccc
David Phillips <david@acz.org>
2013-01-01 22:14:44 -0800

    Add basic string functions

--- presto-main/src/test/java/com/facebook/presto/TestFunctions.java
+++ presto-main/src/test/java/com/facebook/presto/TestFunctions.java
@@ -70,6 +70,37 @@ public class TestFunctions
     private final DualTableDataStreamProvider dataProvider = new DualTableDataStreamProvider();
 
     @Test
+    public void testConcat()
+            throws Exception
+    {
+        assertFunction("CONCAT('hello', ' world')", "hello world");
+        assertFunction("CONCAT('', '')", "");
+        assertFunction("CONCAT('what', '')", "what");
+        assertFunction("CONCAT('', 'what')", "what");
+        assertFunction("CONCAT(CONCAT('this', ' is'), ' cool')", "this is cool");
+        assertFunction("CONCAT('this', CONCAT(' is', ' cool'))", "this is cool");
+    }
+
+    @Test
+    public void testLength()
+            throws Exception
+    {
+        assertFunction("LENGTH('')", 0);
+        assertFunction("LENGTH('hello')", 5);
+        assertFunction("LENGTH('Quadratically')", 13);
+    }
+
+    @Test
+    public void testReverse()
+            throws Exception
+    {
+        assertFunction("REVERSE('')", "");
+        assertFunction("REVERSE('hello')", "olleh");
+        assertFunction("REVERSE('Quadratically')", "yllacitardauQ");
+        assertFunction("REVERSE('racecar')", "racecar");
+    }
+
+    @Test
     public void testSubstring()
             throws Exception
     {
@@ -82,6 +113,60 @@ public class TestFunctions
         assertFunction("SUBSTR('Quadratically', -50, 4)", "");
         assertFunction("SUBSTR('Quadratically', 0, 4)", "");
         assertFunction("SUBSTR('Quadratically', 5, 0)", "");
+    }
+
+    @Test
+    public void testLeftTrim()
+            throws Exception
+    {
+        assertFunction("LTRIM('')", "");
+        assertFunction("LTRIM('   ')", "");
+        assertFunction("LTRIM('  hello  ')", "hello  ");
+        assertFunction("LTRIM('  hello')", "hello");
+        assertFunction("LTRIM('hello  ')", "hello  ");
+        assertFunction("LTRIM(' hello world ')", "hello world ");
+    }
+
+    @Test
+    public void testRightTrim()
+            throws Exception
+    {
+        assertFunction("RTRIM('')", "");
+        assertFunction("RTRIM('   ')", "");
+        assertFunction("RTRIM('  hello  ')", "  hello");
+        assertFunction("RTRIM('  hello')", "  hello");
+        assertFunction("RTRIM('hello  ')", "hello");
+        assertFunction("RTRIM(' hello world ')", " hello world");
+    }
+
+    @Test
+    public void testTrim()
+            throws Exception
+    {
+        assertFunction("TRIM('')", "");
+        assertFunction("TRIM('   ')", "");
+        assertFunction("TRIM('  hello  ')", "hello");
+        assertFunction("TRIM('  hello')", "hello");
+        assertFunction("TRIM('hello  ')", "hello");
+        assertFunction("TRIM(' hello world ')", "hello world");
+    }
+
+    @Test
+    public void testLower()
+            throws Exception
+    {
+        assertFunction("LOWER('')", "");
+        assertFunction("LOWER('Hello World')", "hello world");
+        assertFunction("LOWER('WHAT!!')", "what!!");
+    }
+
+    @Test
+    public void testUpper()
+            throws Exception
+    {
+        assertFunction("UPPER('')", "");
+        assertFunction("UPPER('Hello World')", "HELLO WORLD");
+        assertFunction("UPPER('what!!')", "WHAT!!");
     }
 
     private void assertFunction(String projection, long expected)
