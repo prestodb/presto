38b6324ad72dd7f8ba5186aa860f80dd617c3aa7
David Phillips <david@acz.org>
2013-01-09 14:21:11 -0800

    Fix SHOW TABLES and SHOW COLUMNS for internal tables

--- presto-main/src/main/java/com/facebook/presto/metadata/InternalMetadata.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/InternalMetadata.java
@@ -1,10 +1,15 @@
 package com.facebook.presto.metadata;
 
+import com.google.common.collect.ImmutableList;
+
 import javax.inject.Inject;
+import java.util.List;
 
 import static com.facebook.presto.metadata.InformationSchemaMetadata.INFORMATION_SCHEMA;
+import static com.facebook.presto.metadata.InformationSchemaMetadata.listInformationSchemaTables;
 import static com.facebook.presto.metadata.MetadataUtil.checkTableName;
 import static com.facebook.presto.metadata.SystemTables.SYSTEM_SCHEMA;
+import static com.facebook.presto.metadata.SystemTables.listSystemTables;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class InternalMetadata
@@ -29,14 +34,32 @@ public class InternalMetadata
             return DualTable.getMetadata(catalogName, schemaName, tableName);
         }
 
-        if (schemaName.equals(INFORMATION_SCHEMA)) {
-            return informationSchema.getTableMetadata(catalogName, schemaName, tableName);
-        }
-
-        if (schemaName.equals(SYSTEM_SCHEMA)) {
-            return systemTables.getTableMetadata(catalogName, schemaName, tableName);
+        switch (schemaName) {
+            case INFORMATION_SCHEMA:
+                return informationSchema.getTableMetadata(catalogName, schemaName, tableName);
+            case SYSTEM_SCHEMA:
+                return systemTables.getTableMetadata(catalogName, schemaName, tableName);
         }
 
         return null;
+    }
+
+    @Override
+    public List<QualifiedTableName> listTables(String catalogName, String schemaName)
+    {
+        switch (schemaName) {
+            case INFORMATION_SCHEMA:
+                return listInformationSchemaTables(catalogName);
+            case SYSTEM_SCHEMA:
+                return listSystemTables(catalogName);
+        }
+        return ImmutableList.of();
+    }
+
+    @Override
+    public List<TableColumn> listTableColumns(String catalogName, String schemaName, String tableName)
+    {
+        // hack for SHOW COLUMNS
+        return ImmutableList.of();
     }
 }
