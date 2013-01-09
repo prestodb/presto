9da1e7defb5f7ccefb6f2958c60b54d569114134
Eric Hwang <ehwang@fb.com>
2013-01-08 22:25:32 -0800

    Temporary support for single presto supporting multiple hive dbs

--- presto-main/src/test/java/com/facebook/presto/split/TestImportClientFactory.java
+++ presto-main/src/test/java/com/facebook/presto/split/TestImportClientFactory.java
@@ -15,22 +15,24 @@ public class TestImportClientFactory
             throws Exception
     {
         ServiceSelector selector = new StaticServiceSelector(
-                serviceDescriptor("hive-metastore").addProperty("thrift", "meta1.test:9083").build(),
-                serviceDescriptor("hive-metastore").addProperty("thrift", "meta2.text:9083").build(),
+                serviceDescriptor("hive-metastore").addProperty("thrift", "meta1.test:9083").addProperty("name", "fuu").build(),
+                serviceDescriptor("hive-metastore").addProperty("thrift", "meta2.text:9083").addProperty("name", "fuu").build(),
+                serviceDescriptor("hive-metastore").addProperty("thrift", "meta3.text:9083").addProperty("name", "bar").build(),
                 serviceDescriptor("hive-metastore").addProperty("thrift", "missing-port").build(),
                 serviceDescriptor("hive-metastore").build());
 
         ImportClientFactory factory = new ImportClientFactory(selector);
-        assertInstanceOf(factory.getClient("hive"), HiveClient.class);
+        assertInstanceOf(factory.getClient("hive_fuu"), HiveClient.class);
+        assertInstanceOf(factory.getClient("hive_bar"), HiveClient.class);
     }
 
-    @Test(expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "hive metastore not available: general")
+    @Test(expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "hive metastore not available for name fuu in pool general")
     public void testGetClientFailure()
     {
         ServiceSelector selector = new StaticServiceSelector(
-                serviceDescriptor("hive-metastore").addProperty("thrift", "missing-port").build(),
+                serviceDescriptor("hive-metastore").addProperty("thrift", "missing-port").addProperty("name", "fuu").build(),
                 serviceDescriptor("hive-metastore").build());
 
-        new ImportClientFactory(selector).getClient("hive");
+        new ImportClientFactory(selector).getClient("hive_fuu");
     }
 }
