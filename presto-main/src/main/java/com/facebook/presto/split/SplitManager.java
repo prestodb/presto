bd0371c97658f8d4deebeeb23d070e103f12a821
Eric Hwang <ehwang@fb.com>
2013-01-16 14:54:09 -0800

    Stopgap to prevent OOM when too many splits

--- presto-main/src/main/java/com/facebook/presto/split/SplitManager.java
+++ presto-main/src/main/java/com/facebook/presto/split/SplitManager.java
@@ -1,5 +1,6 @@
 package com.facebook.presto.split;
 
+import com.facebook.presto.execution.QueryManagerConfig;
 import com.facebook.presto.ingest.SerializedPartitionChunk;
 import com.facebook.presto.metadata.ColumnHandle;
 import com.facebook.presto.metadata.ImportColumnHandle;
@@ -37,6 +38,7 @@ import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.BiMap;
+import com.google.common.collect.ForwardingIterator;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
@@ -47,6 +49,7 @@ import com.google.inject.Inject;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -58,6 +61,7 @@ import static com.facebook.presto.util.IterableUtils.limit;
 import static com.facebook.presto.util.IterableUtils.shuffle;
 import static com.facebook.presto.util.RetryDriver.retry;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Predicates.in;
 import static com.google.common.base.Predicates.instanceOf;
 import static com.google.common.base.Predicates.or;
@@ -68,27 +72,66 @@ public class SplitManager
     private final ShardManager shardManager;
     private final ImportClientFactory importClientFactory;
     private final Metadata metadata;
+    private final long maxSplitCount;
 
     @Inject
-    public SplitManager(NodeManager nodeManager, ShardManager shardManager, ImportClientFactory importClientFactory, Metadata metadata)
+    public SplitManager(NodeManager nodeManager, ShardManager shardManager, ImportClientFactory importClientFactory, Metadata metadata, QueryManagerConfig config)
     {
         this.nodeManager = checkNotNull(nodeManager, "nodeManager is null");
         this.shardManager = checkNotNull(shardManager, "shardManager is null");
         this.importClientFactory = checkNotNull(importClientFactory, "importClientFactory is null");
         this.metadata = checkNotNull(metadata, "metadata is null");
+        this.maxSplitCount = checkNotNull(config, "config is null").getMaxSplitCount();
     }
 
     public Iterable<SplitAssignments> getSplitAssignments(Session session, TableHandle handle, Expression predicate, Map<Symbol, ColumnHandle> mappings)
     {
+        Iterable<SplitAssignments> assignments;
         switch (handle.getDataSourceType()) {
             case NATIVE:
-                return getNativeSplitAssignments((NativeTableHandle) handle);
+                assignments = getNativeSplitAssignments((NativeTableHandle) handle);
+                break;
             case INTERNAL:
-                return getInternalSplitAssignments((InternalTableHandle) handle, predicate, mappings);
+                assignments = getInternalSplitAssignments((InternalTableHandle) handle, predicate, mappings);
+                break;
             case IMPORT:
-                return getImportSplitAssignments(session, (ImportTableHandle) handle, predicate, mappings);
+                assignments = getImportSplitAssignments(session, (ImportTableHandle) handle, predicate, mappings);
+                break;
+            default:
+                throw new IllegalArgumentException("unsupported handle type: " + handle);
         }
-        throw new IllegalArgumentException("unsupported handle type: " + handle);
+        // TODO: temporary solution to prevent large queries from killing the server
+        return throwIfExceedsSize(assignments, maxSplitCount);
+    }
+
+    private static <T> Iterable<T> throwIfExceedsSize(final Iterable<T> iterable, final long maxSize)
+    {
+        return new Iterable<T>()
+        {
+            @Override
+            public Iterator<T> iterator()
+            {
+                return new ForwardingIterator<T>()
+                {
+                    private final Iterator<T> iterator = iterable.iterator();
+                    private long count = 0;
+
+                    @Override
+                    protected Iterator<T> delegate()
+                    {
+                        return iterator;
+                    }
+
+                    @Override
+                    public T next()
+                    {
+                        count++;
+                        checkState(count <= maxSize, "max split count exceeded: %s", maxSize);
+                        return super.next();
+                    }
+                };
+            }
+        };
     }
 
     private Iterable<SplitAssignments> getNativeSplitAssignments(NativeTableHandle handle)
