39607548b4550338c3c913b9627e8c71daf11159
Henning Schmiedehausen <hgschmie@fb.com>
2013-01-16 16:31:17 -0800

    Use TupleInfo to find the right offsets into the intermediate data. Add setLong and setDouble with implicit offset 0 to TupleInfo to match the existing getLong and getDouble functions.

--- presto-main/src/main/java/com/facebook/presto/tuple/TupleInfo.java
+++ presto-main/src/main/java/com/facebook/presto/tuple/TupleInfo.java
@@ -285,6 +285,11 @@ public class TupleInfo
         return slice.getLong(offset + getOffset(field));
     }
 
+    public void setLong(Slice slice, int field, long value)
+    {
+        setLong(slice, 0, field, value);
+    }
+
     /**
      * Sets the specified field to the specified long value.
      *
@@ -307,6 +312,11 @@ public class TupleInfo
         checkState(types.get(field) == DOUBLE, "Expected DOUBLE");
 
         return slice.getDouble(offset + getOffset(field));
+    }
+
+    public void setDouble(Slice slice, int field, double value)
+    {
+        setDouble(slice, 0, field, value);
     }
 
     /**
