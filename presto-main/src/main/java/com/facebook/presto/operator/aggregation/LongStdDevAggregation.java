6e24ee2a96fafd8cbe54676c0b73c9b6fee6f3a2
Henning Schmiedehausen <hgschmie@fb.com>
2013-01-18 13:24:59 -0800

    After benchmarking the stats functions, rejigger the code a bit to make double_stddev and double_stddev_pop a mindboggling 27% (!) faster and all the long methods a little faster.
    Before:
    
          stat_long_variance avg results :: elapsed_millis: 20.22
      stat_long_variance_pop avg results :: elapsed_millis: 18.76
        stat_double_variance avg results :: elapsed_millis: 13.92
    stat_double_variance_pop avg results :: elapsed_millis: 14.20
            stat_long_stddev avg results :: elapsed_millis: 20.06
        stat_long_stddev_pop avg results :: elapsed_millis: 20.34
          stat_double_stddev avg results :: elapsed_millis: 18.78
      stat_double_stddev_pop avg results :: elapsed_millis: 18.64
    
    After:
    
          stat_long_variance avg results :: elapsed_millis: 19.31 (-4.5%)
      stat_long_variance_pop avg results :: elapsed_millis: 18.73 (-0.2%)
        stat_double_variance avg results :: elapsed_millis: 13.73 (-1.4%)
    stat_double_variance_pop avg results :: elapsed_millis: 13.61 (-4.2%)
            stat_long_stddev avg results :: elapsed_millis: 18.70 (-6.8%)
        stat_long_stddev_pop avg results :: elapsed_millis: 18.67 (-8.2%)
          stat_double_stddev avg results :: elapsed_millis: 13.56 (-27.8%)
      stat_double_stddev_pop avg results :: elapsed_millis: 13.51 (-27.6%)

--- presto-main/src/main/java/com/facebook/presto/operator/aggregation/LongStdDevAggregation.java
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/LongStdDevAggregation.java
@@ -1,9 +1,10 @@
 package com.facebook.presto.operator.aggregation;
 
-import com.facebook.presto.block.BlockCursor;
+import com.facebook.presto.block.BlockBuilder;
+import com.facebook.presto.slice.Slice;
 
 public class LongStdDevAggregation
-    extends AbstractStdDevAggregation
+    extends LongVarianceAggregation
 {
     public static final LongStdDevAggregation STDDEV_INSTANCE = new LongStdDevAggregation(false);
     public static final LongStdDevAggregation STDDEV_POP_INSTANCE = new LongStdDevAggregation(true);
@@ -14,9 +15,16 @@ public class LongStdDevAggregation
     }
 
     @Override
-    protected double getX(final BlockCursor cursor)
+    public void evaluateFinal(final Slice valueSlice, final int valueOffset, final BlockBuilder output)
     {
-        return (double) cursor.getLong(0);
+        final Double result = DoubleStdDevAggregation.buildFinalStdDev(population, valueSlice, valueOffset);
+
+        if (result == null) {
+            output.appendNull();
+        }
+        else {
+            output.append(result.doubleValue());
+        }
     }
 }
 
--- presto-main/src/main/java/com/facebook/presto/operator/aggregation/LongVarianceAggregation.java
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/LongVarianceAggregation.java
@@ -1,9 +1,12 @@
 package com.facebook.presto.operator.aggregation;
 
+import com.facebook.presto.block.Block;
+import com.facebook.presto.slice.Slice;
+
 import com.facebook.presto.block.BlockCursor;
 
 public class LongVarianceAggregation
-    extends AbstractVarianceAggregation
+    extends DoubleVarianceAggregation
 {
     public static final LongVarianceAggregation VARIANCE_INSTANCE = new LongVarianceAggregation(false);
     public static final LongVarianceAggregation VARIANCE_POP_INSTANCE = new LongVarianceAggregation(true);
@@ -14,8 +17,63 @@ public class LongVarianceAggregation
     }
 
     @Override
-    protected double getX(final BlockCursor cursor)
+    public void addInput(final int positionCount, final Block block, final Slice valueSlice, final int valueOffset)
     {
-        return (double) cursor.getLong(0);
+        boolean hasValue = !VARIANCE_CONTEXT_INFO.isNull(valueSlice, valueOffset, 0);
+        long count = hasValue ? VARIANCE_CONTEXT_INFO.getLong(valueSlice, valueOffset, 0) : 0;
+        double mean = VARIANCE_CONTEXT_INFO.getDouble(valueSlice, valueOffset, 1);
+        double m2 = VARIANCE_CONTEXT_INFO.getDouble(valueSlice, valueOffset, 2);
+
+        final BlockCursor cursor = block.cursor();
+
+        while (cursor.advanceNextPosition()) {
+            if (cursor.isNull(0)) {
+                continue;
+            }
+
+            // There is now at least one value present.
+            hasValue = true;
+
+            count++;
+            final double x = cursor.getLong(0);
+            final double delta = x - mean;
+            mean += (delta / count);
+            m2 += (delta * (x - mean));
+        }
+
+        if (hasValue) {
+            VARIANCE_CONTEXT_INFO.setNotNull(valueSlice, valueOffset, 0);
+            VARIANCE_CONTEXT_INFO.setLong(valueSlice, valueOffset, 0, count);
+            VARIANCE_CONTEXT_INFO.setDouble(valueSlice, valueOffset, 1, mean);
+            VARIANCE_CONTEXT_INFO.setDouble(valueSlice, valueOffset, 2, m2);
+        }
+    }
+
+    @Override
+    public void addInput(final BlockCursor cursor, final Slice valueSlice, final int valueOffset)
+    {
+        boolean hasValue = !VARIANCE_CONTEXT_INFO.isNull(valueSlice, valueOffset, 0);
+
+        if (cursor.isNull(0)) {
+            return;
+        }
+
+        long count = hasValue ? VARIANCE_CONTEXT_INFO.getLong(valueSlice, valueOffset, 0) : 0;
+        double mean = VARIANCE_CONTEXT_INFO.getDouble(valueSlice, valueOffset, 1);
+        double m2 = VARIANCE_CONTEXT_INFO.getDouble(valueSlice, valueOffset, 2);
+
+        count++;
+        final double x = cursor.getLong(0);
+        final double delta = x - mean;
+        mean += (delta / count);
+        m2 += (delta * (x - mean));
+
+        if (!hasValue) {
+            VARIANCE_CONTEXT_INFO.setNotNull(valueSlice, valueOffset, 0);
+        }
+
+        VARIANCE_CONTEXT_INFO.setLong(valueSlice, valueOffset, 0, count);
+        VARIANCE_CONTEXT_INFO.setDouble(valueSlice, valueOffset, 1, mean);
+        VARIANCE_CONTEXT_INFO.setDouble(valueSlice, valueOffset, 2, m2);
     }
 }
