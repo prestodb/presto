8b576fbd5344d7fe262325f3886601c5cc8554ef
Martin Traverso <martint@fb.com>
2013-01-28 09:01:33 -0800

    Add benchmark to compare single vs multiple fields in aggregation query

---
+++ presto-main/src/test/java/com/facebook/presto/benchmark/SqlDistinctSingleField.java
@@ -0,0 +1,39 @@
+package com.facebook.presto.benchmark;
+
+import com.facebook.presto.block.BlockCursor;
+import com.facebook.presto.operator.Operator;
+import com.facebook.presto.operator.OperatorStats;
+import com.facebook.presto.operator.Page;
+import com.facebook.presto.operator.PageIterator;
+import com.facebook.presto.tpch.TpchBlocksProvider;
+
+public class SqlDistinctSingleField
+        extends AbstractSqlBenchmark
+{
+    public SqlDistinctSingleField()
+    {
+        super("sql_distinct_single", 10, 20, "SELECT DISTINCT shippriority FROM orders");
+    }
+
+    @Override
+    protected long execute(TpchBlocksProvider blocksProvider)
+    {
+        Operator operator = createBenchmarkedOperator(blocksProvider);
+
+        long outputRows = 0;
+        PageIterator iterator = operator.iterator(new OperatorStats());
+        while (iterator.hasNext()) {
+            Page page = iterator.next();
+            BlockCursor cursor = page.getBlock(0).cursor();
+            while (cursor.advanceNextPosition()) {
+                outputRows++;
+            }
+        }
+        return outputRows;
+    }
+
+    public static void main(String[] args)
+    {
+        new SqlDistinctSingleField().runBenchmark(new SimpleLineBenchmarkResultWriter(System.out));
+    }
+}
