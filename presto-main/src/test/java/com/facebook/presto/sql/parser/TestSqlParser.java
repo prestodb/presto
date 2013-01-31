88e5391d0bc7367f82eb6fe005b7b316a89620ee
David Phillips <david@acz.org>
2013-01-31 12:36:04 -0800

    Move statement builder test to separate class

--- presto-main/src/test/java/com/facebook/presto/sql/parser/TestSqlParser.java
+++ presto-main/src/test/java/com/facebook/presto/sql/parser/TestSqlParser.java
@@ -8,15 +8,9 @@ import com.facebook.presto.sql.tree.QualifiedName;
 import com.facebook.presto.sql.tree.Query;
 import com.facebook.presto.sql.tree.SortItem;
 import com.facebook.presto.sql.tree.Statement;
-import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
-import com.google.common.io.Resources;
-import org.antlr.runtime.tree.CommonTree;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-
-import static com.facebook.presto.sql.parser.TreePrinter.treeToString;
 import static com.facebook.presto.sql.tree.QueryUtil.selectList;
 import static com.facebook.presto.sql.tree.QueryUtil.table;
 import static java.lang.String.format;
@@ -67,37 +61,6 @@ public class TestSqlParser
     public void testExpressionWithTrailingJunk()
     {
         SqlParser.createExpression("1 + 1 x");
-    }
-
-    @Test
-    public void testStatementBuilder()
-            throws Exception
-    {
-        printStatement("select * from foo");
-
-        printStatement("select * from foo a (x, y, z)");
-
-        printStatement("select show from foo");
-        printStatement("select extract(day from x), extract(dow from x) from y");
-
-        printStatement("" +
-                "select * from foo a " +
-                "join bar b using (bar_id) " +
-                "left join zoo on (a.x = zoo.y) " +
-                "cross join (d natural inner join e)");
-
-        printStatement("select * from information_schema.tables");
-
-        printStatement("show tables");
-        printStatement("show tables from information_schema");
-        printStatement("show tables like '%'");
-        printStatement("show tables from information_schema like '%'");
-
-        for (int i = 1; i <= 22; i++) {
-            if (i != 15) {
-                printStatement(getTpchQuery(i));
-            }
-        }
     }
 
     @Test(expectedExceptions = ParsingException.class, expectedExceptionsMessageRegExp = "line 1:1: no viable alternative at character '@'")
@@ -155,49 +118,6 @@ public class TestSqlParser
             assertEquals(e.getLineNumber(), 3);
             assertEquals(e.getColumnNumber(), 7);
         }
-    }
-
-    private static void printStatement(String sql)
-    {
-        println(sql.trim());
-        println("");
-
-        CommonTree tree = SqlParser.parseStatement(sql);
-        println(treeToString(tree));
-        println("");
-
-        Statement statement = SqlParser.createStatement(tree);
-        println(statement.toString());
-        println("");
-    }
-
-    private static void println(String s)
-    {
-        if (Boolean.parseBoolean(System.getProperty("printParse"))) {
-            System.out.println(s);
-        }
-    }
-
-    private static String getTpchQuery(int q)
-            throws IOException
-    {
-        return fixTpchQuery(readResource("tpch/queries/" + q + ".sql"));
-    }
-
-    private static String readResource(String name)
-            throws IOException
-    {
-        return Resources.toString(Resources.getResource(name), Charsets.UTF_8);
-    }
-
-    private static String fixTpchQuery(String s)
-    {
-        s = s.replaceFirst("(?m);$", "");
-        s = s.replaceAll("(?m)^:[xo]$", "");
-        s = s.replaceAll("(?m)^:n -1$", "");
-        s = s.replaceAll("(?m)^:n ([0-9]+)$", "LIMIT $1");
-        s = s.replaceAll("([^']):([0-9]+)", "$1$2");
-        return s;
     }
 
     private static void assertStatement(String query, Statement expected)
---
+++ presto-main/src/test/java/com/facebook/presto/sql/parser/TestStatementBuilder.java
@@ -0,0 +1,92 @@
+package com.facebook.presto.sql.parser;
+
+import com.facebook.presto.sql.tree.Statement;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import org.antlr.runtime.tree.CommonTree;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static com.facebook.presto.sql.parser.TreePrinter.treeToString;
+import static com.google.common.base.Strings.repeat;
+
+public class TestStatementBuilder
+{
+    @Test
+    public void testStatementBuilder()
+            throws Exception
+    {
+        printStatement("select * from foo");
+
+        printStatement("select * from foo a (x, y, z)");
+
+        printStatement("select show from foo");
+        printStatement("select extract(day from x), extract(dow from x) from y");
+
+        printStatement("" +
+                "select * from foo a " +
+                "join bar b using (bar_id) " +
+                "left join zoo on (a.x = zoo.y) " +
+                "cross join (d natural inner join e)");
+
+        printStatement("select * from information_schema.tables");
+
+        printStatement("show tables");
+        printStatement("show tables from information_schema");
+        printStatement("show tables like '%'");
+        printStatement("show tables from information_schema like '%'");
+
+        for (int i = 1; i <= 22; i++) {
+            if (i != 15) {
+                printStatement(getTpchQuery(i));
+            }
+        }
+    }
+
+    private static void printStatement(String sql)
+    {
+        println(sql.trim());
+        println("");
+
+        CommonTree tree = SqlParser.parseStatement(sql);
+        println(treeToString(tree));
+        println("");
+
+        Statement statement = SqlParser.createStatement(tree);
+        println(statement.toString());
+        println("");
+
+        println(repeat("=", 60));
+        println("");
+    }
+
+    private static void println(String s)
+    {
+        if (Boolean.parseBoolean(System.getProperty("printParse"))) {
+            System.out.println(s);
+        }
+    }
+
+    private static String getTpchQuery(int q)
+            throws IOException
+    {
+        return fixTpchQuery(readResource("tpch/queries/" + q + ".sql"));
+    }
+
+    private static String readResource(String name)
+            throws IOException
+    {
+        return Resources.toString(Resources.getResource(name), Charsets.UTF_8);
+    }
+
+    private static String fixTpchQuery(String s)
+    {
+        s = s.replaceFirst("(?m);$", "");
+        s = s.replaceAll("(?m)^:[xo]$", "");
+        s = s.replaceAll("(?m)^:n -1$", "");
+        s = s.replaceAll("(?m)^:n ([0-9]+)$", "LIMIT $1");
+        s = s.replaceAll("([^']):([0-9]+)", "$1$2");
+        return s;
+    }
+}
