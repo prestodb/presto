86b1ca84d7baf3205415edc1c730c7206d1d74f8
David Phillips <david@acz.org>
2013-02-05 23:07:47 -0800

    Handle lexer errors properly in console

--- presto-main/src/main/java/com/facebook/presto/sql/parser/StatementSplitter.java
+++ presto-main/src/main/java/com/facebook/presto/sql/parser/StatementSplitter.java
@@ -2,7 +2,9 @@ package com.facebook.presto.sql.parser;
 
 import com.google.common.collect.ImmutableList;
 import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonToken;
+import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.Token;
 import org.antlr.runtime.TokenSource;
 
@@ -100,6 +102,62 @@ public class StatementSplitter
 
     private static TokenSource getLexer(String sql)
     {
-        return new StatementLexer(new CaseInsensitiveStream(new ANTLRStringStream(sql)));
+        return new ErrorHandlingLexer(new CaseInsensitiveStream(new ANTLRStringStream(sql)));
+    }
+
+    /**
+     * ANTLR has two options for handling lexer errors: attempt to ignore the
+     * bad input and continue, or abort lexing by throwing an exception.
+     * However, we need to tokenize all input even if it's invalid and
+     * reconstruct the original input from the tokens, but neither of those
+     * options allows for that. So, we create an alternative: convert lexer
+     * errors into a special token, which preserves all of the original input
+     * text. ANTLR doesn't provide a way to do this, so we create a copy of
+     * the {@link org.antlr.runtime.Lexer#nextToken} method with the
+     * appropriate modifications for creating the special error token.
+     */
+    private static class ErrorHandlingLexer
+            extends StatementLexer
+    {
+        public ErrorHandlingLexer(CharStream input)
+        {
+            super(input);
+        }
+
+        @SuppressWarnings("ObjectEquality")
+        @Override
+        public Token nextToken()
+        {
+            while (true) {
+                state.token = null;
+                state.channel = Token.DEFAULT_CHANNEL;
+                state.tokenStartCharIndex = input.index();
+                state.tokenStartCharPositionInLine = input.getCharPositionInLine();
+                state.tokenStartLine = input.getLine();
+                state.text = null;
+                if (input.LA(1) == CharStream.EOF) {
+                    Token eof = new CommonToken(input, Token.EOF, Token.DEFAULT_CHANNEL, input.index(), input.index());
+                    eof.setLine(getLine());
+                    eof.setCharPositionInLine(getCharPositionInLine());
+                    return eof;
+                }
+                try {
+                    mTokens();
+                    if (state.token == null) {
+                        emit();
+                    }
+                    else if (state.token == Token.SKIP_TOKEN) {
+                        continue;
+                    }
+                    return state.token;
+                }
+                catch (RecognitionException re) {
+                    input.consume();
+                    state.type = LEXER_ERROR;
+                    emit();
+                    return state.token;
+                }
+            }
+        }
     }
 }
