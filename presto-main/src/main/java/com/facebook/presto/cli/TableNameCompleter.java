98455ee0084ee4adaf88c838e5d09990956afc01
Henning Schmiedehausen <hgschmie@fb.com>
2013-02-13 12:27:18 -0800

    add feedback from code review

--- presto-main/src/main/java/com/facebook/presto/cli/TableNameCompleter.java
+++ presto-main/src/main/java/com/facebook/presto/cli/TableNameCompleter.java
@@ -30,40 +30,48 @@ public class TableNameCompleter
     @Override
     public int complete(String buffer, int cursor, List<CharSequence> candidates)
     {
-        if (cursor > 0) {
-            for (int pos = cursor - 1; pos >= -1; pos--) {
-                if (pos == -1 || Character.isWhitespace(buffer.charAt(pos))) {
-                    String prefix = buffer.substring(pos + 1, cursor);
-                    List<String> completion = ImmutableList.copyOf(Splitter.on('.').limit(3).split(prefix));
+        if (cursor <= 0) {
+            return cursor;
+        }
+        int blankPos = findLastBlank(buffer.substring(0, cursor));
 
-                    int len = 0;
-                    SortedSet<String> sortedCandidates = new TreeSet<>();
-                    switch (completion.size()) {
-                        case 0:
-                            break;
-                        case 1:
-                            loadTableNames(sortedCandidates, clientSession.getCatalog(), clientSession.getSchema(), completion.get(0));
-                            loadSchemaNames(sortedCandidates, clientSession.getCatalog(), completion.get(0));
-                            break;
-                        case 2:
-                            // only complete on the last field of the dot separated name.
-                            len += completion.get(0).length() + 1;
-                            loadSchemaNames(sortedCandidates, completion.get(0), completion.get(1));
-                            loadTableNames(sortedCandidates, clientSession.getCatalog(), completion.get(0), completion.get(1));
-                            break;
-                        default:
-                            // only complete on the last field of the dot separated name.
-                            len += completion.get(0).length() + 1;
-                            len += completion.get(1).length() + 1;
-                            loadTableNames(sortedCandidates, completion.get(0), completion.get(1), completion.get(2));
-                            break;
-                    }
-                    candidates.addAll(sortedCandidates);
-                    return pos + len + 1;
-                }
+        String prefix = buffer.substring(blankPos + 1, cursor);
+        List<String> completion = ImmutableList.copyOf(Splitter.on('.').limit(3).split(prefix));
+
+        int len = 0;
+        SortedSet<String> sortedCandidates = new TreeSet<>();
+        switch (completion.size()) {
+        case 0:
+            break;
+        case 1:
+            loadTableNames(sortedCandidates, clientSession.getCatalog(), clientSession.getSchema(), completion.get(0));
+            loadSchemaNames(sortedCandidates, clientSession.getCatalog(), completion.get(0));
+            break;
+        case 2:
+            // only complete on the last field of the dot separated name.
+            len += completion.get(0).length() + 1;
+            loadSchemaNames(sortedCandidates, completion.get(0), completion.get(1));
+            loadTableNames(sortedCandidates, clientSession.getCatalog(), completion.get(0), completion.get(1));
+            break;
+        default:
+            // only complete on the last field of the dot separated name.
+            len += completion.get(0).length() + 1;
+            len += completion.get(1).length() + 1;
+            loadTableNames(sortedCandidates, completion.get(0), completion.get(1), completion.get(2));
+            break;
+        }
+        candidates.addAll(sortedCandidates);
+        return blankPos + len + 1;
+    }
+
+    private int findLastBlank(String buffer)
+    {
+        for (int i = buffer.length() - 1; i >= 0 ; i--) {
+            if (Character.isWhitespace(buffer.charAt(i))) {
+                return i;
             }
         }
-        return cursor;
+        return -1;
     }
 
     private void loadSchemaNames(Collection<String> candidates, String catalogName, String prefix)
