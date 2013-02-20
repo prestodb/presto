1a8525749a15417894ba021bdad7171cd1bc9687
David Phillips <david@acz.org>
2013-02-20 13:12:55 -0800

    Add integration tests for prism client

--- presto-prism/src/main/java/com/facebook/presto/hive/PrismServiceClientProvider.java
+++ presto-prism/src/main/java/com/facebook/presto/hive/PrismServiceClientProvider.java
@@ -3,11 +3,15 @@ package com.facebook.presto.hive;
 import com.facebook.nifty.client.UnframedClientConnector;
 import com.facebook.prism.namespaceservice.PrismServiceClient;
 import com.facebook.swift.service.ThriftClient;
+import com.facebook.swift.service.ThriftClientManager;
 import com.google.common.collect.Lists;
 import com.google.common.net.HostAndPort;
 import com.google.inject.Inject;
 
+import javax.annotation.PreDestroy;
 import javax.annotation.concurrent.ThreadSafe;
+
+import java.io.Closeable;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -17,18 +21,22 @@ import static java.lang.String.format;
 
 @ThreadSafe
 public class PrismServiceClientProvider
+        implements Closeable
 {
     private final ThriftClient<PrismServiceClient> prismThriftClient;
+    private final ThriftClientManager thriftClientManager;
     private final SmcLookup smcLookup;
     private final String prismSmcTier;
 
     @Inject
     public PrismServiceClientProvider(
             ThriftClient<PrismServiceClient> prismThriftClient,
+            ThriftClientManager thriftClientManager,
             SmcLookup smcLookup,
             PrismConfig config)
     {
         this.prismThriftClient = checkNotNull(prismThriftClient, "prismThriftClient is null");
+        this.thriftClientManager = checkNotNull(thriftClientManager, "thriftClientManager is null");
         this.smcLookup = checkNotNull(smcLookup, "smcLookup is null");
         prismSmcTier = checkNotNull(config, "config is null").getPrismSmcTier();
     }
@@ -54,6 +62,14 @@ public class PrismServiceClientProvider
             }
         }
         throw new RuntimeException("Unable to connect to any prism servers in tier: " + prismSmcTier, lastException);
+    }
+
+    @PreDestroy
+    @Override
+    public void close()
+    {
+        // TODO: remove when this is fixed in nifty
+        thriftClientManager.close();
     }
 
     private static <T> List<T> shuffle(Iterable<T> iterable)
