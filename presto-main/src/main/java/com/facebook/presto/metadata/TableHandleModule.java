c75585ab7e12ddcf6b3d22a7e94228685fed6cbf
Henning Schmiedehausen <hgschmie@fb.com>
2013-02-19 18:29:33 -0800

    more code feedback, break out TypeIdResolver, use the module binding in airlift.

--- presto-main/src/main/java/com/facebook/presto/metadata/TableHandleModule.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/TableHandleModule.java
@@ -1,7 +1,6 @@
 package com.facebook.presto.metadata;
 
 import com.facebook.presto.tpch.TpchTableHandle;
-import com.fasterxml.jackson.databind.jsontype.TypeIdResolver;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 
@@ -13,11 +12,7 @@ public class TableHandleModule
 {
     public void configure(Binder binder)
     {
-        // replace next three lines with jsonBinder(binder).addModuleBinding().to(TableHandleJacksonModule.class)
-        // when airlift pull request #40 is applied.
-        jsonBinder(binder).addSerializerBinding(TableHandle.class).to(TableHandleJacksonModule.TableHandleSerializer.class);
-        jsonBinder(binder).addDeserializerBinding(TableHandle.class).to(TableHandleJacksonModule.TableHandleDeserializer.class);
-        binder.bind(TypeIdResolver.class).annotatedWith(ForTableHandle.class).to(TableHandleJacksonModule.class);
+        jsonBinder(binder).addModuleBinding().to(TableHandleJacksonModule.class);
 
         bindTableHandle(binder, "native").toInstance(NativeTableHandle.class);
         bindTableHandle(binder, "internal").toInstance(InternalTableHandle.class);
