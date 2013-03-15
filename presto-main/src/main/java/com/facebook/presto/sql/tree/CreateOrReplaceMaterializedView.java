876e2514729de3cd172f31c3a41b14cbb3ca3df7
Henning Schmiedehausen <hgschmie@fb.com>
2013-03-15 15:28:12 -0700

    CREATE OR REPLACE MATERIALIZED VIEW <x> AS SELECT * FROM <y>
    Rework the old import code to be executed as part of a SQL query.

---
+++ presto-main/src/main/java/com/facebook/presto/sql/tree/CreateOrReplaceMaterializedView.java
@@ -0,0 +1,63 @@
+package com.facebook.presto.sql.tree;
+
+import com.google.common.base.Objects;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class CreateOrReplaceMaterializedView
+        extends Statement
+{
+    private final QualifiedName name;
+    private final Statement tableDefinition;
+
+    public CreateOrReplaceMaterializedView(QualifiedName name, Statement tableDefinition)
+    {
+        this.name = checkNotNull(name, "name is null");
+        this.tableDefinition = checkNotNull(tableDefinition, "tableDefinition is null");
+    }
+
+    public QualifiedName getName()
+    {
+        return name;
+    }
+
+    public Statement getTableDefinition()
+    {
+        return tableDefinition;
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context)
+    {
+        return visitor.visitStatement(this, context);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(name, tableDefinition);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        else if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        CreateOrReplaceMaterializedView o = (CreateOrReplaceMaterializedView) obj;
+        return Objects.equal(name, o.name)
+            && Objects.equal(tableDefinition, o.tableDefinition);
+    }
+
+    @Override
+    public String toString()
+    {
+        return Objects.toStringHelper(this)
+            .add("name", name)
+            .add("tableDefinition", tableDefinition)
+            .toString();
+    }
+}
