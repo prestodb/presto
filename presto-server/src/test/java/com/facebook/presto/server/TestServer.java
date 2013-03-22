ae46719b706f1ebbb301313a4398170fc87f7ac2
David Phillips <david@acz.org>
2013-03-21 18:05:48 -0700

    Extract PrestoTestingServer

--- presto-server/src/test/java/com/facebook/presto/server/TestServer.java
+++ presto-server/src/test/java/com/facebook/presto/server/TestServer.java
@@ -1,33 +1,12 @@
 package com.facebook.presto.server;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.io.Files;
-import com.google.inject.Injector;
-import io.airlift.bootstrap.Bootstrap;
-import io.airlift.bootstrap.LifeCycleManager;
-import io.airlift.discovery.client.testing.TestingDiscoveryModule;
-import io.airlift.event.client.InMemoryEventModule;
+import com.google.common.io.Closeables;
 import io.airlift.http.client.ApacheHttpClient;
 import io.airlift.http.client.HttpClient;
 import io.airlift.http.client.StatusResponseHandler;
-import io.airlift.http.server.testing.TestingHttpServer;
-import io.airlift.http.server.testing.TestingHttpServerModule;
-import io.airlift.jaxrs.JaxrsModule;
-import io.airlift.jmx.JmxHttpModule;
-import io.airlift.jmx.JmxModule;
-import io.airlift.json.JsonModule;
-import io.airlift.log.LogJmxModule;
-import io.airlift.node.testing.TestingNodeModule;
-import io.airlift.testing.FileUtils;
-import io.airlift.tracetoken.TraceTokenModule;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.weakref.jmx.guice.MBeanModule;
-
-import java.io.File;
-import java.net.URI;
-import java.util.Map;
 
 import static io.airlift.http.client.Request.Builder.prepareGet;
 import static io.airlift.http.client.StatusResponseHandler.createStatusResponseHandler;
@@ -36,61 +15,23 @@ import static org.testng.Assert.assertEquals;
 
 public class TestServer
 {
-    private File baseDataDir;
-    private LifeCycleManager lifeCycleManager;
-    private TestingHttpServer server;
+    private TestingPrestoServer server;
     private HttpClient client;
 
     @BeforeMethod
     public void setup()
             throws Exception
     {
-        baseDataDir = Files.createTempDir();
-
-        Map<String, String> serverProperties = ImmutableMap.<String, String>builder()
-                .put("storage-manager.data-directory", baseDataDir.getPath())
-                .put("presto-metastore.db.type", "h2")
-                .put("presto-metastore.db.filename", new File(baseDataDir, "db/MetaStore").getPath())
-                .build();
-
-        Bootstrap app = new Bootstrap(
-                new TestingNodeModule(),
-                new TestingDiscoveryModule(),
-                new TestingHttpServerModule(),
-                new JsonModule(),
-                new JaxrsModule(),
-                new MBeanModule(),
-                new JmxModule(),
-                new JmxHttpModule(),
-                new LogJmxModule(),
-                new InMemoryEventModule(),
-                new TraceTokenModule(),
-                new ServerMainModule());
-
-        Injector injector = app
-                .strictConfig()
-                .doNotInitializeLogging()
-                .setRequiredConfigurationProperties(serverProperties)
-                .initialize();
-
-        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
-
-        server = injector.getInstance(TestingHttpServer.class);
-
+        server = new TestingPrestoServer();
         client = new ApacheHttpClient();
     }
 
+    @SuppressWarnings("deprecation")
     @AfterMethod
     public void teardown()
-            throws Exception
     {
-        if (lifeCycleManager != null) {
-            lifeCycleManager.stop();
-        }
-        if (client != null) {
-            client.close();
-        }
-        FileUtils.deleteRecursively(baseDataDir);
+        Closeables.closeQuietly(server);
+        Closeables.closeQuietly(client);
     }
 
     @Test
@@ -98,14 +39,9 @@ public class TestServer
             throws Exception
     {
         StatusResponseHandler.StatusResponse response = client.execute(
-                prepareGet().setUri(uriFor("/v1/jmx/mbean")).build(),
+                prepareGet().setUri(server.resolve("/v1/jmx/mbean")).build(),
                 createStatusResponseHandler());
 
         assertEquals(response.getStatusCode(), OK.getStatusCode());
-    }
-
-    private URI uriFor(String path)
-    {
-        return server.getBaseUrl().resolve(path);
     }
 }
---
+++ presto-server/src/test/java/com/facebook/presto/server/TestingPrestoServer.java
@@ -0,0 +1,99 @@
+package com.facebook.presto.server;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Files;
+import com.google.inject.Injector;
+import io.airlift.bootstrap.Bootstrap;
+import io.airlift.bootstrap.LifeCycleManager;
+import io.airlift.discovery.client.Announcer;
+import io.airlift.discovery.client.testing.TestingDiscoveryModule;
+import io.airlift.event.client.InMemoryEventModule;
+import io.airlift.http.server.testing.TestingHttpServer;
+import io.airlift.http.server.testing.TestingHttpServerModule;
+import io.airlift.jaxrs.JaxrsModule;
+import io.airlift.jmx.JmxHttpModule;
+import io.airlift.jmx.JmxModule;
+import io.airlift.json.JsonModule;
+import io.airlift.log.LogJmxModule;
+import io.airlift.node.testing.TestingNodeModule;
+import io.airlift.testing.FileUtils;
+import io.airlift.tracetoken.TraceTokenModule;
+import org.weakref.jmx.guice.MBeanModule;
+
+import java.io.Closeable;
+import java.io.File;
+import java.net.URI;
+import java.util.Map;
+
+public class TestingPrestoServer
+        implements Closeable
+{
+    private final File baseDataDir;
+    private final LifeCycleManager lifeCycleManager;
+    private final TestingHttpServer server;
+
+    public TestingPrestoServer()
+            throws Exception
+    {
+        baseDataDir = Files.createTempDir();
+
+        Map<String, String> serverProperties = ImmutableMap.<String, String>builder()
+                .put("storage-manager.data-directory", baseDataDir.getPath())
+                .put("presto-metastore.db.type", "h2")
+                .put("presto-metastore.db.filename", new File(baseDataDir, "db/MetaStore").getPath())
+                .build();
+
+        Bootstrap app = new Bootstrap(
+                new TestingNodeModule(),
+                new TestingDiscoveryModule(),
+                new TestingHttpServerModule(),
+                new JsonModule(),
+                new JaxrsModule(),
+                new MBeanModule(),
+                new JmxModule(),
+                new JmxHttpModule(),
+                new LogJmxModule(),
+                new InMemoryEventModule(),
+                new TraceTokenModule(),
+                new ServerMainModule());
+
+        Injector injector = app
+                .strictConfig()
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperties(serverProperties)
+                .initialize();
+
+        injector.getInstance(Announcer.class).start();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
+
+        server = injector.getInstance(TestingHttpServer.class);
+    }
+
+    @Override
+    public void close()
+    {
+        try {
+            if (lifeCycleManager != null) {
+                lifeCycleManager.stop();
+            }
+        }
+        catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+        finally {
+            FileUtils.deleteRecursively(baseDataDir);
+        }
+    }
+
+    public URI getBaseUrl()
+    {
+        return server.getBaseUrl();
+    }
+
+    public URI resolve(String path)
+    {
+        return server.getBaseUrl().resolve(path);
+    }
+}
