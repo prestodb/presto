c6ba8cc5b5057685f920e4ed6c28fa48fc5c481a
David Phillips <david@acz.org>
2013-03-30 20:36:43 -0700

    Move annotation to upper level

--- presto-server/src/main/java/com/facebook/presto/server/ServerMainModule.java
+++ presto-server/src/main/java/com/facebook/presto/server/ServerMainModule.java
@@ -211,7 +211,7 @@ public class ServerMainModule
         jsonCodecBinder(binder).bindJsonCodec(TaskInfo.class);
         binder.bind(StatementResource.class).in(Scopes.SINGLETON);
         binder.bind(ExecuteResource.class).in(Scopes.SINGLETON);
-        httpClientBinder(binder).bindAsyncHttpClient("execute", ExecuteResource.ForExecute.class);
+        httpClientBinder(binder).bindAsyncHttpClient("execute", ForExecute.class);
 
         binder.install(new HandleJsonModule());
 
