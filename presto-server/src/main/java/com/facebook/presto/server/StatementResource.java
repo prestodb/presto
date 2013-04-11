16767e5187b0d75a180aa8a3678d7afe6bc6f0da
Dain Sundstrom <dain@iq80.com>
2013-04-11 14:41:22 -0700

    Fix bug in StatementResource purger

--- presto-server/src/main/java/com/facebook/presto/server/StatementResource.java
+++ presto-server/src/main/java/com/facebook/presto/server/StatementResource.java
@@ -106,7 +106,7 @@ public class StatementResource
         this.queryManager = checkNotNull(queryManager, "queryManager is null");
         this.httpClient = checkNotNull(httpClient, "httpClient is null");
 
-        queryPurger.scheduleAtFixedRate(new PurgeQueriesRunnable(queries.keySet(), queryManager), 200, 200, TimeUnit.MILLISECONDS);
+        queryPurger.scheduleWithFixedDelay(new PurgeQueriesRunnable(queries.keySet(), queryManager), 200, 200, TimeUnit.MILLISECONDS);
     }
 
     @PreDestroy
@@ -600,9 +600,9 @@ public class StatementResource
                 // do not call queryManager.getQueryInfo() since it updates the heartbeat time
                 Set<QueryId> liveQueries = ImmutableSet.copyOf(transform(queryManager.getAllQueryInfo(), queryIdGetter()));
 
-                Set<QueryId> deadQueries = Sets.difference(liveQueries, queryIdsSnapshot);
+                Set<QueryId> deadQueries = Sets.difference(queryIdsSnapshot, liveQueries);
                 for (QueryId deadQuery : deadQueries) {
-                    queryIdsSnapshot.remove(deadQuery);
+                    queryIds.remove(deadQuery);
                     log.debug("Removed expired query %s", deadQuery);
                 }
             }
