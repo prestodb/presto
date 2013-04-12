982ea3454c1e357d06bc45690b14a31dd4d4052d
Henning Schmiedehausen <hgschmie@fb.com>
2013-04-11 18:20:53 -0700

    Update Presto  PartitionChunk SPI.
    Add methods to PartitionChunk SPI that allow callers to determine what
    a partition a given partition chunk belongs to and whether the a chunk
    is the last one for a partition when iterating over PartitionChunks.

--- presto-spi/src/main/java/com/facebook/presto/spi/PartitionChunk.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/PartitionChunk.java
@@ -5,6 +5,21 @@ import java.util.List;
 
 public interface PartitionChunk
 {
+    /**
+     * Returns the name of the partition that this chunk belongs to.
+     */
+    String getPartitionName();
+
+    /**
+     * Marks the last chunk for a partition. This is transient and only valid
+     * while iterating over the return value from {@link ImportClient#getPartitionChunks(String, String, List, List)} or
+     * {@link ImportClient#getPartitionChunks(String, String, String, List)}.
+     *
+     * If a partition chunk returns true on this method, then no more chunks for the partition named by
+     * {@link PartitionChunk#getPartitionName()} will be returned.
+     */
+    boolean isLastChunk();
+
     long getLength();
 
     List<InetAddress> getHosts();
