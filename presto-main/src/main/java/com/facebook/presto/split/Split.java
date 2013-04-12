fba6b8a7d410861d17026e9e6e66fbca250c925a
Henning Schmiedehausen <hgschmie@fb.com>
2013-04-11 19:32:37 -0700

    Add Writing split.

--- presto-main/src/main/java/com/facebook/presto/split/Split.java
+++ presto-main/src/main/java/com/facebook/presto/split/Split.java
@@ -12,7 +12,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
         @JsonSubTypes.Type(value = NativeSplit.class, name = "native"),
         @JsonSubTypes.Type(value = InternalSplit.class, name = "internal"),
         @JsonSubTypes.Type(value = ImportSplit.class, name = "import"),
-        @JsonSubTypes.Type(value = RemoteSplit.class, name = "remote")})
+        @JsonSubTypes.Type(value = RemoteSplit.class, name = "remote"),
+        @JsonSubTypes.Type(value = WritingSplit.class, name = "writing")})
 public interface Split
 {
     DataSourceType getDataSourceType();
