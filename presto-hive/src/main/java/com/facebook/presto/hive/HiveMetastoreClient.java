68df082a4b244aa754ae7d7ad31d9c3e445a1dab
David Phillips <david@acz.org>
2013-04-16 18:08:10 -0700

    Fix setting timeouts for Hive metastore client

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveMetastoreClient.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveMetastoreClient.java
@@ -1,9 +1,7 @@
 package com.facebook.presto.hive;
 
 import com.facebook.presto.hive.shaded.org.apache.thrift.protocol.TBinaryProtocol;
-import com.facebook.presto.hive.shaded.org.apache.thrift.transport.TSocket;
 import com.facebook.presto.hive.shaded.org.apache.thrift.transport.TTransport;
-import com.facebook.presto.hive.shaded.org.apache.thrift.transport.TTransportException;
 import org.apache.hadoop.hive.metastore.api.ThriftHiveMetastore;
 
 import java.io.Closeable;
@@ -24,13 +22,5 @@ class HiveMetastoreClient
     public void close()
     {
         transport.close();
-    }
-
-    public static HiveMetastoreClient create(String host, int port)
-            throws TTransportException
-    {
-        TSocket tSocket = new TSocket(host, port);
-        tSocket.open();
-        return new HiveMetastoreClient(tSocket);
     }
 }
