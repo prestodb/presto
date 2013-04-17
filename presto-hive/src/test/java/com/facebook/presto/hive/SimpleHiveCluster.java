8b61a6182379dced85e31423fdabef72ae8519e0
David Phillips <david@acz.org>
2013-04-16 18:08:10 -0700

    Fix setting timeouts for Hive metastore client

--- presto-hive/src/test/java/com/facebook/presto/hive/SimpleHiveCluster.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/SimpleHiveCluster.java
@@ -1,11 +1,17 @@
 package com.facebook.presto.hive;
 
+import com.facebook.presto.hive.shaded.org.apache.thrift.transport.TSocket;
+import com.facebook.presto.hive.shaded.org.apache.thrift.transport.TTransport;
 import com.facebook.presto.hive.shaded.org.apache.thrift.transport.TTransportException;
 import com.google.common.base.Throwables;
+import io.airlift.units.Duration;
+
+import java.util.concurrent.TimeUnit;
 
 public class SimpleHiveCluster
         implements HiveCluster
 {
+    private static final Duration TIMEOUT = new Duration(10, TimeUnit.SECONDS);
     private final String host;
     private final int port;
 
@@ -19,7 +25,9 @@ public class SimpleHiveCluster
     public HiveMetastoreClient createMetastoreClient()
     {
         try {
-            return HiveMetastoreClient.create(host, port);
+            TTransport transport = new TSocket(host, port, (int) TIMEOUT.toMillis());
+            transport.open();
+            return new HiveMetastoreClient(transport);
         }
         catch (TTransportException e) {
             throw Throwables.propagate(e);
