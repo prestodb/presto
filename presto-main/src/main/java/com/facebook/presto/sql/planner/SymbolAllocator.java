683dcb8c34406aee4a93ae0d5e52d4b5c9642399
Martin Traverso <martint@fb.com>
2013-04-18 12:39:36 -0700

    Move SymbolAllocator to planner package

--- presto-main/src/main/java/com/facebook/presto/sql/planner/SymbolAllocator.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/SymbolAllocator.java
@@ -1,6 +1,6 @@
-package com.facebook.presto.sql.analyzer;
+package com.facebook.presto.sql.planner;
 
-import com.facebook.presto.sql.planner.Symbol;
+import com.facebook.presto.sql.analyzer.Type;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.QualifiedName;
 import com.facebook.presto.sql.tree.QualifiedNameReference;
