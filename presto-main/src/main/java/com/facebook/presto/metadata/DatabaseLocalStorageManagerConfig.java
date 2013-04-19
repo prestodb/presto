e239880d08e7da2ab34297ce0f0011c6072c57d1
Henning Schmiedehausen <hgschmie@fb.com>
2013-04-19 14:58:39 -0700

    make tasks per node configurable

--- presto-main/src/main/java/com/facebook/presto/metadata/DatabaseLocalStorageManagerConfig.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/DatabaseLocalStorageManagerConfig.java
@@ -3,12 +3,15 @@ package com.facebook.presto.metadata;
 import io.airlift.configuration.Config;
 import io.airlift.configuration.ConfigDescription;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+
 import java.io.File;
 
 public class DatabaseLocalStorageManagerConfig
 {
     private File dataDirectory = new File("var/data");
+    private int tasksPerNode = 32;
 
     @NotNull
     public File getDataDirectory()
@@ -21,6 +24,20 @@ public class DatabaseLocalStorageManagerConfig
     public DatabaseLocalStorageManagerConfig setDataDirectory(File dataDirectory)
     {
         this.dataDirectory = dataDirectory;
+        return this;
+    }
+
+    @Min(1)
+    public int getTasksPerNode()
+    {
+        return tasksPerNode;
+    }
+
+    @Config("storage-manager.tasks-per-node")
+    @ConfigDescription("Number of background tasks")
+    public DatabaseLocalStorageManagerConfig setTasksPerNode(int tasksPerNode)
+    {
+        this.tasksPerNode = tasksPerNode;
         return this;
     }
 }
