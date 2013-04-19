c513fe97d0bba7971ad45bc9f098d8edfb3c1102
Henning Schmiedehausen <hgschmie@fb.com>
2013-04-19 11:24:55 -0700

    code review feedback

--- presto-main/src/main/java/com/facebook/presto/storage/StorageManager.java
+++ presto-main/src/main/java/com/facebook/presto/storage/StorageManager.java
@@ -3,11 +3,19 @@ package com.facebook.presto.storage;
 import com.facebook.presto.metadata.NativeTableHandle;
 import com.facebook.presto.metadata.QualifiedTableName;
 
+/**
+ * Handles the sources for materialized view.
+ *
+ * TODO (after discussion). These methods should be part of the metadata manager and the metadata manager
+ * should handle resolution of table handles (and information storage).
+ *
+ * This change should be made after the big import client / metadata changes went in.
+ */
 public interface StorageManager
 {
-    void insertSourceTable(NativeTableHandle tableHandle, QualifiedTableName sourceTableName);
+    void insertTableSource(NativeTableHandle tableHandle, QualifiedTableName sourceTableName);
 
-    public QualifiedTableName getSourceTable(NativeTableHandle tableHandle);
+    public QualifiedTableName getTableSource(NativeTableHandle tableHandle);
 
-    public void dropSourceTable(NativeTableHandle tableHandle);
+    public void dropTableSource(NativeTableHandle tableHandle);
 }
