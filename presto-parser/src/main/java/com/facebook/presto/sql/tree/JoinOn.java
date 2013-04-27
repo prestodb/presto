df44b0e2d016a27a4290ce91625d8e1527fbbaa9
David Phillips <david@acz.org>
2013-04-26 18:31:37 -0700

    Fix equals and hashcode for joins

--- presto-parser/src/main/java/com/facebook/presto/sql/tree/JoinOn.java
+++ presto-parser/src/main/java/com/facebook/presto/sql/tree/JoinOn.java
@@ -2,6 +2,8 @@ package com.facebook.presto.sql.tree;
 
 import com.google.common.base.Objects;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class JoinOn
         extends JoinCriteria
 {
@@ -9,12 +11,31 @@ public class JoinOn
 
     public JoinOn(Expression expression)
     {
-        this.expression = expression;
+        this.expression = checkNotNull(expression, "expression is null");
     }
 
     public Expression getExpression()
     {
         return expression;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        JoinOn o = (JoinOn) obj;
+        return Objects.equal(expression, o.expression);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(expression);
     }
 
     @Override
--- presto-parser/src/main/java/com/facebook/presto/sql/tree/JoinUsing.java
+++ presto-parser/src/main/java/com/facebook/presto/sql/tree/JoinUsing.java
@@ -1,8 +1,12 @@
 package com.facebook.presto.sql.tree;
 
 import com.google.common.base.Objects;
+import com.google.common.collect.ImmutableList;
 
 import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class JoinUsing
         extends JoinCriteria
@@ -11,12 +15,33 @@ public class JoinUsing
 
     public JoinUsing(List<String> columns)
     {
-        this.columns = columns;
+        checkNotNull(columns, "columns is null");
+        checkArgument(!columns.isEmpty(), "columns is empty");
+        this.columns = ImmutableList.copyOf(columns);
     }
 
     public List<String> getColumns()
     {
         return columns;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        JoinUsing o = (JoinUsing) obj;
+        return Objects.equal(columns, o.columns);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(columns);
     }
 
     @Override
