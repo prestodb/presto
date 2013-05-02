ca403a68fe7e3934cedcd2ef7db616e5f3a00e94
Dain Sundstrom <dain@iq80.com>
2013-05-02 10:21:53 -0700

    Fix broken show table test

--- presto-server/src/test/java/com/facebook/presto/server/TestDistributedQueries.java
+++ presto-server/src/test/java/com/facebook/presto/server/TestDistributedQueries.java
@@ -152,7 +152,7 @@ public class TestDistributedQueries
             @Override
             public String apply(MaterializedTuple input)
             {
-                assertEquals(input.getFieldCount(), 3);
+                assertEquals(input.getFieldCount(), 4);
                 return (String) input.getField(0);
             }
         }));
