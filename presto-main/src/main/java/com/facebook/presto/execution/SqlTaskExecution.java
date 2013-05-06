bbe1b9a42da423745adb29c29c687e2545bb4202
Martin Traverso <martint@fb.com>
2013-05-06 11:12:13 -0700

    Fix starvation (task should run if the query is not done)

--- presto-main/src/main/java/com/facebook/presto/execution/SqlTaskExecution.java
+++ presto-main/src/main/java/com/facebook/presto/execution/SqlTaskExecution.java
@@ -420,7 +420,7 @@ public class SqlTaskExecution
             while (!taskOutput.getState().isDone()) {
                 FutureTask<?> futureTask = scheduledWorkers.pollFirst(1, TimeUnit.SECONDS);
                 // if we got a task and the state is not done, run the task
-                if (futureTask != null && taskOutput.getState().isDone()) {
+                if (futureTask != null && !taskOutput.getState().isDone()) {
                     futureTask.run();
                 }
             }
