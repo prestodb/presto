cd4e8c6894f5fb1f8ed7415591d53502c1c12f19
Martin Traverso <martint@fb.com>
2013-05-06 09:19:01 -0700

    Add benchmark for LIKE

---
+++ presto-main/src/test/java/com/facebook/presto/benchmark/SqlLikeBenchmark.java
@@ -0,0 +1,45 @@
+package com.facebook.presto.benchmark;
+
+import com.facebook.presto.block.Block;
+import com.facebook.presto.block.BlockCursor;
+import com.facebook.presto.operator.Operator;
+import com.facebook.presto.operator.OperatorStats;
+import com.facebook.presto.operator.Page;
+import com.facebook.presto.operator.PageIterator;
+import com.facebook.presto.tpch.TpchBlocksProvider;
+
+public class SqlLikeBenchmark
+        extends AbstractSqlBenchmark
+{
+    public SqlLikeBenchmark()
+    {
+        super("sql_like", 4, 5, "SELECT orderkey FROM lineitem WHERE comment LIKE '%ly%ly%'");
+    }
+
+    @Override
+    protected long[] execute(TpchBlocksProvider blocksProvider)
+    {
+        Operator operator = createBenchmarkedOperator(blocksProvider);
+
+        long outputRows = 0;
+        long outputBytes = 0;
+        PageIterator iterator = operator.iterator(new OperatorStats());
+        while (iterator.hasNext()) {
+            Page page = iterator.next();
+            BlockCursor cursor = page.getBlock(0).cursor();
+            while (cursor.advanceNextPosition()) {
+                outputRows++;
+            }
+
+            for (Block block : page.getBlocks()) {
+                outputBytes += block.getDataSize().toBytes();
+            }
+        }
+        return new long[] {outputRows, outputBytes};
+    }
+
+    public static void main(String[] args)
+    {
+        new SqlLikeBenchmark().runBenchmark(new SimpleLineBenchmarkResultWriter(System.out));
+    }
+}
