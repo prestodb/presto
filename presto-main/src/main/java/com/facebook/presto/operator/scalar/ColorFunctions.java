5ca8f79769070f655b5154953f8c067a0b31567b
Martin Traverso <martint@fb.com>
2013-05-23 23:55:11 -0700

    Add color rendering functions

---
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/ColorFunctions.java
@@ -0,0 +1,222 @@
+package com.facebook.presto.operator.scalar;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+
+import java.awt.Color;
+
+import static com.facebook.presto.operator.scalar.StringFunctions.upper;
+import static com.google.common.base.Charsets.UTF_8;
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.lang.String.format;
+
+public class ColorFunctions
+{
+    private static final String ANSI_RESET = "\u001b[0m";
+
+    private enum SystemColor
+    {
+        BLACK(0),
+        RED(1),
+        GREEN(2),
+        YELLOW(3),
+        BLUE(4),
+        MAGENTA(5),
+        CYAN(6),
+        WHITE(7);
+
+        private final int index;
+
+        SystemColor(int index)
+        {
+            this.index = index;
+        }
+
+        private int getIndex()
+        {
+            return index;
+        }
+    }
+
+    @ScalarFunction
+    public static long color(Slice color)
+    {
+        int rgb = parseRgb(color);
+
+        if (rgb != -1) {
+            return rgb;
+        }
+
+        // encode system colors (0-15) as negative values, offset by one
+        try {
+            SystemColor systemColor = SystemColor.valueOf(upper(color).toString(UTF_8));
+            int index = systemColor.getIndex();
+            return -(index + 1);
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(format("Invalid color: '%s'", color.toString(Charsets.UTF_8)), e);
+        }
+    }
+
+    @ScalarFunction
+    public static long rgb(long red, long green, long blue)
+    {
+        checkArgument(red >= 0 && red <= 255, "red must be between 0 and 255");
+        checkArgument(green >= 0 && green <= 255, "green must be between 0 and 255");
+        checkArgument(blue >= 0 && blue <= 255, "blue must be between 0 and 255");
+
+        return (red << 16) | (green << 8) | blue;
+    }
+
+    /**
+     * Interpolate a color between lowColor and highColor based the provided value
+     *
+     * The value is truncated to the range [low, high] if it's outside.
+     * Color must be a valid rgb value of the form #rgb
+     */
+    @ScalarFunction
+    public static long color(double value, double low, double high, Slice lowColor, Slice highColor)
+    {
+        return color((value - low) * 1.0 / (high - low), lowColor, highColor);
+    }
+
+    /**
+     * Interpolate a color between lowColor and highColor based on the provided value
+     *
+     * The value is truncated to the range [0, 1] if necessary
+     * Color must be a valid rgb value of the form #rgb
+     */
+    @ScalarFunction
+    public static long color(double fraction, Slice lowColor, Slice highColor)
+    {
+        int lowRgb = parseRgb(lowColor);
+        int highRgb = parseRgb(highColor);
+
+        Preconditions.checkArgument(lowRgb != -1, "lowColor not a valid RGB color: %s", lowColor.toString(Charsets.UTF_8));
+        Preconditions.checkArgument(highRgb != -1, "highColor not a valid RGB color: %s", lowColor.toString(Charsets.UTF_8));
+
+        fraction = Math.min(1, fraction);
+        fraction = Math.max(0, fraction);
+
+        return interpolate((float) fraction, lowRgb, highRgb);
+    }
+
+    @ScalarFunction
+    public static Slice render(Slice value, long color)
+    {
+        StringBuilder builder = new StringBuilder(value.length());
+
+        // color
+        builder.append(ansiColorEscape(color))
+                .append(value.toString(Charsets.UTF_8))
+                .append(ANSI_RESET);
+
+        return Slices.copiedBuffer(builder.toString(), Charsets.UTF_8);
+    }
+
+    @ScalarFunction
+    public static Slice render(long value, long color)
+    {
+        return render(Slices.copiedBuffer(Long.toString(value), Charsets.UTF_8), color);
+    }
+
+    @ScalarFunction
+    public static Slice render(double value, long color)
+    {
+        return render(Slices.copiedBuffer(Double.toString(value), Charsets.UTF_8), color);
+    }
+
+
+    private static int interpolate(float fraction, long lowRgb, long highRgb)
+    {
+        float[] lowHsv = Color.RGBtoHSB(getRed(lowRgb), getGreen(lowRgb), getBlue(lowRgb), null);
+        float[] highHsv = Color.RGBtoHSB(getRed(highRgb), getGreen(highRgb), getBlue(highRgb), null);
+
+        float h = fraction * (highHsv[0] - lowHsv[0]) + lowHsv[0];
+        float s = fraction * (highHsv[1] - lowHsv[1]) + lowHsv[1];
+        float v = fraction * (highHsv[2] - lowHsv[2]) + lowHsv[2];
+
+        return Color.HSBtoRGB(h, s, v) & 0xFF_FF_FF;
+    }
+
+    /**
+     * Convert the given color (rgb or system) to an ansi-compatible index (for use with ESC[38;5;<value>m)
+     */
+    private static int toAnsi(int red, int green, int blue)
+    {
+        // rescale to 0-5 range
+        red = red * 6 / 256;
+        green = green * 6 / 256;
+        blue = blue * 6 / 256;
+
+        return 16 + red * 36 + green * 6 + blue;
+    }
+
+    private static String ansiColorEscape(long color)
+    {
+        return "\u001b[38;5;" + toAnsi(color) + 'm';
+    }
+
+    /**
+     * Convert the given color (rgb or system) to an ansi-compatible index (for use with ESC[38;5;<value>m)
+     */
+    private static int toAnsi(long color)
+    {
+        if (color >= 0) { // an rgb value encoded as in Color.getRGB
+            return toAnsi(getRed(color), getGreen(color), getBlue(color));
+        }
+        else {
+            return (int) (-color - 1);
+        }
+    }
+
+    @VisibleForTesting
+    static int parseRgb(Slice color)
+    {
+        if (color.length() != 4 || color.getByte(0) != '#') {
+            return -1;
+        }
+
+        int red = Character.digit((char) color.getByte(1), 16);
+        int green = Character.digit((char) color.getByte(2), 16);
+        int blue = Character.digit((char) color.getByte(3), 16);
+
+        if (red == -1 || green == -1 || blue == -1) {
+            return -1;
+        }
+
+        // replicate the nibbles to turn a color of the form #rgb => #rrggbb (css semantics)
+        red = (red << 4) | red;
+        green = (green << 4) | green;
+        blue = (blue << 4) | blue;
+
+        return (int) rgb(red, green, blue);
+    }
+
+    @VisibleForTesting
+    static int getRed(long color)
+    {
+        checkArgument(color >= 0, "color is not a valid rgb value");
+
+        return (int) ((color >>> 16) & 0xff);
+    }
+
+    @VisibleForTesting
+    static int getGreen(long color)
+    {
+        checkArgument(color >= 0, "color is not a valid rgb value");
+
+        return (int) ((color >>> 8) & 0xff);
+    }
+
+    @VisibleForTesting
+    static int getBlue(long color)
+    {
+        checkArgument(color >= 0, "color is not a valid rgb value");
+
+        return (int) (color & 0xff);
+    }
+}
---
+++ presto-main/src/test/java/com/facebook/presto/operator/scalar/TestColorFunctions.java
@@ -0,0 +1,121 @@
+package com.facebook.presto.operator.scalar;
+
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.operator.scalar.ColorFunctions.color;
+import static com.facebook.presto.operator.scalar.ColorFunctions.getBlue;
+import static com.facebook.presto.operator.scalar.ColorFunctions.getGreen;
+import static com.facebook.presto.operator.scalar.ColorFunctions.getRed;
+import static com.facebook.presto.operator.scalar.ColorFunctions.parseRgb;
+import static com.facebook.presto.operator.scalar.ColorFunctions.render;
+import static com.facebook.presto.operator.scalar.ColorFunctions.rgb;
+import static com.google.common.base.Charsets.UTF_8;
+import static org.testng.Assert.assertEquals;
+
+public class TestColorFunctions
+{
+    @Test
+    public void testParseRgb()
+            throws Exception
+    {
+        assertEquals(parseRgb(toSlice("#000")), 0x00_00_00);
+        assertEquals(parseRgb(toSlice("#FFF")), 0xFF_FF_FF);
+        assertEquals(parseRgb(toSlice("#F00")), 0xFF_00_00);
+        assertEquals(parseRgb(toSlice("#0F0")), 0x00_FF_00);
+        assertEquals(parseRgb(toSlice("#00F")), 0x00_00_FF);
+        assertEquals(parseRgb(toSlice("#700")), 0x77_00_00);
+        assertEquals(parseRgb(toSlice("#070")), 0x00_77_00);
+        assertEquals(parseRgb(toSlice("#007")), 0x00_00_77);
+
+        assertEquals(parseRgb(toSlice("#cde")), 0xCC_DD_EE);
+    }
+
+    @Test
+    public void testGetComponent()
+            throws Exception
+    {
+        assertEquals(getRed(parseRgb(toSlice("#789"))), 0x77);
+        assertEquals(getGreen(parseRgb(toSlice("#789"))), 0x88);
+        assertEquals(getBlue(parseRgb(toSlice("#789"))), 0x99);
+    }
+
+    @Test
+    public void testToRgb()
+            throws Exception
+    {
+        assertEquals(rgb(0xFF, 0, 0), 0xFF_00_00);
+        assertEquals(rgb(0, 0xFF, 0), 0x00_FF_00);
+        assertEquals(rgb(0, 0, 0xFF), 0x00_00_FF);
+    }
+
+    @Test
+    public void testColor()
+            throws Exception
+    {
+        assertEquals(color(toSlice("black")), -1);
+        assertEquals(color(toSlice("red")), -2);
+        assertEquals(color(toSlice("green")), -3);
+        assertEquals(color(toSlice("yellow")), -4);
+        assertEquals(color(toSlice("blue")), -5);
+        assertEquals(color(toSlice("magenta")), -6);
+        assertEquals(color(toSlice("cyan")), -7);
+        assertEquals(color(toSlice("white")), -8);
+
+        assertEquals(color(toSlice("#f00")), 0xFF_00_00);
+        assertEquals(color(toSlice("#0f0")), 0x00_FF_00);
+        assertEquals(color(toSlice("#00f")), 0x00_00_FF);
+    }
+
+    @Test
+    public void testRenderString()
+            throws Exception
+    {
+        assertEquals(render(toSlice("hello"), color(toSlice("red"))), toSlice("\u001b[38;5;1mhello\u001b[0m"));
+
+        assertEquals(render(toSlice("hello"), color(toSlice("#f00"))), toSlice("\u001b[38;5;196mhello\u001b[0m"));
+        assertEquals(render(toSlice("hello"), color(toSlice("#0f0"))), toSlice("\u001b[38;5;46mhello\u001b[0m"));
+        assertEquals(render(toSlice("hello"), color(toSlice("#00f"))), toSlice("\u001b[38;5;21mhello\u001b[0m"));
+    }
+
+    @Test
+    public void testRenderLong()
+            throws Exception
+    {
+        assertEquals(render(1234, color(toSlice("red"))), toSlice("\u001b[38;5;1m1234\u001b[0m"));
+
+        assertEquals(render(1234, color(toSlice("#f00"))), toSlice("\u001b[38;5;196m1234\u001b[0m"));
+        assertEquals(render(1234, color(toSlice("#0f0"))), toSlice("\u001b[38;5;46m1234\u001b[0m"));
+        assertEquals(render(1234, color(toSlice("#00f"))), toSlice("\u001b[38;5;21m1234\u001b[0m"));
+    }
+
+    @Test
+    public void testRenderDouble()
+            throws Exception
+    {
+        assertEquals(render(1234.5678, color(toSlice("red"))), toSlice("\u001b[38;5;1m1234.5678\u001b[0m"));
+
+        assertEquals(render(1234.5678, color(toSlice("#f00"))), toSlice("\u001b[38;5;196m1234.5678\u001b[0m"));
+        assertEquals(render(1234.5678, color(toSlice("#0f0"))), toSlice("\u001b[38;5;46m1234.5678\u001b[0m"));
+        assertEquals(render(1234.5678, color(toSlice("#00f"))), toSlice("\u001b[38;5;21m1234.5678\u001b[0m"));
+    }
+
+    @Test
+    public void testInterpolate()
+            throws Exception
+    {
+        assertEquals(color(0, 0, 255, toSlice("#000"), toSlice("#fff")), 0x00_00_00);
+        assertEquals(color(128, 0, 255, toSlice("#000"), toSlice("#fff")), 0x80_80_80);
+        assertEquals(color(255, 0, 255, toSlice("#000"), toSlice("#fff")), 0xFF_FF_FF);
+
+        assertEquals(color(0.0, toSlice("#000"), toSlice("#fff")), 0x00_00_00);
+        assertEquals(color(0.5, toSlice("#000"), toSlice("#fff")), 0x80_80_80);
+        assertEquals(color(1.0, toSlice("#000"), toSlice("#fff")), 0xFF_FF_FF);
+    }
+
+    private static Slice toSlice(String string)
+    {
+        return Slices.copiedBuffer(string, UTF_8);
+    }
+}
