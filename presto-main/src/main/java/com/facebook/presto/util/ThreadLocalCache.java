f52949845e1a8b12493147dd59eee9eb1fb35a3b
David Phillips <david@acz.org>
2013-05-30 12:20:47 -0700

    Make ThreadLocalCache not allow nulls

--- presto-main/src/main/java/com/facebook/presto/util/ThreadLocalCache.java
+++ presto-main/src/main/java/com/facebook/presto/util/ThreadLocalCache.java
@@ -1,25 +1,36 @@
 package com.facebook.presto.util;
 
+import javax.annotation.Nonnull;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
- * Provides a ThreadLocal cache with a maximum cache size per thread
+ * Provides a ThreadLocal cache with a maximum cache size per thread.
+ * Values must not be null.
  *
  * @param <K> - cache key type
  * @param <V> - cache value type
  */
 public abstract class ThreadLocalCache<K, V>
 {
+    @SuppressWarnings("ThreadLocalNotStaticFinal")
     private final ThreadLocal<LinkedHashMap<K, V>> cache;
 
-    public ThreadLocalCache(final int maxSizePerThread)
+    protected ThreadLocalCache(final int maxSizePerThread)
     {
-        cache = new ThreadLocal<LinkedHashMap<K, V>>() {
+        checkArgument(maxSizePerThread > 0, "max size must be greater than zero");
+        cache = new ThreadLocal<LinkedHashMap<K, V>>()
+        {
+            @SuppressWarnings({"CloneableClassWithoutClone", "ClassExtendsConcreteCollection"})
             @Override
             protected LinkedHashMap<K, V> initialValue()
             {
-                return new LinkedHashMap<K, V>() {
+                return new LinkedHashMap<K, V>()
+                {
                     @Override
                     protected boolean removeEldestEntry(Map.Entry<K, V> eldest)
                     {
@@ -30,18 +41,21 @@ public abstract class ThreadLocalCache<K, V>
         };
     }
 
+    @Nonnull
     protected abstract V load(K key);
 
-    public V get(K key)
+    public final V get(K key)
     {
         LinkedHashMap<K, V> map = cache.get();
-        if (map.containsKey(key)) {
-            return map.get(key);
-        }
-        else {
-            V value = load(key);
-            map.put(key, value);
+
+        V value = map.get(key);
+        if (value != null) {
             return value;
         }
+
+        value = load(key);
+        checkNotNull(value, "value must not be null");
+        map.put(key, value);
+        return value;
     }
 }
