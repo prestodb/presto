13710fbe67f28a499d120d13ef3d6f2d9dc9df85
Dain Sundstrom <dain@iq80.com>
2013-06-12 18:10:29 -0700

    Improve error message of cast operators

--- presto-main/src/main/java/com/facebook/presto/sql/Casts.java
+++ presto-main/src/main/java/com/facebook/presto/sql/Casts.java
@@ -14,75 +14,103 @@ public class Casts
 
     public static Object toBoolean(Object value)
     {
-        if (value instanceof Boolean) {
-            return value;
-        }
-        else if (value instanceof Long) {
-            return ((Long) value).longValue() != 0;
-        }
-        else if (value instanceof Slice) {
-            String stringValue = ((Slice) value).toString(UTF_8).toLowerCase();
-            if (stringValue.equals("t") || stringValue.equals("true") || stringValue.equals("1")) {
-                return true;
+        try {
+            if (value instanceof Boolean) {
+                return value;
             }
-            if (stringValue.equals("f") || stringValue.equals("false") || stringValue.equals("0")) {
-                return false;
+            else if (value instanceof Long) {
+                return ((Long) value).longValue() != 0;
             }
+            else if (value instanceof Slice) {
+                String stringValue = ((Slice) value).toString(UTF_8).toLowerCase();
+                if (stringValue.equals("t") || stringValue.equals("true") || stringValue.equals("1")) {
+                    return true;
+                }
+                if (stringValue.equals("f") || stringValue.equals("false") || stringValue.equals("0")) {
+                    return false;
+                }
+            }
+        }
+        catch (Exception ignored) {
         }
 
-        throw new IllegalArgumentException(String.format("Cannot cast %s to BOOLEAN", value.getClass().getSimpleName()));
+        if (value instanceof Slice) {
+            value = "'" + ((Slice) value).toString(UTF_8) + "'";
+        }
+        throw new IllegalArgumentException(String.format("Cannot cast %s to BOOLEAN", value));
     }
 
     public static Object toLong(Object value)
     {
-        if (value instanceof Long) {
-            return value;
+        try {
+            if (value instanceof Long) {
+                return value;
+            }
+            else if (value instanceof Boolean) {
+                return (Boolean) value ? 1L : 0L;
+            }
+            else if (value instanceof Slice) {
+                String stringValue = ((Slice) value).toString(UTF_8);
+                return Long.parseLong(stringValue);
+            }
+            else if (value instanceof Double) {
+                return (long) MathFunctions.round(((Double) value).doubleValue());
+            }
         }
-        else if (value instanceof Boolean) {
-            return (Boolean) value ? 1L : 0L;
-        }
-        else if (value instanceof Slice) {
-            String stringValue = ((Slice) value).toString(UTF_8);
-            return Long.parseLong(stringValue);
-        }
-        else if (value instanceof Double) {
-            return (long) MathFunctions.round(((Double) value).doubleValue());
+        catch (Exception ignored) {
         }
 
-        throw new IllegalArgumentException(String.format("Cannot cast %s to BIGINT", value.getClass().getSimpleName()));
+        if (value instanceof Slice) {
+            value = "'" + ((Slice) value).toString(UTF_8) + "'";
+        }
+        throw new IllegalArgumentException(String.format("Cannot cast %s to BIGINT", value));
     }
 
     public static Object toSlice(Object value)
     {
-        if (value instanceof Slice) {
-            return value;
+        try {
+            if (value instanceof Slice) {
+                return value;
+            }
+            else if (value instanceof Long || value instanceof Double) {
+                return Slices.copiedBuffer(value.toString(), US_ASCII);
+            }
+            else if (value instanceof Boolean) {
+                return (Boolean) value ? TRUE : FALSE;
+            }
         }
-        else if (value instanceof Long || value instanceof Double) {
-            return Slices.copiedBuffer(value.toString(), US_ASCII);
-        }
-        else if (value instanceof Boolean) {
-            return (Boolean) value ? TRUE : FALSE;
+        catch (Exception ignored) {
         }
 
-        throw new IllegalArgumentException(String.format("Cannot cast %s to VARCHAR", value.getClass().getSimpleName()));
+        if (value instanceof Slice) {
+            value = "'" + ((Slice) value).toString(UTF_8) + "'";
+        }
+        throw new IllegalArgumentException(String.format("Cannot cast %s to VARCHAR", value));
     }
 
     public static Object toDouble(Object value)
     {
-        if (value instanceof Double) {
-            return value;
+        try {
+            if (value instanceof Double) {
+                return value;
+            }
+            else if (value instanceof Boolean) {
+                return ((Boolean) value) ? 1.0 : 0.0;
+            }
+            else if (value instanceof Long) {
+                return ((Long) value).doubleValue();
+            }
+            else if (value instanceof Slice) {
+                String stringValue = ((Slice) value).toString(UTF_8);
+                return Double.parseDouble(stringValue);
+            }
         }
-        else if (value instanceof Boolean) {
-            return ((Boolean) value) ? 1.0 : 0.0;
-        }
-        else if (value instanceof Long) {
-            return ((Long) value).doubleValue();
-        }
-        else if (value instanceof Slice) {
-            String stringValue = ((Slice) value).toString(UTF_8);
-            return Double.parseDouble(stringValue);
+        catch (Exception ignored) {
         }
 
-        throw new IllegalArgumentException(String.format("Cannot cast %s to DOUBLE", value.getClass().getSimpleName()));
+        if (value instanceof Slice) {
+            value = "'" + ((Slice) value).toString(UTF_8) + "'";
+        }
+        throw new IllegalArgumentException(String.format("Cannot cast %s to DOUBLE", value));
     }
 }
