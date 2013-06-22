63264e72dda2131085f984bf76f403180fd26450
David Phillips <david@acz.org>
2013-06-21 19:03:12 -0700

    Use system proxy settings for JDBC driver

--- presto-jdbc/src/main/java/com/facebook/presto/jdbc/QueryExecutor.java
+++ presto-jdbc/src/main/java/com/facebook/presto/jdbc/QueryExecutor.java
@@ -4,6 +4,7 @@ import com.facebook.presto.client.ClientSession;
 import com.facebook.presto.client.QueryResults;
 import com.facebook.presto.client.StatementClient;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.net.HostAndPort;
 import io.airlift.http.client.AsyncHttpClient;
 import io.airlift.http.client.HttpClientConfig;
 import io.airlift.http.client.netty.NettyAsyncHttpClientConfig;
@@ -12,7 +13,13 @@ import io.airlift.http.client.netty.StandaloneNettyAsyncHttpClient;
 import io.airlift.json.JsonCodec;
 import io.airlift.units.Duration;
 
+import javax.annotation.Nullable;
+
 import java.io.Closeable;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -24,7 +31,7 @@ public class QueryExecutor
     private final JsonCodec<QueryResults> queryInfoCodec;
     private final AsyncHttpClient httpClient;
 
-    private QueryExecutor(String userAgent, JsonCodec<QueryResults> queryResultsCodec)
+    private QueryExecutor(String userAgent, JsonCodec<QueryResults> queryResultsCodec, HostAndPort socksProxy)
     {
         checkNotNull(userAgent, "userAgent is null");
         checkNotNull(queryResultsCodec, "queryResultsCodec is null");
@@ -32,7 +39,8 @@ public class QueryExecutor
         this.queryInfoCodec = queryResultsCodec;
         this.httpClient = new StandaloneNettyAsyncHttpClient("jdbc",
                 new HttpClientConfig()
-                        .setConnectTimeout(new Duration(10, TimeUnit.SECONDS)),
+                        .setConnectTimeout(new Duration(10, TimeUnit.SECONDS))
+                        .setSocksProxy(socksProxy),
                 new NettyAsyncHttpClientConfig(),
                 new NettyIoPoolConfig(),
                 ImmutableSet.of(new UserAgentRequestFilter(userAgent)));
@@ -59,6 +67,21 @@ public class QueryExecutor
 
     public static QueryExecutor create(String userAgent)
     {
-        return new QueryExecutor(userAgent, jsonCodec(QueryResults.class));
+        return new QueryExecutor(userAgent, jsonCodec(QueryResults.class), getSystemSocksProxy());
+    }
+
+    @Nullable
+    private static HostAndPort getSystemSocksProxy()
+    {
+        URI uri = URI.create("socket://0.0.0.0:80");
+        for (Proxy proxy : ProxySelector.getDefault().select(uri)) {
+            if (proxy.type() == Proxy.Type.SOCKS) {
+                if (proxy.address() instanceof InetSocketAddress) {
+                    InetSocketAddress address = (InetSocketAddress) proxy.address();
+                    return HostAndPort.fromParts(address.getHostString(), address.getPort());
+                }
+            }
+        }
+        return null;
     }
 }
