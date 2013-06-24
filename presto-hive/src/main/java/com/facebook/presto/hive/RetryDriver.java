e26f5de0a25191db8d6d58d0b24bfa93080332e8
Eric Hwang <ehwang@fb.com>
2013-06-24 14:04:59 -0700

    Add max retry time to RetryDriver

--- presto-hive/src/main/java/com/facebook/presto/hive/RetryDriver.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/RetryDriver.java
@@ -17,22 +17,25 @@ public class RetryDriver
     private static final Logger log = Logger.get(RetryDriver.class);
     private static final int DEFAULT_RETRY_ATTEMPTS = 10;
     private static final Duration DEFAULT_SLEEP_TIME = Duration.valueOf("1s");
+    private static final Duration DEFAULT_MAX_RETRY_TIME = Duration.valueOf("30s");
 
     private final int maxRetryAttempts;
     private final Duration sleepTime;
+    private final Duration maxRetryTime;
     private final List<Class<? extends Exception>> exceptionWhiteList;
 
-    private RetryDriver(int maxRetryAttempts, Duration sleepTime, List<Class<? extends Exception>> exceptionWhiteList)
+    private RetryDriver(int maxRetryAttempts, Duration sleepTime, Duration maxRetryTime, List<Class<? extends Exception>> exceptionWhiteList)
     {
         this.maxRetryAttempts = maxRetryAttempts;
         this.sleepTime = sleepTime;
+        this.maxRetryTime = maxRetryTime;
         this.exceptionWhiteList = exceptionWhiteList;
 
     }
 
     private RetryDriver()
     {
-        this(DEFAULT_RETRY_ATTEMPTS, DEFAULT_SLEEP_TIME, ImmutableList.<Class<? extends Exception>>of());
+        this(DEFAULT_RETRY_ATTEMPTS, DEFAULT_SLEEP_TIME, DEFAULT_MAX_RETRY_TIME, ImmutableList.<Class<? extends Exception>>of());
     }
 
     public static RetryDriver retry()
@@ -43,12 +46,17 @@ public class RetryDriver
     public RetryDriver withMaxRetries(int maxRetryAttempts)
     {
         checkArgument(maxRetryAttempts > 0, "maxRetryAttempts must be greater than zero");
-        return new RetryDriver(maxRetryAttempts, sleepTime, exceptionWhiteList);
+        return new RetryDriver(maxRetryAttempts, sleepTime, maxRetryTime, exceptionWhiteList);
     }
 
     public RetryDriver withSleep(Duration sleepTime)
     {
-        return new RetryDriver(maxRetryAttempts, checkNotNull(sleepTime, "sleepTime is null"), exceptionWhiteList);
+        return new RetryDriver(maxRetryAttempts, checkNotNull(sleepTime, "sleepTime is null"), maxRetryTime, exceptionWhiteList);
+    }
+
+    public RetryDriver withMaxRetryTime(Duration maxRetryTime)
+    {
+        return new RetryDriver(maxRetryAttempts, sleepTime, checkNotNull(maxRetryTime, "maxRetryTime is null"), exceptionWhiteList);
     }
 
     @SafeVarargs
@@ -61,7 +69,7 @@ public class RetryDriver
                 .build();
 
 
-        return new RetryDriver(maxRetryAttempts, sleepTime, exceptions);
+        return new RetryDriver(maxRetryAttempts, sleepTime, maxRetryTime, exceptions);
     }
 
     public RetryDriver stopOnIllegalExceptions()
@@ -75,6 +83,7 @@ public class RetryDriver
         checkNotNull(callableName, "callableName is null");
         checkNotNull(callable, "callable is null");
 
+        long startTime = System.nanoTime();
         int attempt = 0;
         while (true) {
             attempt++;
@@ -87,7 +96,7 @@ public class RetryDriver
                         throw e;
                     }
                 }
-                if (attempt >= maxRetryAttempts) {
+                if (attempt >= maxRetryAttempts || Duration.nanosSince(startTime).compareTo(maxRetryTime) >= 0) {
                     throw e;
                 }
                 else {
