b4bbb4be66ca2aa33be81929068de389c47f702f
Dain Sundstrom <dain@iq80.com>
2013-07-03 21:32:00 -0700

    Parse Hive column values as needed instead of all up front

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveBooleanParser.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveBooleanParser.java
@@ -5,22 +5,32 @@ public class HiveBooleanParser
     @SuppressWarnings("PointlessArithmeticExpression")
     public static Boolean parseHiveBoolean(byte[] bytes, int start, int length)
     {
-        if ((length == 4) &&
-                (toUpperCase(bytes[start + 0]) == 'T') &&
-                (toUpperCase(bytes[start + 1]) == 'R') &&
-                (toUpperCase(bytes[start + 2]) == 'U') &&
-                (toUpperCase(bytes[start + 3]) == 'E')) {
+        if (isTrue(bytes, start, length)) {
             return true;
         }
-        if ((length == 5) &&
+        if (isFalse(bytes, start, length)) {
+            return false;
+        }
+        return null;
+    }
+
+    public static boolean isFalse(byte[] bytes, int start, int length)
+    {
+        return (length == 5) &&
                 (toUpperCase(bytes[start + 0]) == 'F') &&
                 (toUpperCase(bytes[start + 1]) == 'A') &&
                 (toUpperCase(bytes[start + 2]) == 'L') &&
                 (toUpperCase(bytes[start + 3]) == 'S') &&
-                (toUpperCase(bytes[start + 4]) == 'E')) {
-            return false;
-        }
-        return null;
+                (toUpperCase(bytes[start + 4]) == 'E');
+    }
+
+    public static boolean isTrue(byte[] bytes, int start, int length)
+    {
+        return (length == 4) &&
+                (toUpperCase(bytes[start + 0]) == 'T') &&
+                (toUpperCase(bytes[start + 1]) == 'R') &&
+                (toUpperCase(bytes[start + 2]) == 'U') &&
+                (toUpperCase(bytes[start + 3]) == 'E');
     }
 
     private static byte toUpperCase(byte b)
