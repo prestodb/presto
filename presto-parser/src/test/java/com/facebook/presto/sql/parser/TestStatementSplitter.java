3ad83e5d1ce3eeba32dac7fd98236675aec0441a
David Phillips <david@acz.org>
2013-07-11 11:14:28 -0700

    Support custom terminators in StatementSplitter

--- presto-parser/src/test/java/com/facebook/presto/sql/parser/TestStatementSplitter.java
+++ presto-parser/src/test/java/com/facebook/presto/sql/parser/TestStatementSplitter.java
@@ -1,9 +1,14 @@
 package com.facebook.presto.sql.parser;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static com.facebook.presto.sql.parser.StatementSplitter.Statement;
 import static com.facebook.presto.sql.parser.StatementSplitter.squeezeStatement;
+import static com.google.common.base.Preconditions.checkArgument;
 import static org.testng.Assert.assertEquals;
 
 public class TestStatementSplitter
@@ -36,7 +41,7 @@ public class TestStatementSplitter
     public void testSplitterSingle()
     {
         StatementSplitter splitter = new StatementSplitter("select * from foo;");
-        assertEquals(splitter.getCompleteStatements(), ImmutableList.of("select * from foo"));
+        assertEquals(splitter.getCompleteStatements(), statements("select * from foo", ";"));
         assertEquals(splitter.getPartialStatement(), "");
     }
 
@@ -44,7 +49,7 @@ public class TestStatementSplitter
     public void testSplitterMultiple()
     {
         StatementSplitter splitter = new StatementSplitter(" select * from  foo ; select * from t; select * from ");
-        assertEquals(splitter.getCompleteStatements(), ImmutableList.of("select * from  foo", "select * from t"));
+        assertEquals(splitter.getCompleteStatements(), statements("select * from  foo", ";", "select * from t", ";"));
         assertEquals(splitter.getPartialStatement(), "select * from");
     }
 
@@ -52,7 +57,16 @@ public class TestStatementSplitter
     public void testSplitterMultipleWithEmpty()
     {
         StatementSplitter splitter = new StatementSplitter("; select * from  foo ; select * from t;;;select * from ");
-        assertEquals(splitter.getCompleteStatements(), ImmutableList.of("select * from  foo", "select * from t"));
+        assertEquals(splitter.getCompleteStatements(), statements("select * from  foo", ";", "select * from t", ";"));
+        assertEquals(splitter.getPartialStatement(), "select * from");
+    }
+
+    @Test
+    public void testSplitterCustomDelimiters()
+    {
+        String sql = "// select * from  foo // select * from t;//select * from ";
+        StatementSplitter splitter = new StatementSplitter(sql, ImmutableSet.of(";", "//"));
+        assertEquals(splitter.getCompleteStatements(), statements("select * from  foo", "//", "select * from t", ";"));
         assertEquals(splitter.getPartialStatement(), "select * from");
     }
 
@@ -60,7 +74,7 @@ public class TestStatementSplitter
     public void testSplitterErrorBeforeComplete()
     {
         StatementSplitter splitter = new StatementSplitter(" select * from z# oops ; select ");
-        assertEquals(splitter.getCompleteStatements(), ImmutableList.of("select * from z# oops"));
+        assertEquals(splitter.getCompleteStatements(), statements("select * from z# oops", ";"));
         assertEquals(splitter.getPartialStatement(), "select");
     }
 
@@ -68,7 +82,7 @@ public class TestStatementSplitter
     public void testSplitterErrorAfterComplete()
     {
         StatementSplitter splitter = new StatementSplitter("select * from foo; select z# oops ");
-        assertEquals(splitter.getCompleteStatements(), ImmutableList.of("select * from foo"));
+        assertEquals(splitter.getCompleteStatements(), statements("select * from foo", ";"));
         assertEquals(splitter.getPartialStatement(), "select z# oops");
     }
 
@@ -93,8 +107,8 @@ public class TestStatementSplitter
     @Test
     public void testSplitterWithEscapedSingleQuote()
     {
-        String sql = "select 'hello''world' from dual";
-        StatementSplitter splitter = new StatementSplitter(sql + ";");
+        String sql = "select 'hello''world' from dual;";
+        StatementSplitter splitter = new StatementSplitter(sql);
         assertEquals(splitter.getCompleteStatements(), ImmutableList.of(sql));
         assertEquals(splitter.getPartialStatement(), "");
     }
@@ -102,8 +116,8 @@ public class TestStatementSplitter
     @Test
     public void testSplitterWithQuotedIdentifier()
     {
-        String sql = "select \"0\"\"bar\" from dual";
-        StatementSplitter splitter = new StatementSplitter(sql + ";");
+        String sql = "select \"0\"\"bar\" from dual;";
+        StatementSplitter splitter = new StatementSplitter(sql);
         assertEquals(splitter.getCompleteStatements(), ImmutableList.of(sql));
         assertEquals(splitter.getPartialStatement(), "");
     }
@@ -148,9 +162,26 @@ public class TestStatementSplitter
     }
 
     @Test
+    public void testSqueezeStatementAlternateDelimiter()
+    {
+        String sql = "select   *  from\n foo\n  order by x // ";
+        assertEquals(squeezeStatement(sql), "select * from foo order by x //");
+    }
+
+    @Test
     public void testSqueezeStatementError()
     {
         String sql = "select   *  from z#oops";
         assertEquals(squeezeStatement(sql), "select * from z#oops");
+    }
+
+    private static List<Statement> statements(String... args)
+    {
+        checkArgument(args.length % 2 == 0, "arguments not paired");
+        ImmutableList.Builder<Statement> list = ImmutableList.builder();
+        for (int i = 0; i < args.length; i += 2) {
+            list.add(new Statement(args[i], args[i] + 1));
+        }
+        return list.build();
     }
 }
