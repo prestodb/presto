504c5ccaea9ed02c989e93ad72ed7cde28ff4636
Eric Hwang <ehwang@fb.com>
2013-07-23 12:30:54 -0700

    SemiJoin operator

--- presto-main/src/main/java/com/facebook/presto/operator/SourceSetProvider.java
+++ presto-main/src/main/java/com/facebook/presto/operator/SourceSetProvider.java
@@ -1,47 +1,43 @@
-/*
- * Copyright 2004-present Facebook. All Rights Reserved.
- */
 package com.facebook.presto.operator;
 
 import com.facebook.presto.execution.TaskMemoryManager;
 import com.facebook.presto.tuple.TupleInfo;
-import com.google.common.base.Supplier;
+import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 
 import javax.annotation.concurrent.GuardedBy;
-import javax.annotation.concurrent.ThreadSafe;
+import javax.inject.Provider;
 
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-@ThreadSafe
-public class SourceHashSupplier
-        implements Supplier<SourceHash>
+public class SourceSetProvider
+        implements Provider<ChannelSet>
 {
     private final StoppableOperator stoppableSource;
-    private final int hashChannel;
+    private final int setChannel;
     private final int expectedPositions;
     private final OperatorStats operatorStats;
     private final TaskMemoryManager taskMemoryManager;
 
     @GuardedBy("this")
-    private SourceHash sourceHash;
+    private ChannelSet channelSet;
 
     @GuardedBy("this")
     private Throwable buildException;
 
-    public SourceHashSupplier(Operator source, int hashChannel, int expectedPositions, TaskMemoryManager taskMemoryManager, OperatorStats operatorStats)
+    public SourceSetProvider(Operator stoppableSource, int setChannel, int expectedPositions, TaskMemoryManager taskMemoryManager, OperatorStats operatorStats)
     {
-        checkNotNull(source, "source is null");
-        checkArgument(hashChannel >= 0, "hashChannel must be greater than or equal to zero");
+        checkNotNull(stoppableSource, "stoppableSource is null");
+        checkArgument(setChannel >= 0, "setChannel must be greater than or equal to zero");
         checkArgument(expectedPositions >= 0, "expectedPositions must be greater than or equal to zero");
         checkNotNull(taskMemoryManager, "taskMemoryManager is null");
         checkNotNull(operatorStats, "operatorStats is null");
 
-        this.stoppableSource = new StoppableOperator(source);
-        this.hashChannel = hashChannel;
+        this.stoppableSource = new StoppableOperator(stoppableSource);
+        this.setChannel = setChannel;
         this.expectedPositions = expectedPositions;
         this.operatorStats = operatorStats;
         this.taskMemoryManager = taskMemoryManager;
@@ -58,23 +54,23 @@ public class SourceHashSupplier
     }
 
     @Override
-    public synchronized SourceHash get()
+    public synchronized ChannelSet get()
     {
-        if (sourceHash == null) {
+        if (channelSet == null) {
             if (buildException != null) {
                 throw Throwables.propagate(buildException);
             }
 
             try {
                 PageIterator iterator = stoppableSource.iterator(operatorStats);
-                sourceHash = new SourceHash(iterator, hashChannel, expectedPositions, taskMemoryManager, operatorStats);
+                channelSet = new ChannelSet(iterator, setChannel, expectedPositions, taskMemoryManager, operatorStats);
             }
             catch (Throwable e) {
                 buildException = e;
                 throw Throwables.propagate(buildException);
             }
         }
-        return new SourceHash(sourceHash);
+        return new ChannelSet(channelSet);
     }
 
     public void close()
