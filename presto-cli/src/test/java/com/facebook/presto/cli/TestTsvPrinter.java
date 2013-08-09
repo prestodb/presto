976c05ea6b3dd60d416c5ffbb7eea88fb673dfd2
David Phillips <david@acz.org>
2013-08-09 12:54:23 -0700

    Use escaping rather than enclosing quotes for TSV

--- presto-cli/src/test/java/com/facebook/presto/cli/TestTsvPrinter.java
+++ presto-cli/src/test/java/com/facebook/presto/cli/TestTsvPrinter.java
@@ -10,59 +10,56 @@ import static com.facebook.presto.cli.TestAlignedTuplePrinter.row;
 import static com.facebook.presto.cli.TestAlignedTuplePrinter.rows;
 import static org.testng.Assert.assertEquals;
 
-public class TestCsvPrinter
+public class TestTsvPrinter
 {
     @Test
-    public void testCsvPrinting()
+    public void testTsvPrinting()
             throws Exception
     {
         StringWriter writer = new StringWriter();
         List<String> fieldNames = ImmutableList.of("first", "last", "quantity");
-        OutputPrinter printer = new CsvPrinter(fieldNames, writer, true);
+        OutputPrinter printer = new TsvPrinter(fieldNames, writer, true);
 
         printer.printRows(rows(
                 row("hello", "world", 123),
                 row("a", null, 4.5),
-                row("some long\ntext that\ndoes not\nfit on\none line", "more\ntext", 4567),
-                row("bye", "done", -15)),
+                row("some long\ntext\tdone", "more\ntext", 4567),
+                row("bye", "done", -15),
+                row("oops\0a\nb\rc\bd\fe\tf\\g\1done", "escape", 9)),
                 true);
         printer.finish();
 
         String expected = "" +
-                "\"first\",\"last\",\"quantity\"\n" +
-                "\"hello\",\"world\",\"123\"\n" +
-                "\"a\",\"\",\"4.5\"\n" +
-                "\"some long\n" +
-                "text that\n" +
-                "does not\n" +
-                "fit on\n" +
-                "one line\",\"more\n" +
-                "text\",\"4567\"\n" +
-                "\"bye\",\"done\",\"-15\"\n";
+                "first\tlast\tquantity\n" +
+                "hello\tworld\t123\n" +
+                "a\t\t4.5\n" +
+                "some long\\ntext\\tdone\tmore\\ntext\t4567\n" +
+                "bye\tdone\t-15\n" +
+                "oops\\0a\\nb\\rc\\bd\\fe\\tf\\\\g\1done\tescape\t9\n";
 
         assertEquals(writer.getBuffer().toString(), expected);
     }
 
     @Test
-    public void testCsvPrintingNoRows()
+    public void testTsvPrintingNoRows()
             throws Exception
     {
         StringWriter writer = new StringWriter();
         List<String> fieldNames = ImmutableList.of("first", "last");
-        OutputPrinter printer = new CsvPrinter(fieldNames, writer, true);
+        OutputPrinter printer = new TsvPrinter(fieldNames, writer, true);
 
         printer.finish();
 
-        assertEquals(writer.getBuffer().toString(), "\"first\",\"last\"\n");
+        assertEquals(writer.getBuffer().toString(), "first\tlast\n");
     }
 
     @Test
-    public void testCsvPrintingNoHeader()
+    public void testTsvPrintingNoHeader()
             throws Exception
     {
         StringWriter writer = new StringWriter();
         List<String> fieldNames = ImmutableList.of("first", "last", "quantity");
-        OutputPrinter printer = new CsvPrinter(fieldNames, writer, false);
+        OutputPrinter printer = new TsvPrinter(fieldNames, writer, false);
 
         printer.printRows(rows(
                 row("hello", "world", 123),
@@ -71,8 +68,8 @@ public class TestCsvPrinter
         printer.finish();
 
         String expected = "" +
-                "\"hello\",\"world\",\"123\"\n" +
-                "\"a\",\"\",\"4.5\"\n";
+                "hello\tworld\t123\n" +
+                "a\t\t4.5\n";
 
         assertEquals(writer.getBuffer().toString(), expected);
     }
