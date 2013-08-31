74f363996615ef7a82c002bb9aec8e4213f57b0b
Dain Sundstrom <dain@iq80.com>
2013-08-31 16:00:24 -0700

    Add new style in memory exchange operator

---
+++ presto-main/src/main/java/com/facebook/presto/noperator/InMemoryExchangeSourceOperator.java
@@ -0,0 +1,88 @@
+package com.facebook.presto.noperator;
+
+import com.facebook.presto.execution.TaskMemoryManager;
+import com.facebook.presto.operator.OperatorStats;
+import com.facebook.presto.operator.Page;
+import com.facebook.presto.tuple.TupleInfo;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkState;
+
+public class InMemoryExchangeSourceOperator
+        implements NewOperator
+{
+    public static class InMemoryExchangeSourceOperatorFactory
+            implements NewOperatorFactory
+    {
+        private final InMemoryExchange inMemoryExchange;
+        private boolean closed;
+
+        public InMemoryExchangeSourceOperatorFactory(InMemoryExchange inMemoryExchange)
+        {
+            this.inMemoryExchange = inMemoryExchange;
+        }
+
+        @Override
+        public List<TupleInfo> getTupleInfos()
+        {
+            return inMemoryExchange.getTupleInfos();
+        }
+
+        @Override
+        public NewOperator createOperator(OperatorStats operatorStats, TaskMemoryManager taskMemoryManager)
+        {
+            checkState(!closed, "Factory is already closed");
+            return new InMemoryExchangeSourceOperator(inMemoryExchange);
+        }
+
+        @Override
+        public void close()
+        {
+            closed = true;
+        }
+    }
+
+    private final InMemoryExchange exchange;
+
+    public InMemoryExchangeSourceOperator(InMemoryExchange exchange)
+    {
+        this.exchange = exchange;
+    }
+
+    @Override
+    public List<TupleInfo> getTupleInfos()
+    {
+        return exchange.getTupleInfos();
+    }
+
+    @Override
+    public void finish()
+    {
+        exchange.finish();
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return exchange.isFinished();
+    }
+
+    @Override
+    public boolean needsInput()
+    {
+        return false;
+    }
+
+    @Override
+    public void addInput(Page page)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Page getOutput()
+    {
+        return exchange.removePage();
+    }
+}
