cc4e6b7e847e5889642e3e3e1a53dd98c9d54ced
Dain Sundstrom <dain@iq80.com>
2013-08-31 16:00:31 -0700

    Remove old operator framework

--- presto-main/src/main/java/com/facebook/presto/sql/gen/OperatorFactory.java
+++
@@ -1,9 +0,0 @@
-package com.facebook.presto.sql.gen;
-
-import com.facebook.presto.operator.Operator;
-import com.facebook.presto.sql.analyzer.Session;
-
-public interface OperatorFactory
-{
-    Operator createOperator(Operator source, Session session);
-}
