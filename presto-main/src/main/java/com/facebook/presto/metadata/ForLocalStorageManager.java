d8b09b81538911025afa1aee3d52e15664326604
David Phillips <david@acz.org>
2013-09-26 20:08:58 -0700

    Rename annotation for LocalStorageManager

--- presto-main/src/main/java/com/facebook/presto/metadata/ForLocalStorageManager.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/ForLocalStorageManager.java
@@ -26,6 +26,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({FIELD, PARAMETER, METHOD})
 @Qualifier
-public @interface ForStorageManager
+public @interface ForLocalStorageManager
 {
 }
