67efffa38bcea5531d7f07532a97db7a921d5ecb
Dain Sundstrom <dain@iq80.com>
2013-10-12 12:56:13 -0700

    Rewrite ChannelSet to use BlockBuilder instead of manual slice building

--- presto-main/src/test/java/com/facebook/presto/operator/TestHashSemiJoinOperator.java
+++ presto-main/src/test/java/com/facebook/presto/operator/TestHashSemiJoinOperator.java
@@ -260,7 +260,7 @@ public class TestHashSemiJoinOperator
 
         OperatorContext operatorContext = driverContext.addOperatorContext(0, ValuesOperator.class.getSimpleName());
         Operator buildOperator = new ValuesOperator(operatorContext, rowPagesBuilder(SINGLE_LONG)
-                .addSequencePage(1000, 20)
+                .addSequencePage(10000, 20)
                 .build());
         SetBuilderOperatorFactory setBuilderOperatorFactory = new SetBuilderOperatorFactory(1, buildOperator.getTupleInfos(), 0, 10);
         Operator setBuilderOperator = setBuilderOperatorFactory.createOperator(driverContext);
