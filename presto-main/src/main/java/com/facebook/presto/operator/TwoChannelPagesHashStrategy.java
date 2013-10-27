a681865fd3d0123e970dda989b31f3af7d3d8f79
Dain Sundstrom <dain@iq80.com>
2013-10-27 12:53:31 -0700

    Generate HashJoin

---
+++ presto-main/src/main/java/com/facebook/presto/operator/TwoChannelPagesHashStrategy.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator;
+
+import com.facebook.presto.block.BlockCursor;
+import com.facebook.presto.block.RandomAccessBlock;
+
+import java.util.List;
+
+public class TwoChannelPagesHashStrategy
+        implements PagesHashStrategy
+{
+    private final List<RandomAccessBlock> channelA;
+    private final List<RandomAccessBlock> channelB;
+    private final List<RandomAccessBlock> hashChannelA;
+    private final List<RandomAccessBlock> hashChannelB;
+
+    public TwoChannelPagesHashStrategy(List<List<RandomAccessBlock>> channels)
+    {
+        this.channelA = channels.get(0);
+        this.channelB = channels.get(1);
+        this.hashChannelA = channels.get(2);
+        this.hashChannelB = channels.get(3);
+    }
+
+    @Override
+    public int getChannelCount()
+    {
+        return 2;
+    }
+
+    @Override
+    public void appendTo(int blockIndex, int blockPosition, PageBuilder pageBuilder, int outputChannelOffset)
+    {
+        channelA.get(blockIndex).appendTo(blockPosition, pageBuilder.getBlockBuilder(outputChannelOffset + 0));
+        channelB.get(blockIndex).appendTo(blockPosition, pageBuilder.getBlockBuilder(outputChannelOffset + 1));
+    }
+
+    @Override
+    public int hashPosition(int blockIndex, int blockPosition)
+    {
+        int result = 0;
+        result = result * 31 + hashChannelA.get(blockIndex).hashCode(blockPosition);
+        result = result * 31 + hashChannelB.get(blockIndex).hashCode(blockPosition);
+        return result;
+    }
+
+    @Override
+    public boolean positionEqualsCursors(int blockIndex, int blockPosition, BlockCursor[] cursors)
+    {
+        if (!hashChannelA.get(blockIndex).equals(blockPosition, cursors[0])) {
+            return false;
+        }
+        if (!hashChannelB.get(blockIndex).equals(blockPosition, cursors[1])) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean positionEqualsPosition(int leftBlockIndex, int leftBlockPosition, int rightBlockIndex, int rightBlockPosition)
+    {
+        if (!hashChannelA.get(leftBlockIndex).equals(leftBlockPosition, hashChannelA.get(rightBlockIndex), rightBlockPosition)) {
+            return false;
+        }
+        if (!hashChannelB.get(leftBlockIndex).equals(leftBlockPosition, hashChannelB.get(rightBlockIndex), rightBlockPosition)) {
+            return false;
+        }
+
+        return true;
+    }
+}
