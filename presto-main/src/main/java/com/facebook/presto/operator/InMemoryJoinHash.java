537881a73ed094228264e2892979b847bb555da5
Dain Sundstrom <dain@iq80.com>
2013-10-27 12:53:31 -0700

    Generate HashJoin

---
+++ presto-main/src/main/java/com/facebook/presto/operator/InMemoryJoinHash.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator;
+
+import com.facebook.presto.block.BlockCursor;
+import it.unimi.dsi.fastutil.HashCommon;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+
+import java.util.Arrays;
+
+import static com.facebook.presto.operator.SyntheticAddress.decodePosition;
+import static com.facebook.presto.operator.SyntheticAddress.decodeSliceIndex;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static io.airlift.slice.SizeOf.sizeOfIntArray;
+import static it.unimi.dsi.fastutil.HashCommon.murmurHash3;
+
+public final class InMemoryJoinHash
+        implements JoinHash
+{
+    private final LongArrayList addresses;
+    private final PagesHashStrategy pagesHashStrategy;
+
+    private final int channelCount;
+    private final int mask;
+    private final int[] key;
+    private final int[] positionLinks;
+
+    public InMemoryJoinHash(LongArrayList addresses, PagesHashStrategy pagesHashStrategy, OperatorContext operatorContext)
+    {
+        this.addresses = checkNotNull(addresses, "addresses is null");
+        this.pagesHashStrategy = checkNotNull(pagesHashStrategy, "pagesHashStrategy is null");
+        this.channelCount = pagesHashStrategy.getChannelCount();
+
+        checkNotNull(operatorContext, "operatorContext is null");
+
+        // reserve memory for the arrays
+        int hashSize = HashCommon.arraySize(addresses.size(), 0.75f);
+        operatorContext.reserveMemory(sizeOfIntArray(hashSize) + sizeOfIntArray(addresses.size()));
+
+        mask = hashSize - 1;
+        key = new int[hashSize];
+        Arrays.fill(key, -1);
+
+        this.positionLinks = new int[addresses.size()];
+        Arrays.fill(positionLinks, -1);
+
+        // index pages
+        for (int position = 0; position < addresses.size(); position++) {
+            int pos = (murmurHash3(hashPosition(position) ^ mask)) & mask;
+
+            // look for an empty slot or a slot containing this key
+            while (key[pos] != -1) {
+                int currentKey = key[pos];
+                if (positionEqualsPosition(currentKey, position)) {
+                    // found a slot for this key
+                    // link the new key position to the current key position
+                    positionLinks[position] = currentKey;
+
+                    // key[pos] updated outside of this loop
+                    break;
+                }
+                // increment position and mask to handler wrap around
+                pos = (pos + 1) & mask;
+            }
+
+            key[pos] = position;
+        }
+    }
+
+    @Override
+    public final int getChannelCount()
+    {
+        return channelCount;
+    }
+
+    @Override
+    public final int getJoinPosition(BlockCursor... cursors)
+    {
+        int pos = (murmurHash3(hashCursor(cursors) ^ mask)) & mask;
+
+        while (key[pos] != -1) {
+            if (positionEqualsCurrentRow(key[pos], cursors)) {
+                return key[pos];
+            }
+            // increment position and mask to handler wrap around
+            pos = (pos + 1) & mask;
+        }
+        return -1;
+    }
+
+    @Override
+    public final int getNextJoinPosition(int currentPosition)
+    {
+        return positionLinks[currentPosition];
+    }
+
+    public void appendTo(int position, PageBuilder pageBuilder, int outputChannelOffset)
+    {
+        long pageAddress = addresses.getLong(position);
+        int blockIndex = decodeSliceIndex(pageAddress);
+        int blockPosition = decodePosition(pageAddress);
+
+        pagesHashStrategy.appendTo(blockIndex, blockPosition, pageBuilder, outputChannelOffset);
+    }
+
+    private int hashCursor(BlockCursor... cursors)
+    {
+        int result = 0;
+        for (BlockCursor cursor : cursors) {
+            result = result * 31 + cursor.calculateHashCode();
+        }
+        return result;
+    }
+
+    private int hashPosition(int position)
+    {
+        long pageAddress = addresses.getLong(position);
+        int blockIndex = decodeSliceIndex(pageAddress);
+        int blockPosition = decodePosition(pageAddress);
+
+        return pagesHashStrategy.hashPosition(blockIndex, blockPosition);
+    }
+
+    private boolean positionEqualsCurrentRow(int position, BlockCursor... cursors)
+    {
+        long pageAddress = addresses.getLong(position);
+        int blockIndex = decodeSliceIndex(pageAddress);
+        int blockPosition = decodePosition(pageAddress);
+
+        return pagesHashStrategy.positionEqualsCursors(blockIndex, blockPosition, cursors);
+    }
+
+    private boolean positionEqualsPosition(int leftPosition, int rightPosition)
+    {
+        long leftPageAddress = addresses.getLong(leftPosition);
+        int leftBlockIndex = decodeSliceIndex(leftPageAddress);
+        int leftBlockPosition = decodePosition(leftPageAddress);
+
+        long rightPageAddress = addresses.getLong(rightPosition);
+        int rightBlockIndex = decodeSliceIndex(rightPageAddress);
+        int rightBlockPosition = decodePosition(rightPageAddress);
+
+        return pagesHashStrategy.positionEqualsPosition(leftBlockIndex, leftBlockPosition, rightBlockIndex, rightBlockPosition);
+    }
+}
