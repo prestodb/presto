94f35123fbaedb4c19c2810f73592f258cfad89a
David Phillips <david@acz.org>
2013-11-05 23:03:59 -0800

    Fix download link in docs

--- presto-docs/src/main/sphinx/ext/download.py
+++ presto-docs/src/main/sphinx/ext/download.py
@@ -33,7 +33,7 @@ def maven_download(group, artifact, version, packaging, classifier):
     base = 'http://search.maven.org/remotecontent?filepath='
     group_path = group.replace('.', '/')
     filename = maven_filename(artifact, version, packaging, classifier)
-    return base + '/'.join((group_path, artifact, filename))
+    return base + '/'.join((group_path, artifact, version, filename))
 
 
 def setup(app):
