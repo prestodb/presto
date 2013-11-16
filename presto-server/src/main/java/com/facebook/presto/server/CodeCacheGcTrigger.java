66c9f86e8016b87bb559aa8abe3e0b480d5d202f
David Phillips <david@acz.org>
2013-11-16 11:25:39 -0800

    Fix logging for full code cache condition

--- presto-server/src/main/java/com/facebook/presto/server/CodeCacheGcTrigger.java
+++ presto-server/src/main/java/com/facebook/presto/server/CodeCacheGcTrigger.java
@@ -1,6 +1,5 @@
 package com.facebook.presto.server;
 
-import com.google.common.base.Preconditions;
 import io.airlift.log.Logger;
 
 import java.lang.management.ManagementFactory;
@@ -22,7 +21,6 @@ final class CodeCacheGcTrigger
         // Hack to work around bugs in java 7 related to code cache management.
         // See http://mail.openjdk.java.net/pipermail/hotspot-compiler-dev/2013-August/011333.html for more info.
         final MemoryPoolMXBean codeCacheMbean = findCodeCacheMBean();
-        Preconditions.checkNotNull(codeCacheMbean, "Could not obtain a reference to the 'Code Cache' MemoryPoolMXBean");
 
         Thread gcThread = new Thread(new Runnable()
         {
@@ -35,15 +33,16 @@ final class CodeCacheGcTrigger
                 while (!Thread.currentThread().isInterrupted()) {
                     long used = codeCacheMbean.getUsage().getUsed();
                     long max = codeCacheMbean.getUsage().getMax();
+
+                    if (used > 0.95 * max) {
+                        log.error("Code Cache is more than 95% full. JIT may stop working.");
+                    }
                     if (used > 0.7 * max) {
                         // Due to some obscure bug in hotspot (java 7), once the code cache fills up the JIT stops compiling and never recovers from this condition.
                         // By forcing classes to unload from the perm gen, we let the code cache evictor make room before the cache fills up.
                         // For best results, the server should be run with -XX:+UseConcMarkSweepGC -XX:+ExplicitGCInvokesConcurrent -XX:+CMSClassUnloadingEnabled
                         log.info("Triggering GC to avoid Code Cache eviction bugs");
                         System.gc();
-                    }
-                    else if (used > 0.95 * max) {
-                        log.error("Code Cache is more than 95% full. JIT may stop working.");
                     }
 
                     try {
@@ -67,6 +66,6 @@ final class CodeCacheGcTrigger
                 return bean;
             }
         }
-        return null;
+        throw new RuntimeException("Could not obtain a reference to the 'Code Cache' MemoryPoolMXBean");
     }
 }
