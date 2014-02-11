68ae53e4e14e5e7c69af1aafa9864e8c71df55d3
Christopher Berner <cberner@fb.com>
2014-02-11 11:01:46 -0800

    Fix warnings in AbstractTestQueries

--- presto-main/src/test/java/com/facebook/presto/AbstractTestQueries.java
+++ presto-main/src/test/java/com/facebook/presto/AbstractTestQueries.java
@@ -1813,7 +1813,7 @@ public abstract class AbstractTestQueries
         assertQuery("SELECT \"TOTALPRICE\" \"my price\" FROM \"ORDERS\"");
     }
 
-    @Test(expectedExceptions = Exception.class, expectedExceptionsMessageRegExp = ".*orderkey_1.*")
+    @Test(expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = ".*orderkey_1.*")
     public void testInvalidColumn()
             throws Exception
     {
@@ -2961,7 +2961,7 @@ public abstract class AbstractTestQueries
                         MILLISECONDS.toSeconds(new DateTime(1970, 1, 1, 3, 4, 0, 0, DateTimeZone.UTC).getMillis()) + ",  " +
                         MILLISECONDS.toSeconds(new DateTime(1960, 1, 22, 3, 4, 0, 0, DateTimeZone.UTC).getMillis()) + ",  " +
                         MILLISECONDS.toSeconds(new DateTime(2013, 3, 22, 0, 0, 0, 0, DateTimeZone.UTC).getMillis()) + ",  " +
-                        String.valueOf(TimeUnit.DAYS.toSeconds(123)));
+                        TimeUnit.DAYS.toSeconds(123));
     }
 
     @Test
