a6bce64efbe80b38ec71433c179b4b368e077152
Christopher Berner <cberner@fb.com>
2014-02-26 12:20:16 -0800

    Add index to h2 on custkey
    Makes test suite almost twice as fast

--- presto-main/src/test/java/com/facebook/presto/AbstractTestQueries.java
+++ presto-main/src/test/java/com/facebook/presto/AbstractTestQueries.java
@@ -3045,6 +3045,7 @@ public abstract class AbstractTestQueries
                 "  shippriority BIGINT NOT NULL,\n" +
                 "  comment VARCHAR(79) NOT NULL\n" +
                 ")");
+        handle.execute("CREATE INDEX custkey_index ON orders (custkey)");
         TpchTableHandle ordersHandle = tpchMetadata.getTableHandle(new SchemaTableName(TINY_SCHEMA_NAME, ORDERS.getTableName()));
         insertRows(tpchMetadata.getTableMetadata(ordersHandle), handle, createTpchRecordSet(ORDERS, ordersHandle.getScaleFactor()));
 
