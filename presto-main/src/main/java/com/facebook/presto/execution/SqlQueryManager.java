cd38da14e4a0d0c4ae7437235cdd70a1ed479b71
Martin Traverso <martint@fb.com>
2014-03-17 15:04:17 -0700

    Record "started" counter for queries that fail to parse
    In the last release we started tracking parsing failures for error categorization,
    but the change was missing the call to increment the started counter.
    
    As a result, the started and completed counters get permanently out of sync
    and cause the number of running queries to be reported as 0.

--- presto-main/src/main/java/com/facebook/presto/execution/SqlQueryManager.java
+++ presto-main/src/main/java/com/facebook/presto/execution/SqlQueryManager.java
@@ -350,6 +350,7 @@ public class SqlQueryManager
         QueryExecution execution = new FailedQueryExecution(queryId, query, session, self, queryExecutor, cause);
 
         queries.put(queryId, execution);
+        stats.queryStarted();
         queryMonitor.createdEvent(execution.getQueryInfo());
         queryMonitor.completionEvent(execution.getQueryInfo());
         stats.queryFinished(execution.getQueryInfo());
