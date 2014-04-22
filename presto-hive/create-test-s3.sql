9504d5fc7d654eb8dbc9506e8b8b009ae4c51dff
David Phillips <david@acz.org>
2014-04-21 17:38:41 -0700

    Simplify test table creation for S3

--- presto-hive/create-test-s3.sql
+++ presto-hive/create-test-s3.sql
@@ -5,6 +5,9 @@ CREATE EXTERNAL TABLE presto_test_s3 (
 COMMENT 'Presto test S3 table'
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
-LOCATION 's3://presto-test-hive/presto_test_s3'
 TBLPROPERTIES ('RETENTION'='-1')
+;
+
+ALTER TABLE presto_test_s3
+SET LOCATION 's3://presto-test-hive/presto_test_s3'
 ;
