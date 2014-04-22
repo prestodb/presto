ceabc7759a7fbe53bf49bee6dcd2b23cc4a76711
David Phillips <david@acz.org>
2014-04-22 16:08:54 -0700

    Fix size of encoded pages
    This encodes pages on the wire using the actual buffer size rather than the
    allocated buffer size (which is at least 64k even for an empty page).

--- presto-main/src/test/java/com/facebook/presto/serde/TestPagesSerde.java
+++ presto-main/src/test/java/com/facebook/presto/serde/TestPagesSerde.java
@@ -13,10 +13,12 @@
  */
 package com.facebook.presto.serde;
 
-import com.facebook.presto.spi.block.Block;
-import com.facebook.presto.spi.block.BlockBuilderStatus;
 import com.facebook.presto.operator.Page;
+import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.block.BlockBuilder;
+import com.facebook.presto.spi.block.BlockBuilderStatus;
 import io.airlift.slice.DynamicSliceOutput;
+import io.airlift.slice.Slice;
 import org.testng.annotations.Test;
 
 import java.util.Iterator;
@@ -25,7 +27,9 @@ import static com.facebook.presto.operator.PageAssertions.assertPageEquals;
 import static com.facebook.presto.serde.PagesSerde.readPages;
 import static com.facebook.presto.serde.PagesSerde.writePages;
 import static com.facebook.presto.serde.TestingBlockEncodingManager.createTestingBlockEncodingManager;
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 
 public class TestPagesSerde
@@ -48,5 +52,60 @@ public class TestPagesSerde
         assertPageEquals(pageIterator.next(), expectedPage);
         assertPageEquals(pageIterator.next(), expectedPage);
         assertFalse(pageIterator.hasNext());
+    }
+
+    @Test
+    public void testBigintSerializedSize()
+    {
+        BlockBuilder builder = BIGINT.createBlockBuilder(new BlockBuilderStatus());
+
+        // empty page
+        Page page = new Page(builder.build());
+        int pageSize = serializedSize(page);
+        assertEquals(pageSize, 26); // page overhead
+
+        // page with one value
+        page = new Page(builder.append(123).build());
+        int firstValueSize = serializedSize(page) - pageSize;
+        assertEquals(firstValueSize, 8 + 1); // value size + value overhead
+
+        // page with two values
+        page = new Page(builder.append(456).build());
+        int secondValueSize = serializedSize(page) - (pageSize + firstValueSize);
+        assertEquals(secondValueSize, 8 + 1); // value size + value overhead
+    }
+
+    @Test
+    public void testVarcharSerializedSize()
+    {
+        BlockBuilder builder = VARCHAR.createBlockBuilder(new BlockBuilderStatus());
+
+        // empty page
+        Page page = new Page(builder.build());
+        int pageSize = serializedSize(page);
+        assertEquals(pageSize, 27); // page overhead
+
+        // page with one value
+        page = new Page(builder.append("alice").build());
+        int firstValueSize = serializedSize(page) - pageSize;
+        assertEquals(firstValueSize, 5 + 5); // "alice" + value overhead
+
+        // page with two values
+        page = new Page(builder.append("bob").build());
+        int secondValueSize = serializedSize(page) - (pageSize + firstValueSize);
+        assertEquals(secondValueSize, 3 + 5); // "bob" + value overhead
+    }
+
+    private static int serializedSize(Page expectedPage)
+    {
+        DynamicSliceOutput sliceOutput = new DynamicSliceOutput(1024);
+        writePages(createTestingBlockEncodingManager(), sliceOutput, expectedPage);
+        Slice slice = sliceOutput.slice();
+
+        Iterator<Page> pageIterator = readPages(createTestingBlockEncodingManager(), slice.getInput());
+        assertPageEquals(pageIterator.next(), expectedPage);
+        assertFalse(pageIterator.hasNext());
+
+        return slice.length();
     }
 }
