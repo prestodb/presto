f743f3cf840067a53396b284ecff19a3940ec7cd
Dain Sundstrom <dain@iq80.com>
2014-04-22 16:53:39 -0700

    Fix deserialization of metadata in example plugin

--- presto-example-http/src/main/java/com/facebook/presto/example/ExampleModule.java
+++ presto-example-http/src/main/java/com/facebook/presto/example/ExampleModule.java
@@ -13,12 +13,20 @@
  */
 package com.facebook.presto.example;
 
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
 
+import javax.inject.Inject;
+
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static io.airlift.configuration.ConfigurationModule.bindConfig;
+import static io.airlift.json.JsonBinder.jsonBinder;
 import static io.airlift.json.JsonCodec.listJsonCodec;
 import static io.airlift.json.JsonCodecBinder.jsonCodecBinder;
 
@@ -26,15 +34,19 @@ public class ExampleModule
         implements Module
 {
     private final String connectorId;
+    private final TypeManager typeManager;
 
-    public ExampleModule(String connectorId)
+    public ExampleModule(String connectorId, TypeManager typeManager)
     {
         this.connectorId = checkNotNull(connectorId, "connector id is null");
+        this.typeManager = checkNotNull(typeManager, "typeManager is null");
     }
 
     @Override
     public void configure(Binder binder)
     {
+        binder.bind(TypeManager.class).toInstance(typeManager);
+
         binder.bind(ExampleConnector.class).in(Scopes.SINGLETON);
         binder.bind(ExampleConnectorId.class).toInstance(new ExampleConnectorId(connectorId));
         binder.bind(ExampleMetadata.class).in(Scopes.SINGLETON);
@@ -44,6 +56,28 @@ public class ExampleModule
         binder.bind(ExampleHandleResolver.class).in(Scopes.SINGLETON);
         bindConfig(binder).to(ExampleConfig.class);
 
+        jsonBinder(binder).addDeserializerBinding(Type.class).to(TypeDeserializer.class);
         jsonCodecBinder(binder).bindMapJsonCodec(String.class, listJsonCodec(ExampleTable.class));
+    }
+
+    public static final class TypeDeserializer
+            extends FromStringDeserializer<Type>
+    {
+        private final TypeManager typeManager;
+
+        @Inject
+        public TypeDeserializer(TypeManager typeManager)
+        {
+            super(Type.class);
+            this.typeManager = checkNotNull(typeManager, "typeManager is null");
+        }
+
+        @Override
+        protected Type _deserialize(String value, DeserializationContext context)
+        {
+            Type type = typeManager.getType(value);
+            checkArgument(type != null, "Unknown type %s", value);
+            return type;
+        }
     }
 }
