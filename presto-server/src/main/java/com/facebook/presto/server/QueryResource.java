ea271b62475fc6652d2ee46e7b951813310d6a6d
David Phillips <david@acz.org>
2014-04-30 14:04:06 -0700

    Align QueryResource with StatementResource

--- presto-server/src/main/java/com/facebook/presto/server/QueryResource.java
+++ presto-server/src/main/java/com/facebook/presto/server/QueryResource.java
@@ -18,14 +18,11 @@ import com.facebook.presto.execution.QueryInfo;
 import com.facebook.presto.execution.QueryManager;
 import com.facebook.presto.execution.StageId;
 import com.facebook.presto.spi.Session;
-import com.facebook.presto.spi.type.TimeZoneKey;
-import com.facebook.presto.spi.type.TimeZoneNotSupportedException;
 import com.google.common.collect.ImmutableList;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
@@ -50,10 +47,10 @@ import static com.facebook.presto.client.PrestoHeaders.PRESTO_SCHEMA;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_SOURCE;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_TIME_ZONE;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_USER;
-import static com.facebook.presto.spi.Session.DEFAULT_CATALOG;
-import static com.facebook.presto.spi.Session.DEFAULT_SCHEMA;
-import static com.google.common.base.Preconditions.checkArgument;
+import static com.facebook.presto.server.StatementResource.assertRequest;
+import static com.facebook.presto.server.StatementResource.getTimeZoneKey;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Strings.isNullOrEmpty;
 import static com.google.common.net.HttpHeaders.USER_AGENT;
 import static io.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
 
@@ -63,7 +60,6 @@ import static io.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
 @Path("/v1/query")
 public class QueryResource
 {
-    private static final String NO_CACHE = "no-cache";
     private final QueryManager queryManager;
 
     @Inject
@@ -78,7 +74,7 @@ public class QueryResource
         return extractBasicQueryInfo(queryManager.getAllQueryInfo());
     }
 
-    private List<BasicQueryInfo> extractBasicQueryInfo(List<QueryInfo> allQueryInfo)
+    private static List<BasicQueryInfo> extractBasicQueryInfo(List<QueryInfo> allQueryInfo)
     {
         ImmutableList.Builder<BasicQueryInfo> basicQueryInfo = ImmutableList.builder();
         for (QueryInfo queryInfo : allQueryInfo) {
@@ -104,21 +100,22 @@ public class QueryResource
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createQuery(String query,
+    public Response createQuery(
+            String statement,
             @HeaderParam(PRESTO_USER) String user,
             @HeaderParam(PRESTO_SOURCE) String source,
-            @HeaderParam(PRESTO_CATALOG) @DefaultValue(DEFAULT_CATALOG) String catalog,
-            @HeaderParam(PRESTO_SCHEMA) @DefaultValue(DEFAULT_SCHEMA) String schema,
+            @HeaderParam(PRESTO_CATALOG) String catalog,
+            @HeaderParam(PRESTO_SCHEMA) String schema,
             @HeaderParam(PRESTO_TIME_ZONE) String timeZoneId,
             @HeaderParam(PRESTO_LANGUAGE) String language,
             @HeaderParam(USER_AGENT) String userAgent,
             @Context HttpServletRequest requestContext,
             @Context UriInfo uriInfo)
     {
-        checkNotNull(query, "query is null");
-        checkArgument(!query.isEmpty(), "query is empty");
-        checkNotNull(catalog, "catalog is null");
-        checkNotNull(schema, "schema is null");
+        assertRequest(!isNullOrEmpty(statement), "SQL statement is empty");
+        assertRequest(!isNullOrEmpty(user), "User (%s) is empty", PRESTO_USER);
+        assertRequest(!isNullOrEmpty(catalog), "Catalog (%s) is empty", PRESTO_CATALOG);
+        assertRequest(!isNullOrEmpty(schema), "Schema (%s) is empty", PRESTO_SCHEMA);
 
         if (timeZoneId == null) {
             timeZoneId = TimeZone.getDefault().getID();
@@ -131,15 +128,9 @@ public class QueryResource
 
         String remoteUserAddress = requestContext.getRemoteAddr();
 
-        Session session;
-        try {
-            session = new Session(user, source, catalog, schema, TimeZoneKey.getTimeZoneKey(timeZoneId), locale, remoteUserAddress, userAgent);
-        }
-        catch (TimeZoneNotSupportedException e) {
-            return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
-        }
+        Session session = new Session(user, source, catalog, schema, getTimeZoneKey(timeZoneId), locale, remoteUserAddress, userAgent);
 
-        QueryInfo queryInfo = queryManager.createQuery(session, query);
+        QueryInfo queryInfo = queryManager.createQuery(session, statement);
         URI pagesUri = uriBuilderFrom(uriInfo.getRequestUri()).appendPath(queryInfo.getQueryId().toString()).build();
         return Response.created(pagesUri).entity(queryInfo).build();
     }
