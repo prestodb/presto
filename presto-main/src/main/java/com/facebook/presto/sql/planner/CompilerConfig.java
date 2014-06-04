5be6babfdd091405b525ef84924ffa77ed32baf0
Martin Traverso <martint@fb.com>
2014-06-04 13:22:32 -0700

    Add config option to disable interpreter fallback
    The option is compiler.interpreter-enabled. When set to false, if the engine
    fails to compile an expression to bytecode, the query will abort instead of
    falling back to interpreted mode.
    
    LocalQueryRunner and DistributedQueryRunner are now configured with that option
    off so that we can catch certain subtle bugs in the compiler during unit testing .

---
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/CompilerConfig.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.planner;
+
+import com.facebook.presto.operator.Description;
+import io.airlift.configuration.Config;
+
+public class CompilerConfig
+{
+    private boolean interpreterEnabled = true;
+
+    public boolean isInterpreterEnabled()
+    {
+        return interpreterEnabled;
+    }
+
+    @Config("compiler.interpreter-enabled")
+    @Description("Allows evaluation to fall back to interpreter if compilation fails")
+    public CompilerConfig setInterpreterEnabled(boolean interpreterEnabled)
+    {
+        this.interpreterEnabled = interpreterEnabled;
+        return this;
+    }
+}
