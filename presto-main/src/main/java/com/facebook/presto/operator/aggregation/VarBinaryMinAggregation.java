a6c091ba571ec181e4f7e1b1529033590ac6beb9
Christopher Berner <cberner@fb.com>
2014-06-05 12:35:32 -0700

    Convert varchar min/max aggregations to use new aggregation base class

--- presto-main/src/main/java/com/facebook/presto/operator/aggregation/VarBinaryMinAggregation.java
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/VarBinaryMinAggregation.java
@@ -13,20 +13,15 @@
  */
 package com.facebook.presto.operator.aggregation;
 
-import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.operator.aggregation.state.SliceState;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockCursor;
-import com.facebook.presto.operator.GroupByIdBlock;
-import com.facebook.presto.util.array.ObjectBigArray;
-import com.google.common.base.Optional;
 import io.airlift.slice.Slice;
 
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkState;
 
 public class VarBinaryMinAggregation
-        extends SimpleAggregationFunction
+        extends AbstractAggregationFunction<SliceState>
 {
     public static final VarBinaryMinAggregation VAR_BINARY_MIN = new VarBinaryMinAggregation();
 
@@ -36,114 +31,19 @@ public class VarBinaryMinAggregation
     }
 
     @Override
-    protected GroupedAccumulator createGroupedAccumulator(Optional<Integer> maskChannel, Optional<Integer> sampleWeightChannel, double confidence, int valueChannel)
+    protected void processInput(SliceState state, BlockCursor cursor)
     {
-        // Min/max are not effected by distinct, so ignore it.
-        checkArgument(confidence == 1.0, "min does not support approximate queries");
-        return new VarBinaryGroupedAccumulator(valueChannel);
-    }
-
-    public static class VarBinaryGroupedAccumulator
-            extends SimpleGroupedAccumulator
-    {
-        private final ObjectBigArray<Slice> minValues;
-        private long sizeOfValues;
-
-        public VarBinaryGroupedAccumulator(int valueChannel)
-        {
-            super(valueChannel, VARCHAR, VARCHAR, Optional.<Integer>absent(), Optional.<Integer>absent());
-            this.minValues = new ObjectBigArray<>();
-        }
-
-        @Override
-        public long getEstimatedSize()
-        {
-            return minValues.sizeOf() + sizeOfValues;
-        }
-
-        @Override
-        protected void processInput(GroupByIdBlock groupIdsBlock, Block valuesBlock, Optional<Block> maskBlock, Optional<Block> sampleWeightBlock)
-        {
-            minValues.ensureCapacity(groupIdsBlock.getGroupCount());
-
-            BlockCursor values = valuesBlock.cursor();
-
-            for (int position = 0; position < groupIdsBlock.getPositionCount(); position++) {
-                checkState(values.advanceNextPosition());
-
-                // skip null values
-                if (!values.isNull()) {
-                    long groupId = groupIdsBlock.getGroupId(position);
-
-                    Slice value = values.getSlice();
-                    Slice currentValue = minValues.get(groupId);
-                    if (currentValue == null || value.compareTo(currentValue) < 0) {
-                        minValues.set(groupId, value);
-
-                        // update size
-                        if (currentValue != null) {
-                            sizeOfValues -= currentValue.length();
-                        }
-                        sizeOfValues += value.length();
-                    }
-                }
-            }
-            checkState(!values.advanceNextPosition());
-        }
-
-        @Override
-        public void evaluateFinal(int groupId, BlockBuilder output)
-        {
-            Slice value = minValues.get((long) groupId);
-            if (value == null) {
-                output.appendNull();
-            }
-            else {
-                output.appendSlice(value);
-            }
-        }
+        state.setSlice(min(state.getSlice(), cursor.getSlice()));
     }
 
     @Override
-    protected Accumulator createAccumulator(Optional<Integer> maskChannel, Optional<Integer> sampleWeightChannel, double confidence, int valueChannel)
+    protected void evaluateFinal(SliceState state, BlockBuilder out)
     {
-        // Min/max are not effected by distinct, so ignore it.
-        checkArgument(confidence == 1.0, "min does not support approximate queries");
-        return new VarBinaryMinAccumulator(valueChannel);
-    }
-
-    public static class VarBinaryMinAccumulator
-            extends SimpleAccumulator
-    {
-        private Slice min;
-
-        public VarBinaryMinAccumulator(int valueChannel)
-        {
-            super(valueChannel, VARCHAR, VARCHAR, Optional.<Integer>absent(), Optional.<Integer>absent());
+        if (state.getSlice() != null) {
+            out.appendSlice(state.getSlice());
         }
-
-        @Override
-        protected void processInput(Block block, Optional<Block> maskBlock, Optional<Block> sampleWeightBlock)
-        {
-            BlockCursor values = block.cursor();
-
-            for (int position = 0; position < block.getPositionCount(); position++) {
-                checkState(values.advanceNextPosition());
-                if (!values.isNull()) {
-                    min = min(min, values.getSlice());
-                }
-            }
-        }
-
-        @Override
-        public void evaluateFinal(BlockBuilder out)
-        {
-            if (min != null) {
-                out.appendSlice(min);
-            }
-            else {
-                out.appendNull();
-            }
+        else {
+            out.appendNull();
         }
     }
 
