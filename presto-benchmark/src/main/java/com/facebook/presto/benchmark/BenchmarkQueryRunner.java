dfa2974b32183dccdeda8c6d9b518c860476a61e
Dain Sundstrom <dain@iq80.com>
2014-06-08 14:36:49 -0700

    Change benchmarks to be reusable in connectors

--- presto-benchmark/src/main/java/com/facebook/presto/benchmark/BenchmarkQueryRunner.java
+++ presto-benchmark/src/main/java/com/facebook/presto/benchmark/BenchmarkQueryRunner.java
@@ -14,100 +14,31 @@
 package com.facebook.presto.benchmark;
 
 import com.facebook.presto.metadata.InMemoryNodeManager;
-import com.facebook.presto.metadata.Metadata;
-import com.facebook.presto.metadata.QualifiedTableName;
-import com.facebook.presto.raptor.RaptorConnectorFactory;
 import com.facebook.presto.spi.ConnectorSession;
-import com.facebook.presto.spi.NodeManager;
-import com.facebook.presto.spi.block.BlockEncodingSerde;
-import com.facebook.presto.spi.type.TypeManager;
 import com.facebook.presto.testing.LocalQueryRunner;
 import com.facebook.presto.tpch.TpchConnectorFactory;
-import com.facebook.presto.tpch.testing.SampledTpchConnectorFactory;
-import com.facebook.presto.type.TypeRegistry;
-import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
 
-import java.io.File;
 import java.util.Locale;
-import java.util.Map;
 
 import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
-import static com.facebook.presto.testing.TestingBlockEncodingManager.createTestingBlockEncodingManager;
+import static com.facebook.presto.tpch.TpchMetadata.TINY_SCHEMA_NAME;
 
 public final class BenchmarkQueryRunner
 {
-    private static final String TPCH_SAMPLED_CACHE_DIR = System.getProperty("tpchSampledCacheDir", "/tmp/presto_tpch/sampled_data_cache");
-    private static final String TPCH_CACHE_DIR = System.getProperty("tpchCacheDir", "/tmp/presto_tpch/data_cache");
-
-    private BenchmarkQueryRunner() {}
-
-    public static LocalQueryRunner createLocalSampledQueryRunner()
+    private BenchmarkQueryRunner()
     {
-        ConnectorSession session = new ConnectorSession("user", "test", "default", "default", UTC_KEY, Locale.ENGLISH, null, null);
-        LocalQueryRunner localQueryRunner = new LocalQueryRunner(session);
-
-        // add sampled tpch
-        InMemoryNodeManager nodeManager = localQueryRunner.getNodeManager();
-        localQueryRunner.createCatalog("tpch_sampled", new SampledTpchConnectorFactory(nodeManager, 1, 2), ImmutableMap.<String, String>of());
-
-        // add raptor
-        RaptorConnectorFactory raptorConnectorFactory = createRaptorConnectorFactory(TPCH_SAMPLED_CACHE_DIR, nodeManager);
-        localQueryRunner.createCatalog("default", raptorConnectorFactory, ImmutableMap.<String, String>of());
-
-        Metadata metadata = localQueryRunner.getMetadata();
-        if (!metadata.getTableHandle(session, new QualifiedTableName("default", "default", "orders")).isPresent()) {
-            localQueryRunner.execute("CREATE TABLE orders AS SELECT * FROM tpch_sampled.sf1.orders");
-        }
-        if (!metadata.getTableHandle(session, new QualifiedTableName("default", "default", "lineitem")).isPresent()) {
-            localQueryRunner.execute("CREATE TABLE lineitem AS SELECT * FROM tpch_sampled.sf1.lineitem");
-        }
-        return localQueryRunner;
     }
 
     public static LocalQueryRunner createLocalQueryRunner()
     {
-        ConnectorSession session = new ConnectorSession("user", "test", "default", "default", UTC_KEY, Locale.ENGLISH, null, null);
+        ConnectorSession session = new ConnectorSession("user", "test", "tpch", TINY_SCHEMA_NAME, UTC_KEY, Locale.ENGLISH, null, null);
         LocalQueryRunner localQueryRunner = new LocalQueryRunner(session);
 
         // add tpch
         InMemoryNodeManager nodeManager = localQueryRunner.getNodeManager();
         localQueryRunner.createCatalog("tpch", new TpchConnectorFactory(nodeManager, 1), ImmutableMap.<String, String>of());
 
-        // add raptor
-        RaptorConnectorFactory raptorConnectorFactory = createRaptorConnectorFactory(TPCH_CACHE_DIR, nodeManager);
-        localQueryRunner.createCatalog("default", raptorConnectorFactory, ImmutableMap.<String, String>of());
-
-        Metadata metadata = localQueryRunner.getMetadata();
-        if (!metadata.getTableHandle(session, new QualifiedTableName("default", "default", "orders")).isPresent()) {
-            localQueryRunner.execute("CREATE TABLE orders AS SELECT * FROM tpch.sf1.orders");
-        }
-        if (!metadata.getTableHandle(session, new QualifiedTableName("default", "default", "lineitem")).isPresent()) {
-            localQueryRunner.execute("CREATE TABLE lineitem AS SELECT * FROM tpch.sf1.lineitem");
-        }
         return localQueryRunner;
-    }
-
-    private static RaptorConnectorFactory createRaptorConnectorFactory(String cacheDir, NodeManager nodeManager)
-    {
-        try {
-            File dataDir = new File(cacheDir);
-            File databaseDir = new File(dataDir, "db");
-
-            Map<String, String> config = ImmutableMap.<String, String>builder()
-                    .put("metadata.db.type", "h2")
-                    .put("metadata.db.filename", databaseDir.getAbsolutePath())
-                    .put("storage.data-directory", dataDir.getAbsolutePath())
-                    .put("storage.compress", "false")
-                    .build();
-
-            BlockEncodingSerde blockEncodingSerde = createTestingBlockEncodingManager();
-            TypeManager typeManager = new TypeRegistry();
-
-            return new RaptorConnectorFactory(config, nodeManager, blockEncodingSerde, typeManager);
-        }
-        catch (Exception e) {
-            throw Throwables.propagate(e);
-        }
     }
 }
