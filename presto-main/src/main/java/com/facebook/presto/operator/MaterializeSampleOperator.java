ecc20386ae6d37bca077be87d31869730a455053
Dain Sundstrom <dain@iq80.com>
2014-06-17 09:43:29 -0700

    Change MaterializeSampleOperator to not use BlockCursors

--- presto-main/src/main/java/com/facebook/presto/operator/MaterializeSampleOperator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/MaterializeSampleOperator.java
@@ -13,7 +13,7 @@
  */
 package com.facebook.presto.operator;
 
-import com.facebook.presto.spi.block.BlockCursor;
+import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.type.Type;
 import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -67,8 +67,9 @@ public class MaterializeSampleOperator
     private final List<Type> types;
     private final int sampleWeightChannel;
     private boolean finishing;
-    private BlockCursor[] cursors;
-    private BlockCursor sampleWeightCursor;
+    private int position = -1;
+    private Block[] blocks;
+    private Block sampleWeightBlock;
     private long remainingWeight;
     private PageBuilder pageBuilder;
 
@@ -78,7 +79,7 @@ public class MaterializeSampleOperator
         this.sampleWeightChannel = sampleWeightChannel;
         this.types = ImmutableList.copyOf(checkNotNull(types, "types is null"));
         this.pageBuilder = new PageBuilder(types);
-        this.cursors = new BlockCursor[types.size()];
+        this.blocks = new Block[types.size()];
     }
 
     @Override
@@ -102,7 +103,7 @@ public class MaterializeSampleOperator
     @Override
     public boolean isFinished()
     {
-        return finishing && sampleWeightCursor == null && pageBuilder.isEmpty();
+        return finishing && sampleWeightBlock == null && pageBuilder.isEmpty();
     }
 
     @Override
@@ -114,7 +115,7 @@ public class MaterializeSampleOperator
     @Override
     public boolean needsInput()
     {
-        if (finishing || sampleWeightCursor != null) {
+        if (finishing || sampleWeightBlock != null) {
             return false;
         }
         return true;
@@ -125,20 +126,16 @@ public class MaterializeSampleOperator
     {
         checkNotNull(page, "page is null");
         checkState(!finishing, "Operator is finishing");
-        checkState(sampleWeightCursor == null, "Current page has not been completely processed yet");
+        checkState(sampleWeightBlock == null, "Current page has not been completely processed yet");
 
-        BlockCursor[] cursors = new BlockCursor[page.getChannelCount()];
-        for (int i = 0; i < cursors.length; i++) {
-            cursors[i] = page.getBlock(i).cursor();
-        }
+        this.position = -1;
+        this.sampleWeightBlock = page.getBlock(sampleWeightChannel);
 
-        this.sampleWeightCursor = cursors[sampleWeightChannel];
-
-        for (int i = 0, j = 0; i < cursors.length; i++) {
+        for (int i = 0, j = 0; i < page.getChannelCount(); i++) {
             if (i == sampleWeightChannel) {
                 continue;
             }
-            this.cursors[j] = cursors[i];
+            this.blocks[j] = page.getBlock(i);
             j++;
         }
     }
@@ -150,29 +147,23 @@ public class MaterializeSampleOperator
             return true;
         }
 
-        if (sampleWeightCursor == null) {
+        if (sampleWeightBlock == null) {
             return false;
         }
 
-        boolean advanced;
         // Read rows until we find one that has a non-zero weight
-        do {
-            advanced = sampleWeightCursor.advanceNextPosition();
-            for (BlockCursor cursor : cursors) {
-                checkState(advanced == cursor.advanceNextPosition());
+        position++;
+        while (position < sampleWeightBlock.getPositionCount()) {
+            checkState(!(sampleWeightBlock.isNull(position)), "Encountered NULL sample weight");
+            if (sampleWeightBlock.getLong(position) != 0) {
+                remainingWeight = sampleWeightBlock.getLong(position) - 1;
+                return true;
             }
-            checkState(!(advanced && sampleWeightCursor.isNull()), "Encountered NULL sample weight");
-        } while(advanced && sampleWeightCursor.getLong() == 0);
-
-        if (!advanced) {
-            sampleWeightCursor = null;
-            Arrays.fill(cursors, null);
+            position++;
         }
-        else {
-            remainingWeight = sampleWeightCursor.getLong() - 1;
-        }
-
-        return advanced;
+        sampleWeightBlock = null;
+        Arrays.fill(blocks, null);
+        return false;
     }
 
     @Override
@@ -182,13 +173,13 @@ public class MaterializeSampleOperator
             // We might be outputting empty rows, if $sampleWeight is the only column (such as in a COUNT(*) query)
             pageBuilder.declarePosition();
 
-            for (int i = 0; i < cursors.length; i++) {
-                cursors[i].appendTo(pageBuilder.getBlockBuilder(i));
+            for (int i = 0; i < blocks.length; i++) {
+                blocks[i].appendTo(position, pageBuilder.getBlockBuilder(i));
             }
         }
 
         // only flush full pages unless we are done
-        if (pageBuilder.isFull() || (finishing && !pageBuilder.isEmpty() && sampleWeightCursor == null)) {
+        if (pageBuilder.isFull() || (finishing && !pageBuilder.isEmpty() && sampleWeightBlock == null)) {
             Page page = pageBuilder.build();
             pageBuilder.reset();
             return page;
