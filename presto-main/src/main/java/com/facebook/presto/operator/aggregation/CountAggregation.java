60ec398e82ee15f9d56f7dda3bfbf4627fae2864
Christopher Berner <cberner@fb.com>
2014-06-19 11:28:37 -0700

    Remove support for sample weight from exact COUNT aggregation

--- presto-main/src/main/java/com/facebook/presto/operator/aggregation/CountAggregation.java
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/CountAggregation.java
@@ -13,12 +13,12 @@
  */
 package com.facebook.presto.operator.aggregation;
 
+import com.facebook.presto.operator.GroupByIdBlock;
+import com.facebook.presto.operator.Page;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
 import com.facebook.presto.spi.block.BlockCursor;
-import com.facebook.presto.operator.GroupByIdBlock;
-import com.facebook.presto.operator.Page;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.util.array.LongBigArray;
 import com.google.common.base.Optional;
@@ -62,15 +62,16 @@ public class CountAggregation
     @Override
     public CountGroupedAccumulator createGroupedAggregation(Optional<Integer> maskChannel, Optional<Integer> sampleWeightChannel, double confidence, int... argumentChannels)
     {
+        checkArgument(!sampleWeightChannel.isPresent(), "Sampled data not supported");
         checkArgument(confidence == 1.0, "count does not support approximate queries");
-        return new CountGroupedAccumulator(maskChannel, sampleWeightChannel);
+        return new CountGroupedAccumulator(maskChannel);
     }
 
     @Override
     public GroupedAccumulator createGroupedIntermediateAggregation(double confidence)
     {
         checkArgument(confidence == 1.0, "count does not support approximate queries");
-        return new CountGroupedAccumulator(Optional.<Integer>absent(), Optional.<Integer>absent());
+        return new CountGroupedAccumulator(Optional.<Integer>absent());
     }
 
     public static class CountGroupedAccumulator
@@ -78,13 +79,11 @@ public class CountAggregation
     {
         private final LongBigArray counts;
         private final Optional<Integer> maskChannel;
-        private final Optional<Integer> sampleWeightChannel;
 
-        public CountGroupedAccumulator(Optional<Integer> maskChannel, Optional<Integer> sampleWeightChannel)
+        public CountGroupedAccumulator(Optional<Integer> maskChannel)
         {
             this.counts = new LongBigArray();
             this.maskChannel = maskChannel;
-            this.sampleWeightChannel = sampleWeightChannel;
         }
 
         @Override
@@ -110,13 +109,13 @@ public class CountAggregation
         {
             counts.ensureCapacity(groupIdsBlock.getGroupCount());
             BlockCursor masks = maskChannel.isPresent() ? page.getBlock(maskChannel.get()).cursor() : null;
-            BlockCursor sampleWeights = sampleWeightChannel.isPresent() ? page.getBlock(sampleWeightChannel.get()).cursor() : null;
 
             for (int position = 0; position < groupIdsBlock.getPositionCount(); position++) {
                 long groupId = groupIdsBlock.getGroupId(position);
                 checkState(masks == null || masks.advanceNextPosition());
-                checkState(sampleWeights == null || sampleWeights.advanceNextPosition());
-                counts.add(groupId, SimpleAggregationFunction.computeSampleWeight(masks, sampleWeights));
+                if (masks == null || masks.getBoolean()) {
+                    counts.increment(groupId);
+                }
             }
         }
 
@@ -152,15 +151,16 @@ public class CountAggregation
     @Override
     public CountAccumulator createAggregation(Optional<Integer> maskChannel, Optional<Integer> sampleWeightChannel, double confidence, int... argumentChannels)
     {
+        checkArgument(!sampleWeightChannel.isPresent(), "Sampled data not supported");
         checkArgument(confidence == 1.0, "count does not support approximate queries");
-        return new CountAccumulator(maskChannel, sampleWeightChannel);
+        return new CountAccumulator(maskChannel);
     }
 
     @Override
     public CountAccumulator createIntermediateAggregation(double confidence)
     {
         checkArgument(confidence == 1.0, "count does not support approximate queries");
-        return new CountAccumulator(Optional.<Integer>absent(), Optional.<Integer>absent());
+        return new CountAccumulator(Optional.<Integer>absent());
     }
 
     public static class CountAccumulator
@@ -168,12 +168,10 @@ public class CountAggregation
     {
         private long count;
         private final Optional<Integer> maskChannel;
-        private final Optional<Integer> sampleWeightChannel;
 
-        public CountAccumulator(Optional<Integer> maskChannel, Optional<Integer> sampleWeightChannel)
+        public CountAccumulator(Optional<Integer> maskChannel)
         {
             this.maskChannel = maskChannel;
-            this.sampleWeightChannel = sampleWeightChannel;
         }
 
         @Override
@@ -197,22 +195,16 @@ public class CountAggregation
         @Override
         public void addInput(Page page)
         {
-            if (!maskChannel.isPresent() && !sampleWeightChannel.isPresent()) {
+            if (!maskChannel.isPresent()) {
                 count += page.getPositionCount();
             }
             else {
-                BlockCursor masks = null;
-                if (maskChannel.isPresent()) {
-                    masks = page.getBlock(maskChannel.get()).cursor();
-                }
-                BlockCursor sampleWeights = null;
-                if (sampleWeightChannel.isPresent()) {
-                    sampleWeights = page.getBlock(sampleWeightChannel.get()).cursor();
-                }
+                BlockCursor masks = page.getBlock(maskChannel.get()).cursor();
                 for (int i = 0; i < page.getPositionCount(); i++) {
                     checkState(masks == null || masks.advanceNextPosition());
-                    checkState(sampleWeights == null || sampleWeights.advanceNextPosition());
-                    count += SimpleAggregationFunction.computeSampleWeight(masks, sampleWeights);
+                    if (masks == null || masks.getBoolean()) {
+                        count++;
+                    }
                 }
             }
         }
