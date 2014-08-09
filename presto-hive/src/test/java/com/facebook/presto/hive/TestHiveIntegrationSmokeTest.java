5bf672088fe2d73dc2f303e96c295b700ef9860a
Dain Sundstrom <dain@iq80.com>
2014-08-09 01:17:09 -0700

    Add Hive write support for varbinary, date and timestamp
    Fixes #1425

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
@@ -13,11 +13,22 @@
  */
 package com.facebook.presto.hive;
 
+import com.facebook.presto.testing.MaterializedResult;
+import com.facebook.presto.testing.MaterializedRow;
 import com.facebook.presto.tests.AbstractTestIntegrationSmokeTest;
+import org.joda.time.DateTime;
+import org.testng.annotations.Test;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 import static com.facebook.presto.hive.HiveQueryRunner.createQueryRunner;
 import static com.facebook.presto.hive.HiveQueryRunner.createSampledSession;
 import static io.airlift.tpch.TpchTable.ORDERS;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.joda.time.DateTimeZone.UTC;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 
 public class TestHiveIntegrationSmokeTest
         extends AbstractTestIntegrationSmokeTest
@@ -26,5 +37,37 @@ public class TestHiveIntegrationSmokeTest
             throws Exception
     {
         super(createQueryRunner(ORDERS), createSampledSession());
+    }
+
+    @Test
+    public void createTableWithEveryType()
+            throws Exception
+    {
+        String query = "" +
+                "CREATE TABLE test_types_table AS " +
+                "SELECT" +
+                " 'foo' _varchar" +
+                ", cast('bar' as varbinary) _varbinary" +
+                ", 1 _bigint" +
+                ", 3.14 _double" +
+                ", true _boolean" +
+                ", DATE '1980-05-07' _date" +
+                ", TIMESTAMP '1980-05-07 11:22:33.456' _timestamp";
+
+        assertQuery(query, "SELECT 1");
+
+        MaterializedResult results = queryRunner.execute(getSession(), "SELECT * FROM test_types_table").toJdbcTypes();
+        assertEquals(results.getRowCount(), 1);
+        MaterializedRow row = results.getMaterializedRows().get(0);
+        assertEquals(row.getField(0), "foo");
+        assertEquals(row.getField(1), "bar".getBytes(UTF_8));
+        assertEquals(row.getField(2), 1L);
+        assertEquals(row.getField(3), 3.14);
+        assertEquals(row.getField(4), true);
+        assertEquals(row.getField(5), new Date(new DateTime(1980, 5, 7, 0, 0, 0, UTC).getMillis()));
+        assertEquals(row.getField(6), new Timestamp(new DateTime(1980, 5, 7, 11, 22, 33, 456, UTC).getMillis()));
+        assertQueryTrue("DROP TABLE test_types_table");
+
+        assertFalse(queryRunner.tableExists(getSession(), "test_types_table"));
     }
 }
