39c2aa0de45287cfdf12177b1e0db04f778df582
Dain Sundstrom <dain@iq80.com>
2014-08-17 19:48:26 -0700

    Simplify OrderingCompiler

--- presto-main/src/main/java/com/facebook/presto/sql/gen/OrderingCompiler.java
+++ presto-main/src/main/java/com/facebook/presto/sql/gen/OrderingCompiler.java
@@ -23,6 +23,7 @@ import com.facebook.presto.byteCode.MethodDefinition;
 import com.facebook.presto.byteCode.ParameterizedType;
 import com.facebook.presto.byteCode.SmartClassWriter;
 import com.facebook.presto.byteCode.Variable;
+import com.facebook.presto.byteCode.expression.ByteCodeExpression;
 import com.facebook.presto.byteCode.instruction.LabelNode;
 import com.facebook.presto.operator.PagesIndex;
 import com.facebook.presto.operator.PagesIndexComparator;
@@ -52,7 +53,6 @@ import org.objectweb.asm.util.TraceClassVisitor;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Method;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -62,11 +62,21 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static com.facebook.presto.byteCode.Access.FINAL;
+import static com.facebook.presto.byteCode.Access.PRIVATE;
 import static com.facebook.presto.byteCode.Access.PUBLIC;
+import static com.facebook.presto.byteCode.Access.STATIC;
+import static com.facebook.presto.byteCode.Access.VOLATILE;
 import static com.facebook.presto.byteCode.Access.a;
 import static com.facebook.presto.byteCode.NamedParameterDefinition.arg;
 import static com.facebook.presto.byteCode.ParameterizedType.type;
 import static com.facebook.presto.byteCode.ParameterizedType.typeFromPathName;
+import static com.facebook.presto.byteCode.expression.ByteCodeExpression.constantInt;
+import static com.facebook.presto.byteCode.expression.ByteCodeExpression.getStatic;
+import static com.facebook.presto.byteCode.expression.ByteCodeExpression.invokeStatic;
+import static com.facebook.presto.sql.gen.Bootstrap.BOOTSTRAP_METHOD;
+import static com.facebook.presto.sql.gen.Bootstrap.CALL_SITES_FIELD_NAME;
+import static com.facebook.presto.sql.gen.ByteCodeUtils.setCallSitesField;
+import static com.facebook.presto.sql.gen.SqlTypeByteCodeExpression.constantType;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class OrderingCompiler
@@ -79,8 +89,6 @@ public class OrderingCompiler
     private static final boolean DUMP_BYTE_CODE_RAW = false;
     private static final boolean RUN_ASM_VERIFIER = false; // verifier doesn't work right now
     private static final AtomicReference<String> DUMP_CLASS_FILES_TO = new AtomicReference<>();
-
-    private final Method bootstrapMethod = null;
 
     private final LoadingCache<PagesIndexComparatorCacheKey, PagesIndexOrdering> pagesIndexOrderings = CacheBuilder.newBuilder().maximumSize(1000).build(
             new CacheLoader<PagesIndexComparatorCacheKey, PagesIndexOrdering>()
@@ -136,22 +144,27 @@ public class OrderingCompiler
             List<SortOrder> sortOrders,
             DynamicClassLoader classLoader)
     {
-        ClassDefinition classDefinition = new ClassDefinition(new CompilerContext(bootstrapMethod),
+        CallSiteBinder callSiteBinder = new CallSiteBinder();
+
+        ClassDefinition classDefinition = new ClassDefinition(new CompilerContext(BOOTSTRAP_METHOD),
                 a(PUBLIC, FINAL),
                 typeFromPathName("PagesIndexComparator" + CLASS_ID.incrementAndGet()),
                 type(Object.class),
                 type(PagesIndexComparator.class));
 
-        generateConstructor(classDefinition);
-        generateCompareTo(classDefinition, sortTypes, sortChannels, sortOrders);
+        classDefinition.declareField(a(PRIVATE, STATIC, VOLATILE), CALL_SITES_FIELD_NAME, Map.class);
 
-        Class<? extends PagesIndexComparator> joinHashClass = defineClass(classDefinition, PagesIndexComparator.class, classLoader);
-        return joinHashClass;
+        generateConstructor(classDefinition);
+        generateCompareTo(classDefinition, callSiteBinder, sortTypes, sortChannels, sortOrders);
+
+        Class<? extends PagesIndexComparator> comparatorClass = defineClass(classDefinition, PagesIndexComparator.class, classLoader);
+        setCallSitesField(comparatorClass, callSiteBinder.getBindings());
+        return comparatorClass;
     }
 
     private void generateConstructor(ClassDefinition classDefinition)
     {
-        classDefinition.declareConstructor(new CompilerContext(bootstrapMethod),
+        classDefinition.declareConstructor(new CompilerContext(BOOTSTRAP_METHOD),
                 a(PUBLIC))
                 .getBody()
                 .comment("super();")
@@ -160,10 +173,10 @@ public class OrderingCompiler
                 .ret();
     }
 
-    private void generateCompareTo(ClassDefinition classDefinition, List<Type> sortTypes, List<Integer> sortChannels, List<SortOrder> sortOrders)
+    private void generateCompareTo(ClassDefinition classDefinition, CallSiteBinder callSiteBinder, List<Type> sortTypes, List<Integer> sortChannels, List<SortOrder> sortOrders)
     {
-        CompilerContext compilerContext = new CompilerContext(bootstrapMethod);
-        MethodDefinition compareToMethod = classDefinition.declareMethod(compilerContext,
+        CompilerContext context = new CompilerContext(BOOTSTRAP_METHOD);
+        MethodDefinition compareToMethod = classDefinition.declareMethod(context,
                 a(PUBLIC),
                 "compareTo",
                 type(int.class),
@@ -171,109 +184,76 @@ public class OrderingCompiler
                 arg("leftPosition", int.class),
                 arg("rightPosition", int.class));
 
-        Variable valueAddresses = compilerContext.declareVariable(LongArrayList.class, "valueAddresses");
+        Variable valueAddresses = context.declareVariable(LongArrayList.class, "valueAddresses");
         compareToMethod
                 .getBody()
                 .comment("LongArrayList valueAddresses = pagesIndex.valueAddresses")
-                .getVariable("pagesIndex")
-                .invokeVirtual(PagesIndex.class, "getValueAddresses", LongArrayList.class)
-                .putVariable(valueAddresses);
+                .append(valueAddresses.set(context.getVariable("pagesIndex").invoke("getValueAddresses", LongArrayList.class)));
 
-        Variable leftPageAddress = compilerContext.declareVariable(long.class, "leftPageAddress");
+        Variable leftPageAddress = context.declareVariable(long.class, "leftPageAddress");
         compareToMethod
                 .getBody()
                 .comment("long leftPageAddress = valueAddresses.getLong(leftPosition)")
-                .getVariable(valueAddresses)
-                .getVariable("leftPosition")
-                .invokeVirtual(LongArrayList.class, "getLong", long.class, int.class)
-                .putVariable(leftPageAddress);
+                .append(leftPageAddress.set(valueAddresses.invoke("getLong", long.class, context.getVariable("leftPosition"))));
 
-        Variable leftBlockIndex = compilerContext.declareVariable(int.class, "leftBlockIndex");
+        Variable leftBlockIndex = context.declareVariable(int.class, "leftBlockIndex");
         compareToMethod
                 .getBody()
                 .comment("int leftBlockIndex = decodeSliceIndex(leftPageAddress)")
-                .getVariable(leftPageAddress)
-                .invokeStatic(SyntheticAddress.class, "decodeSliceIndex", int.class, long.class)
-                .putVariable(leftBlockIndex);
+                .append(leftBlockIndex.set(invokeStatic(SyntheticAddress.class, "decodeSliceIndex", int.class, leftPageAddress)));
 
-        Variable leftBlockPosition = compilerContext.declareVariable(int.class, "leftBlockPosition");
+        Variable leftBlockPosition = context.declareVariable(int.class, "leftBlockPosition");
         compareToMethod
                 .getBody()
                 .comment("int leftBlockPosition = decodePosition(leftPageAddress)")
-                .getVariable(leftPageAddress)
-                .invokeStatic(SyntheticAddress.class, "decodePosition", int.class, long.class)
-                .putVariable(leftBlockPosition);
+                .append(leftBlockPosition.set(invokeStatic(SyntheticAddress.class, "decodePosition", int.class, leftPageAddress)));
 
-        Variable rightPageAddress = compilerContext.declareVariable(long.class, "rightPageAddress");
+        Variable rightPageAddress = context.declareVariable(long.class, "rightPageAddress");
         compareToMethod
                 .getBody()
                 .comment("long rightPageAddress = valueAddresses.getLong(rightPosition);")
-                .getVariable(valueAddresses)
-                .getVariable("rightPosition")
-                .invokeVirtual(LongArrayList.class, "getLong", long.class, int.class)
-                .putVariable(rightPageAddress);
+                .append(rightPageAddress.set(valueAddresses.invoke("getLong", long.class, context.getVariable("rightPosition"))));
 
-        Variable rightBlockIndex = compilerContext.declareVariable(int.class, "rightBlockIndex");
+        Variable rightBlockIndex = context.declareVariable(int.class, "rightBlockIndex");
         compareToMethod
                 .getBody()
                 .comment("int rightBlockIndex = decodeSliceIndex(rightPageAddress)")
-                .getVariable(rightPageAddress)
-                .invokeStatic(SyntheticAddress.class, "decodeSliceIndex", int.class, long.class)
-                .putVariable(rightBlockIndex);
+                .append(rightBlockIndex.set(invokeStatic(SyntheticAddress.class, "decodeSliceIndex", int.class, rightPageAddress)));
 
-        Variable rightBlockPosition = compilerContext.declareVariable(int.class, "rightBlockPosition");
+        Variable rightBlockPosition = context.declareVariable(int.class, "rightBlockPosition");
         compareToMethod
                 .getBody()
                 .comment("int rightBlockPosition = decodePosition(rightPageAddress)")
-                .getVariable(rightPageAddress)
-                .invokeStatic(SyntheticAddress.class, "decodePosition", int.class, long.class)
-                .putVariable(rightBlockPosition);
+                .append(rightBlockPosition.set(invokeStatic(SyntheticAddress.class, "decodePosition", int.class, rightPageAddress)));
 
         for (int i = 0; i < sortChannels.size(); i++) {
             int sortChannel = sortChannels.get(i);
             SortOrder sortOrder = sortOrders.get(i);
 
-            Block block = new Block(compilerContext)
+            Block block = new Block(context)
                     .setDescription("compare channel " + sortChannel + " " + sortOrder);
 
-            block.comment("push sortOrder")
-                    .getStaticField(SortOrder.class, sortOrder.name(), SortOrder.class);
-
             Type sortType = sortTypes.get(i);
-            block.comment("push sortType")
-                    .invokeStatic(sortType.getClass(), "getInstance", sortType.getClass());
 
-            block.comment("push leftBlock -- pagesIndex.getChannel(sortChannel).get(leftBlockIndex)")
-                    .getVariable("pagesIndex")
-                    .push(sortChannel)
-                    .invokeVirtual(PagesIndex.class, "getChannel", ObjectArrayList.class, int.class)
-                    .getVariable(leftBlockIndex)
-                    .invokeVirtual(ObjectArrayList.class, "get", Object.class, int.class)
-                    .checkCast(com.facebook.presto.spi.block.Block.class);
+            ByteCodeExpression leftBlock = context.getVariable("pagesIndex")
+                    .invoke("getChannel", ObjectArrayList.class, constantInt(sortChannel))
+                    .invoke("get", Object.class, leftBlockIndex)
+                    .cast(com.facebook.presto.spi.block.Block.class);
 
-            block.comment("push leftBlockPosition")
-                    .getVariable(leftBlockPosition);
+            ByteCodeExpression rightBlock = context.getVariable("pagesIndex")
+                    .invoke("getChannel", ObjectArrayList.class, constantInt(sortChannel))
+                    .invoke("get", Object.class, rightBlockIndex)
+                    .cast(com.facebook.presto.spi.block.Block.class);
 
-            block.comment("push rightBlock -- pagesIndex.getChannel(sortChannel).get(rightBlockIndex)")
-                    .getVariable("pagesIndex")
-                    .push(sortChannel)
-                    .invokeVirtual(PagesIndex.class, "getChannel", ObjectArrayList.class, int.class)
-                    .getVariable(rightBlockIndex)
-                    .invokeVirtual(ObjectArrayList.class, "get", Object.class, int.class)
-                    .checkCast(com.facebook.presto.spi.block.Block.class);
-
-            block.comment("push rightBlockPosition")
-                    .getVariable(rightBlockPosition);
-
-            block.comment("invoke compareTo")
-                    .invokeVirtual(SortOrder.class,
-                            "compareBlockValue",
+            block.append(getStatic(SortOrder.class, sortOrder.name())
+                    .invoke("compareBlockValue",
                             int.class,
-                            Type.class,
-                            com.facebook.presto.spi.block.Block.class,
-                            int.class,
-                            com.facebook.presto.spi.block.Block.class,
-                            int.class);
+                            ImmutableList.of(Type.class, com.facebook.presto.spi.block.Block.class, int.class, com.facebook.presto.spi.block.Block.class, int.class),
+                            constantType(context, callSiteBinder, sortType),
+                            leftBlock,
+                            leftBlockPosition,
+                            rightBlock,
+                            rightBlockPosition));
 
             LabelNode equal = new LabelNode("equal");
             block.comment("if (compare != 0) return compare")
