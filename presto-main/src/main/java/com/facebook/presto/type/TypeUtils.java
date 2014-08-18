bbd836e74531dfc68137000237507fea7743ad2e
Dain Sundstrom <dain@iq80.com>
2014-08-17 19:30:55 -0700

    Add utility methods for equals and hash of type
    Utility methods performn equals and hash code of sql values with respect for nulls

---
+++ presto-main/src/main/java/com/facebook/presto/type/TypeUtils.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.type;
+
+import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.type.Type;
+
+public final class TypeUtils
+{
+    private TypeUtils()
+    {
+    }
+
+    public static int hashPosition(Type type, Block block, int position)
+    {
+        if (block.isNull(position)) {
+            return 0;
+        }
+        return type.hash(block, position);
+    }
+
+    public static boolean positionEqualsPosition(Type type, Block leftBlock, int leftPosition, Block rightBlock, int rightPosition)
+    {
+        boolean leftIsNull = leftBlock.isNull(leftPosition);
+        boolean rightIsNull = rightBlock.isNull(rightPosition);
+        if (leftIsNull || rightIsNull) {
+            return leftIsNull && rightIsNull;
+        }
+        return type.equalTo(leftBlock, leftPosition, rightBlock, rightPosition);
+    }
+}
