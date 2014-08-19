7a07d08aea69d1e96f9500195ad48e2cef47b25d
Dain Sundstrom <dain@iq80.com>
2014-08-18 23:12:47 -0700

    Add ByteCodeExpression test utils
    Add ConstantByteCodeExpression test
    Improve rendering of ConstantByteCodeExpression

---
+++ presto-main/src/test/java/com/facebook/presto/byteCode/expression/ByteCodeExpressionAssertions.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.byteCode.expression;
+
+import com.facebook.presto.byteCode.ClassDefinition;
+import com.facebook.presto.byteCode.ClassInfoLoader;
+import com.facebook.presto.byteCode.CompilerContext;
+import com.facebook.presto.byteCode.DumpByteCodeVisitor;
+import com.facebook.presto.byteCode.DynamicClassLoader;
+import com.facebook.presto.byteCode.SmartClassWriter;
+import com.google.common.collect.ImmutableList;
+import org.objectweb.asm.ClassWriter;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static com.facebook.presto.byteCode.Access.FINAL;
+import static com.facebook.presto.byteCode.Access.PUBLIC;
+import static com.facebook.presto.byteCode.Access.STATIC;
+import static com.facebook.presto.byteCode.Access.a;
+import static com.facebook.presto.byteCode.ParameterizedType.type;
+import static com.facebook.presto.byteCode.ParameterizedType.typeFromPathName;
+import static com.facebook.presto.sql.gen.Bootstrap.BOOTSTRAP_METHOD;
+import static org.testng.Assert.assertEquals;
+
+public final class ByteCodeExpressionAssertions
+{
+    private ByteCodeExpressionAssertions()
+    {
+    }
+
+    private static final AtomicLong CLASS_ID = new AtomicLong();
+    private static final boolean DUMP_BYTE_CODE_TREE = false;
+
+    public static void assertByteCodeExpression(ByteCodeExpression expression, Object expected, String expectedRendering)
+            throws Exception
+    {
+        assertEquals(expression.toString(), expectedRendering);
+
+        ClassDefinition classDefinition = new ClassDefinition(new CompilerContext(BOOTSTRAP_METHOD),
+                a(PUBLIC, FINAL),
+                typeFromPathName("Test_" + CLASS_ID.incrementAndGet()),
+                type(Object.class));
+
+        classDefinition.declareMethod(new CompilerContext(BOOTSTRAP_METHOD), a(PUBLIC, STATIC), "test", expression.getType())
+                .getBody()
+                .append(expression.ret());
+
+        if (DUMP_BYTE_CODE_TREE) {
+            DumpByteCodeVisitor dumpByteCode = new DumpByteCodeVisitor(System.out);
+            dumpByteCode.visitClass(classDefinition);
+        }
+
+        DynamicClassLoader classLoader = new DynamicClassLoader();
+        ClassWriter cw = new SmartClassWriter(ClassInfoLoader.createClassInfoLoader(ImmutableList.of(classDefinition), classLoader));
+        classDefinition.visit(cw);
+
+        Class<?> clazz = classLoader.defineClass(classDefinition.getType().getJavaClassName(), cw.toByteArray());
+        Object actual = clazz.getMethod("test").invoke(null);
+        assertEquals(actual, expected);
+    }
+}
