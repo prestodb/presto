a46911f2efdd71ee6448530ae6a9d08f10d2ed16
Dain Sundstrom <dain@iq80.com>
2014-08-21 13:45:32 -0700

    Add isPrimitive to ParameterizedType

--- presto-main/src/main/java/com/facebook/presto/byteCode/expression/GetFieldByteCodeExpression.java
+++ presto-main/src/main/java/com/facebook/presto/byteCode/expression/GetFieldByteCodeExpression.java
@@ -71,7 +71,7 @@ class GetFieldByteCodeExpression
     public GetFieldByteCodeExpression(@Nullable ByteCodeExpression instance, ParameterizedType declaringClass, String name, ParameterizedType type)
     {
         super(type);
-        checkArgument(instance == null || instance.getType().getPrimitiveType() == null, "Type %s does not have fields", getType());
+        checkArgument(instance == null || !instance.getType().isPrimitive(), "Type %s does not have fields", getType());
         this.instance = instance;
         this.declaringClass = checkNotNull(declaringClass, "declaringClass is null");
         this.name = checkNotNull(name, "name is null");
