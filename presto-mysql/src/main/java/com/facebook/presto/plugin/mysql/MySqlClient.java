f877d2db95a3e4a7f5ab0d91ac6d253c5f05eef4
David Phillips <david@acz.org>
2014-08-25 18:10:53 -0700

    Add MySQL connector

---
+++ presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlClient.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.plugin.mysql;
+
+import com.facebook.presto.plugin.jdbc.BaseJdbcClient;
+import com.facebook.presto.plugin.jdbc.BaseJdbcConfig;
+import com.facebook.presto.plugin.jdbc.JdbcConnectorId;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableSet;
+import com.mysql.jdbc.Driver;
+
+import javax.inject.Inject;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Set;
+
+public class MySqlClient
+        extends BaseJdbcClient
+{
+    @Inject
+    public MySqlClient(JdbcConnectorId connectorId, BaseJdbcConfig config)
+            throws SQLException
+    {
+        super(connectorId, config, "`", new Driver());
+    }
+
+    @Override
+    public Set<String> getSchemaNames()
+    {
+        // for MySQL, we need to list catalogs instead of schemas
+        try (Connection connection = driver.connect(connectionUrl, connectionProperties);
+                ResultSet resultSet = connection.getMetaData().getCatalogs()) {
+            ImmutableSet.Builder<String> schemaNames = ImmutableSet.builder();
+            while (resultSet.next()) {
+                String schemaName = resultSet.getString("TABLE_CAT").toLowerCase();
+                // skip internal schemas
+                if (!schemaName.equals("information_schema") && !schemaName.equals("mysql")) {
+                    schemaNames.add(schemaName);
+                }
+            }
+            return schemaNames.build();
+        }
+        catch (SQLException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    @Override
+    protected ResultSet getTables(Connection connection, String schemaName, String tableName)
+            throws SQLException
+    {
+        // MySQL maps their "database" to SQL catalogs and does not have schemas
+        return connection.getMetaData().getTables(schemaName, null, tableName, new String[] {"TABLE"});
+    }
+
+    @Override
+    protected String toSqlType(Type type)
+    {
+        String sqlType = super.toSqlType(type);
+        switch (sqlType) {
+            case "varchar":
+                return "mediumtext";
+            case "varbinary":
+                return "mediumblob";
+            case "time with timezone":
+                return "time";
+            case "timestamp":
+            case "timestamp with timezone":
+                return "datetime";
+        }
+        return sqlType;
+    }
+}
---
+++ presto-mysql/src/test/java/com/facebook/presto/plugin/mysql/TestMySqlDistributedQueries.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.plugin.mysql;
+
+import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.testing.QueryRunner;
+import com.facebook.presto.tests.AbstractTestQueries;
+import com.facebook.presto.tests.DistributedQueryRunner;
+import com.facebook.presto.tpch.TpchPlugin;
+import com.google.common.collect.ImmutableMap;
+import io.airlift.log.Logger;
+import io.airlift.testing.mysql.TestingMySqlServer;
+import org.testng.annotations.AfterClass;
+
+import java.util.Map;
+
+import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
+import static com.facebook.presto.tests.QueryAssertions.copyAllTables;
+import static com.facebook.presto.tpch.TpchMetadata.TINY_SCHEMA_NAME;
+import static io.airlift.units.Duration.nanosSince;
+import static java.util.Locale.ENGLISH;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class TestMySqlDistributedQueries
+        extends AbstractTestQueries
+{
+    private static final Logger log = Logger.get(TestMySqlDistributedQueries.class);
+
+    private final TestingMySqlServer mysqlServer;
+
+    @SuppressWarnings("UnusedDeclaration")
+    public TestMySqlDistributedQueries()
+            throws Exception
+    {
+        this(new TestingMySqlServer("testuser", "testpass", "tpch"));
+    }
+
+    public TestMySqlDistributedQueries(TestingMySqlServer mysqlServer)
+            throws Exception
+    {
+        super(createQueryRunner(mysqlServer));
+        this.mysqlServer = mysqlServer;
+    }
+
+    @AfterClass(alwaysRun = true)
+    @SuppressWarnings({"EmptyTryBlock", "UnusedDeclaration"})
+    public void destroy()
+    {
+        try (QueryRunner queryRunner = this.queryRunner;
+                TestingMySqlServer mysqlServer = this.mysqlServer) {
+            // use try-with-resources to close everything safely
+        }
+    }
+
+    private static QueryRunner createQueryRunner(TestingMySqlServer server)
+            throws Exception
+    {
+        try {
+            return doCreateQueryRunner(server);
+        }
+        catch (Exception e) {
+            // use try-with-resources to close everything safely
+            try (TestingMySqlServer ignored = server) {
+                throw e;
+            }
+        }
+    }
+
+    private static QueryRunner doCreateQueryRunner(TestingMySqlServer server)
+            throws Exception
+    {
+        DistributedQueryRunner queryRunner = new DistributedQueryRunner(createSession("tpch"), 2);
+
+        queryRunner.installPlugin(new TpchPlugin());
+        queryRunner.createCatalog("tpch", "tpch");
+
+        Map<String, String> properties = ImmutableMap.of("connection-url", server.getJdbcUrl());
+
+        queryRunner.installPlugin(new MySqlPlugin());
+        queryRunner.createCatalog("mysql", "mysql", properties);
+
+        log.info("Loading data...");
+        long startTime = System.nanoTime();
+        copyAllTables(queryRunner, "tpch", TINY_SCHEMA_NAME, createSession("tpch"));
+        log.info("Loading complete in %s", nanosSince(startTime).toString(SECONDS));
+
+        return queryRunner;
+    }
+
+    private static ConnectorSession createSession(String schema)
+    {
+        return new ConnectorSession("user", "test", "mysql", schema, UTC_KEY, ENGLISH, null, null);
+    }
+}
