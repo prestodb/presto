0b69d016dc973973e6e2bed807a7fe5820162d5f
David Phillips <david@acz.org>
2014-09-04 16:17:14 -0700

    Add Hive integration tests for all file formats

---
+++ presto-hive/src/test/sql/drop-test-hive12.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS presto_test_types_textfile;
+DROP TABLE IF EXISTS presto_test_types_sequencefile;
+DROP TABLE IF EXISTS presto_test_types_rctext;
+DROP TABLE IF EXISTS presto_test_types_rcbinary;
+DROP TABLE IF EXISTS presto_test_types_orc;
