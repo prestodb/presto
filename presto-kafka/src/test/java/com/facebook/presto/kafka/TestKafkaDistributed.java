1f46301e479f9c30280aef53a54e66a2da78c5bd
Dain Sundstrom <dain@iq80.com>
2014-09-16 11:29:37 -0700

    Align Kafka integration tests with other connectors

--- presto-kafka/src/test/java/com/facebook/presto/kafka/TestKafkaDistributed.java
+++ presto-kafka/src/test/java/com/facebook/presto/kafka/TestKafkaDistributed.java
@@ -13,109 +13,42 @@
  */
 package com.facebook.presto.kafka;
 
-import com.facebook.presto.kafka.util.CodecSupplier;
 import com.facebook.presto.kafka.util.EmbeddedKafka;
-import com.facebook.presto.kafka.util.TestUtils;
-import com.facebook.presto.metadata.QualifiedTableName;
-import com.facebook.presto.spi.ConnectorSession;
-import com.facebook.presto.spi.SchemaTableName;
-import com.facebook.presto.testing.QueryRunner;
 import com.facebook.presto.tests.AbstractTestDistributedQueries;
-import com.facebook.presto.tests.DistributedQueryRunner;
-import com.facebook.presto.tpch.TpchMetadata;
-import com.facebook.presto.tpch.TpchPlugin;
-import com.google.common.collect.ImmutableMap;
-import io.airlift.json.JsonCodec;
-import io.airlift.log.Logger;
 import io.airlift.tpch.TpchTable;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-import static com.facebook.presto.kafka.util.TestUtils.loadTpchTopic;
-import static com.facebook.presto.kafka.util.TestUtils.loadTpchTopicDescription;
-import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
-import static com.google.common.base.Preconditions.checkState;
-import static io.airlift.units.Duration.nanosSince;
-import static java.util.Locale.ENGLISH;
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static com.facebook.presto.kafka.KafkaQueryRunner.createKafkaQueryRunner;
+import static com.facebook.presto.kafka.util.EmbeddedKafka.createEmbeddedKafka;
+import static io.airlift.testing.Closeables.closeAllRuntimeException;
 
 @Test
 public class TestKafkaDistributed
         extends AbstractTestDistributedQueries
 {
-    private static final Logger log = Logger.get("TestQueries");
-
-    private static final String TPCH_NAME = "tpch";
-    private static final ConnectorSession TPCH_SESSION = new ConnectorSession("user", "test", "kafka", TPCH_NAME, UTC_KEY, ENGLISH, null, null);
-
-    private EmbeddedKafka embeddedKafka;
+    private final EmbeddedKafka embeddedKafka;
 
     public TestKafkaDistributed()
             throws Exception
     {
-        super(createQueryRunner());
-        checkState(queryRunner instanceof DistributedQueryRunner, "test must be run using the DistributedQueryRunner");
+        this(createEmbeddedKafka());
     }
 
-    private DistributedQueryRunner queryRunner()
-    {
-        return DistributedQueryRunner.class.cast(queryRunner);
-    }
-
-    @BeforeClass
-    public void startKafka()
+    public TestKafkaDistributed(EmbeddedKafka embeddedKafka)
             throws Exception
     {
-        embeddedKafka = EmbeddedKafka.createEmbeddedKafka();
-        embeddedKafka.start();
-
-        JsonCodec<KafkaTopicDescription> topicDescriptionJsonCodec = new CodecSupplier<>(KafkaTopicDescription.class, queryRunner().getCoordinator().getMetadata()).get();
-
-        ImmutableMap.Builder<SchemaTableName, KafkaTopicDescription> topicDescriptionBuilder = ImmutableMap.builder();
-
-        log.info("Loading data...");
-        long startTime = System.nanoTime();
-
-        for (TpchTable<?> table : TpchTable.getTables()) {
-            String tableName = table.getTableName();
-            SchemaTableName tpchTable = new SchemaTableName(TPCH_NAME, tableName);
-
-            log.info("Loading table %s...", tableName);
-
-            embeddedKafka.createTopics(tpchTable.toString());
-
-            topicDescriptionBuilder.put(loadTpchTopicDescription(topicDescriptionJsonCodec, tpchTable.toString(), tpchTable));
-            loadTpchTopic(embeddedKafka, queryRunner().getClient(), tpchTable.toString(), new QualifiedTableName(TPCH_NAME, TpchMetadata.TINY_SCHEMA_NAME, tableName));
-        }
-
-        TestUtils.installKafkaPlugin(embeddedKafka, queryRunner, topicDescriptionBuilder.build());
-
-        log.info("Loading complete in %s", nanosSince(startTime).toString(SECONDS));
+        super(createKafkaQueryRunner(embeddedKafka, TpchTable.getTables()));
+        this.embeddedKafka = embeddedKafka;
     }
 
     @AfterClass(alwaysRun = true)
-    @SuppressWarnings({"EmptyTryBlock", "UnusedDeclaration"})
     public void destroy()
             throws IOException
     {
-        try (QueryRunner queryRunner = this.queryRunner;
-                EmbeddedKafka embeddedKafka = this.embeddedKafka) {
-            // use try-with-resources to close everything safely
-        }
-    }
-
-    private static QueryRunner createQueryRunner()
-            throws Exception
-    {
-        DistributedQueryRunner queryRunner = new DistributedQueryRunner(TPCH_SESSION, 4);
-
-        queryRunner.installPlugin(new TpchPlugin());
-        queryRunner.createCatalog(TPCH_NAME, TPCH_NAME);
-
-        return queryRunner;
+        closeAllRuntimeException(queryRunner, embeddedKafka);
     }
 
     //
@@ -164,34 +97,6 @@ public class TestKafkaDistributed
 
     @Override
     public void testViewMetadata()
-            throws Exception
-    {
-    }
-
-    //
-    // Kafka connector does not support sampling.
-    //
-
-    @Override
-    public void testApproximateQueryCount()
-            throws Exception
-    {
-    }
-
-    @Override
-    public void testApproximateQueryCountCustkey()
-            throws Exception
-    {
-    }
-
-    @Override
-    public void testApproximateQuerySum()
-            throws Exception
-    {
-    }
-
-    @Override
-    public void testApproximateQueryAverage()
             throws Exception
     {
     }
