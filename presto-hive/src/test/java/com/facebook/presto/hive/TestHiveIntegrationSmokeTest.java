4bf1b43300107776193b159fab2fe5a0b57bcbde
Dain Sundstrom <dain@iq80.com>
2014-09-16 15:58:38 -0700

    Align Hive integration tests with other connectors

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
@@ -13,86 +13,18 @@
  */
 package com.facebook.presto.hive;
 
-import com.facebook.presto.hive.metastore.InMemoryHiveMetastore;
-import com.facebook.presto.spi.ConnectorSession;
-import com.facebook.presto.testing.QueryRunner;
 import com.facebook.presto.tests.AbstractTestIntegrationSmokeTest;
-import com.facebook.presto.tests.DistributedQueryRunner;
-import com.facebook.presto.tpch.TpchMetadata;
-import com.facebook.presto.tpch.TpchPlugin;
-import com.facebook.presto.tpch.testing.SampledTpchPlugin;
-import com.google.common.collect.ImmutableMap;
-import io.airlift.log.Logger;
-import org.apache.hadoop.hive.metastore.api.Database;
-import org.testng.annotations.AfterClass;
 
-import java.io.File;
-import java.util.Map;
-
-import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
-import static com.facebook.presto.tests.QueryAssertions.copyTable;
-import static io.airlift.units.Duration.nanosSince;
-import static java.util.Locale.ENGLISH;
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static com.facebook.presto.hive.HiveQueryRunner.createQueryRunner;
+import static com.facebook.presto.hive.HiveQueryRunner.createSampledSession;
+import static io.airlift.tpch.TpchTable.ORDERS;
 
 public class TestHiveIntegrationSmokeTest
         extends AbstractTestIntegrationSmokeTest
 {
-    private static final Logger log = Logger.get("TestQueries");
-    private static final String TPCH_SAMPLED_SCHEMA = "tpch_sampled";
-
     public TestHiveIntegrationSmokeTest()
             throws Exception
     {
-        super(createQueryRunner(), createSession(TPCH_SAMPLED_SCHEMA));
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void destroy()
-    {
-        queryRunner.close();
-    }
-
-    private static QueryRunner createQueryRunner()
-            throws Exception
-    {
-        DistributedQueryRunner queryRunner = new DistributedQueryRunner(createSession("tpch"), 4);
-
-        try {
-            queryRunner.installPlugin(new TpchPlugin());
-            queryRunner.createCatalog("tpch", "tpch");
-
-            queryRunner.installPlugin(new SampledTpchPlugin());
-            queryRunner.createCatalog("tpch_sampled", "tpch_sampled");
-
-            File baseDir = queryRunner.getCoordinator().getBaseDataDir().toFile();
-            InMemoryHiveMetastore metastore = new InMemoryHiveMetastore();
-            metastore.createDatabase(new Database("tpch", null, new File(baseDir, "tpch").toURI().toString(), null));
-            metastore.createDatabase(new Database("tpch_sampled", null, new File(baseDir, "tpch_sampled").toURI().toString(), null));
-
-            queryRunner.installPlugin(new HivePlugin("hive", metastore));
-            Map<String, String> hiveProperties = ImmutableMap.<String, String>builder()
-                    .put("hive.metastore.uri", "thrift://localhost:8080")
-                    .put("hive.allow-drop-table", "true")
-                    .build();
-            queryRunner.createCatalog("hive", "hive", hiveProperties);
-
-            log.info("Loading data...");
-            long startTime = System.nanoTime();
-            copyTable(queryRunner, "tpch", TpchMetadata.TINY_SCHEMA_NAME, "orders", createSession("tpch"));
-            copyTable(queryRunner, "tpch_sampled", TpchMetadata.TINY_SCHEMA_NAME, "orders", createSession(TPCH_SAMPLED_SCHEMA));
-            log.info("Loading complete in %s", nanosSince(startTime).toString(SECONDS));
-
-            return queryRunner;
-        }
-        catch (Exception e) {
-            queryRunner.close();
-            throw e;
-        }
-    }
-
-    private static ConnectorSession createSession(String schema)
-    {
-        return new ConnectorSession("user", "test", "hive", schema, UTC_KEY, ENGLISH, null, null);
+        super(createQueryRunner(ORDERS), createSampledSession());
     }
 }
