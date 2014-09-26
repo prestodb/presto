63159c3e8dc2c5ff027df4932e029b581d082d3c
Henning Schmiedehausen <henning@groupon.com>
2014-09-25 17:06:42 -0700

    Allow Kafka brokers without port definition.
    If the kafka catalog file contains brokers without a port specified,
    use the default Kafka port (9092).

--- presto-kafka/src/main/java/com/facebook/presto/kafka/KafkaConnectorConfig.java
+++ presto-kafka/src/main/java/com/facebook/presto/kafka/KafkaConnectorConfig.java
@@ -33,6 +33,8 @@ import static com.google.common.collect.Iterables.transform;
 
 public class KafkaConnectorConfig
 {
+    private static final int KAFKA_DEFAULT_PORT = 9092;
+
     /**
      * Seed nodes for Kafka cluster. At least one must exist.
      */
@@ -170,7 +172,8 @@ public class KafkaConnectorConfig
             @Override
             public HostAddress apply(String value)
             {
-                return HostAddress.fromString(value);
+                HostAddress address = HostAddress.fromString(value);
+                return address.hasPort() ? address : HostAddress.fromParts(address.getHostText(), KAFKA_DEFAULT_PORT);
             }
         };
     }
