00ba06d661d1dd93e9dac3d5bf4f04cc1ceaa9fa
Christopher Berner <christopherberner@gmail.com>
2014-10-03 15:48:59 -0700

    Fix exponential running time in expression optimizer

--- presto-docs/src/main/sphinx/release/release-0.78.rst
+++ presto-docs/src/main/sphinx/release/release-0.78.rst
@@ -8,3 +8,8 @@ ARRAY and MAP Types in Hive Connector
 The Hive connector now returns arrays and maps instead of json encoded strings,
 for columns whose underlying type is array or map. Please note that this is a backwards
 incompatible change, and the :ref:`json_functions` will no longer work on these columns.
+
+General Changes
+---------------
+
+* Fix expression optimizer, so that it runs in linear time instead of exponential time.
---
+++ presto-main/src/test/java/com/facebook/presto/sql/TestExpressionOptimizer.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql;
+
+import com.facebook.presto.metadata.FunctionRegistry;
+import com.facebook.presto.metadata.OperatorType;
+import com.facebook.presto.metadata.Signature;
+import com.facebook.presto.spi.type.StandardTypes;
+import com.facebook.presto.sql.relational.CallExpression;
+import com.facebook.presto.sql.relational.ConstantExpression;
+import com.facebook.presto.sql.relational.RowExpression;
+import com.facebook.presto.sql.relational.optimizer.ExpressionOptimizer;
+import com.facebook.presto.type.TypeRegistry;
+import com.google.common.collect.ImmutableList;
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.SessionTestUtils.TEST_SESSION;
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
+
+public class TestExpressionOptimizer
+{
+    @Test(timeOut = 10_000)
+    public void testPossibleQuadraticOptimizationTime()
+    {
+        TypeRegistry typeManager = new TypeRegistry();
+        ExpressionOptimizer optimizer = new ExpressionOptimizer(new FunctionRegistry(typeManager, false), typeManager, TEST_SESSION);
+        RowExpression expression = new ConstantExpression(1L, BIGINT);
+        for (int i = 0; i < 100; i++) {
+            Signature signature = Signature.internalOperator(OperatorType.ADD.name(), StandardTypes.BIGINT, StandardTypes.BIGINT, StandardTypes.BIGINT);
+            expression = new CallExpression(signature, BIGINT, ImmutableList.of(expression, new ConstantExpression(1L, BIGINT)));
+        }
+        optimizer.optimize(expression);
+    }
+}
