3c2ac38a78571a3b833e74a3a1219929e2159eb0
David Phillips <david@acz.org>
2014-10-06 20:39:41 -0700

    Remove missing Hive drop test table

--- presto-hive/src/test/sql/drop-test.sql
+++ presto-hive/src/test/sql/drop-test.sql
@@ -19,4 +19,6 @@ DROP TABLE IF EXISTS presto_test_bucketed_by_string_int;
 DROP TABLE IF EXISTS presto_test_bucketed_by_bigint_boolean;
 DROP TABLE IF EXISTS presto_test_bucketed_by_double_float;
 
+DROP TABLE IF EXISTS presto_test_partition_schema_change;
+
 DROP VIEW IF EXISTS presto_test_view;
