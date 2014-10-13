81602efb17e41db427b221d3b088570784249716
David Phillips <david@acz.org>
2014-10-13 14:42:17 -0700

    Move PagesSerde to block package

--- presto-main/src/main/java/com/facebook/presto/server/PagesResponseWriter.java
+++ presto-main/src/main/java/com/facebook/presto/server/PagesResponseWriter.java
@@ -14,7 +14,7 @@
 package com.facebook.presto.server;
 
 import com.facebook.presto.spi.Page;
-import com.facebook.presto.serde.PagesSerde;
+import com.facebook.presto.block.PagesSerde;
 import com.facebook.presto.spi.block.BlockEncodingSerde;
 import com.google.common.base.Throwables;
 import com.google.common.reflect.TypeToken;
