f74e835a46ab19f38950f0d9736ac67f9c2eb3e9
Nezih Yigitbasi <nyigitbasi@netflix.com>
2014-10-17 15:29:38 -0700

    Added map_keys and map_values functions

---
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/MapValues.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator.scalar;
+
+import com.facebook.presto.metadata.FunctionInfo;
+import com.facebook.presto.metadata.ParametricScalar;
+import com.facebook.presto.metadata.Signature;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
+import com.facebook.presto.type.ArrayType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import io.airlift.slice.Slice;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandle;
+import java.util.List;
+import java.util.Map;
+
+import static com.facebook.presto.metadata.Signature.typeParameter;
+import static com.facebook.presto.type.TypeUtils.parameterizedTypeName;
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.lang.invoke.MethodHandles.lookup;
+
+public class MapValues extends ParametricScalar
+{
+    public static final MapValues MAP_VALUES = new MapValues();
+    private static final Signature SIGNATURE = new Signature("map_values", ImmutableList.of(typeParameter("K"), typeParameter("V")), "array<V>", ImmutableList.of("map<K,V>"), false, false);
+    private static final MethodHandle METHOD_HANDLE;
+    public static final String DESCRIPTION = "Returns the values of the given map<K,V> as an array";
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    static {
+        try {
+            METHOD_HANDLE = lookup().unreflect(MapValues.class.getMethod("getValues", Slice.class));
+        }
+        catch (ReflectiveOperationException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    @Override
+    public Signature getSignature()
+    {
+        return SIGNATURE;
+    }
+
+    @Override
+    public boolean isHidden()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isDeterministic()
+    {
+        return true;
+    }
+    @Override
+    public String getDescription()
+    {
+        return DESCRIPTION;
+    }
+
+    @Override
+    public FunctionInfo specialize(Map<String, Type> types, int arity, TypeManager typeManager)
+    {
+        checkArgument(arity == 1, "map_values expects only one argument");
+        Type keyType = types.get("K");
+        Type valueType = types.get("V");
+        return new FunctionInfo(new Signature("map_values", parameterizedTypeName("array", valueType.getTypeSignature()), parameterizedTypeName("map", keyType.getTypeSignature(), valueType.getTypeSignature())), DESCRIPTION, false, METHOD_HANDLE, true, true, ImmutableList.of(false));
+    }
+
+    public static Slice getValues(Slice map)
+    {
+        List<Object> values = Lists.newArrayList(); //allow nulls
+        final com.fasterxml.jackson.databind.type.MapType type = mapper.getTypeFactory().constructMapType(
+                Map.class, String.class, Object.class);
+        final Map<String, Object> jsonMap;
+        try {
+            jsonMap = mapper.readValue(map.getInput(), type);
+        }
+        catch (IOException e) {
+            return null;
+        }
+        values.addAll(jsonMap.values());
+        return ArrayType.toStackRepresentation(values);
+    }
+}
