089010916c10ce2f525aec83a689fa30fb9d5d60
bjlvxin <bjlvxin@BJXX-BJLVXIN-T.360buyAD.local>
2014-11-12 16:46:59 +0800

    untest

--- presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/BaseJdbcClient.java
+++ presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/BaseJdbcClient.java
@@ -97,7 +97,6 @@ public class BaseJdbcClient
     {
         this.connectorId = checkNotNull(connectorId, "connectorId is null").toString();
         this.identifierQuote = checkNotNull(identifierQuote, "identifierQuote is null");
-        //test
         this.driver = checkNotNull(driver, "driver is null");
 
         checkNotNull(config, "config is null");
