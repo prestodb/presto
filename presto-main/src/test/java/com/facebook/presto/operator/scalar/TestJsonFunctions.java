18acb6d5d67c1832a3b357afdb5f08be1c464be4
Nezih Yigitbasi <nyigitbasi@netflix.com>
2014-11-18 13:49:27 -0800

    Implement json equality operators

--- presto-main/src/test/java/com/facebook/presto/operator/scalar/TestJsonFunctions.java
+++ presto-main/src/test/java/com/facebook/presto/operator/scalar/TestJsonFunctions.java
@@ -13,11 +13,15 @@
  */
 package com.facebook.presto.operator.scalar;
 
+import com.facebook.presto.metadata.ParametricFunction;
+import com.facebook.presto.spi.PrestoException;
+import com.google.common.collect.ImmutableList;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.airlift.slice.Slices.utf8Slice;
 import static java.lang.String.format;
+import static org.testng.Assert.fail;
 
 public class TestJsonFunctions
 {
@@ -27,6 +31,7 @@ public class TestJsonFunctions
     public void setUp()
     {
         functionAssertions = new FunctionAssertions();
+        functionAssertions.addFunctions(ImmutableList.<ParametricFunction>of(new MapConstructor(2, functionAssertions.getMetadata().getTypeManager())));
     }
 
     @Test
@@ -211,8 +216,19 @@ public class TestJsonFunctions
         for (String value : new String[] {"'x'", "2.5", "8", "true", "cast(null as varchar)"}) {
             for (String array : new String[] {"", "123", "[", "[1,0,]", "[1,,0]"}) {
                 assertFunction(format("JSON_ARRAY_CONTAINS('%s', %s)", array, value), null);
-                assertFunction(format("JSON_ARRAY_CONTAINS(CAST('%s' AS JSON), %s)", array, value), null);
             }
+        }
+    }
+
+    @Test
+    public void testInvalidJsonCast()
+    {
+        try {
+            assertFunction("CAST('INVALID' AS JSON)", null);
+            fail();
+        }
+        catch (PrestoException e) {
+            // Expected
         }
     }
 
@@ -233,8 +249,30 @@ public class TestJsonFunctions
         assertFunction(format("JSON_SIZE(CAST('%s' AS JSON), '%s')", "{\"x\": {\"a\" : 1, \"b\" : 2} }", "$.x.a"), 0);
         assertFunction(format("JSON_SIZE(CAST('%s' AS JSON), '%s')", "[1,2,3]", "$"), 3);
         assertFunction(format("JSON_SIZE(null, '%s')", "$"), null);
-        assertFunction(format("JSON_SIZE(CAST('%s' AS JSON), '%s')", "INVALID_JSON", "$"), null);
         assertFunction(format("JSON_SIZE(CAST('%s' AS JSON), null)", "[1,2,3]"), null);
+    }
+
+    @Test
+    public void testJsonEquality()
+    {
+        assertFunction("CAST('[1,2,3]' AS JSON) = CAST('[1,2,3]' AS JSON)", true);
+        assertFunction("CAST('{\"a\":1, \"b\":2}' AS JSON) = CAST('{\"b\":2, \"a\":1}' AS JSON)", true);
+        assertFunction("CAST('{\"a\":1, \"b\":2}' AS JSON) = CAST(MAP('b', 2, 'a', 1) AS JSON)", true);
+        assertFunction("CAST('null' AS JSON) = CAST('null' AS JSON)", true);
+        assertFunction("CAST('true' AS JSON) = CAST('true' AS JSON)", true);
+        assertFunction("CAST('{\"x\":\"y\"}' AS JSON) = CAST('{\"x\":\"y\"}' AS JSON)", true);
+        assertFunction("CAST('[1,2,3]' AS JSON) = CAST('[2,3,1]' AS JSON)", false);
+        assertFunction("CAST('{\"p_1\": 1, \"p_2\":\"v_2\", \"p_3\":null, \"p_4\":true, \"p_5\": {\"p_1\":1}}' AS JSON) = " +
+                "CAST('{\"p_2\":\"v_2\", \"p_4\":true, \"p_1\": 1, \"p_3\":null, \"p_5\": {\"p_1\":1}}' AS JSON)", true);
+
+        assertFunction("CAST('[1,2,3]' AS JSON) != CAST('[1,2,3]' AS JSON)", false);
+        assertFunction("CAST('{\"a\":1, \"b\":2}' AS JSON) != CAST('{\"b\":2, \"a\":1}' AS JSON)", false);
+        assertFunction("CAST('null' AS JSON) != CAST('null' AS JSON)", false);
+        assertFunction("CAST('true' AS JSON) != CAST('true' AS JSON)", false);
+        assertFunction("CAST('{\"x\":\"y\"}' AS JSON) != CAST('{\"x\":\"y\"}' AS JSON)", false);
+        assertFunction("CAST('[1,2,3]' AS JSON) != CAST('[2,3,1]' AS JSON)", true);
+        assertFunction("CAST('{\"p_1\": 1, \"p_2\":\"v_2\", \"p_3\":null, \"p_4\":true, \"p_5\": {\"p_1\":1}}' AS JSON) != " +
+                "CAST('{\"p_2\":\"v_2\", \"p_4\":true, \"p_1\": 1, \"p_3\":null, \"p_5\": {\"p_1\":1}}' AS JSON)", false);
     }
 
     private void assertFunction(String projection, Object expected)
