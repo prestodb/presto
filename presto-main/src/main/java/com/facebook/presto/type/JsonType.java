27f019c0a9358f694f2e9f7e38cce7376f767e0f
Nezih Yigitbasi <nyigitbasi@netflix.com>
2014-11-18 11:07:58 -0800

    Implement hash/equalTo for json type

--- presto-main/src/main/java/com/facebook/presto/type/JsonType.java
+++ presto-main/src/main/java/com/facebook/presto/type/JsonType.java
@@ -45,6 +45,20 @@ public class JsonType
     }
 
     @Override
+    public boolean equalTo(Block leftBlock, int leftPosition, Block rightBlock, int rightPosition)
+    {
+        Slice leftValue = leftBlock.getSlice(leftPosition, 0, leftBlock.getLength(leftPosition));
+        Slice rightValue = rightBlock.getSlice(rightPosition, 0, rightBlock.getLength(rightPosition));
+        return leftValue.equals(rightValue);
+    }
+
+    @Override
+    public int hash(Block block, int position)
+    {
+        return block.getSlice(position, 0, block.getLength(position)).hashCode();
+    }
+
+    @Override
     public int compareTo(Block leftBlock, int leftPosition, Block rightBlock, int rightPosition)
     {
         int leftLength = leftBlock.getLength(leftPosition);
