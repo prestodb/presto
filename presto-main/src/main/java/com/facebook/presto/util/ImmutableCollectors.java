44c1de70de3054dee1be6232b2718d6fea8856c7
Martin Traverso <martint@fb.com>
2014-12-03 23:41:39 -0800

    Minor improvements to ImmutableCollectors
    - Rename methods to toImmutableList and toImmutableSet
    - Simplify method signatures
    - Remove unnecessary type specification for lambda args
    - Declare set collector as UNORDERED to give stream implementation
      more room for optimization

--- presto-main/src/main/java/com/facebook/presto/util/ImmutableCollectors.java
+++ presto-main/src/main/java/com/facebook/presto/util/ImmutableCollectors.java
@@ -22,27 +22,28 @@ public final class ImmutableCollectors
 {
     private ImmutableCollectors() {}
 
-    public static <T> Collector<T, ImmutableList.Builder<T>, ImmutableList<T>> immutableListCollector()
+    public static <T> Collector<T, ?, ImmutableList<T>> toImmutableList()
     {
         return Collector.<T, ImmutableList.Builder<T>, ImmutableList<T>>of(
                 ImmutableList.Builder::new,
                 ImmutableList.Builder::add,
-                (ImmutableList.Builder<T> left, ImmutableList.Builder<T> right) -> {
+                (left, right) -> {
                     left.addAll(right.build());
                     return left;
                 },
                 ImmutableList.Builder::build);
     }
 
-    public static <T> Collector<T, ImmutableSet.Builder<T>, ImmutableSet<T>> immutableSetCollector()
+    public static <T> Collector<T, ?, ImmutableSet<T>> toImmutableSet()
     {
         return Collector.<T, ImmutableSet.Builder<T>, ImmutableSet<T>>of(
                 ImmutableSet.Builder::new,
                 ImmutableSet.Builder::add,
-                (ImmutableSet.Builder<T> left, ImmutableSet.Builder<T> right) -> {
+                (left, right) -> {
                     left.addAll(right.build());
                     return left;
                 },
-                ImmutableSet.Builder::build);
+                ImmutableSet.Builder::build,
+                Collector.Characteristics.UNORDERED);
     }
 }
