d624982ebf0c1931a6241666d84e550ef3e1afcb
Nileema Shingte <nileema.shingte@gmail.com>
2014-12-07 10:33:05 -0800

    Add hash partition count as a session property

--- presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
+++ presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
@@ -18,6 +18,7 @@ public final class SystemSessionProperties
     public static final String BIG_QUERY = "experimental_big_query";
     private static final String OPTIMIZE_HASH_GENERATION = "optimize_hash_generation";
     private static final String DISTRIBUTED_JOIN = "distributed_join";
+    private static final String HASH_PARTITION_COUNT = "hash_partition_count";
 
     private SystemSessionProperties() {}
 
@@ -36,6 +37,20 @@ public final class SystemSessionProperties
         return Boolean.valueOf(enabled);
     }
 
+    private static int getNumber(String propertyName, Session session, int defaultValue)
+    {
+        String count = session.getSystemProperties().get(propertyName);
+        if (count != null) {
+            try {
+                return Integer.valueOf(count);
+            }
+            catch (NumberFormatException ignored) {
+            }
+        }
+
+        return defaultValue;
+    }
+
     public static boolean isOptimizeHashGenerationEnabled(Session session, boolean defaultValue)
     {
         return isEnabled(OPTIMIZE_HASH_GENERATION, session, defaultValue);
@@ -44,5 +59,10 @@ public final class SystemSessionProperties
     public static boolean isDistributedJoinEnabled(Session session, boolean defaultValue)
     {
         return isEnabled(DISTRIBUTED_JOIN, session, defaultValue);
+    }
+
+    public static int getHashPartitionCount(Session session, int defaultValue)
+    {
+        return getNumber(HASH_PARTITION_COUNT, session, defaultValue);
     }
 }
