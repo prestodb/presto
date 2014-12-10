0aa64c25efa22707a7dcfe0162570afec5ad94a8
Martin Traverso <martint@fb.com>
2014-12-09 23:07:32 -0800

    Remove transformer functions from PluginClassloader

--- presto-main/src/main/java/com/facebook/presto/server/PluginClassLoader.java
+++ presto-main/src/main/java/com/facebook/presto/server/PluginClassLoader.java
@@ -13,7 +13,6 @@
  */
 package com.facebook.presto.server;
 
-import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
@@ -45,8 +44,8 @@ class PluginClassLoader
                 parent,
                 hiddenClasses,
                 parentFirstClasses,
-                Iterables.transform(hiddenClasses, classNameToResource()),
-                Iterables.transform(parentFirstClasses, classNameToResource()));
+                Iterables.transform(hiddenClasses, PluginClassLoader::classNameToResource),
+                Iterables.transform(parentFirstClasses, PluginClassLoader::classNameToResource));
     }
 
     public PluginClassLoader(List<URL> urls,
@@ -215,8 +214,8 @@ class PluginClassLoader
         return false;
     }
 
-    private static Function<String, String> classNameToResource()
+    private static String classNameToResource(String className)
     {
-        return className -> className.replace('.', '/');
+        return className.replace('.', '/');
     }
 }
