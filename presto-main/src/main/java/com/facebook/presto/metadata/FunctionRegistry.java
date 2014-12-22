4c5ca1049e5577a484e2313455917dc6d5816552
Christopher Berner <christopherberner@gmail.com>
2014-12-22 15:40:34 -0800

    Fix IntelliJ warnings in FunctionRegistry

--- presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
@@ -331,7 +331,7 @@ public class FunctionRegistry
         return Iterables.any(functions.get(name), ParametricFunction::isAggregate);
     }
 
-    public FunctionInfo resolveFunction(QualifiedName name, List<TypeSignature> parameterTypes, final boolean approximate)
+    public FunctionInfo resolveFunction(QualifiedName name, List<TypeSignature> parameterTypes, boolean approximate)
     {
         List<ParametricFunction> candidates = functions.get(name).stream()
                 .filter(function -> function.isScalar() || function.isApproximate() == approximate)
@@ -444,7 +444,7 @@ public class FunctionRegistry
     @VisibleForTesting
     public List<ParametricFunction> listOperators()
     {
-        final Set<String> operatorNames = Arrays.asList(OperatorType.values()).stream()
+        Set<String> operatorNames = Arrays.asList(OperatorType.values()).stream()
                 .map(FunctionRegistry::mangleOperatorName)
                 .collect(toImmutableSet());
 
