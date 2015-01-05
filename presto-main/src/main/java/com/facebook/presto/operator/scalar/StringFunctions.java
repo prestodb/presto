5e8c248e4fcc898ceb493ccc9f3ad1ee59eb58e7
David Phillips <david@acz.org>
2015-01-05 11:34:09 -0800

    Categorize errors for string functions

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/StringFunctions.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/StringFunctions.java
@@ -14,9 +14,11 @@
 package com.facebook.presto.operator.scalar;
 
 import com.facebook.presto.operator.Description;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.type.SqlType;
 import com.google.common.base.Ascii;
+import com.google.common.primitives.Ints;
 import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
 
@@ -25,21 +27,32 @@ import javax.annotation.Nullable;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 
-import static com.google.common.base.Preconditions.checkArgument;
+import static com.facebook.presto.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMENT;
+import static com.facebook.presto.util.Failures.checkCondition;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class StringFunctions
 {
     private StringFunctions() {}
 
-    @Description("convert unicode code point to a string")
+    @Description("convert Unicode code point to a string")
     @ScalarFunction
     @SqlType(StandardTypes.VARCHAR)
     public static Slice chr(@SqlType(StandardTypes.BIGINT) long codepoint)
     {
-        char[] utf16 = Character.toChars((int) codepoint);
+        char[] utf16 = codePointChars(codepoint);
         ByteBuffer utf8 = UTF_8.encode(CharBuffer.wrap(utf16));
         return Slices.wrappedBuffer(utf8.array(), 0, utf8.limit());
+    }
+
+    private static char[] codePointChars(long codepoint)
+    {
+        try {
+            return Character.toChars(Ints.checkedCast(codepoint));
+        }
+        catch (IllegalArgumentException e) {
+            throw new PrestoException(INVALID_FUNCTION_ARGUMENT, "Not a valid Unicode code point: " + codepoint);
+        }
     }
 
     @Description("concatenates given strings")
@@ -157,7 +170,7 @@ public final class StringFunctions
     @SqlType(StandardTypes.VARCHAR)
     public static Slice splitPart(@SqlType(StandardTypes.VARCHAR) Slice string, @SqlType(StandardTypes.VARCHAR) Slice delimiter, @SqlType(StandardTypes.BIGINT) long index)
     {
-        checkArgument(index > 0, "Index must be greater than zero");
+        checkCondition(index > 0, INVALID_FUNCTION_ARGUMENT, "Index must be greater than zero");
 
         if (delimiter.length() == 0) {
             if (index > string.length()) {
