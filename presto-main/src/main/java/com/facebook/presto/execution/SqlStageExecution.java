486d1df6b22e089bfe2f62b7f487ad09fb4d5fc3
Dain Sundstrom <dain@iq80.com>
2015-01-27 13:45:53 -0800

    Fix error message in SqlStageExecution

--- presto-main/src/main/java/com/facebook/presto/execution/SqlStageExecution.java
+++ presto-main/src/main/java/com/facebook/presto/execution/SqlStageExecution.java
@@ -894,7 +894,7 @@ public class SqlStageExecution
                     else if (any(taskStates, equalTo(TaskState.ABORTED))) {
                         // A task should only be in the aborted state if the STAGE is done (ABORTED or FAILED)
                         stageState.set(StageState.FAILED);
-                        failureCauses.add(new PrestoException(StandardErrorCode.INTERNAL_ERROR, "A task is in the ABORTED state but stage is " + stageState));
+                        failureCauses.add(new PrestoException(StandardErrorCode.INTERNAL_ERROR, "A task is in the ABORTED state but stage is " + currentState));
                     }
                     else if (currentState != StageState.PLANNED && currentState != StageState.SCHEDULING) {
                         // all tasks are now scheduled, so we can check the finished state
