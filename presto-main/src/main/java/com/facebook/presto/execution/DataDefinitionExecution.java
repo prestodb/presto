fad65a7ba6656142846c245155dac1821cc6291e
David Phillips <david@acz.org>
2015-02-04 07:57:41 -0800

    Use message arguments for checkArgument call

--- presto-main/src/main/java/com/facebook/presto/execution/DataDefinitionExecution.java
+++ presto-main/src/main/java/com/facebook/presto/execution/DataDefinitionExecution.java
@@ -149,7 +149,7 @@ public class DataDefinitionExecution<T extends Statement>
                 QueryStateMachine stateMachine)
         {
             DataDefinitionTask<T> task = getTask(statement);
-            checkArgument(task != null, "no task for statement: " + statement.getClass());
+            checkArgument(task != null, "no task for statement: %s", statement.getClass().getSimpleName());
             return new DataDefinitionExecution<>(task, statement, session, metadata, stateMachine);
         }
 
