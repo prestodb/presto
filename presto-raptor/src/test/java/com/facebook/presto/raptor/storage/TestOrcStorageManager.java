9edeba66017b63e07e27d3a067fd9f16dbe9c126
David Phillips <david@acz.org>
2015-02-05 12:55:30 -0800

    Add Raptor tests for exceptional double values

--- presto-raptor/src/test/java/com/facebook/presto/raptor/storage/TestOrcStorageManager.java
+++ presto-raptor/src/test/java/com/facebook/presto/raptor/storage/TestOrcStorageManager.java
@@ -209,6 +209,11 @@ public class TestOrcStorageManager
                 .row(123, "hello", wrappedBuffer(bytes1), dateValue(new DateTime(2001, 8, 22, 0, 0, 0, 0, UTC)), true, 123.45)
                 .row(null, null, null, null, null, null)
                 .row(456, "bye", wrappedBuffer(bytes3), dateValue(new DateTime(2005, 4, 22, 0, 0, 0, 0, UTC)), false, 987.65)
+                .row(881, "-inf", null, null, null, Double.NEGATIVE_INFINITY)
+                .row(882, "+inf", null, null, null, Double.POSITIVE_INFINITY)
+                .row(883, "nan", null, null, null, Double.NaN)
+                .row(884, "min", null, null, null, Double.MIN_VALUE)
+                .row(885, "max", null, null, null, Double.MAX_VALUE)
                 .build();
 
         sink.appendPages(pages);
@@ -221,6 +226,11 @@ public class TestOrcStorageManager
                 .row(123, "hello", sqlBinary(bytes1), sqlDate(2001, 8, 22), true, 123.45)
                 .row(null, null, null, null, null, null)
                 .row(456, "bye", sqlBinary(bytes3), sqlDate(2005, 4, 22), false, 987.65)
+                .row(881, "-inf", null, null, null, Double.NEGATIVE_INFINITY)
+                .row(882, "+inf", null, null, null, Double.POSITIVE_INFINITY)
+                .row(883, "nan", null, null, null, Double.NaN)
+                .row(884, "min", null, null, null, Double.MIN_VALUE)
+                .row(885, "max", null, null, null, Double.MAX_VALUE)
                 .build();
 
         // no tuple domain (all)
