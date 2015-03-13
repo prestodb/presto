36033d13359cdd9333ddb89f0beeac5b62d1c819
monicalee <monni.lee@gmail.com>
2015-03-13 14:39:12 -0700

    ParametricScalar contains() function

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayFunctions.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayFunctions.java
@@ -13,26 +13,35 @@
  */
 package com.facebook.presto.operator.scalar;
 
-import com.facebook.presto.spi.block.Block;
+//import com.facebook.presto.spi.type.StandardTypes;
+import com.facebook.presto.metadata.FunctionInfo;
+import com.facebook.presto.metadata.FunctionRegistry;
+import com.facebook.presto.metadata.ParametricScalar;
+import com.facebook.presto.metadata.Signature;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
-import com.facebook.presto.spi.block.VariableWidthBlockBuilder;
-import com.facebook.presto.spi.type.BigintType;
-import com.facebook.presto.spi.type.BooleanType;
-import com.facebook.presto.spi.type.DoubleType;
 import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.spi.type.VarcharType;
+import com.facebook.presto.spi.type.TypeManager;
+import com.facebook.presto.spi.type.TypeSignature;
 import com.facebook.presto.type.SqlType;
+import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
 
 import javax.annotation.Nullable;
+import java.lang.invoke.MethodHandle;
+import java.util.Map;
 
-import static com.facebook.presto.type.TypeUtils.readStructuralBlock;
-import static com.facebook.presto.type.TypeUtils.buildStructuralSlice;
-import static com.facebook.presto.type.TypeUtils.createBlock;
+import static com.facebook.presto.metadata.Signature.typeParameter;
+import static com.facebook.presto.spi.type.TypeSignature.parseTypeSignature;
+import static com.facebook.presto.type.TypeUtils.parameterizedTypeName;
+import static com.facebook.presto.util.Reflection.methodHandle;
+
+//import javax.annotation.Nullable;
 
 public final class ArrayFunctions
+        extends ParametricScalar
 {
     private ArrayFunctions()
     {
@@ -42,16 +51,63 @@ public final class ArrayFunctions
     @SqlType("array<unknown>")
     public static Slice arrayConstructor()
     {
-        BlockBuilder blockBuilder = new VariableWidthBlockBuilder(new BlockBuilderStatus(), 0);
-        return buildStructuralSlice(blockBuilder);
+        return Slices.utf8Slice("[]");
     }
 
+    private static final String FUNCTION_NAME = "contains";
+    private static final Signature SIGNATURE = new Signature(FUNCTION_NAME, ImmutableList.of(typeParameter("T")), "array<T>", ImmutableList.of("array<T>", "T"), false, false);
+
+    @Override
+    public Signature getSignature()
+    {
+        return SIGNATURE;
+    }
+
+    @Override
+    public boolean isHidden()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isDeterministic()
+    {
+        return true;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "Determines whether given value exists in given array; returns boolean";
+    }
+
+    @Override
+    public FunctionInfo specialize(Map<String, Type> types, int arity, TypeManager typeManager, FunctionRegistry functionRegistry)
+    {
+        Type type = types.get("T");
+        TypeSignature valueType = type.getTypeSignature();
+        TypeSignature arrayType = parameterizedTypeName("array", valueType);
+        TypeSignature returnType = parseTypeSignature(StandardTypes.BOOLEAN);
+        MethodHandle methodHandle = methodHandle(JsonFunctions.class, "jsonArrayContains", Slice.class, type.getJavaType());
+        Signature signature = new Signature(FUNCTION_NAME, returnType, arrayType, valueType);
+
+        return new FunctionInfo(signature, getDescription(), isHidden(), methodHandle, isDeterministic(), false, ImmutableList.of(false, false));
+    }
+
+    @Nullable
+    public static Boolean equals(Type type, Slice slice, Slice value)
+    {
+        BlockBuilder blockBuilder = type.createBlockBuilder(new BlockBuilderStatus());
+        blockBuilder.writeBytes(slice, 0, slice.length());
+        return JsonFunctions.jsonArrayContains(blockBuilder.closeEntry().build(), 0, value);
+    }
+/*
     @Nullable
     @ScalarFunction
     @SqlType(StandardTypes.BOOLEAN)
     public static Boolean contains(@SqlType("array<bigint>") Slice slice, @SqlType(StandardTypes.BIGINT) long value)
     {
-        return arrayContains(slice, BigintType.BIGINT, value);
+        return JsonFunctions.jsonArrayContains(slice, value);
     }
 
     @Nullable
@@ -59,7 +115,7 @@ public final class ArrayFunctions
     @SqlType(StandardTypes.BOOLEAN)
     public static Boolean contains(@SqlType("array<boolean>") Slice slice, @SqlType(StandardTypes.BOOLEAN) boolean value)
     {
-        return arrayContains(slice, BooleanType.BOOLEAN, value);
+        return JsonFunctions.jsonArrayContains(slice, value);
     }
 
     @Nullable
@@ -67,7 +123,7 @@ public final class ArrayFunctions
     @SqlType(StandardTypes.BOOLEAN)
     public static Boolean contains(@SqlType("array<double>") Slice slice, @SqlType(StandardTypes.DOUBLE) double value)
     {
-        return arrayContains(slice, DoubleType.DOUBLE, value);
+        return JsonFunctions.jsonArrayContains(slice, value);
     }
 
     @Nullable
@@ -75,21 +131,6 @@ public final class ArrayFunctions
     @SqlType(StandardTypes.BOOLEAN)
     public static Boolean contains(@SqlType("array<varchar>") Slice slice, @SqlType(StandardTypes.VARCHAR) Slice value)
     {
-        return arrayContains(slice, VarcharType.VARCHAR, value);
-    }
-
-    private static Boolean arrayContains(Slice slice, Type type, Object value)
-    {
-        Block block = readStructuralBlock(slice);
-        Block valueBlock = createBlock(type, value);
-
-        //TODO: This could be quite slow, it should use parametric equals
-        for (int i = 0; i < block.getPositionCount(); i++) {
-            if (type.equalTo(block, i, valueBlock, 0)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
+        return JsonFunctions.jsonArrayContains(slice, value);
+    }*/
 }
