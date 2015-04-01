0590e1298ff0a1877c0f879e26ec84cc659870d9
Zhenxiao Luo <zluo@netflix.com>
2015-03-31 17:18:55 -0700

    Make array comparison functions use MethodHandle

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayEqualOperator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayEqualOperator.java
@@ -15,10 +15,13 @@ package com.facebook.presto.operator.scalar;
 import com.facebook.presto.metadata.FunctionInfo;
 import com.facebook.presto.metadata.FunctionRegistry;
 import com.facebook.presto.metadata.ParametricOperator;
+import com.facebook.presto.spi.PrestoException;
+import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.spi.type.TypeManager;
 import com.facebook.presto.spi.type.TypeSignature;
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
 
@@ -28,8 +31,12 @@ import java.util.Map;
 import static com.facebook.presto.metadata.FunctionRegistry.operatorInfo;
 import static com.facebook.presto.metadata.OperatorType.EQUAL;
 import static com.facebook.presto.metadata.Signature.comparableTypeParameter;
+import static com.facebook.presto.type.ArrayType.ARRAY_NULL_ELEMENT_MSG;
+import static com.facebook.presto.type.TypeUtils.castValue;
+import static com.facebook.presto.type.TypeUtils.checkElementNotNull;
+import static com.facebook.presto.type.TypeUtils.readStructuralBlock;
+import static com.facebook.presto.spi.StandardErrorCode.INTERNAL_ERROR;
 import static com.facebook.presto.spi.type.TypeSignature.parseTypeSignature;
-import static com.facebook.presto.type.TypeUtils.createBlock;
 import static com.facebook.presto.util.Reflection.methodHandle;
 
 public class ArrayEqualOperator
@@ -37,7 +44,7 @@ public class ArrayEqualOperator
 {
     public static final ArrayEqualOperator ARRAY_EQUAL = new ArrayEqualOperator();
     private static final TypeSignature RETURN_TYPE = parseTypeSignature(StandardTypes.BOOLEAN);
-    public static final MethodHandle METHOD_HANDLE = methodHandle(ArrayEqualOperator.class, "equals", Type.class, Slice.class, Slice.class);
+    private static final MethodHandle METHOD_HANDLE = methodHandle(ArrayEqualOperator.class, "equals", MethodHandle.class, Type.class, Slice.class, Slice.class);
 
     private ArrayEqualOperator()
     {
@@ -47,15 +54,38 @@ public class ArrayEqualOperator
     @Override
     public FunctionInfo specialize(Map<String, Type> types, int arity, TypeManager typeManager, FunctionRegistry functionRegistry)
     {
-        Type type = types.get("T");
-        type = typeManager.getParameterizedType(StandardTypes.ARRAY, ImmutableList.of(type.getTypeSignature()), ImmutableList.of());
+        Type elementType = types.get("T");
+        Type type = typeManager.getParameterizedType(StandardTypes.ARRAY, ImmutableList.of(elementType.getTypeSignature()), ImmutableList.of());
         TypeSignature typeSignature = type.getTypeSignature();
-        return operatorInfo(EQUAL, RETURN_TYPE, ImmutableList.of(typeSignature, typeSignature), METHOD_HANDLE.bindTo(type), false, ImmutableList.of(false, false));
+        MethodHandle equalsFunction = functionRegistry.resolveOperator(EQUAL, ImmutableList.of(elementType, elementType)).getMethodHandle();
+        MethodHandle method = METHOD_HANDLE.bindTo(equalsFunction).bindTo(elementType);
+        return operatorInfo(EQUAL, RETURN_TYPE, ImmutableList.of(typeSignature, typeSignature), method, false, ImmutableList.of(false, false));
     }
 
-    public static boolean equals(Type type, Slice left, Slice right)
+    public static boolean equals(MethodHandle equalsFunction, Type type, Slice left, Slice right)
     {
-        //TODO: This could be quite slow, it should use parametric equals
-        return type.equalTo(createBlock(type, left), 0, createBlock(type, right), 0);
+        Block leftArray = readStructuralBlock(left);
+        Block rightArray = readStructuralBlock(right);
+        if (leftArray.getPositionCount() != rightArray.getPositionCount()) {
+            return false;
+        }
+        for (int i = 0; i < leftArray.getPositionCount(); i++) {
+            checkElementNotNull(leftArray.isNull(i), ARRAY_NULL_ELEMENT_MSG);
+            checkElementNotNull(rightArray.isNull(i), ARRAY_NULL_ELEMENT_MSG);
+            Object leftElement = castValue(type, leftArray, i);
+            Object rightElement = castValue(type, rightArray, i);
+            try {
+                if (!(boolean) equalsFunction.invoke(leftElement, rightElement)) {
+                    return false;
+                }
+            }
+            catch (Throwable t) {
+                Throwables.propagateIfInstanceOf(t, Error.class);
+                Throwables.propagateIfInstanceOf(t, PrestoException.class);
+
+                throw new PrestoException(INTERNAL_ERROR, t);
+            }
+        }
+        return true;
     }
 }
