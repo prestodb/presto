1559bcf93af51fd4c015d2da1782dda1bb084b75
Eric Hwang <ehwang@fb.com>
2015-04-17 12:58:51 -0700

    Adjust plan optimizer order

--- presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizersFactory.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizersFactory.java
@@ -93,8 +93,9 @@ public class PlanOptimizersFactory
         builder.add(new PickLayout(metadata));
 
         builder.add(new PredicatePushDown(metadata, sqlParser)); // Run predicate push down one more time in case we can leverage new information from layouts' effective predicate
-        builder.add(new PruneUnreferencedOutputs());
         builder.add(new UnaliasSymbolReferences());
+        builder.add(new MergeProjections());
+        builder.add(new PruneUnreferencedOutputs());
         builder.add(new PruneRedundantProjections());
 
         // TODO: consider adding a formal final plan sanitization optimizer that prepares the plan for transmission/execution/logging
