920379dabfbee723710caf498172273c46e405ab
David Phillips <david@acz.org>
2015-05-02 18:27:03 -0700

    Fix variable name in ArrayDistinctFunction

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayDistinctFunction.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayDistinctFunction.java
@@ -86,12 +86,12 @@ public final class ArrayDistinctFunction
         return new FunctionInfo(signature, getDescription(), isHidden(), methodHandle, isDeterministic(), false, ImmutableList.of(false));
     }
 
-    public static Slice distinct(Type type, Slice jsonArray)
+    public static Slice distinct(Type type, Slice array)
     {
-        Block elementsBlock = readStructuralBlock(jsonArray);
+        Block elementsBlock = readStructuralBlock(array);
 
         if (elementsBlock.getPositionCount() == 0) {
-            return jsonArray;
+            return array;
         }
 
         GroupByHash groupByHash = createGroupByHash(ImmutableList.of(type), new int[] {0}, Optional.empty(), elementsBlock.getPositionCount());
