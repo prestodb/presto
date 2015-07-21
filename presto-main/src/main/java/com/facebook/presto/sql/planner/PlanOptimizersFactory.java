d93af1a330f07c5d583e4565744ab1343183a7aa
Nileema Shingte <nileema.shingte@gmail.com>
2015-07-21 15:15:22 -0700

    Fix push down for row_number window function
    Filters and limits should be pushed down to
    RowNumberNode (and TopNRowNumberNode) only when these nodes are
    directly above the WindowNode

--- presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizersFactory.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizersFactory.java
@@ -74,7 +74,7 @@ public class PlanOptimizersFactory
                 new PruneUnreferencedOutputs(), // Make sure to run this before index join. Filtered projections may not have all the columns.
                 new IndexJoinOptimizer(metadata, indexManager), // Run this after projections and filters have been fully simplified and pushed down
                 new CountConstantOptimizer(),
-                new WindowFilterPushDown(metadata), // This must run after PredicatePushDown so that it squashes any successive filter nodes
+                new WindowFilterPushDown(metadata), // This must run after PredicatePushDown and LimitPushDown so that it squashes any successive filter nodes and limits
                 new HashGenerationOptimizer(featuresConfig.isOptimizeHashGeneration()), // This must run after all other optimizers have run to that all the PlanNodes are created
                 new MergeProjections(),
                 new PruneUnreferencedOutputs(), // Make sure to run this at the end to help clean the plan for logging/execution and not remove info that other optimizers might need at an earlier point
