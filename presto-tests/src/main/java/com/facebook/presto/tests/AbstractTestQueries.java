605f832eedccd9b15fa769246c9b064233a2a045
Haozhun Jin <haozhun.jin@gmail.com>
2015-07-23 10:13:57 -0700

    Add UnknownOperator and change IS DISTINCT FROM accordingly
    1. If fixes query SELECT NULL UNION SELECT NULL
    2. It avoids future failure when ambiguity check is added to coercion-enabled resolving in FunctionRegistry

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -3455,6 +3455,7 @@ public abstract class AbstractTestQueries
     {
         assertQuery("SELECT orderkey FROM orders UNION SELECT custkey FROM orders");
         assertQuery("SELECT 123 UNION DISTINCT SELECT 123 UNION ALL SELECT 123");
+        assertQuery("SELECT NULL UNION SELECT NULL");
 
         // mixed single-node vs fixed vs source-distributed
         assertQuery("SELECT orderkey FROM orders UNION ALL SELECT 123 UNION ALL (SELECT custkey FROM orders GROUP BY custkey)");
