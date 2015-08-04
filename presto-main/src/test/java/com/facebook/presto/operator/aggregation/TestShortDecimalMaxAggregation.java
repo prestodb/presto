eb70c50badb3ce92e678126ddee857ebdd383336
Lukasz Osipiuk <lukasz.osipiuk@teradata.com>
2015-08-04 13:55:24 +0200

    Test max() and min() aggregation for DECIMAL

---
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestShortDecimalMaxAggregation.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.presto.operator.aggregation;
+
+import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.block.BlockBuilder;
+import com.facebook.presto.spi.block.BlockBuilderStatus;
+import com.facebook.presto.spi.type.DecimalType;
+import com.facebook.presto.spi.type.SqlDecimal;
+import com.google.common.collect.ImmutableList;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class TestShortDecimalMaxAggregation
+        extends AbstractTestAggregationFunction
+{
+    public static final DecimalType SHORT_DECIMAL = DecimalType.createDecimalType(10, 5);
+
+    @Override
+    public Block[] getSequenceBlocks(int start, int length)
+    {
+        BlockBuilder blockBuilder = SHORT_DECIMAL.createBlockBuilder(new BlockBuilderStatus(), length);
+        for (int i = start; i < start + length; i++) {
+            SHORT_DECIMAL.writeLong(blockBuilder, i);
+        }
+        return new Block[] {blockBuilder.build()};
+    }
+
+    @Override
+    public SqlDecimal getExpectedValue(int start, int length)
+    {
+        if (length == 0) {
+            return null;
+        }
+        return new SqlDecimal(BigInteger.valueOf(start + length - 1), 10, 5);
+    }
+
+    @Override
+    protected String getFunctionName()
+    {
+        return "max";
+    }
+
+    @Override
+    protected List<String> getFunctionParameterTypes()
+    {
+        return ImmutableList.of(SHORT_DECIMAL.getTypeSignature().toString());
+    }
+}
