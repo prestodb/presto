d892765eca20e62b8109ef317fb89ed4697497e7
Zhenxiao Luo <zluo@netflix.com>
2015-08-05 18:28:22 -0700

    Fix TestPrestoS3FileSystem.testStaticCredentials

--- presto-hive/src/test/java/com/facebook/presto/hive/TestPrestoS3FileSystem.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestPrestoS3FileSystem.java
@@ -47,7 +47,7 @@ public class TestPrestoS3FileSystem
         Configuration config = new Configuration();
         config.set("fs.s3n.awsSecretAccessKey", "test_secret_access_key");
         config.set("fs.s3n.awsAccessKeyId", "test_access_key_id");
-        // the static credentials should be preferred
+        config.setBoolean(PrestoS3FileSystem.S3_USE_INSTANCE_CREDENTIALS, false);
 
         try (PrestoS3FileSystem fs = new PrestoS3FileSystem()) {
             fs.initialize(new URI("s3n://test-bucket/"), config);
