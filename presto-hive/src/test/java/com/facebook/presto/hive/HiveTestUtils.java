da0955e8690bd023654c56029c8b7cacaac778de
Christopher Berner <christopherberner@gmail.com>
2015-08-14 14:08:26 -0700

    Move test Block creation methods to presto-tests

--- presto-hive/src/test/java/com/facebook/presto/hive/HiveTestUtils.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/HiveTestUtils.java
@@ -20,10 +20,6 @@ import com.facebook.presto.hive.orc.OrcRecordCursorProvider;
 import com.facebook.presto.hive.rcfile.RcFilePageSourceFactory;
 import com.facebook.presto.spi.ColumnHandle;
 import com.facebook.presto.spi.ConnectorSession;
-import com.facebook.presto.spi.block.Block;
-import com.facebook.presto.spi.block.BlockBuilder;
-import com.facebook.presto.spi.block.BlockBuilderStatus;
-import com.facebook.presto.spi.block.InterleavedBlockBuilder;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.testing.TestingConnectorSession;
 import com.facebook.presto.type.TypeRegistry;
@@ -34,7 +30,6 @@ import com.google.common.collect.ImmutableSet;
 import java.util.List;
 
 import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
-import static com.facebook.presto.type.TypeUtils.appendToBlockBuilder;
 import static java.util.Locale.ENGLISH;
 
 public final class HiveTestUtils
@@ -75,31 +70,5 @@ public final class HiveTestUtils
             types.add(TYPE_MANAGER.getType(((HiveColumnHandle) columnHandle).getTypeSignature()));
         }
         return types.build();
-    }
-
-    public static Block arrayBlockOf(Type elementType, Object... values)
-    {
-        BlockBuilder blockBuilder = elementType.createBlockBuilder(new BlockBuilderStatus(), 1024);
-        for (Object value : values) {
-            appendToBlockBuilder(elementType, value, blockBuilder);
-        }
-        return blockBuilder.build();
-    }
-
-    public static Block mapBlockOf(Type keyType, Type valueType, Object key, Object value)
-    {
-        BlockBuilder blockBuilder = new InterleavedBlockBuilder(ImmutableList.of(keyType, valueType), new BlockBuilderStatus(), 1024);
-        appendToBlockBuilder(keyType, key, blockBuilder);
-        appendToBlockBuilder(valueType, value, blockBuilder);
-        return blockBuilder.build();
-    }
-
-    public static Block rowBlockOf(List<Type> parameterTypes, Object... values)
-    {
-        InterleavedBlockBuilder blockBuilder = new InterleavedBlockBuilder(parameterTypes, new BlockBuilderStatus(), 1024);
-        for (int i = 0; i < values.length; i++) {
-            appendToBlockBuilder(parameterTypes.get(i), values[i], blockBuilder);
-        }
-        return blockBuilder.build();
     }
 }
