dain/facebook-presto

Extract scheduler policy from SqlQueryScheduler

presto-main/src/main/java/com/facebook/presto/execution/scheduler/ExecutionPolicy.java
@@ -1,0 +1,23 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.execution.scheduler;
+
+import com.facebook.presto.execution.SqlStageExecution;
+
+import java.util.Collection;
+
+public interface ExecutionPolicy
+{
+    ExecutionSchedule createSchedulerPolicy(Collection<SqlStageExecution> stages);
+}

presto-main/src/main/java/com/facebook/presto/execution/scheduler/SqlQueryScheduler.java
@@ -45,7 +45,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -71,6 +70,7 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class SqlQueryScheduler
 {
     private final QueryStateMachine queryStateMachine;
+    private final ExecutionPolicy executionPolicy;
     private final Map<StageId, SqlStageExecution> stages;
     private final ExecutorService executor;
     private final StageId rootStageId;
@@ -88,9 +88,11 @@ public class SqlQueryScheduler
             int initialHashPartitions,
             ExecutorService executor,
             OutputBuffers rootOutputBuffers,
-            NodeTaskMap nodeTaskMap)
+            NodeTaskMap nodeTaskMap,
+            ExecutionPolicy executionPolicy)
     {
-        this.queryStateMachine = queryStateMachine;
+        this.queryStateMachine = requireNonNull(queryStateMachine, "queryStateMachine is null");
+        this.executionPolicy = requireNonNull(executionPolicy, "schedulerPolicyFactory is null");
 
         // todo come up with a better way to build this, or eliminate this map
         ImmutableMap.Builder<StageId, StageScheduler> stageSchedulers = ImmutableMap.builder();
@@ -279,12 +281,12 @@ public class SqlQueryScheduler
     private void schedule()
     {
         try (SetThreadName ignored = new SetThreadName("Query-%s", queryStateMachine.getQueryId())) {
-            List<SqlStageExecution> schedulingStages = new CopyOnWriteArrayList<>(this.stages.values());
-            schedulingStages.forEach(SqlStageExecution::beginScheduling);
-
-            while (!schedulingStages.isEmpty()) {
+            ExecutionSchedule executionSchedule = executionPolicy.createSchedulerPolicy(stages.values());
+            while (!executionSchedule.isFinished()) {
                 List<CompletableFuture<?>> blockedStages = new ArrayList<>();
-                for (SqlStageExecution stage : schedulingStages) {
+                for (SqlStageExecution stage : executionSchedule.getStagesToSchedule()) {
+                    stage.beginScheduling();
+
                     // perform some scheduling work
                     ScheduleResult result = stageSchedulers.get(stage.getStageId())
                             .schedule();
@@ -292,15 +294,18 @@ public class SqlQueryScheduler
                     // modify parent and children based on the results of the scheduling
                     if (result.isFinished()) {
                         stage.schedulingComplete();
-                        schedulingStages.remove(stage);
                     }
-                    blockedStages.add(result.getBlocked());
+                    else if (!result.getBlocked().isDone()) {
+                        blockedStages.add(result.getBlocked());
+                    }
                     stageLinkages.get(stage.getStageId())
                             .processScheduleResults(stage.getState(), result.getNewTasks());
                 }
 
                 // wait for a state change and then schedule again
-                tryGetFutureValue(firstCompletedFuture(blockedStages), 100, MILLISECONDS);
+                if (!blockedStages.isEmpty()) {
+                    tryGetFutureValue(firstCompletedFuture(blockedStages), 100, MILLISECONDS);
+                }
             }
         }
         catch (Throwable t) {

prestodb/presto

presto-main/src/main/java/com/facebook/presto/execution/scheduler/ExecutionPolicy.java
@@ -1,0 +1,23 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.execution.scheduler;
+
+import com.facebook.presto.execution.SqlStageExecution;
+
+import java.util.Collection;
+
+public interface ExecutionPolicy
+{
+    ExecutionSchedule createExecutionSchedule(Collection<SqlStageExecution> stages);
+}

presto-main/src/main/java/com/facebook/presto/execution/scheduler/SqlQueryScheduler.java
@@ -68,6 +68,7 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class SqlQueryScheduler
 {
     private final QueryStateMachine queryStateMachine;
+    private final ExecutionPolicy executionPolicy;
     private final Map<StageId, SqlStageExecution> stages;
     private final ExecutorService executor;
     private final StageId rootStageId;
@@ -85,9 +86,11 @@ public class SqlQueryScheduler
             int initialHashPartitions,
             ExecutorService executor,
             OutputBuffers rootOutputBuffers,
-            NodeTaskMap nodeTaskMap)
+            NodeTaskMap nodeTaskMap,
+            ExecutionPolicy executionPolicy)
     {
-        this.queryStateMachine = queryStateMachine;
+        this.queryStateMachine = requireNonNull(queryStateMachine, "queryStateMachine is null");
+        this.executionPolicy = requireNonNull(executionPolicy, "schedulerPolicyFactory is null");
 
         // todo come up with a better way to build this, or eliminate this map
         ImmutableMap.Builder<StageId, StageScheduler> stageSchedulers = ImmutableMap.builder();
@@ -276,12 +279,12 @@ public class SqlQueryScheduler
     private void schedule()
     {
         try (SetThreadName ignored = new SetThreadName("Query-%s", queryStateMachine.getQueryId())) {
-            List<SqlStageExecution> schedulingStages = new ArrayList<>(this.stages.values());
-            schedulingStages.forEach(SqlStageExecution::beginScheduling);
-
-            while (!schedulingStages.isEmpty()) {
+            ExecutionSchedule executionSchedule = executionPolicy.createExecutionSchedule(stages.values());
+            while (!executionSchedule.isFinished()) {
                 List<CompletableFuture<?>> blockedStages = new ArrayList<>();
-                for (SqlStageExecution stage : ImmutableList.copyOf(schedulingStages)) {
+                for (SqlStageExecution stage : executionSchedule.getStagesToSchedule()) {
+                    stage.beginScheduling();
+
                     // perform some scheduling work
                     ScheduleResult result = stageSchedulers.get(stage.getStageId())
                             .schedule();
@@ -289,15 +292,18 @@ public class SqlQueryScheduler
                     // modify parent and children based on the results of the scheduling
                     if (result.isFinished()) {
                         stage.schedulingComplete();
-                        schedulingStages.remove(stage);
                     }
-                    blockedStages.add(result.getBlocked());
+                    else if (!result.getBlocked().isDone()) {
+                        blockedStages.add(result.getBlocked());
+                    }
                     stageLinkages.get(stage.getStageId())
                             .processScheduleResults(stage.getState(), result.getNewTasks());
                 }
 
                 // wait for a state change and then schedule again
-                tryGetFutureValue(firstCompletedFuture(blockedStages), 100, MILLISECONDS);
+                if (!blockedStages.isEmpty()) {
+                    tryGetFutureValue(firstCompletedFuture(blockedStages), 100, MILLISECONDS);
+                }
             }
         }
         catch (Throwable t) {