dain/facebook-presto

Add phased execution policy

presto-main/src/main/java/com/facebook/presto/execution/scheduler/PhasedExecutionPolicy.java
@@ -1,0 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.execution.scheduler;
+
+import com.facebook.presto.execution.SqlStageExecution;
+
+import java.util.Collection;
+
+public class PhasedExecutionPolicy
+        implements ExecutionPolicy
+{
+    @Override
+    public ExecutionSchedule createSchedulerPolicy(Collection<SqlStageExecution> stages)
+    {
+        return new PhasedExecutionSchedule(stages);
+    }
+}

prestodb/presto

presto-main/src/main/java/com/facebook/presto/execution/scheduler/PhasedExecutionPolicy.java
@@ -1,0 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.execution.scheduler;
+
+import com.facebook.presto.execution.SqlStageExecution;
+
+import java.util.Collection;
+
+public class PhasedExecutionPolicy
+        implements ExecutionPolicy
+{
+    @Override
+    public ExecutionSchedule createExecutionSchedule(Collection<SqlStageExecution> stages)
+    {
+        return new PhasedExecutionSchedule(stages);
+    }
+}