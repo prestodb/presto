3df7fab1f8e5788dff331bece6201eedf995ae26
Karol Sobczak <Karol.Sobczak@Teradata.com>
2015-08-28 17:00:12 +0200

    Add casting between decimal types

--- presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
@@ -215,6 +215,7 @@ import static com.facebook.presto.type.DecimalCasts.DECIMAL_TO_DOUBLE_CAST;
 import static com.facebook.presto.type.DecimalCasts.DECIMAL_TO_VARCHAR_CAST;
 import static com.facebook.presto.type.DecimalCasts.DOUBLE_TO_DECIMAL_CAST;
 import static com.facebook.presto.type.DecimalCasts.VARCHAR_TO_DECIMAL_CAST;
+import static com.facebook.presto.type.DecimalToDecimalCasts.DECIMAL_TO_DECIMAL_CAST;
 import static com.facebook.presto.type.JsonPathType.JSON_PATH;
 import static com.facebook.presto.type.LikePatternType.LIKE_PATTERN;
 import static com.facebook.presto.type.RegexpType.REGEXP;
@@ -348,6 +349,7 @@ public class FunctionRegistry
                 .function(COUNT_COLUMN)
                 .functions(ROW_HASH_CODE, ROW_TO_JSON, ROW_EQUAL, ROW_NOT_EQUAL)
                 .function(CONCAT)
+                .function(DECIMAL_TO_DECIMAL_CAST)
                 .function(TRY_CAST);
 
         if (experimentalSyntaxEnabled) {
