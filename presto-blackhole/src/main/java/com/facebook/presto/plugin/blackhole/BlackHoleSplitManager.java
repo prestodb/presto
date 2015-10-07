a67cd5362a6adcb24014e13dbf84293facad0b85
David Phillips <david@acz.org>
2015-10-07 16:31:47 -0700

    Rename blackhole splits_count to split_count

--- presto-blackhole/src/main/java/com/facebook/presto/plugin/blackhole/BlackHoleSplitManager.java
+++ presto-blackhole/src/main/java/com/facebook/presto/plugin/blackhole/BlackHoleSplitManager.java
@@ -36,7 +36,7 @@ public final class BlackHoleSplitManager
 
         ImmutableList.Builder<BlackHoleSplit> builder = ImmutableList.<BlackHoleSplit>builder();
 
-        for (int i = 0; i < layout.getSplitsCount(); i++) {
+        for (int i = 0; i < layout.getSplitCount(); i++) {
             builder.add(new BlackHoleSplit(layout.getPagesPerSplit(), layout.getRowsPerPage()));
         }
         return new FixedSplitSource("blackhole", builder.build());
