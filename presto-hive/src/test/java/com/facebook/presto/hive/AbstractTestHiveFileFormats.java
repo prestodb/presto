bb0011d74260a668069e0835843da1c94037cc19
Sailesh Mittal <smittal@twitter.com>
2015-10-07 14:13:23 -0700

    Add another nested struct with non null values after nulls.

--- presto-hive/src/test/java/com/facebook/presto/hive/AbstractTestHiveFileFormats.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/AbstractTestHiveFileFormats.java
@@ -234,6 +234,28 @@ public abstract class AbstractTestHiveFileFormats
                     ImmutableList.of(javaStringObjectInspector, javaStringObjectInspector)), Arrays.asList(null, null), rowBlockOf(ImmutableList.of(VARCHAR, VARCHAR), null, null)))
             .add(new TestColumn("t_struct_non_nulls_after_nulls", getStandardStructObjectInspector(ImmutableList.of("struct_field1", "struct_field2"),
                     ImmutableList.of(javaIntObjectInspector, javaStringObjectInspector)), Arrays.asList(null, "some string"), rowBlockOf(ImmutableList.of(BIGINT, VARCHAR), null, "some string")))
+            .add(new TestColumn("t_nested_struct_non_nulls_after_nulls",
+                    getStandardStructObjectInspector(
+                            ImmutableList.of("struct_field1", "struct_field2", "strict_field3"),
+                            ImmutableList.of(
+                                    javaIntObjectInspector,
+                                    javaStringObjectInspector,
+                                    getStandardStructObjectInspector(
+                                            ImmutableList.of("nested_struct_field1", "nested_struct_field2"),
+                                            ImmutableList.of(javaIntObjectInspector, javaStringObjectInspector)
+                                    )
+                            )
+                    ),
+                    Arrays.asList(null, "some string", Arrays.asList(null, "nested_string2")),
+                    rowBlockOf(
+                            ImmutableList.of(
+                                    BIGINT,
+                                    VARCHAR,
+                                    new RowType(ImmutableList.of(BIGINT, VARCHAR), Optional.empty())
+                            ),
+                            null, "some string", rowBlockOf(ImmutableList.of(BIGINT, VARCHAR), null, "nested_string2")
+                    )
+            ))
             .build();
 
     private static Map<Integer, Integer> mapWithNullKey()
