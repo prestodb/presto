cfd172191e99fe5ae9a613d552d5d43f1ec2a8ec
Christopher Berner <christopherberner@gmail.com>
2015-10-12 17:36:28 -0700

    Replace Maps.newHashMap with new HashMap<>()

--- presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestMultimapAggAggregation.java
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestMultimapAggAggregation.java
@@ -22,10 +22,10 @@ import com.facebook.presto.type.MapType;
 import com.facebook.presto.type.RowType;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -121,7 +121,7 @@ public class TestMultimapAggAggregation
         Signature signature = new Signature(NAME, AGGREGATE, mapType.getTypeSignature(), keyType.getTypeSignature(), valueType.getTypeSignature());
         InternalAggregationFunction aggFunc = metadata.getFunctionRegistry().getAggregateFunctionImplementation(signature);
 
-        Map<K, List<V>> map = Maps.newHashMap();
+        Map<K, List<V>> map = new HashMap<>();
         for (int i = 0; i < expectedKeys.size(); i++) {
             if (!map.containsKey(expectedKeys.get(i))) {
                 map.put(expectedKeys.get(i), new ArrayList<>());
