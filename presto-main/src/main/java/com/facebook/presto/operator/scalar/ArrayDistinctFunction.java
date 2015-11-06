47ddfcc6df2cb7a93ed038ae32857916de42e351
Christopher Berner <christopherberner@gmail.com>
2015-11-06 09:43:09 -0800

    Migrate array_distinct to new framework

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayDistinctFunction.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayDistinctFunction.java
@@ -13,64 +13,30 @@
  */
 package com.facebook.presto.operator.scalar;
 
-import com.facebook.presto.metadata.FunctionRegistry;
-import com.facebook.presto.metadata.SqlScalarFunction;
+import com.facebook.presto.metadata.OperatorType;
+import com.facebook.presto.operator.Description;
 import com.facebook.presto.operator.aggregation.TypedSet;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
+import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.spi.type.TypeManager;
-import com.google.common.collect.ImmutableList;
+import com.facebook.presto.type.SqlType;
 
 import java.lang.invoke.MethodHandle;
-import java.util.Map;
 
-import static com.facebook.presto.metadata.Signature.comparableTypeParameter;
-import static com.facebook.presto.util.Reflection.methodHandle;
-import static com.google.common.base.Preconditions.checkArgument;
-import static java.lang.String.format;
-
+@ScalarFunction("array_distinct")
+@Description("Remove duplicate values from the given array")
 public final class ArrayDistinctFunction
-        extends SqlScalarFunction
 {
-    public static final ArrayDistinctFunction ARRAY_DISTINCT_FUNCTION = new ArrayDistinctFunction();
-    private static final String FUNCTION_NAME = "array_distinct";
-    private static final MethodHandle METHOD_HANDLE = methodHandle(ArrayDistinctFunction.class, "distinct", Type.class, Block.class);
+    private ArrayDistinctFunction() {}
 
-    public ArrayDistinctFunction()
-    {
-        super(FUNCTION_NAME, ImmutableList.of(comparableTypeParameter("E")), "array(E)", ImmutableList.of("array(E)"));
-    }
-
-    @Override
-    public boolean isHidden()
-    {
-        return false;
-    }
-
-    @Override
-    public boolean isDeterministic()
-    {
-        return true;
-    }
-
-    @Override
-    public String getDescription()
-    {
-        return "Remove duplicate values from the given array";
-    }
-
-    @Override
-    public ScalarFunctionImplementation specialize(Map<String, Type> types, int arity, TypeManager typeManager, FunctionRegistry functionRegistry)
-    {
-        checkArgument(types.size() == 1, format("%s expects only one argument", FUNCTION_NAME));
-        Type type = types.get("E");
-        MethodHandle methodHandle = METHOD_HANDLE.bindTo(type);
-        return new ScalarFunctionImplementation(false, ImmutableList.of(false), methodHandle, isDeterministic());
-    }
-
-    public static Block distinct(Type type, Block array)
+    @TypeParameter("E")
+    @SqlType("array(E)")
+    public static Block distinct(
+            @OperatorDependency(operator = OperatorType.EQUAL, returnType = StandardTypes.BOOLEAN, argumentTypes = {"E", "E"}) MethodHandle equals,
+            @TypeParameter("E") Type type,
+            @SqlType("array(E)") Block array)
     {
         if (array.getPositionCount() < 2) {
             return array;
