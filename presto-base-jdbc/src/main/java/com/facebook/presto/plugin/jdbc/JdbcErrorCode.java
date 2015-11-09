1617e8b1fac7748d39717c48398262de7a6d1ee3
Christopher Berner <christopherberner@gmail.com>
2015-11-09 09:50:46 -0800

    Categorize errors in JDBC connector

---
+++ presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/JdbcErrorCode.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.plugin.jdbc;
+
+import com.facebook.presto.spi.ErrorCode;
+import com.facebook.presto.spi.ErrorCodeSupplier;
+
+public enum JdbcErrorCode
+        implements ErrorCodeSupplier
+{
+    JDBC_ERROR(0x0400_0000),
+    JDBC_NON_TRANSIENT_ERROR(0x0400_0001);
+
+    private final ErrorCode errorCode;
+
+    JdbcErrorCode(int code)
+    {
+        errorCode = new ErrorCode(code, name());
+    }
+
+    @Override
+    public ErrorCode toErrorCode()
+    {
+        return errorCode;
+    }
+}
