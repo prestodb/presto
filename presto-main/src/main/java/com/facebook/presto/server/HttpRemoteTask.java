e88c1209314db2f1cf0cb4e93367fa636e4c9195
Nileema Shingte <nileema.shingte@gmail.com>
2015-11-18 14:23:55 -0800

    Update error message for too many requests failed

--- presto-main/src/main/java/com/facebook/presto/server/HttpRemoteTask.java
+++ presto-main/src/main/java/com/facebook/presto/server/HttpRemoteTask.java
@@ -949,8 +949,9 @@ public class HttpRemoteTask
             if (backoff.failure()) {
                 // it is weird to mark the task failed locally and then cancel the remote task, but there is no way to tell a remote task that it is failed
                 PrestoException exception = new PrestoException(TOO_MANY_REQUESTS_FAILED,
-                        format("%s (%s - %s failures, time since last success %s)",
+                        format("%s (%s %s - %s failures, time since last success %s)",
                                 WORKER_NODE_ERROR,
+                                jobDescription,
                                 taskUri,
                                 backoff.getFailureCount(),
                                 backoff.getTimeSinceLastSuccess().convertTo(SECONDS)));
