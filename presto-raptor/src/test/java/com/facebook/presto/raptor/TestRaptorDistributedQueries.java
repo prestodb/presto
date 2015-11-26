db18e6d3f568ab70a853a72e1925a55be8f4f066
Nileema Shingte <nileema.shingte@gmail.com>
2015-11-25 18:05:52 -0800

    Make table properties case insensitive

--- presto-raptor/src/test/java/com/facebook/presto/raptor/TestRaptorDistributedQueries.java
+++ presto-raptor/src/test/java/com/facebook/presto/raptor/TestRaptorDistributedQueries.java
@@ -74,4 +74,12 @@ public class TestRaptorDistributedQueries
             UUID.fromString((String) uuid);
         }
     }
+
+    @Test
+    public void testTableProperties()
+            throws Exception
+    {
+        computeActual("CREATE TABLE test_table_properties_1 (foo BIGINT, bar BIGINT, ds DATE) WITH (ordering=array['foo','bar'], temporal_column='ds')");
+        computeActual("CREATE TABLE test_table_properties_2 (foo BIGINT, bar BIGINT, ds DATE) WITH (ORDERING=array['foo','bar'], TEMPORAL_COLUMN='ds')");
+    }
 }
