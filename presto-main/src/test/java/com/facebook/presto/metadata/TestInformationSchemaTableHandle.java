3cb4e29467ec30e7948c89c5e4769d20aa285bf0
Eric Hwang <ehwang@fb.com>
2015-12-10 15:56:47 -0800

    Split TestJsonTableHandle into system and info_schema tests

--- presto-main/src/test/java/com/facebook/presto/metadata/TestInformationSchemaTableHandle.java
+++ presto-main/src/test/java/com/facebook/presto/metadata/TestInformationSchemaTableHandle.java
@@ -15,18 +15,13 @@ package com.facebook.presto.metadata;
 
 import com.facebook.presto.connector.informationSchema.InformationSchemaHandleResolver;
 import com.facebook.presto.connector.informationSchema.InformationSchemaTableHandle;
-import com.facebook.presto.connector.system.SystemHandleResolver;
-import com.facebook.presto.connector.system.SystemTableHandle;
 import com.facebook.presto.spi.ConnectorHandleResolver;
 import com.facebook.presto.spi.ConnectorTableHandle;
-import com.facebook.presto.spi.SchemaTableName;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
-import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Module;
 import com.google.inject.Stage;
 import com.google.inject.multibindings.MapBinder;
 import io.airlift.json.JsonModule;
@@ -40,19 +35,12 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 @Test(singleThreaded = true)
-public class TestJsonTableHandle
+public class TestInformationSchemaTableHandle
 {
-    private static final String INFORMATION_SCHEMA_CONNECTOR_ID = "information_connector_id";
-    private static final String SYSTEM_CONNECTOR_ID = "system_connector_id";
-    private static final Map<String, Object> SYSTEM_AS_MAP = ImmutableMap.<String, Object>of(
-            "type", "system",
-            "connectorId", SYSTEM_CONNECTOR_ID,
-            "schemaName", "system_schema",
-            "tableName", "system_table");
-
-    private static final Map<String, Object> INFORMATION_SCHEMA_AS_MAP = ImmutableMap.<String, Object>of(
+    private static final String CONNECTOR_ID = "information_connector_id";
+    private static final Map<String, Object> SCHEMA_AS_MAP = ImmutableMap.<String, Object>of(
             "type", "information_schema",
-            "connectorId", INFORMATION_SCHEMA_CONNECTOR_ID,
+            "connectorId", CONNECTOR_ID,
             "catalogName", "information_schema_catalog",
             "schemaName", "information_schema_schema",
             "tableName", "information_schema_table"
@@ -66,29 +54,12 @@ public class TestJsonTableHandle
         Injector injector = Guice.createInjector(Stage.PRODUCTION,
                 new JsonModule(),
                 new HandleJsonModule(),
-                new Module()
-                {
-                    @Override
-                    public void configure(Binder binder)
-                    {
-                        MapBinder<String, ConnectorHandleResolver> connectorHandleResolverBinder = MapBinder.newMapBinder(binder, String.class, ConnectorHandleResolver.class);
-                        connectorHandleResolverBinder.addBinding("system").toInstance(new SystemHandleResolver(SYSTEM_CONNECTOR_ID));
-                        connectorHandleResolverBinder.addBinding("information_schema").toInstance(new InformationSchemaHandleResolver(INFORMATION_SCHEMA_CONNECTOR_ID));
-                    }
+                binder -> {
+                    MapBinder<String, ConnectorHandleResolver> connectorHandleResolverBinder = MapBinder.newMapBinder(binder, String.class, ConnectorHandleResolver.class);
+                    connectorHandleResolverBinder.addBinding("information_schema").toInstance(new InformationSchemaHandleResolver(CONNECTOR_ID));
                 });
 
         objectMapper = injector.getInstance(ObjectMapper.class);
-    }
-
-    @Test
-    public void testSystemSerialize()
-            throws Exception
-    {
-        SystemTableHandle internalHandle = new SystemTableHandle(SYSTEM_CONNECTOR_ID, "system_schema", "system_table");
-
-        assertTrue(objectMapper.canSerialize(SystemTableHandle.class));
-        String json = objectMapper.writeValueAsString(internalHandle);
-        testJsonEquals(json, SYSTEM_AS_MAP);
     }
 
     @Test
@@ -96,41 +67,27 @@ public class TestJsonTableHandle
             throws Exception
     {
         InformationSchemaTableHandle informationSchemaTableHandle = new InformationSchemaTableHandle(
-                INFORMATION_SCHEMA_CONNECTOR_ID,
+                CONNECTOR_ID,
                 "information_schema_catalog",
                 "information_schema_schema",
                 "information_schema_table");
 
         assertTrue(objectMapper.canSerialize(InformationSchemaTableHandle.class));
         String json = objectMapper.writeValueAsString(informationSchemaTableHandle);
-        testJsonEquals(json, INFORMATION_SCHEMA_AS_MAP);
-    }
-
-    @Test
-    public void testSystemDeserialize()
-            throws Exception
-    {
-        String json = objectMapper.writeValueAsString(SYSTEM_AS_MAP);
-
-        ConnectorTableHandle tableHandle = objectMapper.readValue(json, ConnectorTableHandle.class);
-        assertEquals(tableHandle.getClass(), SystemTableHandle.class);
-        SystemTableHandle systemHandle = (SystemTableHandle) tableHandle;
-
-        assertEquals(systemHandle.getConnectorId(), SYSTEM_CONNECTOR_ID);
-        assertEquals(systemHandle.getSchemaTableName(), new SchemaTableName("system_schema", "system_table"));
+        testJsonEquals(json, SCHEMA_AS_MAP);
     }
 
     @Test
     public void testInformationSchemaDeserialize()
             throws Exception
     {
-        String json = objectMapper.writeValueAsString(INFORMATION_SCHEMA_AS_MAP);
+        String json = objectMapper.writeValueAsString(SCHEMA_AS_MAP);
 
         ConnectorTableHandle tableHandle = objectMapper.readValue(json, ConnectorTableHandle.class);
         assertEquals(tableHandle.getClass(), InformationSchemaTableHandle.class);
         InformationSchemaTableHandle informationSchemaHandle = (InformationSchemaTableHandle) tableHandle;
 
-        assertEquals(informationSchemaHandle.getConnectorId(), INFORMATION_SCHEMA_CONNECTOR_ID);
+        assertEquals(informationSchemaHandle.getConnectorId(), CONNECTOR_ID);
         assertEquals(informationSchemaHandle.getCatalogName(), "information_schema_catalog");
         assertEquals(informationSchemaHandle.getSchemaName(), "information_schema_schema");
         assertEquals(informationSchemaHandle.getTableName(), "information_schema_table");
