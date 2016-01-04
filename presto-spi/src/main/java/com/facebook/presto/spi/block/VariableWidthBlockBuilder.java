dfb6305a26c828281ea1f0314c08ea0b1c1bf343
Christopher Berner <christopherberner@gmail.com>
2016-01-04 13:47:42 -0800

    Improve accuracy of BlockBuilder retained size
    This is particularly important for array_agg() because in a grouped
    aggregation it retains O(num_groups) BlockBuilders

--- presto-spi/src/main/java/com/facebook/presto/spi/block/VariableWidthBlockBuilder.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/block/VariableWidthBlockBuilder.java
@@ -97,7 +97,7 @@ public class VariableWidthBlockBuilder
     @Override
     public int getRetainedSizeInBytes()
     {
-        long size = INSTANCE_SIZE + sliceOutput.getUnderlyingSlice().getRetainedSize() + offsets.getUnderlyingSlice().getRetainedSize() + valueIsNull.getUnderlyingSlice().getRetainedSize();
+        long size = INSTANCE_SIZE + sliceOutput.getRetainedSize() + offsets.getRetainedSize() + valueIsNull.getRetainedSize();
         if (size > Integer.MAX_VALUE) {
             return Integer.MAX_VALUE;
         }
