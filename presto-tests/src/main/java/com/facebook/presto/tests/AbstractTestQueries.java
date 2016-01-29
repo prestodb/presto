182f95149c0dd233409f9a356e02a469420ab1ce
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2016-01-29 10:03:36 -0500

    Address comment: rename foo and bar

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -4027,7 +4027,7 @@ public abstract class AbstractTestQueries
         assertExplainDdl("ALTER TABLE orders RENAME TO new_name");
         assertExplainDdl("ALTER TABLE orders RENAME COLUMN orderkey TO new_column_name");
         assertExplainDdl("SET SESSION foo = 'bar'");
-        assertExplainDdl("PREPARE foo FROM SELECT * FROM bar", "PREPARE foo");
+        assertExplainDdl("PREPARE my_query FROM SELECT * FROM orders", "PREPARE my_query");
         assertExplainDdl("RESET SESSION foo");
         assertExplainDdl("START TRANSACTION");
         assertExplainDdl("COMMIT");
