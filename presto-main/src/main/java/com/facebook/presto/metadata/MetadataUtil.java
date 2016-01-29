6d232c4c4bdbb1f5645054778b8483217a490cd8
yuananf <yuananf@gmail.com>
2016-01-29 16:24:46 +0800

    Fix typo in MetadataUtil for qualified object names
    The catalog and schema error messages were reversed.

--- presto-main/src/main/java/com/facebook/presto/metadata/MetadataUtil.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/MetadataUtil.java
@@ -121,9 +121,9 @@ public final class MetadataUtil
         List<String> parts = Lists.reverse(name.getParts());
         String objectName = parts.get(0);
         String schemaName = (parts.size() > 1) ? parts.get(1) : session.getSchema().orElseThrow(() ->
-                new SemanticException(CATALOG_NOT_SPECIFIED, node, "Catalog must be specified when session catalog is not set"));
-        String catalogName = (parts.size() > 2) ? parts.get(2) : session.getCatalog().orElseThrow(() ->
                 new SemanticException(SCHEMA_NOT_SPECIFIED, node, "Schema must be specified when session schema is not set"));
+        String catalogName = (parts.size() > 2) ? parts.get(2) : session.getCatalog().orElseThrow(() ->
+                new SemanticException(CATALOG_NOT_SPECIFIED, node, "Catalog must be specified when session catalog is not set"));
 
         return new QualifiedObjectName(catalogName, schemaName, objectName);
     }
