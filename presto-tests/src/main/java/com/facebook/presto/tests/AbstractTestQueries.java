101c7af155e01c0bc544d7aba2458a2aa2d9f868
arhimondr <andriyrosa@gmail.com>
2016-01-30 23:15:28 +0100

    Allow implicit conversions from DECIMAL to DOUBLE

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -6503,6 +6503,14 @@ public abstract class AbstractTestQueries
         assertEqualsIgnoreOrder(
                 computeActual("SELECT ARRAY[CAST(282 AS DECIMAL(22,1)), CAST(282 AS DECIMAL(10,1))] || CAST(292 AS BIGINT)"),
                 computeActual("SELECT ARRAY[CAST(282 AS DECIMAL(22,1)), CAST(282 AS DECIMAL(10,1)), CAST(292 AS DECIMAL(19,0))]"));
+
+        // DECIMAL - DECIMAL
+        assertQuery("SELECT CAST(1.1 AS DECIMAL(38,1)) + CAST(1.1 AS DOUBLE)");
+        assertQuery("SELECT CAST(1.1 AS DECIMAL(38,1)) = CAST(1.1 AS DOUBLE)");
+        assertQuery("SELECT SIN(CAST(1.1 AS DECIMAL(38,1)))");
+        assertEqualsIgnoreOrder(
+                computeActual("SELECT ARRAY[CAST(282.1 AS DOUBLE), CAST(283.2 AS DOUBLE)] || CAST(101.3 AS DECIMAL(5,1))"),
+                computeActual("SELECT ARRAY[CAST(282.1 AS DOUBLE), CAST(283.2 AS DOUBLE), CAST(101.3 AS DOUBLE)]"));
     }
 
     @Test
