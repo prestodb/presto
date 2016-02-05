f9e22b96f44b99fb2fced47cbe5de0f16c40bd3c
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-02-05 14:35:08 +0100

    Remove unspecified values support from TypeCalculation
    All the variables which are involved in calculation must be explicitly declared.
    After Signature binding refactor there will be no such cases when we need to handle undeclared variables in TypeCalculation.

--- presto-parser/src/test/java/com/facebook/presto/type/TestTypeCalculation.java
+++ presto-parser/src/test/java/com/facebook/presto/type/TestTypeCalculation.java
@@ -16,56 +16,35 @@ package com.facebook.presto.type;
 import com.google.common.collect.ImmutableMap;
 import org.testng.annotations.Test;
 
-import java.util.OptionalLong;
-
 import static com.facebook.presto.type.TypeCalculation.calculateLiteralValue;
 import static org.testng.Assert.assertEquals;
 
 public class TestTypeCalculation
 {
     @Test
-    public void basicUsage()
+    public void testBasicUsage()
     {
-        assertEquals(calculateLiteralValue("42", ImmutableMap.of()), OptionalLong.of(42));
-        assertEquals(calculateLiteralValue("NULL", ImmutableMap.of()), OptionalLong.empty());
-        assertEquals(calculateLiteralValue("null", ImmutableMap.of()), OptionalLong.empty());
-        assertEquals(calculateLiteralValue("x", ImmutableMap.of("X", OptionalLong.of(42))), OptionalLong.of(42));
-        assertEquals(calculateLiteralValue("(42)", ImmutableMap.of()), OptionalLong.of(42));
-        assertEquals(calculateLiteralValue("(NULL)", ImmutableMap.of()), OptionalLong.empty());
-        assertEquals(calculateLiteralValue("(x)", ImmutableMap.of("X", OptionalLong.of(42))), OptionalLong.of(42));
+        assertEquals(calculateLiteralValue("42", ImmutableMap.of()), Long.valueOf(42));
+        assertEquals(calculateLiteralValue("NULL", ImmutableMap.of()), Long.valueOf(0));
+        assertEquals(calculateLiteralValue("null", ImmutableMap.of()), Long.valueOf(0));
+        assertEquals(calculateLiteralValue("x", ImmutableMap.of("x", 42L)), Long.valueOf(42));
+        assertEquals(calculateLiteralValue("(42)", ImmutableMap.of()), Long.valueOf(42));
+        assertEquals(calculateLiteralValue("(NULL)", ImmutableMap.of()), Long.valueOf(0));
+        assertEquals(calculateLiteralValue("(x)", ImmutableMap.of("x", 42L)), Long.valueOf(42));
 
-        assertEquals(calculateLiteralValue("42 + 55", ImmutableMap.of()), OptionalLong.of(42 + 55));
-        assertEquals(calculateLiteralValue("42 - 55", ImmutableMap.of()), OptionalLong.of(42 - 55));
-        assertEquals(calculateLiteralValue("42 * 55", ImmutableMap.of()), OptionalLong.of(42 * 55));
-        assertEquals(calculateLiteralValue("42 / 6", ImmutableMap.of()), OptionalLong.of(42 / 6));
+        assertEquals(calculateLiteralValue("42 + 55", ImmutableMap.of()), Long.valueOf(42 + 55));
+        assertEquals(calculateLiteralValue("42 - 55", ImmutableMap.of()), Long.valueOf(42 - 55));
+        assertEquals(calculateLiteralValue("42 * 55", ImmutableMap.of()), Long.valueOf(42 * 55));
+        assertEquals(calculateLiteralValue("42 / 6", ImmutableMap.of()), Long.valueOf(42 / 6));
 
-        assertEquals(calculateLiteralValue("42 + 55 * 6", ImmutableMap.of()), OptionalLong.of(42 + 55 * 6));
-        assertEquals(calculateLiteralValue("(42 + 55) * 6", ImmutableMap.of()), OptionalLong.of((42 + 55) * 6));
+        assertEquals(calculateLiteralValue("42 + 55 * 6", ImmutableMap.of()), Long.valueOf(42 + 55 * 6));
+        assertEquals(calculateLiteralValue("(42 + 55) * 6", ImmutableMap.of()), Long.valueOf((42 + 55) * 6));
 
-        assertEquals(calculateLiteralValue("min(10,2)", ImmutableMap.of()), OptionalLong.of(2));
-        assertEquals(calculateLiteralValue("min(10,2*10)", ImmutableMap.of()), OptionalLong.of(10));
-        assertEquals(calculateLiteralValue("max(10,2*10)", ImmutableMap.of()), OptionalLong.of(20));
-        assertEquals(calculateLiteralValue("max(10,2)", ImmutableMap.of()), OptionalLong.of(10));
+        assertEquals(calculateLiteralValue("min(10,2)", ImmutableMap.of()), Long.valueOf(2));
+        assertEquals(calculateLiteralValue("min(10,2*10)", ImmutableMap.of()), Long.valueOf(10));
+        assertEquals(calculateLiteralValue("max(10,2*10)", ImmutableMap.of()), Long.valueOf(20));
+        assertEquals(calculateLiteralValue("max(10,2)", ImmutableMap.of()), Long.valueOf(10));
 
-        assertEquals(calculateLiteralValue("x + y", ImmutableMap.of("X", OptionalLong.of(42), "Y", OptionalLong.of(55))), OptionalLong.of(42 + 55));
-        assertEquals(calculateLiteralValue("x + y", ImmutableMap.of("X", OptionalLong.of(42), "Y", OptionalLong.empty())), OptionalLong.empty());
-    }
-
-    @Test
-    public void disallowExpressionFlag()
-    {
-        assertEquals(calculateLiteralValue("x", ImmutableMap.of("X", OptionalLong.of(42)), false), OptionalLong.of(42));
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void failDisallowBinaryExpression()
-    {
-        calculateLiteralValue("x + y", ImmutableMap.of("X", OptionalLong.of(42), "Y", OptionalLong.of(55)), false);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void failDisallowUnaryExpression()
-    {
-        calculateLiteralValue("-y", ImmutableMap.of("Y", OptionalLong.of(55)), false);
+        assertEquals(calculateLiteralValue("x + y", ImmutableMap.of("x", 42L, "y", 55L)), Long.valueOf(42 + 55));
     }
 }
