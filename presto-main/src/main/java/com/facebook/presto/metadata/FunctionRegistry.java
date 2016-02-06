f2ff76bdcdb035679e5f934b7851f749b005f902
Wojciech Biela <Wojciech.Biela@teradata.com>
2016-02-06 02:03:00 +0100

    "sum" aggregation function for decimal(p,s)
    This implementation uses the specialize() method explicitly.
    The return type is a decimal(38,S) where S is the input scale. The
    accumulator uses a BigInteger interally.
    
    Conflicts:
    	presto-main/src/main/java/com/facebook/presto/util/DecimalUtils.java
    	presto-main/src/test/java/com/facebook/presto/type/TestDecimalOperators.java

--- presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
@@ -144,6 +144,7 @@ import static com.facebook.presto.operator.aggregation.ArrayAggregationFunction.
 import static com.facebook.presto.operator.aggregation.ChecksumAggregationFunction.CHECKSUM_AGGREGATION;
 import static com.facebook.presto.operator.aggregation.CountColumn.COUNT_COLUMN;
 import static com.facebook.presto.operator.aggregation.DecimalAverageAggregation.DECIMAL_AVERAGE_AGGREGATION;
+import static com.facebook.presto.operator.aggregation.DecimalSumAggregation.DECIMAL_SUM_AGGREGATION;
 import static com.facebook.presto.operator.aggregation.Histogram.HISTOGRAM;
 import static com.facebook.presto.operator.aggregation.MapAggregationFunction.MAP_AGG;
 import static com.facebook.presto.operator.aggregation.MaxAggregationFunction.MAX_AGGREGATION;
@@ -414,7 +415,8 @@ public class FunctionRegistry
                 .function(CONCAT)
                 .function(DECIMAL_TO_DECIMAL_CAST)
                 .function(TRY_CAST)
-                .function(DECIMAL_AVERAGE_AGGREGATION);
+                .function(DECIMAL_AVERAGE_AGGREGATION)
+                .function(DECIMAL_SUM_AGGREGATION);
 
         if (experimentalSyntaxEnabled) {
             builder.aggregate(ApproximateAverageAggregations.class)
