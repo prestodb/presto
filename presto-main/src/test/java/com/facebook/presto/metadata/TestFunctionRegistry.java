a20b28ed69e16b294b6b608e2da7207c69b4ad28
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-02-15 14:22:13 +0100

    Introduce most specific function selection algorithm
    If more that one function is selected as applicable
    for some particular parameters, the latest one in the candidates list
    is being selected for execution by analyzer. Candidates list is built with reflection,
    so the candidates list order is runtime specific.
    
    This commit introduces most specific function selection algorithm.
    This algorithm is suppose to ensure that selected function for some particular
    parameters will always be the same, and it will always be the most specific.
    
    The main algorithm idea is inspired by Java language specifications:
    https://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.12.2.5
    
    However the actual implemented algorithm has a several differences.
    Implemented algorithm in Presto is always trying to select applicable function
    that doesn't require any coercions for invocation, no matter what type of function
    it is - parametrized, variable arity or generic.
    
    The actual implemented algorithm is next:
    
    1.	Try to select function with explicitly declared parameters by direct match
    2. 	If more than one function selected than throw "ambiguous method invocation exception"
    3. 	Try to select generic function without coercion
    4. 	If more than one function selected than throw "ambiguous method invocation exception"
    5. 	Select all applicable function with coercions
    6. 	Select most specific function
       	One function is more specific than another if the invocation handled by the first method
       	could be passed on to the other one
    7. 	If the most specific function can't be selected than throw
       	"ambiguous method invocation exception"
    
    Although this algorithm covers most of the regular cases, some cases for `UNKNOWN` parameters
    are covered as a special case. `UNKNOWN` actually is not a real type, but rather some specific
    type for the values, types of which is unknown. So the coercion from `UNKNOWN` to Type can
    be considered as type inferring based on the context.
    
    So in addition to the above 7 steps, when one of the arguments is unknown, the following
    3 checks are being applied before throwing the `ambiguous call` exception.
    
    1.	Try to select single function that require only `UNKNOWN` to `TYPE` coercion.
    	If single function is selected - return it as a result.
    2.	If the return type of the multiple selected functions is the same, and
    	the function parameters are not declared as nullable, all that functions
    	are being considered as semantically equivalent, and very first of those
    	is being returned as a selected function.
    3.	If still more that one function are applicable than throw `ambiguous call`
    	exception

--- presto-main/src/test/java/com/facebook/presto/metadata/TestFunctionRegistry.java
+++ presto-main/src/test/java/com/facebook/presto/metadata/TestFunctionRegistry.java
@@ -16,7 +16,10 @@ package com.facebook.presto.metadata;
 import com.facebook.presto.block.BlockEncodingManager;
 import com.facebook.presto.operator.scalar.CustomFunctions;
 import com.facebook.presto.operator.scalar.ScalarFunction;
+import com.facebook.presto.operator.scalar.ScalarFunctionImplementation;
+import com.facebook.presto.spi.block.BlockEncodingSerde;
 import com.facebook.presto.spi.type.StandardTypes;
+import com.facebook.presto.spi.type.TypeManager;
 import com.facebook.presto.spi.type.TypeSignature;
 import com.facebook.presto.sql.analyzer.FeaturesConfig;
 import com.facebook.presto.sql.tree.QualifiedName;
@@ -24,22 +27,30 @@ import com.facebook.presto.type.SqlType;
 import com.facebook.presto.type.TypeRegistry;
 import com.google.common.base.Functions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import org.testng.annotations.Test;
 
+import java.lang.invoke.MethodHandles;
+import java.util.Collections;
 import java.util.List;
 
+import static com.facebook.presto.metadata.FunctionKind.SCALAR;
 import static com.facebook.presto.metadata.FunctionRegistry.getMagicLiteralFunctionSignature;
 import static com.facebook.presto.metadata.FunctionRegistry.mangleOperatorName;
 import static com.facebook.presto.metadata.FunctionRegistry.unmangleOperator;
+import static com.facebook.presto.metadata.Signature.typeVariable;
 import static com.facebook.presto.spi.type.HyperLogLogType.HYPER_LOG_LOG;
 import static com.facebook.presto.spi.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
 import static com.facebook.presto.spi.type.TypeSignature.parseTypeSignature;
 import static com.facebook.presto.type.TypeUtils.resolveTypes;
 import static com.facebook.presto.util.ImmutableCollectors.toImmutableList;
 import static com.google.common.collect.Lists.transform;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 public class TestFunctionRegistry
 {
@@ -135,6 +146,298 @@ public class TestFunctionRegistry
         assertTrue(names.contains("stddev"), "Expected function names " + names + " to contain 'stddev'");
         assertTrue(names.contains("rank"), "Expected function names " + names + " to contain 'rank'");
         assertFalse(names.contains("like"), "Expected function names " + names + " not to contain 'like'");
+    }
+
+    @Test
+    public void testResolveFunctionByExactMatch()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(functionSignature("bigint", "bigint"))
+                .forParameters("bigint", "bigint")
+                .returns(functionSignature("bigint", "bigint"));
+    }
+
+    @Test
+    public void testResolveTypeParametrizedFunction()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(functionSignature(ImmutableList.of("T", "T"), "boolean", ImmutableList.of(typeVariable("T"))))
+                .forParameters("bigint", "bigint")
+                .returns(functionSignature("bigint", "bigint"));
+    }
+
+    @Test
+    public void testResolveFunctionWithCoercion()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("decimal(p,s)", "double"),
+                        functionSignature("decimal(p,s)", "decimal(p,s)"),
+                        functionSignature("double", "double")
+                )
+                .forParameters("bigint", "bigint")
+                .returns(functionSignature("decimal(19,0)", "decimal(19,0)"));
+    }
+
+    @Test
+    public void testAmbiguousCallWithNoCoercion()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("decimal(p,s)", "decimal(p,s)"),
+                        functionSignature(ImmutableList.of("T", "T"), "boolean", ImmutableList.of(typeVariable("T")))
+                )
+                .forParameters("decimal(3,1)", "decimal(3,1)")
+                .returns(functionSignature("decimal(3,1)", "decimal(3,1)"));
+    }
+
+    @Test
+    public void testAmbiguousCallWithCoercion()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("decimal(p,s)", "double"),
+                        functionSignature("double", "decimal(p,s)")
+                )
+                .forParameters("bigint", "bigint")
+                .failsWithMessage("Could not choose a best candidate operator. Explicit type casts must be added.");
+    }
+
+    @Test
+    public void testResolveFunctionWithCoercionInTypes()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("array(decimal(p,s))", "array(double)"),
+                        functionSignature("array(decimal(p,s))", "array(decimal(p,s))"),
+                        functionSignature("array(double)", "array(double)")
+                )
+                .forParameters("array(bigint)", "array(bigint)")
+                .returns(functionSignature("array(decimal(19,0))", "array(decimal(19,0))"));
+    }
+
+    @Test
+    public void testResolveFunctionWithVariableArity()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("double", "double", "double"),
+                        functionSignature("decimal(p,s)").setVariableArity(true)
+                )
+                .forParameters("bigint", "bigint", "bigint")
+                .returns(functionSignature("decimal(19,0)", "decimal(19,0)", "decimal(19,0)"));
+
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("double", "double", "double"),
+                        functionSignature("bigint").setVariableArity(true)
+                )
+                .forParameters("bigint", "bigint", "bigint")
+                .returns(functionSignature("bigint", "bigint", "bigint"));
+    }
+
+    @Test
+    public void testResolveFunctionWithVariadicBound()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("bigint", "bigint", "bigint"),
+                        functionSignature(
+                                ImmutableList.of("T1", "T2", "T3"),
+                                "boolean",
+                                ImmutableList.of(Signature.withVariadicBound("T1", "decimal"),
+                                        Signature.withVariadicBound("T2", "decimal"),
+                                        Signature.withVariadicBound("T3", "decimal")))
+                )
+                .forParameters("unknown", "bigint", "bigint")
+                .returns(functionSignature("bigint", "bigint", "bigint"));
+    }
+
+    @Test
+    public void testResolveFunctionForUnknown()
+            throws Exception
+    {
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("bigint")
+                )
+                .forParameters("unknown")
+                .returns(functionSignature("bigint"));
+
+        // when coercion between the types exist, and the most specific function can be determined with the main algorithm
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("bigint"),
+                        functionSignature("integer")
+                )
+                .forParameters("unknown")
+                .returns(functionSignature("integer"));
+
+        // function that requires only unknown coercion must be preferred
+        assertThatResolveFunction()
+                .among(
+                        functionSignature("bigint", "bigint"),
+                        functionSignature("integer", "integer")
+                )
+                .forParameters("unknown", "bigint")
+                .returns(functionSignature("bigint", "bigint"));
+
+        // when coercion between the types doesn't exist, but the return type is the same, so the random function must be choosen
+        assertThatResolveFunction()
+                .among(
+                        functionSignature(ImmutableList.of("JoniRegExp"), "boolean"),
+                        functionSignature(ImmutableList.of("integer"), "boolean")
+                )
+                .forParameters("unknown")
+                // any function can be selected, but to make it deterministic we sort function signatures alphabetically
+                .returns(functionSignature("integer"));
+
+        // when the return type is different
+        assertThatResolveFunction()
+                .among(
+                        functionSignature(ImmutableList.of("JoniRegExp"), "JoniRegExp"),
+                        functionSignature(ImmutableList.of("integer"), "integer")
+                )
+                .forParameters("unknown")
+                .failsWithMessage("Could not choose a best candidate operator. Explicit type casts must be added.");
+    }
+
+    private SignatureBuilder functionSignature(String... argumentTypes)
+    {
+        return functionSignature(ImmutableList.copyOf(argumentTypes), "boolean");
+    }
+
+    private SignatureBuilder functionSignature(List<String> arguments, String returnType)
+    {
+        return functionSignature(arguments, returnType, ImmutableList.of());
+    }
+
+    private SignatureBuilder functionSignature(List<String> arguments, String returnType, List<TypeVariableConstraint> typeVariableConstraints)
+    {
+        ImmutableSet<String> literalParameters = ImmutableSet.of("p", "s", "p1", "s1", "p2", "s2", "p3", "s3");
+        List<TypeSignature> argumentSignatures = arguments.stream()
+                .map((signature) -> TypeSignature.parseTypeSignature(signature, literalParameters))
+                .collect(toImmutableList());
+        return new SignatureBuilder()
+                .returnType(TypeSignature.parseTypeSignature(returnType, literalParameters))
+                .argumentTypes(argumentSignatures)
+                .typeVariableConstraints(typeVariableConstraints)
+                .kind(SCALAR);
+    }
+
+    private static ResolveFunctionAssertion assertThatResolveFunction()
+    {
+        return new ResolveFunctionAssertion();
+    }
+
+    private static class ResolveFunctionAssertion
+    {
+        private static final String TEST_FUNCTION_NAME = "TEST_FUNCTION_NAME";
+
+        private final TypeRegistry typeRegistry = new TypeRegistry();
+        private final BlockEncodingSerde blockEncoding = new BlockEncodingManager(typeRegistry);
+
+        private List<SignatureBuilder> functionSignatures = ImmutableList.of();
+        private List<TypeSignature> parameterTypes = ImmutableList.of();
+
+        public ResolveFunctionAssertion among(SignatureBuilder... functionSignatures)
+        {
+            this.functionSignatures = ImmutableList.copyOf(functionSignatures);
+            return this;
+        }
+
+        public ResolveFunctionAssertion forParameters(String... parameters)
+        {
+            this.parameterTypes = parseTypeSignatures(parameters);
+            return this;
+        }
+
+        public ResolveFunctionAssertion returns(SignatureBuilder functionSignature)
+        {
+            Signature expectedSignature = functionSignature.name(TEST_FUNCTION_NAME).build();
+            Signature actualSignature = resolveSignature();
+            assertEquals(actualSignature, expectedSignature);
+            return this;
+        }
+
+        public ResolveFunctionAssertion failsWithMessage(String... messages)
+        {
+            try {
+                resolveSignature();
+                fail("didn't fail as expected");
+            }
+            catch (RuntimeException e) {
+                String actualMessage = e.getMessage();
+                for (String expectedMessage : messages) {
+                    if (!actualMessage.contains(expectedMessage)) {
+                        fail(format("%s doesn't contain %s", actualMessage, expectedMessage));
+                    }
+                }
+            }
+            return this;
+        }
+
+        private Signature resolveSignature()
+        {
+            FunctionRegistry functionRegistry = new FunctionRegistry(typeRegistry, blockEncoding, new FeaturesConfig().setExperimentalSyntaxEnabled(false));
+            functionRegistry.addFunctions(createFunctionsFromSignatures());
+            return functionRegistry.resolveFunction(QualifiedName.of(TEST_FUNCTION_NAME), parameterTypes, false);
+        }
+
+        private List<SqlFunction> createFunctionsFromSignatures()
+        {
+            ImmutableList.Builder<SqlFunction> functions = ImmutableList.builder();
+            for (SignatureBuilder functionSignature : functionSignatures) {
+                Signature signature = functionSignature.name(TEST_FUNCTION_NAME).build();
+                functions.add(new SqlScalarFunction(signature)
+                {
+                    @Override
+                    public ScalarFunctionImplementation specialize(
+                            BoundVariables boundVariables,
+                            int arity,
+                            TypeManager typeManager,
+                            FunctionRegistry functionRegistry)
+                    {
+                        return new ScalarFunctionImplementation(false, Collections.nCopies(arity, Boolean.FALSE), MethodHandles.identity(Void.class), true);
+                    }
+
+                    @Override
+                    public boolean isHidden()
+                    {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isDeterministic()
+                    {
+                        return false;
+                    }
+
+                    @Override
+                    public String getDescription()
+                    {
+                        return "testing function that does nothing";
+                    }
+                });
+            }
+            return functions.build();
+        }
+
+        private List<TypeSignature> parseTypeSignatures(String... signatures)
+        {
+            return ImmutableList.copyOf(signatures)
+                    .stream()
+                    .map(TypeSignature::parseTypeSignature)
+                    .collect(toList());
+        }
     }
 
     public static final class ScalarSum
