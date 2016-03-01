d49f6c615c70bf7338b0b054ad87c76c86865f51
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-03-01 12:01:19 +0100

    Rename property `query.max-age` to `query.min-expire-age`.
    This property old name is missleading, suggesting that something will
    definitly happen to query after reaching `query.max-age`.
    The truth is that the value desribes the minimal time after which the
    query MAY but musn't be dropped from history.
    So this time is minimum required in order something (query drop) to happen,
    yet reaching this age is not enough expire the query.
    
    More information about query expiration policy may be found in
    SqlQueryManager::removeExpiredQueries method.
    
    WARNING: This change may invalidate old configurations!

--- presto-main/src/test/java/com/facebook/presto/execution/TestQueryManagerConfig.java
+++ presto-main/src/test/java/com/facebook/presto/execution/TestQueryManagerConfig.java
@@ -27,7 +27,7 @@ public class TestQueryManagerConfig
     public void testDefaults()
     {
         ConfigAssertions.assertRecordedDefaults(ConfigAssertions.recordDefaults(QueryManagerConfig.class)
-                .setMaxQueryAge(new Duration(15, TimeUnit.MINUTES))
+                .setMinQueryExpireAge(new Duration(15, TimeUnit.MINUTES))
                 .setMaxQueryHistory(100)
                 .setClientTimeout(new Duration(5, TimeUnit.MINUTES))
                 .setScheduleSplitBatchSize(1000)
@@ -49,7 +49,7 @@ public class TestQueryManagerConfig
     {
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
                 .put("query.client.timeout", "10s")
-                .put("query.max-age", "30s")
+                .put("query.min-expire-age", "30s")
                 .put("query.max-history", "10")
                 .put("query.schedule-split-batch-size", "99")
                 .put("query.max-concurrent-queries", "10")
@@ -65,7 +65,7 @@ public class TestQueryManagerConfig
                 .build();
 
         QueryManagerConfig expected = new QueryManagerConfig()
-                .setMaxQueryAge(new Duration(30, TimeUnit.SECONDS))
+                .setMinQueryExpireAge(new Duration(30, TimeUnit.SECONDS))
                 .setMaxQueryHistory(10)
                 .setClientTimeout(new Duration(10, TimeUnit.SECONDS))
                 .setScheduleSplitBatchSize(99)
