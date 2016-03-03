prestodb/presto

commit 135626b793b42841c77f6b8c8b7fe80fac8ffc46

Fix failed task logging
Previously we would only record the failed task for a query, if it was
in the output stage

presto-main/src/main/java/com/facebook/presto/event/query/QueryMonitor.java
@@ -39,6 +39,7 @@ import javax.inject.Inject;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -100,15 +101,9 @@ public class QueryMonitor
                 }
             }
 
-            TaskInfo task = null;
-            StageInfo stageInfo = queryInfo.getOutputStage();
-            if (stageInfo != null) {
-                task = stageInfo.getTasks().stream()
-                        .filter(taskInfo -> taskInfo.getState() == TaskState.FAILED)
-                        .findFirst().orElse(null);
-            }
-            String failureHost = task == null ? null : task.getSelf().getHost();
-            String failureTask = task == null ? null : task.getTaskId().toString();
+            Optional<TaskInfo> task = findFailedTask(queryInfo.getOutputStage());
+            String failureHost = task.map(x -> x.getSelf().getHost()).orElse(null);
+            String failureTask = task.map(x -> x.getTaskId().toString()).orElse(null);
 
             eventClient.post(
                     new QueryCompletionEvent(
@@ -156,6 +151,19 @@ public class QueryMonitor
         catch (JsonProcessingException e) {
             throw Throwables.propagate(e);
         }
+    }
+
+    private static Optional<TaskInfo> findFailedTask(StageInfo stageInfo)
+    {
+        for (StageInfo subStage : stageInfo.getSubStages()) {
+            Optional<TaskInfo> task = findFailedTask(subStage);
+            if (task.isPresent()) {
+                return task;
+            }
+        }
+        return stageInfo.getTasks().stream()
+                .filter(taskInfo -> taskInfo.getState() == TaskState.FAILED)
+                .findFirst();
     }
 
     private void logQueryTimeline(QueryInfo queryInfo)

commit 0498c0df5aff14c9f7f88db1fe2e43ed3470aa5b

Fix NPE when query fails during parsing/planning
Commit 135626b793b42841c77f6b8c8b7fe80fac8ffc46 introduced a bug
where queries that fail during parsing/analysis/planning
throw an NPE when the completion event is processed.

This is due to an incorrect assumption that QueryInfos always
have an output stage, which is not true for queries that fail
before execution starts.

presto-main/src/main/java/com/facebook/presto/event/query/QueryMonitor.java
@@ -155,6 +155,10 @@ public class QueryMonitor
 
     private static Optional<TaskInfo> findFailedTask(StageInfo stageInfo)
     {
+        if (stageInfo == null) {
+            return Optional.empty();
+        }
+
         for (StageInfo subStage : stageInfo.getSubStages()) {
             Optional<TaskInfo> task = findFailedTask(subStage);
             if (task.isPresent()) {