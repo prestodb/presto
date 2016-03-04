b1c5aa04d49059d17a21b22f15ab31987d768512
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-03-04 17:11:34 +0100

    Add decimal columns to INSERT product tests

--- presto-product-tests/src/main/resources/sql-tests/testcases/insert/insert_explicit_null.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/insert/insert_explicit_null.sql
@@ -1,7 +1,7 @@
 -- database: presto; groups: insert; mutable_tables: datatype|created; tables: datatype
 -- delimiter: |;
 --!
-insert into ${mutableTables.hive.datatype} values (null, null, null, null, null, null);
+insert into ${mutableTables.hive.datatype} values (null, null, null, null, null, null, null, null);
 select * from ${mutableTables.hive.datatype}
 --!
-null|null|null|null|null|null|
+null|null|null|null|null|null|null|null|
