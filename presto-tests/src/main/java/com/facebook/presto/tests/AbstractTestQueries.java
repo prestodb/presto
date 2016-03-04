567b87925a96b97a1bd9280f67d5f1cd4460e4c4
harunurhan <harunurhan17@gmail.com>
2016-03-04 22:35:30 +0200

    Prune unused columns from VALUES

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -5835,6 +5835,19 @@ public abstract class AbstractTestQueries
     }
 
     @Test
+    public void testValuesWithUnusedColumns()
+            throws Exception
+    {
+        MaterializedResult actual = computeActual("SELECT foo from (values (1, 2)) a(foo, bar)");
+
+        MaterializedResult expected = resultBuilder(getSession(), actual.getTypes())
+                .row(1)
+                .build();
+
+        assertEquals(actual.getMaterializedRows(), expected.getMaterializedRows());
+    }
+
+    @Test
     public void testFilterPushdownWithAggregation()
             throws Exception
     {
