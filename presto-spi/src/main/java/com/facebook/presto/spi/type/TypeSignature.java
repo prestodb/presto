87c65b7bf65a2602c30ef36eb3936b8d17e9b2fd
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-03-11 17:23:54 +0100

    parseTypeSignature automatically detects expressions
    This commits extends type signature parser to handle
    literal computations which may be passed as arguments.
    Currently we need to determine the types of all tokens just after
    parsing type signature.
    Doing proper semantic analysis would be better solution but would
    require a lot of changes to the codebase.
    
    For now we assume that any token which contains arithmetic ('+', '-',
    '/', '*') or is call min() or max() function is not a type parameter but
    literal calculation parameter.
    
    This is just intermediate step. Later we change codebase so literal
    calculations are not allowed directly as parameters to parsed type signatures.
    We require the calculation  be first bound to variables and stored in function context.
    And only allow bound variables as literal parameters.

--- presto-spi/src/main/java/com/facebook/presto/spi/type/TypeSignature.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/type/TypeSignature.java
@@ -26,6 +26,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableList;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toList;
@@ -35,6 +36,11 @@ public class TypeSignature
     private final String base;
     private final List<TypeSignatureParameter> parameters;
     private final boolean calculated;
+
+    public TypeSignature(String base, TypeSignatureParameter... parameters)
+    {
+        this(base, asList(parameters));
+    }
 
     public TypeSignature(String base, List<TypeSignatureParameter> parameters)
     {
@@ -253,7 +259,10 @@ public class TypeSignature
         if (Character.isDigit(signature.charAt(begin))) {
             return TypeSignatureParameter.of(Long.parseLong(parameterName));
         }
-        else if (literalCalculationParameters.contains(parameterName)) {
+        else if (literalCalculationParameters.contains(parameterName) ||
+                parameterName.matches(".*[/\\+\\-\\*].*") ||
+                parameterName.contains("min") ||
+                parameterName.contains("max")) {
             return TypeSignatureParameter.of(new TypeLiteralCalculation(parameterName));
         }
         else {
