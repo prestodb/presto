3e4bd47662bd10a799828a7470a10d469a4b3073
Yang Yang <yangy@fb.com>
2016-03-17 10:38:15 -0700

    Allow time and timestamp without time zone in AT TIME ZONE
    This commit fixed the bugs in ExpressionAnalyzer and DesugaringRewriter:
    1. AT TIME ZONE should allow time and timestamp with or without time
    zone as its input while the old ExpressionAnalyzer only allows time and
    timestamp with time zone.
    2. when rewriting AtTimeZone, value with time must be cast to time with
    time zone, and value with timestamp must be cast to timestamp with time
    zone.
    
    Since AtTimeZone is being rewritten after planning, AtTimeZone could not
    be compiled and executed without planning. Move the AtTimeZone tests
    from TestDateTimeFunctions to AbstractTestQueries in which the queries
    could be exeucted by the full engine.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/DesugaringRewriter.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/DesugaringRewriter.java
@@ -13,7 +13,9 @@
  */
 package com.facebook.presto.sql.planner;
 
+import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.sql.tree.AtTimeZone;
+import com.facebook.presto.sql.tree.Cast;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.ExpressionRewriter;
 import com.facebook.presto.sql.tree.ExpressionTreeRewriter;
@@ -21,14 +23,38 @@ import com.facebook.presto.sql.tree.FunctionCall;
 import com.facebook.presto.sql.tree.QualifiedName;
 import com.google.common.collect.ImmutableList;
 
+import java.util.IdentityHashMap;
+
+import static com.facebook.presto.spi.type.TimeType.TIME;
+import static com.facebook.presto.spi.type.TimeWithTimeZoneType.TIME_WITH_TIME_ZONE;
+import static com.facebook.presto.spi.type.TimestampType.TIMESTAMP;
+import static com.facebook.presto.spi.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
+import static java.util.Objects.requireNonNull;
+
 public class DesugaringRewriter
         extends ExpressionRewriter<Void>
 {
+    private final IdentityHashMap<Expression, Type> expressionTypes;
+
+    public DesugaringRewriter(IdentityHashMap<Expression, Type> expressionTypes)
+    {
+        this.expressionTypes = requireNonNull(expressionTypes, "expressionTypes is null");
+    }
+
     @Override
     public Expression rewriteAtTimeZone(AtTimeZone node, Void context, ExpressionTreeRewriter<Void> treeRewriter)
     {
+        Expression value = treeRewriter.rewrite(node.getValue(), context);
+        Type type = expressionTypes.get(value);
+        if (type.equals(TIME)) {
+            value = new Cast(value, TIME_WITH_TIME_ZONE.getDisplayName());
+        }
+        else if (type.equals(TIMESTAMP)) {
+            value = new Cast(value, TIMESTAMP_WITH_TIME_ZONE.getDisplayName());
+        }
+
         return new FunctionCall(QualifiedName.of("at_timezone"), ImmutableList.of(
-                treeRewriter.rewrite(node.getValue(), context),
+                value,
                 treeRewriter.rewrite(node.getTimeZone(), context)));
     }
 }
