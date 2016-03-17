356145d895c0fc042958cb64283d758de6b30722
Karol Sobczak <Karol.Sobczak@Teradata.com>
2016-03-17 09:51:36 +0100

    Add DECIMAL casting functions

--- presto-main/src/test/java/com/facebook/presto/type/TestRowOperators.java
+++ presto-main/src/test/java/com/facebook/presto/type/TestRowOperators.java
@@ -75,6 +75,9 @@ public class TestRowOperators
         // test_row has both (boolean, boolean) and (boolean, array<bigint>), so this method is non-deterministic
         // assertFunction("test_row(TRUE, NULL).col1", BOOLEAN, null);
 
+        // test_row has both (boolean, boolean) and (boolean, array<bigint>) so this method is non-deterministic
+        // assertFunction("test_row(TRUE, NULL).col1", BOOLEAN, null);
+
         assertFunction("test_row(1, CAST(NULL AS DOUBLE)).col1", DOUBLE, null);
         assertFunction("test_row(TRUE, CAST(NULL AS BOOLEAN)).col1", BOOLEAN, null);
         assertFunction("test_row(TRUE, CAST(NULL AS ARRAY<BIGINT>)).col1", new ArrayType(BIGINT), null);
