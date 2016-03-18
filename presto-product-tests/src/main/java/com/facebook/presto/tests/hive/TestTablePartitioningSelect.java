afbae375855174847dcc6f4ac26cdab6b13c8c3f
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-03-18 18:33:37 +0100

    Unquarantine TestTablePartitioningSelect
    This test was failling because of the resources filtering issue

--- presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestTablePartitioningSelect.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestTablePartitioningSelect.java
@@ -28,7 +28,6 @@ import java.sql.SQLException;
 import java.util.Optional;
 
 import static com.facebook.presto.tests.TestGroups.HIVE_CONNECTOR;
-import static com.facebook.presto.tests.TestGroups.QUARANTINE;
 import static com.teradata.tempto.Requirements.allOf;
 import static com.teradata.tempto.assertions.QueryAssert.Row.row;
 import static com.teradata.tempto.assertions.QueryAssert.assertThat;
@@ -87,7 +86,7 @@ public class TestTablePartitioningSelect
         );
     }
 
-    @Test(groups = {HIVE_CONNECTOR, QUARANTINE})
+    @Test(groups = {HIVE_CONNECTOR})
     public void testSelectPartitionedHiveTableDifferentFormats()
             throws SQLException
     {
