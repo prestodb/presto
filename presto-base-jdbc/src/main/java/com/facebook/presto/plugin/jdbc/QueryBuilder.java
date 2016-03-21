b6d4d0d28f0ae8ddbfbf84ddc080f91f17d3e800
David Phillips <david@acz.org>
2016-03-21 10:47:10 -0700

    Convert JDBC QueryBuilder to stream API

--- presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/QueryBuilder.java
+++ presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/QueryBuilder.java
@@ -34,8 +34,9 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static com.google.common.collect.Iterables.getOnlyElement;
-import static com.google.common.collect.Iterables.transform;
+import static java.util.Collections.nCopies;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.joining;
 
 public class QueryBuilder
 {
@@ -73,8 +74,13 @@ public class QueryBuilder
     {
         StringBuilder sql = new StringBuilder();
 
+        String columnNames = columns.stream()
+                .map(JdbcColumnHandle::getColumnName)
+                .map(this::quote)
+                .collect(joining(", "));
+
         sql.append("SELECT ");
-        Joiner.on(", ").appendTo(sql, transform(columns, column -> quote(column.getColumnName())));
+        sql.append(columnNames);
         if (columns.isEmpty()) {
             sql.append("null");
         }
@@ -198,7 +204,11 @@ public class QueryBuilder
             disjuncts.add(toPredicate(columnName, "=", getOnlyElement(singleValues), type, accumulator));
         }
         else if (singleValues.size() > 1) {
-            disjuncts.add(quote(columnName) + " IN (" + Joiner.on(",").join(transform(singleValues, value -> bindValue(value, type, accumulator))) + ")");
+            for (Object value : singleValues) {
+                bindValue(value, type, accumulator);
+            }
+            String values = Joiner.on(",").join(nCopies(singleValues.size(), "?"));
+            disjuncts.add(quote(columnName) + " IN (" + values + ")");
         }
 
         // Add nullability disjuncts
@@ -212,7 +222,8 @@ public class QueryBuilder
 
     private String toPredicate(String columnName, String operator, Object value, Type type, List<TypeAndValue> accumulator)
     {
-        return quote(columnName) + " " + operator + " " + bindValue(value, type, accumulator);
+        bindValue(value, type, accumulator);
+        return quote(columnName) + " " + operator + " ?";
     }
 
     private String quote(String name)
@@ -221,10 +232,9 @@ public class QueryBuilder
         return quote + name + quote;
     }
 
-    private static String bindValue(Object value, Type type, List<TypeAndValue> accumulator)
+    private static void bindValue(Object value, Type type, List<TypeAndValue> accumulator)
     {
         checkArgument(isAcceptedType(type), "Can't handle type: %s", type);
         accumulator.add(new TypeAndValue(type, value));
-        return "?";
     }
 }
