bbd57719b279e87af612e7272f251bbf2826a2d3
David Phillips <david@acz.org>
2016-03-21 21:26:09 -0700

    Add retry for inserting created shards

--- presto-raptor/src/main/java/com/facebook/presto/raptor/metadata/DatabaseShardRecorder.java
+++ presto-raptor/src/main/java/com/facebook/presto/raptor/metadata/DatabaseShardRecorder.java
@@ -14,14 +14,23 @@
 package com.facebook.presto.raptor.metadata;
 
 import com.facebook.presto.raptor.util.DaoSupplier;
+import com.facebook.presto.spi.PrestoException;
+import io.airlift.log.Logger;
 
 import javax.inject.Inject;
 
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static com.facebook.presto.raptor.util.DatabaseUtil.metadataError;
+import static com.facebook.presto.raptor.util.DatabaseUtil.runIgnoringConstraintViolation;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class DatabaseShardRecorder
         implements ShardRecorder
 {
+    private static final Logger log = Logger.get(DatabaseShardRecorder.class);
+
     private final ShardDao dao;
 
     @Inject
@@ -33,6 +42,24 @@ public class DatabaseShardRecorder
     @Override
     public void recordCreatedShard(long transactionId, UUID shardUuid)
     {
-        dao.insertCreatedShard(shardUuid, transactionId);
+        int maxAttempts = 5;
+        for (int attempt = 1; attempt <= maxAttempts; attempt++) {
+            try {
+                runIgnoringConstraintViolation(() -> dao.insertCreatedShard(shardUuid, transactionId));
+            }
+            catch (PrestoException e) {
+                if (attempt == maxAttempts) {
+                    throw e;
+                }
+                log.warn(e, "Failed to insert created shard on attempt %s, will retry", attempt);
+                try {
+                    long millis = attempt * 2000L;
+                    MILLISECONDS.sleep(millis + ThreadLocalRandom.current().nextLong(0, millis));
+                }
+                catch (InterruptedException ie) {
+                    throw metadataError(ie);
+                }
+            }
+        }
     }
 }
