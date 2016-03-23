acbd4dd7bf2543cc16eb28208984c3a27c50c33c
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-03-23 09:43:44 +0100

    Fix multiple_inserts_one_table product test
    Explicit decimal cast is required. Trailing zeros are stripped.

--- presto-product-tests/src/main/resources/sql-tests/testcases/insert/multiple_inserts_one_table.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/insert/multiple_inserts_one_table.sql
@@ -3,7 +3,7 @@
 --!
 insert into ${mutableTables.hive.datatype} values(1, cast(2.1 as double), 'abc', cast('2014-01-01' as date), cast('2015-01-01 03:15:16 UTC' as timestamp), FALSE, DECIMAL '-999.99', DECIMAL '-99999999999999999999.9999999999');
 insert into ${mutableTables.hive.datatype} values(1, cast(2.1 as double), 'abc', cast('2014-01-01' as date), cast('2015-01-01 03:15:16 UTC' as timestamp), FALSE, DECIMAL '999.99', DECIMAL '99999999999999999999.9999999999');
-insert into ${mutableTables.hive.datatype} values(1, cast(2.1 as double), 'abc', cast('2014-01-01' as date), cast('2015-01-01 03:15:16 UTC' as timestamp), FALSE, DECIMAL '000.00', DECIMAL '00000000000000000000.0000000000');
+insert into ${mutableTables.hive.datatype} values(1, cast(2.1 as double), 'abc', cast('2014-01-01' as date), cast('2015-01-01 03:15:16 UTC' as timestamp), FALSE, CAST(0 AS DECIMAL(5,2)), CAST(0 AS DECIMAL(30,10)));
 select * from ${mutableTables.hive.datatype}
 --!
 1|2.1|abc|2014-01-01|2015-01-01 03:15:16|false|-999.99|-99999999999999999999.9999999999|
