0bcf5533356398e5b9dcd16429583ca9c15fe810
Nezih Yigitbasi <nyigitbasi@netflix.com>
2016-03-25 23:39:26 -0700

    Use hash code operator for hashing array elements

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayHashCodeOperator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/ArrayHashCodeOperator.java
@@ -26,15 +26,19 @@ import java.util.Map;
 
 import static com.facebook.presto.metadata.OperatorType.HASH_CODE;
 import static com.facebook.presto.metadata.Signature.comparableTypeParameter;
+import static com.facebook.presto.metadata.Signature.internalOperator;
+import static com.facebook.presto.operator.scalar.CombineHashFunction.getHash;
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.facebook.presto.type.ArrayType.ARRAY_NULL_ELEMENT_MSG;
 import static com.facebook.presto.type.TypeUtils.checkElementNotNull;
+import static com.facebook.presto.type.TypeUtils.hashPosition;
 import static com.facebook.presto.util.Reflection.methodHandle;
 
 public class ArrayHashCodeOperator
         extends SqlOperator
 {
     public static final ArrayHashCodeOperator ARRAY_HASH_CODE = new ArrayHashCodeOperator();
-    public static final MethodHandle METHOD_HANDLE = methodHandle(ArrayHashCodeOperator.class, "hash", Type.class, Block.class);
+    public static final MethodHandle METHOD_HANDLE = methodHandle(ArrayHashCodeOperator.class, "hash", MethodHandle.class, Type.class, Block.class);
 
     private ArrayHashCodeOperator()
     {
@@ -45,16 +49,16 @@ public class ArrayHashCodeOperator
     public ScalarFunctionImplementation specialize(Map<String, Type> types, int arity, TypeManager typeManager, FunctionRegistry functionRegistry)
     {
         Type type = types.get("T");
-        return new ScalarFunctionImplementation(false, ImmutableList.of(false), METHOD_HANDLE.bindTo(type), isDeterministic());
+        MethodHandle hashCodeFunction = functionRegistry.getScalarFunctionImplementation(internalOperator(HASH_CODE, BIGINT, ImmutableList.of(type))).getMethodHandle();
+        return new ScalarFunctionImplementation(false, ImmutableList.of(false), METHOD_HANDLE.bindTo(hashCodeFunction).bindTo(type), isDeterministic());
     }
 
-    public static long hash(Type type, Block block)
+    public static long hash(MethodHandle hashCodeFunction, Type type, Block block)
     {
         int hash = 0;
         for (int i = 0; i < block.getPositionCount(); i++) {
             checkElementNotNull(block.isNull(i), ARRAY_NULL_ELEMENT_MSG);
-            // TODO: This should be migrated away from Type.hash and invoke a MethodHandle for the hash code operator instead
-            hash = (int) CombineHashFunction.getHash(hash, type.hash(block, i));
+            hash = (int) getHash(hash, hashPosition(hashCodeFunction, type, block, i));
         }
         return hash;
     }
