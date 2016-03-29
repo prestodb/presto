65673ea3f15a487ff241cac5debfc2dbfee27569
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-03-29 09:02:55 +0200

    Serialize aggregation States to VARBINARY
    Serialize to VARBINARY instead of VARCHAR for correct encoding in spiller

--- presto-main/src/main/java/com/facebook/presto/operator/aggregation/state/StateCompiler.java
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/state/StateCompiler.java
@@ -77,7 +77,7 @@ import static com.facebook.presto.operator.aggregation.state.StateCompilerUtils.
 import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.facebook.presto.spi.type.BooleanType.BOOLEAN;
 import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
-import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
+import static com.facebook.presto.spi.type.VarbinaryType.VARBINARY;
 import static com.facebook.presto.sql.gen.SqlTypeBytecodeExpression.constantType;
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
@@ -158,7 +158,7 @@ public class StateCompiler
 
         Type type;
         if (fields.size() > 1) {
-            type = VARCHAR;
+            type = VARBINARY;
         }
         else {
             Class<?> stackType = fields.get(0).getType();
@@ -175,7 +175,7 @@ public class StateCompiler
                 type = BIGINT;
             }
             else if (stackType == Slice.class) {
-                type = VARCHAR;
+                type = VARBINARY;
             }
             else {
                 throw new IllegalArgumentException("Unsupported type: " + stackType);
