f6a1a16c312a841dd3fefff7d3a58bbc56007f9d
Maciej Grzybek <maciej.grzybek@teradata.com>
2016-03-29 13:24:17 -0400

    Update product test to reflect case insensitive column rename
    This fixes 394637c851f613d1b8510074bb63b5e426f3fece

--- presto-product-tests/src/main/java/com/facebook/presto/tests/AlterTableTests.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/AlterTableTests.java
@@ -20,6 +20,7 @@ import org.testng.annotations.Test;
 
 import static com.facebook.presto.tests.TestGroups.ALTER_TABLE;
 import static com.facebook.presto.tests.TestGroups.SMOKE;
+import static com.teradata.tempto.assertions.QueryAssert.Row.row;
 import static com.teradata.tempto.assertions.QueryAssert.assertThat;
 import static com.teradata.tempto.query.QueryExecutor.query;
 import static com.teradata.tempto.query.QueryType.UPDATE;
@@ -32,20 +33,70 @@ public class AlterTableTests
     @Test(groups = {ALTER_TABLE, SMOKE})
     public void renameTable()
     {
-        String tableName = "to_be_renamed_table_name";
+        String tableName = "to_be_renamed";
         String renamedTableName = "renamed_table_name";
+
         query(format("DROP TABLE IF EXISTS %s", tableName));
         query(format("DROP TABLE IF EXISTS %s", renamedTableName));
 
-        query(format("CREATE TABLE %s AS SELECT * FROM nation", tableName));
-        assertThat(query(format("ALTER TABLE %s RENAME TO %s", tableName, renamedTableName), UPDATE))
-                .hasRowsCount(1);
+        try {
+            query(format("CREATE TABLE %s AS SELECT * FROM nation", tableName));
 
-        assertThat(query(format("SELECT * FROM %s", renamedTableName)))
-                .hasRowsCount(25);
+            assertThat(query(format("ALTER TABLE %s RENAME TO %s", tableName, renamedTableName), UPDATE))
+                    .hasRowsCount(1);
 
-        // rename back to original name
-        assertThat(query(format("ALTER TABLE %s RENAME TO %s", renamedTableName, tableName), UPDATE))
-                .hasRowsCount(1);
+            assertThat(query(format("SELECT * FROM %s", renamedTableName)))
+                    .hasRowsCount(25);
+
+            // rename back to original name
+            assertThat(query(format("ALTER TABLE %s RENAME TO %s", renamedTableName, tableName), UPDATE))
+                    .hasRowsCount(1);
+        }
+        finally {
+            query(format("DROP TABLE %s", tableName));
+        }
+    }
+
+    @Test(groups = {ALTER_TABLE, SMOKE})
+    public void renameColumn()
+    {
+        String tableName = "tableName";
+        try {
+            query(format("CREATE TABLE %s AS SELECT * FROM nation", tableName));
+            assertThat(query(format("ALTER TABLE %s RENAME COLUMN n_nationkey TO nationkey", tableName), UPDATE))
+                    .hasRowsCount(1);
+            assertThat(query(format("SELECT count(nationkey) FROM %s", tableName)))
+                    .containsExactly(row(25));
+            assertThat(() -> query(format("ALTER TABLE %s RENAME COLUMN nationkey TO nATIoNkEy", tableName)))
+                    .failsWithMessage("Column 'nationkey' already exists");
+            assertThat(() -> query(format("ALTER TABLE %s RENAME COLUMN nationkey TO n_regionkeY", tableName)))
+                    .failsWithMessage("Column 'n_regionkey' already exists");
+
+            assertThat(query(format("ALTER TABLE %s RENAME COLUMN nationkey TO n_nationkey", tableName)));
+        }
+        finally {
+            query(format("DROP TABLE %s", tableName));
+        }
+    }
+
+    @Test(groups = {ALTER_TABLE, SMOKE})
+    public void addColumn()
+    {
+        String tableName = "tableName";
+        try {
+            query(format("CREATE TABLE %s AS SELECT * FROM nation", tableName));
+
+            assertThat(query(format("SELECT count(1) FROM %s", tableName)))
+                    .containsExactly(row(25));
+            assertThat(query(format("ALTER TABLE %s ADD COLUMN some_new_column BIGINT", tableName)))
+                    .hasRowsCount(1);
+            assertThat(() -> query(format("ALTER TABLE %s ADD COLUMN n_nationkey BIGINT", tableName)))
+                    .failsWithMessage("Column 'n_nationkey' already exists");
+            assertThat(() -> query(format("ALTER TABLE %s ADD COLUMN n_naTioNkEy BIGINT", tableName)))
+                    .failsWithMessage("Column 'n_naTioNkEy' already exists");
+        }
+        finally {
+            query(format("DROP TABLE %s", tableName));
+        }
     }
 }
