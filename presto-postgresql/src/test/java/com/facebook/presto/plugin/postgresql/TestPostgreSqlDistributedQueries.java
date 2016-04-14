b552b76c45206e81de5dc2051062483c7eb7bd81
Karol Sobczak <karol.sobczak@karolsobczak.com>
2016-04-14 12:02:07 +0200

    Varchar(x) support in JDBC

--- presto-postgresql/src/test/java/com/facebook/presto/plugin/postgresql/TestPostgreSqlDistributedQueries.java
+++ presto-postgresql/src/test/java/com/facebook/presto/plugin/postgresql/TestPostgreSqlDistributedQueries.java
@@ -13,6 +13,8 @@
  */
 package com.facebook.presto.plugin.postgresql;
 
+import com.facebook.presto.testing.MaterializedResult;
+import com.facebook.presto.testing.MaterializedRow;
 import com.facebook.presto.tests.AbstractTestQueries;
 import io.airlift.testing.postgresql.TestingPostgreSqlServer;
 import io.airlift.tpch.TpchTable;
@@ -26,7 +28,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import static com.facebook.presto.plugin.postgresql.PostgreSqlQueryRunner.createPostgreSqlQueryRunner;
+import static com.facebook.presto.spi.type.VarcharType.createUnboundedVarcharType;
+import static com.facebook.presto.spi.type.VarcharType.createVarcharType;
+import static com.google.common.collect.Iterables.getOnlyElement;
 import static io.airlift.testing.Closeables.closeAllRuntimeException;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
@@ -76,6 +82,51 @@ public class TestPostgreSqlDistributedQueries
         assertQuery("SELECT orderkey FROM test_view", "SELECT orderkey FROM orders");
 
         execute("DROP VIEW IF EXISTS tpch.test_view");
+    }
+
+    @Test
+    public void testPrestoCreatedParameterizedVarchar()
+            throws Exception
+    {
+        assertUpdate("CREATE TABLE presto_test_parameterized_varchar AS SELECT " +
+                "CAST('a' AS varchar(255)) text_a," +
+                "CAST('b' AS varchar(20000)) text_b," +
+                "CAST('unbounded' AS varchar) text_unbounded", 1);
+        assertTrue(queryRunner.tableExists(getSession(), "presto_test_parameterized_varchar"));
+        assertTableColumnNames("presto_test_parameterized_varchar", "text_a", "text_b", "text_unbounded");
+
+        MaterializedResult materializedRows = computeActual("SELECT * from presto_test_parameterized_varchar");
+        assertEquals(materializedRows.getTypes().get(0), createVarcharType(255));
+        assertEquals(materializedRows.getTypes().get(1), createVarcharType(20000));
+        assertEquals(materializedRows.getTypes().get(2), createUnboundedVarcharType());
+
+        MaterializedRow row = getOnlyElement(materializedRows);
+        assertEquals(row.getField(0), "a");
+        assertEquals(row.getField(1), "b");
+        assertEquals(row.getField(2), "unbounded");
+        assertUpdate("DROP TABLE presto_test_parameterized_varchar");
+    }
+
+    @Test
+    public void testPostgreSqlCreatedParameterizedVarchar()
+            throws Exception
+    {
+        execute("CREATE TABLE tpch.postgresql_test_parameterized_varchar (" +
+                "text_a varchar(32), " +
+                "text_b varchar(20000), " +
+                "text_c text)");
+        execute("INSERT INTO tpch.postgresql_test_parameterized_varchar VALUES('a', 'b', 'c')");
+
+        MaterializedResult materializedRows = computeActual("SELECT * from postgresql_test_parameterized_varchar");
+        assertEquals(materializedRows.getTypes().get(0), createVarcharType(32));
+        assertEquals(materializedRows.getTypes().get(1), createVarcharType(20000));
+        assertEquals(materializedRows.getTypes().get(2), createUnboundedVarcharType());
+
+        MaterializedRow row = getOnlyElement(materializedRows);
+        assertEquals(row.getField(0), "a");
+        assertEquals(row.getField(1), "b");
+        assertEquals(row.getField(2), "c");
+        assertUpdate("DROP TABLE postgresql_test_parameterized_varchar");
     }
 
     private void execute(String sql)
