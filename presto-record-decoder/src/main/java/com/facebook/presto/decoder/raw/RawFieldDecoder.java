b12546497a661b01ab34e85ff26ab9dc502a0252
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-04-21 15:09:46 +0200

    Add VARCHAR truncating to TestRawDecoder

--- presto-record-decoder/src/main/java/com/facebook/presto/decoder/raw/RawFieldDecoder.java
+++ presto-record-decoder/src/main/java/com/facebook/presto/decoder/raw/RawFieldDecoder.java
@@ -17,6 +17,7 @@ import com.facebook.presto.decoder.DecoderColumnHandle;
 import com.facebook.presto.decoder.FieldDecoder;
 import com.facebook.presto.decoder.FieldValueProvider;
 import com.facebook.presto.spi.PrestoException;
+import com.facebook.presto.spi.type.Varchars;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
@@ -220,7 +221,11 @@ public class RawFieldDecoder
             }
 
             if (fieldType == FieldType.BYTE) {
-                return Slices.wrappedBuffer(value.slice());
+                Slice slice = Slices.wrappedBuffer(value.slice());
+                if (Varchars.isVarcharType(columnHandle.getType())) {
+                    slice = Varchars.truncateToLength(slice, columnHandle.getType());
+                }
+                return slice;
             }
 
             throw new PrestoException(DECODER_CONVERSION_NOT_SUPPORTED, format("conversion %s to Slice not supported", fieldType));
