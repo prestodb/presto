e11ff93a70ebab575bdafb11f8b095aaa9789352
Raghav Sethi <raghavsethi@fb.com>
2016-04-20 21:38:15 -0700

    Fix bad type info in window function and aggregate
    We should not have been using getTypeWithCoercions on those
    callsites.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/QueryPlanner.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/QueryPlanner.java
@@ -388,7 +388,7 @@ class QueryPlanner
         boolean needPostProjectionCoercion = false;
         for (FunctionCall aggregate : analysis.getAggregates(node)) {
             Expression rewritten = subPlan.rewrite(aggregate);
-            Symbol newSymbol = symbolAllocator.newSymbol(rewritten, analysis.getTypeWithCoercions(aggregate));
+            Symbol newSymbol = symbolAllocator.newSymbol(rewritten, analysis.getType(aggregate));
 
             // TODO: this is a hack, because we apply coercions to the output of expressions, rather than the arguments to expressions.
             // Therefore we can end up with this implicit cast, and have to move it into a post-projection
@@ -567,7 +567,7 @@ class QueryPlanner
 
             // Rewrite function call in terms of pre-projected inputs
             Expression rewritten = subPlan.rewrite(windowFunction);
-            Symbol newSymbol = symbolAllocator.newSymbol(rewritten, analysis.getTypeWithCoercions(windowFunction));
+            Symbol newSymbol = symbolAllocator.newSymbol(rewritten, analysis.getType(windowFunction));
 
             boolean needCoercion = rewritten instanceof Cast;
             // Strip out the cast and add it back as a post-projection
