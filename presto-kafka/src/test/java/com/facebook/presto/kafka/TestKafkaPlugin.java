9c546283d8c6f7393d5d01c5ab7acc651ae178df
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-04-22 18:49:21 +0200

    Refactor TypeRegistry
    Make all the methods from TypeRegistry to operate on Type
    instead of TypeSignature. Make `coerceTypeBase` method public,
    and use it in SignatureBinder instead of `getCommonSupperType`
    and `canCoerce` that were operation on TypeSignature.

--- presto-kafka/src/test/java/com/facebook/presto/kafka/TestKafkaPlugin.java
+++ presto-kafka/src/test/java/com/facebook/presto/kafka/TestKafkaPlugin.java
@@ -104,6 +104,12 @@ public class TestKafkaPlugin
         {
             return Optional.empty();
         }
+
+        @Override
+        public Optional<Type> coerceTypeBase(Type sourceType, String resultTypeBase)
+        {
+            throw new UnsupportedOperationException();
+        }
     }
 
     private static class TestingNodeManager
