5b7a0dba1553e5ea03451fc62317876c55c6f563
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-04-26 15:23:40 +0200

    Allow implicit conversions from INTEGER to DECIMAL

--- presto-main/src/main/java/com/facebook/presto/type/TypeRegistry.java
+++ presto-main/src/main/java/com/facebook/presto/type/TypeRegistry.java
@@ -352,6 +352,8 @@ public final class TypeRegistry
                         return Optional.of(BIGINT);
                     case StandardTypes.DOUBLE:
                         return Optional.of(DOUBLE);
+                    case StandardTypes.DECIMAL:
+                        return Optional.of(createDecimalType(10, 0));
                     default:
                         return Optional.empty();
                 }
