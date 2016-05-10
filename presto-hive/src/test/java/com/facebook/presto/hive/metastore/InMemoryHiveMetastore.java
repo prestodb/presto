b4cef872f8429ea78bc13f1ac2b3fa5eb3723d3a
David Phillips <david@acz.org>
2016-05-09 18:28:03 -0700

    Fix Hive distributed tests for alter table

--- presto-hive/src/test/java/com/facebook/presto/hive/metastore/InMemoryHiveMetastore.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/metastore/InMemoryHiveMetastore.java
@@ -170,7 +170,7 @@ public class InMemoryHiveMetastore
 
         // if the name did not change, this is a simple schema change
         if (oldName.equals(newName)) {
-            if (relations.replace(oldName, newTable) != null) {
+            if (relations.replace(oldName, newTable) == null) {
                 throw new TableNotFoundException(oldName);
             }
             return;
