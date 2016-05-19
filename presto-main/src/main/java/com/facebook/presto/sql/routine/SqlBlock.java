dain/facebook-presto

Add initial stored routine compiler

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlBlock.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class SqlBlock
+        implements SqlStatement
+{
+    private final Optional<SqlLabel> label;
+    private final List<SqlVariable> variables;
+    private final List<SqlStatement> statements;
+
+    public SqlBlock(List<SqlVariable> variables, List<SqlStatement> statements)
+    {
+        this(Optional.empty(), variables, statements);
+    }
+
+    public SqlBlock(Optional<SqlLabel> label, List<SqlVariable> variables, List<SqlStatement> statements)
+    {
+        this.label = checkNotNull(label, "label is null");
+        this.variables = ImmutableList.copyOf(checkNotNull(variables, "variables is null"));
+        this.statements = ImmutableList.copyOf(checkNotNull(statements, "statements is null"));
+    }
+
+    public Optional<SqlLabel> getLabel()
+    {
+        return label;
+    }
+
+    public List<SqlVariable> getVariables()
+    {
+        return variables;
+    }
+
+    public List<SqlStatement> getStatements()
+    {
+        return statements;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitBlock(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlBreak.java
@@ -1,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class SqlBreak
+        implements SqlStatement
+{
+    private final SqlLabel target;
+
+    public SqlBreak(SqlLabel target)
+    {
+        this.target = checkNotNull(target, "target is null");
+    }
+
+    public SqlLabel getTarget()
+    {
+        return target;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitBreak(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlContinue.java
@@ -1,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class SqlContinue
+        implements SqlStatement
+{
+    private final SqlLabel target;
+
+    public SqlContinue(SqlLabel target)
+    {
+        this.target = checkNotNull(target, "target is null");
+    }
+
+    public SqlLabel getTarget()
+    {
+        return target;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitContinue(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlRoutine.java
@@ -1,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.facebook.presto.spi.type.Type;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class SqlRoutine
+        implements SqlNode
+{
+    private final Type returnType;
+    private final List<SqlVariable> parameters;
+    private final SqlStatement body;
+
+    public SqlRoutine(Type returnType, List<SqlVariable> parameters, SqlStatement body)
+    {
+        this.returnType = checkNotNull(returnType, "returnType is null");
+        this.parameters = checkNotNull(parameters, "parameters is null");
+        this.body = checkNotNull(body, "body is null");
+    }
+
+    public Type getReturnType()
+    {
+        return returnType;
+    }
+
+    public List<SqlVariable> getParameters()
+    {
+        return parameters;
+    }
+
+    public SqlStatement getBody()
+    {
+        return body;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitRoutine(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlSet.java
@@ -1,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.facebook.presto.sql.relational.RowExpression;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class SqlSet
+        implements SqlStatement
+{
+    private final SqlVariable target;
+    private final RowExpression value;
+
+    public SqlSet(SqlVariable target, RowExpression value)
+    {
+        this.target = checkNotNull(target, "target is null");
+        this.value = checkNotNull(value, "value is null");
+    }
+
+    public SqlVariable getTarget()
+    {
+        return target;
+    }
+
+    public RowExpression getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitSet(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlVariable.java
@@ -1,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.relational.RowExpression;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class SqlVariable
+        implements SqlNode
+{
+    private final int field;
+    private final Type type;
+    private final RowExpression defaultValue;
+
+    public SqlVariable(int field, Type type, RowExpression defaultValue)
+    {
+        this.field = field;
+        this.type = checkNotNull(type, "type is null");
+        this.defaultValue = checkNotNull(defaultValue, "value is null");
+    }
+
+    public int getField()
+    {
+        return field;
+    }
+
+    public Type getType()
+    {
+        return type;
+    }
+
+    public RowExpression getDefaultValue()
+    {
+        return defaultValue;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitVariable(this, context);
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("field", field)
+                .add("type", type)
+                .add("defaultValue", defaultValue)
+                .toString();
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/WhenClause.java
@@ -1,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.facebook.presto.sql.relational.RowExpression;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class WhenClause
+{
+    private final RowExpression value;
+    private final SqlBlock block;
+
+    public WhenClause(RowExpression value, SqlBlock block)
+    {
+        this.value = checkNotNull(value, "value is null");
+        this.block = checkNotNull(block, "body is null");
+    }
+
+    public RowExpression getValue()
+    {
+        return value;
+    }
+
+    public SqlBlock getBlock()
+    {
+        return block;
+    }
+}

prestodb/presto

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlBlock.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+
+public class SqlBlock
+        implements SqlStatement
+{
+    private final Optional<SqlLabel> label;
+    private final List<SqlVariable> variables;
+    private final List<SqlStatement> statements;
+
+    public SqlBlock(List<SqlVariable> variables, List<SqlStatement> statements)
+    {
+        this(Optional.empty(), variables, statements);
+    }
+
+    public SqlBlock(Optional<SqlLabel> label, List<SqlVariable> variables, List<SqlStatement> statements)
+    {
+        this.label = requireNonNull(label, "label is null");
+        this.variables = ImmutableList.copyOf(requireNonNull(variables, "variables is null"));
+        this.statements = ImmutableList.copyOf(requireNonNull(statements, "statements is null"));
+    }
+
+    public Optional<SqlLabel> getLabel()
+    {
+        return label;
+    }
+
+    public List<SqlVariable> getVariables()
+    {
+        return variables;
+    }
+
+    public List<SqlStatement> getStatements()
+    {
+        return statements;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitBlock(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlBreak.java
@@ -1,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import static java.util.Objects.requireNonNull;
+
+public class SqlBreak
+        implements SqlStatement
+{
+    private final SqlLabel target;
+
+    public SqlBreak(SqlLabel target)
+    {
+        this.target = requireNonNull(target, "target is null");
+    }
+
+    public SqlLabel getTarget()
+    {
+        return target;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitBreak(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlContinue.java
@@ -1,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import static java.util.Objects.requireNonNull;
+
+public class SqlContinue
+        implements SqlStatement
+{
+    private final SqlLabel target;
+
+    public SqlContinue(SqlLabel target)
+    {
+        this.target = requireNonNull(target, "target is null");
+    }
+
+    public SqlLabel getTarget()
+    {
+        return target;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitContinue(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlRoutine.java
@@ -1,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.facebook.presto.spi.type.Type;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+public class SqlRoutine
+        implements SqlNode
+{
+    private final Type returnType;
+    private final List<SqlVariable> parameters;
+    private final SqlStatement body;
+
+    public SqlRoutine(Type returnType, List<SqlVariable> parameters, SqlStatement body)
+    {
+        this.returnType = requireNonNull(returnType, "returnType is null");
+        this.parameters = requireNonNull(parameters, "parameters is null");
+        this.body = requireNonNull(body, "body is null");
+    }
+
+    public Type getReturnType()
+    {
+        return returnType;
+    }
+
+    public List<SqlVariable> getParameters()
+    {
+        return parameters;
+    }
+
+    public SqlStatement getBody()
+    {
+        return body;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitRoutine(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlSet.java
@@ -1,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.facebook.presto.sql.relational.RowExpression;
+
+import static java.util.Objects.requireNonNull;
+
+public class SqlSet
+        implements SqlStatement
+{
+    private final SqlVariable target;
+    private final RowExpression value;
+
+    public SqlSet(SqlVariable target, RowExpression value)
+    {
+        this.target = requireNonNull(target, "target is null");
+        this.value = requireNonNull(value, "value is null");
+    }
+
+    public SqlVariable getTarget()
+    {
+        return target;
+    }
+
+    public RowExpression getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitSet(this, context);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/SqlVariable.java
@@ -1,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.relational.RowExpression;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static java.util.Objects.requireNonNull;
+
+public class SqlVariable
+        implements SqlNode
+{
+    private final int field;
+    private final Type type;
+    private final RowExpression defaultValue;
+
+    public SqlVariable(int field, Type type, RowExpression defaultValue)
+    {
+        this.field = field;
+        this.type = requireNonNull(type, "type is null");
+        this.defaultValue = requireNonNull(defaultValue, "value is null");
+    }
+
+    public int getField()
+    {
+        return field;
+    }
+
+    public Type getType()
+    {
+        return type;
+    }
+
+    public RowExpression getDefaultValue()
+    {
+        return defaultValue;
+    }
+
+    @Override
+    public <C, R> R accept(SqlNodeVisitor<C, R> visitor, C context)
+    {
+        return visitor.visitVariable(this, context);
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("field", field)
+                .add("type", type)
+                .add("defaultValue", defaultValue)
+                .toString();
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/routine/WhenClause.java
@@ -1,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.routine;
+
+import com.facebook.presto.sql.relational.RowExpression;
+
+import static java.util.Objects.requireNonNull;
+
+public class WhenClause
+{
+    private final RowExpression value;
+    private final SqlBlock block;
+
+    public WhenClause(RowExpression value, SqlBlock block)
+    {
+        this.value = requireNonNull(value, "value is null");
+        this.block = requireNonNull(block, "body is null");
+    }
+
+    public RowExpression getValue()
+    {
+        return value;
+    }
+
+    public SqlBlock getBlock()
+    {
+        return block;
+    }
+}