bogynick/bogdans

Add support for prepared statements to the cli
Testing: manual

presto-client/src/main/java/com/facebook/presto/client/StatementClient.java
@@ -40,8 +40,10 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static com.facebook.presto.client.PrestoHeaders.PRESTO_ADDED_PREPARE;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_CLEAR_SESSION;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_CLEAR_TRANSACTION_ID;
+import static com.facebook.presto.client.PrestoHeaders.PRESTO_DEALLOCATED_PREPARE;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_SET_SESSION;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_STARTED_TRANSACTION_ID;
 import static com.google.common.base.MoreObjects.firstNonNull;
@@ -80,6 +82,8 @@ public class StatementClient
     private final AtomicReference<QueryResults> currentResults = new AtomicReference<>();
     private final Map<String, String> setSessionProperties = new ConcurrentHashMap<>();
     private final Set<String> resetSessionProperties = Sets.newConcurrentHashSet();
+    private final Map<String, String> addedPreparedStatements = new ConcurrentHashMap<>();
+    private final Set<String> deallocatedPreparedStatements = Sets.newConcurrentHashSet();
     private final AtomicReference<String> startedtransactionId = new AtomicReference<>();
     private final AtomicBoolean clearTransactionId = new AtomicBoolean();
     private final AtomicBoolean closed = new AtomicBoolean();
@@ -137,6 +141,11 @@ public class StatementClient
         Map<String, String> property = session.getProperties();
         for (Entry<String, String> entry : property.entrySet()) {
             builder.addHeader(PrestoHeaders.PRESTO_SESSION, entry.getKey() + "=" + entry.getValue());
+        }
+
+        Map<String, String> statements = session.getPreparedStatements();
+        for (Entry<String, String> entry : statements.entrySet()) {
+            builder.addHeader(PrestoHeaders.PRESTO_PREPARED_STATEMENT, entry.getKey() + "=" + entry.getValue());
         }
 
         builder.setHeader(PrestoHeaders.PRESTO_TRANSACTION_ID, session.getTransactionId() == null ? "NONE" : session.getTransactionId());
@@ -199,6 +208,16 @@ public class StatementClient
     public Set<String> getResetSessionProperties()
     {
         return ImmutableSet.copyOf(resetSessionProperties);
+    }
+
+    public Map<String, String> getAddedPreparedStatements()
+    {
+        return ImmutableMap.copyOf(addedPreparedStatements);
+    }
+
+    public Set<String> getDeallocatedPreparedStatements()
+    {
+        return ImmutableSet.copyOf(deallocatedPreparedStatements);
     }
 
     public String getStartedtransactionId()
@@ -286,6 +305,17 @@ public class StatementClient
         }
         for (String clearSession : response.getHeaders().get(PRESTO_CLEAR_SESSION)) {
             resetSessionProperties.add(clearSession);
+        }
+
+        for (String addedPrepares : response.getHeaders().get(PRESTO_ADDED_PREPARE)) {
+            List<String> keyValue = SESSION_HEADER_SPLITTER.splitToList(addedPrepares);
+            if (keyValue.size() != 2) {
+                continue;
+            }
+            addedPreparedStatements.put(keyValue.get(0), keyValue.get(1));
+        }
+        for (String deallocatedPrepares : response.getHeaders().get(PRESTO_DEALLOCATED_PREPARE)) {
+            deallocatedPreparedStatements.add(deallocatedPrepares);
         }
 
         String startedTransactionId = response.getHeader(PRESTO_STARTED_TRANSACTION_ID);

prestodb/presto

presto-client/src/main/java/com/facebook/presto/client/StatementClient.java
@@ -40,8 +40,10 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static com.facebook.presto.client.PrestoHeaders.PRESTO_ADDED_PREPARE;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_CLEAR_SESSION;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_CLEAR_TRANSACTION_ID;
+import static com.facebook.presto.client.PrestoHeaders.PRESTO_DEALLOCATED_PREPARE;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_SET_SESSION;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_STARTED_TRANSACTION_ID;
 import static com.google.common.base.MoreObjects.firstNonNull;
@@ -80,6 +82,8 @@ public class StatementClient
     private final AtomicReference<QueryResults> currentResults = new AtomicReference<>();
     private final Map<String, String> setSessionProperties = new ConcurrentHashMap<>();
     private final Set<String> resetSessionProperties = Sets.newConcurrentHashSet();
+    private final Map<String, String> addedPreparedStatements = new ConcurrentHashMap<>();
+    private final Set<String> deallocatedPreparedStatements = Sets.newConcurrentHashSet();
     private final AtomicReference<String> startedtransactionId = new AtomicReference<>();
     private final AtomicBoolean clearTransactionId = new AtomicBoolean();
     private final AtomicBoolean closed = new AtomicBoolean();
@@ -134,6 +138,11 @@ public class StatementClient
         Map<String, String> property = session.getProperties();
         for (Entry<String, String> entry : property.entrySet()) {
             builder.addHeader(PrestoHeaders.PRESTO_SESSION, entry.getKey() + "=" + entry.getValue());
+        }
+
+        Map<String, String> statements = session.getPreparedStatements();
+        for (Entry<String, String> entry : statements.entrySet()) {
+            builder.addHeader(PrestoHeaders.PRESTO_PREPARED_STATEMENT, entry.getKey() + "=" + entry.getValue());
         }
 
         builder.setHeader(PrestoHeaders.PRESTO_TRANSACTION_ID, session.getTransactionId() == null ? "NONE" : session.getTransactionId());
@@ -196,6 +205,16 @@ public class StatementClient
     public Set<String> getResetSessionProperties()
     {
         return ImmutableSet.copyOf(resetSessionProperties);
+    }
+
+    public Map<String, String> getAddedPreparedStatements()
+    {
+        return ImmutableMap.copyOf(addedPreparedStatements);
+    }
+
+    public Set<String> getDeallocatedPreparedStatements()
+    {
+        return ImmutableSet.copyOf(deallocatedPreparedStatements);
     }
 
     public String getStartedtransactionId()
@@ -289,6 +308,17 @@ public class StatementClient
         }
         for (String clearSession : response.getHeaders(PRESTO_CLEAR_SESSION)) {
             resetSessionProperties.add(clearSession);
+        }
+
+        for (String entry : response.getHeaders(PRESTO_ADDED_PREPARE)) {
+            List<String> keyValue = SESSION_HEADER_SPLITTER.splitToList(entry);
+            if (keyValue.size() != 2) {
+                continue;
+            }
+            this.addedPreparedStatements.put(keyValue.get(0), keyValue.get(1));
+        }
+        for (String entry : response.getHeaders(PRESTO_DEALLOCATED_PREPARE)) {
+            this.deallocatedPreparedStatements.add(entry);
         }
 
         String startedTransactionId = response.getHeader(PRESTO_STARTED_TRANSACTION_ID);