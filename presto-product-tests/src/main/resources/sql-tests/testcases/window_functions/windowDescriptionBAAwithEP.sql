d949be5ccba83bdff975ae8345a04926cd648667
Eric Diven <eric.diven@teradata.com>
2016-06-08 13:30:34 -0400

    Add product tests with mergeable WindowNodes
    WindowNodes that have identical specifications can be merged. This adds product
    tests covering a number of situations with mergeable windows. Note that window
    functions that take extra parameters result in a different plan than ones that
    do not. nth_value(), for example, takes a parameter specifying which value in
    the window to take.
    
    These functions produce a plan with a ProjectNode that cannot be pruned by the
    PruneIdentityProjections optimizer, and so are special cases that should be
    tested.

---
+++ presto-product-tests/src/main/resources/sql-tests/testcases/window_functions/windowDescriptionBAAwithEP.sql
@@ -0,0 +1,8 @@
+-- database: presto; groups: window;
+select
+suppkey, orderkey, partkey,
+nth_value(quantity, 4) over (partition by partkey order by orderkey rows between UNBOUNDED preceding and CURRENT ROW) nth_value_quantity_B,
+sum(quantity) over (partition by suppkey order by orderkey rows between unbounded preceding and current row) sum_quantity_A,
+sum(discount) over (partition by suppkey order by orderkey rows between unbounded preceding and current row) sum_discount_A
+
+from tpch.tiny.lineitem where (partkey = 272 or partkey = 273) and suppkey > 50
