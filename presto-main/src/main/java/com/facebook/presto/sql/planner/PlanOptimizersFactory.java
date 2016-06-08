f508c23a8bf638a3085c7d2b4af566889ff803f8
Eric Diven <eric.diven@teradata.com>
2016-06-08 13:31:20 -0400

    Merge WindowNodes with identical specifications
    This optimizer is limited to merging WindowNodes with identical specifications
    in groups of WindowNodes in a plan that do not contain other types of
    PlanNodes.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizersFactory.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizersFactory.java
@@ -28,6 +28,7 @@ import com.facebook.presto.sql.planner.optimizations.ImplementIntersectAsUnion;
 import com.facebook.presto.sql.planner.optimizations.ImplementSampleAsFilter;
 import com.facebook.presto.sql.planner.optimizations.IndexJoinOptimizer;
 import com.facebook.presto.sql.planner.optimizations.LimitPushDown;
+import com.facebook.presto.sql.planner.optimizations.MergeIdenticalWindows;
 import com.facebook.presto.sql.planner.optimizations.MergeProjections;
 import com.facebook.presto.sql.planner.optimizations.MetadataDeleteOptimizer;
 import com.facebook.presto.sql.planner.optimizations.MetadataQueryOptimizer;
@@ -87,6 +88,7 @@ public class PlanOptimizersFactory
                 new IndexJoinOptimizer(metadata), // Run this after projections and filters have been fully simplified and pushed down
                 new CountConstantOptimizer(),
                 new WindowFilterPushDown(metadata), // This must run after PredicatePushDown and LimitPushDown so that it squashes any successive filter nodes and limits
+                new MergeIdenticalWindows(),
                 new MergeProjections(),
                 new PruneUnreferencedOutputs(), // Make sure to run this at the end to help clean the plan for logging/execution and not remove info that other optimizers might need at an earlier point
                 new PruneIdentityProjections(), // This MUST run after PruneUnreferencedOutputs as it may introduce new redundant projections
