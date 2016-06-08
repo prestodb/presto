ba8ce087656a8fc18ab77f040fe7fc5d9036fdd1
Artur Gajowy <artur.gajowy@gmail.com>
2016-06-08 13:10:11 +0200

    Introduce `compose.sh` scripts in env dirs for easier `-f ...` passing
    Those will allow for running arbitrary docker-compose commands without
    tedious passing of multiple `-f` arguments.

---
+++ presto-product-tests/conf/docker/singlenode-kerberos-hdfs-impersonation/compose.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker-compose \
+-f ${BASH_SOURCE%/*}/../common/standard.yml \
+-f ${BASH_SOURCE%/*}/../common/kerberos.yml \
+-f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
+-f ${BASH_SOURCE%/*}/docker-compose.yml \
+"$@"
