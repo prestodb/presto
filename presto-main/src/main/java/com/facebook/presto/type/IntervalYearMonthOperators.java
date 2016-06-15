e48df11f915c62bb2684efe2e5e28be456677792
David Phillips <david@acz.org>
2016-06-14 18:01:10 -0700

    Move interval utility methods to client module

--- presto-main/src/main/java/com/facebook/presto/type/IntervalYearMonthOperators.java
+++ presto-main/src/main/java/com/facebook/presto/type/IntervalYearMonthOperators.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.type;
 
+import com.facebook.presto.client.IntervalYearMonth;
 import com.facebook.presto.operator.scalar.ScalarOperator;
 import com.facebook.presto.spi.type.StandardTypes;
 import io.airlift.slice.Slice;
@@ -151,7 +152,7 @@ public final class IntervalYearMonthOperators
     @SqlType(StandardTypes.VARCHAR)
     public static Slice castToSlice(@SqlType(StandardTypes.INTERVAL_YEAR_TO_MONTH) long value)
     {
-        return utf8Slice(SqlIntervalYearMonth.formatMonths(value));
+        return utf8Slice(IntervalYearMonth.formatMonths(value));
     }
 
     @ScalarOperator(HASH_CODE)
