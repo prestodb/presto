41282831a41bb69d846a1aaf50a4c7ec992ec6c0
Christina Wallin <christina.wallin@teradata.com>
2016-06-15 18:07:58 -0400

    Update JDBC type for REAL

--- presto-jdbc/src/test/java/com/facebook/presto/jdbc/TestJdbcResultSet.java
+++ presto-jdbc/src/test/java/com/facebook/presto/jdbc/TestJdbcResultSet.java
@@ -97,30 +97,32 @@ public class TestJdbcResultSet
     public void testObjectTypes()
             throws Exception
     {
-        String sql = "SELECT 123, 12300000000, 0.1, true, 'hello', 1.0 / 0.0, 0.0 / 0.0, ARRAY[1, 2], cast('foo' as char(5))";
+        String sql = "SELECT 123, 12300000000, REAL '123.45', 0.1, true, 'hello', 1.0 / 0.0, 0.0 / 0.0, ARRAY[1, 2], cast('foo' as char(5))";
         try (ResultSet rs = statement.executeQuery(sql)) {
             ResultSetMetaData metadata = rs.getMetaData();
-            assertEquals(metadata.getColumnCount(), 9);
+            assertEquals(metadata.getColumnCount(), 10);
             assertEquals(metadata.getColumnType(1), Types.INTEGER);
             assertEquals(metadata.getColumnType(2), Types.BIGINT);
-            assertEquals(metadata.getColumnType(3), Types.DOUBLE);
-            assertEquals(metadata.getColumnType(4), Types.BOOLEAN);
-            assertEquals(metadata.getColumnType(5), Types.LONGNVARCHAR);
-            assertEquals(metadata.getColumnType(6), Types.DOUBLE);
+            assertEquals(metadata.getColumnType(3), Types.REAL);
+            assertEquals(metadata.getColumnType(4), Types.DOUBLE);
+            assertEquals(metadata.getColumnType(5), Types.BOOLEAN);
+            assertEquals(metadata.getColumnType(6), Types.LONGNVARCHAR);
             assertEquals(metadata.getColumnType(7), Types.DOUBLE);
-            assertEquals(metadata.getColumnType(8), Types.ARRAY);
-            assertEquals(metadata.getColumnType(9), Types.CHAR);
+            assertEquals(metadata.getColumnType(8), Types.DOUBLE);
+            assertEquals(metadata.getColumnType(9), Types.ARRAY);
+            assertEquals(metadata.getColumnType(10), Types.CHAR);
 
             assertTrue(rs.next());
             assertEquals(rs.getObject(1), 123);
             assertEquals(rs.getObject(2), 12300000000L);
-            assertEquals(rs.getObject(3), 0.1d);
-            assertEquals(rs.getObject(4), true);
-            assertEquals(rs.getObject(5), "hello");
-            assertEquals(rs.getObject(6), Double.POSITIVE_INFINITY);
-            assertEquals(rs.getObject(7), Double.NaN);
-            assertEquals(rs.getArray(8).getArray(), new int[] {1, 2});
-            assertEquals(rs.getObject(9), "foo  ");
+            assertEquals(rs.getObject(3), 123.45f);
+            assertEquals(rs.getObject(4), 0.1d);
+            assertEquals(rs.getObject(5), true);
+            assertEquals(rs.getObject(6), "hello");
+            assertEquals(rs.getObject(7), Double.POSITIVE_INFINITY);
+            assertEquals(rs.getObject(8), Double.NaN);
+            assertEquals(rs.getArray(9).getArray(), new int[] {1, 2});
+            assertEquals(rs.getObject(10), "foo  ");
         }
     }
 
