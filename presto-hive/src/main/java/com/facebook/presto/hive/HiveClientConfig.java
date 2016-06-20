35cbc93c6d27b31b8430181e35f4b0ca665e2cd0
Christopher Berner <christopherberner@gmail.com>
2016-06-20 15:06:07 -0700

    Add hive.ipc-ping-interval config
    hive.dfs-timeout no longer controls ipc.ping.interval. Instead
    hive.ipc-ping-interval controls it.

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveClientConfig.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveClientConfig.java
@@ -74,6 +74,7 @@ public class HiveClientConfig
     private HostAndPort metastoreSocksProxy;
     private Duration metastoreTimeout = new Duration(10, TimeUnit.SECONDS);
 
+    private Duration ipcPingInterval = new Duration(10, TimeUnit.SECONDS);
     private Duration dfsTimeout = new Duration(10, TimeUnit.SECONDS);
     private Duration dfsConnectTimeout = new Duration(500, TimeUnit.MILLISECONDS);
     private int dfsConnectMaxRetries = 5;
@@ -434,6 +435,20 @@ public class HiveClientConfig
     public HiveClientConfig setResourceConfigFiles(List<String> files)
     {
         this.resourceConfigFiles = (files == null) ? null : ImmutableList.copyOf(files);
+        return this;
+    }
+
+    @NotNull
+    @MinDuration("1ms")
+    public Duration getIpcPingInterval()
+    {
+        return ipcPingInterval;
+    }
+
+    @Config("hive.ipc-ping-interval")
+    public HiveClientConfig setIpcPingInterval(Duration pingInterval)
+    {
+        this.ipcPingInterval = pingInterval;
         return this;
     }
 
