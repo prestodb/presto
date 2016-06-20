2a8f12e49c15a3a369ce9fa67f5b1953c9f23578
Christopher Berner <christopherberner@gmail.com>
2016-06-20 15:07:14 -0700

    Change default hive.dfs-timeout to 60s
    On some versions of HDFS, a low timeout like this can cause
    unrecoverable errors during writing.

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveClientConfig.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveClientConfig.java
@@ -58,7 +58,7 @@ public class TestHiveClientConfig
                 .setForceLocalScheduling(false)
                 .setMaxConcurrentFileRenames(20)
                 .setRecursiveDirWalkerEnabled(false)
-                .setDfsTimeout(new Duration(10, TimeUnit.SECONDS))
+                .setDfsTimeout(new Duration(60, TimeUnit.SECONDS))
                 .setIpcPingInterval(new Duration(10, TimeUnit.SECONDS))
                 .setDfsConnectTimeout(new Duration(500, TimeUnit.MILLISECONDS))
                 .setDfsConnectMaxRetries(5)
