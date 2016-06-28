070c07906052a1104239288bebd454c7ad08acc7
Maciej Grzybek <maciej.grzybek@teradata.com>
2016-06-28 12:45:22 -0400

    Add FloatCovariance functions

---
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestFloatCovarianceSampAggregation.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator.aggregation;
+
+import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.type.StandardTypes;
+import com.google.common.collect.ImmutableList;
+import org.apache.commons.math3.stat.correlation.Covariance;
+
+import java.util.List;
+
+import static com.facebook.presto.block.BlockAssertions.createFloatSequenceBlock;
+import static com.facebook.presto.operator.aggregation.AggregationTestUtils.constructDoublePrimitiveArray;
+
+public class TestFloatCovarianceSampAggregation
+        extends AbstractTestAggregationFunction
+{
+    @Override
+    public Block[] getSequenceBlocks(int start, int length)
+    {
+        return new Block[]{createFloatSequenceBlock(start, start + length), createFloatSequenceBlock(start + 5, start + 5 + length)};
+    }
+
+    @Override
+    protected String getFunctionName()
+    {
+        return "covar_samp";
+    }
+
+    @Override
+    protected List<String> getFunctionParameterTypes()
+    {
+        return ImmutableList.of(StandardTypes.FLOAT, StandardTypes.FLOAT);
+    }
+
+    @Override
+    public Object getExpectedValue(int start, int length)
+    {
+        if (length <= 1) {
+            return null;
+        }
+        return (float) new Covariance().covariance(constructDoublePrimitiveArray(start + 5, length), constructDoublePrimitiveArray(start, length), true);
+    }
+}
