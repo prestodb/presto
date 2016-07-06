3f7e46a5b886d425a87bd8a0883dffceee467588
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-07-06 15:11:44 +0200

    Refactor TestLogicalPlanner to test logical plan creation
    I just removed tests whose the main purpose was to check if logical plan
    assertions are working. Instead I left test whose the main purpose is to
    check if logical planner return correct plan.
    
    Logical plan assertions are tested anyway, I do think we need special
    tests for that.

--- presto-main/src/test/java/com/facebook/presto/sql/planner/TestLogicalPlanner.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/TestLogicalPlanner.java
@@ -17,11 +17,7 @@ import com.facebook.presto.spi.predicate.Domain;
 import com.facebook.presto.sql.planner.assertions.PlanAssert;
 import com.facebook.presto.sql.planner.assertions.PlanMatchPattern;
 import com.facebook.presto.sql.planner.plan.EnforceSingleRowNode;
-import com.facebook.presto.sql.planner.plan.FilterNode;
-import com.facebook.presto.sql.planner.plan.JoinNode;
-import com.facebook.presto.sql.planner.plan.OutputNode;
 import com.facebook.presto.sql.planner.plan.PlanNode;
-import com.facebook.presto.sql.planner.plan.TableScanNode;
 import com.facebook.presto.testing.LocalQueryRunner;
 import com.facebook.presto.tpch.TpchConnectorFactory;
 import com.google.common.collect.ImmutableList;
@@ -67,32 +63,20 @@ public class TestLogicalPlanner
     }
 
     @Test
-    public void testPlanDsl()
+    public void testJoin()
     {
-        assertPlan("SELECT orderkey FROM orders WHERE orderkey IN (1, 2, 3)",
-                node(OutputNode.class,
-                        node(FilterNode.class,
-                                tableScan("orders"))));
-
-        String simpleJoinQuery = "SELECT o.orderkey FROM orders o, lineitem l WHERE l.orderkey = o.orderkey";
-        assertPlan(simpleJoinQuery,
-                any(
-                        any(
-                                node(JoinNode.class,
-                                        any(
-                                                tableScan("orders")),
-                                        any(
-                                                any(
-                                                        tableScan("lineitem")))))));
-
-        assertPlan(simpleJoinQuery,
+        assertPlan("SELECT o.orderkey FROM orders o, lineitem l WHERE l.orderkey = o.orderkey",
                 anyTree(
-                        node(JoinNode.class,
-                                anyTree(),
-                                anyTree())));
+                        join(INNER, ImmutableList.of(aliasPair("O", "L")),
+                                any(
+                                        tableScan("orders").withSymbol("orderkey", "O")),
+                                anyTree(
+                                        tableScan("lineitem").withSymbol("orderkey", "L")))));
+    }
 
-        assertPlan(simpleJoinQuery, anyTree(node(TableScanNode.class)));
-
+    @Test
+    public void testUncorrelatedSubqueries()
+    {
         assertPlan("SELECT * FROM orders WHERE orderkey = (SELECT orderkey FROM lineitem ORDER BY orderkey LIMIT 1)",
                 anyTree(
                         join(INNER, ImmutableList.of(aliasPair("X", "Y")),
