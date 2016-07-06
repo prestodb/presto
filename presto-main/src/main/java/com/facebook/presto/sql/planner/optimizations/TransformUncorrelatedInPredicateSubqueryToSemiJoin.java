7bf68e270ec9e01b869a2e50a6f07f7b420c86e0
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-07-06 11:17:24 +0200

    Refactor TransformUncorrelatedInPredicateSubqueryToSemiJoin
    Remove unnessasary class InPredicateRewriteResult and remove copy paste
    code of adding inPredicate mappings by pushing it down to the
    rewriteInPredicates method.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/TransformUncorrelatedInPredicateSubqueryToSemiJoin.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/TransformUncorrelatedInPredicateSubqueryToSemiJoin.java
@@ -99,28 +99,26 @@ public class TransformUncorrelatedInPredicateSubqueryToSemiJoin
         @Override
         public PlanNode visitFilter(FilterNode node, RewriteContext<Void> context)
         {
-            InPredicateRewriteResult inPredicateRewriteResult = rewriteInPredicates(
+            PlanNode rewrittenNode = rewriteInPredicates(
                     context.defaultRewrite(node, context.get()),
                     ImmutableList.of(node.getPredicate()));
             inPredicateMappings.add(inPredicateRewriteResult.inPredicateMapping);
 
             return new FilterNode(
-                    inPredicateRewriteResult.node.getId(),
-                    getOnlyElement(inPredicateRewriteResult.node.getSources()),
+                    rewrittenNode.getId(),
+                    getOnlyElement(rewrittenNode.getSources()),
                     replaceInPredicates(node.getPredicate()));
         }
 
         @Override
         public PlanNode visitProject(ProjectNode node, RewriteContext<Void> context)
         {
-            InPredicateRewriteResult inPredicateRewriteResult = rewriteInPredicates(
+            PlanNode rewrittenNode = rewriteInPredicates(
                     context.defaultRewrite(node, context.get()),
                     node.getAssignments().values());
 
-            inPredicateMappings.add(inPredicateRewriteResult.inPredicateMapping);
-
-            return new ProjectNode(inPredicateRewriteResult.node.getId(),
-                    getOnlyElement(inPredicateRewriteResult.node.getSources()),
+            return new ProjectNode(rewrittenNode.getId(),
+                    getOnlyElement(rewrittenNode.getSources()),
                     replaceInPredicateInAssignments(node));
         }
 
@@ -134,19 +132,8 @@ public class TransformUncorrelatedInPredicateSubqueryToSemiJoin
                 rewrittenNode = rewriteWith(rewriter, rewrittenNode, null);
                 inPredicateMapping.putAll(rewriter.getInPredicateMapping());
             }
-            return new InPredicateRewriteResult(rewrittenNode, inPredicateMapping.build());
-        }
-
-        private static class InPredicateRewriteResult
-        {
-            private final PlanNode node;
-            private final Map<InPredicate, Expression> inPredicateMapping;
-
-            private InPredicateRewriteResult(PlanNode node, Map<InPredicate, Expression> inPredicateMapping)
-            {
-                this.node = requireNonNull(node, "node is null");
-                this.inPredicateMapping = requireNonNull(inPredicateMapping, "inPredicateMapping is null");
-            }
+            inPredicateMappings.add(inPredicateMapping.build());
+            return rewrittenNode;
         }
 
         private Map<Symbol, Expression> replaceInPredicateInAssignments(ProjectNode node)
