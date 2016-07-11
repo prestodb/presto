prestodb/presto

commit 4ed06d97c22d67a685fc716597f33b1767748bd5

Capture partition count in Plan
When local parallel is enabled, the coordinator creates a query plan that
requires parallel execution, but parallelism is set on each worker using
the task concurrency property.  If the coordinator and worker have different
settings for this property, the execution can fail.

Instead of dividing the parallelism planning between the coordinator and the
worker, the coordinator makes the decision and records this in the plan.

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/AddLocalExchanges.java
@@ -425,8 +425,8 @@ public class AddLocalExchanges
             StreamPreferredProperties requiredProperties;
             StreamPreferredProperties preferredProperties;
             if (getTaskWriterCount(session) > 1) {
-                requiredProperties = fixedParallelism();
-                preferredProperties = fixedParallelism();
+                requiredProperties = fixedParallelism(getTaskWriterCount(session));
+                preferredProperties = fixedParallelism(getTaskWriterCount(session));
             }
             else {
                 requiredProperties = singleStream();
@@ -486,7 +486,7 @@ public class AddLocalExchanges
                         REPARTITION,
                         LOCAL,
                         new PartitioningScheme(
-                                fixedHashPartitioning(preferredPartitionColumns.get()),
+                                fixedHashPartitioning(getTaskConcurrency(session), preferredPartitionColumns.get()),
                                 node.getOutputSymbols(),
                                 Optional.empty()),
                         sources,
@@ -499,7 +499,7 @@ public class AddLocalExchanges
                     idAllocator.getNextId(),
                     REPARTITION,
                     LOCAL,
-                    new PartitioningScheme(fixedRandomPartitioning(), node.getOutputSymbols()),
+                    new PartitioningScheme(fixedRandomPartitioning(getTaskConcurrency(session)), node.getOutputSymbols()),
                     sources,
                     inputLayouts);
             ExchangeNode exchangeNode = result;
@@ -616,7 +616,7 @@ public class AddLocalExchanges
                         LOCAL,
                         planWithProperties.getNode(),
                         new PartitioningScheme(
-                                fixedRandomPartitioning(),
+                                fixedRandomPartitioning(getTaskConcurrency(session)),
                                 planWithProperties.getNode().getOutputSymbols()));
 
                 return deriveProperties(exchangeNode, planWithProperties.getProperties());
@@ -629,7 +629,8 @@ public class AddLocalExchanges
                         LOCAL,
                         planWithProperties.getNode(),
                         requiredPartitionColumns.get(),
-                        Optional.empty());
+                        Optional.empty(),
+                        requiredProperties.getStreamCount().orElse(getTaskConcurrency(session)));
                 return deriveProperties(exchangeNode, planWithProperties.getProperties());
             }
 

presto-main/src/main/java/com/facebook/presto/operator/exchange/LocalExchange.java
@@ -26,16 +26,18 @@ import java.io.Closeable;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import static com.facebook.presto.operator.exchange.LocalExchangeSink.finishedLocalExchangeSink;
+import static com.facebook.presto.sql.planner.SystemPartitioningHandle.getSystemPartitionCount;
 import static com.facebook.presto.sql.planner.SystemPartitioningHandle.isFixedBroadcastPartitioning;
 import static com.facebook.presto.sql.planner.SystemPartitioningHandle.isFixedHashPartitioning;
 import static com.facebook.presto.sql.planner.SystemPartitioningHandle.isFixedRandomPartitioning;
-import static com.facebook.presto.sql.planner.SystemPartitioningHandle.isSinglePartitioning;
 import static com.facebook.presto.util.ImmutableCollectors.toImmutableList;
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static io.airlift.units.DataSize.Unit.MEGABYTE;
 import static java.util.Objects.requireNonNull;
@@ -65,17 +67,15 @@ public class LocalExchange
 
     public LocalExchange(
             Partitioning partitioning,
-            int defaultConcurrency,
             List<? extends Type> types,
             List<Integer> partitionChannels,
             Optional<Integer> partitionHashChannel)
     {
-        this(partitioning, defaultConcurrency, types, partitionChannels, partitionHashChannel, DEFAULT_MAX_BUFFERED_BYTES);
+        this(partitioning, types, partitionChannels, partitionHashChannel, DEFAULT_MAX_BUFFERED_BYTES);
     }
 
     public LocalExchange(
             Partitioning partitioning,
-            int defaultConcurrency,
             List<? extends Type> types,
             List<Integer> partitionChannels,
             Optional<Integer> partitionHashChannel,
@@ -83,14 +83,10 @@ public class LocalExchange
     {
         this.types = ImmutableList.copyOf(requireNonNull(types, "types is null"));
 
-        int bufferCount;
-        if (isSinglePartitioning(partitioning)) {
-            bufferCount = 1;
-        }
-        else {
-            bufferCount = defaultConcurrency;
-        }
+        OptionalInt partitionCount = getSystemPartitionCount(partitioning);
+        checkArgument(partitionCount.isPresent(), "Unsupported local exchange partitioning %s", partitioning);
 
+        int bufferCount = partitionCount.getAsInt();
         ImmutableList.Builder<LocalExchangeSource> sources = ImmutableList.builder();
         for (int i = 0; i < bufferCount; i++) {
             sources.add(new LocalExchangeSource(types, source -> checkAllSourcesFinished()));

presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
@@ -173,8 +173,6 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-import static com.facebook.presto.SystemSessionProperties.getTaskConcurrency;
-import static com.facebook.presto.SystemSessionProperties.getTaskWriterCount;
 import static com.facebook.presto.metadata.FunctionKind.SCALAR;
 import static com.facebook.presto.operator.DistinctLimitOperator.DistinctLimitOperatorFactory;
 import static com.facebook.presto.operator.NestedLoopBuildOperator.NestedLoopBuildOperatorFactory;
@@ -202,7 +200,6 @@ import static com.facebook.presto.util.ImmutableCollectors.toImmutableSet;
 import static com.google.common.base.Functions.forMap;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
-import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.Iterables.concat;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static java.lang.String.format;
@@ -1219,6 +1216,7 @@ public class LocalExecutionPlanner
         public PhysicalOperation visitIndexSource(IndexSourceNode node, LocalExecutionPlanContext context)
         {
             checkState(context.getIndexSourceContext().isPresent(), "Must be in an index source context");
+            checkArgument(!context.getDriverInstanceCount().isPresent(), "Plan context must not have driver instance count set for index source");
             IndexSourceContext indexSourceContext = context.getIndexSourceContext().get();
 
             SetMultimap<Symbol, Integer> indexLookupToProbeInput = indexSourceContext.getIndexLookupToProbeInput();
@@ -1616,16 +1614,11 @@ public class LocalExecutionPlanner
         @Override
         public PhysicalOperation visitTableWriter(TableWriterNode node, LocalExecutionPlanContext context)
         {
-            // Set table writer count
-            if (node.getPartitioningScheme().isPresent()) {
-                context.setDriverInstanceCount(1);
-            }
-            else {
-                context.setDriverInstanceCount(getTaskWriterCount(session));
-            }
-
             // serialize writes by forcing data through a single writer
             PhysicalOperation source = node.getSource().accept(this, context);
+
+            // planning the source must establish a fixed driver count
+            checkArgument(context.getDriverInstanceCount().isPresent(), "Table writer must have a fixed partition count");
 
             Optional<Integer> sampleWeightChannel = node.getSampleWeightSymbol().map(source::symbolToChannel);
 
@@ -1706,19 +1699,7 @@ public class LocalExecutionPlanner
         public PhysicalOperation visitExchange(ExchangeNode node, LocalExecutionPlanContext context)
         {
             checkArgument(node.getScope() == LOCAL, "Only local exchanges are supported in the local planner");
-
-            int driverInstanceCount;
-            if (node.getType() == ExchangeNode.Type.GATHER) {
-                driverInstanceCount = 1;
-                context.setDriverInstanceCount(1);
-            }
-            else if (context.getDriverInstanceCount().isPresent()) {
-                driverInstanceCount = context.getDriverInstanceCount().getAsInt();
-            }
-            else {
-                driverInstanceCount = getTaskConcurrency(session);
-                context.setDriverInstanceCount(driverInstanceCount);
-            }
+            checkArgument(!context.getDriverInstanceCount().isPresent(), "Plan context must not have driver instance count set for local exchange");
 
             List<Type> types = getSourceOperatorTypes(node, context.getTypes());
             List<Integer> channels = node.getPartitioningScheme().getPartitioning().getArguments().stream()
@@ -1727,7 +1708,8 @@ public class LocalExecutionPlanner
             Optional<Integer> hashChannel = node.getPartitioningScheme().getHashColumn()
                     .map(symbol -> node.getOutputSymbols().indexOf(symbol));
 
-            LocalExchange localExchange = new LocalExchange(node.getPartitioningScheme().getPartitioning(), driverInstanceCount, types, channels, hashChannel);
+            LocalExchange localExchange = new LocalExchange(node.getPartitioningScheme().getPartitioning(), types, channels, hashChannel);
+            context.setDriverInstanceCount(localExchange.getBufferCount());
 
             for (int i = 0; i < node.getSources().size(); i++) {
                 PlanNode sourceNode = node.getSources().get(i);
@@ -1746,10 +1728,6 @@ public class LocalExecutionPlanner
 
             // the main driver is not an input... the exchange sources are the input for the plan
             context.setInputDriver(false);
-
-            // instance count must match the number of partitions in the exchange
-            verify(context.getDriverInstanceCount().getAsInt() == localExchange.getBufferCount(),
-                    "driver instance count must match the number of exchange partitions");
 
             return new PhysicalOperation(new LocalExchangeSourceOperatorFactory(context.getNextOperatorId(), node.getId(), localExchange), makeLayout(node));
         }

presto-main/src/main/java/com/facebook/presto/sql/planner/NodePartitioningManager.java
@@ -93,7 +93,7 @@ public class NodePartitioningManager
         requireNonNull(partitioningHandle, "partitioningHandle is null");
 
         if (partitioningHandle.getConnectorHandle() instanceof SystemPartitioningHandle) {
-            return ((SystemPartitioningHandle) partitioningHandle.getConnectorHandle()).getNodePartitionMap(session, nodeScheduler);
+            return ((SystemPartitioningHandle) partitioningHandle.getConnectorHandle()).getNodePartitionMap(nodeScheduler);
         }
 
         ConnectorNodePartitioningProvider partitioningProvider = partitioningProviders.get(partitioningHandle.getConnectorId().get());

presto-main/src/main/java/com/facebook/presto/sql/planner/SystemPartitioningHandle.java
@@ -13,7 +13,6 @@
  */
 package com.facebook.presto.sql.planner;
 
-import com.facebook.presto.Session;
 import com.facebook.presto.execution.scheduler.NodeScheduler;
 import com.facebook.presto.execution.scheduler.NodeSelector;
 import com.facebook.presto.operator.HashGenerator;
@@ -33,8 +32,8 @@ import com.google.common.collect.ImmutableMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.OptionalInt;
 
-import static com.facebook.presto.SystemSessionProperties.getHashPartitionCount;
 import static com.facebook.presto.spi.StandardErrorCode.NO_NODES_AVAILABLE;
 import static com.facebook.presto.util.Failures.checkCondition;
 import static com.google.common.base.MoreObjects.toStringHelper;
@@ -46,7 +45,7 @@ public final class SystemPartitioningHandle
 {
     public static Partitioning singlePartition()
     {
-        return createSystemPartitioning(SystemPartitioning.FIXED, SystemPartitionFunction.SINGLE, ImmutableList.of());
+        return createSystemPartitioning(SystemPartitioning.FIXED, SystemPartitionFunction.SINGLE, OptionalInt.of(1), ImmutableList.of());
     }
 
     public static boolean isSinglePartitioning(Partitioning partitioning)
@@ -61,12 +60,12 @@ public final class SystemPartitioningHandle
 
     public static Partitioning coordinatorOnlyPartition()
     {
-        return createSystemPartitioning(SystemPartitioning.COORDINATOR_ONLY, SystemPartitionFunction.SINGLE, ImmutableList.of());
+        return createSystemPartitioning(SystemPartitioning.COORDINATOR_ONLY, SystemPartitionFunction.SINGLE, OptionalInt.of(1), ImmutableList.of());
     }
 
-    public static Partitioning fixedHashPartitioning(List<Symbol> columns)
+    public static Partitioning fixedHashPartitioning(int partitionCount, List<Symbol> columns)
     {
-        return createSystemPartitioning(SystemPartitioning.FIXED, SystemPartitionFunction.HASH, columns);
+        return createSystemPartitioning(SystemPartitioning.FIXED, SystemPartitionFunction.HASH, OptionalInt.of(partitionCount), columns);
     }
 
     public static boolean isFixedHashPartitioning(Partitioning partitioning)
@@ -74,9 +73,9 @@ public final class SystemPartitioningHandle
         return isSystemPartitioning(partitioning, SystemPartitionFunction.HASH);
     }
 
-    public static Partitioning fixedRandomPartitioning()
+    public static Partitioning fixedRandomPartitioning(int partitionCount)
     {
-        return createSystemPartitioning(SystemPartitioning.FIXED, SystemPartitionFunction.ROUND_ROBIN);
+        return createSystemPartitioning(SystemPartitioning.FIXED, SystemPartitionFunction.ROUND_ROBIN, OptionalInt.of(partitionCount));
     }
 
     public static boolean isFixedRandomPartitioning(Partitioning partitioning)
@@ -84,9 +83,9 @@ public final class SystemPartitioningHandle
         return isSystemPartitioning(partitioning, SystemPartitionFunction.ROUND_ROBIN);
     }
 
-    public static Partitioning fixedBroadcastPartitioning()
+    public static Partitioning fixedBroadcastPartitioning(int partitionCount)
     {
-        return createSystemPartitioning(SystemPartitioning.FIXED, SystemPartitionFunction.BROADCAST);
+        return createSystemPartitioning(SystemPartitioning.FIXED, SystemPartitionFunction.BROADCAST, OptionalInt.of(partitionCount));
     }
 
     public static boolean isFixedBroadcastPartitioning(Partitioning partitioning)
@@ -101,12 +100,12 @@ public final class SystemPartitioningHandle
 
     public static Partitioning unknownPartitioning()
     {
-        return createSystemPartitioning(SystemPartitioning.UNKNOWN, SystemPartitionFunction.UNKNOWN);
+        return createSystemPartitioning(SystemPartitioning.UNKNOWN, SystemPartitionFunction.UNKNOWN, OptionalInt.empty());
     }
 
     public static Partitioning unknownPartitioning(List<Symbol> columns)
     {
-        return createSystemPartitioning(SystemPartitioning.UNKNOWN, SystemPartitionFunction.UNKNOWN, columns);
+        return createSystemPartitioning(SystemPartitioning.UNKNOWN, SystemPartitionFunction.UNKNOWN, OptionalInt.empty(), columns);
     }
 
     public static boolean isUnknownPartitioning(PartitioningHandle partitioningHandle)
@@ -127,6 +126,15 @@ public final class SystemPartitioningHandle
         return false;
     }
 
+    public static OptionalInt getSystemPartitionCount(Partitioning partitioning)
+    {
+        ConnectorPartitioningHandle connectorHandle = partitioning.getHandle().getConnectorHandle();
+        if (connectorHandle instanceof SystemPartitioningHandle) {
+            return ((SystemPartitioningHandle) connectorHandle).getPartitionCount();
+        }
+        return OptionalInt.empty();
+    }
+
     private enum SystemPartitioning
     {
         FIXED,
@@ -134,26 +142,30 @@ public final class SystemPartitioningHandle
         COORDINATOR_ONLY
     }
 
-    private static Partitioning createSystemPartitioning(SystemPartitioning partitioning, SystemPartitionFunction function)
+    private static Partitioning createSystemPartitioning(SystemPartitioning partitioning, SystemPartitionFunction function, OptionalInt partitionCount)
     {
-        return createSystemPartitioning(partitioning, function, ImmutableList.of());
+        return createSystemPartitioning(partitioning, function, partitionCount, ImmutableList.of());
     }
 
-    private static Partitioning createSystemPartitioning(SystemPartitioning partitioning, SystemPartitionFunction function, List<Symbol> columns)
+    private static Partitioning createSystemPartitioning(SystemPartitioning partitioning, SystemPartitionFunction function, OptionalInt partitionCount, List<Symbol> columns)
     {
-        return Partitioning.create(new PartitioningHandle(Optional.empty(), Optional.empty(), new SystemPartitioningHandle(partitioning, function)), columns);
+        return Partitioning.create(new PartitioningHandle(Optional.empty(), Optional.empty(), new SystemPartitioningHandle(partitioning, function, partitionCount)), columns);
     }
 
     private final SystemPartitioning partitioning;
     private final SystemPartitionFunction function;
+    private final OptionalInt partitionCount;
 
     @JsonCreator
     public SystemPartitioningHandle(
             @JsonProperty("partitioning") SystemPartitioning partitioning,
-            @JsonProperty("function") SystemPartitionFunction function)
+            @JsonProperty("function") SystemPartitionFunction function,
+            @JsonProperty("partitionCount") OptionalInt partitionCount)
     {
         this.partitioning = requireNonNull(partitioning, "partitioning is null");
         this.function = requireNonNull(function, "function is null");
+        this.partitionCount = requireNonNull(partitionCount, "partitionCount is null");
+        checkArgument(!partitionCount.isPresent() || partitionCount.getAsInt() > 0);
     }
 
     @JsonProperty
@@ -168,10 +180,16 @@ public final class SystemPartitioningHandle
         return function;
     }
 
+    @JsonProperty
+    public OptionalInt getPartitionCount()
+    {
+        return partitionCount;
+    }
+
     @Override
     public boolean isSingleNode()
     {
-        return partitioning == SystemPartitioning.COORDINATOR_ONLY || function == SystemPartitionFunction.SINGLE;
+        return partitionCount.isPresent() && partitionCount.getAsInt() == 1;
     }
 
     @Override
@@ -204,23 +222,24 @@ public final class SystemPartitioningHandle
     public String toString()
     {
         if (partitioning == SystemPartitioning.FIXED) {
-            return function.toString();
+            String functionName = function.toString();
+            if (function != SystemPartitionFunction.SINGLE && partitionCount.isPresent()) {
+                functionName += "_" + partitionCount.getAsInt();
+            }
+            return functionName;
         }
         return partitioning.toString();
     }
 
-    public NodePartitionMap getNodePartitionMap(Session session, NodeScheduler nodeScheduler)
+    public NodePartitionMap getNodePartitionMap(NodeScheduler nodeScheduler)
     {
         NodeSelector nodeSelector = nodeScheduler.createNodeSelector(null);
         List<Node> nodes;
         if (partitioning == SystemPartitioning.COORDINATOR_ONLY) {
             nodes = ImmutableList.of(nodeSelector.selectCurrentNode());
         }
-        else if (function == SystemPartitionFunction.SINGLE) {
-            nodes = nodeSelector.selectRandomNodes(1);
-        }
         else if (partitioning == SystemPartitioning.FIXED) {
-            nodes = nodeSelector.selectRandomNodes(getHashPartitionCount(session));
+            nodes = nodeSelector.selectRandomNodes(partitionCount.getAsInt());
         }
         else {
             throw new IllegalArgumentException("Unsupported plan distribution " + partitioning);
@@ -247,7 +266,7 @@ public final class SystemPartitioningHandle
         return new PartitionFunction(bucketFunction, bucketToPartition);
     }
 
-    enum SystemPartitionFunction
+    private enum SystemPartitionFunction
     {
         SINGLE {
             @Override

commit 6c44dfbaf789d4ebc47f3e5615399a8849347e68

Force single writer when partitioning scheme exists
This fixes a regression introduced by 4ed06d97

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/AddLocalExchanges.java
@@ -424,7 +424,8 @@ public class AddLocalExchanges
         {
             StreamPreferredProperties requiredProperties;
             StreamPreferredProperties preferredProperties;
-            if (getTaskWriterCount(session) > 1) {
+            // Force single writer when partition function exists
+            if (getTaskWriterCount(session) > 1 && !node.getPartitioningScheme().isPresent()) {
                 requiredProperties = fixedParallelism(getTaskWriterCount(session));
                 preferredProperties = fixedParallelism(getTaskWriterCount(session));
             }