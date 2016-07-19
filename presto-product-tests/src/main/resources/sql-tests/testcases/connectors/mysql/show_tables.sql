516db3d170b1fe9c6d63ae6dd95edff86071d358
Maciej Grzybek <maciej.grzybek@teradata.com>
2016-07-19 13:31:14 -0400

    Change MySQL product tests to reuse MySQL specific table
    MySQL specific table was introduced in previous commit.
    The same table can be used for all other tests.

--- presto-product-tests/src/main/resources/sql-tests/testcases/connectors/mysql/show_tables.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/connectors/mysql/show_tables.sql
@@ -1,8 +1,8 @@
--- database: presto; groups: mysql_connector; tables: mysql.workers_jdbc, mysql.datatype_jdbc
+-- database: presto; groups: mysql_connector; tables: mysql.workers_mysql, mysql.datatype_jdbc
 -- queryType: SELECT;
 --!
 show tables from mysql.test
 --!
 -- delimiter: |; trimValues: true; ignoreOrder: true;
 datatype_jdbc|
-workers_jdbc|
+workers_mysql|
