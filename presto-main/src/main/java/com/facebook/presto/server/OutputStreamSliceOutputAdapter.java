05a163f4234172253eb7776f6e000081268c6c47
Floria Kluster <floria.kluster@gmx.de>
2016-07-21 01:30:50 +0200

    Fixing checkstyle errors and adding license headers

--- presto-main/src/main/java/com/facebook/presto/server/OutputStreamSliceOutputAdapter.java
+++ presto-main/src/main/java/com/facebook/presto/server/OutputStreamSliceOutputAdapter.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.facebook.presto.server;
 
 import io.airlift.slice.RuntimeIOException;
@@ -14,7 +27,6 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.primitives.Ints.checkedCast;
 import static io.airlift.slice.SizeOf.SIZE_OF_BYTE;
 import static io.airlift.slice.SizeOf.SIZE_OF_DOUBLE;
@@ -50,7 +62,9 @@ public final class OutputStreamSliceOutputAdapter
     public OutputStreamSliceOutputAdapter(OutputStream outputStream, int bufferSize)
     {
         checkArgument(bufferSize >= MINIMUM_CHUNK_SIZE, "minimum buffer size of " + MINIMUM_CHUNK_SIZE + " required");
-        checkNotNull(outputStream, "outputStream is null");
+        if (outputStream == null) {
+            throw new NullPointerException("outputStream is null");
+        }
 
         this.outputStream = outputStream;
         this.buffer = new byte[bufferSize];
