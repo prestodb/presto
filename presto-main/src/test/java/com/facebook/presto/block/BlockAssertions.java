8f2ab1720d6c0e69d8989a2c8fb8df3a96f3d73d
Karol Sobczak <Karol.Sobczak@Teradata.com>
2016-07-27 12:01:39 +0200

    Add decimal casts benchmark

--- presto-main/src/test/java/com/facebook/presto/block/BlockAssertions.java
+++ presto-main/src/test/java/com/facebook/presto/block/BlockAssertions.java
@@ -24,6 +24,7 @@ import com.facebook.presto.type.ArrayType;
 import io.airlift.slice.Slice;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -484,9 +485,10 @@ public final class BlockAssertions
     public static Block createShortDecimalSequenceBlock(int start, int end, DecimalType type)
     {
         BlockBuilder builder = type.createFixedSizeBlockBuilder(end - start);
+        long base = BigInteger.TEN.pow(type.getScale()).longValue();
 
         for (int i = start; i < end; ++i) {
-            type.writeLong(builder, i);
+            type.writeLong(builder, base * i);
         }
 
         return builder.build();
@@ -495,9 +497,10 @@ public final class BlockAssertions
     public static Block createLongDecimalSequenceBlock(int start, int end, DecimalType type)
     {
         BlockBuilder builder = type.createFixedSizeBlockBuilder(end - start);
+        BigInteger base = BigInteger.TEN.pow(type.getScale());
 
         for (int i = start; i < end; ++i) {
-            type.writeSlice(builder, encodeUnscaledValue(i));
+            type.writeSlice(builder, encodeUnscaledValue(BigInteger.valueOf(i).multiply(base)));
         }
 
         return builder.build();
