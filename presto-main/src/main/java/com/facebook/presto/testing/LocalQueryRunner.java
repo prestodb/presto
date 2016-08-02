d83bb4c29e12f7fb55d0e5de8950b0416771d753
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-08-02 18:52:41 +0200

    Compile JoinFilterFunction implementation

--- presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
+++ presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
@@ -95,6 +95,7 @@ import com.facebook.presto.sql.analyzer.Analyzer;
 import com.facebook.presto.sql.analyzer.FeaturesConfig;
 import com.facebook.presto.sql.analyzer.QueryExplainer;
 import com.facebook.presto.sql.gen.ExpressionCompiler;
+import com.facebook.presto.sql.gen.JoinFilterFunctionCompiler;
 import com.facebook.presto.sql.parser.SqlParser;
 import com.facebook.presto.sql.planner.CompilerConfig;
 import com.facebook.presto.sql.planner.LocalExecutionPlanner;
@@ -194,7 +195,8 @@ public class LocalQueryRunner
     private final PageSinkManager pageSinkManager;
     private final TransactionManager transactionManager;
 
-    private final ExpressionCompiler compiler;
+    private final ExpressionCompiler expressionCompiler;
+    private final JoinFilterFunctionCompiler joinFilterFunctionCompiler;
     private final ConnectorManager connectorManager;
     private final ImmutableMap<Class<? extends Statement>, DataDefinitionTask<?>> dataDefinitionTask;
 
@@ -251,7 +253,8 @@ public class LocalQueryRunner
         this.eventListener = new TestingEventListenerManager();
         this.pageSourceManager = new PageSourceManager();
 
-        this.compiler = new ExpressionCompiler(metadata);
+        this.expressionCompiler = new ExpressionCompiler(metadata);
+        this.joinFilterFunctionCompiler = new JoinFilterFunctionCompiler(metadata);
 
         this.connectorManager = new ConnectorManager(
                 metadata,
@@ -526,7 +529,8 @@ public class LocalQueryRunner
                 nodePartitioningManager,
                 pageSinkManager,
                 null,
-                compiler,
+                expressionCompiler,
+                joinFilterFunctionCompiler,
                 new IndexJoinLookupStats(),
                 new CompilerConfig().setInterpreterEnabled(false), // make sure tests fail if compiler breaks
                 new TaskManagerConfig().setTaskConcurrency(4));
