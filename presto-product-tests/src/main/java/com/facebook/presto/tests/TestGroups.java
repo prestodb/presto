8b828c26e062c126ba0b519cb8ac359f4a444a50
Christina Wallin <christina.wallin@teradata.com>
2016-08-09 17:34:59 -0400

    Add tests for long prepared statements

--- presto-product-tests/src/main/java/com/facebook/presto/tests/TestGroups.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/TestGroups.java
@@ -54,6 +54,7 @@ public final class TestGroups
     public static final String CASSANDRA = "cassandra";
     public static final String LDAP = "ldap";
     public static final String LDAP_CLI = "ldap_cli";
+    public static final String PREPARED_STATEMENTS = "prepared_statements";
 
     private TestGroups() {}
 }
