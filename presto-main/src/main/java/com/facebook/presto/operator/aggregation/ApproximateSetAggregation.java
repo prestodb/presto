dc8d2875e7b84d953029d277907c40a5e5375eef
Philipp Unterbrunner <philippu@fb.com>
2016-08-19 16:42:15 -0700

    Added empty_approx_set() and related tests

--- presto-main/src/main/java/com/facebook/presto/operator/aggregation/ApproximateSetAggregation.java
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/ApproximateSetAggregation.java
@@ -30,7 +30,7 @@ import io.airlift.stats.cardinality.HyperLogLog;
 @AggregationFunction("approx_set")
 public final class ApproximateSetAggregation
 {
-    private static final int NUMBER_OF_BUCKETS = 4096;
+    public static final int NUMBER_OF_BUCKETS = 4096;
     private static final AccumulatorStateSerializer<HyperLogLogState> SERIALIZER = new StateCompiler().generateStateSerializer(HyperLogLogState.class);
 
     private ApproximateSetAggregation() {}
