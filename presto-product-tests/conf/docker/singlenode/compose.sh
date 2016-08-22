68488de2fdb944ecdff6d49fafb7dee57bcd1f68
Artur Gajowy <artur.gajowy@gmail.com>
2016-08-22 16:37:56 +0200

    Reorganize docker volumes to only mount necessary parts of presto
    This makes the volume definitions say more clearly what files
    are needed to run the product tests.
    
    The new volume layout is:
    
    /docker/volumes
    |-- presto-server -> $PRESTO_SERVER_DIR
    |-- presto-cli/presto-cli-executable.jar -> $PRESTO_CLI_JAR
    |-- presto-product-tests/presto-product-tests-executable.jar
    |   -> $PRODUCT_TESTS_JAR
    |-- conf -> presto-product-tests/conf
    |-- tempto/tempto-configuration-local.yaml
    |   -> one of the .yaml in presto-product-tests/conf/tempto
    `-- test-reports -> presto-product-tests/target/test-reports
    
    Only the relevant volumes are mounted for each container.

--- presto-product-tests/conf/docker/singlenode/compose.sh
+++ presto-product-tests/conf/docker/singlenode/compose.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+source ${BASH_SOURCE%/*}/../../../target/classes/presto.env
+export PRESTO_VERSION
+
 docker-compose \
 -f ${BASH_SOURCE%/*}/../common/standard.yml \
 -f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
