c67f92eb6af8aea2fbd1ffdd37c885f6417aee04
Christopher Berner <christopherberner@gmail.com>
2016-08-24 18:26:43 -0700

    Add JavaDoc to ResourceGroup

--- presto-main/src/main/java/com/facebook/presto/execution/resourceGroups/ResourceGroup.java
+++ presto-main/src/main/java/com/facebook/presto/execution/resourceGroups/ResourceGroup.java
@@ -25,37 +25,68 @@ public interface ResourceGroup
 
     DataSize getSoftMemoryLimit();
 
+    /**
+     * Threshold on total distributed memory usage after which new queries
+     * will queue instead of starting.
+     */
     void setSoftMemoryLimit(DataSize limit);
 
     Duration getSoftCpuLimit();
 
+    /**
+     * Threshold on total distributed CPU usage after which max running queries will be reduced.
+     */
     void setSoftCpuLimit(Duration limit);
 
     Duration getHardCpuLimit();
 
+    /**
+     * Threshold on total distributed CPU usage after which new queries
+     * will queue instead of starting.
+     */
     void setHardCpuLimit(Duration limit);
 
     long getCpuQuotaGenerationMillisPerSecond();
 
+    /**
+     * Rate at which distributed CPU usage quota regenerates.
+     */
     void setCpuQuotaGenerationMillisPerSecond(long rate);
 
     int getMaxRunningQueries();
 
+    /**
+     * Maximum number of concurrently running queries, after which
+     * new queries will queue instead of starting.
+     */
     void setMaxRunningQueries(int maxRunningQueries);
 
     int getMaxQueuedQueries();
 
+    /**
+     * Maximum number of queued queries after which submitted queries will be rejected.
+     */
     void setMaxQueuedQueries(int maxQueuedQueries);
 
     int getSchedulingWeight();
 
+    /**
+     * Scheduling weight of this group in its parent group.
+     */
     void setSchedulingWeight(int weight);
 
     SchedulingPolicy getSchedulingPolicy();
 
+    /**
+     * Scheduling policy to use when dividing resources among child resource groups,
+     * or among queries submitted to this group.
+     */
     void setSchedulingPolicy(SchedulingPolicy policy);
 
     boolean getJmxExport();
 
+    /**
+     * Whether to export statistics about this group and allow configuration via JMX.
+     */
     void setJmxExport(boolean export);
 }
