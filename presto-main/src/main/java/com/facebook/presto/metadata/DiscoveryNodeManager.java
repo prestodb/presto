e9402ba1e3ae1a7ee3d1733c42c75205863b4215
kuldeepsidhu88 <kuldeepsidhu88@users.noreply.github.com>
2016-08-25 10:08:21 +0530

    Update node version mismatch message

--- presto-main/src/main/java/com/facebook/presto/metadata/DiscoveryNodeManager.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/DiscoveryNodeManager.java
@@ -181,7 +181,7 @@ public final class DiscoveryNodeManager
                 // record current node
                 if (node.getNodeIdentifier().equals(nodeInfo.getNodeId())) {
                     currentNode = node;
-                    checkState(currentNode.getNodeVersion().equals(expectedNodeVersion), "INVARIANT: current node version should be equal to expected node version");
+                    checkState(currentNode.getNodeVersion().equals(expectedNodeVersion), "INVARIANT: current node version (%s) should be equal to %s", currentNode.getNodeVersion(), expectedNodeVersion);
                 }
 
                 switch (nodeState) {
