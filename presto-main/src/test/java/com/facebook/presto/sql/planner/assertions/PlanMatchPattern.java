6ce4c7a6cb4669d2909d1cf4a7ed4de33b02c367
Eric Diven <eric.diven@teradata.com>
2016-08-26 14:23:49 -0400

    Verify window specification in plan and functions/frames in AST
    The FunctionCalls in WindowNodes come directly from the AST, and are not filled
    in with defaults when the user omits e.g. the orderings in a query. The DSL for
    the unit tests should verify as much information about the window from the plan
    and only the function calls and their associated frame information in the AST.
    
    Conflicts:
    	presto-main/src/test/java/com/facebook/presto/sql/planner/assertions/PlanMatchPattern.java

--- presto-main/src/test/java/com/facebook/presto/sql/planner/assertions/PlanMatchPattern.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/assertions/PlanMatchPattern.java
@@ -17,6 +17,7 @@ import com.facebook.presto.Session;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.spi.predicate.Domain;
 import com.facebook.presto.sql.parser.SqlParser;
+import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.plan.ApplyNode;
 import com.facebook.presto.sql.planner.plan.FilterNode;
 import com.facebook.presto.sql.planner.plan.JoinNode;
@@ -29,15 +30,17 @@ import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.FunctionCall;
 import com.facebook.presto.sql.tree.QualifiedName;
 import com.facebook.presto.sql.tree.SymbolReference;
-import com.facebook.presto.sql.tree.Window;
+import com.facebook.presto.sql.tree.WindowFrame;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
 
 import static com.facebook.presto.util.ImmutableCollectors.toImmutableList;
 import static com.google.common.base.Preconditions.checkState;
@@ -86,9 +89,9 @@ public final class PlanMatchPattern
         return node(TableScanNode.class).with(new TableScanMatcher(expectedTableName, constraint));
     }
 
-    public static PlanMatchPattern window(List<FunctionCall> functionCalls, PlanMatchPattern source)
+    public static PlanMatchPattern window(WindowNode.Specification specification, List<FunctionCall> functionCalls, PlanMatchPattern source)
     {
-        return node(WindowNode.class, source).with(new WindowMatcher(functionCalls));
+        return any(source).with(new WindowMatcher(specification, functionCalls));
     }
 
     public static PlanMatchPattern project(PlanMatchPattern source)
@@ -169,29 +172,15 @@ public final class PlanMatchPattern
         return sourcePatterns.isEmpty();
     }
 
-    private static List<Expression> toExpressionList(String... args)
+    /*
+     * Caveat Emptor: FunctionCall's come from the parser, and represent what
+     * the user has typed. As such, they don't contain a WindowFrame if one
+     * isn't specified in the SQL. In this case, the correct value to pass is
+     * Optional.empty()
+     */
+    public static FunctionCall functionCall(String name, Optional<WindowFrame> frame, SymbolReference... args)
     {
-        ImmutableList.Builder<Expression> builder = ImmutableList.builder();
-        for (String arg : args) {
-            if (arg.equals("*")) {
-                builder.add(new PlanMatchPattern.AnySymbolReference());
-            }
-            else {
-                builder.add(new SymbolReference(arg));
-            }
-        }
-
-        return builder.build();
-    }
-
-    public static FunctionCall functionCall(String name, Window window, boolean distinct, String... args)
-    {
-        return new FunctionCall(QualifiedName.of(name), Optional.of(window), distinct, toExpressionList(args));
-    }
-
-    public static FunctionCall functionCall(String name, String... args)
-    {
-        return new RelaxedEqualityFunctionCall(QualifiedName.of(name), toExpressionList(args));
+        return new RelaxedEqualityFunctionCall(QualifiedName.of(name), Arrays.asList(args), frame);
     }
 
     @Override
@@ -247,10 +236,144 @@ public final class PlanMatchPattern
         return Strings.repeat("    ", indent);
     }
 
-    private static class AnySymbolReference
-            extends SymbolReference
+    public static SymbolStem symbolStem(String stem)
     {
-        AnySymbolReference()
+        return new SymbolStem(stem);
+    }
+
+    /*
+     * All comments below about Symbols apply to SymbolReferences as well.
+     *
+     * This is the result of a compromise to satisfy as many of the following
+     * constraints as possible:
+     *
+     * 1) Many objects we want to compare in plans already define equals
+     * methods. We should use these to the greatest degree possible in the
+     * various Matchers, because there's no test-specific code that can get
+     * out of date.
+     * 2) Many of said objects contain either Symbols.
+     * 3) Symbols in the final Plan may be decorated by the SymbolAllocator to
+     * make them unique as needed.
+     * 4) There's no way to know a-priori what the decorated name of a Symbol
+     * will be in the plan. Even if we could, future changes to the planner
+     * might change the name of the Symbols in the plan, rendering the expected
+     * values in tests that depended on them out of date.
+     *
+     * The solution to writing expected plans that match actual plans and are
+     * as self-maintaining as possible is to extend Symbol so that the
+     * following returns true:
+     *   expectedSymbol.equals(actualDecoratedSymbol)
+     *
+     * This ensures that we can construct expected objects containing Symbols
+     * and use their built-in equals() methods to compare them to the actual
+     * objects in the plan.
+     *
+     * To actually implement this, we need to handle comparing the equality
+     * comparison between e.g. column_name and column_name_829. This is done by
+     * treating column_name as a stem and making sure that everything prior to
+     * the last underscore in column_name_829 is the same as the stem.
+     *
+     * This basically reverses the transformation SymbolAllocator does to
+     * create the unique column_name_829 Symbol name in the first place.
+     *
+     * A different approach to this that is problematic is to have a
+     * TestSymbolAllocator that keeps track of the reverse mappings in a Map
+     * having entries such as "column_name_927" -> "column_name". The issue
+     * with this is that constructing the expected plan for a query would have
+     * to depend on the actual plan, which is what actually has the
+     * SymbolAllocator. Having the expected value for a test depend on the
+     * actual value seems fishy.
+     */
+    private interface Stem<T>
+    {
+        String getStem();
+        String getOtherName(T other);
+
+        default boolean stemEquals(Object o, Class<T> clazz, Function<T, String> getName)
+        {
+            if (this == o) {
+                return true;
+            }
+
+            if (o == null) {
+                return false;
+            }
+
+            if (!clazz.equals(o.getClass())) {
+                return false;
+            }
+
+            T other = clazz.cast(o);
+            String name = getOtherName(other);
+            int endIndex = name.lastIndexOf('_');
+            String otherStem = endIndex == -1 ? name : name.substring(0, endIndex);
+            return getStem().equals(otherStem);
+        }
+    }
+
+    private static class SymbolStem extends Symbol
+        implements Stem<Symbol>
+    {
+        private final String stem;
+
+        private SymbolStem(String stem)
+        {
+            super(stem + "_*");
+            this.stem = requireNonNull(stem);
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            // Inconsistent with hashCode. See below.
+            return stemEquals(o, Symbol.class, Symbol::getName);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            /*
+             * hashCode() and equals() are inconsistent with each other. In theory, we'd like to
+             * throw UnsupportedOperationException here. We can't, however because creating a
+             * WindowNode.Specification with more than one Ordering requires being able to put
+             * Symbols in a hash table.
+             *
+             * It turns out that this being inconsistent with equals ends up not mattering. We
+             * can't meaningfully do an equals comparison on the hash tables representing the
+             * expected Orderings and the actual Orderings because that would require knowing
+             * the mangled symbols in the expected value a priori. Instead, we do an equals
+             * comparison that relies solely on equals() returning true for mangled and
+             * non-mangled symbols
+             */
+            return stem.hashCode();
+        }
+
+        @Override
+        public String getStem()
+        {
+            return stem;
+        }
+
+        @Override
+        public String getOtherName(Symbol other)
+        {
+            return other.getName();
+        }
+    }
+
+    public static SymbolReference anySymbolReference()
+    {
+        return new AnySymbolReference();
+    }
+
+    public static SymbolReference symbolReferenceStem(String stem)
+    {
+        return new SymbolReferenceStem(stem);
+    }
+
+    private static class AnySymbolReference extends SymbolReference
+    {
+        private AnySymbolReference()
         {
             super("*");
         }
@@ -262,7 +385,7 @@ public final class PlanMatchPattern
                 return true;
             }
 
-            if (o == null || !SymbolReference.class.isInstance(o)) {
+            if (o == null || !SymbolReference.class.equals(o.getClass())) {
                 return false;
             }
 
@@ -276,12 +399,57 @@ public final class PlanMatchPattern
         }
     }
 
-    private static class RelaxedEqualityFunctionCall
-            extends FunctionCall
+    private static class SymbolReferenceStem extends SymbolReference
+            implements Stem<SymbolReference>
     {
-        RelaxedEqualityFunctionCall(QualifiedName name, List<Expression> arguments)
+        private final String stem;
+
+        private SymbolReferenceStem(String stem)
+        {
+            super(stem + "_*");
+            this.stem = requireNonNull(stem);
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            return stemEquals(o, SymbolReference.class, SymbolReference::getName);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getStem()
+        {
+            return stem;
+        }
+
+        @Override
+        public String getOtherName(SymbolReference other)
+        {
+            return other.getName();
+        }
+    }
+
+    private static class RelaxedEqualityFunctionCall extends FunctionCall
+    {
+        /*
+         * FunctionCalls for window functions must have a Window. By the time
+         * the we've gone through the optimizers, everything but the Frame
+         * has a corresponding entry in the WindowNode. Since the WindowNode
+         * has had default entries filled in, we only need to compare the
+         * window frame with the one in the FunctionCall in equals.
+         */
+        Optional<WindowFrame> frame;
+
+        private RelaxedEqualityFunctionCall(QualifiedName name, List<Expression> arguments, Optional<WindowFrame> frame)
         {
             super(name, arguments);
+            this.frame = requireNonNull(frame);
         }
 
         @Override
@@ -290,18 +458,29 @@ public final class PlanMatchPattern
             if (this == obj) {
                 return true;
             }
+
             if (obj == null || obj.getClass() != FunctionCall.class) {
                 return false;
             }
+
             FunctionCall o = (FunctionCall) obj;
+
+            /*
+             * A FunctionCall representing a window function must have a
+             * window. If not, there's a failure somewhere upstream.
+             */
+            checkState(o.getWindow().isPresent());
+
             return Objects.equals(getName(), o.getName()) &&
-                    Objects.equals(getArguments(), o.getArguments());
+                    Objects.equals(getArguments(), o.getArguments()) &&
+                    Objects.equals(frame, o.getWindow().get().getFrame());
         }
 
         @Override
         public int hashCode()
         {
-            return Objects.hash(getName(), getArguments());
+            // This is a test object. We shouldn't put it in a hash table.
+            throw new UnsupportedOperationException();
         }
     }
 }
--- presto-main/src/test/java/com/facebook/presto/sql/planner/assertions/WindowMatcher.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/assertions/WindowMatcher.java
@@ -16,26 +16,91 @@ package com.facebook.presto.sql.planner.assertions;
 
 import com.facebook.presto.Session;
 import com.facebook.presto.metadata.Metadata;
+import com.facebook.presto.spi.block.SortOrder;
+import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.WindowNode;
 import com.facebook.presto.sql.tree.FunctionCall;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import static com.facebook.presto.util.ImmutableCollectors.toImmutableList;
 import static java.util.Objects.requireNonNull;
 
 final class WindowMatcher
         implements Matcher
 {
+    private final WindowNode.Specification specification;
     private final List<FunctionCall> functionCalls;
 
-    WindowMatcher(List<FunctionCall> functionCalls)
+    WindowMatcher(WindowNode.Specification specification, List<FunctionCall> functionCalls)
     {
+        this.specification = specification;
         this.functionCalls = ImmutableList.copyOf(requireNonNull(functionCalls, "functionCalls is null"));
+    }
+
+    private static boolean samePartitionAndOrder(
+            WindowNode.Specification expected,
+            WindowNode.Specification actual)
+    {
+        if (!expected.getPartitionBy().equals(actual.getPartitionBy()) ||
+                !expected.getOrderBy().equals(actual.getOrderBy())) {
+            return false;
+        }
+
+        Map<Symbol, SortOrder> expectedOrderings = expected.getOrderings();
+        Map<Symbol, SortOrder> actualOrderings = actual.getOrderings();
+
+        if (expectedOrderings.size() != actualOrderings.size()) {
+            return false;
+        }
+
+        /*
+         * Ooof. Sad story time.
+         *
+         * Symbols in the plan get decorated with a number if the same symbol is used twice. E.g. if two parts
+         * of a query both refer to a column foo, you'll end up with symbols foo and foo_32 (for example).
+         *
+         * When we verify the orderings in a plan, the orderings take the form of a Map<Symbol, SortOrder>.
+         * Unfortunately, we have no way of knowing a priori what the number affixed to the symbol name, and
+         * even if we did, we'd be relying on implementation details of the SymbolAllocator.
+         *
+         * What's further unfortunate is that we can't just extend Symbol with something that compares equal
+         * to a Symbol with a name that has a unique id affixed. This is because we end up going through
+         * Map.equals, and if you dig deep enough you end up finding that the hashCodes would have to match too.
+         * Since we can't change how Symbol.hashCode is implemented (and wouldn't want to!), we're back to
+         * needing to know the unknowable.
+         *
+         * Another approach to making to WindowNode.Specifications compare with .equals() that doesn't work
+         * is extending Map to do the comparison in a way that doesn't rely on hashCode. That doesn't work
+         * because WindowNode.Specification copies the members of the map you pass to its constructor.
+         *
+         * Instead, we implement the comparison here in a way that doesn't rely on hashCode. This is brittle
+         * because if somebody adds another field to Specification that's included in a equals comparison,
+         * this need to be updated too.
+         */
+        Comparator<Map.Entry<Symbol, SortOrder>> entryComparator =
+                (Map.Entry<Symbol, SortOrder> x, Map.Entry<Symbol, SortOrder> y) -> x.getKey().compareTo(y.getKey());
+
+        List<Map.Entry<Symbol, SortOrder>> actualEntries = actualOrderings
+                .entrySet()
+                .stream()
+                .sorted(entryComparator)
+                .collect(toImmutableList());
+
+        List<Map.Entry<Symbol, SortOrder>> expectedEntries = expectedOrderings
+                .entrySet()
+                .stream()
+                .sorted(entryComparator)
+                .collect(toImmutableList());
+
+        return expectedEntries.equals(actualEntries);
     }
 
     @Override
@@ -46,6 +111,11 @@ final class WindowMatcher
         }
 
         WindowNode windowNode = (WindowNode) node;
+
+        if (!samePartitionAndOrder(specification, windowNode.getSpecification())) {
+            return false;
+        }
+
         LinkedList<FunctionCall> actualCalls = windowNode.getWindowFunctions().values().stream()
                 .map(WindowNode.Function::getFunctionCall)
                 .collect(Collectors.toCollection(LinkedList::new));
@@ -69,6 +139,7 @@ final class WindowMatcher
     public String toString()
     {
         return MoreObjects.toStringHelper(this)
+                .add("specification", specification)
                 .add("functionCalls", functionCalls)
                 .toString();
     }
