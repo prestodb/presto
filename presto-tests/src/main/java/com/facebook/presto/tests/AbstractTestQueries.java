a388a620dade0b9160381c4587ac2d2d497e6050
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-08-29 12:37:04 +0200

    Add missing @Test annotation for test method

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -4824,6 +4824,7 @@ public abstract class AbstractTestQueries
         assertEquals(result.getOnlyColumnAsSet(), ImmutableSet.of(getSession().getSchema().get()));
     }
 
+    @Test
     public void testShowTables()
             throws Exception
     {
