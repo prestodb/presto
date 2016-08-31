cb4e556f11fb119ed7cfd4f4c8c333bf33fc41da
Karol Sobczak <Karol.Sobczak@Teradata.com>
2016-08-31 13:47:26 +0200

    Make Char(x) comparable.
    Trailing spaces are relevant.
    Equal/Non-equal is supported for Chars with the same length.
    Inequality Char operators are supported for Chars with arbitrary lengths.

---
+++ presto-main/src/main/java/com/facebook/presto/type/CharOperators.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.type;
+
+import com.facebook.presto.spi.function.LiteralParameters;
+import com.facebook.presto.spi.function.ScalarOperator;
+import com.facebook.presto.spi.function.SqlType;
+import com.facebook.presto.spi.type.StandardTypes;
+import io.airlift.slice.Slice;
+import io.airlift.slice.XxHash64;
+
+import static com.facebook.presto.spi.function.OperatorType.BETWEEN;
+import static com.facebook.presto.spi.function.OperatorType.EQUAL;
+import static com.facebook.presto.spi.function.OperatorType.GREATER_THAN;
+import static com.facebook.presto.spi.function.OperatorType.GREATER_THAN_OR_EQUAL;
+import static com.facebook.presto.spi.function.OperatorType.HASH_CODE;
+import static com.facebook.presto.spi.function.OperatorType.LESS_THAN;
+import static com.facebook.presto.spi.function.OperatorType.LESS_THAN_OR_EQUAL;
+import static com.facebook.presto.spi.function.OperatorType.NOT_EQUAL;
+
+public final class CharOperators
+{
+    private CharOperators() {}
+
+    @LiteralParameters({"x"})
+    @ScalarOperator(EQUAL)
+    @SqlType(StandardTypes.BOOLEAN)
+    public static boolean equal(@SqlType("char(x)") Slice left, @SqlType("char(x)") Slice right)
+    {
+        return left.equals(right);
+    }
+
+    @LiteralParameters({"x"})
+    @ScalarOperator(NOT_EQUAL)
+    @SqlType(StandardTypes.BOOLEAN)
+    public static boolean notEqual(@SqlType("char(x)") Slice left, @SqlType("char(x)") Slice right)
+    {
+        return !left.equals(right);
+    }
+
+    @LiteralParameters({"x"})
+    @ScalarOperator(LESS_THAN)
+    @SqlType(StandardTypes.BOOLEAN)
+    public static boolean lessThan(@SqlType("char(x)") Slice left, @SqlType("char(x)") Slice right)
+    {
+        return left.compareTo(right) < 0;
+    }
+
+    @LiteralParameters({"x"})
+    @ScalarOperator(LESS_THAN_OR_EQUAL)
+    @SqlType(StandardTypes.BOOLEAN)
+    public static boolean lessThanOrEqual(@SqlType("char(x)") Slice left, @SqlType("char(x)") Slice right)
+    {
+        return left.compareTo(right) <= 0;
+    }
+
+    @LiteralParameters({"x"})
+    @ScalarOperator(GREATER_THAN)
+    @SqlType(StandardTypes.BOOLEAN)
+    public static boolean greaterThan(@SqlType("char(x)") Slice left, @SqlType("char(x)") Slice right)
+    {
+        return left.compareTo(right) > 0;
+    }
+
+    @LiteralParameters({"x"})
+    @ScalarOperator(GREATER_THAN_OR_EQUAL)
+    @SqlType(StandardTypes.BOOLEAN)
+    public static boolean greaterThanOrEqual(@SqlType("char(x)") Slice left, @SqlType("char(x)") Slice right)
+    {
+        return left.compareTo(right) >= 0;
+    }
+
+    @LiteralParameters({"x"})
+    @ScalarOperator(BETWEEN)
+    @SqlType(StandardTypes.BOOLEAN)
+    public static boolean between(@SqlType("char(x)") Slice value, @SqlType("char(x)") Slice min, @SqlType("char(x)") Slice max)
+    {
+        return min.compareTo(value) <= 0 && value.compareTo(max) <= 0;
+    }
+
+    @LiteralParameters("x")
+    @ScalarOperator(HASH_CODE)
+    @SqlType(StandardTypes.BIGINT)
+    public static long hashCode(@SqlType("char(x)") Slice value)
+    {
+        return XxHash64.hash(value);
+    }
+}
