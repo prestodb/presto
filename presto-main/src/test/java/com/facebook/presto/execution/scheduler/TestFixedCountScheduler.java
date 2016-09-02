3ee2b80e21821a0e11a798136054d5cff003245b
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-09-02 16:02:26 +0200

    Fix Intellij warnings for TestFixedCountScheduler
     - remove unnecessary casting
     - do not ignore result of assertions

--- presto-main/src/test/java/com/facebook/presto/execution/scheduler/TestFixedCountScheduler.java
+++ presto-main/src/test/java/com/facebook/presto/execution/scheduler/TestFixedCountScheduler.java
@@ -58,7 +58,7 @@ public class TestFixedCountScheduler
         FixedCountScheduler nodeScheduler = new FixedCountScheduler(
                 (node, partition) -> taskFactory.createTableScanTask(
                         new TaskId("test", 1, 1),
-                        (Node) node, ImmutableList.of(),
+                        node, ImmutableList.of(),
                         new PartitionedSplitCountTracker(delta -> { })),
                 generateRandomNodes(1));
 
@@ -66,7 +66,7 @@ public class TestFixedCountScheduler
         assertTrue(result.isFinished());
         assertTrue(result.getBlocked().isDone());
         assertEquals(result.getNewTasks().size(), 1);
-        result.getNewTasks().iterator().next().getNodeId().equals("other 0");
+        assertTrue(result.getNewTasks().iterator().next().getNodeId().equals("other 0"));
     }
 
     @Test
@@ -76,7 +76,7 @@ public class TestFixedCountScheduler
         FixedCountScheduler nodeScheduler = new FixedCountScheduler(
                 (node, partition) -> taskFactory.createTableScanTask(
                         new TaskId("test", 1, 1),
-                        (Node) node, ImmutableList.of(),
+                        node, ImmutableList.of(),
                         new PartitionedSplitCountTracker(delta -> { })),
                 generateRandomNodes(5));
 
