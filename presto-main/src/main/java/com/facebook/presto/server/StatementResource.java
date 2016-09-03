9c9a2559bdfff5c425ad784b5d640e0a1f2832ba
David Phillips <david@acz.org>
2016-09-03 16:48:32 -0700

    Fix warnings in StatementResource

--- presto-main/src/main/java/com/facebook/presto/server/StatementResource.java
+++ presto-main/src/main/java/com/facebook/presto/server/StatementResource.java
@@ -206,11 +206,11 @@ public class StatementResource
         ResponseBuilder response = Response.ok(queryResults);
 
         // add set session properties
-        query.getSetSessionProperties().entrySet().stream()
+        query.getSetSessionProperties().entrySet()
                 .forEach(entry -> response.header(PRESTO_SET_SESSION, entry.getKey() + '=' + entry.getValue()));
 
         // add clear session properties
-        query.getResetSessionProperties().stream()
+        query.getResetSessionProperties()
                 .forEach(name -> response.header(PRESTO_CLEAR_SESSION, name));
 
         // add added prepare statements
@@ -421,7 +421,7 @@ public class StatementResource
 
                     // Return a single value for clients that require a result.
                     columns = ImmutableList.of(new Column("result", "boolean", new ClientTypeSignature(StandardTypes.BOOLEAN, ImmutableList.of())));
-                    data = ImmutableSet.<List<Object>>of(ImmutableList.<Object>of(true));
+                    data = ImmutableSet.of(ImmutableList.of(true));
                 }
             }
 
@@ -573,11 +573,11 @@ public class StatementResource
 
         private static List<Column> createColumnsList(QueryInfo queryInfo)
         {
-            requireNonNull(queryInfo, "queryInfo is null");
-            checkArgument(queryInfo.getOutputStage().isPresent(), "outputStage is not present");
+            StageInfo outputStage = queryInfo.getOutputStage()
+                    .orElseThrow(() -> new IllegalArgumentException("outputStage not present"));
 
             List<String> names = queryInfo.getFieldNames();
-            List<Type> types = queryInfo.getOutputStage().get().getTypes();
+            List<Type> types = outputStage.getTypes();
 
             checkArgument(names.size() == types.size(), "names and types size mismatch");
 
