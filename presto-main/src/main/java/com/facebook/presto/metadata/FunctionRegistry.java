5f5d15647b7bc6291ad7505a50b55fee81701178
Wojciech Biela <Wojciech.Biela@teradata.com>
2016-09-09 11:38:33 +0200

    Round scalar functions for decimal(p,s)
    There are two variants of this method round(num) and round(num, scale).
    
    The first is a shorthand for round(num, 0). However currently those
    methods return different results in terms of output precision and scale.
    Due to an internal temporary limitation (not being able to evaluate the
    scale literal parameter at semantic analysis time) the result scale of
    the round(num) result is 0, while the precision is fitted to store a
    possible additional digit on the left. The result of round(num, scale)
    is always the original input scale and precision (+1 for the possible
    rounding result).
    
    Negative scale is permitted, allows to clear the positions left of the
    decimal dot with proper rounding.

--- presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
@@ -220,6 +220,7 @@ import static com.facebook.presto.operator.scalar.MapToJsonCast.MAP_TO_JSON;
 import static com.facebook.presto.operator.scalar.MathFunctions.DECIMAL_CEILING_FUNCTIONS;
 import static com.facebook.presto.operator.scalar.MathFunctions.DECIMAL_FLOOR_FUNCTION;
 import static com.facebook.presto.operator.scalar.MathFunctions.DECIMAL_MOD_FUNCTION;
+import static com.facebook.presto.operator.scalar.MathFunctions.DECIMAL_ROUND_FUNCTIONS;
 import static com.facebook.presto.operator.scalar.Re2JCastToRegexpFunction.castCharToRe2JRegexp;
 import static com.facebook.presto.operator.scalar.Re2JCastToRegexpFunction.castVarcharToRe2JRegexp;
 import static com.facebook.presto.operator.scalar.RowEqualOperator.ROW_EQUAL;
@@ -498,6 +499,7 @@ public class FunctionRegistry
                 .functions(DECIMAL_CEILING_FUNCTIONS)
                 .function(DECIMAL_FLOOR_FUNCTION)
                 .function(DECIMAL_MOD_FUNCTION)
+                .functions(DECIMAL_ROUND_FUNCTIONS)
                 .function(TRY_CAST);
 
         builder.function(new ArrayAggregationFunction(featuresConfig.isLegacyArrayAgg()));
