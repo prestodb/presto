953e765b62a3f44e776faccbd7ffc78f7cc3e7ef
Martin Traverso <mtraverso@gmail.com>
2016-09-25 19:24:48 -0700

    Allow parsing -ea Java versions

--- presto-main/src/test/java/com/facebook/presto/server/TestJavaVersion.java
+++ presto-main/src/test/java/com/facebook/presto/server/TestJavaVersion.java
@@ -30,6 +30,7 @@ public class TestJavaVersion
         assertEquals(JavaVersion.parse("1.8.0_5"), new JavaVersion(8, 0, OptionalInt.of(5)));
         assertEquals(JavaVersion.parse("1.8.0_20"), new JavaVersion(8, 0, OptionalInt.of(20)));
         assertEquals(JavaVersion.parse("1.8.1_25"), new JavaVersion(8, 1, OptionalInt.of(25)));
+        assertEquals(JavaVersion.parse("1.8.0_60-ea"), new JavaVersion(8, 0, OptionalInt.of(60)));
     }
 
     @Test
