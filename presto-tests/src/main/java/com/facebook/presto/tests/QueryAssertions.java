a0a39467d350e1613cdd93cfc766d018a7e23121
David Phillips <david@acz.org>
2016-09-29 18:29:06 -0700

    Fix naming in QueryAssertions.assertContains

--- presto-tests/src/main/java/com/facebook/presto/tests/QueryAssertions.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/QueryAssertions.java
@@ -155,16 +155,16 @@ public final class QueryAssertions
         }
     }
 
-    public static void assertContains(MaterializedResult actual, MaterializedResult expected)
+    public static void assertContains(MaterializedResult all, MaterializedResult expectedSubset)
     {
-        for (MaterializedRow row : expected.getMaterializedRows()) {
-            if (!actual.getMaterializedRows().contains(row)) {
-                fail(format("expected row missing: %s\nActual %s rows:\n    %s\nExpected %s rows:\n    %s\n",
+        for (MaterializedRow row : expectedSubset.getMaterializedRows()) {
+            if (!all.getMaterializedRows().contains(row)) {
+                fail(format("expected row missing: %s\nAll %s rows:\n    %s\nExpected subset %s rows:\n    %s\n",
                         row,
-                        actual.getMaterializedRows().size(),
-                        Joiner.on("\n    ").join(Iterables.limit(actual, 100)),
-                        expected.getMaterializedRows().size(),
-                        Joiner.on("\n    ").join(Iterables.limit(expected, 100))));
+                        all.getMaterializedRows().size(),
+                        Joiner.on("\n    ").join(Iterables.limit(all, 100)),
+                        expectedSubset.getMaterializedRows().size(),
+                        Joiner.on("\n    ").join(Iterables.limit(expectedSubset, 100))));
             }
         }
     }
