dain/facebook-presto

Add full distribution support to Raptor

presto-raptor/src/main/java/com/facebook/presto/raptor/RaptorBucketFunction.java
@@ -1,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.raptor;
+
+import com.facebook.presto.spi.ConnectorBucketFunction;
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.block.Block;
+import io.airlift.slice.XxHash64;
+
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class RaptorBucketFunction
+        implements ConnectorBucketFunction
+{
+    private final int bucketCount;
+
+    public RaptorBucketFunction(int bucketCount)
+    {
+        checkArgument(bucketCount > 0, "bucketCount must be at least one");
+        this.bucketCount = bucketCount;
+    }
+
+    @SuppressWarnings("NumericCastThatLosesPrecision")
+    @Override
+    public int getBucket(Page page, int position)
+    {
+        long hash = 0;
+        for (Block block : page.getBlocks()) {
+            long value = BIGINT.getLong(block, position);
+            hash = (hash * 31) + XxHash64.hash(value);
+        }
+        int value = (int) (hash & Integer.MAX_VALUE);
+        return value % bucketCount;
+    }
+}

presto-raptor/src/main/java/com/facebook/presto/raptor/RaptorModule.java
@@ -56,6 +56,7 @@ public class RaptorModule
         binder.bind(RaptorPageSourceProvider.class).in(Scopes.SINGLETON);
         binder.bind(RaptorPageSinkProvider.class).in(Scopes.SINGLETON);
         binder.bind(RaptorHandleResolver.class).in(Scopes.SINGLETON);
+        binder.bind(RaptorDistributionProvider.class).in(Scopes.SINGLETON);
         binder.bind(RaptorSessionProperties.class).in(Scopes.SINGLETON);
         binder.bind(RaptorTableProperties.class).in(Scopes.SINGLETON);
         binder.bind(NodeSupplier.class).to(RaptorNodeSupplier.class).in(Scopes.SINGLETON);

presto-raptor/src/main/java/com/facebook/presto/raptor/RaptorPageSink.java
@@ -16,6 +16,7 @@ package com.facebook.presto.raptor;
 import com.facebook.presto.raptor.metadata.ShardInfo;
 import com.facebook.presto.raptor.storage.StorageManager;
 import com.facebook.presto.raptor.util.PageBuffer;
+import com.facebook.presto.spi.ConnectorBucketFunction;
 import com.facebook.presto.spi.ConnectorPageSink;
 import com.facebook.presto.spi.Page;
 import com.facebook.presto.spi.PageBuilder;
@@ -29,7 +30,6 @@ import com.google.common.collect.ImmutableList;
 import io.airlift.json.JsonCodec;
 import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
-import io.airlift.slice.XxHash64;
 import io.airlift.units.DataSize;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
@@ -58,7 +58,6 @@ public class RaptorPageSink
     private final List<Type> columnTypes;
     private final List<Integer> sortFields;
     private final List<SortOrder> sortOrders;
-    private final OptionalInt bucketCount;
     private final int[] bucketFields;
     private final long maxBufferBytes;
 
@@ -92,10 +91,9 @@ public class RaptorPageSink
         this.sortFields = ImmutableList.copyOf(sortColumnIds.stream().map(columnIds::indexOf).collect(toList()));
         this.sortOrders = ImmutableList.copyOf(requireNonNull(sortOrders, "sortOrders is null"));
 
-        this.bucketCount = requireNonNull(bucketCount, "bucketCount is null");
         this.bucketFields = bucketColumnIds.stream().mapToInt(columnIds::indexOf).toArray();
 
-        this.pageWriter = bucketCount.isPresent() ? new BucketedPageWriter() : new SimplePageWriter();
+        this.pageWriter = bucketCount.isPresent() ? new BucketedPageWriter(bucketCount.getAsInt()) : new SimplePageWriter();
 
         for (int field : bucketFields) {
             if (!columnTypes.get(field).equals(BIGINT)) {
@@ -183,18 +181,6 @@ public class RaptorPageSink
         return new Page(blocks);
     }
 
-    @SuppressWarnings("NumericCastThatLosesPrecision")
-    private int computeBucketNumber(Page page, int position)
-    {
-        long hash = 0;
-        for (int field : bucketFields) {
-            long value = BIGINT.getLong(page.getBlock(field), position);
-            hash = (hash * 31) + XxHash64.hash(value);
-        }
-        int value = (int) (hash & Integer.MAX_VALUE);
-        return value % bucketCount.getAsInt();
-    }
-
     private interface PageWriter
     {
         void appendPage(Page page);
@@ -223,13 +209,25 @@ public class RaptorPageSink
     private class BucketedPageWriter
             implements PageWriter
     {
+        private final ConnectorBucketFunction bucketFunction;
         private final Int2ObjectMap<PageStore> pageStores = new Int2ObjectOpenHashMap<>();
+
+        public BucketedPageWriter(int bucketCount)
+        {
+            this.bucketFunction = new RaptorBucketFunction(bucketCount);
+        }
 
         @Override
         public void appendPage(Page page)
         {
+            Block[] blocks = new Block[bucketFields.length];
+            for (int i = 0; i < bucketFields.length; i++) {
+                blocks[i] = page.getBlock(bucketFields[i]);
+            }
+            Page bucketArgs = new Page(page.getPositionCount(), blocks);
+
             for (int position = 0; position < page.getPositionCount(); position++) {
-                int bucket = computeBucketNumber(page, position);
+                int bucket = bucketFunction.getBucket(bucketArgs, position);
 
                 PageStore store = pageStores.get(bucket);
                 if (store == null) {

prestodb/presto

presto-raptor/src/main/java/com/facebook/presto/raptor/RaptorBucketFunction.java
@@ -1,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.raptor;
+
+import com.facebook.presto.spi.BucketFunction;
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.block.Block;
+import io.airlift.slice.XxHash64;
+
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class RaptorBucketFunction
+        implements BucketFunction
+{
+    private final int bucketCount;
+
+    public RaptorBucketFunction(int bucketCount)
+    {
+        checkArgument(bucketCount > 0, "bucketCount must be at least one");
+        this.bucketCount = bucketCount;
+    }
+
+    @SuppressWarnings("NumericCastThatLosesPrecision")
+    @Override
+    public int getBucket(Page page, int position)
+    {
+        long hash = 0;
+        for (Block block : page.getBlocks()) {
+            long value = BIGINT.getLong(block, position);
+            hash = (hash * 31) + XxHash64.hash(value);
+        }
+        int value = (int) (hash & Integer.MAX_VALUE);
+        return value % bucketCount;
+    }
+}

presto-raptor/src/main/java/com/facebook/presto/raptor/RaptorModule.java
@@ -55,6 +55,8 @@ public class RaptorModule
         binder.bind(RaptorSplitManager.class).in(Scopes.SINGLETON);
         binder.bind(RaptorPageSourceProvider.class).in(Scopes.SINGLETON);
         binder.bind(RaptorPageSinkProvider.class).in(Scopes.SINGLETON);
+        binder.bind(RaptorHandleResolver.class).in(Scopes.SINGLETON);
+        binder.bind(RaptorNodePartitioningProvider.class).in(Scopes.SINGLETON);
         binder.bind(RaptorSessionProperties.class).in(Scopes.SINGLETON);
         binder.bind(RaptorTableProperties.class).in(Scopes.SINGLETON);
         binder.bind(NodeSupplier.class).to(RaptorNodeSupplier.class).in(Scopes.SINGLETON);

presto-raptor/src/main/java/com/facebook/presto/raptor/RaptorPageSink.java
@@ -16,6 +16,7 @@ package com.facebook.presto.raptor;
 import com.facebook.presto.raptor.metadata.ShardInfo;
 import com.facebook.presto.raptor.storage.StorageManager;
 import com.facebook.presto.raptor.util.PageBuffer;
+import com.facebook.presto.spi.BucketFunction;
 import com.facebook.presto.spi.ConnectorPageSink;
 import com.facebook.presto.spi.Page;
 import com.facebook.presto.spi.PageBuilder;
@@ -29,7 +30,6 @@ import com.google.common.collect.ImmutableList;
 import io.airlift.json.JsonCodec;
 import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
-import io.airlift.slice.XxHash64;
 import io.airlift.units.DataSize;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
@@ -58,7 +58,6 @@ public class RaptorPageSink
     private final List<Type> columnTypes;
     private final List<Integer> sortFields;
     private final List<SortOrder> sortOrders;
-    private final OptionalInt bucketCount;
     private final int[] bucketFields;
     private final long maxBufferBytes;
 
@@ -92,10 +91,9 @@ public class RaptorPageSink
         this.sortFields = ImmutableList.copyOf(sortColumnIds.stream().map(columnIds::indexOf).collect(toList()));
         this.sortOrders = ImmutableList.copyOf(requireNonNull(sortOrders, "sortOrders is null"));
 
-        this.bucketCount = requireNonNull(bucketCount, "bucketCount is null");
         this.bucketFields = bucketColumnIds.stream().mapToInt(columnIds::indexOf).toArray();
 
-        this.pageWriter = bucketCount.isPresent() ? new BucketedPageWriter() : new SimplePageWriter();
+        this.pageWriter = bucketCount.isPresent() ? new BucketedPageWriter(bucketCount.getAsInt()) : new SimplePageWriter();
 
         for (int field : bucketFields) {
             if (!columnTypes.get(field).equals(BIGINT)) {
@@ -183,18 +181,6 @@ public class RaptorPageSink
         return new Page(blocks);
     }
 
-    @SuppressWarnings("NumericCastThatLosesPrecision")
-    private int computeBucketNumber(Page page, int position)
-    {
-        long hash = 0;
-        for (int field : bucketFields) {
-            long value = BIGINT.getLong(page.getBlock(field), position);
-            hash = (hash * 31) + XxHash64.hash(value);
-        }
-        int value = (int) (hash & Integer.MAX_VALUE);
-        return value % bucketCount.getAsInt();
-    }
-
     private interface PageWriter
     {
         void appendPage(Page page);
@@ -223,13 +209,25 @@ public class RaptorPageSink
     private class BucketedPageWriter
             implements PageWriter
     {
+        private final BucketFunction bucketFunction;
         private final Int2ObjectMap<PageStore> pageStores = new Int2ObjectOpenHashMap<>();
+
+        public BucketedPageWriter(int bucketCount)
+        {
+            this.bucketFunction = new RaptorBucketFunction(bucketCount);
+        }
 
         @Override
         public void appendPage(Page page)
         {
+            Block[] blocks = new Block[bucketFields.length];
+            for (int i = 0; i < bucketFields.length; i++) {
+                blocks[i] = page.getBlock(bucketFields[i]);
+            }
+            Page bucketArgs = new Page(page.getPositionCount(), blocks);
+
             for (int position = 0; position < page.getPositionCount(); position++) {
-                int bucket = computeBucketNumber(page, position);
+                int bucket = bucketFunction.getBucket(bucketArgs, position);
 
                 PageStore store = pageStores.get(bucket);
                 if (store == null) {