d37d345f6b168a610f2dfc2879dc00bce089f72b
Christina Wallin <christina.wallin@teradata.com>
2016-10-05 10:23:59 -0400

    Fix varbinary spilling test to use varbinary blocks

--- presto-main/src/test/java/com/facebook/presto/spiller/TestBinaryFileSpiller.java
+++ presto-main/src/test/java/com/facebook/presto/spiller/TestBinaryFileSpiller.java
@@ -62,7 +62,7 @@ public class TestBinaryFileSpiller
 
         BlockBuilder col1 = BIGINT.createBlockBuilder(new BlockBuilderStatus(), 1);
         BlockBuilder col2 = DOUBLE.createBlockBuilder(new BlockBuilderStatus(), 1);
-        BlockBuilder col3 = VARCHAR.createBlockBuilder(new BlockBuilderStatus(), 1);
+        BlockBuilder col3 = VARBINARY.createBlockBuilder(new BlockBuilderStatus(), 1);
 
         col1.writeLong(42).closeEntry();
         col2.writeLong(doubleToLongBits(43.0)).closeEntry();
