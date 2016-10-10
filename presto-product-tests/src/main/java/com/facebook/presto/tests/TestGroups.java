190f2605b6e75f3bc84b5aadfa6215ec37e241f4
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-10-10 15:24:06 -0400

    Add product test that verifies HTTP port is closed on multinode-tls

--- presto-product-tests/src/main/java/com/facebook/presto/tests/TestGroups.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/TestGroups.java
@@ -57,6 +57,7 @@ public final class TestGroups
     public static final String LDAP = "ldap";
     public static final String LDAP_CLI = "ldap_cli";
     public static final String SKIP_ON_CDH = "skip_on_cdh";
+    public static final String TLS = "tls";
 
     private TestGroups() {}
 }
