574747c4928be608b4405eef069a9cf2c14f07f4
Maciej Grzybek <maciej.grzybek@teradata.com>
2016-10-13 13:53:32 -0400

    Add Cassandra to product test environments

--- presto-product-tests/conf/docker/singlenode-kerberos-hdfs-impersonation/compose.sh
+++ presto-product-tests/conf/docker/singlenode-kerberos-hdfs-impersonation/compose.sh
@@ -6,5 +6,6 @@ docker-compose \
 -f ${BASH_SOURCE%/*}/../common/standard.yml \
 -f ${BASH_SOURCE%/*}/../common/kerberos.yml \
 -f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
+-f ${BASH_SOURCE%/*}/../common/cassandra.yml \
 -f ${BASH_SOURCE%/*}/docker-compose.yml \
 "$@"
