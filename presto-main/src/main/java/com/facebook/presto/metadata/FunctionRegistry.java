ca3238c56605b26994ad90c0d7954c3e61b9a711
Anton Petrov <petroav@users.noreply.github.com>
2016-10-19 14:12:49 -0400

    Add a synthetic ListLiteralType with casts
    - The type is needed by the GROUPING() function which expects
    arguments in the form of lists. The addition of this type allows Presto
    to constant fold the arguments prior to execution instead of
    deserializing a Block at runtime for every invocation.
    - This commit also adds the necessary casts to convert from an array
    type to a list type.

--- presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
@@ -86,6 +86,7 @@ import com.facebook.presto.operator.scalar.JoniRegexpCasts;
 import com.facebook.presto.operator.scalar.JoniRegexpFunctions;
 import com.facebook.presto.operator.scalar.JsonFunctions;
 import com.facebook.presto.operator.scalar.JsonOperators;
+import com.facebook.presto.operator.scalar.ListLiteralCast;
 import com.facebook.presto.operator.scalar.MapCardinalityFunction;
 import com.facebook.presto.operator.scalar.MapDistinctFromOperator;
 import com.facebook.presto.operator.scalar.MapEqualOperator;
@@ -515,6 +516,7 @@ public class FunctionRegistry
                 .scalar(MapToMapCast.class)
                 .scalars(EmptyMapConstructor.class)
                 .scalar(TypeOfFunction.class)
+                .scalars(ListLiteralCast.class)
                 .function(ZIP_WITH_FUNCTION)
                 .functions(ZIP_FUNCTIONS)
                 .functions(ARRAY_JOIN, ARRAY_JOIN_WITH_NULL_REPLACEMENT)
