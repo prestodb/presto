8b8a34bdba937464dbae0f6904a440da2299a268
Rongrong Zhong <rongrong@fb.com>
2016-10-31 16:15:57 +0000

    Remove effectivePredicate from HiveSplit.getInfo()

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveSplit.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveSplit.java
@@ -187,7 +187,6 @@ public class HiveSplit
                 .put("table", table)
                 .put("forceLocalScheduling", forceLocalScheduling)
                 .put("partitionName", partitionName)
-                .put("effectivePredicate", effectivePredicate)
                 .build();
     }
 
