8103a241e8fc9be6fe140e27eacbae116c70eb87
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-11-10 09:06:12 +0300

    Fix checkstyle

--- presto-parser/src/test/java/com/facebook/presto/sql/parser/TestStatementBuilder.java
+++ presto-parser/src/test/java/com/facebook/presto/sql/parser/TestStatementBuilder.java
@@ -123,7 +123,7 @@ public class TestStatementBuilder
         printStatement("select * from a.b.c.e.f.g");
 
         printStatement("select \"TOTALPRICE\" \"my price\" from \"$MY\"\"ORDERS\"");
-        
+
         printStatement("select * from foo tablesample system (10+1)");
         printStatement("select * from foo tablesample system (10) join bar tablesample bernoulli (30) on a.id = b.id");
         printStatement("select * from foo tablesample system (10) join bar tablesample bernoulli (30) on not(a.id > b.id)");
