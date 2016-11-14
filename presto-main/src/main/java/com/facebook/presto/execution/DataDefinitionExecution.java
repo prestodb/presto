c6298fd48bc00c9f91be4878a57852c78947563a
Maciej Grzybek <maciej.grzybek@teradata.com>
2016-11-14 14:17:12 -0500

    Fail query on endQuery() failure

--- presto-main/src/main/java/com/facebook/presto/execution/DataDefinitionExecution.java
+++ presto-main/src/main/java/com/facebook/presto/execution/DataDefinitionExecution.java
@@ -69,14 +69,6 @@ public class DataDefinitionExecution<T extends Statement>
         this.accessControl = requireNonNull(accessControl, "accessControl is null");
         this.stateMachine = requireNonNull(stateMachine, "stateMachine is null");
         this.parameters = parameters;
-
-        stateMachine.addStateChangeListener(state -> {
-            if (statement instanceof CatalogRelatedStatement) {
-                if (state.isDone()) {
-                    notifyEndQuery();
-                }
-            }
-        });
     }
 
     private void notifyBeginQuery(Statement statement)
@@ -140,11 +132,22 @@ public class DataDefinitionExecution<T extends Statement>
 
             CompletableFuture<?> future = task.execute(statement, transactionManager, metadata, accessControl, stateMachine, parameters);
             future.whenComplete((o, throwable) -> {
-                if (throwable == null) {
+                Throwable failure = throwable;
+                if (statement instanceof CatalogRelatedStatement) {
+                    try {
+                        notifyEndQuery();
+                    }
+                    catch (Throwable t) {
+                        if (failure == null) {
+                            failure = t;
+                        }
+                    }
+                }
+                if (failure == null) {
                     stateMachine.transitionToFinishing();
                 }
                 else {
-                    fail(throwable);
+                    fail(failure);
                 }
             });
         }
