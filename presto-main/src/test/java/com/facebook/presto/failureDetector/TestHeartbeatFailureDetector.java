8a2dd966c22344cdf4c8c5b90869d9b4ed9c3de9
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-11-21 12:10:57 -0500

    Support secure internal communication

--- presto-main/src/test/java/com/facebook/presto/failureDetector/TestHeartbeatFailureDetector.java
+++ presto-main/src/test/java/com/facebook/presto/failureDetector/TestHeartbeatFailureDetector.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.failureDetector;
 
 import com.facebook.presto.execution.QueryManagerConfig;
+import com.facebook.presto.server.InternalCommunicationConfig;
 import com.google.inject.Binder;
 import com.google.inject.Injector;
 import com.google.inject.Key;
@@ -59,6 +60,7 @@ public class TestHeartbeatFailureDetector
                     @Override
                     public void configure(Binder binder)
                     {
+                        configBinder(binder).bindConfig(InternalCommunicationConfig.class);
                         configBinder(binder).bindConfig(QueryManagerConfig.class);
                         discoveryBinder(binder).bindSelector("presto");
                         discoveryBinder(binder).bindHttpAnnouncement("presto");
