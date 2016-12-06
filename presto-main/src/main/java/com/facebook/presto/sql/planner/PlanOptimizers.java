b5b470e9f74f01e583d58f9c471695f50b26a781
Martin Traverso <mtraverso@gmail.com>
2016-12-05 23:15:32 -0800

    Add EvaluateZeroLimit rule

--- presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
@@ -17,6 +17,7 @@ import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.sql.analyzer.FeaturesConfig;
 import com.facebook.presto.sql.parser.SqlParser;
 import com.facebook.presto.sql.planner.iterative.IterativeOptimizer;
+import com.facebook.presto.sql.planner.iterative.rule.EvaluateZeroLimit;
 import com.facebook.presto.sql.planner.iterative.rule.ImplementBernoulliSampleAsFilter;
 import com.facebook.presto.sql.planner.iterative.rule.SimplifyCountOverConstant;
 import com.facebook.presto.sql.planner.optimizations.AddExchanges;
@@ -83,6 +84,8 @@ public class PlanOptimizers
                 new DesugaringOptimizer(metadata, sqlParser), // Clean up all the sugar in expressions, e.g. AtTimeZone, must be run before all the other optimizers
                 new CanonicalizeExpressions(),
                 new IterativeOptimizer(ImmutableSet.of(
+                        new EvaluateZeroLimit(),
+
                         new SimplifyCountOverConstant(),
                         new ImplementBernoulliSampleAsFilter(),
                         new com.facebook.presto.sql.planner.iterative.rule.ImplementFilteredAggregations()
