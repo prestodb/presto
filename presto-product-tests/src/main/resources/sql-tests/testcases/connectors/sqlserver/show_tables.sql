57ece5aea447b4f9c44d41566ccfc976cc24c3aa
Sanjay Sharma <sanjay.sharma@teradata.com>
2016-12-14 20:04:14 -0500

    Add SQL Server convention based product-tests

---
+++ presto-product-tests/src/main/resources/sql-tests/testcases/connectors/sqlserver/show_tables.sql
@@ -0,0 +1,7 @@
+-- database: presto; groups: sqlserver; tables: sqlserver.dbo.workers_sqlserver
+-- queryType: SELECT;
+--!
+show tables from sqlserver.dbo
+--!
+-- delimiter: |; trimValues: true; ignoreExcessRows: true; ignoreOrder: true;
+workers_sqlserver|
