d2f7d511451342fe3e8a66f0a3f2a1c9e1c65bc9
Amruta Gokhale <amruta.gokhale@teradata.com>
2016-12-30 11:48:53 -0500

    Upgrade product test docker images version to 12

--- presto-product-tests/conf/docker/common/compose-commons.sh
+++ presto-product-tests/conf/docker/common/compose-commons.sh
@@ -23,7 +23,7 @@ function export_canonical_path() {
 
 source ${BASH_SOURCE%/*}/../../../bin/locations.sh
 
-export DOCKER_IMAGES_VERSION=${DOCKER_IMAGES_VERSION:-10}
+export DOCKER_IMAGES_VERSION=${DOCKER_IMAGES_VERSION:-12}
 export HADOOP_MASTER_IMAGE=${HADOOP_MASTER_IMAGE:-"teradatalabs/cdh5-hive:${DOCKER_IMAGES_VERSION}"}
 
 # The following variables are defined to enable running product tests with arbitrary/downloaded jars
