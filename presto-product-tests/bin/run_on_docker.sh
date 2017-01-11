93ce64d2785b7c6e8bdfd44456dbe16d39c98626
Sanjay Sharma <sanjay.sharma@teradata.com>
2017-01-11 10:58:33 -0500

    Pass product-tests even if containers fail to stop

--- presto-product-tests/bin/run_on_docker.sh
+++ presto-product-tests/bin/run_on_docker.sh
@@ -102,7 +102,9 @@ function stop_docker_compose_containers() {
     stop_application_runner_containers ${ENVIRONMENT}
 
     # stop containers started with "up", removing their volumes
-    environment_compose down -v
+    # Some containers (SQL Server) fail to stop on Travis after running the tests. We don't have an easy way to
+    # reproduce this locally. Since all the tests complete successfully , we ignore this failure.
+    environment_compose down -v || true
   fi
 
   echo "Docker compose containers stopped: [$ENVIRONMENT]"
