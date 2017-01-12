5344beb55433fb6aa094fb711a97abf070844999
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-01-12 15:12:13 +0100

    Split ErrorMessages into regions

--- presto-cli/src/main/java/com/facebook/presto/cli/ErrorMessages.java
+++ presto-cli/src/main/java/com/facebook/presto/cli/ErrorMessages.java
@@ -251,6 +251,7 @@ public class ErrorMessages
         builder.append("========= TECHNICAL DETAILS END =========\n\n");
     }
 
+    //region Standard QueryError format
     private static void standardQueryErrorMessage(StringBuilder builder, StatementClient client)
     {
         QueryError error = extractQueryError(client);
@@ -294,7 +295,9 @@ public class ErrorMessages
             builder.append(padding + "^");
         }
     }
+    //endregion
 
+    //region Helpers
     private static QueryError extractQueryError(StatementClient client)
     {
         QueryResults results = client.finalResults();
@@ -302,4 +305,5 @@ public class ErrorMessages
         checkState(error != null);
         return error;
     }
+    //endregion
 }
