f95872423d580b355dd9249ec109f6690dc96b1a
Piotr Nowojski <piotr.nowojski@teradata.com>
2017-01-14 18:43:53 +0100

    Fix partial aggregation push down through union
    PartialAggregationPushDown was not able to push aggregation through
    more then one ExchangeNode. When it found plan:
    
    Aggregation -> Exchange -> Exchange
    
    it was transforming it to:
    
    Aggregation Final -> Exchange -> Aggregation Partial -> Project -> Exchange
    
    This projection was preventing PartialAggregationPushDown to push
    "Aggregation Partial" further down.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PartialAggregationPushDown.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PartialAggregationPushDown.java
@@ -146,31 +146,25 @@ public class PartialAggregationPushDown
             for (int i = 0; i < exchange.getSources().size(); i++) {
                 PlanNode source = exchange.getSources().get(i);
 
-                if (!exchange.getOutputSymbols().equals(exchange.getInputs().get(i))) {
-                    // Add an identity projection to preserve the inputs to the aggregation, if necessary.
-                    // This allows us to avoid having to rewrite the symbols in the aggregation node below.
-                    Assignments.Builder assignments = Assignments.builder();
-                    for (int outputIndex = 0; outputIndex < exchange.getOutputSymbols().size(); outputIndex++) {
-                        Symbol output = exchange.getOutputSymbols().get(outputIndex);
-                        Symbol input = exchange.getInputs().get(i).get(outputIndex);
-                        assignments.put(output, input.toSymbolReference());
+                SymbolMapper.Builder mappingsBuilder = SymbolMapper.builder();
+                for (int outputIndex = 0; outputIndex < exchange.getOutputSymbols().size(); outputIndex++) {
+                    Symbol output = exchange.getOutputSymbols().get(outputIndex);
+                    Symbol input = exchange.getInputs().get(i).get(outputIndex);
+                    if (!output.equals(input)) {
+                        mappingsBuilder.put(output, input);
                     }
-
-                    source = new ProjectNode(idAllocator.getNextId(), source, assignments.build());
                 }
 
-                // Since this exchange source is now guaranteed to have the same symbols as the inputs to the the partial
-                // aggregation, we can build a new AggregationNode without any further symbol rewrites
-                partials.add(new AggregationNode(
-                        idAllocator.getNextId(),
-                        source,
-                        partial.getAggregations(),
-                        partial.getFunctions(),
-                        partial.getMasks(),
-                        partial.getGroupingSets(),
-                        partial.getStep(),
-                        partial.getHashSymbol(),
-                        partial.getGroupIdSymbol()));
+                SymbolMapper symbolMapper = mappingsBuilder.build();
+                AggregationNode mappedPartial = symbolMapper.map(partial, source);
+
+                Assignments.Builder assignments = Assignments.builder();
+
+                for (Symbol output : partial.getOutputSymbols()) {
+                    Symbol input = symbolMapper.map(output);
+                    assignments.put(output, input.toSymbolReference());
+                }
+                partials.add(new ProjectNode(idAllocator.getNextId(), mappedPartial, assignments.build()));
             }
 
             for (PlanNode node : partials) {
