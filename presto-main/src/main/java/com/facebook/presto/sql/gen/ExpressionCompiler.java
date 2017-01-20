ccaebd0742fa0d57d6701c2758f9963deb9bf1ed
ebyhr <ebyhry@gmail.com>
2017-01-21 00:15:22 +0900

    Show hit/miss rate in ExpressionCompiler via JMX

--- presto-main/src/main/java/com/facebook/presto/sql/gen/ExpressionCompiler.java
+++ presto-main/src/main/java/com/facebook/presto/sql/gen/ExpressionCompiler.java
@@ -26,6 +26,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableList;
 import org.weakref.jmx.Managed;
+import org.weakref.jmx.Nested;
 
 import javax.inject.Inject;
 
@@ -80,6 +81,14 @@ public class ExpressionCompiler
     {
         return pageProcessors.size();
     }
+
+    @Managed
+    @Nested
+    public CacheStatsMBean getPageCacheStats() { return new CacheStatsMBean(pageProcessors.stats()); }
+
+    @Managed
+    @Nested
+    public CacheStatsMBean getCursorCacheStats() { return new CacheStatsMBean(cursorProcessors.stats()); }
 
     public Supplier<CursorProcessor> compileCursorProcessor(RowExpression filter, List<RowExpression> projections, Object uniqueKey)
     {
