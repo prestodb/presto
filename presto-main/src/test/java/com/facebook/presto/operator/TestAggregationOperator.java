27db779dcdf64a9abc08036ab1ba8490b8953bad
Artur Gajowy <artur.gajowy@gmail.com>
2017-01-23 08:59:26 +0100

    Fix TestAggregationOperator affected by TestNG bug
    See https://github.com/cbeust/testng/issues/543.

--- presto-main/src/test/java/com/facebook/presto/operator/TestAggregationOperator.java
+++ presto-main/src/test/java/com/facebook/presto/operator/TestAggregationOperator.java
@@ -107,7 +107,7 @@ public class TestAggregationOperator
                         DOUBLE_SUM.bind(ImmutableList.of(5), Optional.empty()),
                         maxVarcharColumn.bind(ImmutableList.of(6), Optional.empty())));
 
-        MaterializedResult expected = resultBuilder(driverContext.getSession(), BIGINT, BIGINT, DOUBLE, VARCHAR, BIGINT, BIGINT, DOUBLE, VARCHAR)
+        MaterializedResult expected = resultBuilder(driverContext.getSession(), BIGINT, BIGINT, DOUBLE, VARCHAR, BIGINT, BIGINT, REAL, DOUBLE, VARCHAR)
                 .row(100L, 4950L, 49.5, "399", 100L, 54950L, 44950.0f, 54950.0, "599")
                 .build();
 
