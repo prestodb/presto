34f5fa1e3b3ecae41fd6266e97b48263bd20d569
Rongrong Zhong <rongrong@fb.com>
2017-01-22 23:24:42 -0800

    Reduce memory usage for min_by and max_by
    * Generate state factory and serializer use StateCompiler
    * Geneate bytecode for input, combine and output according to input type and min/max

--- presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestMinMaxByAggregation.java
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestMinMaxByAggregation.java
@@ -15,27 +15,15 @@ package com.facebook.presto.operator.aggregation;
 
 import com.facebook.presto.metadata.MetadataManager;
 import com.facebook.presto.metadata.Signature;
-import com.facebook.presto.operator.aggregation.state.MaxOrMinByState;
-import com.facebook.presto.operator.aggregation.state.MaxOrMinByStateFactory;
-import com.facebook.presto.operator.aggregation.state.MaxOrMinByStateSerializer;
-import com.facebook.presto.spi.ConnectorSession;
-import com.facebook.presto.spi.block.Block;
-import com.facebook.presto.spi.block.BlockBuilder;
-import com.facebook.presto.spi.block.BlockBuilderStatus;
-import com.facebook.presto.spi.type.AbstractFixedWidthType;
+import com.facebook.presto.operator.aggregation.state.StateCompiler;
 import com.facebook.presto.spi.type.DecimalType;
 import com.facebook.presto.spi.type.SqlDecimal;
 import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.type.RowType;
-import com.facebook.presto.type.TypeRegistry;
 import com.google.common.collect.ImmutableList;
-import io.airlift.slice.Slices;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import static com.facebook.presto.block.BlockAssertions.createArrayBigintBlock;
@@ -46,26 +34,15 @@ import static com.facebook.presto.block.BlockAssertions.createShortDecimalsBlock
 import static com.facebook.presto.block.BlockAssertions.createStringsBlock;
 import static com.facebook.presto.metadata.FunctionKind.AGGREGATE;
 import static com.facebook.presto.operator.aggregation.AggregationTestUtils.assertAggregation;
-import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
 import static com.facebook.presto.spi.type.TypeSignature.parseTypeSignature;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
 import static com.facebook.presto.util.ImmutableCollectors.toImmutableSet;
-import static io.airlift.slice.SizeOf.SIZE_OF_DOUBLE;
-import static java.lang.Double.doubleToLongBits;
-import static java.lang.Double.longBitsToDouble;
 import static java.util.Arrays.asList;
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 public class TestMinMaxByAggregation
 {
     private static final MetadataManager METADATA = MetadataManager.createTestMetadataManager();
-
-    @BeforeClass
-    public void setup()
-    {
-        ((TypeRegistry) METADATA.getTypeManager()).addType(CustomDoubleType.CUSTOM_DOUBLE);
-    }
 
     @Test
     public void testAllRegistered()
@@ -76,8 +53,10 @@ public class TestMinMaxByAggregation
 
         for (Type keyType : orderableTypes) {
             for (Type valueType : getTypes()) {
-                assertNotNull(METADATA.getFunctionRegistry().getAggregateFunctionImplementation(new Signature("min_by", AGGREGATE, valueType.getTypeSignature(), valueType.getTypeSignature(), keyType.getTypeSignature())));
-                assertNotNull(METADATA.getFunctionRegistry().getAggregateFunctionImplementation(new Signature("max_by", AGGREGATE, valueType.getTypeSignature(), valueType.getTypeSignature(), keyType.getTypeSignature())));
+                if (StateCompiler.getSupportedFieldTypes().contains(valueType.getJavaType())) {
+                    assertNotNull(METADATA.getFunctionRegistry().getAggregateFunctionImplementation(new Signature("min_by", AGGREGATE, valueType.getTypeSignature(), valueType.getTypeSignature(), keyType.getTypeSignature())));
+                    assertNotNull(METADATA.getFunctionRegistry().getAggregateFunctionImplementation(new Signature("max_by", AGGREGATE, valueType.getTypeSignature(), valueType.getTypeSignature(), keyType.getTypeSignature())));
+                }
             }
         }
     }
@@ -277,115 +256,5 @@ public class TestMinMaxByAggregation
                 SqlDecimal.of("3.3"),
                 createShortDecimalsBlock("1.1", "2.2", "3.3", "4.4"),
                 createShortDecimalsBlock("1.2", "1.0", "2.0", "1.5"));
-    }
-
-    @Test
-    public void testStateDeserializer()
-            throws Exception
-    {
-        String[] keys = {"loooooong string", "short string"};
-        double[] values = {3.14, 2.71};
-
-        MaxOrMinByStateSerializer serializer = new MaxOrMinByStateSerializer(DOUBLE, VARCHAR);
-        BlockBuilder builder = new RowType(ImmutableList.of(VARCHAR, DOUBLE), Optional.empty()).createBlockBuilder(new BlockBuilderStatus(), 2);
-
-        for (int i = 0; i < keys.length; i++) {
-            serializer.serialize(makeState(keys[i], values[i]), builder);
-        }
-
-        Block serialized = builder.build();
-
-        for (int i = 0; i < keys.length; i++) {
-            MaxOrMinByState deserialized = new MaxOrMinByStateFactory().createSingleState();
-            serializer.deserialize(serialized, i, deserialized);
-            assertEquals(VARCHAR.getSlice(deserialized.getKey(), 0), Slices.utf8Slice(keys[i]));
-            assertEquals(DOUBLE.getDouble(deserialized.getValue(), 0), values[i]);
-        }
-    }
-
-    private static MaxOrMinByState makeState(String key, double value)
-    {
-        MaxOrMinByState result = new MaxOrMinByStateFactory().createSingleState();
-        result.setKey(createStringsBlock(key));
-        result.setValue(createDoublesBlock(value));
-        return result;
-    }
-
-    private static class CustomDoubleType
-            extends AbstractFixedWidthType
-    {
-        public static final CustomDoubleType CUSTOM_DOUBLE = new CustomDoubleType();
-        public static final String NAME = "custom_double";
-
-        private CustomDoubleType()
-        {
-            super(parseTypeSignature(NAME), double.class, SIZE_OF_DOUBLE);
-        }
-
-        @Override
-        public boolean isComparable()
-        {
-            return true;
-        }
-
-        @Override
-        public boolean isOrderable()
-        {
-            return true;
-        }
-
-        @Override
-        public Object getObjectValue(ConnectorSession session, Block block, int position)
-        {
-            if (block.isNull(position)) {
-                return null;
-            }
-            return longBitsToDouble(block.getLong(position, 0));
-        }
-
-        @Override
-        public boolean equalTo(Block leftBlock, int leftPosition, Block rightBlock, int rightPosition)
-        {
-            long leftValue = leftBlock.getLong(leftPosition, 0);
-            long rightValue = rightBlock.getLong(rightPosition, 0);
-            return leftValue == rightValue;
-        }
-
-        @Override
-        public long hash(Block block, int position)
-        {
-            return block.getLong(position, 0);
-        }
-
-        @Override
-        public int compareTo(Block leftBlock, int leftPosition, Block rightBlock, int rightPosition)
-        {
-            double leftValue = longBitsToDouble(leftBlock.getLong(leftPosition, 0));
-            double rightValue = longBitsToDouble(rightBlock.getLong(rightPosition, 0));
-            return Double.compare(leftValue, rightValue);
-        }
-
-        @Override
-        public void appendTo(Block block, int position, BlockBuilder blockBuilder)
-        {
-            if (block.isNull(position)) {
-                blockBuilder.appendNull();
-            }
-            else {
-                blockBuilder.writeLong(block.getLong(position, 0)).closeEntry();
-            }
-        }
-
-        @Override
-        public double getDouble(Block block, int position)
-        {
-            return longBitsToDouble(block.getLong(position, 0));
-        }
-
-        @Override
-        public void writeDouble(BlockBuilder blockBuilder, double value)
-        {
-            blockBuilder.writeLong(doubleToLongBits(value)).closeEntry();
-        }
     }
 }
