defabcba4e1b89d5df536da51258cfdbfea906cb
Eric Diven <eric.diven@teradata.com>
2017-01-25 16:06:52 -0500

    Improve properties/URL parameter handling

---
+++ presto-jdbc/src/main/java/com/facebook/presto/jdbc/ConnectionProperties.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.jdbc;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static java.util.Collections.unmodifiableMap;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+
+final class ConnectionProperties
+{
+    public static final ConnectionProperty<String> USER = new User();
+    public static final ConnectionProperty<String> PASSWORD = new Password();
+    public static final ConnectionProperty<Boolean> SECURE = new Secure();
+
+    private static final Set<ConnectionProperty<?>> ALL_PROPERTIES = ImmutableSet.<ConnectionProperty<?>>builder()
+            .add(USER)
+            .add(PASSWORD)
+            .add(SECURE)
+            .build();
+
+    private static final Map<String, ConnectionProperty<?>> KEY_LOOKUP = unmodifiableMap(ALL_PROPERTIES.stream()
+            .collect(toMap(ConnectionProperty::getKey, identity())));
+
+    private static final Map<String, String> DEFAULTS;
+
+    static {
+        ImmutableMap.Builder<String, String> defaults = ImmutableMap.builder();
+        for (ConnectionProperty<?> property : ALL_PROPERTIES) {
+            property.getDefault().ifPresent(value -> defaults.put(property.getKey(), value));
+        }
+        DEFAULTS = defaults.build();
+    }
+
+    private ConnectionProperties() {}
+
+    public static ConnectionProperty<?> forKey(String propertiesKey)
+    {
+        return KEY_LOOKUP.get(propertiesKey);
+    }
+
+    public static Set<ConnectionProperty<?>> allProperties()
+    {
+        return ALL_PROPERTIES;
+    }
+
+    public static Map<String, String> getDefaults()
+    {
+        return DEFAULTS;
+    }
+
+    private static class User
+            extends AbstractConnectionProperty<String>
+    {
+        public User()
+        {
+            super("user", REQUIRED, STRING_CONVERTER);
+        }
+    }
+
+    private static class Password
+            extends AbstractConnectionProperty<String>
+    {
+        public Password()
+        {
+            super("password", NOT_REQUIRED, STRING_CONVERTER);
+        }
+    }
+
+    private static class Secure
+            extends AbstractConnectionProperty<Boolean>
+    {
+        public Secure()
+        {
+            super("secure", Optional.of("false"), NOT_REQUIRED, BOOLEAN_CONVERTER);
+        }
+    }
+}
