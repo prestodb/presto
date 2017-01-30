b045c70ff845baf76f71d75f9128a7b0b0547feb
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-01-30 08:05:03 +0100

    Add TPC-DS queries as product tests
    tpcds tests are disabled to be run in each travis build. It is going to
    be run externally in prest-checks. This is due that execution duration
    in about 2 hours.

---
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q22.sql
@@ -0,0 +1,17 @@
+-- database: presto_tpcds; groups: tpcds; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
+SELECT
+  "i_product_name"
+, "i_brand"
+, "i_class"
+, "i_category"
+, "avg"("inv_quantity_on_hand") "qoh"
+FROM
+  inventory
+, date_dim
+, item
+WHERE ("inv_date_sk" = "d_date_sk")
+   AND ("inv_item_sk" = "i_item_sk")
+   AND ("d_month_seq" BETWEEN 1200 AND (1200 + 11))
+GROUP BY ROLLUP (i_product_name, i_brand, i_class, i_category)
+ORDER BY "qoh" ASC, "i_product_name" ASC, "i_brand" ASC, "i_class" ASC, "i_category" ASC
+LIMIT 100
