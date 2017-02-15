bfdd2716b32d2a37e86927def3fd459cfbbdff36
Maciej Grzybek <maciej.grzybek@teradata.com>
2017-02-15 16:37:54 -0500

    Extract common code for findGroupEnd

--- presto-main/src/main/java/com/facebook/presto/operator/WindowOperator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/WindowOperator.java
@@ -28,6 +28,7 @@ import com.google.common.primitives.Ints;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 import static com.facebook.presto.spi.block.SortOrder.ASC_NULLS_LAST;
@@ -452,13 +453,7 @@ public class WindowOperator
             return page.getPositionCount();
         }
 
-        // TODO: do position binary search
-        int endPosition = startPosition + 1;
-        while (endPosition < page.getPositionCount() &&
-                pagesHashStrategy.rowEqualsRow(endPosition - 1, page, endPosition, page)) {
-            endPosition++;
-        }
-        return endPosition;
+        return findEndPosition(startPosition, page.getPositionCount() - 1, (firstPosition, secondPosition) -> pagesHashStrategy.rowEqualsRow(firstPosition, page, secondPosition, page));
     }
 
     // Assumes input grouped on relevant pagesHashStrategy columns
@@ -472,12 +467,18 @@ public class WindowOperator
             return pagesIndex.getPositionCount();
         }
 
+        return findEndPosition(startPosition, pagesIndex.getPositionCount() - 1, (firstPosition, secondPosition) -> pagesIndex.positionEqualsPosition(pagesHashStrategy, firstPosition, secondPosition));
+    }
+
+    // comparator is suppose to return true if positions given as parameters are equal
+    private static int findEndPosition(int firstPosition, int lastPosition, BiFunction<Integer, Integer, Boolean> comparator)
+    {
         // TODO: do position binary search
-        int endPosition = startPosition + 1;
-        while ((endPosition < pagesIndex.getPositionCount()) &&
-                pagesIndex.positionEqualsPosition(pagesHashStrategy, endPosition - 1, endPosition)) {
+        int endPosition = firstPosition + 1;
+        while ((endPosition <= lastPosition) && comparator.apply(endPosition - 1, endPosition)) {
             endPosition++;
         }
+
         return endPosition;
     }
 }
