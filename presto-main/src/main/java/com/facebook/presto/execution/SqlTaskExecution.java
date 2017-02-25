26604fde31e9f4586c18209285b7b5c3bc80d51e
Dain Sundstrom <dain@iq80.com>
2017-02-25 10:20:01 -0800

    Fix IDE warnings in SqlTaskExecution

--- presto-main/src/main/java/com/facebook/presto/execution/SqlTaskExecution.java
+++ presto-main/src/main/java/com/facebook/presto/execution/SqlTaskExecution.java
@@ -304,7 +304,7 @@ public class SqlTaskExecution
         return updatedUnpartitionedSources;
     }
 
-    private void schedulePartitionedSource(TaskSource source)
+    private synchronized void schedulePartitionedSource(TaskSource source)
     {
         // if this is not for the currently scheduling source, save off the splits for
         // when the source is scheduled
@@ -335,12 +335,12 @@ public class SqlTaskExecution
         }
     }
 
-    private boolean isSchedulingSource(PlanNodeId sourceId)
+    private synchronized boolean isSchedulingSource(PlanNodeId sourceId)
     {
         return !sourceStartOrder.isEmpty() && sourceStartOrder.peek().equals(sourceId);
     }
 
-    private void scheduleUnpartitionedSource(TaskSource source, Map<PlanNodeId, TaskSource> updatedUnpartitionedSources)
+    private synchronized void scheduleUnpartitionedSource(TaskSource source, Map<PlanNodeId, TaskSource> updatedUnpartitionedSources)
     {
         // create new source
         TaskSource newSource;
@@ -419,7 +419,7 @@ public class SqlTaskExecution
         }
     }
 
-    public Set<PlanNodeId> getNoMoreSplits()
+    public synchronized Set<PlanNodeId> getNoMoreSplits()
     {
         ImmutableSet.Builder<PlanNodeId> noMoreSplits = ImmutableSet.builder();
         for (Entry<PlanNodeId, DriverSplitRunnerFactory> entry : partitionedDriverFactories.entrySet()) {
