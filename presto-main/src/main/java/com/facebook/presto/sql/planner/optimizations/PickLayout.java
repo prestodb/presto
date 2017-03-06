aa5222546a18392a5f63eab16228d9c6e21c76ef
Christina Wallin <christina.wallin@teradata.com>
2017-03-06 16:38:14 -0500

    Extract TableLayout rewrite to a separate class
    This class will be used in both the old and iterative optimizers.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PickLayout.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PickLayout.java
@@ -15,12 +15,7 @@ package com.facebook.presto.sql.planner.optimizations;
 
 import com.facebook.presto.Session;
 import com.facebook.presto.metadata.Metadata;
-import com.facebook.presto.metadata.TableLayoutResult;
-import com.facebook.presto.spi.ColumnHandle;
-import com.facebook.presto.spi.Constraint;
-import com.facebook.presto.spi.predicate.TupleDomain;
 import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.sql.planner.DomainTranslator;
 import com.facebook.presto.sql.planner.PlanNodeIdAllocator;
 import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.SymbolAllocator;
@@ -28,18 +23,10 @@ import com.facebook.presto.sql.planner.plan.FilterNode;
 import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.SimplePlanRewriter;
 import com.facebook.presto.sql.planner.plan.TableScanNode;
-import com.facebook.presto.sql.planner.plan.ValuesNode;
 import com.facebook.presto.sql.tree.BooleanLiteral;
-import com.facebook.presto.sql.tree.Expression;
-import com.google.common.collect.ImmutableBiMap;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
-import static com.facebook.presto.sql.ExpressionUtils.combineConjuncts;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -66,17 +53,11 @@ public class PickLayout
     private static class Rewriter
             extends SimplePlanRewriter<Void>
     {
-        private final Metadata metadata;
-        private final Session session;
-        private final SymbolAllocator symbolAllocator;
-        private final PlanNodeIdAllocator idAllocator;
+        private final TableLayoutRewriter tableLayoutRewriter;
 
         public Rewriter(Metadata metadata, Session session, SymbolAllocator symbolAllocator, PlanNodeIdAllocator idAllocator)
         {
-            this.metadata = metadata;
-            this.session = session;
-            this.symbolAllocator = symbolAllocator;
-            this.idAllocator = idAllocator;
+            this.tableLayoutRewriter = new TableLayoutRewriter(metadata, session, symbolAllocator, idAllocator);
         }
 
         @Override
@@ -89,7 +70,7 @@ public class PickLayout
         public PlanNode visitFilter(FilterNode node, RewriteContext<Void> context)
         {
             if (node.getSource() instanceof TableScanNode && !((TableScanNode) node.getSource()).getLayout().isPresent()) {
-                return planTableScan((TableScanNode) node.getSource(), node.getPredicate());
+                return tableLayoutRewriter.planTableScan((TableScanNode) node.getSource(), node.getPredicate());
             }
 
             return context.defaultRewrite(node);
@@ -102,51 +83,9 @@ public class PickLayout
                 return node;
             }
 
-            return planTableScan(node, BooleanLiteral.TRUE_LITERAL);
+            return tableLayoutRewriter.planTableScan(node, BooleanLiteral.TRUE_LITERAL);
         }
 
-        private PlanNode planTableScan(TableScanNode node, Expression predicate)
-        {
-            DomainTranslator.ExtractionResult decomposedPredicate = DomainTranslator.fromPredicate(
-                    metadata,
-                    session,
-                    predicate,
-                    symbolAllocator.getTypes());
 
-            TupleDomain<ColumnHandle> simplifiedConstraint = decomposedPredicate.getTupleDomain()
-                    .transform(node.getAssignments()::get)
-                    .intersect(node.getCurrentConstraint());
-
-            List<TableLayoutResult> layouts = metadata.getLayouts(
-                    session, node.getTable(),
-                    new Constraint<>(simplifiedConstraint, bindings -> true),
-                    Optional.of(ImmutableSet.copyOf(node.getAssignments().values())));
-
-            if (layouts.isEmpty()) {
-                return new ValuesNode(idAllocator.getNextId(), node.getOutputSymbols(), ImmutableList.of());
-            }
-
-            TableLayoutResult layout = layouts.get(0);
-
-            TableScanNode result = new TableScanNode(
-                    node.getId(),
-                    node.getTable(),
-                    node.getOutputSymbols(),
-                    node.getAssignments(),
-                    Optional.of(layout.getLayout().getHandle()),
-                    simplifiedConstraint.intersect(layout.getLayout().getPredicate()),
-                    Optional.ofNullable(node.getOriginalConstraint()).orElse(predicate));
-
-            Map<ColumnHandle, Symbol> assignments = ImmutableBiMap.copyOf(node.getAssignments()).inverse();
-            Expression resultingPredicate = combineConjuncts(
-                    decomposedPredicate.getRemainingExpression(),
-                    DomainTranslator.toPredicate(layout.getUnenforcedConstraint().transform(assignments::get)));
-
-            if (!BooleanLiteral.TRUE_LITERAL.equals(resultingPredicate)) {
-                return new FilterNode(idAllocator.getNextId(), result, resultingPredicate);
-            }
-
-            return result;
-        }
     }
 }
