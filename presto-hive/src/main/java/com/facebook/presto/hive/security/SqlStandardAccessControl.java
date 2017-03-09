e878ee58fb1d9fd415eb8dedb81a3fde8482f8ee
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-03-09 18:18:17 -0500

    Rename getGrantOptionForPrivilege to hasGrantOptionForPrivilege

--- presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
@@ -247,7 +247,7 @@ public class SqlStandardAccessControl
         if (!checkTablePermission(transaction, identity, tableName, SELECT)) {
             denySelectTable(tableName.toString());
         }
-        else if (!getGrantOptionForPrivilege(transaction, identity, Privilege.SELECT, tableName)) {
+        else if (!hasGrantOptionForPrivilege(transaction, identity, Privilege.SELECT, tableName)) {
             denyCreateViewWithSelect(tableName.toString());
         }
     }
@@ -258,7 +258,7 @@ public class SqlStandardAccessControl
         if (!checkTablePermission(transaction, identity, viewName, SELECT)) {
             denySelectView(viewName.toString());
         }
-        if (!getGrantOptionForPrivilege(transaction, identity, Privilege.SELECT, viewName)) {
+        if (!hasGrantOptionForPrivilege(transaction, identity, Privilege.SELECT, viewName)) {
             denyCreateViewWithSelect(viewName.toString());
         }
     }
@@ -284,7 +284,7 @@ public class SqlStandardAccessControl
         }
 
         HivePrivilege hivePrivilege = toHivePrivilege(privilege);
-        if (hivePrivilege == null || !getGrantOptionForPrivilege(transaction, identity, privilege, tableName)) {
+        if (hivePrivilege == null || !hasGrantOptionForPrivilege(transaction, identity, privilege, tableName)) {
             denyGrantTablePrivilege(privilege.name(), tableName.toString());
         }
     }
@@ -297,7 +297,7 @@ public class SqlStandardAccessControl
         }
 
         HivePrivilege hivePrivilege = toHivePrivilege(privilege);
-        if (hivePrivilege == null || !getGrantOptionForPrivilege(transaction, identity, privilege, tableName)) {
+        if (hivePrivilege == null || !hasGrantOptionForPrivilege(transaction, identity, privilege, tableName)) {
             denyRevokeTablePrivilege(privilege.name(), tableName.toString());
         }
     }
@@ -432,7 +432,7 @@ public class SqlStandardAccessControl
         return privilegeSet.containsAll(ImmutableSet.copyOf(requiredPrivileges));
     }
 
-    private boolean getGrantOptionForPrivilege(ConnectorTransactionHandle transaction, ConnectorIdentity identity, Privilege privilege, SchemaTableName tableName)
+    private boolean hasGrantOptionForPrivilege(ConnectorTransactionHandle transaction, ConnectorIdentity identity, Privilege privilege, SchemaTableName tableName)
     {
         SemiTransactionalHiveMetastore metastore = metastoreProvider.apply(((HiveTransactionHandle) transaction));
         return listApplicableTablePrivileges(
