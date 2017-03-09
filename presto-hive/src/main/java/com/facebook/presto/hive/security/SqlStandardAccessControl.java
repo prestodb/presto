3560f6215557ed1ab740b97e204a72256378cf38
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-03-09 18:20:50 -0500

    Remove hive privilege null check

--- presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
@@ -283,8 +283,7 @@ public class SqlStandardAccessControl
             return;
         }
 
-        HivePrivilege hivePrivilege = toHivePrivilege(privilege);
-        if (hivePrivilege == null || !hasGrantOptionForPrivilege(transaction, identity, privilege, tableName)) {
+        if (!hasGrantOptionForPrivilege(transaction, identity, privilege, tableName)) {
             denyGrantTablePrivilege(privilege.name(), tableName.toString());
         }
     }
@@ -296,8 +295,7 @@ public class SqlStandardAccessControl
             return;
         }
 
-        HivePrivilege hivePrivilege = toHivePrivilege(privilege);
-        if (hivePrivilege == null || !hasGrantOptionForPrivilege(transaction, identity, privilege, tableName)) {
+        if (!hasGrantOptionForPrivilege(transaction, identity, privilege, tableName)) {
             denyRevokeTablePrivilege(privilege.name(), tableName.toString());
         }
     }
