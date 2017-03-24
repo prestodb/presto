6e7c9ca25cfc7940c961638fbc06b692b7b869c0
Lukasz Osipiuk <lukasz@osipiuk.net>
2017-03-24 14:30:21 +0100

    Remove unnecessary empty line after LICENSE comment

--- presto-blackhole/src/main/java/com/facebook/presto/plugin/blackhole/BlackHoleConnector.java
+++ presto-blackhole/src/main/java/com/facebook/presto/plugin/blackhole/BlackHoleConnector.java
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.facebook.presto.plugin.blackhole;
 
 import com.facebook.presto.spi.connector.Connector;
--- presto-main/src/main/java/com/facebook/presto/operator/StandardJoinFilterFunction.java
+++ presto-main/src/main/java/com/facebook/presto/operator/StandardJoinFilterFunction.java
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.facebook.presto.operator;
 
 import com.facebook.presto.spi.Page;
