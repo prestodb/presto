c4073ab745c8b4be53ed8c3e7c88eb3f88f196fe
Aleksei Statkevich <astatkevich@fb.com>
2017-03-24 19:01:08 -0700

    Improve javadoc for ConnectorTableLayout properties

--- presto-spi/src/main/java/com/facebook/presto/spi/ConnectorTableLayout.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/ConnectorTableLayout.java
@@ -76,7 +76,10 @@ public class ConnectorTableLayout
     }
 
     /**
-     * The columns from the original table provided by this layout. A layout may provide only a subset of columns.
+     * The columns from the original table provided by this layout.
+     * <p>
+     * A layout may provide only a subset of columns.
+     * Optional.empty() means "all columns" while empty list means "no columns".
      */
     public Optional<List<ColumnHandle>> getColumns()
     {
@@ -84,8 +87,11 @@ public class ConnectorTableLayout
     }
 
     /**
-     * A predicate that describes the universe of data in this layout. It may be used by the query engine to
-     * infer additional properties and perform further optimizations
+     * A predicate that describes the universe of data in this layout.
+     * <p>
+     * For example, one of the columns might be always greater than zero due to the nature of the data.
+     * It may be used by the query engine to infer additional properties and perform further optimizations,
+     * such as removing unnecessary filters.
      */
     public TupleDomain<ColumnHandle> getPredicate()
     {
@@ -97,6 +103,8 @@ public class ConnectorTableLayout
      * <p>
      * If the table is node partitioned, the connector guarantees that each combination of values for
      * the distributed columns will be contained within a single worker.
+     * Can be used across tables.
+     * For example, it's used in Raptor for collocated joins.
      */
     public Optional<ConnectorNodePartitioning> getNodePartitioning()
     {
@@ -104,13 +112,15 @@ public class ConnectorTableLayout
     }
 
     /**
-     * The partitioning for the table streams.
+     * The partitioning for the table streams (splits).
+     * <p>
      * If empty, the table layout is partitioned arbitrarily.
      * Otherwise, table steams are partitioned on the given set of columns (or unpartitioned, if the set is empty)
      * <p>
      * If the table is partitioned, the connector guarantees that each combination of values for
      * the partition columns will be contained within a single split (i.e., partitions cannot
-     * straddle multiple splits)
+     * straddle multiple splits).
+     * This property is useful for sharded tables and can be used by the engine to avoid partial aggregations.
      */
     public Optional<Set<ColumnHandle>> getStreamPartitioningColumns()
     {
@@ -121,6 +131,7 @@ public class ConnectorTableLayout
      * A collection of discrete predicates describing the data in this layout. The union of
      * these predicates is expected to be equivalent to the overall predicate returned
      * by {@link #getPredicate()}. They may be used by the engine for further optimizations.
+     * This is a way to support Hive partitions.
      */
     public Optional<DiscretePredicates> getDiscretePredicates()
     {
@@ -128,7 +139,16 @@ public class ConnectorTableLayout
     }
 
     /**
-     * Properties describing the layout of the data (grouping/sorting) within each partition
+     * Describes the layout of the data (grouping/sorting) within a split.
+     * <p>
+     * There are the following properties:
+     * - sorting property says that data per split will be coming in a sorted order. Example: 1 2 2 3 5.
+     * - grouping property is a weaker guarantee that says that data will be coming in groups of the same values coming together, but
+     * not necessarily in the overall sorted order. Example: 3 3 1 5 5 5 2 2 2.
+     * - constant property says that all values are the same.
+     * Local properties can be combined in a hierarchical way, for example values are sorted by column A then (within the same value of A)
+     * grouped by column B and then (within the same value of A and B) are constant.
+     * This information can be used to efficiently run window functions by avoiding sorting and re-partitioning of incoming data.
      */
     public List<LocalProperty<ColumnHandle>> getLocalProperties()
     {
