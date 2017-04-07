81e5465c9f41890ee581c204b3341604a2bae9b5
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-04-08 01:03:18 +0200

    Support GroupReference in PlanPrinter

--- presto-main/src/main/java/com/facebook/presto/sql/planner/planPrinter/PlanPrinter.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/planPrinter/PlanPrinter.java
@@ -38,6 +38,7 @@ import com.facebook.presto.sql.planner.PartitioningScheme;
 import com.facebook.presto.sql.planner.PlanFragment;
 import com.facebook.presto.sql.planner.SubPlan;
 import com.facebook.presto.sql.planner.Symbol;
+import com.facebook.presto.sql.planner.iterative.GroupReference;
 import com.facebook.presto.sql.planner.plan.AggregationNode;
 import com.facebook.presto.sql.planner.plan.ApplyNode;
 import com.facebook.presto.sql.planner.plan.AssignUniqueId;
@@ -1093,6 +1094,14 @@ public class PlanPrinter
             print(indent, "- AssignUniqueId => [%s]", formatOutputs(node.getOutputSymbols()));
             printCost(indent + 2, node);
             printStats(indent + 2, node.getId());
+
+            return processChildren(node, indent + 1);
+        }
+
+        @Override
+        public Void visitGroupReference(GroupReference node, Integer indent)
+        {
+            print(indent, "- GroupReference[%s] => [%s]", node.getGroupId(), formatOutputs(node.getOutputSymbols()));
 
             return processChildren(node, indent + 1);
         }
