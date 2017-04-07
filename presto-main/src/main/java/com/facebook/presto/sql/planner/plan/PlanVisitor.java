88cb767c67b5b01e2eaefbc559e0c3c02e237a35
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-04-08 01:03:16 +0200

    Support PlanVisitor in GroupReference

--- presto-main/src/main/java/com/facebook/presto/sql/planner/plan/PlanVisitor.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/plan/PlanVisitor.java
@@ -13,6 +13,8 @@
  */
 package com.facebook.presto.sql.planner.plan;
 
+import com.facebook.presto.sql.planner.iterative.GroupReference;
+
 public abstract class PlanVisitor<C, R>
 {
     protected abstract R visitPlan(PlanNode node, C context);
@@ -183,6 +185,11 @@ public abstract class PlanVisitor<C, R>
     }
 
     public R visitAssignUniqueId(AssignUniqueId node, C context)
+    {
+        return visitPlan(node, context);
+    }
+
+    public R visitGroupReference(GroupReference node, C context)
     {
         return visitPlan(node, context);
     }
