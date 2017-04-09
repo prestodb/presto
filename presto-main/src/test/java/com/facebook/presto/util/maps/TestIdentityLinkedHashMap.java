bad894df30d15511c85c666d2cda71c0884b9101
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-04-09 14:49:23 +0200

    Rename test class to match convention

--- presto-main/src/test/java/com/facebook/presto/util/maps/TestIdentityLinkedHashMap.java
+++ presto-main/src/test/java/com/facebook/presto/util/maps/TestIdentityLinkedHashMap.java
@@ -27,7 +27,7 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotSame;
 import static org.testng.Assert.assertTrue;
 
-public class IdentityLinkedHashMapTest
+public class TestIdentityLinkedHashMap
 {
     @Test
     public void testUsesIdentityAsEquivalenceForKeys()
