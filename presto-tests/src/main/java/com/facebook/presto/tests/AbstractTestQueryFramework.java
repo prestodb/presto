0ac0d6e4662a74019ec69a2fea1bc8d710c54caf
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-04-10 14:22:28 +0200

    Prevent NullPointerException in assertExceptionMessage

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueryFramework.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueryFramework.java
@@ -42,6 +42,7 @@ import java.util.OptionalLong;
 import static com.facebook.presto.sql.SqlFormatter.formatSql;
 import static com.facebook.presto.transaction.TransactionBuilder.transaction;
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Strings.nullToEmpty;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.airlift.testing.Closeables.closeAllRuntimeException;
 import static java.lang.String.format;
@@ -239,7 +240,7 @@ public abstract class AbstractTestQueryFramework
 
     private static void assertExceptionMessage(String sql, Exception exception, @Language("RegExp") String regex)
     {
-        if (!exception.getMessage().matches(regex)) {
+        if (!nullToEmpty(exception.getMessage()).matches(regex)) {
             fail(format("Expected exception message '%s' to match '%s' for query: %s", exception.getMessage(), regex, sql), exception);
         }
     }
