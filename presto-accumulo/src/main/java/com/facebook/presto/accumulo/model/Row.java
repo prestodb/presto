219232a36da053ba2179e20519ace1dfd2640f46
Adam J. Shook <adamjshook@gmail.com>
2017-04-20 12:11:31 -0400

    Fix incorrect date conversion in Row#valueFromString
    Addresses an incorrect test when creating a Date object from a string,
    passing in number of days instead of milliseconds.  Note that this
    function is only used for testing and doesn't affect the normal
    operations of the connector.

--- presto-accumulo/src/main/java/com/facebook/presto/accumulo/model/Row.java
+++ presto-accumulo/src/main/java/com/facebook/presto/accumulo/model/Row.java
@@ -33,7 +33,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static com.facebook.presto.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMENT;
@@ -202,7 +201,7 @@ public class Row
             return Boolean.parseBoolean(str);
         }
         else if (type.equals(DATE)) {
-            return new Date(TimeUnit.MILLISECONDS.toDays(DATE_PARSER.parseDateTime(str).getMillis()));
+            return new Date(DATE_PARSER.parseDateTime(str).getMillis());
         }
         else if (type.equals(DOUBLE)) {
             return Double.parseDouble(str);
