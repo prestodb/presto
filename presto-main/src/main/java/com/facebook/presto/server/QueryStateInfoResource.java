surli/backup-experiments

Add entrypoint to get QueryStateInfo by QueryId

presto-main/src/main/java/com/facebook/presto/server/QueryStateInfoResource.java
@@ -16,17 +16,20 @@ package com.facebook.presto.server;
 import com.facebook.presto.execution.QueryInfo;
 import com.facebook.presto.execution.QueryManager;
 import com.facebook.presto.execution.resourceGroups.ResourceGroupManager;
+import com.facebook.presto.spi.QueryId;
 import com.facebook.presto.spi.resourceGroups.ResourceGroupId;
 import com.facebook.presto.spi.resourceGroups.ResourceGroupInfo;
-import com.google.common.collect.ImmutableList;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
@@ -36,6 +39,7 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static java.util.Objects.requireNonNull;
 import static java.util.function.Function.identity;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 @Path("/v1/queryState")
 public class QueryStateInfoResource
@@ -53,17 +57,21 @@ public class QueryStateInfoResource
     }
 
     @GET
-    public List<QueryStateInfo> getQueryDiagnosticsByUser(@QueryParam("user") String user)
+    public List<QueryStateInfo> getQueryStateInfos(@QueryParam("user") String user)
     {
-        if (isNullOrEmpty(user)) {
-            return ImmutableList.of();
+        List<QueryInfo> queryInfos = queryManager.getAllQueryInfo();
+
+        if (!isNullOrEmpty(user)) {
+            queryInfos = queryInfos.stream()
+                    .filter(queryInfo -> Pattern.matches(user, queryInfo.getSession().getUser()))
+                    .collect(toImmutableList());
         }
-        List<QueryInfo> userQueries = queryManager.getAllQueryInfo().stream()
-                .filter(queryInfo -> Pattern.matches(user, queryInfo.getSession().getUser()))
+
+        queryInfos = queryInfos.stream()
                 .filter(queryInfo -> !queryInfo.getState().isDone())
                 .collect(toImmutableList());
 
-        Map<ResourceGroupId, ResourceGroupInfo> rootResourceGroupInfos = userQueries.stream()
+        Map<ResourceGroupId, ResourceGroupInfo> rootResourceGroupInfos = queryInfos.stream()
                 .map(queryInfo -> queryManager.getQueryResourceGroup(queryInfo.getQueryId()))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
@@ -71,11 +79,32 @@ public class QueryStateInfoResource
                 .distinct()
                 .collect(toImmutableMap(identity(), resourceGroupManager::getResourceGroupInfo));
 
-        return userQueries.stream()
+        return queryInfos.stream()
                 .map(queryInfo -> createQueryStateInfo(
                         queryInfo,
                         queryManager.getQueryResourceGroup(queryInfo.getQueryId()),
                         queryManager.getQueryResourceGroup(queryInfo.getQueryId()).map(ResourceGroupId::getRoot).map(rootResourceGroupInfos::get)))
                 .collect(toImmutableList());
+    }
+
+    @GET
+    @Path("{queryId}")
+    public Response getQueryStateInfo(@PathParam("queryId") String queryId)
+    {
+        try {
+            QueryInfo queryInfo = queryManager.getQueryInfo(new QueryId(queryId));
+            return Response
+                    .ok()
+                    .entity(createQueryStateInfo(
+                            queryInfo,
+                            queryManager.getQueryResourceGroup(queryInfo.getQueryId()),
+                            queryManager.getQueryResourceGroup(queryInfo.getQueryId())
+                                    .map(ResourceGroupId::getRoot)
+                                    .map(resourceGroupManager::getResourceGroupInfo)))
+                    .build();
+        }
+        catch (NoSuchElementException e) {
+            return Response.status(NOT_FOUND).build();
+        }
     }
 }

presto-spi/src/main/java/com/facebook/presto/spi/resourceGroups/ResourceGroupInfo.java
@@ -50,7 +50,21 @@ public class ResourceGroupInfo
             @JsonProperty("numEligibleSubGroups") int numEligibleSubGroups,
             @JsonProperty("memoryUsage") DataSize memoryUsage,
             @JsonProperty("numAggregatedRunningQueries") int numAggregatedRunningQueries,
-            @JsonProperty("numAggregatedQueuedQueries") int numAggregatedQueuedQueries,
+            @JsonProperty("numAggregatedQueuedQueries") int numAggregatedQueuedQueries)
+    {
+        this(id, softMemoryLimit, maxRunningQueries, maxQueuedQueries, state, numEligibleSubGroups, memoryUsage, numAggregatedQueuedQueries, numAggregatedRunningQueries, emptyList());
+    }
+
+    public ResourceGroupInfo(
+            ResourceGroupId id,
+            DataSize softMemoryLimit,
+            int maxRunningQueries,
+            int maxQueuedQueries,
+            ResourceGroupState state,
+            int numEligibleSubGroups,
+            DataSize memoryUsage,
+            int numAggregatedRunningQueries,
+            int numAggregatedQueuedQueries,
             List<ResourceGroupInfo> subGroups)
     {
         this.id = requireNonNull(id, "id is null");
@@ -142,7 +156,6 @@ public class ResourceGroupInfo
                 getNumEligibleSubGroups(),
                 getMemoryUsage(),
                 getNumAggregatedRunningQueries(),
-                getNumAggregatedQueuedQueries(),
-                emptyList());
+                getNumAggregatedQueuedQueries());
     }
 }

prestodb/presto

presto-main/src/main/java/com/facebook/presto/server/QueryStateInfoResource.java
@@ -16,17 +16,22 @@ package com.facebook.presto.server;
 import com.facebook.presto.execution.QueryInfo;
 import com.facebook.presto.execution.QueryManager;
 import com.facebook.presto.execution.resourceGroups.ResourceGroupManager;
+import com.facebook.presto.spi.QueryId;
 import com.facebook.presto.spi.resourceGroups.ResourceGroupId;
 import com.facebook.presto.spi.resourceGroups.ResourceGroupInfo;
-import com.google.common.collect.ImmutableList;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
@@ -36,6 +41,7 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static java.util.Objects.requireNonNull;
 import static java.util.function.Function.identity;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 @Path("/v1/queryState")
 public class QueryStateInfoResource
@@ -53,17 +59,22 @@ public class QueryStateInfoResource
     }
 
     @GET
-    public List<QueryStateInfo> getQueryDiagnosticsByUser(@QueryParam("user") String user)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<QueryStateInfo> getQueryStateInfos(@QueryParam("user") String user)
     {
-        if (isNullOrEmpty(user)) {
-            return ImmutableList.of();
+        List<QueryInfo> queryInfos = queryManager.getAllQueryInfo();
+
+        if (!isNullOrEmpty(user)) {
+            queryInfos = queryInfos.stream()
+                    .filter(queryInfo -> Pattern.matches(user, queryInfo.getSession().getUser()))
+                    .collect(toImmutableList());
         }
-        List<QueryInfo> userQueries = queryManager.getAllQueryInfo().stream()
-                .filter(queryInfo -> Pattern.matches(user, queryInfo.getSession().getUser()))
+
+        queryInfos = queryInfos.stream()
                 .filter(queryInfo -> !queryInfo.getState().isDone())
                 .collect(toImmutableList());
 
-        Map<ResourceGroupId, ResourceGroupInfo> rootResourceGroupInfos = userQueries.stream()
+        Map<ResourceGroupId, ResourceGroupInfo> rootResourceGroupInfos = queryInfos.stream()
                 .map(queryInfo -> queryManager.getQueryResourceGroup(queryInfo.getQueryId()))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
@@ -71,11 +82,30 @@ public class QueryStateInfoResource
                 .distinct()
                 .collect(toImmutableMap(identity(), resourceGroupManager::getResourceGroupInfo));
 
-        return userQueries.stream()
+        return queryInfos.stream()
                 .map(queryInfo -> createQueryStateInfo(
                         queryInfo,
                         queryManager.getQueryResourceGroup(queryInfo.getQueryId()),
                         queryManager.getQueryResourceGroup(queryInfo.getQueryId()).map(ResourceGroupId::getRoot).map(rootResourceGroupInfos::get)))
                 .collect(toImmutableList());
+    }
+
+    @GET
+    @Path("{queryId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public QueryStateInfo getQueryStateInfo(@PathParam("queryId") String queryId)
+            throws WebApplicationException
+    {
+        try {
+            QueryInfo queryInfo = queryManager.getQueryInfo(new QueryId(queryId));
+            Optional<ResourceGroupId> resourceGroup = queryManager.getQueryResourceGroup(queryInfo.getQueryId());
+            return createQueryStateInfo(
+                        queryInfo,
+                        resourceGroup,
+                        resourceGroup.map(ResourceGroupId::getRoot).map(resourceGroupManager::getResourceGroupInfo));
+        }
+        catch (NoSuchElementException e) {
+            throw new WebApplicationException(NOT_FOUND);
+        }
     }
 }

presto-spi/src/main/java/com/facebook/presto/spi/resourceGroups/ResourceGroupInfo.java
@@ -50,7 +50,21 @@ public class ResourceGroupInfo
             @JsonProperty("numEligibleSubGroups") int numEligibleSubGroups,
             @JsonProperty("memoryUsage") DataSize memoryUsage,
             @JsonProperty("numAggregatedRunningQueries") int numAggregatedRunningQueries,
-            @JsonProperty("numAggregatedQueuedQueries") int numAggregatedQueuedQueries,
+            @JsonProperty("numAggregatedQueuedQueries") int numAggregatedQueuedQueries)
+    {
+        this(id, softMemoryLimit, maxRunningQueries, maxQueuedQueries, state, numEligibleSubGroups, memoryUsage, numAggregatedRunningQueries, numAggregatedQueuedQueries, emptyList());
+    }
+
+    public ResourceGroupInfo(
+            ResourceGroupId id,
+            DataSize softMemoryLimit,
+            int maxRunningQueries,
+            int maxQueuedQueries,
+            ResourceGroupState state,
+            int numEligibleSubGroups,
+            DataSize memoryUsage,
+            int numAggregatedRunningQueries,
+            int numAggregatedQueuedQueries,
             List<ResourceGroupInfo> subGroups)
     {
         this.id = requireNonNull(id, "id is null");
@@ -142,7 +156,6 @@ public class ResourceGroupInfo
                 getNumEligibleSubGroups(),
                 getMemoryUsage(),
                 getNumAggregatedRunningQueries(),
-                getNumAggregatedQueuedQueries(),
-                emptyList());
+                getNumAggregatedQueuedQueries());
     }
 }