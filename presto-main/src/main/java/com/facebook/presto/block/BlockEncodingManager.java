surli/backup-experiments

Add MapBlock with built-in hash table to enable O(1) map access
This commit introduce 2 new pairs of Block/BlockWriter for maps. MapBlock
replaces ArrayBlock<InterleavedBlock> to represent a column of map, whereas
MapElementBlock represents a single map. These Blocks have additional read
access methods that enable O(1) map access.

This commit did not change all readers/writers to access and/or construct map
in the new way.  Additional work is necessary to change all call sites.

* On the read side, map subscript is changed. All other readers still
  take the legacy slow path.
* On the write side, map constructor and MapType.createBlockBuilder is changed.
  As a result, most writers are going to construct the new blocks. ORC writer
  and Parquet writer constructs Blocks for maps directly without going through
  MapType.createBlockBuilder.

presto-main/src/main/java/com/facebook/presto/block/BlockEncodingManager.java
@@ -23,6 +23,8 @@ import com.facebook.presto.spi.block.FixedWidthBlockEncoding;
 import com.facebook.presto.spi.block.IntArrayBlockEncoding;
 import com.facebook.presto.spi.block.InterleavedBlockEncoding;
 import com.facebook.presto.spi.block.LongArrayBlockEncoding;
+import com.facebook.presto.spi.block.MapBlockEncoding;
+import com.facebook.presto.spi.block.MapElementBlockEncoding;
 import com.facebook.presto.spi.block.RunLengthBlockEncoding;
 import com.facebook.presto.spi.block.ShortArrayBlockEncoding;
 import com.facebook.presto.spi.block.SliceArrayBlockEncoding;
@@ -71,6 +73,8 @@ public final class BlockEncodingManager
         addBlockEncodingFactory(DictionaryBlockEncoding.FACTORY);
         addBlockEncodingFactory(ArrayBlockEncoding.FACTORY);
         addBlockEncodingFactory(InterleavedBlockEncoding.FACTORY);
+        addBlockEncodingFactory(MapBlockEncoding.FACTORY);
+        addBlockEncodingFactory(MapElementBlockEncoding.FACTORY);
         addBlockEncodingFactory(RunLengthBlockEncoding.FACTORY);
 
         for (BlockEncodingFactory<?> factory : requireNonNull(blockEncodingFactories, "blockEncodingFactories is null")) {

presto-main/src/main/java/com/facebook/presto/operator/scalar/MapSubscriptOperator.java
@@ -20,6 +20,7 @@ import com.facebook.presto.metadata.SqlOperator;
 import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.block.MapElementBlock;
 import com.facebook.presto.spi.function.OperatorType;
 import com.facebook.presto.spi.type.BooleanType;
 import com.facebook.presto.spi.type.Type;
@@ -108,6 +109,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, boolean key)
     {
+        if (map instanceof MapElementBlock) {
+            MapElementBlock mapBlock = (MapElementBlock) map;
+            int valuePosition = mapBlock.seekKeyExact(key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof MapElementBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invokeExact(keyType.getBoolean(map, position), key)) {
@@ -129,6 +142,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, long key)
     {
+        if (map instanceof MapElementBlock) {
+            MapElementBlock mapBlock = (MapElementBlock) map;
+            int valuePosition = mapBlock.seekKeyExact(key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof MapElementBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invokeExact(keyType.getLong(map, position), key)) {
@@ -150,6 +175,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, double key)
     {
+        if (map instanceof MapElementBlock) {
+            MapElementBlock mapBlock = (MapElementBlock) map;
+            int valuePosition = mapBlock.seekKeyExact(key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof MapElementBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invokeExact(keyType.getDouble(map, position), key)) {
@@ -171,6 +208,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, Slice key)
     {
+        if (map instanceof MapElementBlock) {
+            MapElementBlock mapBlock = (MapElementBlock) map;
+            int valuePosition = mapBlock.seekKeyExact(key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof MapElementBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invokeExact(keyType.getSlice(map, position), key)) {
@@ -192,6 +241,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, Object key)
     {
+        if (map instanceof MapElementBlock) {
+            MapElementBlock mapBlock = (MapElementBlock) map;
+            int valuePosition = mapBlock.seekKeyExact((Block) key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof MapElementBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invoke(keyType.getObject(map, position), key)) {

presto-spi/src/test/java/com/facebook/presto/spi/block/TestingBlockEncodingSerde.java
@@ -54,6 +54,8 @@ public final class TestingBlockEncodingSerde
         addBlockEncodingFactory(DictionaryBlockEncoding.FACTORY);
         addBlockEncodingFactory(ArrayBlockEncoding.FACTORY);
         addBlockEncodingFactory(InterleavedBlockEncoding.FACTORY);
+        addBlockEncodingFactory(MapBlockEncoding.FACTORY);
+        addBlockEncodingFactory(MapElementBlockEncoding.FACTORY);
 
         for (BlockEncodingFactory<?> factory : requireNonNull(blockEncodingFactories, "blockEncodingFactories is null")) {
             addBlockEncodingFactory(factory);

presto-tests/src/main/java/com/facebook/presto/tests/StructuralTestUtil.java
@@ -13,13 +13,21 @@
  */
 package com.facebook.presto.tests;
 
+import com.facebook.presto.block.BlockEncodingManager;
+import com.facebook.presto.metadata.FunctionRegistry;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
 import com.facebook.presto.spi.block.InterleavedBlockBuilder;
 import com.facebook.presto.spi.type.DecimalType;
 import com.facebook.presto.spi.type.Decimals;
+import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
+import com.facebook.presto.spi.type.TypeSignatureParameter;
+import com.facebook.presto.sql.analyzer.FeaturesConfig;
+import com.facebook.presto.type.MapType;
+import com.facebook.presto.type.TypeRegistry;
 import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
 
@@ -31,6 +39,12 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 public final class StructuralTestUtil
 {
+    private static final TypeManager TYPE_MANAGER = new TypeRegistry();
+    static {
+        // associate TYPE_MANAGER with a function registry
+        new FunctionRegistry(TYPE_MANAGER, new BlockEncodingManager(TYPE_MANAGER), new FeaturesConfig());
+    }
+
     private StructuralTestUtil() {}
 
     public static boolean arrayBlocksEqual(Type elementType, Block block1, Block block2)
@@ -79,23 +93,29 @@ public final class StructuralTestUtil
 
     public static Block mapBlockOf(Type keyType, Type valueType, Object key, Object value)
     {
-        BlockBuilder blockBuilder = new InterleavedBlockBuilder(ImmutableList.of(keyType, valueType), new BlockBuilderStatus(), 1024);
-        appendToBlockBuilder(keyType, key, blockBuilder);
-        appendToBlockBuilder(valueType, value, blockBuilder);
-        return blockBuilder.build();
+        MapType mapType = mapType(keyType, valueType);
+        BlockBuilder blockBuilder = mapType.createBlockBuilder(new BlockBuilderStatus(), 10);
+        BlockBuilder mapElementBlockBuilder = blockBuilder.beginBlockEntry();
+        appendToBlockBuilder(keyType, key, mapElementBlockBuilder);
+        appendToBlockBuilder(valueType, value, mapElementBlockBuilder);
+        blockBuilder.closeEntry();
+        return mapType.getObject(blockBuilder, 0);
     }
 
     public static Block mapBlockOf(Type keyType, Type valueType, Object[] keys, Object[] values)
     {
         checkArgument(keys.length == values.length, "keys/values must have the same length");
-        BlockBuilder blockBuilder = new InterleavedBlockBuilder(ImmutableList.of(keyType, valueType), new BlockBuilderStatus(), 1024);
+        MapType mapType = mapType(keyType, valueType);
+        BlockBuilder blockBuilder = mapType.createBlockBuilder(new BlockBuilderStatus(), 10);
+        BlockBuilder mapElementBlockBuilder = blockBuilder.beginBlockEntry();
         for (int i = 0; i < keys.length; i++) {
             Object key = keys[i];
             Object value = values[i];
-            appendToBlockBuilder(keyType, key, blockBuilder);
-            appendToBlockBuilder(valueType, value, blockBuilder);
+            appendToBlockBuilder(keyType, key, mapElementBlockBuilder);
+            appendToBlockBuilder(valueType, value, mapElementBlockBuilder);
         }
-        return blockBuilder.build();
+        blockBuilder.closeEntry();
+        return mapType.getObject(blockBuilder, 0);
     }
 
     public static Block rowBlockOf(List<Type> parameterTypes, Object... values)
@@ -129,5 +149,12 @@ public final class StructuralTestUtil
             Slice sliceDecimal = Decimals.encodeUnscaledValue(decimal.unscaledValue());
             return mapBlockOf(type, type, sliceDecimal, sliceDecimal);
         }
+    }
+
+    public static MapType mapType(Type keyType, Type valueType)
+    {
+        return (MapType) TYPE_MANAGER.getParameterizedType(StandardTypes.MAP, ImmutableList.of(
+                TypeSignatureParameter.of(keyType.getTypeSignature()),
+                TypeSignatureParameter.of(valueType.getTypeSignature())));
     }
 }

prestodb/presto

presto-main/src/main/java/com/facebook/presto/block/BlockEncodingManager.java
@@ -23,8 +23,10 @@ import com.facebook.presto.spi.block.FixedWidthBlockEncoding;
 import com.facebook.presto.spi.block.IntArrayBlockEncoding;
 import com.facebook.presto.spi.block.InterleavedBlockEncoding;
 import com.facebook.presto.spi.block.LongArrayBlockEncoding;
+import com.facebook.presto.spi.block.MapBlockEncoding;
 import com.facebook.presto.spi.block.RunLengthBlockEncoding;
 import com.facebook.presto.spi.block.ShortArrayBlockEncoding;
+import com.facebook.presto.spi.block.SingleMapBlockEncoding;
 import com.facebook.presto.spi.block.SliceArrayBlockEncoding;
 import com.facebook.presto.spi.block.VariableWidthBlockEncoding;
 import com.facebook.presto.spi.type.TypeManager;
@@ -71,6 +73,8 @@ public final class BlockEncodingManager
         addBlockEncodingFactory(DictionaryBlockEncoding.FACTORY);
         addBlockEncodingFactory(ArrayBlockEncoding.FACTORY);
         addBlockEncodingFactory(InterleavedBlockEncoding.FACTORY);
+        addBlockEncodingFactory(MapBlockEncoding.FACTORY);
+        addBlockEncodingFactory(SingleMapBlockEncoding.FACTORY);
         addBlockEncodingFactory(RunLengthBlockEncoding.FACTORY);
 
         for (BlockEncodingFactory<?> factory : requireNonNull(blockEncodingFactories, "blockEncodingFactories is null")) {

presto-main/src/main/java/com/facebook/presto/operator/scalar/MapSubscriptOperator.java
@@ -20,6 +20,7 @@ import com.facebook.presto.metadata.SqlOperator;
 import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.block.SingleMapBlock;
 import com.facebook.presto.spi.function.OperatorType;
 import com.facebook.presto.spi.type.BooleanType;
 import com.facebook.presto.spi.type.Type;
@@ -108,6 +109,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, boolean key)
     {
+        if (map instanceof SingleMapBlock) {
+            SingleMapBlock mapBlock = (SingleMapBlock) map;
+            int valuePosition = mapBlock.seekKeyExact(key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof SingleMapBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invokeExact(keyType.getBoolean(map, position), key)) {
@@ -129,6 +142,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, long key)
     {
+        if (map instanceof SingleMapBlock) {
+            SingleMapBlock mapBlock = (SingleMapBlock) map;
+            int valuePosition = mapBlock.seekKeyExact(key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof SingleMapBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invokeExact(keyType.getLong(map, position), key)) {
@@ -150,6 +175,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, double key)
     {
+        if (map instanceof SingleMapBlock) {
+            SingleMapBlock mapBlock = (SingleMapBlock) map;
+            int valuePosition = mapBlock.seekKeyExact(key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof SingleMapBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invokeExact(keyType.getDouble(map, position), key)) {
@@ -171,6 +208,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, Slice key)
     {
+        if (map instanceof SingleMapBlock) {
+            SingleMapBlock mapBlock = (SingleMapBlock) map;
+            int valuePosition = mapBlock.seekKeyExact(key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof SingleMapBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invokeExact(keyType.getSlice(map, position), key)) {
@@ -192,6 +241,18 @@ public class MapSubscriptOperator
     @UsedByGeneratedCode
     public static Object subscript(boolean legacyMissingKey, FunctionInvoker functionInvoker, MethodHandle keyEqualsMethod, Type keyType, Type valueType, ConnectorSession session, Block map, Object key)
     {
+        if (map instanceof SingleMapBlock) {
+            SingleMapBlock mapBlock = (SingleMapBlock) map;
+            int valuePosition = mapBlock.seekKeyExact((Block) key);
+            if (valuePosition == -1) {
+                if (legacyMissingKey) {
+                    return null;
+                }
+                throw throwMissingKeyException(keyType, functionInvoker, key, session);
+            }
+            return readNativeValue(valueType, mapBlock, valuePosition);
+        }
+        // TODO: assume that map is always instanceof SingleMapBlock once all map producing code is updated.
         for (int position = 0; position < map.getPositionCount(); position += 2) {
             try {
                 if ((boolean) keyEqualsMethod.invoke(keyType.getObject(map, position), key)) {

presto-spi/src/test/java/com/facebook/presto/spi/block/TestingBlockEncodingSerde.java
@@ -54,6 +54,8 @@ public final class TestingBlockEncodingSerde
         addBlockEncodingFactory(DictionaryBlockEncoding.FACTORY);
         addBlockEncodingFactory(ArrayBlockEncoding.FACTORY);
         addBlockEncodingFactory(InterleavedBlockEncoding.FACTORY);
+        addBlockEncodingFactory(MapBlockEncoding.FACTORY);
+        addBlockEncodingFactory(SingleMapBlockEncoding.FACTORY);
 
         for (BlockEncodingFactory<?> factory : requireNonNull(blockEncodingFactories, "blockEncodingFactories is null")) {
             addBlockEncodingFactory(factory);

presto-tests/src/main/java/com/facebook/presto/tests/StructuralTestUtil.java
@@ -13,13 +13,21 @@
  */
 package com.facebook.presto.tests;
 
+import com.facebook.presto.block.BlockEncodingManager;
+import com.facebook.presto.metadata.FunctionRegistry;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
 import com.facebook.presto.spi.block.InterleavedBlockBuilder;
 import com.facebook.presto.spi.type.DecimalType;
 import com.facebook.presto.spi.type.Decimals;
+import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
+import com.facebook.presto.spi.type.TypeSignatureParameter;
+import com.facebook.presto.sql.analyzer.FeaturesConfig;
+import com.facebook.presto.type.MapType;
+import com.facebook.presto.type.TypeRegistry;
 import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
 
@@ -31,6 +39,12 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 public final class StructuralTestUtil
 {
+    private static final TypeManager TYPE_MANAGER = new TypeRegistry();
+    static {
+        // associate TYPE_MANAGER with a function registry
+        new FunctionRegistry(TYPE_MANAGER, new BlockEncodingManager(TYPE_MANAGER), new FeaturesConfig());
+    }
+
     private StructuralTestUtil() {}
 
     public static boolean arrayBlocksEqual(Type elementType, Block block1, Block block2)
@@ -79,23 +93,29 @@ public final class StructuralTestUtil
 
     public static Block mapBlockOf(Type keyType, Type valueType, Object key, Object value)
     {
-        BlockBuilder blockBuilder = new InterleavedBlockBuilder(ImmutableList.of(keyType, valueType), new BlockBuilderStatus(), 1024);
-        appendToBlockBuilder(keyType, key, blockBuilder);
-        appendToBlockBuilder(valueType, value, blockBuilder);
-        return blockBuilder.build();
+        MapType mapType = mapType(keyType, valueType);
+        BlockBuilder blockBuilder = mapType.createBlockBuilder(new BlockBuilderStatus(), 10);
+        BlockBuilder singleMapBlockWriter = blockBuilder.beginBlockEntry();
+        appendToBlockBuilder(keyType, key, singleMapBlockWriter);
+        appendToBlockBuilder(valueType, value, singleMapBlockWriter);
+        blockBuilder.closeEntry();
+        return mapType.getObject(blockBuilder, 0);
     }
 
     public static Block mapBlockOf(Type keyType, Type valueType, Object[] keys, Object[] values)
     {
         checkArgument(keys.length == values.length, "keys/values must have the same length");
-        BlockBuilder blockBuilder = new InterleavedBlockBuilder(ImmutableList.of(keyType, valueType), new BlockBuilderStatus(), 1024);
+        MapType mapType = mapType(keyType, valueType);
+        BlockBuilder blockBuilder = mapType.createBlockBuilder(new BlockBuilderStatus(), 10);
+        BlockBuilder singleMapBlockWriter = blockBuilder.beginBlockEntry();
         for (int i = 0; i < keys.length; i++) {
             Object key = keys[i];
             Object value = values[i];
-            appendToBlockBuilder(keyType, key, blockBuilder);
-            appendToBlockBuilder(valueType, value, blockBuilder);
+            appendToBlockBuilder(keyType, key, singleMapBlockWriter);
+            appendToBlockBuilder(valueType, value, singleMapBlockWriter);
         }
-        return blockBuilder.build();
+        blockBuilder.closeEntry();
+        return mapType.getObject(blockBuilder, 0);
     }
 
     public static Block rowBlockOf(List<Type> parameterTypes, Object... values)
@@ -129,5 +149,12 @@ public final class StructuralTestUtil
             Slice sliceDecimal = Decimals.encodeUnscaledValue(decimal.unscaledValue());
             return mapBlockOf(type, type, sliceDecimal, sliceDecimal);
         }
+    }
+
+    public static MapType mapType(Type keyType, Type valueType)
+    {
+        return (MapType) TYPE_MANAGER.getParameterizedType(StandardTypes.MAP, ImmutableList.of(
+                TypeSignatureParameter.of(keyType.getTypeSignature()),
+                TypeSignatureParameter.of(valueType.getTypeSignature())));
     }
 }