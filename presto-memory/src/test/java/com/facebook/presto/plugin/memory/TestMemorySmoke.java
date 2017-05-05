c5524d062db088bc74080de314c61899704aa34c
Piotr Nowojski <piotr.nowojski@teradata.com>
2017-05-05 11:44:45 +0200

    Refactor presto-memory smoke tests

--- presto-memory/src/test/java/com/facebook/presto/plugin/memory/TestMemorySmoke.java
+++ presto-memory/src/test/java/com/facebook/presto/plugin/memory/TestMemorySmoke.java
@@ -13,12 +13,10 @@
  */
 package com.facebook.presto.plugin.memory;
 
-import com.facebook.presto.Session;
 import com.facebook.presto.metadata.QualifiedObjectName;
 import com.facebook.presto.testing.MaterializedResult;
 import com.facebook.presto.testing.MaterializedRow;
 import com.facebook.presto.testing.QueryRunner;
-import com.google.common.collect.Iterables;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -48,7 +46,7 @@ public class TestMemorySmoke
             throws SQLException
     {
         int tablesBeforeCreate = listMemoryTables().size();
-        queryRunner.execute("CREATE TABLE test as SELECT * FROM tpch.tiny.nation");
+        queryRunner.execute("CREATE TABLE test AS SELECT * FROM tpch.tiny.nation");
         assertEquals(listMemoryTables().size(), tablesBeforeCreate + 1);
 
         queryRunner.execute(format("DROP TABLE test"));
@@ -59,12 +57,12 @@ public class TestMemorySmoke
     public void createTableWhenTableIsAlreadyCreated()
             throws SQLException
     {
-        String createTableSql = "CREATE TABLE nation as SELECT * FROM tpch.tiny.nation";
+        String createTableSql = "CREATE TABLE nation AS SELECT * FROM tpch.tiny.nation";
         try {
             queryRunner.execute(createTableSql);
             fail("Expected exception to be thrown here!");
         }
-        catch (RuntimeException ex) { // it has to RuntimeException as FailureInfo$FailureException is private
+        catch (RuntimeException ex) { // it has to be RuntimeException as FailureInfo$FailureException is private
             assertTrue(ex.getMessage().equals("line 1:1: Destination table 'memory.default.nation' already exists"));
         }
     }
@@ -73,15 +71,15 @@ public class TestMemorySmoke
     public void select()
             throws SQLException
     {
-        queryRunner.execute("CREATE TABLE test_select as SELECT * FROM tpch.tiny.nation");
+        queryRunner.execute("CREATE TABLE test_select AS SELECT * FROM tpch.tiny.nation");
 
-        assertThatQueryReturnsSameValueAs("SELECT * FROM test_select ORDER BY nationkey", "SELECT * FROM tpch.tiny.nation ORDER BY nationkey");
+        assertQuery("SELECT * FROM test_select ORDER BY nationkey", "SELECT * FROM tpch.tiny.nation ORDER BY nationkey");
 
-        assertThatQueryReturnsValue("INSERT INTO test_select SELECT * FROM tpch.tiny.nation", 25L);
+        assertQueryResult("INSERT INTO test_select SELECT * FROM tpch.tiny.nation", 25L);
 
-        assertThatQueryReturnsValue("INSERT INTO test_select SELECT * FROM tpch.tiny.nation", 25L);
+        assertQueryResult("INSERT INTO test_select SELECT * FROM tpch.tiny.nation", 25L);
 
-        assertThatQueryReturnsValue("SELECT count(*) FROM test_select", 75L);
+        assertQueryResult("SELECT count(*) FROM test_select", 75L);
     }
 
     @Test
@@ -89,9 +87,9 @@ public class TestMemorySmoke
             throws SQLException
     {
         queryRunner.execute("CREATE TABLE test_empty (a BIGINT)");
-        assertThatQueryReturnsValue("SELECT count(*) FROM test_empty", 0L);
-        assertThatQueryReturnsValue("INSERT INTO test_empty SELECT nationkey FROM tpch.tiny.nation", 25L);
-        assertThatQueryReturnsValue("SELECT count(*) FROM test_empty", 25L);
+        assertQueryResult("SELECT count(*) FROM test_empty", 0L);
+        assertQueryResult("INSERT INTO test_empty SELECT nationkey FROM tpch.tiny.nation", 25L);
+        assertQueryResult("SELECT count(*) FROM test_empty", 25L);
     }
 
     @Test
@@ -99,31 +97,31 @@ public class TestMemorySmoke
             throws SQLException
     {
         queryRunner.execute("CREATE TABLE filtered_out AS SELECT nationkey FROM tpch.tiny.nation WHERE nationkey < 0");
-        assertThatQueryReturnsValue("SELECT count(*) FROM filtered_out", 0L);
-        assertThatQueryReturnsValue("INSERT INTO filtered_out SELECT nationkey FROM tpch.tiny.nation", 25L);
-        assertThatQueryReturnsValue("SELECT count(*) FROM filtered_out", 25L);
+        assertQueryResult("SELECT count(*) FROM filtered_out", 0L);
+        assertQueryResult("INSERT INTO filtered_out SELECT nationkey FROM tpch.tiny.nation", 25L);
+        assertQueryResult("SELECT count(*) FROM filtered_out", 25L);
     }
 
     @Test
     public void selectFromEmptyTable()
             throws SQLException
     {
-        queryRunner.execute("CREATE TABLE test_select_empty as SELECT * FROM tpch.tiny.nation WHERE nationkey > 1000");
+        queryRunner.execute("CREATE TABLE test_select_empty AS SELECT * FROM tpch.tiny.nation WHERE nationkey > 1000");
 
-        assertThatQueryReturnsValue("SELECT count(*) FROM test_select_empty", 0L);
+        assertQueryResult("SELECT count(*) FROM test_select_empty", 0L);
     }
 
     @Test
     public void selectSingleRow()
     {
-        assertThatQueryReturnsSameValueAs("SELECT * FROM nation WHERE nationkey = 1", "SELECT * FROM tpch.tiny.nation WHERE nationkey = 1");
+        assertQuery("SELECT * FROM nation WHERE nationkey = 1", "SELECT * FROM tpch.tiny.nation WHERE nationkey = 1");
     }
 
     @Test
     public void selectColumnsSubset()
             throws SQLException
     {
-        assertThatQueryReturnsSameValueAs("SELECT nationkey, regionkey FROM nation ORDER BY nationkey", "SELECT nationkey, regionkey FROM tpch.tiny.nation ORDER BY nationkey");
+        assertQuery("SELECT nationkey, regionkey FROM nation ORDER BY nationkey", "SELECT nationkey, regionkey FROM tpch.tiny.nation ORDER BY nationkey");
     }
 
     private List<QualifiedObjectName> listMemoryTables()
@@ -131,31 +129,25 @@ public class TestMemorySmoke
         return queryRunner.listTables(queryRunner.getDefaultSession(), "memory", "default");
     }
 
-    private void assertThatQueryReturnsValue(String sql, Object expected)
+    private void assertQueryResult(String sql, Object... expected)
     {
-        assertThatQueryReturnsValue(sql, expected, null);
+        MaterializedResult rows = queryRunner.execute(sql);
+        assertEquals(rows.getRowCount(), expected.length);
+
+        for (int i = 0; i < expected.length; i++) {
+            MaterializedRow materializedRow = rows.getMaterializedRows().get(i);
+            int fieldCount = materializedRow.getFieldCount();
+            assertTrue(fieldCount == 1, format("Expected only one column, but got '%d'", fieldCount));
+            Object value = materializedRow.getField(0);
+            assertEquals(value, expected[i]);
+            assertTrue(materializedRow.getFieldCount() == 1);
+        }
     }
 
-    private void assertThatQueryReturnsValue(String sql, Object expected, Session session)
+    private void assertQuery(String sql, String expected)
     {
-        MaterializedResult rows = session == null ? queryRunner.execute(sql) : queryRunner.execute(session, sql);
-        MaterializedRow materializedRow = Iterables.getOnlyElement(rows);
-        int fieldCount = materializedRow.getFieldCount();
-        assertTrue(fieldCount == 1, format("Expected only one column, but got '%d'", fieldCount));
-        Object value = materializedRow.getField(0);
-        assertEquals(value, expected);
-        assertTrue(Iterables.getOnlyElement(rows).getFieldCount() == 1);
-    }
-
-    private void assertThatQueryReturnsSameValueAs(String sql, String compareSql)
-    {
-        assertThatQueryReturnsSameValueAs(sql, compareSql, null);
-    }
-
-    private void assertThatQueryReturnsSameValueAs(String sql, String compareSql, Session session)
-    {
-        MaterializedResult rows = session == null ? queryRunner.execute(sql) : queryRunner.execute(session, sql);
-        MaterializedResult expectedRows = session == null ? queryRunner.execute(compareSql) : queryRunner.execute(session, compareSql);
+        MaterializedResult rows = queryRunner.execute(sql);
+        MaterializedResult expectedRows = queryRunner.execute(expected);
 
         assertEquals(rows, expectedRows);
     }
