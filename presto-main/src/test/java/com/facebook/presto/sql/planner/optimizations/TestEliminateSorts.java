7b431b36a201f077653aec63fb062aa4ae7b024e
Martin Traverso <mtraverso@gmail.com>
2017-05-17 11:04:22 -0700

    Remove deprecated MergeProjections optimizer
    It was replaced in 0.174

--- presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestEliminateSorts.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestEliminateSorts.java
@@ -92,7 +92,6 @@ public class TestEliminateSorts
                 new UnaliasSymbolReferences(),
                 new AddExchanges(queryRunner.getMetadata(), new SqlParser()),
                 new PruneUnreferencedOutputs(),
-                new MergeProjections(),
                 new IterativeOptimizer(new StatsRecorder(), ImmutableSet.of(new RemoveRedundantIdentityProjections()))
         );
 
