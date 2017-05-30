608d977227428f37857533de11d09ebb84d525c4
Lukasz Osipiuk <lukasz@osipiuk.net>
2017-05-31 00:19:32 +0200

    Simplify plan building in TestTransformCorrelatedScalarAggregationToJoin

--- presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/TestTransformCorrelatedScalarAggregationToJoin.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/TestTransformCorrelatedScalarAggregationToJoin.java
@@ -15,30 +15,19 @@ package com.facebook.presto.sql.planner.iterative.rule;
 
 import com.facebook.presto.block.BlockEncodingManager;
 import com.facebook.presto.metadata.FunctionRegistry;
-import com.facebook.presto.metadata.Signature;
-import com.facebook.presto.spi.type.TypeSignature;
 import com.facebook.presto.sql.analyzer.FeaturesConfig;
-import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.iterative.Rule;
 import com.facebook.presto.sql.planner.iterative.rule.test.PlanBuilder;
 import com.facebook.presto.sql.planner.iterative.rule.test.RuleTester;
-import com.facebook.presto.sql.planner.plan.AggregationNode;
 import com.facebook.presto.sql.planner.plan.Assignments;
 import com.facebook.presto.sql.planner.plan.JoinNode;
-import com.facebook.presto.sql.planner.plan.PlanNode;
-import com.facebook.presto.sql.tree.FunctionCall;
-import com.facebook.presto.sql.tree.QualifiedName;
 import com.facebook.presto.type.TypeRegistry;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.List;
-import java.util.Optional;
-
 import static com.facebook.presto.spi.type.BigintType.BIGINT;
-import static com.facebook.presto.sql.analyzer.TypeSignatureProvider.fromTypeSignatures;
 import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.aggregation;
 import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.assignUniqueId;
 import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.expression;
@@ -49,8 +38,6 @@ import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.values
 
 public class TestTransformCorrelatedScalarAggregationToJoin
 {
-    private static final QualifiedName SUM = QualifiedName.of("sum");
-
     private RuleTester tester;
     private FunctionRegistry functionRegistry;
     private Rule rule;
@@ -101,8 +88,10 @@ public class TestTransformCorrelatedScalarAggregationToJoin
                 .on(p -> p.lateral(
                         ImmutableList.of(p.symbol("corr", BIGINT)),
                         p.values(p.symbol("corr", BIGINT)),
-                        createSumAggregation(p, p.symbol("a", BIGINT), ImmutableList.of(ImmutableList.of(p.symbol("b", BIGINT))),
-                                p.values(p.symbol("a", BIGINT), p.symbol("b", BIGINT)))))
+                    p.aggregation(ab -> ab
+                        .source(p.values(p.symbol("a", BIGINT), p.symbol("b", BIGINT)))
+                        .addAggregation(p.symbol("sum", BIGINT), PlanBuilder.expression("sum(a)"), ImmutableList.of(BIGINT))
+                        .groupingSets(ImmutableList.of(ImmutableList.of(p.symbol("b", BIGINT)))))))
                 .doesNotFire();
     }
 
@@ -113,8 +102,10 @@ public class TestTransformCorrelatedScalarAggregationToJoin
                 .on(p -> p.lateral(
                         ImmutableList.of(p.symbol("corr", BIGINT)),
                         p.values(p.symbol("corr", BIGINT)),
-                        createSumAggregation(p, p.symbol("a", BIGINT), ImmutableList.of(ImmutableList.of()),
-                                p.values(p.symbol("a", BIGINT), p.symbol("b", BIGINT)))))
+                    p.aggregation(ab -> ab
+                        .source(p.values(p.symbol("a", BIGINT), p.symbol("b", BIGINT)))
+                        .addAggregation(p.symbol("sum", BIGINT), PlanBuilder.expression("sum(a)"), ImmutableList.of(BIGINT))
+                        .groupingSets(ImmutableList.of(ImmutableList.of())))))
                 .matches(
                         project(ImmutableMap.of("sum_1", expression("sum_1"), "corr", expression("corr")),
                                 aggregation(ImmutableMap.of("sum_1", functionCall("sum", ImmutableList.of("a"))),
@@ -134,8 +125,10 @@ public class TestTransformCorrelatedScalarAggregationToJoin
                         ImmutableList.of(p.symbol("corr", BIGINT)),
                         p.values(p.symbol("corr", BIGINT)),
                         p.project(Assignments.of(p.symbol("expr", BIGINT), p.expression("sum + 1")),
-                                createSumAggregation(p, p.symbol("a", BIGINT), ImmutableList.of(ImmutableList.of()),
-                                        p.values(p.symbol("a", BIGINT), p.symbol("b", BIGINT))))))
+                            p.aggregation(ab -> ab
+                                .source(p.values(p.symbol("a", BIGINT), p.symbol("b", BIGINT)))
+                                .addAggregation(p.symbol("sum", BIGINT), PlanBuilder.expression("sum(a)"), ImmutableList.of(BIGINT))
+                                .groupingSets(ImmutableList.of(ImmutableList.of()))))))
                 .matches(
                         project(ImmutableMap.of("corr", expression("corr"), "expr", expression("(\"sum_1\" + 1)")),
                                 aggregation(ImmutableMap.of("sum_1", functionCall("sum", ImmutableList.of("a"))),
@@ -145,16 +138,5 @@ public class TestTransformCorrelatedScalarAggregationToJoin
                                                         values(ImmutableMap.of("corr", 0))),
                                                 project(ImmutableMap.of("non_null", expression("true")),
                                                         values(ImmutableMap.of("a", 0, "b", 1)))))));
-    }
-
-    private AggregationNode createSumAggregation(PlanBuilder p, Symbol symbol, List<List<Symbol>> groupingSets, PlanNode source)
-    {
-        FunctionCall functionCall = new FunctionCall(SUM, ImmutableList.of(p.symbol("a", BIGINT).toSymbolReference()));
-        TypeSignature typeSignature = p.getSymbols().get(symbol).getTypeSignature();
-        Signature signature = functionRegistry.resolveFunction(SUM, fromTypeSignatures(ImmutableList.of(typeSignature)));
-
-        AggregationNode.Aggregation aggregation = new AggregationNode.Aggregation(functionCall, signature, Optional.empty());
-
-        return p.aggregation(source, ImmutableMap.of(p.symbol("sum", BIGINT), aggregation), groupingSets, AggregationNode.Step.SINGLE, Optional.empty(), Optional.empty());
     }
 }
