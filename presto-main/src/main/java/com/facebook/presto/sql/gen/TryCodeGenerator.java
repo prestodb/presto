53e650b0eb5714372222332e496497619be8d30c
Leiqing Cai <leiqing@fb.com>
2017-06-05 12:43:32 -0700

    Improve error handling for varargs functions
    Avoid generating bytecode that exceeds the method parameter limit.

--- presto-main/src/main/java/com/facebook/presto/sql/gen/TryCodeGenerator.java
+++ presto-main/src/main/java/com/facebook/presto/sql/gen/TryCodeGenerator.java
@@ -42,10 +42,12 @@ import static com.facebook.presto.bytecode.expression.BytecodeExpressions.consta
 import static com.facebook.presto.spi.StandardErrorCode.DIVISION_BY_ZERO;
 import static com.facebook.presto.spi.StandardErrorCode.INVALID_CAST_ARGUMENT;
 import static com.facebook.presto.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMENT;
+import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.facebook.presto.spi.StandardErrorCode.NUMERIC_VALUE_OUT_OF_RANGE;
 import static com.facebook.presto.sql.gen.BytecodeUtils.boxPrimitiveIfNecessary;
 import static com.facebook.presto.sql.gen.BytecodeUtils.invoke;
 import static com.facebook.presto.sql.gen.BytecodeUtils.unboxPrimitiveIfNecessary;
+import static com.facebook.presto.util.Failures.checkCondition;
 import static com.facebook.presto.util.Reflection.methodHandle;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
@@ -95,6 +97,7 @@ public class TryCodeGenerator
             RowExpression innerRowExpression,
             CallSiteBinder callSiteBinder)
     {
+        checkCondition(inputParameters.size() <= 254, NOT_SUPPORTED, "Too many arguments for method");
         MethodDefinition method = classDefinition.declareMethod(a(PUBLIC), methodName, type(returnType), inputParameters);
         Scope calleeMethodScope = method.getScope();
 
