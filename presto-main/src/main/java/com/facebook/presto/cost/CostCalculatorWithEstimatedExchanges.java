49c36c6b635ec40c5d84d22e1882531d7c2f9277
Piotr Nowojski <piotr.nowojski@teradata.com>
2017-06-16 15:31:47 +0200

    Implement CostCalculators

--- presto-main/src/main/java/com/facebook/presto/cost/CostCalculatorWithEstimatedExchanges.java
+++ presto-main/src/main/java/com/facebook/presto/cost/CostCalculatorWithEstimatedExchanges.java
@@ -19,14 +19,21 @@ import com.facebook.presto.metadata.InternalNodeManager;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.iterative.Lookup;
+import com.facebook.presto.sql.planner.plan.AggregationNode;
+import com.facebook.presto.sql.planner.plan.JoinNode;
 import com.facebook.presto.sql.planner.plan.PlanNode;
+import com.facebook.presto.sql.planner.plan.PlanVisitor;
+import com.facebook.presto.sql.planner.plan.SemiJoinNode;
 
 import javax.annotation.concurrent.ThreadSafe;
 import javax.inject.Inject;
 
 import java.util.Map;
 
-import static com.facebook.presto.cost.PlanNodeCostEstimate.UNKNOWN_COST;
+import static com.facebook.presto.cost.PlanNodeCostEstimate.ZERO_COST;
+import static com.facebook.presto.sql.planner.plan.ExchangeNode.Scope.REMOTE;
+import static com.facebook.presto.sql.planner.plan.ExchangeNode.Type.REPARTITION;
+import static com.facebook.presto.sql.planner.plan.ExchangeNode.Type.REPLICATE;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -54,6 +61,93 @@ public class CostCalculatorWithEstimatedExchanges
     @Override
     public PlanNodeCostEstimate calculateCost(PlanNode planNode, Lookup lookup, Session session, Map<Symbol, Type> types)
     {
-        return UNKNOWN_COST;
+        ExchangeCostEstimator exchangeCostEstimator = new ExchangeCostEstimator(
+                session,
+                types,
+                lookup,
+                numberOfNodes);
+        PlanNodeCostEstimate estimatedExchangeCost = planNode.accept(exchangeCostEstimator, null);
+
+        return costCalculator.calculateCost(planNode, lookup, session, types).add(estimatedExchangeCost);
+    }
+
+    private class ExchangeCostEstimator
+            extends PlanVisitor<PlanNodeCostEstimate, Void>
+    {
+        private final Session session;
+        private final Map<Symbol, Type> types;
+        private final Lookup lookup;
+        private final int numberOfNodes;
+
+        public ExchangeCostEstimator(Session session, Map<Symbol, Type> types, Lookup lookup, int numberOfNodes)
+        {
+            this.session = requireNonNull(session, "session is null");
+            this.types = requireNonNull(types, "types is null");
+            this.lookup = lookup;
+            this.numberOfNodes = numberOfNodes;
+        }
+
+        @Override
+        protected PlanNodeCostEstimate visitPlan(PlanNode node, Void context)
+        {
+            return ZERO_COST;
+        }
+
+        @Override
+        public PlanNodeCostEstimate visitAggregation(AggregationNode node, Void context)
+        {
+            return CostCalculatorUsingExchanges.calculateExchangeCost(
+                    numberOfNodes,
+                    getStats(node.getSource()),
+                    REPARTITION,
+                    REMOTE);
+        }
+
+        @Override
+        public PlanNodeCostEstimate visitJoin(JoinNode node, Void context)
+        {
+            return calculateJoinCost(
+                    node.getLeft(),
+                    node.getRight(),
+                    node.getDistributionType().orElse(JoinNode.DistributionType.PARTITIONED).equals(JoinNode.DistributionType.REPLICATED));
+        }
+
+        @Override
+        public PlanNodeCostEstimate visitSemiJoin(SemiJoinNode node, Void context)
+        {
+            return calculateJoinCost(
+                    node.getSource(),
+                    node.getFilteringSource(),
+                    node.getDistributionType().orElse(SemiJoinNode.DistributionType.PARTITIONED).equals(SemiJoinNode.DistributionType.REPLICATED));
+        }
+
+        private PlanNodeCostEstimate calculateJoinCost(PlanNode probe, PlanNode build, boolean replicated)
+        {
+            if (replicated) {
+                return CostCalculatorUsingExchanges.calculateExchangeCost(
+                        numberOfNodes,
+                        getStats(build),
+                        REPLICATE,
+                        REMOTE);
+            }
+            else {
+                PlanNodeCostEstimate probeCost = CostCalculatorUsingExchanges.calculateExchangeCost(
+                        numberOfNodes,
+                        getStats(probe),
+                        REPARTITION,
+                        REMOTE);
+                PlanNodeCostEstimate buildCost = CostCalculatorUsingExchanges.calculateExchangeCost(
+                        numberOfNodes,
+                        getStats(build),
+                        REPARTITION,
+                        REMOTE);
+                return probeCost.add(buildCost);
+            }
+        }
+
+        private PlanNodeStatsEstimate getStats(PlanNode node)
+        {
+            return lookup.getStats(node, session, types);
+        }
     }
 }
