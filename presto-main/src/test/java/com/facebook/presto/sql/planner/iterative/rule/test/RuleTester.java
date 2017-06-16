406206b25a84bf179b4caec7abf9bd918ddf5452
Lukasz Osipiuk <lukasz@osipiuk.net>
2017-06-16 15:31:57 +0200

    Allow passing external QueryRunner to RuleTester

--- presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/RuleTester.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/RuleTester.java
@@ -37,17 +37,28 @@ public class RuleTester
 
     public RuleTester()
     {
-        session = testSessionBuilder()
+        this(createQueryRunner());
+    }
+
+    private static LocalQueryRunner createQueryRunner()
+    {
+        Session session = testSessionBuilder()
                 .setCatalog("local")
                 .setSchema("tiny")
                 .setSystemProperty("task_concurrency", "1") // these tests don't handle exchanges from local parallel
                 .build();
 
-        queryRunner = new LocalQueryRunner(session);
+        LocalQueryRunner queryRunner = new LocalQueryRunner(session);
         queryRunner.createCatalog(session.getCatalog().get(),
                 new TpchConnectorFactory(1),
                 ImmutableMap.<String, String>of());
+        return queryRunner;
+    }
 
+    public RuleTester(LocalQueryRunner queryRunner)
+    {
+        this.queryRunner = queryRunner;
+        this.session = queryRunner.getDefaultSession();
         this.metadata = queryRunner.getMetadata();
         this.transactionManager = queryRunner.getTransactionManager();
         this.accessControl = queryRunner.getAccessControl();
