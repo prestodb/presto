08c922fec035ab0cb1308244360fea8cce4892f2
Alan Post <alan.post@teradata.com>
2017-06-20 11:11:31 -0400

    Add PruneCrossJoinColumns optimization rule
    Add PruneCrossJoinColumns, and add a test, and enable the rule.
    
    This is a separate rule from non-cross joins because non-cross joins
    support output symbol selection, and so need two rules, one to prune
    using a project from above, and one to insert projects over its
    children.  Cross joins, on the other hand, always pass through all the
    symbols of their children, so they can push a project through in one
    step.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
@@ -32,6 +32,7 @@ import com.facebook.presto.sql.planner.iterative.rule.MergeLimitWithDistinct;
 import com.facebook.presto.sql.planner.iterative.rule.MergeLimitWithSort;
 import com.facebook.presto.sql.planner.iterative.rule.MergeLimitWithTopN;
 import com.facebook.presto.sql.planner.iterative.rule.MergeLimits;
+import com.facebook.presto.sql.planner.iterative.rule.PruneCrossJoinColumns;
 import com.facebook.presto.sql.planner.iterative.rule.PruneSemiJoinColumns;
 import com.facebook.presto.sql.planner.iterative.rule.PruneSemiJoinFilteringSourceColumns;
 import com.facebook.presto.sql.planner.iterative.rule.PruneTableScanColumns;
@@ -127,6 +128,7 @@ public class PlanOptimizers
 
         // TODO: Once we've migrated handling all the plan node types, replace uses of PruneUnreferencedOutputs with an IterativeOptimizer containing these rules.
         Set<Rule> columnPruningRules = ImmutableSet.of(
+                new PruneCrossJoinColumns(),
                 new PruneSemiJoinColumns(),
                 new PruneSemiJoinFilteringSourceColumns(),
                 new PruneValuesColumns(),
