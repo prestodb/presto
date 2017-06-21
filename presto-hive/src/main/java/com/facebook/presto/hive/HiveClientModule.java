53a3444ce6068e6cce56886623da12a2326bac53
Dain Sundstrom <dain@iq80.com>
2017-06-21 11:50:12 -0700

    Add connectorId to NamenodeStats JMX name

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveClientModule.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveClientModule.java
@@ -81,7 +81,7 @@ public class HiveClientModule
         binder.bind(HiveTableProperties.class).in(Scopes.SINGLETON);
 
         binder.bind(NamenodeStats.class).in(Scopes.SINGLETON);
-        newExporter(binder).export(NamenodeStats.class).as(generatedNameOf(NamenodeStats.class));
+        newExporter(binder).export(NamenodeStats.class).as(generatedNameOf(NamenodeStats.class, connectorId));
 
         binder.bind(HiveMetastoreClientFactory.class).in(Scopes.SINGLETON);
         binder.bind(HiveCluster.class).to(StaticHiveCluster.class).in(Scopes.SINGLETON);
