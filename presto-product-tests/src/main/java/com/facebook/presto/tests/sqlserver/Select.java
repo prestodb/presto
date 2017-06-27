49d5fb48a4080b58917ab79a8e556c054f2e71da
Sanjay Sharma <sanjay990@gmail.com>
2017-06-27 13:10:16 -0400

    Fix failing test for SQL Server

--- presto-product-tests/src/main/java/com/facebook/presto/tests/sqlserver/Select.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/sqlserver/Select.java
@@ -156,7 +156,7 @@ public class Select
                                 Timestamp.valueOf("9999-12-31 23:59:59.999"), Timestamp.valueOf("2079-06-06 00:00:00"),
                                 Double.valueOf("12345678912.3456756"), Float.valueOf("12345678.6557")
                         ),
-                        row(null, null, null, null, null, null, null, null, null, null, null, null, null, null)
+                        row(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null)
                 );
     }
 
