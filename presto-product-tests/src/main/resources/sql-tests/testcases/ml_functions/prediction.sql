7c64e31adcf42446f2bde3c444e06b23e5c4f8df
Haozhun Jin <haozhun.jin@gmail.com>
2017-06-28 11:15:25 -0700

    Fix product test ml_functions to avoid constant folding

--- presto-product-tests/src/main/resources/sql-tests/testcases/ml_functions/prediction.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/ml_functions/prediction.sql
@@ -1,6 +1,6 @@
 -- database: presto; groups: ml_functions
 --!
-SELECT classify(features(1, 2), model)
+SELECT classify(features(1, 2 + random(1)), model)
 FROM (
   SELECT learn_classifier(labels, features) AS model
   FROM (VALUES (1, features(1, 2))) t (labels, features)
--- presto-product-tests/src/main/resources/sql-tests/testcases/ml_functions/varcharPrediction.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/ml_functions/varcharPrediction.sql
@@ -1,6 +1,6 @@
 -- database: presto; groups: ml_functions
 --!
-SELECT classify(features(1, 2), model)
+SELECT classify(features(1, 2 + random(1)), model)
 FROM (
   SELECT learn_classifier(labels, features) AS model
   FROM (VALUES ('cat', features(1, 2))) t (labels, features)
