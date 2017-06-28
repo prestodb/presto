89da491de7cb95f9065f622d46500d1fd7200b9f
Haozhun Jin <haozhun.jin@gmail.com>
2017-06-28 11:14:20 -0700

    Rename product test group ml_connector to ml_functions

--- presto-product-tests/src/main/resources/sql-tests/testcases/ml_functions/prediction.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/ml_functions/prediction.sql
@@ -1,9 +1,9 @@
--- database: presto; groups: ml_connector
+-- database: presto; groups: ml_functions
 --!
 SELECT classify(features(1, 2), model)
 FROM (
   SELECT learn_classifier(labels, features) AS model
-  FROM (VALUES ('cat', features(1, 2))) t (labels, features)
+  FROM (VALUES (1, features(1, 2))) t (labels, features)
 ) t2
 --!
-cat|
+1|
