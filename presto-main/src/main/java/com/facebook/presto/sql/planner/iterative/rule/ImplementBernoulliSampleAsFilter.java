96798b6426b90cbd74b04c00bef74b8483ca242a
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-07-07 11:29:20 +0200

    Remove extra explicit type

--- presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/ImplementBernoulliSampleAsFilter.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/ImplementBernoulliSampleAsFilter.java
@@ -25,7 +25,6 @@ import com.facebook.presto.sql.planner.plan.SampleNode;
 import com.facebook.presto.sql.tree.ComparisonExpression;
 import com.facebook.presto.sql.tree.ComparisonExpressionType;
 import com.facebook.presto.sql.tree.DoubleLiteral;
-import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.FunctionCall;
 import com.facebook.presto.sql.tree.QualifiedName;
 import com.google.common.collect.ImmutableList;
@@ -72,7 +71,7 @@ public class ImplementBernoulliSampleAsFilter
                 sample.getSource(),
                 new ComparisonExpression(
                         ComparisonExpressionType.LESS_THAN,
-                        new FunctionCall(QualifiedName.of("rand"), ImmutableList.<Expression>of()),
+                        new FunctionCall(QualifiedName.of("rand"), ImmutableList.of()),
                         new DoubleLiteral(Double.toString(sample.getSampleRatio())))));
     }
 }
