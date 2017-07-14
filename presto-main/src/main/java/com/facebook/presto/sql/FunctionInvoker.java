02d1c913364f4c1ae6ef1db27c5980a832141271
Maciej Grzybek <maciej.grzybek@teradata.com>
2017-07-14 11:11:29 -0400

    Use ExpressionInterpreter.invoke inside FunctionInvoker.invoke

--- presto-main/src/main/java/com/facebook/presto/sql/FunctionInvoker.java
+++ presto-main/src/main/java/com/facebook/presto/sql/FunctionInvoker.java
@@ -17,16 +17,11 @@ import com.facebook.presto.metadata.FunctionRegistry;
 import com.facebook.presto.metadata.Signature;
 import com.facebook.presto.operator.scalar.ScalarFunctionImplementation;
 import com.facebook.presto.spi.ConnectorSession;
-import com.google.common.base.Defaults;
-import com.google.common.base.Throwables;
+import com.facebook.presto.sql.planner.ExpressionInterpreter;
 
-import java.lang.invoke.MethodHandle;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 public class FunctionInvoker
@@ -51,42 +46,6 @@ public class FunctionInvoker
     public Object invoke(Signature function, ConnectorSession session, List<Object> arguments)
     {
         ScalarFunctionImplementation implementation = registry.getScalarFunctionImplementation(function);
-        MethodHandle method = implementation.getMethodHandle();
-
-        List<Object> actualArguments = new ArrayList<>(arguments.size() + 1);
-
-        Iterator<Object> iterator = arguments.iterator();
-        for (int i = 0; i < method.type().parameterCount(); i++) {
-            Class<?> parameterType = method.type().parameterType(i);
-            if (parameterType == ConnectorSession.class) {
-                actualArguments.add(session);
-            }
-            else {
-                checkArgument(iterator.hasNext(), "Not enough arguments provided for method: %s", method.type());
-                Object argument = iterator.next();
-                if (implementation.getNullFlags().get(i)) {
-                    boolean isNull = argument == null;
-                    if (isNull) {
-                        argument = Defaults.defaultValue(parameterType);
-                    }
-                    actualArguments.add(argument);
-                    actualArguments.add(isNull);
-                    // Skip the next method parameter which is marked @IsNull
-                    i++;
-                }
-                else {
-                    actualArguments.add(argument);
-                }
-            }
-        }
-
-        checkArgument(!iterator.hasNext(), "Too many arguments provided for method: %s", method.type());
-
-        try {
-            return method.invokeWithArguments(actualArguments);
-        }
-        catch (Throwable throwable) {
-            throw Throwables.propagate(throwable);
-        }
+        return ExpressionInterpreter.invoke(session, implementation, arguments);
     }
 }
