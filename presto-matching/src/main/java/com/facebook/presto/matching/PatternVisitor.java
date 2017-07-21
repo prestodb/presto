a550e0c1168665d70e366b4d53dd52871602f06b
Artur Gajowy <artur.gajowy@gmail.com>
2017-07-22 00:42:08 +0200

    Add filtering Pattern-s

--- presto-matching/src/main/java/com/facebook/presto/matching/PatternVisitor.java
+++ presto-matching/src/main/java/com/facebook/presto/matching/PatternVisitor.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.matching;
 
 import com.facebook.presto.matching.pattern.CapturePattern;
+import com.facebook.presto.matching.pattern.FilterPattern;
 import com.facebook.presto.matching.pattern.TypeOfPattern;
 import com.facebook.presto.matching.pattern.WithPattern;
 
@@ -24,6 +25,8 @@ public interface PatternVisitor
     void visitWith(WithPattern<?> pattern);
 
     void visitCapture(CapturePattern<?> pattern);
+
+    void visitFilter(FilterPattern<?> pattern);
 
     default void visitPrevious(Pattern pattern)
     {
