c382bf95e53f0ab645dc1096d008b3aed0cce194
Anton Petrov <petroav@users.noreply.github.com>
2017-07-24 17:08:53 -0400

    Unquarantine TPC-DS q27, q36, q70 and q86
    Adjust result sets with extra whitespace on the CHAR
    columns.

--- presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q86.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q86.sql
@@ -1,4 +1,4 @@
--- database: presto_tpcds; groups: tpcds, quarantine; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
+-- database: presto_tpcds; groups: tpcds; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
 --- quarantined as grouping is not yet supported, see https://github.com/prestodb/presto/pull/6704
 SELECT
   "sum"("ws_net_paid") "total_sum"
