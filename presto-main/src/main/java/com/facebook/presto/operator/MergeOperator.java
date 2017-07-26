e79d269be617f01e19a0be9c3104e9de3060a404
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-07-26 18:46:59 -0400

    Implement MergeOperator
    The MergeOperator merges N sorted streams of data together.
    It contains one ExchangeClient per remote split. In order
    to accurately merge the data, the operator needs from
    each of the remote locations either at least one
    page or for that remote location to be finished sending data.
    
    As in other operators like LookupJoinOperator that construct
    pages, the MergeOperator only returns full pages.

---
+++ presto-main/src/main/java/com/facebook/presto/operator/MergeOperator.java
@@ -0,0 +1,314 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator;
+
+import com.facebook.presto.execution.SystemMemoryUsageListener;
+import com.facebook.presto.execution.buffer.PagesSerde;
+import com.facebook.presto.execution.buffer.PagesSerdeFactory;
+import com.facebook.presto.memory.LocalMemoryContext;
+import com.facebook.presto.metadata.Split;
+import com.facebook.presto.operator.MergeSortProcessor.PageWithPosition;
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.PageBuilder;
+import com.facebook.presto.spi.UpdatablePageSource;
+import com.facebook.presto.spi.block.SortOrder;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.split.RemoteSplit;
+import com.facebook.presto.sql.planner.plan.PlanNodeId;
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Closer;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.Objects.requireNonNull;
+
+public class MergeOperator
+        implements SourceOperator, Closeable
+{
+    public static class MergeOperatorFactory
+            implements SourceOperatorFactory
+    {
+        private final int operatorId;
+        private final PlanNodeId sourceId;
+        private final ExchangeClientSupplier exchangeClientSupplier;
+        private final PagesSerdeFactory serdeFactory;
+        private final List<Type> types;
+        private final List<Integer> outputChannels;
+        private final List<Type> outputTypes;
+        private final List<Integer> sortChannels;
+        private final List<SortOrder> sortOrder;
+        private final PageComparatorFactory pageComparatorFactory;
+        private boolean closed;
+
+        public MergeOperatorFactory(
+                int operatorId,
+                PlanNodeId sourceId,
+                ExchangeClientSupplier exchangeClientSupplier,
+                PagesSerdeFactory serdeFactory,
+                PageComparatorFactory pageComparatorFactory,
+                List<Type> types,
+                List<Integer> outputChannels,
+                List<Integer> sortChannels,
+                List<SortOrder> sortOrder)
+        {
+            this.operatorId = operatorId;
+            this.sourceId = requireNonNull(sourceId, "sourceId is null");
+            this.exchangeClientSupplier = requireNonNull(exchangeClientSupplier, "exchangeClientSupplier is null");
+            this.serdeFactory = requireNonNull(serdeFactory, "serdeFactory is null");
+            this.types = requireNonNull(types, "types is null");
+            this.outputChannels = requireNonNull(outputChannels, "outputChannels is null");
+            this.outputTypes = outputTypes(types, outputChannels);
+            this.sortChannels = requireNonNull(sortChannels, "sortChannels is null");
+            this.sortOrder = requireNonNull(sortOrder, "sortOrder is null");
+            this.pageComparatorFactory = requireNonNull(pageComparatorFactory, "pageComparatorFactory is null");
+        }
+
+        private static List<Type> outputTypes(List<? extends Type> sourceTypes, List<Integer> outputChannels)
+        {
+            return outputChannels.stream()
+                    .map(sourceTypes::get)
+                    .collect(toImmutableList());
+        }
+
+        @Override
+        public PlanNodeId getSourceId()
+        {
+            return sourceId;
+        }
+
+        @Override
+        public List<Type> getTypes()
+        {
+            return outputTypes;
+        }
+
+        @Override
+        public SourceOperator createOperator(DriverContext driverContext)
+        {
+            checkState(!closed, "Factory is already closed");
+            OperatorContext operatorContext = driverContext.addOperatorContext(operatorId, sourceId, MergeOperator.class.getSimpleName());
+
+            return new MergeOperator(
+                    operatorContext,
+                    sourceId,
+                    () -> exchangeClientSupplier.get(new UpdateSystemMemory(driverContext.getPipelineContext())),
+                    serdeFactory.createPagesSerde(),
+                    pageComparatorFactory.create(types, sortChannels, sortOrder),
+                    outputChannels,
+                    outputTypes);
+        }
+
+        @Override
+        public void close()
+        {
+            closed = true;
+        }
+    }
+
+    @NotThreadSafe
+    private static final class UpdateSystemMemory
+            implements SystemMemoryUsageListener
+    {
+        private final PipelineContext pipelineContext;
+
+        public UpdateSystemMemory(PipelineContext pipelineContext)
+        {
+            this.pipelineContext = requireNonNull(pipelineContext, "pipelineContext is null");
+        }
+
+        @Override
+        public void updateSystemMemoryUsage(long deltaMemoryInBytes)
+        {
+            if (deltaMemoryInBytes > 0) {
+                pipelineContext.reserveSystemMemory(deltaMemoryInBytes);
+            }
+            else {
+                pipelineContext.freeSystemMemory(-deltaMemoryInBytes);
+            }
+        }
+    }
+
+    private final OperatorContext operatorContext;
+    private final PlanNodeId sourceId;
+    private final Supplier<ExchangeClient> exchangeClientSupplier;
+    private final PagesSerde pagesSerde;
+    private final PageComparator comparator;
+    private final List<Integer> outputChannels;
+    private final List<Type> outputTypes;
+    private final PageBuilder pageBuilder;
+    private final LocalMemoryContext pageBuilderLocalMemoryContext;
+
+    private final SettableFuture<Void> blockedOnSplits = SettableFuture.create();
+
+    private final List<PageSupplier> pageSuppliers = new ArrayList<>();
+    private final Closer closer = Closer.create();
+
+    private boolean closed;
+
+    private MergeSortProcessor mergeSortProcessor;
+
+    public MergeOperator(
+            OperatorContext operatorContext,
+            PlanNodeId sourceId,
+            Supplier<ExchangeClient> exchangeClientSupplier,
+            PagesSerde pagesSerde,
+            PageComparator comparator,
+            List<Integer> outputChannels,
+            List<Type> outputTypes)
+    {
+        this.operatorContext = requireNonNull(operatorContext, "operatorContext is null");
+        this.sourceId = requireNonNull(sourceId, "sourceId is null");
+        this.exchangeClientSupplier = requireNonNull(exchangeClientSupplier, "exchangeClientSupplier is null");
+        this.pagesSerde = requireNonNull(pagesSerde, "pagesSerde is null");
+        this.comparator = requireNonNull(comparator, "comparator is null");
+        this.outputChannels = requireNonNull(outputChannels, "outputChannels is null");
+        this.outputTypes = requireNonNull(outputTypes, "outputTypes is null");
+        this.pageBuilder = new PageBuilder(outputTypes);
+        this.pageBuilderLocalMemoryContext = operatorContext.getSystemMemoryContext().newLocalMemoryContext();
+    }
+
+    @Override
+    public PlanNodeId getSourceId()
+    {
+        return sourceId;
+    }
+
+    @Override
+    public Supplier<Optional<UpdatablePageSource>> addSplit(Split split)
+    {
+        requireNonNull(split, "split is null");
+        checkArgument(split.getConnectorSplit() instanceof RemoteSplit, "split is not a remote split");
+        checkState(!blockedOnSplits.isDone(), "noMoreSplits has been called already");
+
+        URI location = ((RemoteSplit) split.getConnectorSplit()).getLocation();
+        ExchangeClient exchangeClient = closer.register(exchangeClientSupplier.get());
+        exchangeClient.addLocation(location);
+        exchangeClient.noMoreLocations();
+        pageSuppliers.add(new RemoteExchangePageSupplier(exchangeClient, pagesSerde));
+
+        return Optional::empty;
+    }
+
+    @Override
+    public void noMoreSplits()
+    {
+        mergeSortProcessor = new MergeSortProcessor(comparator, ImmutableList.copyOf(pageSuppliers), operatorContext.getSystemMemoryContext().newAggregatedMemoryContext());
+        closer.register(mergeSortProcessor);
+        blockedOnSplits.set(null);
+    }
+
+    @Override
+    public OperatorContext getOperatorContext()
+    {
+        return operatorContext;
+    }
+
+    @Override
+    public List<Type> getTypes()
+    {
+        return outputTypes;
+    }
+
+    @Override
+    public void finish()
+    {
+        close();
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return closed || (mergeSortProcessor != null && mergeSortProcessor.isFinished() && pageBuilder.isEmpty());
+    }
+
+    @Override
+    public ListenableFuture<?> isBlocked()
+    {
+        if (!blockedOnSplits.isDone()) {
+            return blockedOnSplits;
+        }
+        return mergeSortProcessor.isBlocked();
+    }
+
+    @Override
+    public boolean needsInput()
+    {
+        return false;
+    }
+
+    @Override
+    public void addInput(Page page)
+    {
+        throw new UnsupportedOperationException(getClass().getName() + " can not take input");
+    }
+
+    @Override
+    public Page getOutput()
+    {
+        checkState(mergeSortProcessor != null, "mergeSources must be non-null when getting output");
+
+        if (closed) {
+            return null;
+        }
+
+        while (!pageBuilder.isFull()) {
+            PageWithPosition pageWithPosition = mergeSortProcessor.poll();
+            if (pageWithPosition == null) {
+                break;
+            }
+
+            pageWithPosition.appendTo(pageBuilder, outputChannels, outputTypes);
+            pageBuilderLocalMemoryContext.setBytes(pageBuilder.getRetainedSizeInBytes());
+        }
+
+        if (pageBuilder.isEmpty()) {
+            return null;
+        }
+
+        // As in LookupJoinOperator, only flush full pages unless we are done
+        if (pageBuilder.isFull() || mergeSortProcessor.isFinished()) {
+            Page page = pageBuilder.build();
+            operatorContext.recordGeneratedInput(page.getSizeInBytes(), page.getPositionCount());
+            pageBuilder.reset();
+            return page;
+        }
+
+        return null;
+    }
+
+    @Override
+    public void close()
+    {
+        try {
+            closed = true;
+            closer.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ presto-main/src/test/java/com/facebook/presto/operator/TestMergeOperator.java
@@ -0,0 +1,387 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator;
+
+import com.facebook.presto.execution.buffer.PagesSerdeFactory;
+import com.facebook.presto.execution.buffer.TestingPagesSerdeFactory;
+import com.facebook.presto.metadata.RemoteTransactionHandle;
+import com.facebook.presto.metadata.Split;
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.block.SortOrder;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.split.RemoteSplit;
+import com.facebook.presto.sql.planner.plan.PlanNodeId;
+import com.facebook.presto.testing.MaterializedResult;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
+import io.airlift.http.client.HttpClient;
+import io.airlift.http.client.testing.TestingHttpClient;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.facebook.presto.RowPagesBuilder.rowPagesBuilder;
+import static com.facebook.presto.SessionTestUtils.TEST_SESSION;
+import static com.facebook.presto.operator.OperatorAssertion.assertOperatorIsBlocked;
+import static com.facebook.presto.operator.OperatorAssertion.assertOperatorIsUnblocked;
+import static com.facebook.presto.operator.OperatorAssertion.toMaterializedResult;
+import static com.facebook.presto.operator.PageAssertions.assertPageEquals;
+import static com.facebook.presto.spi.block.SortOrder.ASC_NULLS_FIRST;
+import static com.facebook.presto.spi.block.SortOrder.DESC_NULLS_FIRST;
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
+import static com.facebook.presto.spi.type.IntegerType.INTEGER;
+import static com.facebook.presto.testing.TestingTaskContext.createTaskContext;
+import static com.facebook.presto.testing.assertions.Assert.assertEquals;
+import static io.airlift.concurrent.Threads.daemonThreadsNamed;
+import static java.util.concurrent.Executors.newScheduledThreadPool;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
+@Test(singleThreaded = true)
+public class TestMergeOperator
+{
+    private static final String TASK_1_ID = "task1";
+    private static final String TASK_2_ID = "task2";
+    private static final String TASK_3_ID = "task3";
+
+    private ScheduledExecutorService executor;
+    private PagesSerdeFactory serdeFactory;
+    private HttpClient httpClient;
+    private ExchangeClientSupplier exchangeClientSupplier;
+    private AtomicInteger operatorId = new AtomicInteger();
+
+    private LoadingCache<String, TestingTaskBuffer> taskBuffers;
+
+    @BeforeClass
+    public void setUp()
+    {
+        executor = newScheduledThreadPool(10, daemonThreadsNamed("test-merge-operator-%s"));
+        serdeFactory = new TestingPagesSerdeFactory();
+
+        taskBuffers = CacheBuilder.newBuilder().build(CacheLoader.from(TestingTaskBuffer::new));
+        httpClient = new TestingHttpClient(new TestingExchangeHttpClientHandler(taskBuffers), executor);
+        exchangeClientSupplier = new ExchangeClientFactory(new ExchangeClientConfig(), httpClient, executor);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown()
+            throws Exception
+    {
+        executor.shutdownNow();
+        executor = null;
+        serdeFactory = null;
+        httpClient.close();
+        httpClient = null;
+        exchangeClientSupplier = null;
+    }
+
+    @BeforeMethod
+    public void setUpMethod()
+    {
+        taskBuffers.invalidateAll();
+    }
+
+    @Test
+    public void testSingleStream()
+            throws Exception
+    {
+        List<Type> types = ImmutableList.of(BIGINT, BIGINT);
+
+        MergeOperator operator = createMergeOperator(types, ImmutableList.of(1), ImmutableList.of(0, 1), ImmutableList.of(ASC_NULLS_FIRST, ASC_NULLS_FIRST));
+        assertFalse(operator.isFinished());
+        assertFalse(operator.isBlocked().isDone());
+
+        operator.addSplit(createRemoteSplit(TASK_1_ID));
+        assertFalse(operator.isFinished());
+        assertFalse(operator.isBlocked().isDone());
+
+        operator.noMoreSplits();
+        assertFalse(operator.isFinished());
+        assertFalse(operator.isBlocked().isDone());
+
+        List<Page> input = rowPagesBuilder(types)
+                .row(1, 1)
+                .row(2, 2)
+                .pageBreak()
+                .row(3, 3)
+                .row(4, 4)
+                .build();
+
+        assertOperatorIsBlocked(operator);
+        taskBuffers.getUnchecked(TASK_1_ID).addPage(input.get(0), false);
+        assertOperatorIsUnblocked(operator);
+
+        assertTrue(pullAvailablePages(operator).isEmpty(), "Should get no pages because the page is not yet full");
+
+        taskBuffers.getUnchecked(TASK_1_ID).addPage(input.get(1), true);
+        assertOperatorIsUnblocked(operator);
+
+        Page expected = rowPagesBuilder(BIGINT)
+                .row(1)
+                .row(2)
+                .row(3)
+                .row(4)
+                .build()
+                .get(0);
+        assertPageEquals(ImmutableList.of(BIGINT), pullAvailablePages(operator).get(0), expected);
+        assertTrue(operator.isFinished());
+
+        operator.close();
+    }
+
+    @Test
+    public void testMergeDifferentTypes()
+            throws Exception
+    {
+        ImmutableList<Type> types = ImmutableList.of(BIGINT, INTEGER);
+        MergeOperator operator = createMergeOperator(types, ImmutableList.of(1, 0), ImmutableList.of(1, 0), ImmutableList.of(DESC_NULLS_FIRST, ASC_NULLS_FIRST));
+        operator.addSplit(createRemoteSplit(TASK_1_ID));
+        operator.addSplit(createRemoteSplit(TASK_2_ID));
+        operator.noMoreSplits();
+
+        List<Page> task1Pages = rowPagesBuilder(types)
+                .row(0, null)
+                .row(1, 4)
+                .row(2, 3)
+                .build();
+
+        List<Page> task2Pages = rowPagesBuilder(types)
+                .row(null, 5)
+                .row(2, 5)
+                .row(4, 3)
+                .build();
+
+        taskBuffers.getUnchecked(TASK_1_ID).addPages(task1Pages, true);
+        assertOperatorIsBlocked(operator);
+        taskBuffers.getUnchecked(TASK_2_ID).addPages(task2Pages, true);
+        assertOperatorIsUnblocked(operator);
+
+        ImmutableList<Type> outputTypes = ImmutableList.of(INTEGER, BIGINT);
+        Page expected = rowPagesBuilder(outputTypes)
+                .row(null, 0)
+                .row(5, null)
+                .row(5, 2)
+                .row(4, 1)
+                .row(3, 2)
+                .row(3, 4)
+                .build()
+                .get(0);
+
+        assertPageEquals(outputTypes, pullAvailablePages(operator).get(0), expected);
+        assertTrue(operator.isFinished());
+        operator.close();
+    }
+
+    @Test
+    public void testMultipleStreamsSameOutputColumns()
+            throws Exception
+    {
+        List<Type> types = ImmutableList.of(BIGINT, BIGINT, BIGINT);
+
+        MergeOperator operator = createMergeOperator(types, ImmutableList.of(0, 1, 2), ImmutableList.of(0), ImmutableList.of(ASC_NULLS_FIRST));
+        operator.addSplit(createRemoteSplit(TASK_1_ID));
+        operator.addSplit(createRemoteSplit(TASK_2_ID));
+        operator.addSplit(createRemoteSplit(TASK_3_ID));
+        operator.noMoreSplits();
+
+        assertFalse(operator.isFinished());
+        assertFalse(operator.isBlocked().isDone());
+
+        List<Page> source1Pages = rowPagesBuilder(types)
+                .row(1, 1, 2)
+                .row(8, 1, 1)
+                .row(19, 1, 3)
+                .row(27, 1, 4)
+                .row(41, 2, 5)
+                .pageBreak()
+                .row(55, 1, 2)
+                .row(89, 1, 3)
+                .row(101, 1, 4)
+                .row(202, 1, 3)
+                .row(399, 2, 2)
+                .pageBreak()
+                .row(400, 1, 1)
+                .row(401, 1, 7)
+                .row(402, 1, 6)
+                .build();
+
+        List<Page> source2Pages = rowPagesBuilder(types)
+                .row(2, 1, 2)
+                .row(8, 1, 1)
+                .row(19, 1, 3)
+                .row(25, 1, 4)
+                .row(26, 2, 5)
+                .pageBreak()
+                .row(56, 1, 2)
+                .row(66, 1, 3)
+                .row(77, 1, 4)
+                .row(88, 1, 3)
+                .row(99, 2, 2)
+                .pageBreak()
+                .row(99, 1, 1)
+                .row(100, 1, 7)
+                .row(100, 1, 6)
+                .build();
+
+        List<Page> source3Pages = rowPagesBuilder(types)
+                .row(88, 1, 3)
+                .row(89, 1, 3)
+                .row(90, 1, 3)
+                .row(91, 1, 4)
+                .row(92, 2, 5)
+                .pageBreak()
+                .row(93, 1, 2)
+                .row(94, 1, 3)
+                .row(95, 1, 4)
+                .row(97, 1, 3)
+                .row(98, 2, 2)
+                .build();
+
+        List<Page> outputPages = new ArrayList<>();
+        taskBuffers.getUnchecked(TASK_1_ID).addPage(source1Pages.get(0), false);
+        assertOperatorIsBlocked(operator);
+        taskBuffers.getUnchecked(TASK_2_ID).addPage(source2Pages.get(0), false);
+        assertOperatorIsBlocked(operator);
+        taskBuffers.getUnchecked(TASK_3_ID).addPage(source3Pages.get(0), false);
+        outputPages.addAll(pullAvailablePages(operator));
+
+        taskBuffers.getUnchecked(TASK_1_ID).addPage(source1Pages.get(1), false);
+        assertOperatorIsBlocked(operator);
+        taskBuffers.getUnchecked(TASK_2_ID).addPage(source2Pages.get(1), false);
+        outputPages.addAll(pullAvailablePages(operator));
+
+        taskBuffers.getUnchecked(TASK_3_ID).addPage(source3Pages.get(1), true);
+        outputPages.addAll(pullAvailablePages(operator));
+
+        taskBuffers.getUnchecked(TASK_2_ID).addPage(source2Pages.get(2), true);
+        outputPages.addAll(pullAvailablePages(operator));
+
+        taskBuffers.getUnchecked(TASK_1_ID).addPage(source1Pages.get(2), true);
+        assertOperatorIsUnblocked(operator);
+        List<Page> expectedPages = rowPagesBuilder(types)
+                .row(1, 1, 2)
+                .row(2, 1, 2)
+                .row(8, 1, 1)
+                .row(8, 1, 1)
+                .row(19, 1, 3)
+                .row(19, 1, 3)
+                .row(25, 1, 4)
+                .row(26, 2, 5)
+                .row(27, 1, 4)
+                .row(41, 2, 5)
+                .row(55, 1, 2)
+                .row(56, 1, 2)
+                .row(66, 1, 3)
+                .row(77, 1, 4)
+                .row(88, 1, 3)
+                .row(88, 1, 3)
+                .row(89, 1, 3)
+                .row(89, 1, 3)
+                .row(90, 1, 3)
+                .row(91, 1, 4)
+                .row(92, 2, 5)
+                .row(93, 1, 2)
+                .row(94, 1, 3)
+                .row(95, 1, 4)
+                .row(97, 1, 3)
+                .row(98, 2, 2)
+                .row(99, 2, 2)
+                .row(99, 1, 1)
+                .row(100, 1, 7)
+                .row(100, 1, 6)
+                .row(101, 1, 4)
+                .row(202, 1, 3)
+                .row(399, 2, 2)
+                .row(400, 1, 1)
+                .row(401, 1, 7)
+                .row(402, 1, 6)
+                .build();
+        outputPages.addAll(pullAvailablePages(operator));
+
+        MaterializedResult actual = toMaterializedResult(operator.getOperatorContext().getSession(), types, outputPages);
+        MaterializedResult expected = toMaterializedResult(operator.getOperatorContext().getSession(), types, expectedPages);
+        assertEquals(actual, expected);
+
+        assertOperatorIsUnblocked(operator);
+        assertTrue(operator.isFinished());
+
+        assertNull(operator.getOutput());
+
+        operator.close();
+    }
+
+    private MergeOperator createMergeOperator(List<Type> sourceTypes, List<Integer> outputChannels, List<Integer> sortChannels, List<SortOrder> sortOrder)
+    {
+        MergeOperator.MergeOperatorFactory factory = new MergeOperator.MergeOperatorFactory(
+                operatorId.getAndIncrement(),
+                new PlanNodeId("plan_node_id" + operatorId.getAndIncrement()),
+                exchangeClientSupplier,
+                serdeFactory,
+                new SimplePageComparator.Factory(),
+                sourceTypes,
+                outputChannels,
+                sortChannels,
+                sortOrder);
+        DriverContext driverContext = createTaskContext(executor, executor, TEST_SESSION)
+                .addPipelineContext(0, true, true)
+                .addDriverContext();
+        return (MergeOperator) factory.createOperator(driverContext);
+    }
+
+    private static Split createRemoteSplit(String taskId)
+    {
+        return new Split(ExchangeOperator.REMOTE_CONNECTOR_ID, new RemoteTransactionHandle(), new RemoteSplit(URI.create("http://localhost/" + taskId)));
+    }
+
+    private static List<Page> pullAvailablePages(Operator operator)
+            throws InterruptedException
+    {
+        long endTime = System.nanoTime() + TimeUnit.SECONDS.toNanos(1);
+        List<Page> outputPages = new ArrayList<>();
+
+        assertOperatorIsUnblocked(operator);
+
+        while (operator.isBlocked().isDone() && System.nanoTime() < endTime) {
+            assertEquals(operator.needsInput(), false);
+            if (operator.isFinished()) {
+                break;
+            }
+
+            Page outputPage = operator.getOutput();
+            if (outputPage != null) {
+                outputPages.add(outputPage);
+            }
+            else {
+                Thread.sleep(10);
+            }
+        }
+
+        // verify state
+        assertEquals(operator.needsInput(), false, "Operator still wants input");
+        assertNull(operator.getOutput(), "Not finished pulling pages");
+
+        return outputPages;
+    }
+}
