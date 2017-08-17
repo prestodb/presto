17170497ad54f6f45b17289c19e17c0c7b715ff0
Dain Sundstrom <dain@iq80.com>
2017-08-16 22:03:28 -0700

    Add additional context information to HdfsEnvironment
    Expose user, queryId, schema and table to HdfsEnvironment so file system
    implementations can attribute usage to a query.

--- presto-hive/src/main/java/com/facebook/presto/hive/metastore/file/FileHiveMetastore.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/metastore/file/FileHiveMetastore.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.hive.metastore.file;
 
 import com.facebook.presto.hive.HdfsEnvironment;
+import com.facebook.presto.hive.HdfsEnvironment.HdfsContext;
 import com.facebook.presto.hive.HiveType;
 import com.facebook.presto.hive.SchemaAlreadyExistsException;
 import com.facebook.presto.hive.TableAlreadyExistsException;
@@ -89,7 +90,7 @@ public class FileHiveMetastore
 
     private final HdfsEnvironment hdfsEnvironment;
     private final Path catalogDirectory;
-    private final String metastoreUser;
+    private final HdfsContext hdfsContext;
     private final FileSystem metadataFileSystem;
 
     private final JsonCodec<DatabaseMetadata> databaseCodec = JsonCodec.jsonCodec(DatabaseMetadata.class);
@@ -107,9 +108,9 @@ public class FileHiveMetastore
     {
         this.hdfsEnvironment = requireNonNull(hdfsEnvironment, "hdfsEnvironment is null");
         this.catalogDirectory = new Path(requireNonNull(catalogDirectory, "baseDirectory is null"));
-        this.metastoreUser = requireNonNull(metastoreUser, "metastoreUser is null");
+        this.hdfsContext = new HdfsContext(requireNonNull(metastoreUser, "metastoreUser is null"));
         try {
-            metadataFileSystem = hdfsEnvironment.getFileSystem(metastoreUser, this.catalogDirectory);
+            metadataFileSystem = hdfsEnvironment.getFileSystem(hdfsContext, this.catalogDirectory);
         }
         catch (IOException e) {
             throw new PrestoException(HIVE_METASTORE_ERROR, e);
@@ -214,7 +215,7 @@ public class FileHiveMetastore
         else if (table.getTableType().equals(EXTERNAL_TABLE.name())) {
             try {
                 Path externalLocation = new Path(table.getStorage().getLocation());
-                FileSystem externalFileSystem = hdfsEnvironment.getFileSystem(metastoreUser, externalLocation);
+                FileSystem externalFileSystem = hdfsEnvironment.getFileSystem(hdfsContext, externalLocation);
                 if (!externalFileSystem.isDirectory(externalLocation)) {
                     throw new PrestoException(HIVE_METASTORE_ERROR, "External table location does not exist");
                 }
@@ -531,7 +532,7 @@ public class FileHiveMetastore
         else if (table.getTableType().equals(EXTERNAL_TABLE.name())) {
             try {
                 Path externalLocation = new Path(partition.getStorage().getLocation());
-                FileSystem externalFileSystem = hdfsEnvironment.getFileSystem(metastoreUser, externalLocation);
+                FileSystem externalFileSystem = hdfsEnvironment.getFileSystem(hdfsContext, externalLocation);
                 if (!externalFileSystem.isDirectory(externalLocation)) {
                     throw new PrestoException(HIVE_METASTORE_ERROR, "External partition location does not exist");
                 }
