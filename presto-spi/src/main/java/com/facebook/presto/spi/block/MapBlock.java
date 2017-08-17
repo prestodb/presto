0f4f4b11f34c6ec50506e68326b086e8380a33e6
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-08-17 22:42:32 +0200

    Fix theoretical race condition in getSizeInBytes()
    `long` is not guaranteed to be updated atomically, unless it is
    `volatile`.

--- presto-spi/src/main/java/com/facebook/presto/spi/block/MapBlock.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/block/MapBlock.java
@@ -41,7 +41,7 @@ public class MapBlock
     private final Block valueBlock;
     private final int[] hashTables; // hash to location in map;
 
-    private long sizeInBytes;
+    private volatile long sizeInBytes;
     private final long retainedSizeInBytes;
 
     /**
@@ -125,7 +125,6 @@ public class MapBlock
     @Override
     public long getSizeInBytes()
     {
-        // this is racy but is safe because sizeInBytes is an int and the calculation is stable
         if (sizeInBytes < 0) {
             calculateSize();
         }
