76f50d6170a5b2ab0746f5a5cfdc13d9f6a3e6ab
Leiqing Cai <leiqing@fb.com>
2017-08-18 15:05:31 -0700

    Fix Unnest.equals() by comparing field withOrdinality

--- presto-parser/src/test/java/com/facebook/presto/sql/parser/TestSqlParser.java
+++ presto-parser/src/test/java/com/facebook/presto/sql/parser/TestSqlParser.java
@@ -1738,21 +1738,21 @@ public class TestSqlParser
                                 new Table(QualifiedName.of("t")),
                                 new Unnest(ImmutableList.of(new Identifier("a")), false),
                                 Optional.empty())));
-        assertStatement("SELECT * FROM t CROSS JOIN UNNEST(a) WITH ORDINALITY",
+        assertStatement("SELECT * FROM t CROSS JOIN UNNEST(a, b) WITH ORDINALITY",
                 simpleQuery(
                         selectList(new AllColumns()),
                         new Join(
                                 Join.Type.CROSS,
                                 new Table(QualifiedName.of("t")),
-                                new Unnest(ImmutableList.of(new Identifier("a")), true),
+                                new Unnest(ImmutableList.of(new Identifier("a"), new Identifier("b")), true),
                                 Optional.empty())));
-        assertStatement("SELECT * FROM t FULL JOIN UNNEST(a) ON true",
+        assertStatement("SELECT * FROM t FULL JOIN UNNEST(a) AS tmp (c) ON true",
                 simpleQuery(
                         selectList(new AllColumns()),
                         new Join(
                                 Join.Type.FULL,
                                 new Table(QualifiedName.of("t")),
-                                new Unnest(ImmutableList.of(new Identifier("a")), true),
+                                new AliasedRelation(new Unnest(ImmutableList.of(new Identifier("a")), false), new Identifier("tmp"), ImmutableList.of(new Identifier("c"))),
                                 Optional.of(new JoinOn(BooleanLiteral.TRUE_LITERAL)))));
     }
 
