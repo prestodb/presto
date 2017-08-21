82821985dc14c02274a54b4328136b53df123699
David Phillips <david@acz.org>
2017-08-21 16:08:31 -0700

    Update to Airbase 69 and Airlift 0.150
    Also update testing MySQL server to 5.7.19

--- presto-main/src/main/java/com/facebook/presto/server/testing/TestingPrestoServer.java
+++ presto-main/src/main/java/com/facebook/presto/server/testing/TestingPrestoServer.java
@@ -180,7 +180,6 @@ public class TestingPrestoServer
                 .putAll(properties)
                 .put("coordinator", String.valueOf(coordinator))
                 .put("presto.version", "testversion")
-                .put("http-client.max-threads", "16")
                 .put("task.concurrency", "4")
                 .put("task.max-worker-threads", "4")
                 .put("exchange.client-threads", "4");
