9fdfd77bd01ff18dcde17aa15e657721aaafd52f
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-08-27 21:40:01 +0200

    Make resolveGroup to accept only GroupReference
    resolveGroup suggests that it resolves groups (GroupReference`s).
    Accepting regular plan nodes is error-prone, because:
     - it returns proper node even if the node is not stored within a Memo
     - it is not defensive as it does not matter what caller pass there,
     method is going to work anyway
     - resolving regular plan nodes does not make much sense

--- presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/Lookup.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/Lookup.java
@@ -14,12 +14,11 @@
 package com.facebook.presto.sql.planner.iterative;
 
 import com.facebook.presto.sql.planner.plan.PlanNode;
-import com.google.common.collect.ImmutableList;
 
 import java.util.List;
 import java.util.function.Function;
 
-import static com.google.common.base.Verify.verify;
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.Iterables.getOnlyElement;
 
 public interface Lookup
@@ -35,15 +34,17 @@ public interface Lookup
     @Deprecated
     default PlanNode resolve(PlanNode node)
     {
-        return getOnlyElement(resolveGroup(node));
+        if (node instanceof GroupReference) {
+            return getOnlyElement(resolveGroup(node));
+        }
+        return node;
     }
 
     /**
      * Resolves nodes by materializing GroupReference nodes
      * representing symbolic references to other nodes.
      * <p>
-     * If the node is not a GroupReference, it returns the
-     * singleton of the argument node.
+     * @throws IllegalArgumentException if the node is not a GroupReference
      */
     List<PlanNode> resolveGroup(PlanNode node);
 
@@ -54,19 +55,15 @@ public interface Lookup
     static Lookup noLookup()
     {
         return node -> {
-            verify(!(node instanceof GroupReference), "Unexpected GroupReference");
-            return ImmutableList.of(node);
+            throw new UnsupportedOperationException();
         };
     }
 
     static Lookup from(Function<GroupReference, List<PlanNode>> resolver)
     {
         return node -> {
-            if (node instanceof GroupReference) {
-                return resolver.apply((GroupReference) node);
-            }
-
-            return ImmutableList.of(node);
+            checkArgument(node instanceof GroupReference, "Node is not a GroupReference");
+            return resolver.apply((GroupReference) node);
         };
     }
 }
