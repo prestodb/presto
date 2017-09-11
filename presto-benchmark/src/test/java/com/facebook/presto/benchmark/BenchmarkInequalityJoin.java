912d08db70921a0e07111c049d834abc0ef97542
anusudarsan <anu.sudarsan@teradata.com>
2017-09-11 10:13:15 +0200

    Add tests for non-equi join condition optimization

--- presto-benchmark/src/test/java/com/facebook/presto/benchmark/BenchmarkInequalityJoin.java
+++ presto-benchmark/src/test/java/com/facebook/presto/benchmark/BenchmarkInequalityJoin.java
@@ -107,6 +107,20 @@ public class BenchmarkInequalityJoin
                 .execute("SELECT count(*) FROM t1 JOIN t2 on (t1.bucket = t2.bucket) WHERE t1.val1 < t2.val2");
     }
 
+    @Benchmark
+    public List<Page> benchmarkJoinWithArithmeticInPredicate(Context context)
+    {
+        return context.getQueryRunner()
+                .execute("SELECT count(*) FROM t1 JOIN t2 on (t1.bucket = t2.bucket) AND t1.val1 < t2.val2 + 10");
+    }
+
+    @Benchmark
+    public List<Page> benchmarkJoinWithFunctionPredicate(Context context)
+    {
+        return context.getQueryRunner()
+                .execute("SELECT count(*) FROM t1 JOIN t2 on (t1.bucket = t2.bucket) AND t1.val1 < sin(t2.val2)");
+    }
+
     public static void main(String[] args)
             throws RunnerException
     {
