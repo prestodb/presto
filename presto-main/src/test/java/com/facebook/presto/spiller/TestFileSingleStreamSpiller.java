42140864e3eaa00b0540324f8a1a39b5e504bcb5
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-09-23 00:37:21 +0200

    Close spill files as soon as they are read
    `SingleStreamSpiller` provides iterator of `Page`-s backed by an
    `InputFileStream`. The stream needs to be closed once the iterator is
    exhausted, to avoid descriptors' "temporary leak".

--- presto-main/src/test/java/com/facebook/presto/spiller/TestFileSingleStreamSpiller.java
+++ presto-main/src/test/java/com/facebook/presto/spiller/TestFileSingleStreamSpiller.java
@@ -33,6 +33,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 
 import static com.facebook.presto.spi.type.BigintType.BIGINT;
@@ -81,14 +82,16 @@ public class TestFileSingleStreamSpiller
         // for spilling memory should be accounted only during spill() method is executing
         assertEquals(memoryContext.getBytes(), 0);
 
-        ImmutableList<Page> spilledPages = ImmutableList.copyOf(spiller.getSpilledPages());
+        Iterator<Page> spilledPagesIterator = spiller.getSpilledPages();
+        assertEquals(memoryContext.getBytes(), FileSingleStreamSpiller.BUFFER_SIZE);
+        ImmutableList<Page> spilledPages = ImmutableList.copyOf(spilledPagesIterator);
+        assertEquals(memoryContext.getBytes(), 0);
 
         assertEquals(4, spilledPages.size());
         for (int i = 0; i < 4; ++i) {
             PageAssertions.assertPageEquals(TYPES, page, spilledPages.get(i));
         }
 
-        assertEquals(memoryContext.getBytes(), FileSingleStreamSpiller.BUFFER_SIZE);
         spiller.close();
         assertEquals(listFiles(spillPath.toPath()).size(), 0);
         assertEquals(memoryContext.getBytes(), 0);
