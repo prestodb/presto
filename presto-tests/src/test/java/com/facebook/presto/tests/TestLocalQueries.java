4c7dea71f4403961f7e2b20fa19972739aa3f4b2
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-11-19 20:51:51 +0100

    Test IterativeOptimizer with table with plenty columns

--- presto-tests/src/test/java/com/facebook/presto/tests/TestLocalQueries.java
+++ presto-tests/src/test/java/com/facebook/presto/tests/TestLocalQueries.java
@@ -17,13 +17,26 @@ import com.facebook.presto.Session;
 import com.facebook.presto.connector.ConnectorId;
 import com.facebook.presto.metadata.SessionPropertyManager;
 import com.facebook.presto.plugin.blackhole.BlackHoleConnectorFactory;
+import com.facebook.presto.plugin.blackhole.BlackHoleMetadata;
+import com.facebook.presto.spi.ColumnMetadata;
+import com.facebook.presto.spi.ConnectorTableMetadata;
+import com.facebook.presto.spi.SchemaTableName;
+import com.facebook.presto.spi.type.BigintType;
 import com.facebook.presto.testing.LocalQueryRunner;
 import com.facebook.presto.testing.MaterializedResult;
 import com.facebook.presto.tpch.TpchConnectorFactory;
 import com.google.common.collect.ImmutableMap;
+import io.airlift.units.Duration;
 import org.testng.annotations.Test;
 
+import java.util.stream.IntStream;
+
 import static com.facebook.presto.SystemSessionProperties.PUSH_PARTIAL_AGGREGATION_THROUGH_JOIN;
+import static com.facebook.presto.plugin.blackhole.BlackHoleConnector.FIELD_LENGTH_PROPERTY;
+import static com.facebook.presto.plugin.blackhole.BlackHoleConnector.PAGES_PER_SPLIT_PROPERTY;
+import static com.facebook.presto.plugin.blackhole.BlackHoleConnector.PAGE_PROCESSING_DELAY;
+import static com.facebook.presto.plugin.blackhole.BlackHoleConnector.ROWS_PER_PAGE_PROPERTY;
+import static com.facebook.presto.plugin.blackhole.BlackHoleConnector.SPLIT_COUNT_PROPERTY;
 import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
 import static com.facebook.presto.testing.MaterializedResult.resultBuilder;
@@ -31,6 +44,7 @@ import static com.facebook.presto.testing.TestingSession.TESTING_CATALOG;
 import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
 import static com.facebook.presto.testing.assertions.Assert.assertEquals;
 import static com.facebook.presto.tpch.TpchMetadata.TINY_SCHEMA_NAME;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public class TestLocalQueries
         extends AbstractTestQueries
@@ -91,5 +105,42 @@ public class TestLocalQueries
                 .build();
 
         assertEquals(result, expectedStatistics);
+    }
+
+    @Test(timeOut = 10 * 60 * 1000)
+    public void testPlannerWithPlentyColumnsTable()
+            throws Exception
+    {
+        Session session = Session.builder(getSession())
+                .setCatalog(BLACKHOLE_CATALOG)
+                .build();
+
+        String tableName = "plenty_columns";
+        createPlentyColumnsTable(session, tableName);
+
+        computeActual(session, String.format("SELECT * FROM %s", tableName));
+        computeActual(session, String.format("SELECT * FROM %s a, %s b WHERE a.column_1 = b.column_1", tableName, tableName));
+    }
+
+    private void createPlentyColumnsTable(Session session, String tableName)
+    {
+        // I wish CREATE TABLE was easier in tests
+        LocalQueryRunner queryRunner = (LocalQueryRunner) getQueryRunner();
+        queryRunner.inTransaction(session, txSession -> {
+            ConnectorTableMetadata tableMetadata = new ConnectorTableMetadata(
+                    new SchemaTableName(BlackHoleMetadata.SCHEMA_NAME, tableName),
+                    IntStream.range(0, 2000)
+                            .mapToObj(i -> new ColumnMetadata("column_" + i, BigintType.BIGINT))
+                            .collect(toImmutableList()),
+                    ImmutableMap.of(
+                            SPLIT_COUNT_PROPERTY, 0,
+                            PAGES_PER_SPLIT_PROPERTY, 0,
+                            ROWS_PER_PAGE_PROPERTY, 0,
+                            FIELD_LENGTH_PROPERTY, 0,
+                            PAGE_PROCESSING_DELAY, Duration.succinctNanos(0L)));
+
+            queryRunner.getMetadata().createTable(txSession, BLACKHOLE_CATALOG, tableMetadata, true);
+            return null;
+        });
     }
 }
