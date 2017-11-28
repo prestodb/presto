c46580fd0ebd20d43017d53c794b302a51bab256
Christina Wallin <christina.wallin@teradata.com>
2017-11-28 14:51:06 +0100

    Add SHOW CURRENT ROLES
    Instead of select * from information_schema.roles, SHOW CURRENT ROLES
    rewrites to select * from information_schema.enabled_roles.
    
    All users can see what roles they're currently using, so no need
    for access control checks.

--- presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
+++ presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -932,7 +932,8 @@ class AstBuilder
     {
         return new ShowRoles(
                 getLocation(context),
-                getIdentifierIfPresent(context.identifier()));
+                getIdentifierIfPresent(context.identifier()),
+                context.CURRENT() != null);
     }
 
     @Override
