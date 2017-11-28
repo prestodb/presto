ad6f88a877e3def2f925ac2845ef0cadb64e1338
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-11-28 14:50:36 +0100

    Introduce <catalog>.information_schema.roles table

--- presto-main/src/main/java/com/facebook/presto/connector/informationSchema/InformationSchemaMetadata.java
+++ presto-main/src/main/java/com/facebook/presto/connector/informationSchema/InformationSchemaMetadata.java
@@ -73,6 +73,7 @@ public class InformationSchemaMetadata
     public static final SchemaTableName TABLE_VIEWS = new SchemaTableName(INFORMATION_SCHEMA, "views");
     public static final SchemaTableName TABLE_SCHEMATA = new SchemaTableName(INFORMATION_SCHEMA, "schemata");
     public static final SchemaTableName TABLE_TABLE_PRIVILEGES = new SchemaTableName(INFORMATION_SCHEMA, "table_privileges");
+    public static final SchemaTableName TABLE_ROLES = new SchemaTableName(INFORMATION_SCHEMA, "roles");
 
     public static final Map<SchemaTableName, ConnectorTableMetadata> TABLES = schemaMetadataBuilder()
             .table(tableMetadataBuilder(TABLE_COLUMNS)
@@ -112,6 +113,9 @@ public class InformationSchemaMetadata
                     .column("privilege_type", createUnboundedVarcharType())
                     .column("is_grantable", BOOLEAN)
                     .column("with_hierarchy", BOOLEAN)
+                    .build())
+            .table(tableMetadataBuilder(TABLE_ROLES)
+                    .column("role_name", createUnboundedVarcharType())
                     .build())
             .build();
 
