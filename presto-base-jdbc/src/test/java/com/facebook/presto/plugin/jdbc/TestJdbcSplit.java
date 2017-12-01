733bab885ba7fe2d15fce1c6c7014319005dceed
Piotr Findeisen <piotr.findeisen@gmail.com>
2017-12-01 21:08:39 +0100

    Remove connection parameters from JdbcSplit and JdbcOutputTableHandle
    Since the parameters are fed back to `BaseJdbcClient` which already
    knows them, it is not necessary to pass them around.

--- presto-base-jdbc/src/test/java/com/facebook/presto/plugin/jdbc/TestJdbcSplit.java
+++ presto-base-jdbc/src/test/java/com/facebook/presto/plugin/jdbc/TestJdbcSplit.java
@@ -15,7 +15,6 @@ package com.facebook.presto.plugin.jdbc;
 
 import com.facebook.presto.spi.predicate.TupleDomain;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import io.airlift.json.JsonCodec;
 import org.testng.annotations.Test;
 
@@ -24,7 +23,7 @@ import static org.testng.Assert.assertEquals;
 
 public class TestJdbcSplit
 {
-    private final JdbcSplit split = new JdbcSplit("connectorId", "catalog", "schemaName", "tableName", "connectionUrl", ImmutableMap.of(), TupleDomain.all());
+    private final JdbcSplit split = new JdbcSplit("connectorId", "catalog", "schemaName", "tableName", TupleDomain.all());
 
     @Test
     public void testAddresses()
@@ -33,7 +32,7 @@ public class TestJdbcSplit
         assertEquals(split.getAddresses(), ImmutableList.of());
         assertEquals(split.isRemotelyAccessible(), true);
 
-        JdbcSplit jdbcSplit = new JdbcSplit("connectorId", "catalog", "schemaName", "tableName", "connectionUrl", ImmutableMap.of(), TupleDomain.all());
+        JdbcSplit jdbcSplit = new JdbcSplit("connectorId", "catalog", "schemaName", "tableName", TupleDomain.all());
         assertEquals(jdbcSplit.getAddresses(), ImmutableList.of());
     }
 
