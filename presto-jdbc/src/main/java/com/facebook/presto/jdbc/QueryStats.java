82f4527c0b10421fc8d0f87b4376a1c738cd5360
Rongrong Zhong <rongrong@fb.com>
2017-12-05 15:14:44 -0800

    Add peakMemoryBytes to QueryStats in JDBC driver

--- presto-jdbc/src/main/java/com/facebook/presto/jdbc/QueryStats.java
+++ presto-jdbc/src/main/java/com/facebook/presto/jdbc/QueryStats.java
@@ -39,6 +39,7 @@ public final class QueryStats
     private final long elapsedTimeMillis;
     private final long processedRows;
     private final long processedBytes;
+    private final long peakMemoryBytes;
     private final Optional<StageStats> rootStage;
 
     public QueryStats(
@@ -58,6 +59,7 @@ public final class QueryStats
             long elapsedTimeMillis,
             long processedRows,
             long processedBytes,
+            long peakMemoryBytes,
             Optional<StageStats> rootStage)
     {
         this.queryId = requireNonNull(queryId, "queryId is null");
@@ -76,6 +78,7 @@ public final class QueryStats
         this.elapsedTimeMillis = elapsedTimeMillis;
         this.processedRows = processedRows;
         this.processedBytes = processedBytes;
+        this.peakMemoryBytes = peakMemoryBytes;
         this.rootStage = requireNonNull(rootStage, "rootStage is null");
     }
 
@@ -98,6 +101,7 @@ public final class QueryStats
                 stats.getElapsedTimeMillis(),
                 stats.getProcessedRows(),
                 stats.getProcessedBytes(),
+                stats.getPeakMemoryBytes(),
                 Optional.ofNullable(stats.getRootStage()).map(StageStats::create));
     }
 
@@ -179,6 +183,11 @@ public final class QueryStats
     public long getProcessedBytes()
     {
         return processedBytes;
+    }
+
+    public long getPeakMemoryBytes()
+    {
+        return peakMemoryBytes;
     }
 
     public Optional<StageStats> getRootStage()
