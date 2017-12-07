035aa48a784c32ea95dd9894a084447a172c2f18
Aleksei Statkevich <astatkevich@fb.com>
2017-12-07 12:47:40 -0800

    Remove thrift client stats module
    Stats provided by ThriftClientStatsModule appear to be not very useful,
    especially given that we now have a number of stats populated by
    Thrift connector itself.

--- presto-thrift-connector/src/main/java/com/facebook/presto/connector/thrift/ThriftConnectorFactory.java
+++ presto-thrift-connector/src/main/java/com/facebook/presto/connector/thrift/ThriftConnectorFactory.java
@@ -21,7 +21,6 @@ import com.facebook.presto.spi.connector.ConnectorFactory;
 import com.facebook.presto.spi.type.TypeManager;
 import com.facebook.swift.codec.guice.ThriftCodecModule;
 import com.facebook.swift.service.guice.ThriftClientModule;
-import com.facebook.swift.service.guice.ThriftClientStatsModule;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import io.airlift.bootstrap.Bootstrap;
@@ -69,7 +68,6 @@ public class ThriftConnectorFactory
                     new MBeanModule(),
                     new ThriftCodecModule(),
                     new ThriftClientModule(),
-                    new ThriftClientStatsModule(),
                     binder -> {
                         binder.bind(MBeanServer.class).toInstance(new RebindSafeMBeanServer(getPlatformMBeanServer()));
                         binder.bind(TypeManager.class).toInstance(context.getTypeManager());
