f1e8053eb81ec4c96a54452f8a8f7f07865c8630
Aleksei Statkevich <astatkevich@fb.com>
2017-12-07 12:48:07 -0800

    Remove unused json module from Thrift connector

--- presto-thrift-connector/src/main/java/com/facebook/presto/connector/thrift/ThriftConnectorFactory.java
+++ presto-thrift-connector/src/main/java/com/facebook/presto/connector/thrift/ThriftConnectorFactory.java
@@ -24,7 +24,6 @@ import com.facebook.swift.service.guice.ThriftClientModule;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import io.airlift.bootstrap.Bootstrap;
-import io.airlift.json.JsonModule;
 import org.weakref.jmx.guice.MBeanModule;
 
 import javax.management.MBeanServer;
@@ -64,7 +63,6 @@ public class ThriftConnectorFactory
     {
         try {
             Bootstrap app = new Bootstrap(
-                    new JsonModule(),
                     new MBeanModule(),
                     new ThriftCodecModule(),
                     new ThriftClientModule(),
