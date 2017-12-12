37d8664c6556b2ffcda24b82827ce1ca167612fd
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-12-12 10:27:03 +0100

    Make possible to change JDBC driver class and jar in product tests

--- presto-product-tests/bin/run_on_docker.sh
+++ presto-product-tests/bin/run_on_docker.sh
@@ -51,7 +51,8 @@ function run_product_tests() {
   run_in_application_runner_container \
     java "-Djava.util.logging.config.file=/docker/volumes/conf/tempto/logging.properties" \
     -Duser.timezone=Asia/Kathmandu \
-    -jar "/docker/volumes/presto-product-tests/presto-product-tests-executable.jar" \
+    -cp "/docker/volumes/jdbc/driver.jar:/docker/volumes/presto-product-tests/presto-product-tests-executable.jar" \
+    com.facebook.presto.tests.TemptoProductTestRunner \
     --report-dir "/docker/volumes/test-reports" \
     --config "tempto-configuration.yaml,/docker/volumes/tempto/tempto-configuration-local.yaml" \
     "$@" \
