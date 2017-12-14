7ad1912c9ca64a0113348b6f4ca639c13778cba3
Raghav Sethi <raghavsethi.rs@gmail.com>
2017-12-14 01:07:48 -0800

    Fix resource group validation for WEIGHTED_FAIR

--- presto-resource-group-managers/src/main/java/com/facebook/presto/resourceGroups/AbstractResourceConfigurationManager.java
+++ presto-resource-group-managers/src/main/java/com/facebook/presto/resourceGroups/AbstractResourceConfigurationManager.java
@@ -35,7 +35,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
 
-import static com.facebook.presto.spi.resourceGroups.SchedulingPolicy.WEIGHTED;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static io.airlift.units.DataSize.Unit.BYTE;
@@ -68,15 +67,20 @@ public abstract class AbstractResourceConfigurationManager
                 checkArgument(group.getSoftCpuLimit().get().compareTo(group.getHardCpuLimit().get()) <= 0, "Soft CPU limit cannot be greater than hard CPU limit");
             }
             if (group.getSchedulingPolicy().isPresent()) {
-                if (group.getSchedulingPolicy().get() == WEIGHTED) {
-                    for (ResourceGroupSpec subGroup : group.getSubGroups()) {
-                        checkArgument(subGroup.getSchedulingWeight().isPresent(), "Must specify scheduling weight for each sub group when using \"weighted\" scheduling policy");
-                    }
-                }
-                else {
-                    for (ResourceGroupSpec subGroup : group.getSubGroups()) {
-                        checkArgument(!subGroup.getSchedulingWeight().isPresent(), "Must use \"weighted\" scheduling policy when using scheduling weight");
-                    }
+                switch (group.getSchedulingPolicy().get()) {
+                    case WEIGHTED:
+                        for (ResourceGroupSpec subGroup : group.getSubGroups()) {
+                            checkArgument(subGroup.getSchedulingWeight().isPresent(), "Must specify scheduling weight for each sub group when using \"weighted\" scheduling policy");
+                        }
+                        break;
+                    case QUERY_PRIORITY:
+                    case FAIR:
+                        for (ResourceGroupSpec subGroup : group.getSubGroups()) {
+                            checkArgument(!subGroup.getSchedulingWeight().isPresent(), "Must use \"weighted\" or \"weighted_fair\" scheduling policy when using scheduling weight");
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
         }
