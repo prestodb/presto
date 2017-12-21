ac834e827000bf4436ab140bdbe27b7d14ae5d47
Piotr Findeisen <piotr.findeisen@gmail.com>
2017-12-21 14:10:09 +0100

    Use ZonedDateTime for TIMESTAMP WITH TIME ZONE in tests
    See #9527 for rationale.

--- presto-spi/src/main/java/com/facebook/presto/spi/type/SqlTimestampWithTimeZone.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/type/SqlTimestampWithTimeZone.java
@@ -26,7 +26,9 @@ import static com.facebook.presto.spi.type.DateTimeEncoding.unpackZoneKey;
 
 public final class SqlTimestampWithTimeZone
 {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss.SSS VV");
+    // This needs to be Locale-independent, work the same in Joda and Java Time and should never change, as it defines the external API data format.
+    public static final String JSON_FORMAT = "uuuu-MM-dd HH:mm:ss.SSS VV";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(JSON_FORMAT);
 
     private final long millisUtc;
     private final TimeZoneKey timeZoneKey;
