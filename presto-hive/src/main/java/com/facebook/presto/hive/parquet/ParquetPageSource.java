7ba0d0406e8afe3321aef375674696497bdf23c9
David Phillips <david@acz.org>
2017-12-27 11:30:32 -0800

    Fix error categorization for corrupt files during read

--- presto-hive/src/main/java/com/facebook/presto/hive/parquet/ParquetPageSource.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/parquet/ParquetPageSource.java
@@ -38,6 +38,7 @@ import java.util.Optional;
 import java.util.Properties;
 
 import static com.facebook.presto.hive.HiveColumnHandle.ColumnType.REGULAR;
+import static com.facebook.presto.hive.HiveErrorCode.HIVE_BAD_DATA;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_CURSOR_ERROR;
 import static com.facebook.presto.hive.parquet.ParquetTypeUtils.getDescriptor;
 import static com.facebook.presto.hive.parquet.ParquetTypeUtils.getFieldIndex;
@@ -209,6 +210,10 @@ public class ParquetPageSource
             closeWithSuppression(e);
             throw e;
         }
+        catch (ParquetCorruptionException e) {
+            closeWithSuppression(e);
+            throw new PrestoException(HIVE_BAD_DATA, e);
+        }
         catch (IOException | RuntimeException e) {
             closeWithSuppression(e);
             throw new PrestoException(HIVE_CURSOR_ERROR, e);
@@ -271,6 +276,9 @@ public class ParquetPageSource
             try {
                 Block block = parquetReader.readPrimitive(columnDescriptor, type);
                 lazyBlock.setBlock(block);
+            }
+            catch (ParquetCorruptionException e) {
+                throw new PrestoException(HIVE_BAD_DATA, e);
             }
             catch (IOException e) {
                 throw new PrestoException(HIVE_CURSOR_ERROR, e);
