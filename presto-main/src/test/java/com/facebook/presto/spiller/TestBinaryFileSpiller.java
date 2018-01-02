80fa95becf80d4edddc59532308562e4b97e909a
Nezih Yigitbasi <nezihy@fb.com>
2018-01-02 14:01:47 -0800

    Release spill buffer system memory on operator close

--- presto-main/src/test/java/com/facebook/presto/spiller/TestBinaryFileSpiller.java
+++ presto-main/src/test/java/com/facebook/presto/spiller/TestBinaryFileSpiller.java
@@ -148,11 +148,12 @@ public class TestBinaryFileSpiller
             spiller.spill(spill.iterator()).get();
         }
         assertEquals(spillerStats.getTotalSpilledBytes() - spilledBytesBefore, spilledBytes);
-        assertEquals(memoryContext.getBytes(), 0);
+        // At this point, the buffers should still be accounted for in the memory context, because
+        // the spiller (FileSingleStreamSpiller) doesn't release its memory reservation until it's closed.
+        assertEquals(memoryContext.getBytes(), spills.length * FileSingleStreamSpiller.BUFFER_SIZE);
 
         List<Iterator<Page>> actualSpills = spiller.getSpills();
         assertEquals(actualSpills.size(), spills.length);
-        assertEquals(memoryContext.getBytes(), spills.length * FileSingleStreamSpiller.BUFFER_SIZE);
 
         for (int i = 0; i < actualSpills.size(); i++) {
             List<Page> actualSpill = ImmutableList.copyOf(actualSpills.get(i));
