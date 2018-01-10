bc9b4ac6f88f89f5d711ee954b710e06c49a3433
Nezih Yigitbasi <nezihy@fb.com>
2018-01-10 12:36:13 -0800

    Simplify array and map unnesters

--- presto-main/src/main/java/com/facebook/presto/operator/MapUnnester.java
+++ presto-main/src/main/java/com/facebook/presto/operator/MapUnnester.java
@@ -16,11 +16,11 @@ package com.facebook.presto.operator;
 import com.facebook.presto.spi.PageBuilder;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
-import com.facebook.presto.spi.type.MapType;
 import com.facebook.presto.spi.type.Type;
 
 import javax.annotation.Nullable;
 
+import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 
 public class MapUnnester
@@ -29,28 +29,14 @@ public class MapUnnester
     private final Type keyType;
     private final Type valueType;
     private Block block;
-    private final int channelCount;
 
     private int position;
     private int positionCount;
 
-    public MapUnnester(MapType mapType, @Nullable Block mapBlock)
+    public MapUnnester(Type keyType, Type valueType)
     {
-        this.channelCount = 2;
-        requireNonNull(mapType, "mapType is null");
-        this.keyType = mapType.getKeyType();
-        this.valueType = mapType.getValueType();
-
-        this.block = mapBlock;
-        this.positionCount = mapBlock == null ? 0 : mapBlock.getPositionCount();
-    }
-
-    protected void appendTo(PageBuilder pageBuilder, int outputChannelOffset)
-    {
-        BlockBuilder keyBlockBuilder = pageBuilder.getBlockBuilder(outputChannelOffset);
-        BlockBuilder valueBlockBuilder = pageBuilder.getBlockBuilder(outputChannelOffset + 1);
-        keyType.appendTo(block, position++, keyBlockBuilder);
-        valueType.appendTo(block, position++, valueBlockBuilder);
+        this.keyType = requireNonNull(keyType, "keyType is null");
+        this.valueType = requireNonNull(valueType, "valueType is null");
     }
 
     @Override
@@ -62,13 +48,17 @@ public class MapUnnester
     @Override
     public final int getChannelCount()
     {
-        return channelCount;
+        return 2;
     }
 
     @Override
     public final void appendNext(PageBuilder pageBuilder, int outputChannelOffset)
     {
-        appendTo(pageBuilder, outputChannelOffset);
+        checkState(block != null, "block is null");
+        BlockBuilder keyBlockBuilder = pageBuilder.getBlockBuilder(outputChannelOffset);
+        BlockBuilder valueBlockBuilder = pageBuilder.getBlockBuilder(outputChannelOffset + 1);
+        keyType.appendTo(block, position++, keyBlockBuilder);
+        valueType.appendTo(block, position++, valueBlockBuilder);
     }
 
     @Override
