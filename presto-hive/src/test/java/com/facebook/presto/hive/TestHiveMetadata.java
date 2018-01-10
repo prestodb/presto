c541bd89f923b3d80612d35cc123b3a26a7fcdf0
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2018-01-10 09:28:56 +0100

    Remove unused variables in TestHiveMetadata

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveMetadata.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveMetadata.java
@@ -14,10 +14,7 @@
 package com.facebook.presto.hive;
 
 import com.facebook.presto.spi.SchemaTableName;
-import com.facebook.presto.spi.predicate.Domain;
 import com.facebook.presto.spi.predicate.NullableValue;
-import com.facebook.presto.spi.predicate.TupleDomain;
-import com.facebook.presto.spi.predicate.TupleDomain.ColumnDomain;
 import com.facebook.presto.spi.type.TypeSignature;
 import com.facebook.presto.spi.type.VarcharType;
 import com.google.common.collect.ImmutableList;
@@ -45,8 +42,6 @@ public class TestHiveMetadata
         ImmutableList.Builder<HivePartition> partitions = ImmutableList.builder();
 
         for (int i = 0; i < 5_000; i++) {
-            ColumnDomain<HiveColumnHandle> columnDomain = new ColumnDomain<>(TEST_COLUMN_HANDLE, Domain.singleValue(VarcharType.VARCHAR, Slices.utf8Slice(Integer.toString(i))));
-            TupleDomain<HiveColumnHandle> tupleDomain = TupleDomain.fromColumnDomains(Optional.of(ImmutableList.of(columnDomain)));
             partitions.add(new HivePartition(
                     new SchemaTableName("test", "test"),
                     Integer.toString(i),
@@ -62,8 +57,6 @@ public class TestHiveMetadata
         ImmutableList.Builder<HivePartition> partitions = ImmutableList.builder();
 
         for (int i = 0; i < 5; i++) {
-            ColumnDomain<HiveColumnHandle> columnDomain = new ColumnDomain<>(TEST_COLUMN_HANDLE, Domain.onlyNull(VarcharType.VARCHAR));
-            TupleDomain<HiveColumnHandle> tupleDomain = TupleDomain.fromColumnDomains(Optional.of(ImmutableList.of(columnDomain)));
             partitions.add(new HivePartition(
                     new SchemaTableName("test", "test"),
                     Integer.toString(i),
@@ -79,16 +72,12 @@ public class TestHiveMetadata
         ImmutableList.Builder<HivePartition> partitions = ImmutableList.builder();
 
         for (int i = 0; i < 5; i++) {
-            ColumnDomain<HiveColumnHandle> columnDomain = new ColumnDomain<>(TEST_COLUMN_HANDLE, Domain.singleValue(VarcharType.VARCHAR, Slices.utf8Slice(Integer.toString(i))));
-            TupleDomain<HiveColumnHandle> tupleDomain = TupleDomain.fromColumnDomains(Optional.of(ImmutableList.of(columnDomain)));
             partitions.add(new HivePartition(
                     new SchemaTableName("test", "test"),
                     Integer.toString(i),
                     ImmutableMap.of(TEST_COLUMN_HANDLE, NullableValue.of(VarcharType.VARCHAR, Slices.utf8Slice(Integer.toString(i)))), ImmutableList.of()));
         }
 
-        ColumnDomain<HiveColumnHandle> columnDomain = new ColumnDomain<>(TEST_COLUMN_HANDLE, Domain.onlyNull(VarcharType.VARCHAR));
-        TupleDomain<HiveColumnHandle> tupleDomain = TupleDomain.fromColumnDomains(Optional.of(ImmutableList.of(columnDomain)));
         partitions.add(new HivePartition(
                 new SchemaTableName("test", "test"),
                 "null",
