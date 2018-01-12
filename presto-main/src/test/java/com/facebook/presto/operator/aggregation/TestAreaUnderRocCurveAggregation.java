5fd3113affbe9bd4e1f38a2a567b7747d30601c0
Takuya Kitazawa <k.takuti@gmail.com>
2018-01-12 09:14:30 +0900

    WIP: Update AUC test cases
    as https://github.com/prestodb/presto/pull/8918

--- presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestAreaUnderRocCurveAggregation.java
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestAreaUnderRocCurveAggregation.java
@@ -15,6 +15,9 @@ package com.facebook.presto.operator.aggregation;
 
 import com.facebook.presto.metadata.MetadataManager;
 import com.facebook.presto.metadata.Signature;
+import com.facebook.presto.operator.aggregation.groupByAggregations.AggregationTestInput;
+import com.facebook.presto.operator.aggregation.groupByAggregations.AggregationTestInputBuilder;
+import com.facebook.presto.operator.aggregation.groupByAggregations.AggregationTestOutput;
 import com.facebook.presto.spi.block.Block;
 import org.testng.annotations.Test;
 
@@ -32,41 +35,91 @@ public class TestAreaUnderRocCurveAggregation
     @Test
     public void testSingleClass()
     {
-        testAggregation(null, createBooleansBlock(true, true, true), createDoublesBlock(0.3, 0.1, 0.2));
-        testAggregation(null, createBooleansBlock(false, false, false), createDoublesBlock(0.3, 0.1, 0.2));
+        testRunner(null, createBooleansBlock(true, true, true), createDoublesBlock(0.3, 0.1, 0.2));
+        testRunner(null, createBooleansBlock(false, false, false), createDoublesBlock(0.3, 0.1, 0.2));
     }
 
     @Test
     public void testTrivial()
     {
         // max
-        testAggregation(1.0, createBooleansBlock(true, true, false), createDoublesBlock(0.3, 0.2, 0.1));
+        testRunner(1.0, createBooleansBlock(true, true, false), createDoublesBlock(0.3, 0.2, 0.1));
 
         // min
-        testAggregation(0.0, createBooleansBlock(false, false, true), createDoublesBlock(0.3, 0.2, 0.1));
+        testRunner(0.0, createBooleansBlock(false, false, true), createDoublesBlock(0.3, 0.2, 0.1));
 
         // mid
-        testAggregation(0.5, createBooleansBlock(true, false, true), createDoublesBlock(0.3, 0.2, 0.1));
+        testRunner(0.5, createBooleansBlock(true, false, true), createDoublesBlock(0.3, 0.2, 0.1));
     }
 
     @Test
-    public void testAUC()
+    public void testAuc()
     {
         // In Python scikit-learn machine learning library:
         // >>> from sklearn.metrics import roc_auc_score
         // >>> roc_auc_score([0, 1, 0, 1, 1], [0.5, 0.3, 0.2, 0.8, 0.7])
         // 0.83333333333333326
-        testAggregation(0.8333333333, createBooleansBlock(false, true, false, true, true), createDoublesBlock(0.5, 0.3, 0.2, 0.8, 0.7));
+        testRunner(0.8333333333, createBooleansBlock(false, true, false, true, true), createDoublesBlock(0.5, 0.3, 0.2, 0.8, 0.7));
     }
 
-    private static void testAggregation(Double expectedValue, Block... blocks)
+    private static void testRunner(Double expectedValue, Block labels, Block scores)
     {
-        InternalAggregationFunction function = METADATA.getFunctionRegistry().getAggregateFunctionImplementation(
+        InternalAggregationFunction function = getInternalAggregationFunction();
+
+        AggregationTestInputBuilder testInputBuilder = new AggregationTestInputBuilder(
+                new Block[] {labels, scores},
+                function);
+        AggregationTestInput test = testInputBuilder.build();
+
+        AggregationTestOutput expectedOutput = new AggregationTestOutput(expectedValue);
+
+        test.runPagesOnAccumulatorWithAssertion(0L, test.createGroupedAccumulator(), expectedOutput);
+    }
+
+    @Test
+    public void testAucOnMultipleGroups()
+    {
+        InternalAggregationFunction function = getInternalAggregationFunction();
+
+        Block labels1 = createBooleansBlock(false, true);
+        Block scores1 = createDoublesBlock(0.5, 0.3); // sort by score: false, true
+        AggregationTestInputBuilder testInputBuilder1 = new AggregationTestInputBuilder(
+                new Block[] {labels1, scores1},
+                function);
+        AggregationTestInput test1 = testInputBuilder1.build();
+        AggregationTestOutput expectedOutput1 = new AggregationTestOutput(0.0);
+
+        GroupedAccumulator groupedAccumulator = test1.createGroupedAccumulator();
+
+        test1.runPagesOnAccumulatorWithAssertion(0L, groupedAccumulator, expectedOutput1);
+
+        Block labels2 = createBooleansBlock(false, true, true);
+        Block scores2 = createDoublesBlock(0.2, 0.8, 0.7); // sort by score: true, true, false
+        AggregationTestInputBuilder testInputBuilder2 = new AggregationTestInputBuilder(
+                new Block[] {labels2, scores2},
+                function);
+        AggregationTestInput test2 = testInputBuilder2.build();
+        AggregationTestOutput expectedOutput2 = new AggregationTestOutput(1.0);
+
+        test2.runPagesOnAccumulatorWithAssertion(255L, groupedAccumulator, expectedOutput2);
+    }
+
+    @Test
+    public void testAucSingleState()
+    {
+        assertAggregation(getInternalAggregationFunction(),
+                0.8333333333,
+                createBooleansBlock(false, true, false, true, true),
+                createDoublesBlock(0.5, 0.3, 0.2, 0.8, 0.7));
+    }
+
+    private static InternalAggregationFunction getInternalAggregationFunction()
+    {
+        return METADATA.getFunctionRegistry().getAggregateFunctionImplementation(
                 new Signature("area_under_roc_curve",
                         AGGREGATE,
                         DOUBLE.getTypeSignature(),
                         BOOLEAN.getTypeSignature(),
                         DOUBLE.getTypeSignature()));
-        assertAggregation(function, expectedValue, blocks);
     }
 }
