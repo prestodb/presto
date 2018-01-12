ac2126ba1e879437e40e0481fbf20c8ef09334ac
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2018-01-12 14:25:36 +0100

    Fix formatting compose.sh scripts

--- presto-product-tests/conf/docker/singlenode-kerberos-hdfs-impersonation/compose.sh
+++ presto-product-tests/conf/docker/singlenode-kerberos-hdfs-impersonation/compose.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 source "${BASH_SOURCE%/*}/../common/compose-commons.sh"
 
 docker-compose \
--f ${BASH_SOURCE%/*}/../common/standard.yml \
--f ${BASH_SOURCE%/*}/../common/kerberos.yml \
--f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
--f ${BASH_SOURCE%/*}/docker-compose.yml \
-"$@"
+    -f ${BASH_SOURCE%/*}/../common/standard.yml \
+    -f ${BASH_SOURCE%/*}/../common/kerberos.yml \
+    -f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
+    -f ${BASH_SOURCE%/*}/docker-compose.yml \
+    "$@"
--- presto-product-tests/conf/docker/singlenode-sqlserver/compose.sh
+++ presto-product-tests/conf/docker/singlenode-sqlserver/compose.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 source "${BASH_SOURCE%/*}/../common/compose-commons.sh"
 
 docker-compose \
--f ${BASH_SOURCE%/*}/../common/standard.yml \
--f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
--f ${BASH_SOURCE%/*}/docker-compose.yml \
-"$@"
+    -f ${BASH_SOURCE%/*}/../common/standard.yml \
+    -f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
+    -f ${BASH_SOURCE%/*}/docker-compose.yml \
+    "$@"
