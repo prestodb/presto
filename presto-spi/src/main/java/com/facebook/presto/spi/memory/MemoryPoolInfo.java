0212ece8646a5442caf94958d3a61eaac80c86e9
Min(Dongmin Yu) <miniway@gmail.com>
2018-01-19 00:17:04 +0900

    Fix MemoryPoolInfo queryMemoryRevocableReservations initialization

--- presto-spi/src/main/java/com/facebook/presto/spi/memory/MemoryPoolInfo.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/memory/MemoryPoolInfo.java
@@ -43,7 +43,7 @@ public final class MemoryPoolInfo
         this.reservedBytes = reservedBytes;
         this.reservedRevocableBytes = reservedRevocableBytes;
         this.queryMemoryReservations = unmodifiableMap(new HashMap<>(queryMemoryReservations));
-        this.queryMemoryRevocableReservations = unmodifiableMap(new HashMap<>(queryMemoryReservations));
+        this.queryMemoryRevocableReservations = unmodifiableMap(new HashMap<>(queryMemoryRevocableReservations));
     }
 
     @JsonProperty
