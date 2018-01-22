6f2c2b0b94b52405aae05f639173d8ed8cceba2f
Lukasz Osipiuk <lukasz@osipiuk.net>
2018-01-22 10:17:44 +0100

    Add clearRanges to ColumnStatistics.builder()

--- presto-spi/src/main/java/com/facebook/presto/spi/statistics/ColumnStatistics.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/statistics/ColumnStatistics.java
@@ -91,6 +91,12 @@ public final class ColumnStatistics
             return this;
         }
 
+        public Builder clearRanges()
+        {
+            rangeColumnStatistics.clear();
+            return this;
+        }
+
         public ColumnStatistics build()
         {
             return new ColumnStatistics(nullsFraction, rangeColumnStatistics);
