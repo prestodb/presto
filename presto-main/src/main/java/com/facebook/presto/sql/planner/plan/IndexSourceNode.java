3eac47dfec97401309700871d3b4a79c20090ca5
Martin Traverso <mtraverso@gmail.com>
2018-01-24 15:12:37 -0800

    Allow constraints on columns not output by IndexNode
    The code was incorrectly enforcing that the constraint be
    over columns that were mapped to the output of the index node.
    In fact, it's perfectly valid to have constraints over columns
    the index is aware of but are not output (e.g., due to pruning
    after filter pushdown).

--- presto-main/src/main/java/com/facebook/presto/sql/planner/plan/IndexSourceNode.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/plan/IndexSourceNode.java
@@ -67,11 +67,6 @@ public class IndexSourceNode
         checkArgument(!outputSymbols.isEmpty(), "outputSymbols is empty");
         checkArgument(assignments.keySet().containsAll(lookupSymbols), "Assignments do not include all lookup symbols");
         checkArgument(outputSymbols.containsAll(lookupSymbols), "Lookup symbols need to be part of the output symbols");
-        Set<ColumnHandle> assignedColumnHandles = ImmutableSet.copyOf(assignments.values());
-        currentConstraint.getDomains().ifPresent(handleToDomain ->
-                checkArgument(
-                        assignedColumnHandles.containsAll(handleToDomain.keySet()),
-                        "Tuple domain handles must have assigned symbols"));
     }
 
     @JsonProperty
