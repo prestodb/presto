cc1bd56807ccee4830ab4b8241188a482c89bd0f
Lukasz Osipiuk <lukasz@osipiuk.net>
2018-01-31 12:57:41 +0100

    Add TableScanStatsRule

---
+++ presto-main/src/main/java/com/facebook/presto/cost/StatsUtil.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.cost;
+
+import com.facebook.presto.Session;
+import com.facebook.presto.metadata.FunctionRegistry;
+import com.facebook.presto.metadata.Metadata;
+import com.facebook.presto.metadata.Signature;
+import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.type.BigintType;
+import com.facebook.presto.spi.type.BooleanType;
+import com.facebook.presto.spi.type.DateType;
+import com.facebook.presto.spi.type.DecimalType;
+import com.facebook.presto.spi.type.DoubleType;
+import com.facebook.presto.spi.type.IntegerType;
+import com.facebook.presto.spi.type.RealType;
+import com.facebook.presto.spi.type.SmallintType;
+import com.facebook.presto.spi.type.TinyintType;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.FunctionInvoker;
+
+import java.util.OptionalDouble;
+
+import static java.util.Collections.singletonList;
+
+final class StatsUtil
+{
+    private StatsUtil() {}
+
+    static OptionalDouble toStatsRepresentation(Metadata metadata, Session session, Type type, Object value)
+    {
+        return toStatsRepresentation(metadata.getFunctionRegistry(), session.toConnectorSession(), type, value);
+    }
+
+    static OptionalDouble toStatsRepresentation(FunctionRegistry functionRegistry, ConnectorSession session, Type type, Object value)
+    {
+        if (convertibleToDoubleWithCast(type)) {
+            FunctionInvoker functionInvoker = new FunctionInvoker(functionRegistry);
+            Signature castSignature = functionRegistry.getCoercion(type, DoubleType.DOUBLE);
+            return OptionalDouble.of((double) functionInvoker.invoke(castSignature, session, singletonList(value)));
+        }
+
+        if (DateType.DATE.equals(type)) {
+            return OptionalDouble.of(((Long) value).doubleValue());
+        }
+
+        return OptionalDouble.empty();
+    }
+
+    private static boolean convertibleToDoubleWithCast(Type type)
+    {
+        return type instanceof DecimalType
+                || DoubleType.DOUBLE.equals(type)
+                || RealType.REAL.equals(type)
+                || BigintType.BIGINT.equals(type)
+                || IntegerType.INTEGER.equals(type)
+                || SmallintType.SMALLINT.equals(type)
+                || TinyintType.TINYINT.equals(type)
+                || BooleanType.BOOLEAN.equals(type);
+    }
+}
