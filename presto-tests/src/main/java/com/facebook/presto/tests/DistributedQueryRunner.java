7272f95655d5b7991212fe39de357e9759e6ebf1
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-02-01 12:04:17 +0100

    Add a test that Presto works without iterative optimizer

--- presto-tests/src/main/java/com/facebook/presto/tests/DistributedQueryRunner.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/DistributedQueryRunner.java
@@ -118,12 +118,11 @@ public class DistributedQueryRunner
                 servers.add(worker);
             }
 
-            Map<String, String> extraCoordinatorProperties = ImmutableMap.<String, String>builder()
-                    .put("optimizer.optimize-mixed-distinct-aggregations", "true")
-                    .put("experimental.iterative-optimizer-enabled", "true")
-                    .putAll(extraProperties)
-                    .putAll(coordinatorProperties)
-                    .build();
+            Map<String, String> extraCoordinatorProperties = new HashMap<>();
+            extraCoordinatorProperties.put("optimizer.optimize-mixed-distinct-aggregations", "true");
+            extraCoordinatorProperties.put("experimental.iterative-optimizer-enabled", "true");
+            extraCoordinatorProperties.putAll(extraProperties);
+            extraCoordinatorProperties.putAll(coordinatorProperties);
             coordinator = closer.register(createTestingPrestoServer(discoveryServer.getBaseUrl(), true, extraCoordinatorProperties, parserOptions, environment));
             servers.add(coordinator);
 
