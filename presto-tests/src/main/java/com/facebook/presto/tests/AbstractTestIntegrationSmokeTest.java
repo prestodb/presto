e27c1bb4b5af251e4f95d9f652bc7ba60587604c
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-02-06 16:46:34 +0100

    Add more information_schema tests

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestIntegrationSmokeTest.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestIntegrationSmokeTest.java
@@ -15,6 +15,7 @@ package com.facebook.presto.tests;
 
 import com.facebook.presto.testing.MaterializedResult;
 import com.google.common.collect.ImmutableList;
+import org.intellij.lang.annotations.Language;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -123,43 +124,50 @@ public abstract class AbstractTestIntegrationSmokeTest
     }
 
     @Test
-    public void testInformationSchemaTablesWithoutEqualityConstraint()
+    public void testSelectInformationSchemaTables()
     {
         String catalog = getSession().getCatalog().get();
         String schema = getSession().getSchema().get();
+        String schemaPattern = schema.replaceAll("^.", "_");
 
+        assertQuery("SELECT table_name FROM information_schema.tables WHERE table_schema = '" + schema + "' AND table_name = 'orders'", "VALUES 'orders'");
+        assertQuery("SELECT table_name FROM information_schema.tables WHERE table_schema LIKE '" + schema + "' AND table_name LIKE '%rders'", "VALUES 'orders'");
+        assertQuery("SELECT table_name FROM information_schema.tables WHERE table_schema LIKE '" + schemaPattern + "' AND table_name LIKE '%rders'", "VALUES 'orders'");
         assertQuery(
                 "SELECT table_name FROM information_schema.tables " +
                         "WHERE table_catalog = '" + catalog + "' AND table_schema LIKE '" + schema + "' AND table_name LIKE '%orders'",
                 "VALUES 'orders'");
+        assertQuery("SELECT table_name FROM information_schema.tables WHERE table_catalog = 'something_else'", "SELECT '' WHERE false");
     }
 
     @Test
     public void testSelectInformationSchemaColumns()
     {
+        String catalog = getSession().getCatalog().get();
         String schema = getSession().getSchema().get();
+        String schemaPattern = schema.replaceAll(".$", "_");
+
+        @Language("SQL") String ordersTableWithColumns = "VALUES " +
+                "('orders', 'orderkey'), " +
+                "('orders', 'custkey'), " +
+                "('orders', 'orderstatus'), " +
+                "('orders', 'totalprice'), " +
+                "('orders', 'orderdate'), " +
+                "('orders', 'orderpriority'), " +
+                "('orders', 'clerk'), " +
+                "('orders', 'shippriority'), " +
+                "('orders', 'comment')";
 
         assertQuery("SELECT table_schema FROM information_schema.columns WHERE table_schema = '" + schema + "' GROUP BY table_schema", "VALUES '" + schema + "'");
         assertQuery("SELECT table_name FROM information_schema.columns WHERE table_name = 'orders' GROUP BY table_name", "VALUES 'orders'");
-    }
-
-    @Test
-    public void testInformationSchemaColumnsWithoutEqualityConstraint()
-    {
-        String catalog = getSession().getCatalog().get();
-        String schema = getSession().getSchema().get();
+        assertQuery("SELECT table_name, column_name FROM information_schema.columns WHERE table_schema = '" + schema + "' AND table_name = 'orders'", ordersTableWithColumns);
+        assertQuery("SELECT table_name, column_name FROM information_schema.columns WHERE table_schema = '" + schema + "' AND table_name LIKE '%rders'", ordersTableWithColumns);
+        assertQuery("SELECT table_name, column_name FROM information_schema.columns WHERE table_schema LIKE '" + schemaPattern + "' AND table_name LIKE '_rder_'", ordersTableWithColumns);
         assertQuery(
                 "SELECT table_name, column_name FROM information_schema.columns " +
                         "WHERE table_catalog = '" + catalog + "' AND table_schema = '" + schema + "' AND table_name LIKE '%orders%'",
-                "VALUES ('orders', 'orderkey'), " +
-                        "('orders', 'custkey'), " +
-                        "('orders', 'orderstatus'), " +
-                        "('orders', 'totalprice'), " +
-                        "('orders', 'orderdate'), " +
-                        "('orders', 'orderpriority'), " +
-                        "('orders', 'clerk'), " +
-                        "('orders', 'shippriority'), " +
-                        "('orders', 'comment')");
+                ordersTableWithColumns);
+        assertQuery("SELECT column_name FROM information_schema.columns WHERE table_catalog = 'something_else'", "SELECT '' WHERE false");
     }
 
     @Test
