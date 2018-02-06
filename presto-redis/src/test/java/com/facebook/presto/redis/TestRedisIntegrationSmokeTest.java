f2cc8cf2508bbd41090374ae1855b7adff88a284
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-02-06 15:24:13 +0100

    Fix column listing in Redis when schema is given but table name is not

--- presto-redis/src/test/java/com/facebook/presto/redis/TestRedisIntegrationSmokeTest.java
+++ presto-redis/src/test/java/com/facebook/presto/redis/TestRedisIntegrationSmokeTest.java
@@ -15,7 +15,6 @@ package com.facebook.presto.redis;
 
 import com.facebook.presto.redis.util.EmbeddedRedis;
 import com.facebook.presto.tests.AbstractTestIntegrationSmokeTest;
-import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
@@ -45,11 +44,5 @@ public class TestRedisIntegrationSmokeTest
     public void destroy()
     {
         embeddedRedis.close();
-    }
-
-    @Override
-    public void testInformationSchemaColumnsWithoutEqualityConstraint()
-    {
-        throw new SkipException("this fails for Kafka"); // TODO
     }
 }
