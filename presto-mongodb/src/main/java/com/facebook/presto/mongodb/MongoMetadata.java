3ab5914cfea839505c82eee5efae160447517fc5
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-02-06 16:32:24 +0100

    Move helper method to more reasonable place

--- presto-mongodb/src/main/java/com/facebook/presto/mongodb/MongoMetadata.java
+++ presto-mongodb/src/main/java/com/facebook/presto/mongodb/MongoMetadata.java
@@ -135,6 +135,14 @@ public class MongoMetadata
         return columns.build();
     }
 
+    private List<SchemaTableName> listTables(ConnectorSession session, SchemaTablePrefix prefix)
+    {
+        if (prefix.getTableName() == null) {
+            return listTables(session, prefix.getSchemaName());
+        }
+        return ImmutableList.of(new SchemaTableName(prefix.getSchemaName(), prefix.getTableName()));
+    }
+
     @Override
     public ColumnMetadata getColumnMetadata(ConnectorSession session, ConnectorTableHandle tableHandle, ColumnHandle columnHandle)
     {
@@ -283,14 +291,6 @@ public class MongoMetadata
             return listSchemaNames(session);
         }
         return ImmutableList.of(schemaNameOrNull);
-    }
-
-    private List<SchemaTableName> listTables(ConnectorSession session, SchemaTablePrefix prefix)
-    {
-        if (prefix.getTableName() == null) {
-            return listTables(session, prefix.getSchemaName());
-        }
-        return ImmutableList.of(new SchemaTableName(prefix.getSchemaName(), prefix.getTableName()));
     }
 
     private static List<MongoColumnHandle> buildColumnHandles(ConnectorTableMetadata tableMetadata)
