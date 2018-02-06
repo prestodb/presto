e402e6badeea83c747ced0db29512cbe2a0c0402
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-02-06 13:41:16 +0100

    Simplify information_schema test queries
    The `expected` query is run on H2 where object names are upper-cased. It
    makes reading tests harder. This commit avoids that by using `VALUES` as
    `expected`.

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
@@ -132,33 +132,27 @@ public class TestHiveIntegrationSmokeTest
     @Test
     public void testInformationSchemaTablesWithoutEqualityConstraint()
     {
-        @Language("SQL") String actual = "" +
-                "SELECT lower(table_name) " +
-                "FROM information_schema.tables " +
-                "WHERE table_catalog = '" + catalog + "' AND table_schema LIKE 'tpch' AND table_name LIKE '%orders'";
-
-        @Language("SQL") String expected = "" +
-                "SELECT lower(table_name) " +
-                "FROM information_schema.tables " +
-                "WHERE table_name LIKE '%ORDERS'";
-
-        assertQuery(actual, expected);
+        assertQuery(
+                "SELECT table_name FROM information_schema.tables " +
+                        "WHERE table_catalog = '" + catalog + "' AND table_schema LIKE 'tpch' AND table_name LIKE '%orders'",
+                "VALUES 'orders'");
     }
 
     @Test
     public void testInformationSchemaColumnsWithoutEqualityConstraint()
     {
-        @Language("SQL") String actual = "" +
-                "SELECT lower(table_name), lower(column_name) " +
-                "FROM information_schema.columns " +
-                "WHERE table_catalog = '" + catalog + "' AND table_schema = 'tpch' AND table_name LIKE '%orders%'";
-
-        @Language("SQL") String expected = "" +
-                "SELECT lower(table_name), lower(column_name) " +
-                "FROM information_schema.columns " +
-                "WHERE table_name LIKE '%ORDERS%'";
-
-        assertQuery(actual, expected);
+        assertQuery(
+                "SELECT table_name, column_name FROM information_schema.columns " +
+                        "WHERE table_catalog = '" + catalog + "' AND table_schema = 'tpch' AND table_name LIKE '%orders%'",
+                "VALUES ('orders', 'orderkey'), " +
+                        "('orders', 'custkey'), " +
+                        "('orders', 'orderstatus'), " +
+                        "('orders', 'totalprice'), " +
+                        "('orders', 'orderdate'), " +
+                        "('orders', 'orderpriority'), " +
+                        "('orders', 'clerk'), " +
+                        "('orders', 'shippriority'), " +
+                        "('orders', 'comment')");
     }
 
     @Test
