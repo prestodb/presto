3a16939c58a6686dd8eea960f794a8fa01924e0b
Anu Sudarsan <anuatinfy@gmail.com>
2018-02-08 16:50:09 -0500

    Add basic Avro decoder for Kafka

--- presto-kafka/src/main/java/com/facebook/presto/kafka/KafkaSplitManager.java
+++ presto-kafka/src/main/java/com/facebook/presto/kafka/KafkaSplitManager.java
@@ -25,6 +25,7 @@ import com.facebook.presto.spi.connector.ConnectorTransactionHandle;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.io.CharStreams;
 import io.airlift.log.Logger;
 import kafka.api.PartitionOffsetRequestInfo;
 import kafka.cluster.Broker;
@@ -39,6 +40,13 @@ import kafka.javaapi.consumer.SimpleConsumer;
 
 import javax.inject.Inject;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
@@ -47,6 +55,8 @@ import static com.facebook.presto.kafka.KafkaErrorCode.KAFKA_SPLIT_ERROR;
 import static com.facebook.presto.kafka.KafkaHandleResolver.convertLayout;
 import static com.facebook.presto.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
 import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Locale.ENGLISH;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -108,8 +118,8 @@ public class KafkaSplitManager
                                 metadata.topic(),
                                 kafkaTableHandle.getKeyDataFormat(),
                                 kafkaTableHandle.getMessageDataFormat(),
-                                kafkaTableHandle.getKeyDataSchemaLocation(),
-                                kafkaTableHandle.getMessageDataSchemaLocation(),
+                                kafkaTableHandle.getKeyDataSchemaLocation().map(KafkaSplitManager::readSchema),
+                                kafkaTableHandle.getMessageDataSchemaLocation().map(KafkaSplitManager::readSchema),
                                 part.partitionId(),
                                 offsets[i],
                                 offsets[i - 1],
@@ -127,6 +137,54 @@ public class KafkaSplitManager
             }
             throw new PrestoException(KAFKA_SPLIT_ERROR, format("Cannot list splits for table '%s' reading topic '%s'", kafkaTableHandle.getTableName(), kafkaTableHandle.getTopicName()), e);
         }
+    }
+
+    private static String readSchema(String dataSchemaLocation)
+    {
+        InputStream inputStream = null;
+        try {
+            if (isURI(dataSchemaLocation.trim().toLowerCase(ENGLISH))) {
+                try {
+                    inputStream = new URL(dataSchemaLocation).openStream();
+                }
+                catch (MalformedURLException e) {
+                    // try again before failing
+                    inputStream = new FileInputStream(dataSchemaLocation);
+                }
+            }
+            else {
+                inputStream = new FileInputStream(dataSchemaLocation);
+            }
+            return CharStreams.toString(new InputStreamReader(inputStream, UTF_8));
+        }
+        catch (IOException e) {
+            throw new PrestoException(GENERIC_INTERNAL_ERROR, "Could not parse the AVRO schema at: " + dataSchemaLocation, e);
+        }
+        finally {
+            closeQuietly(inputStream);
+        }
+    }
+
+    private static void closeQuietly(InputStream stream)
+    {
+        try {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        catch (IOException ignored) {
+        }
+    }
+
+    private static boolean isURI(String location)
+    {
+        try {
+            URI.create(location);
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 
     private static long[] findAllOffsets(SimpleConsumer consumer, String topicName, int partitionId)
