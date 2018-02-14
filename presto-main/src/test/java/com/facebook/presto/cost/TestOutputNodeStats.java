6af79a39bc920be65f42cf0fa4e8e4f678044128
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-02-14 12:09:06 +0100

    Remove redundant throws declaration

--- presto-main/src/test/java/com/facebook/presto/cost/TestOutputNodeStats.java
+++ presto-main/src/test/java/com/facebook/presto/cost/TestOutputNodeStats.java
@@ -26,7 +26,6 @@ public class TestOutputNodeStats
 {
     @Test
     public void testStatsForOutputNode()
-            throws Exception
     {
         PlanNodeStatsEstimate stats = PlanNodeStatsEstimate.builder()
                 .setOutputRowCount(100)
