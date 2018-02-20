d1b3902e27fe88483fd2db9692e54952510eca0f
Nezih Yigitbasi <nezihy@fb.com>
2018-02-20 12:10:33 -0800

    Fix query peak user/total memory tracking
    Previously, the query peak user/total memory was incorrectly calculated
    as the peak of task user/total memory.

--- presto-main/src/main/java/com/facebook/presto/execution/MemoryTrackingRemoteTaskFactory.java
+++ presto-main/src/main/java/com/facebook/presto/execution/MemoryTrackingRemoteTaskFactory.java
@@ -64,6 +64,8 @@ public class MemoryTrackingRemoteTaskFactory
             implements StateChangeListener<TaskStatus>
     {
         private final QueryStateMachine stateMachine;
+        private long previousUserMemory;
+        private long previousSystemMemory;
 
         public UpdatePeakMemory(QueryStateMachine stateMachine)
         {
@@ -75,7 +77,11 @@ public class MemoryTrackingRemoteTaskFactory
         {
             long currentUserMemory = newStatus.getMemoryReservation().toBytes();
             long currentSystemMemory = newStatus.getSystemMemoryReservation().toBytes();
-            stateMachine.updateMemoryUsage(currentUserMemory, currentSystemMemory);
+            long deltaUserMemoryInBytes = currentUserMemory - previousUserMemory;
+            long deltaTotalMemoryInBytes = (currentUserMemory + currentSystemMemory) - (previousUserMemory + previousSystemMemory);
+            previousUserMemory = currentUserMemory;
+            previousSystemMemory = currentSystemMemory;
+            stateMachine.updateMemoryUsage(deltaUserMemoryInBytes, deltaTotalMemoryInBytes);
         }
     }
 }
