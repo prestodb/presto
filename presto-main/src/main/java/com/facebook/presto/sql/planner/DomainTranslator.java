b2b5cb0d4650e445bdc5856ea97975b9b8856498
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2018-03-02 12:34:54 +0100

    Fix DomainTranslator for InPredicate with casts and nulls

--- presto-main/src/main/java/com/facebook/presto/sql/planner/DomainTranslator.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/DomainTranslator.java
@@ -432,13 +432,11 @@ public final class DomainTranslator
                     return super.visitComparisonExpression(node, complement);
                 }
 
-                NullableValue value = normalized.getValue();
-                Type valueType = value.getType(); // type of value
                 Type castSourceType = typeOf(castExpression.getExpression(), session, metadata, types); // type of expression which is then cast to type of value
 
                 // we use saturated floor cast value -> castSourceType to rewrite original expression to new one with one cast peeled off the symbol side
                 Optional<Expression> coercedExpression = coerceComparisonWithRounding(
-                        castSourceType, castExpression.getExpression(), valueType, value.getValue(), normalized.getComparisonType());
+                        castSourceType, castExpression.getExpression(), normalized.getValue(), normalized.getComparisonType());
 
                 if (coercedExpression.isPresent()) {
                     return process(coercedExpression.get(), complement);
@@ -584,11 +582,15 @@ public final class DomainTranslator
         private Optional<Expression> coerceComparisonWithRounding(
                 Type symbolExpressionType,
                 Expression symbolExpression,
-                Type valueType,
-                Object value,
+                NullableValue nullableValue,
                 ComparisonExpressionType comparisonType)
         {
-            requireNonNull(value, "value is null");
+            requireNonNull(nullableValue, "nullableValue is null");
+            if (nullableValue.isNull()) {
+                return Optional.empty();
+            }
+            Type valueType = nullableValue.getType();
+            Object value = nullableValue.getValue();
             return floorValue(valueType, symbolExpressionType, value)
                     .map((floorValue) -> rewriteComparisonExpression(symbolExpressionType, symbolExpression, valueType, value, floorValue, comparisonType));
         }
