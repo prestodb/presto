4e79a119993575a1829032325deeee0f8c3f18ab
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2018-03-02 13:57:22 +0100

    Remove unused methods

--- presto-main/src/main/java/com/facebook/presto/sql/planner/SubExpressionExtractor.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/SubExpressionExtractor.java
@@ -32,22 +32,6 @@ public final class SubExpressionExtractor
 {
     private SubExpressionExtractor() {}
 
-    public static List<Expression> extractAll(Iterable<Expression> expressions)
-    {
-        ImmutableList.Builder<Expression> builder = ImmutableList.builder();
-        for (Expression expression : expressions) {
-            extract(builder, expression);
-        }
-        return builder.build();
-    }
-
-    public static List<Expression> extractAll(Expression expression)
-    {
-        ImmutableList.Builder<Expression> builder = ImmutableList.builder();
-        extract(builder, expression);
-        return builder.build();
-    }
-
     public static Set<Expression> extract(Expression expression)
     {
         final ImmutableSet.Builder<Expression> builder = ImmutableSet.builder();
