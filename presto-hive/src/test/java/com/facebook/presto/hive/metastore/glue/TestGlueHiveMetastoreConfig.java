84be87ec8b76a97d617f5d72094dc9aaca8621e8
Rentao Wu <rewu@amazon.com>
2018-03-13 17:00:19 -0700

    Add support for Glue Hive metastore
    Allows users to use AWS Glue Data Catalog as metastore in Hive connector

---
+++ presto-hive/src/test/java/com/facebook/presto/hive/metastore/glue/TestGlueHiveMetastoreConfig.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.hive.metastore.glue;
+
+import com.google.common.collect.ImmutableMap;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static io.airlift.configuration.testing.ConfigAssertions.assertFullMapping;
+import static io.airlift.configuration.testing.ConfigAssertions.assertRecordedDefaults;
+import static io.airlift.configuration.testing.ConfigAssertions.recordDefaults;
+
+public class TestGlueHiveMetastoreConfig
+{
+    @Test
+    public void testDefaults()
+    {
+        assertRecordedDefaults(recordDefaults(GlueHiveMetastoreConfig.class)
+                .setGlueRegion(null)
+                .setPinGlueClientToCurrentRegion(false)
+                .setMaxGlueConnections(5)
+                .setDefaultWarehouseDir(null));
+    }
+
+    @Test
+    public void testExplicitPropertyMapping()
+    {
+        Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("hive.metastore.glue.region", "us-east-1")
+                .put("hive.metastore.glue.pin-client-to-current-region", "true")
+                .put("hive.metastore.glue.max-connections", "10")
+                .put("hive.metastore.glue.default-warehouse-dir", "/location")
+                .build();
+
+        GlueHiveMetastoreConfig expected = new GlueHiveMetastoreConfig()
+                .setGlueRegion("us-east-1")
+                .setPinGlueClientToCurrentRegion(true)
+                .setMaxGlueConnections(10)
+                .setDefaultWarehouseDir("/location");
+
+        assertFullMapping(properties, expected);
+    }
+}
