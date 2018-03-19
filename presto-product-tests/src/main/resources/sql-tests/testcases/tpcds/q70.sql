52a41e57bb183cd9ce6b89b931862f01f6f4206e
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-03-19 16:34:51 +0100

    Remove redundant semicolons from product tests' TPC-DS queries

--- presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q70.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q70.sql
@@ -33,4 +33,3 @@ WHERE ("d1"."d_month_seq" BETWEEN 1200 AND (1200 + 11))
 GROUP BY ROLLUP (s_state, s_county)
 ORDER BY "lochierarchy" DESC, (CASE WHEN ("lochierarchy" = 0) THEN "s_state" END) ASC, "rank_within_parent" ASC
 LIMIT 100
-;
