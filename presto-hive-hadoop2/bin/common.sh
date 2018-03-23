5312bf806c7435545f67b91edd8768714d8c5c75
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-03-23 11:57:27 +0100

    Extract common hive tests container methods to separate file

--- presto-hive-hadoop2/bin/common.sh
+++ presto-hive-hadoop2/bin/common.sh
@@ -2,11 +2,6 @@
 
 set -euo pipefail -x
 
-# http://stackoverflow.com/questions/3572030/bash-script-absolute-path-with-osx
-function absolutepath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
-
 function retry() {
   END=$(($(date +%s) + 600))
 
@@ -76,32 +71,7 @@ DOCKER_COMPOSE_LOCATION="${INTEGRATION_TESTS_ROOT}/conf/docker-compose.yml"
 docker-compose version
 docker version
 
-# stop already running containers
-docker-compose -f "${DOCKER_COMPOSE_LOCATION}" down || true
-
-# catch terminate signals
-trap termination_handler INT TERM
-
-# pull docker images
-if [[ "${CONTINUOUS_INTEGRATION:-false}" == 'true' ]]; then
-    docker-compose -f "${DOCKER_COMPOSE_LOCATION}" pull
-fi
-
-# start containers
-docker-compose -f "${DOCKER_COMPOSE_LOCATION}" up -d
-
-# start docker logs for hadoop container
-docker-compose -f "${DOCKER_COMPOSE_LOCATION}" logs --no-color hadoop-master &
-
-# wait until hadoop processes is started
-retry check_hadoop
-
-# generate test data
-exec_in_hadoop_master_container su hive -s /usr/bin/hive -f /files/sql/create-test.sql
-exec_in_hadoop_master_container su hive -s /usr/bin/hive -f /files/sql/create-test-hive13.sql
-
-stop_unnecessary_hadoop_services
-
+# extract proxy IP
 if [ -n "${DOCKER_MACHINE_NAME:-}" ]
 then
   PROXY=`docker-machine ip`
@@ -109,27 +79,24 @@ else
   PROXY=127.0.0.1
 fi
 
-# run product tests
-pushd $PROJECT_ROOT
-set +e
-./mvnw -pl presto-hive-hadoop2 test -P test-hive-hadoop2 \
-  -Dhive.hadoop2.timeZone=UTC \
-  -DHADOOP_USER_NAME=hive \
-  -Dhive.hadoop2.metastoreHost=hadoop-master \
-  -Dhive.hadoop2.timeZone=Asia/Kathmandu \
-  -Dhive.metastore.thrift.client.socks-proxy=$PROXY:1180 \
-  -Dsun.net.spi.nameservice.provider.1=default \
-  -Dsun.net.spi.nameservice.provider.2=dns,dnsjava \
-  -Ddns.server=$PROXY \
-  -Ddns.port=55353 \
-  -Ddns.search=. &
-PRODUCT_TESTS_PROCESS_ID=$!
+function start_docker_containers() {
+  # stop already running containers
+  docker-compose -f "${DOCKER_COMPOSE_LOCATION}" down || true
 
-wait ${PRODUCT_TESTS_PROCESS_ID}
-EXIT_CODE=$?
-set -e
-popd
+  # catch terminate signals
+  trap termination_handler INT TERM
 
-cleanup_docker_containers
+  # pull docker images
+  if [[ "${CONTINUOUS_INTEGRATION:-false}" == 'true' ]]; then
+    docker-compose -f "${DOCKER_COMPOSE_LOCATION}" pull
+  fi
 
-exit ${EXIT_CODE}
+  # start containers
+  docker-compose -f "${DOCKER_COMPOSE_LOCATION}" up -d
+
+  # start docker logs for hadoop container
+  docker-compose -f "${DOCKER_COMPOSE_LOCATION}" logs --no-color hadoop-master &
+
+  # wait until hadoop processes is started
+  retry check_hadoop
+}
