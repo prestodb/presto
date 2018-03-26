e907dbe391122f632a974c54bb0ff79d04c1bd2d
Łukasz Osipiuk <lukasz@osipiuk.net>
2018-03-26 15:27:42 +0200

    Fix NATION_PARTITIONED_BY_REGIONKEY data files

--- presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestHiveTableStatistics.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestHiveTableStatistics.java
@@ -282,14 +282,14 @@ public class TestHiveTableStatistics
                 row("p_nationkey", null, 5.0, 0.0, null, "1", "24"),
                 row("p_name", null, 6.0, 0.0, null, null, null),
                 row("p_regionkey", null, 3.0, 0.0, null, "1", "3"),
-                row("p_comment", null, 1.0, 0.0, null, null, null),
+                row("p_comment", null, 7.0, 0.0, null, null, null),
                 row(null, null, null, null, 15.0, null, null));
 
         assertThat(query(showStatsPartitionOne)).containsOnly(
                 row("p_nationkey", null, 5.0, 0.0, null, "1", "24"),
                 row("p_name", null, 6.0, 0.0, null, null, null),
                 row("p_regionkey", null, 1.0, 0.0, null, "1", "1"),
-                row("p_comment", null, 1.0, 0.0, null, null, null),
+                row("p_comment", null, 7.0, 0.0, null, null, null),
                 row(null, null, null, null, 5.0, null, null));
 
         assertThat(query(showStatsPartitionTwo)).containsOnly(
@@ -307,21 +307,21 @@ public class TestHiveTableStatistics
                 row("p_nationkey", null, 5.0, 0.0, null, "1", "24"),
                 row("p_name", null, 6.0, 0.0, null, null, null),
                 row("p_regionkey", null, 3.0, 0.0, null, "1", "3"),
-                row("p_comment", null, 1.0, 0.0, null, null, null),
+                row("p_comment", null, 7.0, 0.0, null, null, null),
                 row(null, null, null, null, 15.0, null, null));
 
         assertThat(query(showStatsPartitionOne)).containsOnly(
                 row("p_nationkey", null, 5.0, 0.0, null, "1", "24"),
                 row("p_name", null, 6.0, 0.0, null, null, null),
                 row("p_regionkey", null, 1.0, 0.0, null, "1", "1"),
-                row("p_comment", null, 1.0, 0.0, null, null, null),
+                row("p_comment", null, 7.0, 0.0, null, null, null),
                 row(null, null, null, null, 5.0, null, null));
 
         assertThat(query(showStatsPartitionTwo)).containsOnly(
                 row("p_nationkey", null, 4.0, 0.0, null, "8", "21"),
                 row("p_name", null, 6.0, 0.0, null, null, null),
                 row("p_regionkey", null, 1.0, 0.0, null, "2", "2"),
-                row("p_comment", null, 1.0, 0.0, null, null, null),
+                row("p_comment", null, 5.0, 0.0, null, null, null),
                 row(null, null, null, null, 5.0, null, null));
     }
 
