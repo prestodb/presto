46617f38a383d28abed88042336a07f9d1db7c70
Dain Sundstrom <dain@iq80.com>
2018-03-28 14:10:07 -0700

    Share validation code between MapBlock factories

--- presto-spi/src/main/java/com/facebook/presto/spi/block/MapBlock.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/block/MapBlock.java
@@ -54,17 +54,10 @@ public class MapBlock
             MethodHandle keyNativeHashCode,
             MethodHandle keyBlockHashCode)
     {
-        if (keyBlock.getPositionCount() != valueBlock.getPositionCount()) {
-            throw new IllegalArgumentException(format("keyBlock position count does not match valueBlock position count. %s %s", keyBlock.getPositionCount(), valueBlock.getPositionCount()));
-        }
-        int elementCount = keyBlock.getPositionCount();
-        if (mapIsNull.length != offsets.length - 1) {
-            throw new IllegalArgumentException(format("mapIsNull.length-1 does not match offsets.length. %s %s", mapIsNull.length - 1, offsets.length));
-        }
+        validateConstructorArguments(0, mapIsNull.length, mapIsNull, offsets, keyBlock, valueBlock, mapType.getKeyType(), keyBlockNativeEquals, keyNativeHashCode);
+
         int mapCount = mapIsNull.length;
-        if (offsets[mapCount] != elementCount) {
-            throw new IllegalArgumentException(format("Last element of offsets does not match keyBlock position count. %s %s", offsets[mapCount], keyBlock.getPositionCount()));
-        }
+        int elementCount = keyBlock.getPositionCount();
         int[] hashTables = new int[elementCount * HASH_MULTIPLIER];
         Arrays.fill(hashTables, -1);
         for (int i = 0; i < mapCount; i++) {
@@ -92,6 +85,9 @@ public class MapBlock
                 keyNativeHashCode);
     }
 
+    /**
+     * Create a map block directly without per element validations.
+     */
     static MapBlock createMapBlockInternal(
             int startOffset,
             int positionCount,
@@ -104,9 +100,54 @@ public class MapBlock
             MethodHandle keyBlockNativeEquals,
             MethodHandle keyNativeHashCode)
     {
+        validateConstructorArguments(startOffset, positionCount, mapIsNull, offsets, keyBlock, valueBlock, keyType, keyBlockNativeEquals, keyNativeHashCode);
         return new MapBlock(startOffset, positionCount, mapIsNull, offsets, keyBlock, valueBlock, hashTables, keyType, keyBlockNativeEquals, keyNativeHashCode);
     }
 
+    private static void validateConstructorArguments(
+            int startOffset,
+            int positionCount,
+            boolean[] mapIsNull,
+            int[] offsets,
+            Block keyBlock,
+            Block valueBlock,
+            Type keyType,
+            MethodHandle keyBlockNativeEquals,
+            MethodHandle keyNativeHashCode)
+    {
+        if (startOffset < 0) {
+            throw new IllegalArgumentException("startOffset is negative");
+        }
+
+        if (positionCount < 0) {
+            throw new IllegalArgumentException("positionCount is negative");
+        }
+
+        requireNonNull(mapIsNull, "mapIsNull is null");
+        if (mapIsNull.length - startOffset < positionCount) {
+            throw new IllegalArgumentException("isNull length is less than positionCount");
+        }
+
+        requireNonNull(offsets, "offsets is null");
+        if (offsets.length - startOffset < positionCount + 1) {
+            throw new IllegalArgumentException("offsets length is less than positionCount");
+        }
+
+        requireNonNull(keyBlock, "keyBlock is null");
+        requireNonNull(valueBlock, "valueBlock is null");
+        if (keyBlock.getPositionCount() != valueBlock.getPositionCount()) {
+            throw new IllegalArgumentException(format("keyBlock and valueBlock has different size: %s %s", keyBlock.getPositionCount(), valueBlock.getPositionCount()));
+        }
+
+        requireNonNull(keyType, "keyType is null");
+        requireNonNull(keyBlockNativeEquals, "keyBlockNativeEquals is null");
+        requireNonNull(keyNativeHashCode, "keyNativeHashCode is null");
+    }
+
+    /**
+     * Use createRowBlockInternal or fromKeyValueBlock instead of this method.  The caller of this method is assumed to have
+     * validated the arguments with validateConstructorArguments.
+     */
     private MapBlock(
             int startOffset,
             int positionCount,
@@ -121,18 +162,17 @@ public class MapBlock
     {
         super(keyType, keyNativeHashCode, keyBlockNativeEquals);
 
-        this.startOffset = startOffset;
-        this.positionCount = positionCount;
-        this.mapIsNull = mapIsNull;
-        this.offsets = requireNonNull(offsets, "offsets is null");
-        this.keyBlock = requireNonNull(keyBlock, "keyBlock is null");
-        this.valueBlock = requireNonNull(valueBlock, "valueBlock is null");
-        if (keyBlock.getPositionCount() != valueBlock.getPositionCount()) {
-            throw new IllegalArgumentException(format("keyBlock and valueBlock has different size: %s %s", keyBlock.getPositionCount(), valueBlock.getPositionCount()));
-        }
+        requireNonNull(hashTables, "hashTables is null");
         if (hashTables.length < keyBlock.getPositionCount() * HASH_MULTIPLIER) {
             throw new IllegalArgumentException(format("keyBlock/valueBlock size does not match hash table size: %s %s", keyBlock.getPositionCount(), hashTables.length));
         }
+
+        this.startOffset = startOffset;
+        this.positionCount = positionCount;
+        this.mapIsNull = mapIsNull;
+        this.offsets = offsets;
+        this.keyBlock = keyBlock;
+        this.valueBlock = valueBlock;
         this.hashTables = hashTables;
 
         this.sizeInBytes = -1;
