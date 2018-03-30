28ff85e84fbe193038f9bc72d95f4a343f0ce5e7
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-03-30 12:40:38 +0200

    Add basic TransformCorrelatedLateralJoinToJoin

---
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/TransformCorrelatedLateralJoinToJoin.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.planner.iterative.rule;
+
+import com.facebook.presto.matching.Captures;
+import com.facebook.presto.matching.Pattern;
+import com.facebook.presto.sql.planner.iterative.Rule;
+import com.facebook.presto.sql.planner.optimizations.PlanNodeDecorrelator;
+import com.facebook.presto.sql.planner.optimizations.PlanNodeDecorrelator.DecorrelatedNode;
+import com.facebook.presto.sql.planner.plan.JoinNode;
+import com.facebook.presto.sql.planner.plan.LateralJoinNode;
+import com.facebook.presto.sql.planner.plan.PlanNode;
+import com.google.common.collect.ImmutableList;
+
+import java.util.Optional;
+
+import static com.facebook.presto.matching.Pattern.nonEmpty;
+import static com.facebook.presto.sql.planner.plan.Patterns.LateralJoin.correlation;
+import static com.facebook.presto.sql.planner.plan.Patterns.lateralJoin;
+
+/**
+ * Tries to decorrelate subquery and rewrite it using normal join.
+ * Decorrelated predicates are part of join condition.
+ */
+public class TransformCorrelatedLateralJoinToJoin
+        implements Rule<LateralJoinNode>
+{
+    private static final Pattern<LateralJoinNode> PATTERN = lateralJoin()
+            .with(nonEmpty(correlation()));
+
+    @Override
+    public Pattern<LateralJoinNode> getPattern()
+    {
+        return PATTERN;
+    }
+
+    @Override
+    public Result apply(LateralJoinNode lateralJoinNode, Captures captures, Context context)
+    {
+        PlanNode subquery = lateralJoinNode.getSubquery();
+
+        PlanNodeDecorrelator planNodeDecorrelator = new PlanNodeDecorrelator(context.getIdAllocator(), context.getLookup());
+        Optional<DecorrelatedNode> decorrelatedNodeOptional = planNodeDecorrelator.decorrelateFilters(subquery, lateralJoinNode.getCorrelation());
+
+        return decorrelatedNodeOptional.map(decorrelatedNode ->
+                Result.ofPlanNode(new JoinNode(
+                        context.getIdAllocator().getNextId(),
+                        lateralJoinNode.getType().toJoinNodeType(),
+                        lateralJoinNode.getInput(),
+                        decorrelatedNode.getNode(),
+                        ImmutableList.of(),
+                        lateralJoinNode.getOutputSymbols(),
+                        decorrelatedNode.getCorrelatedPredicates(),
+                        Optional.empty(),
+                        Optional.empty(),
+                        Optional.empty()))).orElseGet(Result::empty);
+    }
+}
