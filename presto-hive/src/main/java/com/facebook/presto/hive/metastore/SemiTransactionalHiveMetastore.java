3e24393b328a139c2cd4121c2e0f6e92c8ca425a
Wenlei Xie <wenlei.xie@gmail.com>
2018-05-03 23:55:57 -0700

    Refactor Hive write path, target path and write mode decision
    Historically, we use the equality of `writePath` and `targetPath`
    and whether `writePath` exists to decide the write mode.
    This is difficult to understand. Thus the `WriteMode` enum was
    introduced in 126fac0194d43028e9e12d079507ab801519a022.
    However, it is only used in `SemiTransactionalMetastore`.
    
    Also, `tagetPath` and `writePath` have to be fetched separately
    from `LocationService`. And there are root path (query level)
    and non-root path (partition/table level). These APIs are confusing.
    
    This commit did the following refactors:
    - Store the `WriteMode` in `LocationHandle`.
    - returns a `WriteInfo` contains `targetPath`, `writePath` and the `writeMode`.

--- presto-hive/src/main/java/com/facebook/presto/hive/metastore/SemiTransactionalHiveMetastore.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/metastore/SemiTransactionalHiveMetastore.java
@@ -18,6 +18,7 @@ import com.facebook.presto.hive.HdfsEnvironment;
 import com.facebook.presto.hive.HdfsEnvironment.HdfsContext;
 import com.facebook.presto.hive.HiveBasicStatistics;
 import com.facebook.presto.hive.HiveType;
+import com.facebook.presto.hive.LocationHandle.WriteMode;
 import com.facebook.presto.hive.PartitionNotFoundException;
 import com.facebook.presto.hive.TableAlreadyExistsException;
 import com.facebook.presto.spi.ConnectorSession;
@@ -1745,24 +1746,6 @@ public class SemiTransactionalHiveMetastore
         SHARED_OPERATION_BUFFERED,
         EXCLUSIVE_OPERATION_BUFFERED,
         FINISHED,
-    }
-
-    public enum WriteMode
-    {
-        STAGE_AND_MOVE_TO_TARGET_DIRECTORY, // common mode for new table or existing table (both new and existing partition)
-        DIRECT_TO_TARGET_NEW_DIRECTORY, // for new table in S3
-        DIRECT_TO_TARGET_EXISTING_DIRECTORY, // for existing table in S3 (both new and existing partition)
-
-        // NOTE: Insert overwrite simulation (partition drops and partition additions in the same
-        // transaction get merged and become one or more partition alterations, and get submitted to
-        // metastore in close succession of each other) is not supported for S3. S3 uses the last
-        // mode for insert into existing table. This is hard to support because the directory
-        // containing the old data cannot be deleted until commit. Nor can the old data be moved
-        // (assuming Hive HDFS directory naming convention shall not be violated). As a result,
-        // subsequent insertion will have to write to directory belonging to existing partition.
-        // This undermines the benefit of having insert overwrite simulation. This also makes
-        // dropping of old partition at commit time hard because data added after the logical
-        // "drop" time was added to the directories to be dropped.
     }
 
     private enum ActionType
