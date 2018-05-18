65c998277bdb34c44a8f6cd90ad13da5f875c901
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-05-18 11:44:37 +0200

    Remove unused catalogs loaded variable

--- presto-main/src/main/java/com/facebook/presto/metadata/StaticCatalogStore.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/StaticCatalogStore.java
@@ -40,7 +40,6 @@ public class StaticCatalogStore
     private final File catalogConfigurationDir;
     private final Set<String> disabledCatalogs;
     private final AtomicBoolean catalogsLoading = new AtomicBoolean();
-    private final AtomicBoolean catalogsLoaded = new AtomicBoolean();
 
     @Inject
     public StaticCatalogStore(ConnectorManager connectorManager, StaticCatalogStoreConfig config)
@@ -57,11 +56,6 @@ public class StaticCatalogStore
         this.disabledCatalogs = ImmutableSet.copyOf(disabledCatalogs);
     }
 
-    public boolean areCatalogsLoaded()
-    {
-        return catalogsLoaded.get();
-    }
-
     public void loadCatalogs()
             throws Exception
     {
@@ -74,8 +68,6 @@ public class StaticCatalogStore
                 loadCatalog(file);
             }
         }
-
-        catalogsLoaded.set(true);
     }
 
     private void loadCatalog(File file)
