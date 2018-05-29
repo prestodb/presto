16a8f5a22a2787ed1e6d06d478004865107ddcaa
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2018-05-29 14:28:52 +0200

    Run maven in batch mode for automation
    This commit just makes the maven output less verbose.
    No progress is logged for artifacts downloading.

--- presto-hive-hadoop2/bin/run_hive_s3_tests.sh
+++ presto-hive-hadoop2/bin/run_hive_s3_tests.sh
@@ -30,7 +30,7 @@ retry check_hadoop
 # run product tests
 pushd $PROJECT_ROOT
 set +e
-./mvnw -pl presto-hive-hadoop2 test -P test-hive-hadoop2-s3 \
+./mvnw -B -pl presto-hive-hadoop2 test -P test-hive-hadoop2-s3 \
   -DHADOOP_USER_NAME=hive \
   -Dhive.hadoop2.metastoreHost=localhost \
   -Dhive.hadoop2.metastorePort=9083 \
