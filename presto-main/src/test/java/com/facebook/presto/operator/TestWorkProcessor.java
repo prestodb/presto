782350d6f392b48ff601dbf7d3d9f75636fad94d
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-06-07 15:39:19 +0200

    Rename TestWorkProcessorUtils to TestWorkProcessor

--- presto-main/src/test/java/com/facebook/presto/operator/TestWorkProcessor.java
+++ presto-main/src/test/java/com/facebook/presto/operator/TestWorkProcessor.java
@@ -33,7 +33,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-public class TestWorkProcessorUtils
+public class TestWorkProcessor
 {
     @Test
     public void testIterator()
