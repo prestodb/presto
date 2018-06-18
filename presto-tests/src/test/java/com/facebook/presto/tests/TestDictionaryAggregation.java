6a72aeb1175eef48e9cc828b33d83b025b024545
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-06-18 10:50:01 +0200

    Add basic test for dictionary aggregation

---
+++ presto-tests/src/test/java/com/facebook/presto/tests/TestDictionaryAggregation.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tests;
+
+import com.facebook.presto.testing.LocalQueryRunner;
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.SystemSessionProperties.DICTIONARY_AGGREGATION;
+import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
+
+public class TestDictionaryAggregation
+        extends AbstractTestQueryFramework
+{
+    public TestDictionaryAggregation()
+    {
+        super(() -> new LocalQueryRunner(testSessionBuilder()
+                .setSystemProperty(DICTIONARY_AGGREGATION, "true")
+                .build()));
+    }
+
+    @Test
+    public void testMixedDistinctWithFilter()
+    {
+        assertQuery(
+                "SELECT " +
+                        "     count(DISTINCT x) FILTER (WHERE x > 0), " +
+                        "     sum(x) " +
+                        "FROM (VALUES 0, 1, 1, 2) t(x)",
+                "VALUES (2, 4)");
+
+        assertQuery(
+                "SELECT count(DISTINCT x) FILTER (where y = 1)" +
+                        "FROM (VALUES (2, 1), (1, 2), (1,1)) t(x, y)",
+                "VALUES 2");
+    }
+}
