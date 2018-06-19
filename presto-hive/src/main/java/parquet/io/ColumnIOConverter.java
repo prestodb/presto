c91807da4af4add602e015c0fd70b5fd707a993b
Yaliang Wang <yaliangw@twitter.com>
2018-06-19 02:21:41 -0700

    Support case-insensitive column lookup in Parquet readers

--- presto-hive/src/main/java/parquet/io/ColumnIOConverter.java
+++ presto-hive/src/main/java/parquet/io/ColumnIOConverter.java
@@ -29,6 +29,7 @@ import java.util.Optional;
 
 import static com.facebook.presto.hive.parquet.ParquetTypeUtils.getArrayElementColumn;
 import static com.facebook.presto.hive.parquet.ParquetTypeUtils.getMapKeyValueColumn;
+import static com.facebook.presto.hive.parquet.ParquetTypeUtils.lookupColumnByName;
 import static com.facebook.presto.spi.type.StandardTypes.ARRAY;
 import static com.facebook.presto.spi.type.StandardTypes.MAP;
 import static com.facebook.presto.spi.type.StandardTypes.ROW;
@@ -60,7 +61,7 @@ public class ColumnIOConverter
             for (int i = 0; i < fields.size(); i++) {
                 NamedTypeSignature namedTypeSignature = fields.get(i).getNamedTypeSignature();
                 String name = namedTypeSignature.getName().get().toLowerCase(Locale.ENGLISH);
-                Optional<Field> field = constructField(parameters.get(i), groupColumnIO.getChild(name));
+                Optional<Field> field = constructField(parameters.get(i), lookupColumnByName(groupColumnIO, name));
                 structHasParameters |= field.isPresent();
                 fieldsBuilder.add(field);
             }
