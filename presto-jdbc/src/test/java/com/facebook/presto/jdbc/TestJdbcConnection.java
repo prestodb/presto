starburstdata/facebook-presto

Replace distributed_join with join_distribution_type session property
The `join_distribution_type` property has three option: "REPARTITIONED",
"REPLICATED", and "AUTOMATIC". This property replaces the
`distributed_join` property. "REPARTITIONED" has the same behavior as
`distribtued_join=true`. "REPLICATED" is equivalent to
`distributed_join=false`. "AUTOMATIC" will use stats to evaluate whether
partitioned or replicated is better, or if no information is available,
it will fall back to partitioned.

The default value for `join_distribution_type` is "REPARTITIONED".

presto-jdbc/src/test/java/com/facebook/presto/jdbc/TestJdbcConnection.java
@@ -146,15 +146,15 @@ public class TestJdbcConnection
     {
         try (Connection connection = createConnection()) {
             assertThat(listSession(connection))
-                    .contains("distributed_join|true|true")
+                    .contains("join_distribution_type|REPARTITIONED|REPARTITIONED")
                     .contains("exchange_compression|false|false");
 
             try (Statement statement = connection.createStatement()) {
-                statement.execute("SET SESSION distributed_join = false");
+                statement.execute("SET SESSION join_distribution_type = 'REPLICATED'");
             }
 
             assertThat(listSession(connection))
-                    .contains("distributed_join|false|true")
+                    .contains("join_distribution_type|REPLICATED|REPARTITIONED")
                     .contains("exchange_compression|false|false");
 
             try (Statement statement = connection.createStatement()) {
@@ -162,7 +162,7 @@ public class TestJdbcConnection
             }
 
             assertThat(listSession(connection))
-                    .contains("distributed_join|false|true")
+                    .contains("join_distribution_type|REPLICATED|REPARTITIONED")
                     .contains("exchange_compression|true|false");
         }
     }

presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
@@ -24,6 +24,8 @@ import java.util.Map;
 
 import static com.facebook.presto.operator.aggregation.histogram.HistogramGroupImplementation.LEGACY;
 import static com.facebook.presto.operator.aggregation.histogram.HistogramGroupImplementation.NEW;
+import static com.facebook.presto.sql.analyzer.FeaturesConfig.JoinDistributionType.REPARTITIONED;
+import static com.facebook.presto.sql.analyzer.FeaturesConfig.JoinDistributionType.REPLICATED;
 import static com.facebook.presto.sql.analyzer.FeaturesConfig.SPILLER_SPILL_PATH;
 import static com.facebook.presto.sql.analyzer.FeaturesConfig.SPILL_ENABLED;
 import static com.facebook.presto.sql.analyzer.RegexLibrary.JONI;
@@ -47,7 +49,7 @@ public class TestFeaturesConfig
                 .setNetworkCostWeight(15)
                 .setResourceGroupsEnabled(false)
                 .setDistributedIndexJoinsEnabled(false)
-                .setDistributedJoinsEnabled(true)
+                .setJoinDistributionType(REPARTITIONED)
                 .setFastInequalityJoins(true)
                 .setColocatedJoinsEnabled(false)
                 .setJoinReorderingEnabled(true)
@@ -103,7 +105,7 @@ public class TestFeaturesConfig
                 .put("deprecated.legacy-order-by", "true")
                 .put("deprecated.legacy-map-subscript", "true")
                 .put("distributed-index-joins-enabled", "true")
-                .put("distributed-joins-enabled", "false")
+                .put("join-distribution-type", "REPLICATED")
                 .put("fast-inequality-joins", "false")
                 .put("colocated-joins-enabled", "true")
                 .put("reorder-joins", "false")
@@ -147,7 +149,7 @@ public class TestFeaturesConfig
                 .setIterativeOptimizerTimeout(new Duration(10, SECONDS))
                 .setEnableNewStatsCalculator(false)
                 .setDistributedIndexJoinsEnabled(true)
-                .setDistributedJoinsEnabled(false)
+                .setJoinDistributionType(REPLICATED)
                 .setFastInequalityJoins(false)
                 .setColocatedJoinsEnabled(true)
                 .setJoinReorderingEnabled(false)

prestodb/presto

presto-jdbc/src/test/java/com/facebook/presto/jdbc/TestJdbcConnection.java
@@ -146,15 +146,15 @@ public class TestJdbcConnection
     {
         try (Connection connection = createConnection()) {
             assertThat(listSession(connection))
-                    .contains("distributed_join|true|true")
+                    .contains("join_distribution_type|REPARTITIONED|REPARTITIONED")
                     .contains("exchange_compression|false|false");
 
             try (Statement statement = connection.createStatement()) {
-                statement.execute("SET SESSION distributed_join = false");
+                statement.execute("SET SESSION join_distribution_type = 'BROADCAST'");
             }
 
             assertThat(listSession(connection))
-                    .contains("distributed_join|false|true")
+                    .contains("join_distribution_type|BROADCAST|REPARTITIONED")
                     .contains("exchange_compression|false|false");
 
             try (Statement statement = connection.createStatement()) {
@@ -162,7 +162,7 @@ public class TestJdbcConnection
             }
 
             assertThat(listSession(connection))
-                    .contains("distributed_join|false|true")
+                    .contains("join_distribution_type|BROADCAST|REPARTITIONED")
                     .contains("exchange_compression|true|false");
         }
     }

presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
@@ -24,6 +24,8 @@ import org.testng.annotations.Test;
 
 import java.util.Map;
 
+import static com.facebook.presto.sql.analyzer.FeaturesConfig.JoinDistributionType.BROADCAST;
+import static com.facebook.presto.sql.analyzer.FeaturesConfig.JoinDistributionType.REPARTITIONED;
 import static com.facebook.presto.sql.analyzer.FeaturesConfig.SPILLER_SPILL_PATH;
 import static com.facebook.presto.sql.analyzer.FeaturesConfig.SPILL_ENABLED;
 import static com.facebook.presto.sql.analyzer.RegexLibrary.JONI;
@@ -47,7 +49,7 @@ public class TestFeaturesConfig
                 .setMemoryCostWeight(10)
                 .setNetworkCostWeight(15)
                 .setDistributedIndexJoinsEnabled(false)
-                .setDistributedJoinsEnabled(true)
+                .setJoinDistributionType(REPARTITIONED)
                 .setGroupedExecutionForAggregationEnabled(false)
                 .setConcurrentLifespansPerTask(0)
                 .setFastInequalityJoins(true)
@@ -117,7 +119,7 @@ public class TestFeaturesConfig
                 .put("deprecated.legacy-join-using", "true")
                 .put("deprecated.legacy-row-field-ordinal-access", "true")
                 .put("distributed-index-joins-enabled", "true")
-                .put("distributed-joins-enabled", "false")
+                .put("join-distribution-type", "BROADCAST")
                 .put("grouped-execution-for-aggregation-enabled", "true")
                 .put("concurrent-lifespans-per-task", "1")
                 .put("fast-inequality-joins", "false")
@@ -167,7 +169,7 @@ public class TestFeaturesConfig
                 .setIterativeOptimizerTimeout(new Duration(10, SECONDS))
                 .setEnableNewStatsCalculator(false)
                 .setDistributedIndexJoinsEnabled(true)
-                .setDistributedJoinsEnabled(false)
+                .setJoinDistributionType(BROADCAST)
                 .setGroupedExecutionForAggregationEnabled(true)
                 .setConcurrentLifespansPerTask(1)
                 .setFastInequalityJoins(false)