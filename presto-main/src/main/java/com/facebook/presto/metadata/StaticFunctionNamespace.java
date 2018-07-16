3f8fe7b2331627f1fb971c2fe862cc3b3da0c526
Cole Bowden <bowdencm@fb.com>
2018-07-16 15:49:57 -0700

    Add FunctionNamespace interface and StaticFunctionNamespace
    The implementation of StaticFunctionNamespace is
    currently just a delegation to the FunctionRegistry.
    This logic will be updated when it is determined
    how functions will be connected to schemas.

---
+++ presto-main/src/main/java/com/facebook/presto/metadata/StaticFunctionNamespace.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.metadata;
+
+import com.facebook.presto.operator.aggregation.InternalAggregationFunction;
+import com.facebook.presto.operator.scalar.ScalarFunctionImplementation;
+import com.facebook.presto.operator.window.WindowFunctionSupplier;
+import com.facebook.presto.spi.block.BlockEncodingSerde;
+import com.facebook.presto.spi.function.OperatorType;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
+import com.facebook.presto.spi.type.TypeSignature;
+import com.facebook.presto.sql.analyzer.FeaturesConfig;
+import com.facebook.presto.sql.analyzer.TypeSignatureProvider;
+import com.facebook.presto.sql.tree.QualifiedName;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+import java.util.List;
+
+@ThreadSafe
+public class StaticFunctionNamespace
+        implements FunctionNamespace
+{
+    private final FunctionRegistry functionRegistry;
+
+    public StaticFunctionNamespace(TypeManager typeManager, BlockEncodingSerde blockEncodingSerde, FeaturesConfig featuresConfig)
+    {
+        functionRegistry = new FunctionRegistry(typeManager, blockEncodingSerde, featuresConfig);
+    }
+
+    public void addFunctions(List<? extends SqlFunction> functions)
+    {
+        functionRegistry.addFunctions(functions);
+    }
+
+    public List<SqlFunction> list()
+    {
+        return functionRegistry.list();
+    }
+
+    public Signature resolveFunction(QualifiedName name, List<TypeSignatureProvider> parameterTypes)
+    {
+        return functionRegistry.resolveFunction(name, parameterTypes);
+    }
+
+    public WindowFunctionSupplier getWindowFunctionImplementation(Signature signature)
+    {
+        return functionRegistry.getWindowFunctionImplementation(signature);
+    }
+
+    public InternalAggregationFunction getAggregateFunctionImplementation(Signature signature)
+    {
+        return functionRegistry.getAggregateFunctionImplementation(signature);
+    }
+
+    public ScalarFunctionImplementation getScalarFunctionImplementation(Signature signature)
+    {
+        return functionRegistry.getScalarFunctionImplementation(signature);
+    }
+
+    public boolean canResolveOperator(OperatorType operatorType, Type returnType, List<? extends Type> argumentTypes)
+    {
+        return functionRegistry.canResolveOperator(operatorType, returnType, argumentTypes);
+    }
+
+    public boolean isRegistered(Signature signature)
+    {
+        return functionRegistry.isRegistered(signature);
+    }
+
+    public Signature resolveOperator(OperatorType operatorType, List<? extends Type> argumentTypes)
+            throws OperatorNotFoundException
+    {
+        return functionRegistry.resolveOperator(operatorType, argumentTypes);
+    }
+
+    public Signature getCoercion(Type fromType, Type toType)
+    {
+        return functionRegistry.getCoercion(fromType, toType);
+    }
+
+    public Signature getCoercion(TypeSignature fromType, TypeSignature toType)
+    {
+        return functionRegistry.getCoercion(fromType, toType);
+    }
+}
