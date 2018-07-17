40b9d6d5abefdf9964a525bd1445f08059e4d627
Nezih Yigitbasi <nezihy@fb.com>
2018-07-17 09:36:45 -0700

    Revert "Make sure to release task-level memory on completion"
    This reverts commit ea5a9c5204ff8f235127db4afd40e50d24549198.
    
    This change introduced a race where the task context destroys the
    aggregated memory context, and a thread does a localContext.setBytes()
    after that resulting in an IllegalArgumentException:
    
    Caused by: java.lang.IllegalArgumentException: tried to free more memory than is reserved by query
    	at com.google.common.base.Preconditions.checkArgument(Preconditions.java:135)
    	at com.facebook.presto.memory.MemoryPool.free(MemoryPool.java:178)
    	at com.facebook.presto.memory.DefaultQueryContext.updateUserMemory(DefaultQueryContext.java:137)
    	at com.facebook.presto.memory.DefaultQueryContext$QueryMemoryReservationHandler.reserveMemory(DefaultQueryContext.java:310)
    	at com.facebook.presto.memory.context.RootAggregatedMemoryContext.updateBytes(RootAggregatedMemoryContext.java:35)
    	at com.facebook.presto.memory.context.ChildAggregatedMemoryContext.updateBytes(ChildAggregatedMemoryContext.java:36)
    	at com.facebook.presto.memory.context.ChildAggregatedMemoryContext.updateBytes(ChildAggregatedMemoryContext.java:36)
    	at com.facebook.presto.memory.context.ChildAggregatedMemoryContext.updateBytes(ChildAggregatedMemoryContext.java:36)
    	at com.facebook.presto.memory.context.ChildAggregatedMemoryContext.updateBytes(ChildAggregatedMemoryContext.java:36)
    	at com.facebook.presto.memory.context.SimpleLocalMemoryContext.setBytes(SimpleLocalMemoryContext.java:63)
    	at com.facebook.presto.operator.OperatorContext$InternalLocalMemoryContext.setBytes(OperatorContext.java:626)
    	at com.facebook.presto.operator.HashBuilderOperator.lambda$close$10(HashBuilderOperator.java:628)
    	at com.google.common.io.Closer.close(Closer.java:214)
    	at com.facebook.presto.operator.HashBuilderOperator.close(HashBuilderOperator.java:630)
    	at com.facebook.presto.operator.HashBuilderOperator.finish(HashBuilderOperator.java:432)
    	at com.facebook.presto.operator.Driver.processInternal(Driver.java:355)
    	at com.facebook.presto.operator.Driver.lambda$processFor$8(Driver.java:282)
    	at com.facebook.presto.operator.Driver.tryWithLock(Driver.java:672)
    	at com.facebook.presto.operator.Driver.processFor(Driver.java:276)
    	at com.facebook.presto.execution.SqlTaskExecution$DriverSplitRunner.processFor(SqlTaskExecution.java:973)
    	at com.facebook.presto.execution.executor.PrioritizedSplitRunner.process(PrioritizedSplitRunner.java:162)
    	at com.facebook.presto.execution.executor.TaskExecutor$TaskRunner.run(TaskExecutor.java:477)
    	... 3 more

--- presto-main/src/test/java/com/facebook/presto/execution/TestSqlTask.java
+++ presto-main/src/test/java/com/facebook/presto/execution/TestSqlTask.java
@@ -21,20 +21,15 @@ import com.facebook.presto.execution.buffer.BufferState;
 import com.facebook.presto.execution.executor.TaskExecutor;
 import com.facebook.presto.memory.DefaultQueryContext;
 import com.facebook.presto.memory.MemoryPool;
-import com.facebook.presto.memory.context.MemoryTrackingContext;
-import com.facebook.presto.operator.PipelineContext;
-import com.facebook.presto.operator.TaskContext;
 import com.facebook.presto.spi.QueryId;
 import com.facebook.presto.spi.memory.MemoryPoolId;
 import com.facebook.presto.spiller.SpillSpaceTracker;
 import com.facebook.presto.sql.planner.LocalExecutionPlanner;
-import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.base.Ticker;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.SettableFuture;
 import io.airlift.stats.TestingGcMonitor;
 import io.airlift.units.DataSize;
 import org.testng.annotations.AfterClass;
@@ -43,7 +38,6 @@ import org.testng.annotations.Test;
 import java.net.URI;
 import java.util.Optional;
 import java.util.OptionalInt;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -110,7 +104,7 @@ public class TestSqlTask
     @Test
     public void testEmptyQuery()
     {
-        SqlTask sqlTask = createInitialTask(Optional.empty());
+        SqlTask sqlTask = createInitialTask();
 
         TaskInfo taskInfo = sqlTask.updateTask(TEST_SESSION,
                 Optional.of(PLAN_FRAGMENT),
@@ -139,7 +133,7 @@ public class TestSqlTask
     public void testSimpleQuery()
             throws Exception
     {
-        SqlTask sqlTask = createInitialTask(Optional.empty());
+        SqlTask sqlTask = createInitialTask();
 
         TaskInfo taskInfo = sqlTask.updateTask(TEST_SESSION,
                 Optional.of(PLAN_FRAGMENT),
@@ -175,7 +169,7 @@ public class TestSqlTask
     @Test
     public void testCancel()
     {
-        SqlTask sqlTask = createInitialTask(Optional.empty());
+        SqlTask sqlTask = createInitialTask();
 
         TaskInfo taskInfo = sqlTask.updateTask(TEST_SESSION,
                 Optional.of(PLAN_FRAGMENT),
@@ -201,43 +195,10 @@ public class TestSqlTask
     }
 
     @Test
-    public void testTaskMemoryReleasedOnCompletion()
-            throws InterruptedException, ExecutionException, TimeoutException
-    {
-        // waitUntilDone future gets completed when the task is done
-        SettableFuture waitUntilDone = SettableFuture.create();
-        SqlTask sqlTask = createInitialTask(Optional.of((input) -> waitUntilDone.set(null)));
-        sqlTask.updateTask(TEST_SESSION,
-                Optional.of(PLAN_FRAGMENT),
-                ImmutableList.of(),
-                createInitialEmptyOutputBuffers(PARTITIONED)
-                        .withBuffer(OUT, 0)
-                        .withNoMoreBufferIds(),
-                OptionalInt.empty());
-
-        TaskContext taskContext = sqlTask.getQueryContext().getTaskContextByTaskId(sqlTask.getTaskId());
-        PipelineContext pipelineContext1 = taskContext.addPipelineContext(0, false, false);
-        PipelineContext pipelineContext2 = taskContext.addPipelineContext(1, false, false);
-
-        pipelineContext1.localSystemMemoryContext().setBytes(100);
-        pipelineContext2.localSystemMemoryContext().setBytes(100);
-        taskContext.localSystemMemoryContext().setBytes(100);
-
-        MemoryTrackingContext memoryTrackingContext = taskContext.getTaskMemoryContext();
-        assertEquals(memoryTrackingContext.getSystemMemory(), 300);
-
-        // canceling the task will destroy the TaskContext, which will destroy
-        // the local/aggregate memory contexts tied to it including the PipelineContexts
-        sqlTask.cancel();
-        waitUntilDone.get(30, SECONDS);
-        assertEquals(memoryTrackingContext.getSystemMemory(), 0);
-    }
-
-    @Test
     public void testAbort()
             throws Exception
     {
-        SqlTask sqlTask = createInitialTask(Optional.empty());
+        SqlTask sqlTask = createInitialTask();
 
         TaskInfo taskInfo = sqlTask.updateTask(TEST_SESSION,
                 Optional.of(PLAN_FRAGMENT),
@@ -262,7 +223,7 @@ public class TestSqlTask
     public void testBufferCloseOnFinish()
             throws Exception
     {
-        SqlTask sqlTask = createInitialTask(Optional.empty());
+        SqlTask sqlTask = createInitialTask();
 
         OutputBuffers outputBuffers = createInitialEmptyOutputBuffers(PARTITIONED).withBuffer(OUT, 0).withNoMoreBufferIds();
         updateTask(sqlTask, EMPTY_SOURCES, outputBuffers);
@@ -289,7 +250,7 @@ public class TestSqlTask
     public void testBufferCloseOnCancel()
             throws Exception
     {
-        SqlTask sqlTask = createInitialTask(Optional.empty());
+        SqlTask sqlTask = createInitialTask();
 
         updateTask(sqlTask, EMPTY_SOURCES, createInitialEmptyOutputBuffers(PARTITIONED).withBuffer(OUT, 0).withNoMoreBufferIds());
 
@@ -311,7 +272,7 @@ public class TestSqlTask
     public void testBufferNotCloseOnFail()
             throws Exception
     {
-        SqlTask sqlTask = createInitialTask(Optional.empty());
+        SqlTask sqlTask = createInitialTask();
 
         updateTask(sqlTask, EMPTY_SOURCES, createInitialEmptyOutputBuffers(PARTITIONED).withBuffer(OUT, 0).withNoMoreBufferIds());
 
@@ -333,7 +294,7 @@ public class TestSqlTask
         assertFalse(sqlTask.getTaskResults(OUT, 0, new DataSize(1, MEGABYTE)).isDone());
     }
 
-    public SqlTask createInitialTask(Optional<Function<SqlTask, ?>> onDone)
+    public SqlTask createInitialTask()
     {
         TaskId taskId = new TaskId("query", 0, nextTaskId.incrementAndGet());
         URI location = URI.create("fake://task/" + taskId);
@@ -353,7 +314,7 @@ public class TestSqlTask
                         new SpillSpaceTracker(new DataSize(1, GIGABYTE))),
                 sqlTaskExecutionFactory,
                 taskNotificationExecutor,
-                onDone.orElseGet(() -> Functions.identity()),
+                Functions.identity(),
                 new DataSize(32, MEGABYTE));
     }
 }
