d6f7548c24137e6cd6cd1755dd90e3c7dc3c2865
Gerlou Shyy <gerlou@fb.com>
2018-07-19 11:25:15 -0700

    Move @BlockPosition and @BlockIndex to SPI

--- presto-spi/src/main/java/com/facebook/presto/spi/function/BlockIndex.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/function/BlockIndex.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.operator.aggregation;
+package com.facebook.presto.spi.function;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
