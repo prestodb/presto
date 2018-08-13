b600533621b99f0627ca887f05600949792050c6
David Phillips <david@acz.org>
2018-08-13 10:25:18 -0700

    Fix formatting in PushAggregationThroughOuterJoin

--- presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/PushAggregationThroughOuterJoin.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/PushAggregationThroughOuterJoin.java
@@ -259,11 +259,7 @@ public class PushAggregationThroughOuterJoin
                 assignmentsBuilder.put(symbol, symbol.toSymbolReference());
             }
         }
-        return Optional.of(
-                new ProjectNode(
-                        idAllocator.getNextId(),
-                                            crossJoin,
-                                            assignmentsBuilder.build()));
+        return Optional.of(new ProjectNode(idAllocator.getNextId(), crossJoin, assignmentsBuilder.build()));
     }
 
     private Optional<MappedAggregationInfo> createAggregationOverNull(AggregationNode referenceAggregation, SymbolAllocator symbolAllocator, PlanNodeIdAllocator idAllocator, Lookup lookup)
@@ -321,10 +317,7 @@ public class PushAggregationThroughOuterJoin
                 Optional.empty(),
                 Optional.empty());
 
-        return Optional.of(
-                new MappedAggregationInfo(
-                        aggregationOverNullRow,
-                                aggregationsSymbolMapping));
+        return Optional.of(new MappedAggregationInfo(aggregationOverNullRow, aggregationsSymbolMapping));
     }
 
     private static boolean isUsingSymbols(AggregationNode.Aggregation aggregation, Set<Symbol> sourceSymbols)
