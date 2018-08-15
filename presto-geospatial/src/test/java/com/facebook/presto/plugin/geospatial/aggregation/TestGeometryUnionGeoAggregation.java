e86a444d4dd51a6bd8259c83f6e3008341a3d7bb
Yongyang Yu <yongyangyu@fb.com>
2018-08-14 22:23:39 -0700

    Add convex_hull_agg function
    convex_hull_agg aggregate function computes convex hull of all input geometries.

--- presto-geospatial/src/test/java/com/facebook/presto/plugin/geospatial/aggregation/TestGeometryUnionGeoAggregation.java
+++ presto-geospatial/src/test/java/com/facebook/presto/plugin/geospatial/aggregation/TestGeometryUnionGeoAggregation.java
@@ -13,34 +13,11 @@
  */
 package com.facebook.presto.plugin.geospatial.aggregation;
 
-import com.esri.core.geometry.ogc.OGCGeometry;
-import com.facebook.presto.block.BlockAssertions;
-import com.facebook.presto.geospatial.serde.GeometrySerde;
-import com.facebook.presto.metadata.FunctionKind;
-import com.facebook.presto.metadata.Signature;
-import com.facebook.presto.operator.aggregation.InternalAggregationFunction;
-import com.facebook.presto.operator.scalar.AbstractTestFunctions;
-import com.facebook.presto.plugin.geospatial.GeoPlugin;
-import com.facebook.presto.plugin.geospatial.GeometryType;
-import com.facebook.presto.spi.Page;
-import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.spi.type.TypeSignature;
-import io.airlift.slice.Slice;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.BiFunction;
-import java.util.stream.Collectors;
-
-import static com.facebook.presto.metadata.FunctionExtractor.extractFunctions;
-import static com.facebook.presto.operator.aggregation.AggregationTestUtils.assertAggregation;
-
-public class TestGeometryUnionAgg
-        extends AbstractTestFunctions
+public class TestGeometryUnionGeoAggregation
+        extends AbstractTestGeoAggregationFunctions
 {
     @DataProvider(name = "point")
     public Object[][] point()
@@ -311,24 +288,6 @@ public class TestGeometryUnionAgg
         };
     }
 
-    private InternalAggregationFunction function;
-
-    @BeforeClass
-    public void registerFunctions()
-    {
-        GeoPlugin plugin = new GeoPlugin();
-        for (Type type : plugin.getTypes()) {
-            functionAssertions.getTypeRegistry().addType(type);
-        }
-        functionAssertions.getMetadata().addFunctions(extractFunctions(plugin.getFunctions()));
-
-        function = functionAssertions.getMetadata().getFunctionRegistry().getAggregateFunctionImplementation(new Signature(
-                "geometry_union_agg",
-                FunctionKind.AGGREGATE,
-                TypeSignature.parseTypeSignature(GeometryType.GEOMETRY_TYPE_NAME),
-                TypeSignature.parseTypeSignature(GeometryType.GEOMETRY_TYPE_NAME)));
-    }
-
     @Test(dataProvider = "point")
     public void testPoint(String testDescription, String expectedWkt, String... wkts)
     {
@@ -371,29 +330,9 @@ public class TestGeometryUnionAgg
         assertAggregatedGeometries(testDescription, expectedWkt, wkts);
     }
 
-    private void assertAggregatedGeometries(String testDescription, String expectedWkt, String... wkts)
+    @Override
+    protected String getFunctionName()
     {
-        List<Slice> geometrySlices = Arrays.stream(wkts)
-                .map(text -> text == null ? null : OGCGeometry.fromText(text))
-                .map(input -> input == null ? null : GeometrySerde.serialize(input))
-                .collect(Collectors.toList());
-
-        // Add a custom equality assertion because the resulting geometry may have its constituent points in a different order
-        BiFunction<Object, Object, Boolean> equalityFunction = (left, right) -> {
-            if (left == null && right == null) {
-                return true;
-            }
-            if (left == null || right == null) {
-                return false;
-            }
-            OGCGeometry leftGeometry = OGCGeometry.fromText(left.toString());
-            OGCGeometry rightGeometry = OGCGeometry.fromText(right.toString());
-            // Check for equality by getting the difference
-            return leftGeometry.difference(rightGeometry).isEmpty() && rightGeometry.difference(leftGeometry).isEmpty();
-        };
-        // Test in forward and reverse order to verify that ordering doesn't affect the output
-        assertAggregation(function, equalityFunction, testDescription, new Page(BlockAssertions.createSlicesBlock(geometrySlices)), expectedWkt);
-        Collections.reverse(geometrySlices);
-        assertAggregation(function, equalityFunction, testDescription, new Page(BlockAssertions.createSlicesBlock(geometrySlices)), expectedWkt);
+        return "geometry_union_agg";
     }
 }
