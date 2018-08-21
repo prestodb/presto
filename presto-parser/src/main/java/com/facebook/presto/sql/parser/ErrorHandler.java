8e044027612e7f383e3ccdf2f976b4212edd1283
Martin Traverso <mtraverso@gmail.com>
2018-08-21 11:15:00 -0700

    Log raised during error handling
    It was logging the parsing exception, not the exception that could
    occur while handling the parsing error due to a bug in the implementation.

--- presto-parser/src/main/java/com/facebook/presto/sql/parser/ErrorHandler.java
+++ presto-parser/src/main/java/com/facebook/presto/sql/parser/ErrorHandler.java
@@ -86,7 +86,7 @@ class ErrorHandler
             message = String.format("mismatched input '%s'. Expecting: %s", ((Token) offendingSymbol).getText(), expected);
         }
         catch (Exception exception) {
-            LOG.error(e, "Unexpected failure when handling parsing error. This is likely a bug in the implementation");
+            LOG.error(exception, "Unexpected failure when handling parsing error. This is likely a bug in the implementation");
         }
 
         throw new ParsingException(message, e, line, charPositionInLine);
