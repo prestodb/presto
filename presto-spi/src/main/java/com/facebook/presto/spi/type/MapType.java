df5131c0c1e52d63a71541885bbe3d289e9b585c
Ying Su <yingsu@fb.com>
2018-09-05 15:59:54 -0700

    Remove null vector from ArrayBlock, MapBlock, and RowBlock when all values are not null
    This reverts commit d7256c59dede509e84ae127ecb20810f09de7014.

--- presto-spi/src/main/java/com/facebook/presto/spi/type/MapType.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/type/MapType.java
@@ -26,6 +26,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.facebook.presto.spi.type.TypeUtils.checkElementNotNull;
@@ -250,7 +251,7 @@ public class MapType
         return "map(" + keyType.getDisplayName() + ", " + valueType.getDisplayName() + ")";
     }
 
-    public Block createBlockFromKeyValue(boolean[] mapIsNull, int[] offsets, Block keyBlock, Block valueBlock)
+    public Block createBlockFromKeyValue(Optional<boolean[]> mapIsNull, int[] offsets, Block keyBlock, Block valueBlock)
     {
         return MapBlock.fromKeyValueBlock(
                 mapIsNull,
@@ -273,7 +274,7 @@ public class MapType
             Type keyType,
             int startOffset,
             int positionCount,
-            boolean[] mapIsNull,
+            Optional<boolean[]> mapIsNull,
             int[] offsets,
             Block keyBlock,
             Block valueBlock,
