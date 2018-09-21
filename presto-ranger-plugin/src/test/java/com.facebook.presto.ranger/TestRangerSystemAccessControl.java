b9f19c8ceb4a9c07602736a7d7a0a9b0280d59ca
zhangyifei <zhangyifei@zbj.com>
2018-09-21 10:06:52 +0800

    fix package error and add unit test

---
+++ presto-ranger-plugin/src/test/java/com.facebook.presto.ranger/TestRangerSystemAccessControl.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.ranger;
+
+import org.testng.annotations.Test;
+
+public class TestRangerSystemAccessControl
+{
+    @Test
+    public void testCheckCanSetUser() throws Exception
+    {
+    }
+
+    @Test
+    public void testFilterCatalogs() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanSetSystemSessionProperty() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanSetCatalogSessionProperty() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanAccessCatalog() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanShowSchemas() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanShowTablesMetadata() throws Exception
+    {
+    }
+
+    @Test
+    public void testFilterTables() throws Exception
+    {
+    }
+
+    @Test
+    public void testFilterSchemas() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanCreateSchema() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanDropSchema() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanRenameSchema() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanCreateTable() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanDropTable() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanRenameTable() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanAddColumn() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanRenameColumn() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanInsertIntoTable() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanDeleteFromTable() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanCreateView() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanDropView() throws Exception
+    {
+    }
+
+    @Test
+    public void testCheckCanSelectFromColumns() throws Exception
+    {
+    }
+}
