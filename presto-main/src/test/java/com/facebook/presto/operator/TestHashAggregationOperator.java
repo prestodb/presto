3b7f8461d2123cd08420405d93efa81665816e94
Saksham Sachdev <sakshams@fb.com>
2018-10-19 16:35:56 +0200

    Extract DummySpillerFactory from TestHashAggregationOperator
    Cherry-pick of https://github.com/prestosql/presto/commit/f01067ba9dda7f4a1bbcabcaf901b3c008969207
    
    Co-authored-by: Atri Sharma <atri@linux.com>

--- presto-main/src/test/java/com/facebook/presto/operator/TestHashAggregationOperator.java
+++ presto-main/src/test/java/com/facebook/presto/operator/TestHashAggregationOperator.java
@@ -80,7 +80,6 @@ import static com.google.common.base.Strings.nullToEmpty;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.common.util.concurrent.Futures.immediateFailedFuture;
-import static com.google.common.util.concurrent.Futures.immediateFuture;
 import static io.airlift.slice.SizeOf.SIZE_OF_DOUBLE;
 import static io.airlift.slice.SizeOf.SIZE_OF_LONG;
 import static io.airlift.units.DataSize.Unit.KILOBYTE;
@@ -779,47 +778,6 @@ public class TestHashAggregationOperator
     private static InternalAggregationFunction getAggregation(String name, Type... arguments)
     {
         return functionManager.getAggregateFunctionImplementation(functionManager.lookupFunction(name, fromTypes(arguments)));
-    }
-
-    private static class DummySpillerFactory
-            implements SpillerFactory
-    {
-        private long spillsCount;
-
-        @Override
-        public Spiller create(List<Type> types, SpillContext spillContext, AggregatedMemoryContext memoryContext)
-        {
-            return new Spiller()
-            {
-                private final List<Iterable<Page>> spills = new ArrayList<>();
-
-                @Override
-                public ListenableFuture<?> spill(Iterator<Page> pageIterator)
-                {
-                    spillsCount++;
-                    spills.add(ImmutableList.copyOf(pageIterator));
-                    return immediateFuture(null);
-                }
-
-                @Override
-                public List<Iterator<Page>> getSpills()
-                {
-                    return spills.stream()
-                            .map(Iterable::iterator)
-                            .collect(toImmutableList());
-                }
-
-                @Override
-                public void close()
-                {
-                }
-            };
-        }
-
-        public long getSpillsCount()
-        {
-            return spillsCount;
-        }
     }
 
     private static class FailingSpillerFactory
