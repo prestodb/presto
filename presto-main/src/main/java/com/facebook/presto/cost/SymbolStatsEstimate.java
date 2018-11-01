e75dbece2bdfb4acc4bcea57223bd6bb1de39463
Andrii Rosa <andriirosa@fb.com>
2018-10-31 17:42:50 -0700

    Remove unused methods from SymbolStatsEstimate

--- presto-main/src/main/java/com/facebook/presto/cost/SymbolStatsEstimate.java
+++ presto-main/src/main/java/com/facebook/presto/cost/SymbolStatsEstimate.java
@@ -93,11 +93,6 @@ public class SymbolStatsEstimate
         return highValue;
     }
 
-    public boolean isRangeEmpty()
-    {
-        return isNaN(lowValue) && isNaN(highValue);
-    }
-
     @JsonProperty
     public double getNullsFraction()
     {
@@ -124,16 +119,6 @@ public class SymbolStatsEstimate
     public double getDistinctValuesCount()
     {
         return distinctValuesCount;
-    }
-
-    public SymbolStatsEstimate mapLowValue(Function<Double, Double> mappingFunction)
-    {
-        return buildFrom(this).setLowValue(mappingFunction.apply(lowValue)).build();
-    }
-
-    public SymbolStatsEstimate mapHighValue(Function<Double, Double> mappingFunction)
-    {
-        return buildFrom(this).setHighValue(mappingFunction.apply(highValue)).build();
     }
 
     public SymbolStatsEstimate mapNullsFraction(Function<Double, Double> mappingFunction)
