b5fb41f3687c1ea34cdaf29afc10fac2887e0003
Andrii Rosa <andriirosa@fb.com>
2018-11-06 10:06:21 -0500

    Ignore failures in stats and cost calculator
    Currently stats and costs are pre-computed for every query.
    There are still some bugs in stats calculator that are being
    triggered in very rare cases that are incredibly hard to catch.
    Given that the stats calculator has to be run for every query, it
    is better to be fail safe. As a failure in stats calculation must
    not result in failing query.

--- presto-main/src/main/java/com/facebook/presto/cost/CachingCostProvider.java
+++ presto-main/src/main/java/com/facebook/presto/cost/CachingCostProvider.java
@@ -18,18 +18,22 @@ import com.facebook.presto.sql.planner.TypeProvider;
 import com.facebook.presto.sql.planner.iterative.GroupReference;
 import com.facebook.presto.sql.planner.iterative.Memo;
 import com.facebook.presto.sql.planner.plan.PlanNode;
+import io.airlift.log.Logger;
 
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Optional;
 
 import static com.facebook.presto.SystemSessionProperties.isEnableStatsCalculator;
+import static com.facebook.presto.SystemSessionProperties.isIgnoreStatsCalculatorFailures;
 import static com.google.common.base.Verify.verify;
 import static java.util.Objects.requireNonNull;
 
 public class CachingCostProvider
         implements CostProvider
 {
+    private static final Logger log = Logger.get(CachingCostProvider.class);
+
     private final CostCalculator costCalculator;
     private final StatsProvider statsProvider;
     private final Optional<Memo> memo;
@@ -61,18 +65,27 @@ public class CachingCostProvider
 
         requireNonNull(node, "node is null");
 
-        if (node instanceof GroupReference) {
-            return getGroupCost((GroupReference) node);
-        }
+        try {
+            if (node instanceof GroupReference) {
+                return getGroupCost((GroupReference) node);
+            }
 
-        PlanNodeCostEstimate cumulativeCost = cache.get(node);
-        if (cumulativeCost != null) {
+            PlanNodeCostEstimate cumulativeCost = cache.get(node);
+            if (cumulativeCost != null) {
+                return cumulativeCost;
+            }
+
+            cumulativeCost = calculateCumulativeCost(node);
+            verify(cache.put(node, cumulativeCost) == null, "Cost already set");
             return cumulativeCost;
         }
-
-        cumulativeCost = calculateCumulativeCost(node);
-        verify(cache.put(node, cumulativeCost) == null, "Cost already set");
-        return cumulativeCost;
+        catch (RuntimeException e) {
+            if (isIgnoreStatsCalculatorFailures(session)) {
+                log.error(e, "Error occurred when computing cost for query %s", session.getQueryId());
+                return PlanNodeCostEstimate.unknown();
+            }
+            throw e;
+        }
     }
 
     private PlanNodeCostEstimate getGroupCost(GroupReference groupReference)
