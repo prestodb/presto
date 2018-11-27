766ed564fc8e12a2566059c3dbc931e07f432492
Ying Su <yingsu@fb.com>
2018-11-26 17:08:18 -0800

    Change ARRAY_INTERSECT to use TypedSet
    This commit improves the performance comparing to the original sort-merge
    solution. The gain is from 2.3x to 11x for different data types.
    
    before
    Benchmark                               (arraySizeString)   (type)  Mode  Cnt        Score        Error  Units
    BenchmarkArrayIntersect.arrayIntersect                 10   BIGINT  avgt   20      816.963 ±     19.150  ns/op
    BenchmarkArrayIntersect.arrayIntersect                 10  VARCHAR  avgt   20     1437.385 ±     32.980  ns/op
    BenchmarkArrayIntersect.arrayIntersect                 10   DOUBLE  avgt   20      741.172 ±     13.893  ns/op
    BenchmarkArrayIntersect.arrayIntersect                 10  BOOLEAN  avgt   20      451.989 ±     12.216  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100   BIGINT  avgt   20    15009.721 ±    576.803  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100  VARCHAR  avgt   20    30356.848 ±    607.700  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100   DOUBLE  avgt   20    14211.824 ±    269.999  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100  BOOLEAN  avgt   20     2698.627 ±    152.828  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000   BIGINT  avgt   20   192445.860 ±   2278.683  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000  VARCHAR  avgt   20   321142.819 ±   7050.989  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000   DOUBLE  avgt   20   203028.772 ±   4898.172  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000  BOOLEAN  avgt   20    22854.985 ±    445.286  ns/op
    
    After
    Without type specialization:
    Benchmark                               (arraySizeString)   (type)  Mode  Cnt       Score      Error  Units
    BenchmarkArrayIntersect.arrayIntersect                 10   BIGINT  avgt   20     665.252 ±   12.076  ns/op
    BenchmarkArrayIntersect.arrayIntersect                 10  VARCHAR  avgt   20    1423.470 ±   47.962  ns/op
    BenchmarkArrayIntersect.arrayIntersect                 10   DOUBLE  avgt   20     706.942 ±   17.706  ns/op
    BenchmarkArrayIntersect.arrayIntersect                 10  BOOLEAN  avgt   20     479.239 ±   11.055  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100   BIGINT  avgt   20    5064.025 ±   97.739  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100  VARCHAR  avgt   20   12172.304 ±  189.358  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100   DOUBLE  avgt   20    5210.446 ±  124.367  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100  BOOLEAN  avgt   20    3406.959 ±   78.486  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000   BIGINT  avgt   20   46661.584 ± 1638.139  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000  VARCHAR  avgt   20  127533.382 ± 4376.716  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000   DOUBLE  avgt   20   53730.848 ± 3782.510  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000  BOOLEAN  avgt   20   26564.037 ±  713.965  ns/op
    
    With type specializations
    Benchmark                               (arraySizeString)   (type)  Mode  Cnt      Score      Error  Units
    BenchmarkArrayIntersect.arrayIntersect                 10   BIGINT  avgt   20    529.935 ±   17.827  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100   BIGINT  avgt   20   3602.600 ±   96.697  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000   BIGINT  avgt   20  34822.319 ± 1018.570  ns/op
    
    BenchmarkArrayIntersect.arrayIntersect                 10  BOOLEAN  avgt   20    63.201 ±  1.498  ns/op
    BenchmarkArrayIntersect.arrayIntersect                100  BOOLEAN  avgt   20   170.389 ±  8.854  ns/op
    BenchmarkArrayIntersect.arrayIntersect               1000  BOOLEAN  avgt   20  1098.351 ± 24.970  ns/op

--- presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestTypedSet.java
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestTypedSet.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.operator.aggregation;
 
+import com.facebook.presto.spi.PageBuilder;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
@@ -33,6 +34,7 @@ import static io.airlift.slice.Slices.utf8Slice;
 import static java.util.Collections.nCopies;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
 public class TestTypedSet
@@ -67,40 +69,134 @@ public class TestTypedSet
     public void testGetElementPosition()
     {
         int elementCount = 100;
-        TypedSet typedSet = new TypedSet(BIGINT, elementCount, FUNCTION_NAME);
+        // Set initialTypedSetEntryCount to a small number to trigger rehash()
+        int initialTypedSetEntryCount = 10;
+        TypedSet typedSet = new TypedSet(BIGINT, initialTypedSetEntryCount, FUNCTION_NAME);
         BlockBuilder blockBuilder = BIGINT.createFixedSizeBlockBuilder(elementCount);
         for (int i = 0; i < elementCount; i++) {
             BIGINT.writeLong(blockBuilder, i);
             typedSet.add(blockBuilder, i);
         }
+
+        assertEquals(typedSet.size(), elementCount);
+
         for (int j = 0; j < blockBuilder.getPositionCount(); j++) {
             assertEquals(typedSet.positionOf(blockBuilder, j), j);
         }
     }
 
     @Test
-    public void testGetElementPositionRandom()
+    public void testGetElementPositionWithNull()
     {
-        BlockBuilder keys = VARCHAR.createBlockBuilder(null, 5);
-        VARCHAR.writeSlice(keys, utf8Slice("hello"));
-        VARCHAR.writeSlice(keys, utf8Slice("bye"));
-        VARCHAR.writeSlice(keys, utf8Slice("abc"));
-
-        TypedSet set = new TypedSet(VARCHAR, keys.getPositionCount(), FUNCTION_NAME);
-        for (int i = 0; i < keys.getPositionCount(); i++) {
-            set.add(keys, i);
+        int elementCount = 100;
+        // Set initialTypedSetEntryCount to a small number to trigger rehash()
+        int initialTypedSetEntryCount = 10;
+        TypedSet typedSet = new TypedSet(BIGINT, initialTypedSetEntryCount, FUNCTION_NAME);
+        BlockBuilder blockBuilder = BIGINT.createFixedSizeBlockBuilder(elementCount);
+        for (int i = 0; i < elementCount; i++) {
+            if (i % 10 == 0) {
+                blockBuilder.appendNull();
+            }
+            else {
+                BIGINT.writeLong(blockBuilder, i);
+            }
+            typedSet.add(blockBuilder, i);
         }
 
-        BlockBuilder values = VARCHAR.createBlockBuilder(null, 5);
-        VARCHAR.writeSlice(values, utf8Slice("bye"));
-        VARCHAR.writeSlice(values, utf8Slice("abc"));
-        VARCHAR.writeSlice(values, utf8Slice("hello"));
-        VARCHAR.writeSlice(values, utf8Slice("bad"));
+        // The internal elementBlock and hashtable of the typedSet should contain
+        // all distinct non-null elements plus one null
+        assertEquals(typedSet.size(), elementCount - elementCount / 10 + 1);
 
-        assertEquals(set.positionOf(values, 2), 0);
-        assertEquals(set.positionOf(values, 1), 2);
-        assertEquals(set.positionOf(values, 0), 1);
-        assertFalse(set.contains(values, 3));
+        int nullCount = 0;
+        for (int j = 0; j < blockBuilder.getPositionCount(); j++) {
+            // The null is only added to typedSet once, so the internal elementBlock subscript is shifted by nullCountMinusOne
+            if (!blockBuilder.isNull(j)) {
+                assertEquals(typedSet.positionOf(blockBuilder, j), j - nullCount + 1);
+            }
+            else {
+                // The first null added to typedSet is at position 0
+                assertEquals(typedSet.positionOf(blockBuilder, j), 0);
+                nullCount++;
+            }
+        }
+    }
+
+    @Test
+    public void testGetElementPositionWithProvidedEmptyBlockBuilder()
+    {
+        int elementCount = 100;
+        // Set initialTypedSetEntryCount to a small number to trigger rehash()
+        int initialTypedSetEntryCount = 10;
+
+        BlockBuilder emptyBlockBuilder = BIGINT.createFixedSizeBlockBuilder(elementCount);
+        TypedSet typedSet = new TypedSet(BIGINT, emptyBlockBuilder, initialTypedSetEntryCount, FUNCTION_NAME);
+        BlockBuilder externalBlockBuilder = BIGINT.createFixedSizeBlockBuilder(elementCount);
+        for (int i = 0; i < elementCount; i++) {
+            if (i % 10 == 0) {
+                externalBlockBuilder.appendNull();
+            }
+            else {
+                BIGINT.writeLong(externalBlockBuilder, i);
+            }
+            typedSet.add(externalBlockBuilder, i);
+        }
+
+        assertEquals(typedSet.size(), emptyBlockBuilder.getPositionCount());
+        assertEquals(typedSet.size(), elementCount - elementCount / 10 + 1);
+
+        for (int j = 0; j < typedSet.size(); j++) {
+            assertEquals(typedSet.positionOf(emptyBlockBuilder, j), j);
+        }
+    }
+
+    @Test
+    public void testGetElementPositionWithProvidedNonEmptyBlockBuilder()
+    {
+        int elementCount = 100;
+        // Set initialTypedSetEntryCount to a small number to trigger rehash()
+        int initialTypedSetEntryCount = 10;
+
+        PageBuilder pageBuilder = new PageBuilder(ImmutableList.of(BIGINT));
+        BlockBuilder firstBlockBuilder = pageBuilder.getBlockBuilder(0);
+
+        for (int i = 0; i < elementCount; i++) {
+            BIGINT.writeLong(firstBlockBuilder, i);
+        }
+        pageBuilder.declarePositions(elementCount);
+
+        // The secondBlockBuilder should already have elementCount rows.
+        BlockBuilder secondBlockBuilder = pageBuilder.getBlockBuilder(0);
+
+        TypedSet typedSet = new TypedSet(BIGINT, secondBlockBuilder, initialTypedSetEntryCount, FUNCTION_NAME);
+        BlockBuilder externalBlockBuilder = BIGINT.createFixedSizeBlockBuilder(elementCount);
+        for (int i = 0; i < elementCount; i++) {
+            if (i % 10 == 0) {
+                externalBlockBuilder.appendNull();
+            }
+            else {
+                BIGINT.writeLong(externalBlockBuilder, i);
+            }
+            typedSet.add(externalBlockBuilder, i);
+        }
+
+        assertEquals(typedSet.size(), secondBlockBuilder.getPositionCount() - elementCount);
+        assertEquals(typedSet.size(), elementCount - elementCount / 10 + 1);
+
+        for (int i = 0; i < typedSet.size(); i++) {
+            int expectedPositionInSecondBlockBuilder = i + elementCount;
+            assertEquals(typedSet.positionOf(secondBlockBuilder, expectedPositionInSecondBlockBuilder), expectedPositionInSecondBlockBuilder);
+        }
+    }
+
+    @Test
+    public void testGetElementPositionRandom()
+    {
+        TypedSet set = new TypedSet(VARCHAR, 1, FUNCTION_NAME);
+        testGetElementPositionRandomFor(set);
+
+        BlockBuilder emptyBlockBuilder = VARCHAR.createBlockBuilder(null, 3);
+        TypedSet setWithPassedInBuilder = new TypedSet(VARCHAR, emptyBlockBuilder, 1, FUNCTION_NAME);
+        testGetElementPositionRandomFor(setWithPassedInBuilder);
     }
 
     @Test
@@ -145,9 +241,46 @@ public class TestTypedSet
         }
     }
 
+    private void testGetElementPositionRandomFor(TypedSet set)
+    {
+        BlockBuilder keys = VARCHAR.createBlockBuilder(null, 5);
+        VARCHAR.writeSlice(keys, utf8Slice("hello"));
+        VARCHAR.writeSlice(keys, utf8Slice("bye"));
+        VARCHAR.writeSlice(keys, utf8Slice("abc"));
+
+        for (int i = 0; i < keys.getPositionCount(); i++) {
+            set.add(keys, i);
+        }
+
+        BlockBuilder values = VARCHAR.createBlockBuilder(null, 5);
+        VARCHAR.writeSlice(values, utf8Slice("bye"));
+        VARCHAR.writeSlice(values, utf8Slice("abc"));
+        VARCHAR.writeSlice(values, utf8Slice("hello"));
+        VARCHAR.writeSlice(values, utf8Slice("bad"));
+        values.appendNull();
+
+        assertEquals(set.positionOf(values, 4), -1);
+        assertEquals(set.positionOf(values, 2), 0);
+        assertEquals(set.positionOf(values, 1), 2);
+        assertEquals(set.positionOf(values, 0), 1);
+        assertFalse(set.contains(values, 3));
+
+        set.add(values, 4);
+        assertTrue(set.contains(values, 4));
+    }
+
     private static void testBigint(Block longBlock, int expectedSetSize)
     {
         TypedSet typedSet = new TypedSet(BIGINT, expectedSetSize, FUNCTION_NAME);
+        testBigintFor(typedSet, longBlock);
+
+        BlockBuilder emptyBlockBuilder = BIGINT.createBlockBuilder(null, expectedSetSize);
+        TypedSet typedSetWithPassedInBuilder = new TypedSet(BIGINT, emptyBlockBuilder, expectedSetSize, FUNCTION_NAME);
+        testBigintFor(typedSetWithPassedInBuilder, longBlock);
+    }
+
+    private static void testBigintFor(TypedSet typedSet, Block longBlock)
+    {
         Set<Long> set = new HashSet<>();
         for (int blockPosition = 0; blockPosition < longBlock.getPositionCount(); blockPosition++) {
             long number = BIGINT.getLong(longBlock, blockPosition);
