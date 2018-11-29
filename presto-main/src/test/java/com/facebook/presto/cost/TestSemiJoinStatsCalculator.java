dad70872c09aa162f4567263f677a49b0455be76
Andrii Rosa <andriirosa@fb.com>
2018-11-29 16:17:21 -0500

    Fix mistake in TestSemiJoinStatsCalculator

--- presto-main/src/test/java/com/facebook/presto/cost/TestSemiJoinStatsCalculator.java
+++ presto-main/src/test/java/com/facebook/presto/cost/TestSemiJoinStatsCalculator.java
@@ -204,7 +204,7 @@ public class TestSemiJoinStatsCalculator
                 .outputRowsCount(inputStatistics.getOutputRowCount() * xStats.getValuesFraction() * 0.5);
 
         // source stats are unknown
-        assertThat(computeSemiJoin(inputStatistics, inputStatistics, unknown, u))
+        assertThat(computeAntiJoin(inputStatistics, inputStatistics, unknown, u))
                 .symbolStats(unknown, stats -> stats
                         .nullsFraction(0)
                         .distinctValuesCountUnknown()
@@ -214,7 +214,7 @@ public class TestSemiJoinStatsCalculator
                 .outputRowsCountUnknown();
 
         // filtering stats are unknown
-        assertThat(computeSemiJoin(inputStatistics, inputStatistics, x, unknown))
+        assertThat(computeAntiJoin(inputStatistics, inputStatistics, x, unknown))
                 .symbolStats(x, stats -> stats
                         .nullsFraction(0)
                         .lowValue(xStats.getLowValue())
