ac91c37fd45cdffc53741d4a78d553f8484d03b6
Masha Basmanova <mbasmanova@fb.com>
2019-01-24 16:33:12 -0500

    Remove usage of deprecate Unsafe methods from ByteArrayUtils
    Usage of deprecated methods causes NoSuchMethodError when running with Java 10+.
    
    java.lang.NoSuchMethodError: sun.misc.Unsafe.getDouble(Ljava/lang/Object;I)D
            at com.facebook.presto.spi.block.ByteArrayUtils.getDouble(ByteArrayUtils.java:57)
            at com.facebook.presto.orc.reader.DoubleStreamReader.scan(DoubleStreamReader.java:264)
            at com.facebook.presto.orc.ColumnGroupReader.advance(ColumnGroupReader.java:473)
            at com.facebook.presto.orc.OrcRecordReader.getNextPage(OrcRecordReader.java:808)
            at com.facebook.presto.hive.orc.OrcPageSource.getNextPage(OrcPageSource.java:149)
            at com.facebook.presto.hive.HivePageSource.getNextPage(HivePageSource.java:226)
            at com.facebook.presto.operator.ScanFilterAndProjectOperator.processPageSource(ScanFilterAndProjectOperator.java:418)
            at com.facebook.presto.operator.ScanFilterAndProjectOperator.getOutput(ScanFilterAndProjectOperator.java:303)
            at com.facebook.presto.operator.Driver.processInternal(Driver.java:389)
            at com.facebook.presto.operator.Driver.lambda$processFor$8(Driver.java:293)
            at com.facebook.presto.operator.Driver.tryWithLock(Driver.java:685)
            at com.facebook.presto.operator.Driver.processFor(Driver.java:286)
            at com.facebook.presto.execution.SqlTaskExecution$DriverSplitRunner.processFor(SqlTaskExecution.java:1076)
            at com.facebook.presto.execution.executor.PrioritizedSplitRunner.process(PrioritizedSplitRunner.java:162)
            at com.facebook.presto.execution.executor.TaskExecutor$TaskRunner.run(TaskExecutor.java:489)
            at com.facebook.presto.$gen.Presto_0_215_117_g026543d____20190124_193910_1.run(Unknown Source)
            at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1167)
            at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:641)
            at java.base/java.lang.Thread.run(Thread.java:844)

--- presto-spi/src/main/java/com/facebook/presto/spi/block/ByteArrayUtils.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/block/ByteArrayUtils.java
@@ -13,20 +13,16 @@
  */
 package com.facebook.presto.spi.block;
 
-import java.lang.reflect.Field;
+import io.airlift.slice.ByteArrays;
 import sun.misc.Unsafe;
 
+import java.lang.reflect.Field;
+
 import static io.airlift.slice.SizeOf.SIZE_OF_LONG;
-import static sun.misc.Unsafe.ARRAY_BOOLEAN_INDEX_SCALE;
+import static java.lang.Math.min;
 import static sun.misc.Unsafe.ARRAY_BYTE_BASE_OFFSET;
-import static sun.misc.Unsafe.ARRAY_DOUBLE_INDEX_SCALE;
-import static sun.misc.Unsafe.ARRAY_FLOAT_INDEX_SCALE;
-import static sun.misc.Unsafe.ARRAY_INT_INDEX_SCALE;
 import static sun.misc.Unsafe.ARRAY_LONG_BASE_OFFSET;
 import static sun.misc.Unsafe.ARRAY_LONG_INDEX_SCALE;
-import static sun.misc.Unsafe.ARRAY_SHORT_INDEX_SCALE;
-
-import static java.lang.Math.min;
 
 
 public class ByteArrayUtils
@@ -47,30 +43,6 @@ public class ByteArrayUtils
     }
   }
 
-    public static long getLong(byte[] array, int offset)
-    {
-        return unsafe.getLong(array, ARRAY_BYTE_BASE_OFFSET + offset);
-    }
-
-        public static double getDouble(byte[] array, int offset)
-    {
-        return unsafe.getDouble(array, ARRAY_BYTE_BASE_OFFSET + offset);
-    }
-
-        public static int getInt(byte[] array, int offset)
-    {
-        return unsafe.getInt(array, ARRAY_BYTE_BASE_OFFSET + offset);
-    }
-
-    public static float getFloat(byte[] array, int offset)
-    {
-        return unsafe.getFloat(array, ARRAY_BYTE_BASE_OFFSET + offset);
-    }
-
-    public static short getShort(byte[] array, int offset)
-    {
-        return unsafe.getShort(array, ARRAY_BYTE_BASE_OFFSET + offset);
-    }
 
     public static void gather( long[] source, int[] positions, int[] rowNumberMap, int sourceOffset, byte[] target, int targetOffset, int numWords)
     {
@@ -81,13 +53,13 @@ public class ByteArrayUtils
         targetOffset += ARRAY_BYTE_BASE_OFFSET;
         if (rowNumberMap == null) {
             for (int i = sourceOffset; i < end; i++) {
-                unsafe.putLong(target,  targetOffset, source[positions[i]]);
+                ByteArrays.setLong(target,  targetOffset, source[positions[i]]);
                 targetOffset += ARRAY_LONG_INDEX_SCALE;
             }
         }
         else {
             for (int i = sourceOffset; i < end; i++) {
-                unsafe.putLong(target, targetOffset, source[rowNumberMap[positions[i]]]);
+                ByteArrays.setLong(target, targetOffset, source[rowNumberMap[positions[i]]]);
                 targetOffset += ARRAY_LONG_INDEX_SCALE;
             }
         }
