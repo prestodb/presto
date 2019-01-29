a683e76a8258a7169d3c8fdf1cd8d9a3eaacc1e1
Rebecca Schlussel <rschlussel@fb.com>
2019-01-29 10:16:32 -0500

    Add option not to delete target directory on failure
    When a metastore operation fails, especially due to timeout we don't
    know whether it ultimately completes. Cleaning up new target
    directories can cause data loss under the following set of
    circumstances.
    
    1. a query reads from and writes to the same partition
    2. the metastore alter table call times out, but eventually completes
    3. the metastore calls to undo the alter table don't succeed
    
    If the query does not read from and write to the same partition, it
    won't cause data loss but there will still be an invalid entry in the
    metastore.
    
    Therefore, we add an option not to cleanup the target directory on
    failure when the write mode is DIRECT_TO_TARGET_NEW_DIRECTORY, i.e.
    where we write the new partition directly to a new target directory.

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveClientConfig.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveClientConfig.java
@@ -127,6 +127,7 @@ public class HiveClientConfig
     private boolean hdfsWireEncryptionEnabled;
 
     private boolean skipDeletionForAlter;
+    private boolean skipTargetCleanupOnRollback;
 
     private boolean bucketExecutionEnabled = true;
     private boolean sortedWritingEnabled = true;
@@ -1010,6 +1011,19 @@ public class HiveClientConfig
     public HiveClientConfig setSkipDeletionForAlter(boolean skipDeletionForAlter)
     {
         this.skipDeletionForAlter = skipDeletionForAlter;
+        return this;
+    }
+
+    public boolean isSkipTargetCleanupOnRollback()
+    {
+        return skipTargetCleanupOnRollback;
+    }
+
+    @Config("hive.skip-target-cleanup-on-rollback")
+    @ConfigDescription("Skip deletion of target directories when a metastore operation fails and the write mode is DIRECT_TO_TARGET_NEW_DIRECTORY")
+    public HiveClientConfig setSkipTargetCleanupOnRollback(boolean skipTargetCleanupOnRollback)
+    {
+        this.skipTargetCleanupOnRollback = skipTargetCleanupOnRollback;
         return this;
     }
 
