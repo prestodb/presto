trinodb/trino

ANALYZE statement: Implement ANALYZE in Hive connector
Extracted-From: https://github.com/prestodb/presto/pull/11376

presto-hive/src/main/java/io/prestosql/plugin/hive/metastore/SemiTransactionalHiveMetastore.java
@@ -24,6 +24,7 @@ import io.airlift.concurrent.MoreFutures;
 import io.airlift.log.Logger;
 import io.prestosql.plugin.hive.HdfsEnvironment;
 import io.prestosql.plugin.hive.HdfsEnvironment.HdfsContext;
+import io.prestosql.plugin.hive.HiveBasicStatistics;
 import io.prestosql.plugin.hive.HiveType;
 import io.prestosql.plugin.hive.LocationHandle.WriteMode;
 import io.prestosql.plugin.hive.PartitionNotFoundException;
@@ -54,6 +55,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalLong;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
@@ -297,6 +299,45 @@ public class SemiTransactionalHiveMetastore
     public synchronized void renameDatabase(String source, String target)
     {
         setExclusive((delegate, hdfsEnvironment) -> delegate.renameDatabase(source, target));
+    }
+
+    // TODO: Allow updating statistics for 2 tables in the same transaction
+    public synchronized void setTableStatistics(Table table, PartitionStatistics tableStatistics)
+    {
+        setExclusive((delegate, hdfsEnvironment) ->
+                delegate.updateTableStatistics(table.getDatabaseName(), table.getTableName(), statistics -> updatePartitionStatistics(statistics, tableStatistics)));
+    }
+
+    // TODO: Allow updating statistics for 2 tables in the same transaction
+    public synchronized void setPartitionStatistics(Table table, Map<List<String>, PartitionStatistics> partitionStatisticsMap)
+    {
+        setExclusive((delegate, hdfsEnvironment) ->
+                partitionStatisticsMap.forEach((partitionValues, newPartitionStats) ->
+                        delegate.updatePartitionStatistics(
+                                table.getDatabaseName(),
+                                table.getTableName(),
+                                getPartitionName(table, partitionValues),
+                                oldPartitionStats -> updatePartitionStatistics(oldPartitionStats, newPartitionStats))));
+    }
+
+    // For HiveBasicStatistics, we only overwrite the original statistics if the new one is not empty.
+    // For HiveColumnStatistics, we always overwrite every statistics.
+    // TODO: Collect file count, on-disk size and in-memory size during ANALYZE
+    private PartitionStatistics updatePartitionStatistics(PartitionStatistics oldPartitionStats, PartitionStatistics newPartitionStats)
+    {
+        HiveBasicStatistics oldBasicStatistics = oldPartitionStats.getBasicStatistics();
+        HiveBasicStatistics newBasicStatistics = newPartitionStats.getBasicStatistics();
+        HiveBasicStatistics updatedBasicStatistics = new HiveBasicStatistics(
+                firstPresent(newBasicStatistics.getFileCount(), oldBasicStatistics.getFileCount()),
+                firstPresent(newBasicStatistics.getRowCount(), oldBasicStatistics.getRowCount()),
+                firstPresent(newBasicStatistics.getInMemoryDataSizeInBytes(), oldBasicStatistics.getInMemoryDataSizeInBytes()),
+                firstPresent(newBasicStatistics.getOnDiskDataSizeInBytes(), oldBasicStatistics.getOnDiskDataSizeInBytes()));
+        return new PartitionStatistics(updatedBasicStatistics, newPartitionStats.getColumnStatistics());
+    }
+
+    private static OptionalLong firstPresent(OptionalLong first, OptionalLong second)
+    {
+        return first.isPresent() ? first : second;
     }
 
     /**
@@ -731,6 +772,11 @@ public class SemiTransactionalHiveMetastore
     {
         Table table = getTable(databaseName, tableName)
                 .orElseThrow(() -> new TableNotFoundException(new SchemaTableName(databaseName, tableName)));
+        return getPartitionName(table, partitionValues);
+    }
+
+    private String getPartitionName(Table table, List<String> partitionValues)
+    {
         List<String> columnNames = table.getPartitionColumns().stream()
                 .map(Column::getName)
                 .collect(toImmutableList());

presto-hive/src/main/java/io/prestosql/plugin/hive/util/Statistics.java
@@ -23,6 +23,7 @@ import io.prestosql.plugin.hive.metastore.DoubleStatistics;
 import io.prestosql.plugin.hive.metastore.HiveColumnStatistics;
 import io.prestosql.plugin.hive.metastore.IntegerStatistics;
 import io.prestosql.spi.Page;
+import io.prestosql.spi.PrestoException;
 import io.prestosql.spi.block.Block;
 import io.prestosql.spi.connector.ConnectorSession;
 import io.prestosql.spi.statistics.ColumnStatisticMetadata;
@@ -51,6 +52,8 @@ import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.collect.Sets.intersection;
+import static io.prestosql.plugin.hive.HiveBasicStatistics.createZeroStatistics;
+import static io.prestosql.plugin.hive.HiveErrorCode.HIVE_UNKNOWN_COLUMN_STATISTIC_TYPE;
 import static io.prestosql.plugin.hive.HiveWriteUtils.createPartitionValues;
 import static io.prestosql.plugin.hive.util.Statistics.ReduceOperator.ADD;
 import static io.prestosql.plugin.hive.util.Statistics.ReduceOperator.MAX;
@@ -70,6 +73,7 @@ import static io.prestosql.spi.type.RealType.REAL;
 import static io.prestosql.spi.type.SmallintType.SMALLINT;
 import static io.prestosql.spi.type.TimestampType.TIMESTAMP;
 import static io.prestosql.spi.type.TinyintType.TINYINT;
+import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public final class Statistics
@@ -242,6 +246,67 @@ public final class Statistics
     private static <T extends Comparable<? super T>> T min(T first, T second)
     {
         return first.compareTo(second) <= 0 ? first : second;
+    }
+
+    public static PartitionStatistics createEmptyPartitionStatistics(Map<String, Type> columnTypes, Map<String, Set<ColumnStatisticType>> columnStatisticsMetadataTypes)
+    {
+        Map<String, HiveColumnStatistics> columnStatistics = columnStatisticsMetadataTypes.entrySet().stream()
+                .collect(toImmutableMap(Entry::getKey, entry -> createColumnStatisticsForEmptyPartition(columnTypes.get(entry.getKey()), entry.getValue())));
+        return new PartitionStatistics(createZeroStatistics(), columnStatistics);
+    }
+
+    private static HiveColumnStatistics createColumnStatisticsForEmptyPartition(Type columnType, Set<ColumnStatisticType> columnStatisticTypes)
+    {
+        requireNonNull(columnType, "columnType is null");
+        HiveColumnStatistics.Builder result = HiveColumnStatistics.builder();
+        for (ColumnStatisticType columnStatisticType : columnStatisticTypes) {
+            switch (columnStatisticType) {
+                case MAX_VALUE_SIZE_IN_BYTES:
+                    result.setMaxValueSizeInBytes(0);
+                    break;
+                case TOTAL_SIZE_IN_BYTES:
+                    result.setTotalSizeInBytes(0);
+                    break;
+                case NUMBER_OF_DISTINCT_VALUES:
+                    result.setDistinctValuesCount(0);
+                    break;
+                case NUMBER_OF_NON_NULL_VALUES:
+                    result.setNullsCount(0);
+                    break;
+                case NUMBER_OF_TRUE_VALUES:
+                    result.setBooleanStatistics(new BooleanStatistics(OptionalLong.of(0L), OptionalLong.of(0L)));
+                    break;
+                case MIN_VALUE:
+                case MAX_VALUE:
+                    setMinMaxForEmptyPartition(columnType, result);
+                    break;
+                default:
+                    throw new PrestoException(HIVE_UNKNOWN_COLUMN_STATISTIC_TYPE, "Unknown column statistics type: " + columnStatisticType.name());
+            }
+        }
+        return result.build();
+    }
+
+    private static void setMinMaxForEmptyPartition(Type type, HiveColumnStatistics.Builder result)
+    {
+        if (type.equals(BIGINT) || type.equals(INTEGER) || type.equals(SMALLINT) || type.equals(TINYINT)) {
+            result.setIntegerStatistics(new IntegerStatistics(OptionalLong.empty(), OptionalLong.empty()));
+        }
+        else if (type.equals(DOUBLE) || type.equals(REAL)) {
+            result.setDoubleStatistics(new DoubleStatistics(OptionalDouble.empty(), OptionalDouble.empty()));
+        }
+        else if (type.equals(DATE)) {
+            result.setDateStatistics(new DateStatistics(Optional.empty(), Optional.empty()));
+        }
+        else if (type.equals(TIMESTAMP)) {
+            result.setIntegerStatistics(new IntegerStatistics(OptionalLong.empty(), OptionalLong.empty()));
+        }
+        else if (type instanceof DecimalType) {
+            result.setDecimalStatistics(new DecimalStatistics(Optional.empty(), Optional.empty()));
+        }
+        else {
+            throw new IllegalArgumentException("Unexpected type: " + type);
+        }
     }
 
     public static Map<List<String>, ComputedStatistics> createComputedStatisticsToPartitionMap(

prestodb/presto

presto-hive/src/main/java/com/facebook/presto/hive/metastore/SemiTransactionalHiveMetastore.java
@@ -15,6 +15,7 @@ package com.facebook.presto.hive.metastore;
 
 import com.facebook.presto.hive.HdfsEnvironment;
 import com.facebook.presto.hive.HdfsEnvironment.HdfsContext;
+import com.facebook.presto.hive.HiveBasicStatistics;
 import com.facebook.presto.hive.HiveType;
 import com.facebook.presto.hive.LocationHandle.WriteMode;
 import com.facebook.presto.hive.PartitionNotFoundException;
@@ -51,6 +52,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalLong;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
@@ -293,6 +295,45 @@ public class SemiTransactionalHiveMetastore
     public synchronized void renameDatabase(String source, String target)
     {
         setExclusive((delegate, hdfsEnvironment) -> delegate.renameDatabase(source, target));
+    }
+
+    // TODO: Allow updating statistics for 2 tables in the same transaction
+    public synchronized void setTableStatistics(Table table, PartitionStatistics tableStatistics)
+    {
+        setExclusive((delegate, hdfsEnvironment) ->
+                delegate.updateTableStatistics(table.getDatabaseName(), table.getTableName(), statistics -> updatePartitionStatistics(statistics, tableStatistics)));
+    }
+
+    // TODO: Allow updating statistics for 2 tables in the same transaction
+    public synchronized void setPartitionStatistics(Table table, Map<List<String>, PartitionStatistics> partitionStatisticsMap)
+    {
+        setExclusive((delegate, hdfsEnvironment) ->
+                partitionStatisticsMap.forEach((partitionValues, newPartitionStats) ->
+                        delegate.updatePartitionStatistics(
+                                table.getDatabaseName(),
+                                table.getTableName(),
+                                getPartitionName(table, partitionValues),
+                                oldPartitionStats -> updatePartitionStatistics(oldPartitionStats, newPartitionStats))));
+    }
+
+    // For HiveBasicStatistics, we only overwrite the original statistics if the new one is not empty.
+    // For HiveColumnStatistics, we always overwrite every statistics.
+    // TODO: Collect file count, on-disk size and in-memory size during ANALYZE
+    private PartitionStatistics updatePartitionStatistics(PartitionStatistics oldPartitionStats, PartitionStatistics newPartitionStats)
+    {
+        HiveBasicStatistics oldBasicStatistics = oldPartitionStats.getBasicStatistics();
+        HiveBasicStatistics newBasicStatistics = newPartitionStats.getBasicStatistics();
+        HiveBasicStatistics updatedBasicStatistics = new HiveBasicStatistics(
+                firstPresent(newBasicStatistics.getFileCount(), oldBasicStatistics.getFileCount()),
+                firstPresent(newBasicStatistics.getRowCount(), oldBasicStatistics.getRowCount()),
+                firstPresent(newBasicStatistics.getInMemoryDataSizeInBytes(), oldBasicStatistics.getInMemoryDataSizeInBytes()),
+                firstPresent(newBasicStatistics.getOnDiskDataSizeInBytes(), oldBasicStatistics.getOnDiskDataSizeInBytes()));
+        return new PartitionStatistics(updatedBasicStatistics, newPartitionStats.getColumnStatistics());
+    }
+
+    private static OptionalLong firstPresent(OptionalLong first, OptionalLong second)
+    {
+        return first.isPresent() ? first : second;
     }
 
     /**
@@ -727,6 +768,11 @@ public class SemiTransactionalHiveMetastore
     {
         Table table = getTable(databaseName, tableName)
                 .orElseThrow(() -> new TableNotFoundException(new SchemaTableName(databaseName, tableName)));
+        return getPartitionName(table, partitionValues);
+    }
+
+    private String getPartitionName(Table table, List<String> partitionValues)
+    {
         List<String> columnNames = table.getPartitionColumns().stream()
                 .map(Column::getName)
                 .collect(toImmutableList());

presto-hive/src/main/java/com/facebook/presto/hive/util/Statistics.java
@@ -23,6 +23,7 @@ import com.facebook.presto.hive.metastore.HiveColumnStatistics;
 import com.facebook.presto.hive.metastore.IntegerStatistics;
 import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.statistics.ColumnStatisticMetadata;
 import com.facebook.presto.spi.statistics.ColumnStatisticType;
@@ -46,6 +47,8 @@ import java.util.OptionalDouble;
 import java.util.OptionalLong;
 import java.util.Set;
 
+import static com.facebook.presto.hive.HiveBasicStatistics.createZeroStatistics;
+import static com.facebook.presto.hive.HiveErrorCode.HIVE_UNKNOWN_COLUMN_STATISTIC_TYPE;
 import static com.facebook.presto.hive.HiveWriteUtils.createPartitionValues;
 import static com.facebook.presto.hive.util.Statistics.ReduceOperator.ADD;
 import static com.facebook.presto.hive.util.Statistics.ReduceOperator.MAX;
@@ -70,6 +73,7 @@ import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.collect.Sets.intersection;
+import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public final class Statistics
@@ -242,6 +246,67 @@ public final class Statistics
     private static <T extends Comparable<? super T>> T min(T first, T second)
     {
         return first.compareTo(second) <= 0 ? first : second;
+    }
+
+    public static PartitionStatistics createEmptyPartitionStatistics(Map<String, Type> columnTypes, Map<String, Set<ColumnStatisticType>> columnStatisticsMetadataTypes)
+    {
+        Map<String, HiveColumnStatistics> columnStatistics = columnStatisticsMetadataTypes.entrySet().stream()
+                .collect(toImmutableMap(Entry::getKey, entry -> createColumnStatisticsForEmptyPartition(columnTypes.get(entry.getKey()), entry.getValue())));
+        return new PartitionStatistics(createZeroStatistics(), columnStatistics);
+    }
+
+    private static HiveColumnStatistics createColumnStatisticsForEmptyPartition(Type columnType, Set<ColumnStatisticType> columnStatisticTypes)
+    {
+        requireNonNull(columnType, "columnType is null");
+        HiveColumnStatistics.Builder result = HiveColumnStatistics.builder();
+        for (ColumnStatisticType columnStatisticType : columnStatisticTypes) {
+            switch (columnStatisticType) {
+                case MAX_VALUE_SIZE_IN_BYTES:
+                    result.setMaxValueSizeInBytes(0);
+                    break;
+                case TOTAL_SIZE_IN_BYTES:
+                    result.setTotalSizeInBytes(0);
+                    break;
+                case NUMBER_OF_DISTINCT_VALUES:
+                    result.setDistinctValuesCount(0);
+                    break;
+                case NUMBER_OF_NON_NULL_VALUES:
+                    result.setNullsCount(0);
+                    break;
+                case NUMBER_OF_TRUE_VALUES:
+                    result.setBooleanStatistics(new BooleanStatistics(OptionalLong.of(0L), OptionalLong.of(0L)));
+                    break;
+                case MIN_VALUE:
+                case MAX_VALUE:
+                    setMinMaxForEmptyPartition(columnType, result);
+                    break;
+                default:
+                    throw new PrestoException(HIVE_UNKNOWN_COLUMN_STATISTIC_TYPE, "Unknown column statistics type: " + columnStatisticType.name());
+            }
+        }
+        return result.build();
+    }
+
+    private static void setMinMaxForEmptyPartition(Type type, HiveColumnStatistics.Builder result)
+    {
+        if (type.equals(BIGINT) || type.equals(INTEGER) || type.equals(SMALLINT) || type.equals(TINYINT)) {
+            result.setIntegerStatistics(new IntegerStatistics(OptionalLong.empty(), OptionalLong.empty()));
+        }
+        else if (type.equals(DOUBLE) || type.equals(REAL)) {
+            result.setDoubleStatistics(new DoubleStatistics(OptionalDouble.empty(), OptionalDouble.empty()));
+        }
+        else if (type.equals(DATE)) {
+            result.setDateStatistics(new DateStatistics(Optional.empty(), Optional.empty()));
+        }
+        else if (type.equals(TIMESTAMP)) {
+            result.setIntegerStatistics(new IntegerStatistics(OptionalLong.empty(), OptionalLong.empty()));
+        }
+        else if (type instanceof DecimalType) {
+            result.setDecimalStatistics(new DecimalStatistics(Optional.empty(), Optional.empty()));
+        }
+        else {
+            throw new IllegalArgumentException("Unexpected type: " + type);
+        }
     }
 
     public static Map<List<String>, ComputedStatistics> createComputedStatisticsToPartitionMap(