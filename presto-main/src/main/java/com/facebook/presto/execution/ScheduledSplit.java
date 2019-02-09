44b468630d99d64574393bcdfa7aee2d1401c7e0
David Phillips <david@acz.org>
2019-02-09 13:01:36 -0800

    Move ScheduledSplit to execution package
    Extracted-From: https://github.com/prestosql/presto/commit/02711a863a08607303077e2f55fb7af7c1ac8baf

--- presto-main/src/main/java/com/facebook/presto/execution/ScheduledSplit.java
+++ presto-main/src/main/java/com/facebook/presto/execution/ScheduledSplit.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto;
+package com.facebook.presto.execution;
 
 import com.facebook.presto.metadata.Split;
 import com.facebook.presto.sql.planner.plan.PlanNodeId;
--- presto-main/src/test/java/com/facebook/presto/execution/TestSqlTaskManager.java
+++ presto-main/src/test/java/com/facebook/presto/execution/TestSqlTaskManager.java
@@ -13,7 +13,6 @@
  */
 package com.facebook.presto.execution;
 
-import com.facebook.presto.ScheduledSplit;
 import com.facebook.presto.TaskSource;
 import com.facebook.presto.execution.buffer.BufferResult;
 import com.facebook.presto.execution.buffer.BufferState;
