8253e3ba56e09859da2378b2f2eecfb9be166e6c
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2019-02-26 19:39:08 -0500

    Add grantor_type and grantee_type columns to table_privileges

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveMetadata.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveMetadata.java
@@ -180,7 +180,7 @@ import static com.facebook.presto.hive.metastore.MetastoreUtil.getProtectMode;
 import static com.facebook.presto.hive.metastore.MetastoreUtil.verifyOnline;
 import static com.facebook.presto.hive.metastore.StorageFormat.VIEW_STORAGE_FORMAT;
 import static com.facebook.presto.hive.metastore.StorageFormat.fromHiveStorageFormat;
-import static com.facebook.presto.hive.metastore.thrift.ThriftMetastoreUtil.listEnabledTablePrivileges;
+import static com.facebook.presto.hive.metastore.thrift.ThriftMetastoreUtil.listEnabledPrincipals;
 import static com.facebook.presto.hive.util.ConfigurationUtils.toJobConf;
 import static com.facebook.presto.hive.util.Statistics.ReduceOperator.ADD;
 import static com.facebook.presto.hive.util.Statistics.createComputedStatisticsToPartitionMap;
@@ -1989,23 +1989,26 @@ public class HiveMetadata
     @Override
     public List<GrantInfo> listTablePrivileges(ConnectorSession session, SchemaTablePrefix schemaTablePrefix)
     {
-        ImmutableList.Builder<GrantInfo> grantInfos = ImmutableList.builder();
+        Set<PrestoPrincipal> principals = listEnabledPrincipals(metastore, session.getIdentity());
+        ImmutableList.Builder<GrantInfo> result = ImmutableList.builder();
         for (SchemaTableName tableName : listTables(session, schemaTablePrefix)) {
-            Set<PrivilegeInfo> privileges =
-                    listEnabledTablePrivileges(metastore, tableName.getSchemaName(), tableName.getTableName(), session.getIdentity()).stream()
-                            .map(HivePrivilegeInfo::toPrivilegeInfo)
-                            .flatMap(Set::stream)
-                            .collect(toImmutableSet());
-
-            grantInfos.add(
-                    new GrantInfo(
-                            privileges,
-                            session.getIdentity(),
-                            tableName,
-                            Optional.empty(), // Can't access grantor
-                            Optional.empty())); // Can't access withHierarchy
+            for (PrestoPrincipal grantee : principals) {
+                Set<HivePrivilegeInfo> hivePrivileges = metastore.listTablePrivileges(tableName.getSchemaName(), tableName.getTableName(), grantee);
+                for (HivePrivilegeInfo hivePrivilege : hivePrivileges) {
+                    Set<PrivilegeInfo> prestoPrivileges = hivePrivilege.toPrivilegeInfo();
+                    for (PrivilegeInfo prestoPrivilege : prestoPrivileges) {
+                        GrantInfo grant = new GrantInfo(
+                                prestoPrivilege,
+                                grantee,
+                                tableName,
+                                Optional.of(hivePrivilege.getGrantor()),
+                                Optional.empty());
+                        result.add(grant);
+                    }
+                }
+            }
         }
-        return grantInfos.build();
+        return result.build();
     }
 
     private void verifyJvmTimeZone()
--- presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestGrantRevoke.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestGrantRevoke.java
@@ -14,12 +14,15 @@
 package com.facebook.presto.tests.hive;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import io.airlift.log.Logger;
 import io.prestodb.tempto.AfterTestWithContext;
 import io.prestodb.tempto.BeforeTestWithContext;
 import io.prestodb.tempto.ProductTest;
 import io.prestodb.tempto.query.QueryExecutor;
 import org.testng.annotations.Test;
+
+import java.util.Set;
 
 import static com.facebook.presto.tests.TestGroups.AUTHORIZATION;
 import static com.facebook.presto.tests.TestGroups.PROFILE_SPECIFIC_TESTS;
@@ -32,10 +35,13 @@ import static io.prestodb.tempto.assertions.QueryAssert.assertThat;
 import static io.prestodb.tempto.context.ContextDsl.executeWith;
 import static io.prestodb.tempto.sql.SqlContexts.createViewAs;
 import static java.lang.String.format;
+import static java.util.stream.Collectors.toSet;
 
 public class TestGrantRevoke
         extends ProductTest
 {
+    private static final Set<String> PREDEFINED_ROLES = ImmutableSet.of("admin", "public");
+
     private String tableName;
     private String viewName;
     private QueryExecutor aliceExecutor;
@@ -63,6 +69,7 @@ public class TestGrantRevoke
         aliceExecutor.executeQuery(format("CREATE TABLE %s(month bigint, day bigint)", tableName));
 
         onPresto().executeQuery("SET ROLE admin");
+        onHive().executeQuery("SET ROLE admin");
         assertAccessDeniedOnAllOperationsOnTable(bobExecutor, tableName);
     }
 
@@ -72,10 +79,31 @@ public class TestGrantRevoke
         try {
             aliceExecutor.executeQuery(format("DROP TABLE IF EXISTS %s", tableName));
             aliceExecutor.executeQuery(format("DROP VIEW IF EXISTS %s", viewName));
+            cleanupRoles();
         }
         catch (Exception e) {
             Logger.get(getClass()).warn(e, "failed to drop table/view");
         }
+    }
+
+    @Test(groups = {AUTHORIZATION, PROFILE_SPECIFIC_TESTS})
+    private void cleanupRoles()
+    {
+        for (String role : listRoles()) {
+            if (!PREDEFINED_ROLES.contains(role)) {
+                onHive().executeQuery(format("DROP ROLE %s", role));
+            }
+        }
+    }
+
+    private Set<String> listRoles()
+    {
+        return ImmutableSet.copyOf(
+                onHive().executeQuery("SHOW ROLES")
+                        .rows()
+                        .stream()
+                        .map(row -> row.get(0).toString())
+                        .collect(toSet()));
     }
 
     @Test(groups = {AUTHORIZATION, PROFILE_SPECIFIC_TESTS})
@@ -103,20 +131,17 @@ public class TestGrantRevoke
     @Test(groups = {AUTHORIZATION, PROFILE_SPECIFIC_TESTS})
     public void testShowGrants()
     {
-        assertThat(aliceExecutor.executeQuery(format("SHOW GRANTS ON %s", tableName)))
-                .containsOnly(ImmutableList.of(
-                        row("alice", "hive", "default", tableName, "SELECT", Boolean.TRUE),
-                        row("alice", "hive", "default", tableName, "INSERT", Boolean.TRUE),
-                        row("alice", "hive", "default", tableName, "UPDATE", Boolean.TRUE),
-                        row("alice", "hive", "default", tableName, "DELETE", Boolean.TRUE)));
-
+        onPresto().executeQuery("CREATE ROLE role1");
+        onPresto().executeQuery(format("GRANT SELECT ON %s TO ROLE role1", tableName));
+        onPresto().executeQuery("GRANT role1 TO USER bob");
         aliceExecutor.executeQuery(format("GRANT SELECT ON %s TO bob WITH GRANT OPTION", tableName));
         aliceExecutor.executeQuery(format("GRANT INSERT ON %s TO bob", tableName));
 
         assertThat(bobExecutor.executeQuery(format("SHOW GRANTS ON %s", tableName)))
                 .containsOnly(ImmutableList.of(
-                        row("bob", "hive", "default", tableName, "SELECT", Boolean.TRUE),
-                        row("bob", "hive", "default", tableName, "INSERT", Boolean.FALSE)));
+                        row("alice", "USER", "bob", "USER", "hive", "default", "alice_owned_table", "SELECT", "YES", null),
+                        row("alice", "USER", "bob", "USER", "hive", "default", "alice_owned_table", "INSERT", "NO", null),
+                        row("hdfs", "USER", "role1", "ROLE", "hive", "default", "alice_owned_table", "SELECT", "NO", null)));
     }
 
     @Test(groups = {AUTHORIZATION, PROFILE_SPECIFIC_TESTS})
@@ -156,7 +181,6 @@ public class TestGrantRevoke
         assertThat(() -> bobExecutor.executeQuery(format("SELECT * FROM %s", tableName)))
                 .failsWithMessage(format("Access Denied: Cannot select from table default.%s", tableName));
         assertThat(aliceExecutor.executeQuery(format("SELECT * FROM %s", tableName))).hasNoRows();
-        onPresto().executeQuery("DROP ROLE role1");
     }
 
     @Test(groups = {AUTHORIZATION, PROFILE_SPECIFIC_TESTS})
@@ -172,8 +196,6 @@ public class TestGrantRevoke
         assertThat(() -> bobExecutor.executeQuery(format("SELECT * FROM %s", tableName)))
                 .failsWithMessage(format("Access Denied: Cannot select from table default.%s", tableName));
         assertThat(aliceExecutor.executeQuery(format("SELECT * FROM %s", tableName))).hasNoRows();
-        onPresto().executeQuery("DROP ROLE role1");
-        onPresto().executeQuery("DROP ROLE role2");
     }
 
     @Test(groups = {AUTHORIZATION, PROFILE_SPECIFIC_TESTS})
