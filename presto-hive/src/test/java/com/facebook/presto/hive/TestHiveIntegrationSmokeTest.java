3e7efe114f3f832592978949cb81a714e3826f07
Masha Basmanova <mbasmanova@fb.com>
2019-03-20 13:26:46 -0400

    Evaluate complex filters on partition columns

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
@@ -3818,6 +3818,10 @@ public class TestHiveIntegrationSmokeTest
                 "SELECT * FROM test_prune_failure\n" +
                 "WHERE x < 0 AND cast(p AS int) > 0");
 
+        assertQueryFails("SELECT * FROM test_prune_failure " +
+                "WHERE x > 0 AND cast(p AS int) > 0",
+                "Cannot cast 'abc' to INT");
+
         assertUpdate("DROP TABLE test_prune_failure");
     }
 
