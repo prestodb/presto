ff524d93ebb5803487444c4577beeb50c7f44f58
Dain Sundstrom <dain@iq80.com>
2019-04-02 15:54:22 -0700

    Deprecate Node.getHttpUri and reduce usages of Node.getHostAndPort
    Extracted-From: https://github.com/prestosql/presto

--- presto-main/src/main/java/com/facebook/presto/memory/ClusterMemoryManager.java
+++ presto-main/src/main/java/com/facebook/presto/memory/ClusterMemoryManager.java
@@ -551,7 +551,7 @@ public class ClusterMemoryManager
         Map<String, Optional<MemoryInfo>> memoryInfo = new HashMap<>();
         for (Entry<String, RemoteNodeMemory> entry : nodes.entrySet()) {
             // workerId is of the form "node_identifier [node_host]"
-            String workerId = entry.getKey() + " [" + entry.getValue().getNode().getHostAndPort().getHostText() + "]";
+            String workerId = entry.getKey() + " [" + entry.getValue().getNode().getHost() + "]";
             memoryInfo.put(workerId, entry.getValue().getInfo());
         }
         return memoryInfo;
