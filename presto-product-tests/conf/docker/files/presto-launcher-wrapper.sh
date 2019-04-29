84e50df77024b95f37de1829c2f3f7a18fb7ff2f
Piotr Findeisen <piotr.findeisen@gmail.com>
2019-04-29 16:00:26 +0200

    Code cleanup

--- presto-product-tests/conf/docker/files/presto-launcher-wrapper.sh
+++ presto-product-tests/conf/docker/files/presto-launcher-wrapper.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 
 CONFIG="$1"
 
+shift 1
+
 PRESTO_CONFIG_DIRECTORY="/docker/volumes/conf/presto/etc"
 CONFIG_PROPERTIES_LOCATION="${PRESTO_CONFIG_DIRECTORY}/${CONFIG}.properties"
 
-if [[ ! -e ${CONFIG_PROPERTIES_LOCATION} ]]; then
-   echo "${CONFIG_PROPERTIES_LOCATION} does not exist"
+if [[ ! -f "${CONFIG_PROPERTIES_LOCATION}" ]]; then
+   echo "${CONFIG_PROPERTIES_LOCATION} does not exist" >&2
    exit 1
 fi
-
-shift 1
 
 /docker/volumes/presto-server/bin/launcher \
   -Dnode.id="${HOSTNAME}" \
