1568cfaf75001a0e1e1b32e063eca838332d2f65
Dain Sundstrom <dain@iq80.com>
2019-05-12 14:20:40 -0700

    Add LocalCoordinatorLocation

--- presto-main/src/main/java/com/facebook/presto/dispatcher/QueuedStatementResource.java
+++ presto-main/src/main/java/com/facebook/presto/dispatcher/QueuedStatementResource.java
@@ -404,7 +404,7 @@ public class QueuedStatementResource
 
         private URI getRedirectUri(CoordinatorLocation coordinatorLocation, UriInfo uriInfo, String xForwardedProto)
         {
-            URI coordinatorUri = coordinatorLocation.getUri(getScheme(xForwardedProto, uriInfo));
+            URI coordinatorUri = coordinatorLocation.getUri(uriInfo, xForwardedProto);
             return uriBuilderFrom(coordinatorUri)
                     .appendPath("/v1/statement/executing")
                     .appendPath(queryId.toString())
