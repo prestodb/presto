bfa2524bae27cf40a021f51922d70af05dca27ee
Dain Sundstrom <dain@iq80.com>
2019-05-13 11:23:02 -0700

    Remove system startup minimum worker requirement
    The normal minimum worker requirement applied to all queries is
    sufficient to cover this case.

--- presto-tests/src/test/java/com/facebook/presto/tests/TestMinWorkerRequirement.java
+++ presto-tests/src/test/java/com/facebook/presto/tests/TestMinWorkerRequirement.java
@@ -24,67 +24,6 @@ import static org.testng.Assert.fail;
 @Test(singleThreaded = true)
 public class TestMinWorkerRequirement
 {
-    @Test(expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "Cluster is still initializing, there are insufficient active worker nodes \\(4\\) to run query")
-    public void testInsufficientInitialWorkerNodes()
-            throws Exception
-    {
-        try (DistributedQueryRunner queryRunner = TpchQueryRunnerBuilder.builder()
-                .setSingleCoordinatorProperty("query-manager.initialization-required-workers", "5")
-                .setNodeCount(4)
-                .build()) {
-            queryRunner.execute("SELECT 1");
-            fail("Expected exception due to insufficient active worker nodes");
-        }
-    }
-
-    @Test(expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "Cluster is still initializing, there are insufficient active worker nodes \\(3\\) to run query")
-    public void testInsufficientInitialWorkerNodesWithCoordinatorExcluded()
-            throws Exception
-    {
-        try (DistributedQueryRunner queryRunner = TpchQueryRunnerBuilder.builder()
-                .setSingleExtraProperty("node-scheduler.include-coordinator", "false")
-                .setSingleCoordinatorProperty("query-manager.initialization-required-workers", "4")
-                .setNodeCount(4)
-                .build()) {
-            queryRunner.execute("SELECT 1");
-            fail("Expected exception due to insufficient active worker nodes");
-        }
-    }
-
-    @Test
-    public void testSufficientInitialWorkerNodes()
-            throws Exception
-    {
-        try (DistributedQueryRunner queryRunner = TpchQueryRunnerBuilder.builder()
-                .setSingleCoordinatorProperty("query-manager.initialization-required-workers", "4")
-                .setNodeCount(4)
-                .build()) {
-            queryRunner.execute("SELECT 1");
-            assertEquals(queryRunner.getCoordinator().refreshNodes().getActiveNodes().size(), 4);
-
-            // Query should still be allowed to run if active workers drop down below the minimum required nodes
-            queryRunner.getServers().get(0).close();
-            assertEquals(queryRunner.getCoordinator().refreshNodes().getActiveNodes().size(), 3);
-            queryRunner.execute("SELECT 1");
-        }
-    }
-
-    @Test
-    public void testInitializationTimeout()
-            throws Exception
-    {
-        try (DistributedQueryRunner queryRunner = TpchQueryRunnerBuilder.builder()
-                .setCoordinatorProperties(ImmutableMap.<String, String>builder()
-                        .put("query-manager.initialization-required-workers", "5")
-                        .put("query-manager.initialization-timeout", "1ns")
-                        .build())
-                .setNodeCount(4)
-                .build()) {
-            queryRunner.execute("SELECT 1");
-            assertEquals(queryRunner.getCoordinator().refreshNodes().getActiveNodes().size(), 4);
-        }
-    }
-
     @Test(expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "Insufficient active worker nodes. Waited 1.00ns for at least 5 workers, but only 4 workers are active")
     public void testInsufficientWorkerNodes()
             throws Exception
