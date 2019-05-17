5d56e507289cf86956cc28941cae9d663f282f18
Masha Basmanova <mbasmanova@fb.com>
2019-05-17 15:51:26 -0400

    Create test bucketed tables in ORC format
    Before this change HiveQueryRunner#copyTpchTablesBucketed created
    tables in text format.

--- presto-hive/src/test/java/com/facebook/presto/hive/HiveQueryRunner.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/HiveQueryRunner.java
@@ -273,13 +273,13 @@ public final class HiveQueryRunner
                 sql = format("CREATE TABLE %s AS SELECT * FROM %s", table.getObjectName(), table);
                 break;
             case "lineitem":
-                sql = format("CREATE TABLE %s WITH (bucketed_by=array['orderkey'], bucket_count=11) AS SELECT * FROM %s", table.getObjectName(), table);
+                sql = format("CREATE TABLE %s WITH (format = 'ORC', bucketed_by=array['orderkey'], bucket_count=11) AS SELECT * FROM %s", table.getObjectName(), table);
                 break;
             case "customer":
-                sql = format("CREATE TABLE %s WITH (bucketed_by=array['custkey'], bucket_count=11) AS SELECT * FROM %s", table.getObjectName(), table);
+                sql = format("CREATE TABLE %s WITH (format = 'ORC', bucketed_by=array['custkey'], bucket_count=11) AS SELECT * FROM %s", table.getObjectName(), table);
                 break;
             case "orders":
-                sql = format("CREATE TABLE %s WITH (bucketed_by=array['custkey'], bucket_count=11) AS SELECT * FROM %s", table.getObjectName(), table);
+                sql = format("CREATE TABLE %s WITH (format = 'ORC', bucketed_by=array['custkey'], bucket_count=11) AS SELECT * FROM %s", table.getObjectName(), table);
                 break;
             default:
                 throw new UnsupportedOperationException();
