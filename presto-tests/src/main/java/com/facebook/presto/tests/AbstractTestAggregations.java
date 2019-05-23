prestodb/presto

commit 13e1d0bc303af57738fa83becb862b621c21012a

Flatten FunctionCall as direct fields in AggregationNode#Aggregation
Previously these information is stored in `FunctionCall`, which is an
`Expression`. This is required by following commits which will make
AggregationNode to use `RowExpression` instead of `Expression`.

presto-main/src/main/java/com/facebook/presto/sql/planner/plan/AggregationNode.java
@@ -16,8 +16,9 @@ package com.facebook.presto.sql.planner.plan;
 import com.facebook.presto.metadata.FunctionManager;
 import com.facebook.presto.operator.aggregation.InternalAggregationFunction;
 import com.facebook.presto.spi.function.FunctionHandle;
+import com.facebook.presto.sql.planner.OrderingScheme;
 import com.facebook.presto.sql.planner.Symbol;
-import com.facebook.presto.sql.tree.FunctionCall;
+import com.facebook.presto.sql.tree.Expression;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
@@ -29,6 +30,7 @@ import javax.annotation.concurrent.Immutable;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -72,8 +74,7 @@ public class AggregationNode
         this.groupIdSymbol = requireNonNull(groupIdSymbol);
 
         boolean noOrderBy = aggregations.values().stream()
-                .map(Aggregation::getCall)
-                .map(FunctionCall::getOrderBy)
+                .map(Aggregation::getOrderBy)
                 .noneMatch(Optional::isPresent);
         checkArgument(noOrderBy || step == SINGLE, "ORDER BY does not support distributed aggregation");
 
@@ -187,8 +188,7 @@ public class AggregationNode
     public boolean hasOrderings()
     {
         return aggregations.values().stream()
-                .map(Aggregation::getCall)
-                .map(FunctionCall::getOrderBy)
+                .map(Aggregation::getOrderBy)
                 .anyMatch(Optional::isPresent);
     }
 
@@ -207,13 +207,11 @@ public class AggregationNode
     public boolean isDecomposable(FunctionManager functionManager)
     {
         boolean hasOrderBy = getAggregations().values().stream()
-                .map(Aggregation::getCall)
-                .map(FunctionCall::getOrderBy)
+                .map(Aggregation::getOrderBy)
                 .anyMatch(Optional::isPresent);
 
         boolean hasDistinct = getAggregations().values().stream()
-                .map(Aggregation::getCall)
-                .anyMatch(FunctionCall::isDistinct);
+                .anyMatch(Aggregation::isDistinct);
 
         boolean decomposableFunctions = getAggregations().values().stream()
                 .map(Aggregation::getFunctionHandle)
@@ -358,25 +356,28 @@ public class AggregationNode
 
     public static class Aggregation
     {
-        private final FunctionCall call;
         private final FunctionHandle functionHandle;
+        private final List<Expression> arguments;
+        private final Optional<Expression> filter;
+        private final Optional<OrderingScheme> orderingScheme;
+        private final boolean isDistinct;
         private final Optional<Symbol> mask;
 
         @JsonCreator
         public Aggregation(
-                @JsonProperty("call") FunctionCall call,
                 @JsonProperty("functionHandle") FunctionHandle functionHandle,
+                @JsonProperty("arguments") List<Expression> arguments,
+                @JsonProperty("filter") Optional<Expression> filter,
+                @JsonProperty("orderBy") Optional<OrderingScheme> orderingScheme,
+                @JsonProperty("isDistinct") boolean isDistinct,
                 @JsonProperty("mask") Optional<Symbol> mask)
         {
-            this.call = requireNonNull(call, "call is null");
             this.functionHandle = requireNonNull(functionHandle, "functionHandle is null");
+            this.arguments = requireNonNull(arguments, "arguments is null");
+            this.filter = requireNonNull(filter, "filter is null");
+            this.orderingScheme = requireNonNull(orderingScheme, "orderingScheme is null");
+            this.isDistinct = isDistinct;
             this.mask = requireNonNull(mask, "mask is null");
-        }
-
-        @JsonProperty
-        public FunctionCall getCall()
-        {
-            return call;
         }
 
         @JsonProperty
@@ -386,9 +387,57 @@ public class AggregationNode
         }
 
         @JsonProperty
+        public List<Expression> getArguments()
+        {
+            return arguments;
+        }
+
+        @JsonProperty
+        public Optional<OrderingScheme> getOrderBy()
+        {
+            return orderingScheme;
+        }
+
+        @JsonProperty
+        public Optional<Expression> getFilter()
+        {
+            return filter;
+        }
+
+        @JsonProperty
+        public boolean isDistinct()
+        {
+            return isDistinct;
+        }
+
+        @JsonProperty
         public Optional<Symbol> getMask()
         {
             return mask;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Aggregation)) {
+                return false;
+            }
+            Aggregation that = (Aggregation) o;
+            return isDistinct == that.isDistinct &&
+                    Objects.equals(functionHandle, that.functionHandle) &&
+                    Objects.equals(arguments, that.arguments) &&
+                    Objects.equals(filter, that.filter) &&
+                    Objects.equals(orderingScheme, that.orderingScheme) &&
+                    Objects.equals(mask, that.mask);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return Objects.hash(functionHandle, arguments, filter, orderingScheme, isDistinct, mask);
         }
     }
 }

presto-main/src/main/java/com/facebook/presto/sql/planner/AnalyzedExpressionRewriter.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.planner;
+
+import com.facebook.presto.Session;
+import com.facebook.presto.execution.warnings.WarningCollector;
+import com.facebook.presto.metadata.Metadata;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.parser.SqlParser;
+import com.facebook.presto.sql.tree.Expression;
+import com.facebook.presto.sql.tree.ExpressionRewriter;
+import com.facebook.presto.sql.tree.ExpressionTreeRewriter;
+import com.facebook.presto.sql.tree.LambdaExpression;
+import com.facebook.presto.sql.tree.NodeRef;
+
+import java.util.Map;
+
+import static com.facebook.presto.sql.analyzer.ExpressionAnalyzer.getExpressionTypes;
+import static java.util.Collections.emptyList;
+
+@Deprecated
+public class AnalyzedExpressionRewriter
+{
+    private final Session session;
+    private final Metadata metadata;
+    private final SqlParser sqlParser;
+    private final TypeProvider typeProvider;
+
+    public AnalyzedExpressionRewriter(Session session, Metadata metadata, SqlParser sqlParser, TypeProvider typeProvider)
+    {
+        this.session = session;
+        this.metadata = metadata;
+        this.sqlParser = sqlParser;
+        this.typeProvider = typeProvider;
+    }
+
+    public Expression rewriteWith(RewriterProvider<Void> rewriterProvider, Expression expression)
+    {
+        return rewriteWith(rewriterProvider, expression, null);
+    }
+
+    public <C> Expression rewriteWith(RewriterProvider<C> rewriterProvider, Expression expression, C context)
+    {
+        // Lambda cannot be analyzed outside the context of function, but its body can be rewritten.
+        if (expression instanceof LambdaExpression) {
+            LambdaExpression lambdaExpression = (LambdaExpression) expression;
+            return new LambdaExpression(lambdaExpression.getArguments(), rewriteWith(rewriterProvider, lambdaExpression.getBody(), context));
+        }
+        Map<NodeRef<Expression>, Type> expressionTypes = getExpressionTypes(
+                session,
+                metadata,
+                sqlParser,
+                typeProvider,
+                expression,
+                emptyList(),
+                WarningCollector.NOOP);
+        return ExpressionTreeRewriter.rewriteWith(rewriterProvider.get(expressionTypes), expression, context);
+    }
+
+    interface RewriterProvider<C>
+    {
+        ExpressionRewriter<C> get(Map<NodeRef<Expression>, Type> expressionTypes);
+    }
+}

presto-main/src/main/java/com/facebook/presto/sql/planner/DesugarAtTimeZoneRewriter.java
@@ -14,7 +14,6 @@
 package com.facebook.presto.sql.planner;
 
 import com.facebook.presto.Session;
-import com.facebook.presto.execution.warnings.WarningCollector;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.sql.parser.SqlParser;
@@ -28,7 +27,6 @@ import com.facebook.presto.sql.tree.NodeRef;
 import com.facebook.presto.sql.tree.QualifiedName;
 import com.facebook.presto.sql.tree.SymbolReference;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 
 import java.util.Map;
 
@@ -36,15 +34,13 @@ import static com.facebook.presto.spi.type.TimeType.TIME;
 import static com.facebook.presto.spi.type.TimeWithTimeZoneType.TIME_WITH_TIME_ZONE;
 import static com.facebook.presto.spi.type.TimestampType.TIMESTAMP;
 import static com.facebook.presto.spi.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
-import static com.facebook.presto.sql.analyzer.ExpressionAnalyzer.getExpressionTypes;
-import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 
 public class DesugarAtTimeZoneRewriter
 {
     public static Expression rewrite(Expression expression, Map<NodeRef<Expression>, Type> expressionTypes)
     {
-        return ExpressionTreeRewriter.rewriteWith(new Visitor(expressionTypes), expression);
+        return ExpressionTreeRewriter.rewriteWith(new Visitor(expressionTypes), expression, null);
     }
 
     private DesugarAtTimeZoneRewriter() {}
@@ -57,9 +53,7 @@ public class DesugarAtTimeZoneRewriter
         if (expression instanceof SymbolReference) {
             return expression;
         }
-        Map<NodeRef<Expression>, Type> expressionTypes = getExpressionTypes(session, metadata, sqlParser, symbolAllocator.getTypes(), expression, emptyList(), WarningCollector.NOOP);
-
-        return rewrite(expression, expressionTypes);
+        return new AnalyzedExpressionRewriter(session, metadata, sqlParser, symbolAllocator.getTypes()).rewriteWith(Visitor::new, expression);
     }
 
     private static class Visitor
@@ -69,14 +63,14 @@ public class DesugarAtTimeZoneRewriter
 
         public Visitor(Map<NodeRef<Expression>, Type> expressionTypes)
         {
-            this.expressionTypes = ImmutableMap.copyOf(requireNonNull(expressionTypes, "expressionTypes is null"));
+            this.expressionTypes = requireNonNull(expressionTypes, "expressionTypes is null");
         }
 
         @Override
         public Expression rewriteAtTimeZone(AtTimeZone node, Void context, ExpressionTreeRewriter<Void> treeRewriter)
         {
             Expression value = treeRewriter.rewrite(node.getValue(), context);
-            Type type = getType(node.getValue());
+            Type type = expressionTypes.get(NodeRef.of(node.getValue()));
             if (type.equals(TIME)) {
                 value = new Cast(value, TIME_WITH_TIME_ZONE.getDisplayName());
             }
@@ -85,11 +79,6 @@ public class DesugarAtTimeZoneRewriter
             }
 
             return new FunctionCall(QualifiedName.of("at_timezone"), ImmutableList.of(value, treeRewriter.rewrite(node.getTimeZone(), context)));
-        }
-
-        private Type getType(Expression expression)
-        {
-            return expressionTypes.get(NodeRef.of(expression));
         }
     }
 }

presto-main/src/main/java/com/facebook/presto/sql/planner/ExpressionExtractor.java
@@ -29,7 +29,6 @@ import com.google.common.collect.ImmutableList;
 import java.util.List;
 
 import static com.facebook.presto.sql.planner.iterative.Lookup.noLookup;
-import static com.facebook.presto.sql.relational.OriginalExpressionUtils.castToRowExpression;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.util.Objects.requireNonNull;
 
@@ -92,7 +91,20 @@ public class ExpressionExtractor
         public Void visitAggregation(AggregationNode node, ImmutableList.Builder<RowExpression> context)
         {
             node.getAggregations().values()
-                    .forEach(aggregation -> context.add(castToRowExpression(aggregation.getCall())));
+                    .forEach(aggregation -> {
+                        aggregation.getArguments()
+                                .stream()
+                                .map(OriginalExpressionUtils::castToRowExpression)
+                                .forEach(context::add);
+                        aggregation.getFilter().map(OriginalExpressionUtils::castToRowExpression).ifPresent(context::add);
+                        aggregation.getOrderBy()
+                                .map(OrderingScheme::getOrderBy)
+                                .orElse(ImmutableList.of())
+                                .stream()
+                                .map(Symbol::toSymbolReference)
+                                .map(OriginalExpressionUtils::castToRowExpression)
+                                .forEach(context::add);
+                    });
             return super.visitAggregation(node, context);
         }
 

presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
@@ -178,8 +178,6 @@ import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.LambdaArgumentDeclaration;
 import com.facebook.presto.sql.tree.LambdaExpression;
 import com.facebook.presto.sql.tree.NodeRef;
-import com.facebook.presto.sql.tree.OrderBy;
-import com.facebook.presto.sql.tree.SortItem;
 import com.facebook.presto.sql.tree.SymbolReference;
 import com.google.common.base.VerifyException;
 import com.google.common.collect.ContiguousSet;
@@ -2525,7 +2523,7 @@ public class LocalExecutionPlanner
             InternalAggregationFunction internalAggregationFunction = functionManager.getAggregateFunctionImplementation(aggregation.getFunctionHandle());
 
             List<Integer> valueChannels = new ArrayList<>();
-            for (Expression argument : aggregation.getCall().getArguments()) {
+            for (Expression argument : aggregation.getArguments()) {
                 if (!(argument instanceof LambdaExpression)) {
                     Symbol argumentSymbol = Symbol.from(argument);
                     valueChannels.add(source.getLayout().get(argumentSymbol));
@@ -2533,7 +2531,7 @@ public class LocalExecutionPlanner
             }
 
             List<LambdaProvider> lambdaProviders = new ArrayList<>();
-            List<LambdaExpression> lambdaExpressions = aggregation.getCall().getArguments().stream()
+            List<LambdaExpression> lambdaExpressions = aggregation.getArguments().stream()
                     .filter(LambdaExpression.class::isInstance)
                     .map(LambdaExpression.class::cast)
                     .collect(toImmutableList());
@@ -2601,17 +2599,10 @@ public class LocalExecutionPlanner
             Optional<Integer> maskChannel = aggregation.getMask().map(value -> source.getLayout().get(value));
             List<SortOrder> sortOrders = ImmutableList.of();
             List<Symbol> sortKeys = ImmutableList.of();
-            if (aggregation.getCall().getOrderBy().isPresent()) {
-                OrderBy orderBy = aggregation.getCall().getOrderBy().get();
-
-                sortKeys = orderBy.getSortItems().stream()
-                        .map(SortItem::getSortKey)
-                        .map(Symbol::from)
-                        .collect(toImmutableList());
-
-                sortOrders = orderBy.getSortItems().stream()
-                        .map(QueryPlanner::toSortOrder)
-                        .collect(toImmutableList());
+            if (aggregation.getOrderBy().isPresent()) {
+                OrderingScheme orderBy = aggregation.getOrderBy().get();
+                sortKeys = orderBy.getOrderBy();
+                sortOrders = orderBy.getOrderingList();
             }
 
             return internalAggregationFunction.bind(
@@ -2621,7 +2612,7 @@ public class LocalExecutionPlanner
                     getChannelsForSymbols(sortKeys, source.getLayout()),
                     sortOrders,
                     pagesIndexFactory,
-                    aggregation.getCall().isDistinct(),
+                    aggregation.isDistinct(),
                     joinCompiler,
                     lambdaProviders,
                     session);

presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
@@ -288,7 +288,7 @@ public class PlanOptimizers
                                         new MultipleDistinctAggregationToMarkDistinct(),
                                         new ImplementBernoulliSampleAsFilter(),
                                         new MergeLimitWithDistinct(),
-                                        new PruneCountAggregationOverScalar(),
+                                        new PruneCountAggregationOverScalar(metadata.getFunctionManager()),
                                         new PruneOrderByInAggregation(metadata.getFunctionManager()),
                                         new RewriteSpatialPartitioningAggregation(metadata)))
                                 .build()),
@@ -357,7 +357,7 @@ public class PlanOptimizers
                         estimatedExchangesCostCalculator,
                         ImmutableSet.of(
                                 new RemoveRedundantIdentityProjections(),
-                                new PushAggregationThroughOuterJoin())),
+                                new PushAggregationThroughOuterJoin(metadata.getFunctionManager()))),
                 inlineProjections,
                 simplifyOptimizer, // Re-run the SimplifyExpressions to simplify any recomposed expressions from other optimizations
                 projectionPushDown,

presto-main/src/main/java/com/facebook/presto/sql/planner/PlannerUtils.java
@@ -14,12 +14,22 @@
 package com.facebook.presto.sql.planner;
 
 import com.facebook.presto.spi.block.SortOrder;
+import com.facebook.presto.sql.tree.Expression;
+import com.facebook.presto.sql.tree.OrderBy;
 import com.facebook.presto.sql.tree.SortItem;
+import com.facebook.presto.sql.tree.SymbolReference;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public class PlannerUtils
 {
-    private PlannerUtils()
-    {}
+    private PlannerUtils() {}
 
     public static SortOrder toSortOrder(SortItem sortItem)
     {
@@ -33,5 +43,30 @@ public class PlannerUtils
             return SortOrder.DESC_NULLS_FIRST;
         }
         return SortOrder.DESC_NULLS_LAST;
+    }
+
+    public static OrderingScheme toOrderingScheme(List<SortItem> sortItems)
+    {
+        return toOrderingScheme(sortItems, item -> {
+            checkArgument(item instanceof SymbolReference, "must be symbol reference");
+            return new Symbol(((SymbolReference) item).getName());
+        });
+    }
+
+    public static OrderingScheme toOrderingScheme(List<SortItem> sortItems, Function<Expression, Symbol> translator)
+    {
+        // The logic is similar to QueryPlanner::sort
+        Map<Symbol, SortOrder> orderings = new LinkedHashMap<>();
+        for (SortItem item : sortItems) {
+            Symbol symbol = translator.apply(item.getSortKey());
+            // don't override existing keys, i.e. when "ORDER BY a ASC, a DESC" is specified
+            orderings.putIfAbsent(symbol, toSortOrder(item));
+        }
+        return new OrderingScheme(orderings.keySet().stream().collect(toImmutableList()), orderings);
+    }
+
+    public static OrderingScheme toOrderingScheme(OrderBy orderBy)
+    {
+        return toOrderingScheme(orderBy.getSortItems());
     }
 }

presto-main/src/main/java/com/facebook/presto/sql/planner/QueryPlanner.java
@@ -558,8 +558,16 @@ class QueryPlanner
                 needPostProjectionCoercion = true;
             }
             aggregationTranslations.put(aggregate, newSymbol);
+            FunctionCall rewrittenFunction = (FunctionCall) rewritten;
 
-            aggregationsBuilder.put(newSymbol, new Aggregation((FunctionCall) rewritten, analysis.getFunctionHandle(aggregate), Optional.empty()));
+            aggregationsBuilder.put(newSymbol,
+                    new Aggregation(
+                            analysis.getFunctionHandle(aggregate),
+                            rewrittenFunction.getArguments(),
+                            rewrittenFunction.getFilter(),
+                            rewrittenFunction.getOrderBy().map(OrderBy::getSortItems).map(PlannerUtils::toOrderingScheme),
+                            rewrittenFunction.isDistinct(),
+                            Optional.empty()));
         }
         Map<Symbol, Aggregation> aggregations = aggregationsBuilder.build();
 
@@ -887,6 +895,7 @@ class QueryPlanner
 
         Iterator<SortItem> sortItems = orderBy.get().getSortItems().iterator();
 
+        // This logic is similar to PlannerUtils::toOrderingScheme
         ImmutableList.Builder<Symbol> orderBySymbols = ImmutableList.builder();
         Map<Symbol, SortOrder> orderings = new HashMap<>();
         for (Expression fieldOrExpression : orderByExpressions) {

commit cf251d2656b5afc718009fd0fbc9e224af97c7bb

Fix serialization of AggregationNode#Aggregation
Commit 13e1d0b introduced a bug in ignoring isDistinct field in JSON
deserialization.

presto-main/src/main/java/com/facebook/presto/sql/planner/plan/AggregationNode.java
@@ -370,7 +370,7 @@ public class AggregationNode
                 @JsonProperty("arguments") List<Expression> arguments,
                 @JsonProperty("filter") Optional<Expression> filter,
                 @JsonProperty("orderBy") Optional<OrderingScheme> orderingScheme,
-                @JsonProperty("isDistinct") boolean isDistinct,
+                @JsonProperty("distinct") boolean isDistinct,
                 @JsonProperty("mask") Optional<Symbol> mask)
         {
             this.functionHandle = requireNonNull(functionHandle, "functionHandle is null");

presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestAggregations.java
@@ -13,12 +13,14 @@
  */
 package com.facebook.presto.tests;
 
+import com.facebook.presto.Session;
 import com.facebook.presto.testing.MaterializedResult;
 import com.facebook.presto.testing.MaterializedRow;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static com.facebook.presto.SessionTestUtils.TEST_SESSION;
 import static com.facebook.presto.testing.MaterializedResult.resultBuilder;
 import static com.facebook.presto.tests.QueryAssertions.assertEqualsIgnoreOrder;
 import static org.testng.Assert.assertEquals;
@@ -206,6 +208,17 @@ public abstract class AbstractTestAggregations
     {
         assertQuery("SELECT COUNT(DISTINCT custkey + 1) FROM orders", "SELECT COUNT(*) FROM (SELECT DISTINCT custkey + 1 FROM orders) t");
         assertQuery("SELECT COUNT(DISTINCT linenumber), COUNT(*) from lineitem where linenumber < 0");
+        assertQuery(" SELECT COUNT(*) FROM (SELECT orderkey, COUNT(DISTINCT partkey) FROM lineitem " +
+                        "  GROUP BY orderkey " +
+                        "HAVING COUNT(DISTINCT partkey) != CARDINALITY(ARRAY_DISTINCT(ARRAY_AGG(partkey))))",
+                "VALUES 0");
+        assertQuery(Session.builder(TEST_SESSION)
+                        .setSystemProperty("use_mark_distinct", "false")
+                        .build(),
+                " SELECT COUNT(*) FROM (SELECT orderkey, COUNT(DISTINCT partkey) FROM lineitem " +
+                        "  GROUP BY orderkey " +
+                        "HAVING COUNT(DISTINCT partkey) != CARDINALITY(ARRAY_DISTINCT(ARRAY_AGG(partkey))))",
+                "VALUES 0");
     }
 
     @Test