bcdf5481c7f611c8068542669a8b0157900b07ad
Gaston Arguindegui <gastonar@fb.com>
2019-05-24 16:27:00 -0700

    Add merge_hll
    Allows HyperLogLog objects to be merged from array.

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/HyperLogLogFunctions.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/HyperLogLogFunctions.java
@@ -13,8 +13,10 @@
  */
 package com.facebook.presto.operator.scalar;
 
+import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.function.Description;
 import com.facebook.presto.spi.function.ScalarFunction;
+import com.facebook.presto.spi.function.SqlNullable;
 import com.facebook.presto.spi.function.SqlType;
 import com.facebook.presto.spi.type.StandardTypes;
 import io.airlift.slice.Slice;
@@ -49,5 +51,39 @@ public final class HyperLogLogFunctions
     public static Slice emptyApproxSet(@SqlType(StandardTypes.DOUBLE) double maxStandardError)
     {
         return HyperLogLog.newInstance(standardErrorToBuckets(maxStandardError)).serialize();
+    }
+
+    @ScalarFunction("merge_hll")
+    @Description("merge the contents of an array of HyperLogLogs")
+    @SqlType(StandardTypes.HYPER_LOG_LOG)
+    @SqlNullable
+    public static Slice scalarMerge(@SqlType("array(HyperLogLog)") Block block)
+    {
+        if (block.getPositionCount() == 0) {
+            return null;
+        }
+
+        HyperLogLog merged = null;
+        int firstNonNullIndex = 0;
+
+        while (firstNonNullIndex < block.getPositionCount() && block.isNull(firstNonNullIndex)) {
+            firstNonNullIndex++;
+        }
+
+        if (firstNonNullIndex == block.getPositionCount()) {
+            return null;
+        }
+
+        Slice initialSlice = block.getSlice(firstNonNullIndex, 0, block.getSliceLength(firstNonNullIndex));
+        merged = HyperLogLog.newInstance(initialSlice);
+
+        for (int i = firstNonNullIndex; i < block.getPositionCount(); i++) {
+            Slice currentSlice = block.getSlice(i, 0, block.getSliceLength(i));
+            if (!block.isNull(i)) {
+                merged.mergeWith(HyperLogLog.newInstance(currentSlice));
+            }
+        }
+
+        return merged.serialize();
     }
 }
