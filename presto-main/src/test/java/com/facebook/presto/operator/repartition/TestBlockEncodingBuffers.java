5e85134fe51f7a5e1a9555dca4cf59b5fbfa2def
Ying Su <yingsu@fb.com>
2019-07-16 08:21:47 -0700

    Optimize repartitioning for INTEGER type

--- presto-main/src/test/java/com/facebook/presto/operator/repartition/TestBlockEncodingBuffers.java
+++ presto-main/src/test/java/com/facebook/presto/operator/repartition/TestBlockEncodingBuffers.java
@@ -47,6 +47,7 @@ import static com.facebook.presto.block.BlockAssertions.Encoding.DICTIONARY;
 import static com.facebook.presto.block.BlockAssertions.Encoding.RUN_LENGTH;
 import static com.facebook.presto.block.BlockAssertions.assertBlockEquals;
 import static com.facebook.presto.block.BlockAssertions.createAllNullsBlock;
+import static com.facebook.presto.block.BlockAssertions.createRandomIntsBlock;
 import static com.facebook.presto.block.BlockAssertions.createRandomLongDecimalsBlock;
 import static com.facebook.presto.block.BlockAssertions.createRandomLongsBlock;
 import static com.facebook.presto.block.BlockAssertions.wrapBlock;
@@ -56,6 +57,7 @@ import static com.facebook.presto.operator.repartition.OptimizedPartitionedOutpu
 import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.facebook.presto.spi.type.DecimalType.createDecimalType;
 import static com.facebook.presto.spi.type.Decimals.MAX_SHORT_PRECISION;
+import static com.facebook.presto.spi.type.IntegerType.INTEGER;
 import static com.facebook.presto.testing.TestingEnvironment.TYPE_MANAGER;
 import static java.lang.Math.toIntExact;
 import static java.util.Objects.requireNonNull;
@@ -75,6 +77,12 @@ public class TestBlockEncodingBuffers
     public void testLongDecimal()
     {
         testBlock(createDecimalType(MAX_SHORT_PRECISION + 1), createRandomLongDecimalsBlock(POSITIONS_PER_BLOCK, true));
+    }
+
+    @Test
+    public void testInteger()
+    {
+        testBlock(INTEGER, createRandomIntsBlock(POSITIONS_PER_BLOCK, true));
     }
 
     private void testBlock(Type type, Block block)
