a9cfeffeccc834500e7ad6f9208f2a0c71f06dc6
Andrii Rosa <andriirosa@fb.com>
2019-10-02 16:57:41 -0400

    Do not update query failureCause when query is FINISHING

--- presto-main/src/test/java/com/facebook/presto/execution/TestQueryStateMachine.java
+++ presto-main/src/test/java/com/facebook/presto/execution/TestQueryStateMachine.java
@@ -425,7 +425,7 @@ public class TestQueryStateMachine
         commitFuture.setException(new RuntimeException("transaction failed"));
         tryGetFutureValue(stateMachine.getStateChange(FAILED), 2, SECONDS);
         assertEquals(stateMachine.getQueryState(), FAILED);
-        // assertEquals(stateMachine.getFailureInfo().get().getMessage(), "transaction failed");
+        assertEquals(stateMachine.getFailureInfo().get().getMessage(), "transaction failed");
     }
 
     private static void assertFinalState(QueryStateMachine stateMachine, QueryState expectedState)
