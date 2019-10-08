7109a2b89e53699cd7ed99823d68a7f7df47acac
Bhavani Hari <harib.kumar@gmail.com>
2019-10-08 11:34:19 -0700

    Handle null dataStream in SliceDictionary reader

--- presto-orc/src/main/java/com/facebook/presto/orc/reader/SliceDictionarySelectiveReader.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/reader/SliceDictionarySelectiveReader.java
@@ -343,7 +343,9 @@ public class SliceDictionarySelectiveReader
             if (inDictionaryStream != null) {
                 inDictionaryStream.skip(dataToSkip);
             }
-            dataStream.skip(dataToSkip);
+            if (dataStream != null) {
+                dataStream.skip(dataToSkip);
+            }
         }
         else {
             if (inDictionaryStream != null) {
