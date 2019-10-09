2b0ac431148bf6c9f65c2c393b3b3e72e356b852
Kelvin Fann <kelvinfann@fb.com>
2019-10-09 16:11:45 -0700

    fixing

--- presto-main/src/main/java/com/facebook/presto/util/GcStatusMonitor.java
+++ presto-main/src/main/java/com/facebook/presto/util/GcStatusMonitor.java
@@ -37,7 +37,6 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
