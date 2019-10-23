3313c1822b059c047e6b612d618a409b8e101626
Bhavani Hari <harib.kumar@gmail.com>
2019-10-23 11:55:24 -0700

    Add tests for DistributedQueries for SelectiveReaders

--- presto-hive/src/main/java/com/facebook/presto/hive/HivePartitionManager.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HivePartitionManager.java
@@ -191,7 +191,7 @@ public class HivePartitionManager
                     partitions,
                     compactEffectivePredicate,
                     effectivePredicate,
-                    TupleDomain.none(),
+                    TupleDomain.all(),
                     hiveBucketHandle,
                     bucketFilter);
         }
