cbc9742ebba0eb3b434ec21385bf5fe0797184a3
Bhavani Hari <harib.kumar@gmail.com>
2019-11-09 23:39:56 -0800

    Fix the size of all-null blocks returned from Byte reader

--- presto-orc/src/main/java/com/facebook/presto/orc/reader/ByteSelectiveStreamReader.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/reader/ByteSelectiveStreamReader.java
@@ -319,7 +319,7 @@ public class ByteSelectiveStreamReader
         checkState(!valuesInUse, "BlockLease hasn't been closed yet");
 
         if (allNulls) {
-            return new RunLengthEncodedBlock(NULL_BLOCK, outputPositionCount);
+            return new RunLengthEncodedBlock(NULL_BLOCK, positionCount);
         }
 
         boolean includeNulls = nullsAllowed && presentStream != null;
