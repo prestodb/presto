9ea39a6ebb8687c06f95daa2db73093718cbced0
Ke Wang <ke1024@fb.com>
2019-11-19 12:43:54 -0800

    Add priority to Compaction process

--- presto-raptor/src/main/java/com/facebook/presto/raptor/storage/organization/CompactionSetCreator.java
+++ presto-raptor/src/main/java/com/facebook/presto/raptor/storage/organization/CompactionSetCreator.java
@@ -70,16 +70,21 @@ public class CompactionSetCreator
         ImmutableSet.Builder<ShardIndexInfo> builder = ImmutableSet.builder();
         ImmutableSet.Builder<OrganizationSet> compactionSets = ImmutableSet.builder();
 
+        int priority = 0;
         for (ShardIndexInfo shard : shards) {
             if (((consumedBytes + shard.getUncompressedSize()) > maxShardSize.toBytes()) ||
                     (consumedRows + shard.getRowCount() > maxShardRows)) {
                 // Finalize this compaction set, and start a new one for the rest of the shards
                 Set<ShardIndexInfo> shardsToCompact = builder.build();
-                addToCompactionSets(compactionSets, shardsToCompact, tableId, tableInfo);
+                addToCompactionSets(compactionSets, shardsToCompact, tableId, tableInfo, priority);
 
+                priority = 0;
                 builder = ImmutableSet.builder();
                 consumedBytes = 0;
                 consumedRows = 0;
+            }
+            if (shard.getDeltaUuid().isPresent()) {
+                priority += 1;
             }
             builder.add(shard);
             consumedBytes += shard.getUncompressedSize();
@@ -88,15 +93,15 @@ public class CompactionSetCreator
 
         // create compaction set for the remaining shards of this day
         Set<ShardIndexInfo> shardsToCompact = builder.build();
-        addToCompactionSets(compactionSets, shardsToCompact, tableId, tableInfo);
+        addToCompactionSets(compactionSets, shardsToCompact, tableId, tableInfo, priority);
         return compactionSets.build();
     }
 
-    private void addToCompactionSets(ImmutableSet.Builder<OrganizationSet> compactionSets, Set<ShardIndexInfo> shardsToCompact, long tableId, Table tableInfo)
+    private void addToCompactionSets(ImmutableSet.Builder<OrganizationSet> compactionSets, Set<ShardIndexInfo> shardsToCompact, long tableId, Table tableInfo, int priority)
     {
         // Add special rule for shard which is too big to compact with other shards but have delta to compact
         if (shardsToCompact.size() > 1 || shardsToCompact.stream().anyMatch(shard -> shard.getDeltaUuid().isPresent())) {
-            compactionSets.add(createOrganizationSet(tableId, tableInfo.isTableSupportsDeltaDelete(), shardsToCompact));
+            compactionSets.add(createOrganizationSet(tableId, tableInfo.isTableSupportsDeltaDelete(), shardsToCompact, priority));
         }
     }
 
