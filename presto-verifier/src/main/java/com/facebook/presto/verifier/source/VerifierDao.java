f074d639ba4e8186accfada14334051f28baf229
Leiqing Cai <caithagoras@gmail.com>
2019-12-01 21:33:29 -0800

    Move verifier_queries table creation SQL to VerifierDao
    Also, remove unused method in VerifierTestUtil.

--- presto-verifier/src/main/java/com/facebook/presto/verifier/source/VerifierDao.java
+++ presto-verifier/src/main/java/com/facebook/presto/verifier/source/VerifierDao.java
@@ -19,12 +19,30 @@ import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.Define;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.util.List;
 
 @RegisterColumnMapper(StringToStringMapColumnMapper.class)
 public interface VerifierDao
 {
+    @SqlUpdate("CREATE TABLE verifier_queries (\n" +
+            "  id int(11) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,\n" +
+            "  suite varchar(256) NOT NULL,\n" +
+            "  name varchar(256) DEFAULT NULL,\n" +
+            "  control_catalog varchar(256) NOT NULL,\n" +
+            "  control_schema varchar(256) NOT NULL,\n" +
+            "  control_query text NOT NULL,\n" +
+            "  control_username varchar(256) DEFAULT NULL,\n" +
+            "  control_password varchar(256) DEFAULT NULL,\n" +
+            "  test_catalog varchar(256) NOT NULL,\n" +
+            "  test_schema varchar(256) NOT NULL,\n" +
+            "  test_query text NOT NULL,\n" +
+            "  test_username varchar(256) DEFAULT NULL,\n" +
+            "  test_password varchar(256) DEFAULT NULL,\n" +
+            "  session_properties_json text DEFAULT NULL)")
+    void createVerifierQueriesTable(@Define("table_name") String tableName);
+
     @SqlQuery("SELECT\n" +
             "  suite,\n" +
             "  name,\n" +
