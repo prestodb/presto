85ed09a43001cd0d239d673f5d12ce7818f6ff23
Rebecca Schlussel <rschlussel@fb.com>
2020-01-29 10:22:13 -0500

    Add configuration property max-stage-retries
    Configuration property to set how many times stages can retry

--- presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
+++ presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
@@ -71,6 +71,7 @@ public final class SystemSessionProperties
     public static final String DYNAMIC_SCHEDULE_FOR_GROUPED_EXECUTION = "dynamic_schedule_for_grouped_execution";
     public static final String RECOVERABLE_GROUPED_EXECUTION = "recoverable_grouped_execution";
     public static final String MAX_FAILED_TASK_PERCENTAGE = "max_failed_task_percentage";
+    public static final String MAX_STAGE_RETRIES = "max_stage_retries";
     public static final String PREFER_STREAMING_OPERATORS = "prefer_streaming_operators";
     public static final String TASK_WRITER_COUNT = "task_writer_count";
     public static final String TASK_PARTITIONED_WRITER_COUNT = "task_partitioned_writer_count";
@@ -249,6 +250,11 @@ public final class SystemSessionProperties
                         RECOVERABLE_GROUPED_EXECUTION,
                         "Experimental: Use recoverable grouped execution when possible",
                         featuresConfig.isRecoverableGroupedExecutionEnabled(),
+                        false),
+                integerProperty(
+                        MAX_STAGE_RETRIES,
+                        "Maximum number of times that stages can be retried",
+                        featuresConfig.getMaxStageRetries(),
                         false),
                 booleanProperty(
                         PREFER_STREAMING_OPERATORS,
@@ -812,6 +818,11 @@ public final class SystemSessionProperties
     public static double getMaxFailedTaskPercentage(Session session)
     {
         return session.getSystemProperty(MAX_FAILED_TASK_PERCENTAGE, Double.class);
+    }
+
+    public static int getMaxStageRetries(Session session)
+    {
+        return session.getSystemProperty(MAX_STAGE_RETRIES, Integer.class);
     }
 
     public static boolean preferStreamingOperators(Session session)
