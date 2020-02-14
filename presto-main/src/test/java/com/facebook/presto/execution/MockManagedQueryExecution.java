2240313e603aa06ebd27d87c60a2dfcb1fcfb58d
Tim Meehan <tdm@fb.com>
2020-02-13 23:27:57 -0800

    Add peak tasks to BasicQueryStats

--- presto-main/src/test/java/com/facebook/presto/execution/MockManagedQueryExecution.java
+++ presto-main/src/test/java/com/facebook/presto/execution/MockManagedQueryExecution.java
@@ -113,6 +113,7 @@ public class MockManagedQueryExecution
                         new Duration(3, NANOSECONDS),
                         new Duration(4, NANOSECONDS),
                         new Duration(5, NANOSECONDS),
+                        5,
                         6,
                         7,
                         8,
