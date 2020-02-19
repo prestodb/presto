e0789f169c5f143db51de9a071dafc33cd89a228
James A. Gill <jagill@fb.com>
2020-02-19 10:28:26 -0500

    Enable spatial join for spherical geography ST_Distance
    Previously, we did not extract a spatial join for any spherical
    calculations.  This enables joins for `ST_Distance(geog1, geog2) < r`
    for spherical geographies `geog1` and `geog2` for inner joins.
    
    Currently `ST_Distance` for geographies only takes points.  Later
    work can extend to other geographic objects, to outer-type joins,
    or to other functions (like `ST_Contains`).

--- presto-geospatial/src/test/java/com/facebook/presto/plugin/geospatial/TestSpatialJoinPlanning.java
+++ presto-geospatial/src/test/java/com/facebook/presto/plugin/geospatial/TestSpatialJoinPlanning.java
@@ -312,6 +312,19 @@ public class TestSpatialJoinPlanning
     }
 
     @Test
+    public void testSphericalDistanceQuery()
+    {
+        // broadcast
+        assertPlan("SELECT b.name, a.name " +
+                        "FROM (VALUES (2.1, 2.1, 'x')) AS a (lng, lat, name), (VALUES (2.1, 2.1, 'x')) AS b (lng, lat, name) " +
+                        "WHERE ST_Distance(to_spherical_geography(ST_Point(a.lng, a.lat)), to_spherical_geography(ST_Point(b.lng, b.lat))) <= 3.1",
+                anyTree(
+                        spatialJoin("st_distance(st_point_a, st_point_b) <= radius",
+                                project(ImmutableMap.of("st_point_a", expression("to_spherical_geography(ST_Point(cast(a_lng as double), cast(a_lat as double)))")), anyTree(values(ImmutableMap.of("a_lng", 0, "a_lat", 1)))),
+                                anyTree(project(ImmutableMap.of("st_point_b", expression("to_spherical_geography(ST_Point(cast(b_lng as double), cast(b_lat as double)))"), "radius", expression("3.1e0")), anyTree(values(ImmutableMap.of("b_lng", 0, "b_lat", 1))))))));
+    }
+
+    @Test
     public void testNotContains()
     {
         assertPlan("SELECT b.name, a.name " +
