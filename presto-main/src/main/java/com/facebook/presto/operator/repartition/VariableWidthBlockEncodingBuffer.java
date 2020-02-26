df3b10a7630dc3a8d0a1aa2e46e7d46e99eb4d36
Ying Su <yingsu@fb.com>
2020-02-26 00:41:23 -0800

    Add ArrayAllocator to BlockEncodingBuffer

--- presto-main/src/main/java/com/facebook/presto/operator/repartition/VariableWidthBlockEncodingBuffer.java
+++ presto-main/src/main/java/com/facebook/presto/operator/repartition/VariableWidthBlockEncodingBuffer.java
@@ -28,6 +28,7 @@
 package com.facebook.presto.operator.repartition;
 
 import com.facebook.presto.spi.block.AbstractVariableWidthBlock;
+import com.facebook.presto.spi.block.ArrayAllocator;
 import com.google.common.annotations.VisibleForTesting;
 import io.airlift.slice.Slice;
 import io.airlift.slice.SliceOutput;
@@ -66,6 +67,11 @@ public class VariableWidthBlockEncodingBuffer
 
     // The last offset in the offsets buffer
     private int lastOffset;
+
+    public VariableWidthBlockEncodingBuffer(ArrayAllocator bufferAllocator)
+    {
+        super(bufferAllocator);
+    }
 
     @Override
     public void accumulateSerializedRowSizes(int[] serializedRowSizes)
