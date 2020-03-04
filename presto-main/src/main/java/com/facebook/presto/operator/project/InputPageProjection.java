prestodb/presto

commit 16d300893cddcf97697d44712cd1baa9a37c18fc

Streamline identity projections

presto-main/src/main/java/com/facebook/presto/operator/project/InputPageProjection.java
@@ -63,6 +63,9 @@ public class InputPageProjection
         if (selectedPositions.isList()) {
             result = block.copyPositions(selectedPositions.getPositions(), selectedPositions.getOffset(), selectedPositions.size());
         }
+        else if (selectedPositions.getOffset() == 0 && selectedPositions.size() == page.getPositionCount()) {
+            result = block;
+        }
         else {
             result = block.getRegion(selectedPositions.getOffset(), selectedPositions.size());
         }

presto-main/src/main/java/com/facebook/presto/operator/repartition/OptimizedPartitionedOutputOperator.java
@@ -164,6 +164,7 @@ public class OptimizedPartitionedOutputOperator
             return;
         }
 
+        page = page.getLoadedPage();
         page = pagePreprocessor.apply(page);
         pagePartitioner.partitionPage(page);
 

presto-main/src/main/java/com/facebook/presto/operator/project/PageProcessor.java
@@ -81,7 +81,8 @@ public class PageProcessor
                 });
         this.projections = requireNonNull(projections, "projections is null").stream()
                 .map(projection -> {
-                    if (projection.getInputChannels().size() == 1 && projection.isDeterministic()) {
+                    if (projection.getInputChannels().size() == 1 && projection.isDeterministic()
+                            && !(projection instanceof InputPageProjection)) {
                         return new DictionaryAwarePageProjection(projection, dictionarySourceIdFunction);
                     }
                     return projection;

commit fbde1736066a6da5424fa026dfca6eb77574aabf

Materialize LazyBlock in scan
Fix the problem downstream operators might receive LazyBlock after scan
introduced by https://github.com/prestodb/presto/pull/14169

presto-main/src/main/java/com/facebook/presto/operator/project/InputPageProjection.java
@@ -64,7 +64,7 @@ public class InputPageProjection
             result = block.copyPositions(selectedPositions.getPositions(), selectedPositions.getOffset(), selectedPositions.size());
         }
         else if (selectedPositions.getOffset() == 0 && selectedPositions.size() == page.getPositionCount()) {
-            result = block;
+            result = block.getLoadedBlock();
         }
         else {
             result = block.getRegion(selectedPositions.getOffset(), selectedPositions.size());

presto-main/src/main/java/com/facebook/presto/operator/repartition/OptimizedPartitionedOutputOperator.java
@@ -164,7 +164,6 @@ public class OptimizedPartitionedOutputOperator
             return;
         }
 
-        page = page.getLoadedPage();
         page = pagePreprocessor.apply(page);
         pagePartitioner.partitionPage(page);
 