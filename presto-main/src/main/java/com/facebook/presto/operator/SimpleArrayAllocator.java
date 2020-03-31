90011b673b09a4938f3e6ba3e6750db2dfa81f3a
Bhavani Hari <harib.kumar@gmail.com>
2020-03-31 14:22:02 -0700

    Fix SimpleArrayAllocator to be able to allocate max configured size of arrays

--- presto-main/src/main/java/com/facebook/presto/operator/SimpleArrayAllocator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/SimpleArrayAllocator.java
@@ -65,7 +65,7 @@ public class SimpleArrayAllocator
     @Override
     public int[] borrowIntArray(int positionCount)
     {
-        checkState(getBorrowedArrayCount() < maxOutstandingArrays, "Requested too many arrays: %s", getBorrowedArrayCount());
+        checkState(getBorrowedArrayCount() <= maxOutstandingArrays, "Requested too many arrays: %s", getBorrowedArrayCount());
         int[] array;
         while (!intArrays.isEmpty() && intArrays.peek().length < positionCount) {
             array = intArrays.pop();
@@ -93,7 +93,7 @@ public class SimpleArrayAllocator
     @Override
     public byte[] borrowByteArray(int positionCount)
     {
-        checkState(getBorrowedArrayCount() < maxOutstandingArrays, "Requested too many arrays: %s", getBorrowedArrayCount());
+        checkState(getBorrowedArrayCount() <= maxOutstandingArrays, "Requested too many arrays: %s", getBorrowedArrayCount());
         byte[] array;
         while (!byteArrays.isEmpty() && byteArrays.peek().length < positionCount) {
             array = byteArrays.pop();
