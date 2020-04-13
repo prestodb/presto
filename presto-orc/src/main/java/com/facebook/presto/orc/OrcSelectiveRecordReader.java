fe66b51ce36680e06d25fec41601a27ed8e8ab1a
Bhavani Hari <harib.kumar@gmail.com>
2020-04-13 08:49:41 -0700

    Add subfield pruning to StripeReader

--- presto-orc/src/main/java/com/facebook/presto/orc/OrcSelectiveRecordReader.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/OrcSelectiveRecordReader.java
@@ -173,6 +173,7 @@ public class OrcSelectiveRecordReader
             boolean cacheable)
     {
         super(includedColumns,
+                requiredSubfields,
                 createStreamReaders(
                         orcDataSource,
                         types,
