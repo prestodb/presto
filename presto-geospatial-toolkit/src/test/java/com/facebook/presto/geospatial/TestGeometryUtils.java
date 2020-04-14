1324c1444275292d7af65ac903f7636cd49d9e7b
James A. Gill <jagill@fb.com>
2020-04-14 16:24:46 -0400

    Refactor flattenCollection to GeometryUtils
    We have need for this function in several places, and it is purely geometric.

--- presto-geospatial-toolkit/src/test/java/com/facebook/presto/geospatial/TestGeometryUtils.java
+++ presto-geospatial-toolkit/src/test/java/com/facebook/presto/geospatial/TestGeometryUtils.java
@@ -14,10 +14,16 @@
 package com.facebook.presto.geospatial;
 
 import com.esri.core.geometry.ogc.OGCGeometry;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Streams;
 import org.locationtech.jts.geom.Envelope;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static com.facebook.presto.geospatial.GeometryUtils.flattenCollection;
 import static com.facebook.presto.geospatial.GeometryUtils.getExtent;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static org.testng.Assert.assertEquals;
 
 public class TestGeometryUtils
@@ -68,5 +74,50 @@ public class TestGeometryUtils
     private void assertGetExtent(String wkt, Rectangle expected)
     {
         assertEquals(getExtent(OGCGeometry.fromText(wkt)), expected);
+    }
+
+    @Test
+    public void testFlattenCollection()
+    {
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("POINT EMPTY"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("POINT (1 2)"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTIPOINT EMPTY"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTIPOINT (1 2)"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTIPOINT (1 2, 3 4)"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("LINESTRING EMPTY"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("LINESTRING (1 2, 3 4)"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTILINESTRING EMPTY"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTILINESTRING ((1 2, 3 4))"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTILINESTRING ((1 2, 3 4), (5 6, 7 8))"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("POLYGON EMPTY"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("POLYGON ((0 0, 0 1, 1 1, 0 0))"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTIPOLYGON EMPTY"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTIPOLYGON (((0 0, 0 1, 1 1, 0 0)))"));
+        assertFlattenLeavesUnchanged(OGCGeometry.fromText("MULTIPOLYGON (((0 0, 0 1, 1 1, 0 0)), ((10 10, 10 11, 11 11, 10 10)))"));
+
+        assertFlattens(OGCGeometry.fromText("GEOMETRYCOLLECTION EMPTY"), ImmutableList.of());
+        assertFlattens(OGCGeometry.fromText("GEOMETRYCOLLECTION (POINT EMPTY)"), OGCGeometry.fromText("POINT EMPTY"));
+        assertFlattens(OGCGeometry.fromText("GEOMETRYCOLLECTION (POINT (0 1))"), OGCGeometry.fromText("POINT (0 1)"));
+        assertFlattens(OGCGeometry.fromText("GEOMETRYCOLLECTION (GEOMETRYCOLLECTION EMPTY)"), ImmutableList.of());
+        assertFlattens(
+                OGCGeometry.fromText("GEOMETRYCOLLECTION (GEOMETRYCOLLECTION (POINT (0 1), POINT (1 2)))"),
+                ImmutableList.of(OGCGeometry.fromText("POINT (0 1)"), OGCGeometry.fromText("POINT (1 2)")));
+    }
+
+    private void assertFlattenLeavesUnchanged(OGCGeometry original)
+    {
+        assertFlattens(original, original);
+    }
+
+    private void assertFlattens(OGCGeometry original, OGCGeometry expected)
+    {
+        assertFlattens(original, ImmutableList.of(expected));
+    }
+
+    private void assertFlattens(OGCGeometry original, List<OGCGeometry> expected)
+    {
+        List<String> expectedWkts = expected.stream().map(g -> g.toString()).sorted().collect(toImmutableList());
+        List<String> result = Streams.stream(flattenCollection(original)).map(g -> g.toString()).sorted().collect(toImmutableList());
+        assertEquals(result, expectedWkts);
     }
 }
