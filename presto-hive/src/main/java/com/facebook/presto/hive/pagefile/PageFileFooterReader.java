5b47518a20b7b78423ba4b36a5d9fb69fb11441c
Vic Zhang <vzhang@fb.com>
2020-04-29 14:15:13 -0400

    Support reading compressed PageFile format

--- presto-hive/src/main/java/com/facebook/presto/hive/pagefile/PageFileFooterReader.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/pagefile/PageFileFooterReader.java
@@ -13,26 +13,35 @@
  */
 package com.facebook.presto.hive.pagefile;
 
+import com.facebook.presto.hive.HiveCompressionCodec;
+import com.facebook.presto.spi.PrestoException;
 import com.google.common.collect.ImmutableList;
-import io.airlift.slice.SliceInput;
+import io.airlift.slice.FixedLengthSliceInput;
 import io.airlift.slice.Slices;
 import org.apache.hadoop.fs.FSDataInputStream;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
+import static com.facebook.presto.hive.HiveErrorCode.HIVE_BAD_DATA;
+import static com.facebook.presto.hive.HiveStorageFormat.PAGEFILE;
 import static com.facebook.presto.hive.pagefile.PageFileFooterOutput.FOOTER_LENGTH_IN_BYTES;
 import static com.google.common.base.Preconditions.checkArgument;
+import static io.airlift.slice.SizeOf.SIZE_OF_INT;
+import static io.airlift.slice.SizeOf.SIZE_OF_LONG;
 import static java.lang.Math.min;
 import static java.lang.Math.toIntExact;
+import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 public class PageFileFooterReader
 {
     private static final int ESTIMATED_FOOTER_SIZE = 1024;
 
-    private List<Long> stripeOffsets;
-    private long footerOffset;
+    private final List<Long> stripeOffsets;
+    private final long footerOffset;
+    private final Optional<HiveCompressionCodec> compressionCodec;
 
     public PageFileFooterReader(
             FSDataInputStream inputStream,
@@ -48,8 +57,9 @@ public class PageFileFooterReader
         int footerSize = Slices.wrappedBuffer(buffer, buffer.length - FOOTER_LENGTH_IN_BYTES, FOOTER_LENGTH_IN_BYTES).getInt(0);
 
         footerOffset = fileSize - footerSize;
+        HiveCompressionCodec compression;
         if (footerOffset < 0) {
-            throw new IOException("Malformed PageFile format, incorrect footer length.");
+            throw new PrestoException(HIVE_BAD_DATA, "Malformed PageFile format, incorrect footer length.");
         }
         else if (footerOffset > 0) {
             if (footerSize > buffer.length) {
@@ -57,15 +67,38 @@ public class PageFileFooterReader
                 inputStream.readFully(footerOffset, buffer);
             }
 
-            SliceInput sliceInput = Slices.wrappedBuffer(buffer, buffer.length - footerSize, footerSize - FOOTER_LENGTH_IN_BYTES).getInput();
+            FixedLengthSliceInput sliceInput = Slices.wrappedBuffer(buffer, buffer.length - footerSize, footerSize - FOOTER_LENGTH_IN_BYTES).getInput();
+            long remainingSize = sliceInput.length();
+            // read compression
+            int compressionStringSize = sliceInput.readInt();
+            remainingSize -= SIZE_OF_INT;
+            String compressionName = sliceInput.readSlice(compressionStringSize).toStringUtf8();
+            remainingSize -= compressionStringSize;
 
-            while (sliceInput.isReadable()) {
+            try {
+                compression = HiveCompressionCodec.valueOf(compressionName);
+            }
+            catch (Exception e) {
+                throw new PrestoException(
+                        HIVE_BAD_DATA,
+                        format("%s is invalid compression method in the footer of %s", compressionName, PAGEFILE.getInputFormat()));
+            }
+
+            // read stripeOffsets
+            int stripeCount = sliceInput.readInt();
+            remainingSize -= SIZE_OF_INT;
+            if (remainingSize != SIZE_OF_LONG * stripeCount) {
+                throw new PrestoException(HIVE_BAD_DATA, "Malformed PageFile format, incorrect stripe count.");
+            }
+            for (int i = 0; i < stripeCount; ++i) {
                 stripeOffsetsBuilder.add(sliceInput.readLong());
             }
         }
         else {
             // empty page file without stripe
+            compression = null;
         }
+        compressionCodec = Optional.ofNullable(compression);
         stripeOffsets = stripeOffsetsBuilder.build();
     }
 
@@ -77,5 +110,10 @@ public class PageFileFooterReader
     public long getFooterOffset()
     {
         return footerOffset;
+    }
+
+    public Optional<HiveCompressionCodec> getCompression()
+    {
+        return compressionCodec;
     }
 }
