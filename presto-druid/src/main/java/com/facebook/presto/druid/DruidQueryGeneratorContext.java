1b994063c773903c07f1197b19480c0ff99cf3d5
Beinan Wang <beinanw@twitter.com>
2020-05-19 15:52:41 -0700

    Skip escaping columnnames for table-scan on hdfs

--- presto-druid/src/main/java/com/facebook/presto/druid/DruidQueryGeneratorContext.java
+++ presto-druid/src/main/java/com/facebook/presto/druid/DruidQueryGeneratorContext.java
@@ -198,6 +198,7 @@ public class DruidQueryGeneratorContext
     {
         return "\"" + identifier + "\"";
     }
+
     public DruidQueryGeneratorContext withVariablesInAggregation(Set<VariableReferenceExpression> newVariablesInAggregation)
     {
         return new DruidQueryGeneratorContext(
@@ -290,7 +291,7 @@ public class DruidQueryGeneratorContext
             VariableReferenceExpression variable = entry.getKey();
             Selection selection = entry.getValue();
             DruidColumnHandle handle = selection.getOrigin() == Origin.TABLE_COLUMN ?
-                    new DruidColumnHandle(selection.getEscapedDefinition(), variable.getType(), DruidColumnHandle.DruidColumnType.REGULAR) :
+                    new DruidColumnHandle(selection.getDefinition(), variable.getType(), DruidColumnHandle.DruidColumnType.REGULAR) :
                     new DruidColumnHandle(variable, DruidColumnHandle.DruidColumnType.DERIVED);
             result.put(variable, handle);
         });
