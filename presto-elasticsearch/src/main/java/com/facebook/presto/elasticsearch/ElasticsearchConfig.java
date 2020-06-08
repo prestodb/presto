ba21188c3843b48f53d2700f636e573c690c55f1
Zhenxiao Luo <zluo@twitter.com>
2020-06-08 13:22:15 -0700

    Make Elasticsearch retry timeout configurable
    Cherry-pick of https://github.com/prestosql/presto/commit/bacd4d53b7bd0d91e2842092d95b06965e4e31d6
    
    Co-authored-by: Martin Traverso <mtraverso@gmail.com>

--- presto-elasticsearch/src/main/java/com/facebook/presto/elasticsearch/ElasticsearchConfig.java
+++ presto-elasticsearch/src/main/java/com/facebook/presto/elasticsearch/ElasticsearchConfig.java
@@ -37,6 +37,7 @@ public class ElasticsearchConfig
     private int maxHits = 1_000;
     private Duration requestTimeout = new Duration(10, SECONDS);
     private Duration connectTimeout = new Duration(1, SECONDS);
+    private Duration maxRetryTime = new Duration(30, SECONDS);
 
     private boolean tlsEnabled;
     private File keystorePath;
@@ -153,6 +154,20 @@ public class ElasticsearchConfig
     public ElasticsearchConfig setConnectTimeout(Duration timeout)
     {
         this.connectTimeout = timeout;
+        return this;
+    }
+
+    @NotNull
+    public Duration getMaxRetryTime()
+    {
+        return maxRetryTime;
+    }
+
+    @Config("elasticsearch.max-retry-time")
+    @ConfigDescription("Maximum timeout in case of multiple retries")
+    public ElasticsearchConfig setMaxRetryTime(Duration maxRetryTime)
+    {
+        this.maxRetryTime = maxRetryTime;
         return this;
     }
 
