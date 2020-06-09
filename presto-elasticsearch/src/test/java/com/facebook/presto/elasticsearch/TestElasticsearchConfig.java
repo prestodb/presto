68ec3c7b6446d2440ded2cfcc46903ce8965f894
Zhenxiao Luo <zluo@twitter.com>
2020-06-09 00:24:12 -0700

    Add support for AWS IAM authorization to Elasticsearch connector
    Cherry-pick of https://github.com/prestosql/presto/commit/19dfa283059b825ce99f1ab1d0954ab0f52bbd8d
    
    Co-authored-by: Martin Traverso <mtraverso@gmail.com>

--- presto-elasticsearch/src/test/java/com/facebook/presto/elasticsearch/TestElasticsearchConfig.java
+++ presto-elasticsearch/src/test/java/com/facebook/presto/elasticsearch/TestElasticsearchConfig.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import static com.facebook.airlift.configuration.testing.ConfigAssertions.assertFullMapping;
 import static com.facebook.airlift.configuration.testing.ConfigAssertions.assertRecordedDefaults;
 import static com.facebook.airlift.configuration.testing.ConfigAssertions.recordDefaults;
+import static com.facebook.presto.elasticsearch.ElasticsearchConfig.Security.AWS;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -47,7 +48,8 @@ public class TestElasticsearchConfig
                 .setKeystorePassword(null)
                 .setTrustStorePath(null)
                 .setTruststorePassword(null)
-                .setVerifyHostnames(true));
+                .setVerifyHostnames(true)
+                .setSecurity(null));
     }
 
     @Test
@@ -70,6 +72,7 @@ public class TestElasticsearchConfig
                 .put("elasticsearch.tls.truststore-path", "/tmp/truststore")
                 .put("elasticsearch.tls.truststore-password", "truststore-password")
                 .put("elasticsearch.tls.verify-hostnames", "false")
+                .put("elasticsearch.security", "AWS")
                 .build();
 
         ElasticsearchConfig expected = new ElasticsearchConfig()
@@ -88,7 +91,8 @@ public class TestElasticsearchConfig
                 .setKeystorePassword("keystore-password")
                 .setTrustStorePath(new File("/tmp/truststore"))
                 .setTruststorePassword("truststore-password")
-                .setVerifyHostnames(false);
+                .setVerifyHostnames(false)
+                .setSecurity(AWS);
 
         assertFullMapping(properties, expected);
     }
