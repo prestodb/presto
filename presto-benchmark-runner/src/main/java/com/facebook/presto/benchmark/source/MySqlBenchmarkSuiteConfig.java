bd91b166c450f34d6c0bd0c4c9d87e5374aef5c4
Leiqing Cai <caithagoras@gmail.com>
2020-06-17 15:53:16 -0700

    Make BenchmarkSuiteSupplier extensible
    - Move the logic of DbBenchmarkSuiteSupplier into an abastract
      base class AbstractJdbiBenchmarkSuiteSupplier.
    - Move MySqlBenchmarkSuiteConfig to the correct package.

--- presto-benchmark-runner/src/main/java/com/facebook/presto/benchmark/source/MySqlBenchmarkSuiteConfig.java
+++ presto-benchmark-runner/src/main/java/com/facebook/presto/benchmark/source/MySqlBenchmarkSuiteConfig.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.benchmark.framework;
+package com.facebook.presto.benchmark.source;
 
 import com.facebook.airlift.configuration.Config;
 
