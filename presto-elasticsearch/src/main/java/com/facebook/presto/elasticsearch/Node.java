a745d70d63e03ba11026d64942fbc390da5edac5
Zhenxiao Luo <zluo@twitter.com>
2020-06-28 13:55:51 -0700

    Revert "Move IndexMetadata, NodesResponse, SearchShardsResponse, Shard, ElasticsearchNode into Elasticsearch client directory"
    This reverts commit eba938f593336df9c9488714ad2dc39224085ba7.

--- presto-elasticsearch/src/main/java/com/facebook/presto/elasticsearch/Node.java
+++ presto-elasticsearch/src/main/java/com/facebook/presto/elasticsearch/Node.java
@@ -11,16 +11,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.elasticsearch.client;
+package com.facebook.presto.elasticsearch;
 
 import static java.util.Objects.requireNonNull;
 
-public class ElasticsearchNode
+public class Node
 {
     private final String id;
     private final String address;
 
-    public ElasticsearchNode(String id, String address)
+    public Node(String id, String address)
     {
         this.id = requireNonNull(id, "id is null");
         this.address = requireNonNull(address, "address is null");
