7caa51373cad4c910db71ad910f5251ba48818e0
Sanket Dige <sanketdige268@gmail.com>
2020-06-28 15:47:31 -0700

    Add invoker security mode for views
    Cherry-pick of https://github.com/prestodb/presto/commit/a2c3a1f1d80a21c35cfb36ecdb41c3ad01415ced and https://github.com/prestosql/presto/commit/c408a78244ac0f9c1360b872b9569dc29c71b62f
    
    Co-authored-by: David Phillips <david@acz.org>

--- presto-main/src/main/java/com/facebook/presto/metadata/MetadataManager.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/MetadataManager.java
@@ -1013,7 +1013,7 @@ public class MetadataManager
             ConnectorViewDefinition view = views.get(viewName.asSchemaTableName());
             if (view != null) {
                 ViewDefinition definition = deserializeView(view.getViewData());
-                if (view.getOwner().isPresent()) {
+                if (view.getOwner().isPresent() && !definition.isRunAsInvoker()) {
                     definition = definition.withOwner(view.getOwner().get());
                 }
                 return Optional.of(definition);
