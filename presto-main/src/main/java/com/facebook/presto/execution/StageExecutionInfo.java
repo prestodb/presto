44fddb1f7712a67af1083eed9166767f4af57e29
Andrii Rosa <andriirosa@fb.com>
2020-07-09 16:42:28 -0400

    Implement QueryMonitor callbacks for Presto on Spark

--- presto-main/src/main/java/com/facebook/presto/execution/StageExecutionInfo.java
+++ presto-main/src/main/java/com/facebook/presto/execution/StageExecutionInfo.java
@@ -13,14 +13,34 @@
  */
 package com.facebook.presto.execution;
 
+import com.facebook.airlift.stats.Distribution.DistributionSnapshot;
+import com.facebook.presto.operator.BlockedReason;
+import com.facebook.presto.operator.OperatorStats;
+import com.facebook.presto.operator.PipelineStats;
+import com.facebook.presto.operator.TaskStats;
+import com.facebook.presto.spi.eventlistener.StageGcStatistics;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
+import io.airlift.units.DataSize;
+import org.joda.time.DateTime;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
+import static com.facebook.presto.execution.StageExecutionState.FINISHED;
+import static io.airlift.units.DataSize.succinctBytes;
+import static io.airlift.units.Duration.succinctDuration;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.toIntExact;
 import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class StageExecutionInfo
 {
@@ -28,6 +48,183 @@ public class StageExecutionInfo
     private final StageExecutionStats stats;
     private final List<TaskInfo> tasks;
     private final Optional<ExecutionFailureInfo> failureCause;
+
+    public static StageExecutionInfo create(
+            StageExecutionId stageExecutionId,
+            StageExecutionState state,
+            Optional<ExecutionFailureInfo> failureInfo,
+            List<TaskInfo> taskInfos,
+            DateTime schedulingComplete,
+            DistributionSnapshot getSplitDistribution,
+            DataSize peakUserMemoryReservation,
+            int finishedLifespans,
+            int totalLifespans)
+    {
+        int totalTasks = taskInfos.size();
+        int runningTasks = 0;
+        int completedTasks = 0;
+
+        int totalDrivers = 0;
+        int queuedDrivers = 0;
+        int runningDrivers = 0;
+        int blockedDrivers = 0;
+        int completedDrivers = 0;
+
+        long cumulativeUserMemory = 0;
+        long userMemoryReservation = 0;
+        long totalMemoryReservation = 0;
+
+        long totalScheduledTime = 0;
+        long totalCpuTime = 0;
+        long retriedCpuTime = 0;
+        long totalBlockedTime = 0;
+
+        long totalAllocation = 0;
+
+        long rawInputDataSize = 0;
+        long rawInputPositions = 0;
+
+        long processedInputDataSize = 0;
+        long processedInputPositions = 0;
+
+        long bufferedDataSize = 0;
+        long outputDataSize = 0;
+        long outputPositions = 0;
+
+        long physicalWrittenDataSize = 0;
+
+        int fullGcCount = 0;
+        int fullGcTaskCount = 0;
+        int minFullGcSec = 0;
+        int maxFullGcSec = 0;
+        int totalFullGcSec = 0;
+
+        boolean fullyBlocked = true;
+        Set<BlockedReason> blockedReasons = new HashSet<>();
+
+        Map<String, OperatorStats> operatorToStats = new HashMap<>();
+        for (TaskInfo taskInfo : taskInfos) {
+            TaskState taskState = taskInfo.getTaskStatus().getState();
+            if (taskState.isDone()) {
+                completedTasks++;
+            }
+            else {
+                runningTasks++;
+            }
+
+            TaskStats taskStats = taskInfo.getStats();
+
+            totalDrivers += taskStats.getTotalDrivers();
+            queuedDrivers += taskStats.getQueuedDrivers();
+            runningDrivers += taskStats.getRunningDrivers();
+            blockedDrivers += taskStats.getBlockedDrivers();
+            completedDrivers += taskStats.getCompletedDrivers();
+
+            cumulativeUserMemory += taskStats.getCumulativeUserMemory();
+
+            long taskUserMemory = taskStats.getUserMemoryReservation().toBytes();
+            long taskSystemMemory = taskStats.getSystemMemoryReservation().toBytes();
+            userMemoryReservation += taskUserMemory;
+            totalMemoryReservation += taskUserMemory + taskSystemMemory;
+
+            totalScheduledTime += taskStats.getTotalScheduledTime().roundTo(NANOSECONDS);
+            totalCpuTime += taskStats.getTotalCpuTime().roundTo(NANOSECONDS);
+            if (state == FINISHED && taskInfo.getTaskStatus().getState() == TaskState.FAILED) {
+                retriedCpuTime += taskStats.getTotalCpuTime().roundTo(NANOSECONDS);
+            }
+            totalBlockedTime += taskStats.getTotalBlockedTime().roundTo(NANOSECONDS);
+            if (!taskState.isDone()) {
+                fullyBlocked &= taskStats.isFullyBlocked();
+                blockedReasons.addAll(taskStats.getBlockedReasons());
+            }
+
+            totalAllocation += taskStats.getTotalAllocation().toBytes();
+
+            rawInputDataSize += taskStats.getRawInputDataSize().toBytes();
+            rawInputPositions += taskStats.getRawInputPositions();
+
+            processedInputDataSize += taskStats.getProcessedInputDataSize().toBytes();
+            processedInputPositions += taskStats.getProcessedInputPositions();
+
+            bufferedDataSize += taskInfo.getOutputBuffers().getTotalBufferedBytes();
+            outputDataSize += taskStats.getOutputDataSize().toBytes();
+            outputPositions += taskStats.getOutputPositions();
+
+            physicalWrittenDataSize += taskStats.getPhysicalWrittenDataSize().toBytes();
+
+            fullGcCount += taskStats.getFullGcCount();
+            fullGcTaskCount += taskStats.getFullGcCount() > 0 ? 1 : 0;
+
+            int gcSec = toIntExact(taskStats.getFullGcTime().roundTo(SECONDS));
+            totalFullGcSec += gcSec;
+            minFullGcSec = min(minFullGcSec, gcSec);
+            maxFullGcSec = max(maxFullGcSec, gcSec);
+
+            for (PipelineStats pipeline : taskStats.getPipelines()) {
+                for (OperatorStats operatorStats : pipeline.getOperatorSummaries()) {
+                    String id = pipeline.getPipelineId() + "." + operatorStats.getOperatorId();
+                    operatorToStats.compute(id, (k, v) -> v == null ? operatorStats : v.add(operatorStats));
+                }
+            }
+        }
+
+        StageExecutionStats stageExecutionStats = new StageExecutionStats(
+                schedulingComplete,
+                getSplitDistribution,
+
+                totalTasks,
+                runningTasks,
+                completedTasks,
+
+                totalLifespans,
+                finishedLifespans,
+
+                totalDrivers,
+                queuedDrivers,
+                runningDrivers,
+                blockedDrivers,
+                completedDrivers,
+
+                cumulativeUserMemory,
+                succinctBytes(userMemoryReservation),
+                succinctBytes(totalMemoryReservation),
+                peakUserMemoryReservation,
+                succinctDuration(totalScheduledTime, NANOSECONDS),
+                succinctDuration(totalCpuTime, NANOSECONDS),
+                succinctDuration(retriedCpuTime, NANOSECONDS),
+                succinctDuration(totalBlockedTime, NANOSECONDS),
+                fullyBlocked && runningTasks > 0,
+                blockedReasons,
+
+                succinctBytes(totalAllocation),
+
+                succinctBytes(rawInputDataSize),
+                rawInputPositions,
+                succinctBytes(processedInputDataSize),
+                processedInputPositions,
+                succinctBytes(bufferedDataSize),
+                succinctBytes(outputDataSize),
+                outputPositions,
+                succinctBytes(physicalWrittenDataSize),
+
+                new StageGcStatistics(
+                        stageExecutionId.getStageId().getId(),
+                        stageExecutionId.getId(),
+                        totalTasks,
+                        fullGcTaskCount,
+                        minFullGcSec,
+                        maxFullGcSec,
+                        totalFullGcSec,
+                        (int) (1.0 * totalFullGcSec / fullGcCount)),
+
+                ImmutableList.copyOf(operatorToStats.values()));
+
+        return new StageExecutionInfo(
+                state,
+                stageExecutionStats,
+                taskInfos,
+                failureInfo);
+    }
 
     @JsonCreator
     public StageExecutionInfo(
--- presto-spark-base/src/main/java/com/facebook/presto/spark/planner/PrestoSparkQueryPlanner.java
+++ presto-spark-base/src/main/java/com/facebook/presto/spark/planner/PrestoSparkQueryPlanner.java
@@ -16,24 +16,35 @@ package com.facebook.presto.spark.planner;
 import com.facebook.presto.Session;
 import com.facebook.presto.cost.CostCalculator;
 import com.facebook.presto.cost.StatsCalculator;
+import com.facebook.presto.execution.Input;
+import com.facebook.presto.execution.Output;
 import com.facebook.presto.execution.QueryPreparer.PreparedQuery;
 import com.facebook.presto.execution.warnings.WarningCollector;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.security.AccessControl;
 import com.facebook.presto.spi.plan.PlanNodeIdAllocator;
+import com.facebook.presto.spi.resourceGroups.QueryType;
 import com.facebook.presto.sql.analyzer.Analysis;
 import com.facebook.presto.sql.analyzer.Analyzer;
 import com.facebook.presto.sql.analyzer.QueryExplainer;
 import com.facebook.presto.sql.parser.SqlParser;
+import com.facebook.presto.sql.planner.InputExtractor;
 import com.facebook.presto.sql.planner.LogicalPlanner;
+import com.facebook.presto.sql.planner.OutputExtractor;
 import com.facebook.presto.sql.planner.Plan;
 import com.facebook.presto.sql.planner.PlanOptimizers;
+import com.facebook.presto.sql.planner.plan.OutputNode;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 import javax.inject.Inject;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static com.facebook.presto.sql.planner.LogicalPlanner.Stage.OPTIMIZED_AND_VALIDATED;
+import static com.facebook.presto.util.StatementUtils.getQueryType;
 import static java.util.Objects.requireNonNull;
 
 public class PrestoSparkQueryPlanner
@@ -65,7 +76,7 @@ public class PrestoSparkQueryPlanner
         this.accessControl = requireNonNull(accessControl, "accessControl is null");
     }
 
-    public PlanAndUpdateType createQueryPlan(Session session, PreparedQuery preparedQuery, WarningCollector warningCollector)
+    public PlanAndMore createQueryPlan(Session session, PreparedQuery preparedQuery, WarningCollector warningCollector)
     {
         PlanNodeIdAllocator idAllocator = new PlanNodeIdAllocator();
 
@@ -90,20 +101,43 @@ public class PrestoSparkQueryPlanner
                 warningCollector);
 
         Analysis analysis = analyzer.analyze(preparedQuery.getStatement());
-        return new PlanAndUpdateType(
-                logicalPlanner.plan(analysis, OPTIMIZED_AND_VALIDATED),
-                Optional.ofNullable(analysis.getUpdateType()));
+        Plan plan = logicalPlanner.plan(analysis, OPTIMIZED_AND_VALIDATED);
+        List<Input> inputs = new InputExtractor(metadata, session).extractInputs(plan.getRoot());
+        Optional<Output> output = new OutputExtractor().extractOutput(plan.getRoot());
+        Optional<QueryType> queryType = getQueryType(preparedQuery.getStatement().getClass());
+        List<String> columnNames = ((OutputNode) plan.getRoot()).getColumnNames();
+        return new PlanAndMore(
+                plan,
+                Optional.ofNullable(analysis.getUpdateType()),
+                columnNames,
+                ImmutableSet.copyOf(inputs),
+                output,
+                queryType);
     }
 
-    public static class PlanAndUpdateType
+    public static class PlanAndMore
     {
         private final Plan plan;
         private final Optional<String> updateType;
+        private final List<String> fieldNames;
+        private final Set<Input> inputs;
+        private final Optional<Output> output;
+        private final Optional<QueryType> queryType;
 
-        public PlanAndUpdateType(Plan plan, Optional<String> updateType)
+        public PlanAndMore(
+                Plan plan,
+                Optional<String> updateType,
+                List<String> fieldNames,
+                Set<Input> inputs,
+                Optional<Output> output,
+                Optional<QueryType> queryType)
         {
             this.plan = requireNonNull(plan, "plan is null");
             this.updateType = requireNonNull(updateType, "updateType is null");
+            this.fieldNames = ImmutableList.copyOf(requireNonNull(fieldNames, "fieldNames is null"));
+            this.inputs = ImmutableSet.copyOf(requireNonNull(inputs, "inputs is null"));
+            this.output = requireNonNull(output, "output is null");
+            this.queryType = requireNonNull(queryType, "queryType is null");
         }
 
         public Plan getPlan()
@@ -114,6 +148,26 @@ public class PrestoSparkQueryPlanner
         public Optional<String> getUpdateType()
         {
             return updateType;
+        }
+
+        public List<String> getFieldNames()
+        {
+            return fieldNames;
+        }
+
+        public Set<Input> getInputs()
+        {
+            return inputs;
+        }
+
+        public Optional<Output> getOutput()
+        {
+            return output;
+        }
+
+        public Optional<QueryType> getQueryType()
+        {
+            return queryType;
         }
     }
 }
