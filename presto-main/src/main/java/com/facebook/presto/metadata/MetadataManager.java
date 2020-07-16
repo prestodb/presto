569bd0f0fb16f69de9bd8a50a155905baea88a2f
Rebecca Schlussel <rschlussel@fb.com>
2020-07-16 16:09:59 -0400

    Honor ignore_stats_calculator_failures in getTableStatistics
    ignore_stats_calculator_failures was getting enforced in
    cachingStatsProvider and cachingCostProvider.  However, InputExtractor
    was calling getTableStatistics directly, so queries could still fail due
    to errors computing stats.  This was particularly a problem when filters
    were pushed down to the table scan, because we compute filter stats as
    part of getTableStatistics().

--- presto-main/src/main/java/com/facebook/presto/metadata/MetadataManager.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/MetadataManager.java
@@ -16,6 +16,7 @@ package com.facebook.presto.metadata;
 import com.facebook.airlift.json.JsonCodec;
 import com.facebook.airlift.json.JsonCodecFactory;
 import com.facebook.airlift.json.ObjectMapperProvider;
+import com.facebook.airlift.log.Logger;
 import com.facebook.presto.Session;
 import com.facebook.presto.block.BlockEncodingManager;
 import com.facebook.presto.common.CatalogSchemaName;
@@ -92,6 +93,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import static com.facebook.airlift.concurrent.MoreFutures.toListenableFuture;
+import static com.facebook.presto.SystemSessionProperties.isIgnoreStatsCalculatorFailures;
 import static com.facebook.presto.common.function.OperatorType.BETWEEN;
 import static com.facebook.presto.common.function.OperatorType.EQUAL;
 import static com.facebook.presto.common.function.OperatorType.GREATER_THAN;
@@ -119,6 +121,8 @@ import static java.util.Objects.requireNonNull;
 public class MetadataManager
         implements Metadata
 {
+    private static final Logger log = Logger.get(MetadataManager.class);
+
     private final FunctionManager functions;
     private final ProcedureRegistry procedures;
     private final TypeManager typeManager;
@@ -502,9 +506,18 @@ public class MetadataManager
     @Override
     public TableStatistics getTableStatistics(Session session, TableHandle tableHandle, List<ColumnHandle> columnHandles, Constraint<ColumnHandle> constraint)
     {
-        ConnectorId connectorId = tableHandle.getConnectorId();
-        ConnectorMetadata metadata = getMetadata(session, connectorId);
-        return metadata.getTableStatistics(session.toConnectorSession(connectorId), tableHandle.getConnectorHandle(), tableHandle.getLayout(), columnHandles, constraint);
+        try {
+            ConnectorId connectorId = tableHandle.getConnectorId();
+            ConnectorMetadata metadata = getMetadata(session, connectorId);
+            return metadata.getTableStatistics(session.toConnectorSession(connectorId), tableHandle.getConnectorHandle(), tableHandle.getLayout(), columnHandles, constraint);
+        }
+        catch (RuntimeException e) {
+            if (isIgnoreStatsCalculatorFailures(session)) {
+                log.error(e, "Error occurred when computing stats for query %s", session.getQueryId());
+                return TableStatistics.empty();
+            }
+            throw e;
+        }
     }
 
     @Override
