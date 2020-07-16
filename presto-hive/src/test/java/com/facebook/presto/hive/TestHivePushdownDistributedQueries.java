26bf3aaa5c4c42781f5d30b1267bf7ae4518a65e
James Sun <jamessun@fb.com>
2020-07-16 00:14:10 -0700

    Enable dynamic filtering for Hive distributed query tests

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHivePushdownDistributedQueries.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHivePushdownDistributedQueries.java
@@ -13,13 +13,27 @@
  */
 package com.facebook.presto.hive;
 
+import com.facebook.presto.Session;
+import com.facebook.presto.operator.OperatorStats;
+import com.facebook.presto.spi.QueryId;
+import com.facebook.presto.spi.plan.FilterNode;
+import com.facebook.presto.spi.plan.PlanNodeId;
+import com.facebook.presto.spi.plan.ProjectNode;
+import com.facebook.presto.spi.plan.TableScanNode;
+import com.facebook.presto.sql.analyzer.FeaturesConfig;
+import com.facebook.presto.sql.planner.Plan;
+import com.facebook.presto.sql.planner.optimizations.PlanNodeSearcher;
 import com.facebook.presto.testing.MaterializedResult;
 import com.facebook.presto.tests.AbstractTestDistributedQueries;
+import com.facebook.presto.tests.DistributedQueryRunner;
+import com.facebook.presto.tests.ResultWithQueryId;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.MoreCollectors;
 import org.testng.annotations.Test;
 
 import java.util.Optional;
 
+import static com.facebook.presto.SystemSessionProperties.JOIN_DISTRIBUTION_TYPE;
 import static com.facebook.presto.hive.HiveQueryRunner.createQueryRunner;
 import static com.facebook.presto.sql.tree.ExplainType.Type.LOGICAL;
 import static com.google.common.collect.Iterables.getOnlyElement;
@@ -31,7 +45,12 @@ public class TestHivePushdownDistributedQueries
 {
     public TestHivePushdownDistributedQueries()
     {
-        super(() -> createQueryRunner(getTables(), ImmutableMap.of("experimental.pushdown-subfields-enabled", "true"), "sql-standard", ImmutableMap.of("hive.pushdown-filter-enabled", "true"), Optional.empty()));
+        super(() -> createQueryRunner(
+                getTables(),
+                ImmutableMap.of("experimental.pushdown-subfields-enabled", "true", "experimental.enable-dynamic-filtering", "true"),
+                "sql-standard",
+                ImmutableMap.of("hive.pushdown-filter-enabled", "true"),
+                Optional.empty()));
     }
 
     @Override
@@ -52,5 +71,54 @@ public class TestHivePushdownDistributedQueries
         String query = "CREATE TABLE copy_orders AS SELECT * FROM orders";
         MaterializedResult result = computeActual("EXPLAIN " + query);
         assertEquals(getOnlyElement(result.getOnlyColumnAsSet()), getExplainPlan(query, LOGICAL));
+    }
+
+    @Test
+    public void testJoinWithEmptyBuildSide()
+    {
+        Session session = Session.builder(getSession())
+                .setSystemProperty(JOIN_DISTRIBUTION_TYPE, FeaturesConfig.JoinDistributionType.BROADCAST.name())
+                .build();
+        DistributedQueryRunner runner = (DistributedQueryRunner) getQueryRunner();
+        ResultWithQueryId<MaterializedResult> result = runner.executeWithQueryId(
+                session,
+                "SELECT * FROM lineitem JOIN orders ON lineitem.orderkey = orders.orderkey AND orders.totalprice = 123.4567");
+        assertEquals(result.getResult().getRowCount(), 0);
+
+        OperatorStats probeStats = searchScanFilterAndProjectOperatorStats(result.getQueryId(), "tpch:lineitem");
+        // Probe-side is not scanned at all, due to dynamic filtering:
+        assertEquals(probeStats.getInputPositions(), 0L);
+    }
+
+    private OperatorStats searchScanFilterAndProjectOperatorStats(QueryId queryId, String tableName)
+    {
+        DistributedQueryRunner runner = (DistributedQueryRunner) getQueryRunner();
+        Plan plan = runner.getQueryPlan(queryId);
+        PlanNodeId nodeId = PlanNodeSearcher.searchFrom(plan.getRoot())
+                .where(node -> {
+                    if (!(node instanceof ProjectNode)) {
+                        return false;
+                    }
+                    ProjectNode projectNode = (ProjectNode) node;
+                    FilterNode filterNode = (FilterNode) projectNode.getSource();
+                    TableScanNode tableScanNode = (TableScanNode) filterNode.getSource();
+                    return tableName.equals(tableScanNode.getTable().getConnectorHandle().toString());
+                })
+                .findOnlyElement()
+                .getId();
+        return runner.getCoordinator()
+                .getQueryManager()
+                .getFullQueryInfo(queryId)
+                .getQueryStats()
+                .getOperatorSummaries()
+                .stream()
+                .filter(summary -> nodeId.equals(summary.getPlanNodeId()))
+                .collect(MoreCollectors.onlyElement());
+    }
+
+    private Long countRows(String tableName)
+    {
+        MaterializedResult result = getQueryRunner().execute("SELECT COUNT() FROM " + tableName);
+        return (Long) result.getOnlyValue();
     }
 }
