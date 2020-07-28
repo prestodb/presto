b8375eaf02fe961d426ab5abd5e06f609a52cc47
James Sun <jamessun@fb.com>
2020-07-27 23:26:07 -0700

    Move TranslateExpressions above all PickTableLayouts

--- presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
@@ -368,12 +368,7 @@ public class PlanOptimizers
                                 new RemoveRedundantIdentityProjections(),
                                 new TransformCorrelatedSingleRowSubqueryToProject())),
                 new CheckSubqueryNodesAreRewritten(),
-                predicatePushDown,
-                new IterativeOptimizer(
-                        ruleStats,
-                        statsCalculator,
-                        estimatedExchangesCostCalculator,
-                        new PickTableLayout(metadata, sqlParser).rules()));
+                predicatePushDown);
 
         // TODO: move this before optimization if possible!!
         // Replace all expressions with row expressions
@@ -385,6 +380,11 @@ public class PlanOptimizers
         // After this point, all planNodes should not contain OriginalExpression
 
         builder.add(
+                new IterativeOptimizer(
+                        ruleStats,
+                        statsCalculator,
+                        estimatedExchangesCostCalculator,
+                        new PickTableLayout(metadata).rules()),
                 new PruneUnreferencedOutputs(),
                 new IterativeOptimizer(
                         ruleStats,
@@ -439,7 +439,7 @@ public class PlanOptimizers
                 ruleStats,
                 statsCalculator,
                 estimatedExchangesCostCalculator,
-                new PickTableLayout(metadata, sqlParser).rules()));
+                new PickTableLayout(metadata).rules()));
 
         // PlanRemoteProjections only handles RowExpression so this need to run after TranslateExpressions
         // Rules applied after this need to handle locality of ProjectNode properly.
