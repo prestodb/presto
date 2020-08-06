53467ec1162faf148059b945860c809a8730316c
Shixuan Fan <shixuan@fb.com>
2020-08-06 11:15:46 -0700

    Support legacy date/timestamp to varchar coercion
    For certain connector migration, we are facing incompatible semantic
    where column type changed. This is to provide a legacy mode so
    that we could disaggregate connector migration from actual query
    migration.
    
    Warnings will be issued for queries relying on this legacy behavior.

--- presto-common/src/main/java/com/facebook/presto/common/function/SqlFunctionProperties.java
+++ presto-common/src/main/java/com/facebook/presto/common/function/SqlFunctionProperties.java
@@ -24,6 +24,7 @@ public class SqlFunctionProperties
 {
     private final boolean parseDecimalLiteralAsDouble;
     private final boolean legacyRowFieldOrdinalAccessEnabled;
+    private final boolean legacyTypeCoercionWarningEnabled;
     private final TimeZoneKey timeZoneKey;
     private final boolean legacyTimestamp;
     private final boolean legacyMapSubscript;
@@ -34,6 +35,7 @@ public class SqlFunctionProperties
     private SqlFunctionProperties(
             boolean parseDecimalLiteralAsDouble,
             boolean legacyRowFieldOrdinalAccessEnabled,
+            boolean legacyTypeCoercionWarningEnabled,
             TimeZoneKey timeZoneKey,
             boolean legacyTimestamp,
             boolean legacyMapSubscript,
@@ -43,6 +45,7 @@ public class SqlFunctionProperties
     {
         this.parseDecimalLiteralAsDouble = parseDecimalLiteralAsDouble;
         this.legacyRowFieldOrdinalAccessEnabled = legacyRowFieldOrdinalAccessEnabled;
+        this.legacyTypeCoercionWarningEnabled = legacyTypeCoercionWarningEnabled;
         this.timeZoneKey = requireNonNull(timeZoneKey, "timeZoneKey is null");
         this.legacyTimestamp = legacyTimestamp;
         this.legacyMapSubscript = legacyMapSubscript;
@@ -59,6 +62,11 @@ public class SqlFunctionProperties
     public boolean isLegacyRowFieldOrdinalAccessEnabled()
     {
         return legacyRowFieldOrdinalAccessEnabled;
+    }
+
+    public boolean isLegacyTypeCoercionWarningEnabled()
+    {
+        return legacyTypeCoercionWarningEnabled;
     }
 
     public TimeZoneKey getTimeZoneKey()
@@ -124,6 +132,7 @@ public class SqlFunctionProperties
     {
         private boolean parseDecimalLiteralAsDouble;
         private boolean legacyRowFieldOrdinalAccessEnabled;
+        private boolean legacyTypeCoercionWarningEnabled;
         private TimeZoneKey timeZoneKey;
         private boolean legacyTimestamp;
         private boolean legacyMapSubscript;
@@ -142,6 +151,12 @@ public class SqlFunctionProperties
         public Builder setLegacyRowFieldOrdinalAccessEnabled(boolean legacyRowFieldOrdinalAccessEnabled)
         {
             this.legacyRowFieldOrdinalAccessEnabled = legacyRowFieldOrdinalAccessEnabled;
+            return this;
+        }
+
+        public Builder setLegacyTypeCoercionWarningEnabled(boolean legacyTypeCoercionWarningEnabled)
+        {
+            this.legacyTypeCoercionWarningEnabled = legacyTypeCoercionWarningEnabled;
             return this;
         }
 
@@ -183,7 +198,7 @@ public class SqlFunctionProperties
 
         public SqlFunctionProperties build()
         {
-            return new SqlFunctionProperties(parseDecimalLiteralAsDouble, legacyRowFieldOrdinalAccessEnabled, timeZoneKey, legacyTimestamp, legacyMapSubscript, sessionStartTime, sessionLocale, sessionUser);
+            return new SqlFunctionProperties(parseDecimalLiteralAsDouble, legacyRowFieldOrdinalAccessEnabled, legacyTypeCoercionWarningEnabled, timeZoneKey, legacyTimestamp, legacyMapSubscript, sessionStartTime, sessionLocale, sessionUser);
         }
     }
 }
