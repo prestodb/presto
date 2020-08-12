61a7c7e077b450103c1beb5420347a568a8dd324
Sujay Jain <sujayjain@fb.com>
2020-08-12 09:21:14 -0700

    Fix OrcSelectivePageSource incorrect memory reporting
    Co-Authored-By: Saksham Sachdev <sakshams@fb.com>

--- presto-hive/src/main/java/com/facebook/presto/hive/orc/OrcSelectivePageSourceFactory.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/orc/OrcSelectivePageSourceFactory.java
@@ -404,7 +404,7 @@ public class OrcSelectivePageSourceFactory
             return new OrcSelectivePageSource(
                     recordReader,
                     orcDataSource,
-                    systemMemoryUsage.newOrcAggregatedMemoryContext(),
+                    systemMemoryUsage,
                     stats);
         }
         catch (Exception e) {
