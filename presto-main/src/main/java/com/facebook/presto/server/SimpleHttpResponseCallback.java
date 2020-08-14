6d30657779b3c8436c8d2e6c97ed99d30db05c21
Tim Meehan <tdm@fb.com>
2020-08-14 12:58:42 -0700

    Make SimpleHttpResponseHandler generic

--- presto-main/src/main/java/com/facebook/presto/server/SimpleHttpResponseCallback.java
+++ presto-main/src/main/java/com/facebook/presto/server/SimpleHttpResponseCallback.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.server.remotetask;
+package com.facebook.presto.server;
 
 public interface SimpleHttpResponseCallback<T>
 {
