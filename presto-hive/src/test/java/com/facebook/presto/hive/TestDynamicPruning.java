1bc26f0c8223d5fe52957b7b3778b626fb0fddd6
Ke Wang <ke1024@fb.com>
2020-09-08 22:22:42 -0700

    Dynamic partition pruning on workers

--- presto-hive/src/test/java/com/facebook/presto/hive/TestDynamicPruning.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestDynamicPruning.java
@@ -17,6 +17,7 @@ import com.facebook.airlift.testing.TempFile;
 import com.facebook.presto.common.predicate.Domain;
 import com.facebook.presto.common.predicate.TupleDomain;
 import com.facebook.presto.common.type.StandardTypes;
+import com.facebook.presto.common.type.TypeSignature;
 import com.facebook.presto.hive.metastore.Column;
 import com.facebook.presto.hive.metastore.Storage;
 import com.facebook.presto.hive.metastore.StorageFormat;
@@ -33,14 +34,18 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalInt;
 
+import static com.facebook.presto.common.predicate.Domain.singleValue;
 import static com.facebook.presto.common.type.IntegerType.INTEGER;
 import static com.facebook.presto.common.type.TypeSignature.parseTypeSignature;
+import static com.facebook.presto.common.type.VarcharType.createVarcharType;
 import static com.facebook.presto.expressions.LogicalRowExpressions.TRUE_CONSTANT;
 import static com.facebook.presto.hive.BucketFunctionType.HIVE_COMPATIBLE;
 import static com.facebook.presto.hive.CacheQuotaRequirement.NO_CACHE_REQUIREMENT;
+import static com.facebook.presto.hive.HiveColumnHandle.ColumnType.PARTITION_KEY;
 import static com.facebook.presto.hive.HiveColumnHandle.ColumnType.REGULAR;
 import static com.facebook.presto.hive.HiveQueryRunner.HIVE_CATALOG;
 import static com.facebook.presto.hive.HiveTestUtils.ROW_EXPRESSION_SERVICE;
@@ -50,20 +55,51 @@ import static com.facebook.presto.hive.HiveTestUtils.getDefaultHiveBatchPageSour
 import static com.facebook.presto.hive.HiveTestUtils.getDefaultHiveRecordCursorProvider;
 import static com.facebook.presto.hive.HiveTestUtils.getDefaultHiveSelectivePageSourceFactories;
 import static com.facebook.presto.hive.HiveType.HIVE_INT;
+import static com.facebook.presto.hive.HiveType.HIVE_STRING;
 import static com.facebook.presto.hive.TestHivePageSink.getColumnHandles;
 import static com.facebook.presto.spi.schedule.NodeSelectionStrategy.NO_PREFERENCE;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static io.airlift.slice.Slices.utf8Slice;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
-public class TestDynamicBucketPruning
+public class TestDynamicPruning
 {
     private static final String SCHEMA_NAME = "test";
     private static final String TABLE_NAME = "test";
     private static final Column BUCKET_COLUMN = new Column("l_orderkey", HIVE_INT, Optional.empty());
+    private static final Column PARTITION_COLUMN = new Column("ds", HIVE_STRING, Optional.empty());
+    private static final HiveColumnHandle PARTITION_HIVE_COLUMN_HANDLE = new HiveColumnHandle(
+            "ds",
+            HIVE_STRING,
+            TypeSignature.parseTypeSignature("varchar"),
+            1,
+            HiveColumnHandle.ColumnType.PARTITION_KEY,
+            Optional.empty(),
+            ImmutableList.of(),
+            Optional.empty());
 
     @Test
     public void testDynamicBucketPruning()
+    {
+        HiveClientConfig config = new HiveClientConfig();
+        MetastoreClientConfig metastoreClientConfig = new MetastoreClientConfig();
+        HiveTransactionHandle transaction = new HiveTransactionHandle();
+        try (TempFile tempFile = new TempFile()) {
+            ConnectorPageSource emptyPageSource = createTestingPageSource(transaction, config, new SplitContext(false, getToSkipTupleDomainForPartition()), metastoreClientConfig, tempFile.file());
+            assertEquals(emptyPageSource.getClass(), HiveEmptySplitPageSource.class);
+
+            ConnectorPageSource nonEmptyPageSource = createTestingPageSource(transaction, config, new SplitContext(false, getToKeepTupleDomainForPartition()), metastoreClientConfig, tempFile.file());
+            assertEquals(nonEmptyPageSource.getClass(), HivePageSource.class);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void testDynamicPartitionPruning()
     {
         HiveClientConfig config = new HiveClientConfig();
         MetastoreClientConfig metastoreClientConfig = new MetastoreClientConfig();
@@ -83,6 +119,8 @@ public class TestDynamicBucketPruning
 
     private static ConnectorPageSource createTestingPageSource(HiveTransactionHandle transaction, HiveClientConfig config, SplitContext splitContext, MetastoreClientConfig metastoreClientConfig, File outputFile)
     {
+        ImmutableList<HivePartitionKey> partitionKeys = ImmutableList.of(new HivePartitionKey(PARTITION_COLUMN.getName(), "2020-09-09"));
+        Map<Integer, Column> partitionSchemaDifference = ImmutableMap.of(1, new Column("ds", HIVE_STRING, Optional.empty()));
         HiveSplit split = new HiveSplit(
                 SCHEMA_NAME,
                 TABLE_NAME,
@@ -98,13 +136,13 @@ public class TestDynamicBucketPruning
                         false,
                         ImmutableMap.of(),
                         ImmutableMap.of()),
-                ImmutableList.of(),
+                partitionKeys,
                 ImmutableList.of(),
                 OptionalInt.of(1),
                 OptionalInt.of(1),
                 NO_PREFERENCE,
                 getColumnHandles().size(),
-                ImmutableMap.of(),
+                partitionSchemaDifference,
                 Optional.empty(),
                 false,
                 Optional.empty(),
@@ -118,7 +156,7 @@ public class TestDynamicBucketPruning
                 transaction,
                 Optional.of(new HiveTableLayoutHandle(
                         new SchemaTableName(SCHEMA_NAME, TABLE_NAME),
-                        ImmutableList.of(),
+                        ImmutableList.of(PARTITION_HIVE_COLUMN_HANDLE),
                         getColumnHandles().stream()
                                 .map(column -> new Column(column.getName(), column.getHiveType(), Optional.empty()))
                                 .collect(toImmutableList()),
@@ -152,6 +190,21 @@ public class TestDynamicBucketPruning
                         Domain.singleValue(INTEGER, 10L)));
     }
 
+    private TupleDomain<ColumnHandle> getToSkipTupleDomainForPartition()
+    {
+        return TupleDomain.withColumnDomains(
+                ImmutableMap.of(
+                        new HiveColumnHandle(
+                                PARTITION_COLUMN.getName(),
+                                PARTITION_COLUMN.getType(),
+                                parseTypeSignature(StandardTypes.VARCHAR),
+                                1,
+                                PARTITION_KEY,
+                                Optional.empty(),
+                                Optional.empty()),
+                        singleValue(createVarcharType(15), utf8Slice("2020-09-08"))));
+    }
+
     private static TupleDomain<ColumnHandle> getToKeepTupleDomain()
     {
         return TupleDomain.withColumnDomains(
@@ -165,6 +218,21 @@ public class TestDynamicBucketPruning
                                 Optional.empty(),
                                 Optional.empty()),
                         Domain.singleValue(INTEGER, 1L)));
+    }
+
+    private TupleDomain<ColumnHandle> getToKeepTupleDomainForPartition()
+    {
+        return TupleDomain.withColumnDomains(
+                ImmutableMap.of(
+                        new HiveColumnHandle(
+                                PARTITION_COLUMN.getName(),
+                                PARTITION_COLUMN.getType(),
+                                parseTypeSignature(StandardTypes.VARCHAR),
+                                1,
+                                PARTITION_KEY,
+                                Optional.empty(),
+                                Optional.empty()),
+                        singleValue(createVarcharType(15), utf8Slice("2020-09-09"))));
     }
 
     private static TestingConnectorSession getSession(HiveClientConfig config)
