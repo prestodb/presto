7b428712b32f61f9f0d81cd2f3f9b24e70544340
Palash Goel <palashgoel7@gmail.com>
2020-09-18 22:59:13 +0530

    [Verifier] Add application-name config

--- presto-verifier/src/main/java/com/facebook/presto/verifier/prestoaction/JdbcPrestoAction.java
+++ presto-verifier/src/main/java/com/facebook/presto/verifier/prestoaction/JdbcPrestoAction.java
@@ -64,6 +64,7 @@ public class JdbcPrestoAction
     private final Duration checksumTimeout;
     private final String testId;
     private final Optional<String> testName;
+    private final String applicationName;
 
     private final RetryDriver<QueryException> networkRetry;
     private final RetryDriver<QueryException> prestoRetry;
@@ -85,6 +86,7 @@ public class JdbcPrestoAction
 
         this.jdbcUrls = requireNonNull(cycle(prestoActionConfig.getJdbcUrls()), "jdbcUrls is null");
         this.queryTimeout = requireNonNull(prestoActionConfig.getQueryTimeout(), "queryTimeout is null");
+        this.applicationName = requireNonNull(prestoActionConfig.getApplicationName(), "applicationName is null");
         this.metadataTimeout = requireNonNull(metadataTimeout, "metadataTimeout is null");
         this.checksumTimeout = requireNonNull(checksumTimeout, "checksumTimeout is null");
         this.testId = requireNonNull(verifierConfig.getTestId(), "testId is null");
@@ -154,7 +156,7 @@ public class JdbcPrestoAction
                 .unwrap(PrestoConnection.class);
 
         try {
-            connection.setClientInfo("ApplicationName", "verifier-test");
+            connection.setClientInfo("ApplicationName", applicationName);
             connection.setClientInfo("ClientInfo", clientInfo);
             connection.setCatalog(queryConfiguration.getCatalog());
             connection.setSchema(queryConfiguration.getSchema());
