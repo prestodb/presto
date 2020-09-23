6a005df78c5162a55d06fa567b33027db553a371
Nikhil Collooru <nikhilcollooru@fb.com>
2020-09-22 21:52:56 -0700

    Handle storage connection exceptions correctly

--- presto-hive/src/main/java/com/facebook/presto/hive/orc/HdfsOrcDataSource.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/orc/HdfsOrcDataSource.java
@@ -13,7 +13,6 @@
  */
 package com.facebook.presto.hive.orc;
 
-import com.facebook.presto.common.FileSystemCommunicationException;
 import com.facebook.presto.hive.FileFormatDataSourceStats;
 import com.facebook.presto.orc.AbstractOrcDataSource;
 import com.facebook.presto.orc.OrcDataSourceId;
@@ -23,6 +22,7 @@ import org.apache.hadoop.fs.FSDataInputStream;
 
 import java.io.IOException;
 
+import static com.facebook.presto.hive.HiveErrorCode.HIVE_FILESYSTEM_ERROR;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_MISSING_DATA;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_UNKNOWN_ERROR;
 import static java.lang.String.format;
@@ -74,7 +74,7 @@ public class HdfsOrcDataSource
                 throw new PrestoException(HIVE_MISSING_DATA, message, e);
             }
             if (e instanceof IOException) {
-                throw new FileSystemCommunicationException(message, e);
+                throw new PrestoException(HIVE_FILESYSTEM_ERROR, message, e);
             }
             throw new PrestoException(HIVE_UNKNOWN_ERROR, message, e);
         }
