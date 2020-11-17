aaef61f1e706e3a7a108444990355a3cf647886b
Rongrong Zhong <rongrong100@gmail.com>
2020-11-16 21:39:51 -0800

    Support Serialized Presto Page in Thrift UDF service
    PrestoThriftBlock has limited type support (only supports array<bigint> for
    array types, no support for map types). The overall performance of Presto
    SerializedPage is better than using PrestoThriftBLock.
    
    Benchmark                                         (typeSignature)  Mode  Cnt        Score        Error  Units
    BenchmarkThriftPageSerDe.testSerializedPageSerde          boolean  avgt   20   166566.169 ±  18267.340  ns/op
    BenchmarkThriftPageSerDe.testSerializedPageSerde           bigint  avgt   20   216654.607 ±  32235.276  ns/op
    BenchmarkThriftPageSerDe.testSerializedPageSerde          varchar  avgt   20  1250560.212 ± 176070.250  ns/op
    BenchmarkThriftPageSerDe.testSerializedPageSerde    array<bigint>  avgt   20   488109.753 ±  55165.876  ns/op
    BenchmarkThriftPageSerDe.testSerializedPageSerde   map<int, real>  avgt   20   633627.296 ±  73473.657  ns/op
    BenchmarkThriftPageSerDe.testThriftPageSerde              boolean  avgt   20   335448.969 ±  22666.853  ns/op
    BenchmarkThriftPageSerDe.testThriftPageSerde               bigint  avgt   20   432328.947 ±  46902.591  ns/op
    BenchmarkThriftPageSerDe.testThriftPageSerde              varchar  avgt   20  1658012.817 ± 196150.353  ns/op
    BenchmarkThriftPageSerDe.testThriftPageSerde        array<bigint>  avgt   20  1291312.373 ± 128077.209  ns/op
    BenchmarkThriftPageSerDe.testThriftPageSerde       map<int, real>  avgt   20        2.971 ±      0.373  ns/op

--- presto-thrift-testing-udf-server/src/main/java/com/facebook/presto/udf/thrift/EchoFirstInputThriftUdfService.java
+++ presto-thrift-testing-udf-server/src/main/java/com/facebook/presto/udf/thrift/EchoFirstInputThriftUdfService.java
@@ -13,6 +13,9 @@
  */
 package com.facebook.presto.udf.thrift;
 
+import com.facebook.presto.common.Page;
+import com.facebook.presto.common.block.BlockEncodingSerde;
+import com.facebook.presto.spi.page.PagesSerde;
 import com.facebook.presto.thrift.api.udf.ThriftFunctionHandle;
 import com.facebook.presto.thrift.api.udf.ThriftUdfPage;
 import com.facebook.presto.thrift.api.udf.ThriftUdfResult;
@@ -20,22 +23,45 @@ import com.facebook.presto.thrift.api.udf.ThriftUdfService;
 import com.facebook.presto.thrift.api.udf.ThriftUdfStats;
 import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.inject.Inject;
 
-import static com.facebook.presto.thrift.api.udf.ThriftUdfPageFormat.PRESTO_THRIFT;
-import static com.google.common.base.Preconditions.checkArgument;
+import java.util.Optional;
+
+import static com.facebook.presto.thrift.api.udf.ThriftUdfPage.prestoPage;
+import static com.facebook.presto.thrift.api.udf.ThriftUdfPage.thriftPage;
 import static com.google.common.util.concurrent.Futures.immediateFuture;
 
 public class EchoFirstInputThriftUdfService
         implements ThriftUdfService
 {
+    private final BlockEncodingSerde blockEncodingSerde;
+
+    @Inject
+    public EchoFirstInputThriftUdfService(BlockEncodingSerde blockEncodingSerde)
+    {
+        this.blockEncodingSerde = blockEncodingSerde;
+    }
+
     @Override
     public ListenableFuture<ThriftUdfResult> invokeUdf(
             ThriftFunctionHandle functionHandle,
             ThriftUdfPage inputs)
     {
-        checkArgument(inputs.getPageFormat().equals(PRESTO_THRIFT));
+        ThriftUdfPage result;
+        switch (inputs.getPageFormat()) {
+            case PRESTO_THRIFT:
+                result = thriftPage(ImmutableList.of(inputs.getThriftBlocks().get(0)));
+                break;
+            case PRESTO_SERIALIZED:
+                PagesSerde pagesSerde = new PagesSerde(blockEncodingSerde, Optional.empty(), Optional.empty(), Optional.empty());
+                Page page = pagesSerde.deserialize(inputs.getPrestoPage().toSerializedPage());
+                result = prestoPage(pagesSerde.serialize(new Page(page.getBlock(0))));
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
         return immediateFuture(new ThriftUdfResult(
-                new ThriftUdfPage(PRESTO_THRIFT, ImmutableList.of(inputs.getThriftBlocks().get(0))),
+                result,
                 new ThriftUdfStats(0)));
     }
 }
