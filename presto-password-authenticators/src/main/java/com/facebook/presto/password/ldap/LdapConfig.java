b7135a7d55b84e8f4a59dfcdd00a2fef891ac42a
imjalpreet <jalpreetnanda@gmail.com>
2020-12-08 00:34:29 +0530

    Add file based password authenticator plugin
    Cherry-pick of https://github.com/prestosql/presto/pull/1912 (https://github.com/prestosql/presto/pull/1912)
    
    Co-authored-by: David Phillips <david@acz.org>
    Co-authored-by: Rupam Kundu <rkundu@qubole.com>

--- presto-password-authenticators/src/main/java/com/facebook/presto/password/ldap/LdapConfig.java
+++ presto-password-authenticators/src/main/java/com/facebook/presto/password/ldap/LdapConfig.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.password;
+package com.facebook.presto.password.ldap;
 
 import com.facebook.airlift.configuration.Config;
 import com.facebook.airlift.configuration.ConfigDescription;
