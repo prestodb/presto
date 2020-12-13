3efa69108d921641dff16fb53ebc48991dac8f96
Tim Meehan <tdm@fb.com>
2020-12-13 11:04:26 -0800

    Determine reserved pool eligibility in resource manager

--- presto-spi/src/main/java/com/facebook/presto/spi/memory/ClusterMemoryPoolInfo.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/memory/ClusterMemoryPoolInfo.java
@@ -16,8 +16,11 @@ package com.facebook.presto.spi.memory;
 import com.facebook.drift.annotations.ThriftConstructor;
 import com.facebook.drift.annotations.ThriftField;
 import com.facebook.drift.annotations.ThriftStruct;
+import com.facebook.presto.spi.QueryId;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
@@ -27,17 +30,28 @@ public final class ClusterMemoryPoolInfo
     private final MemoryPoolInfo memoryPoolInfo;
     private final int blockedNodes;
     private final int assignedQueries;
+    private final Optional<QueryId> largestMemoryQuery;
+
+    public ClusterMemoryPoolInfo(
+            MemoryPoolInfo memoryPoolInfo,
+            int blockedNodes,
+            int assignedQueries)
+    {
+        this(memoryPoolInfo, blockedNodes, assignedQueries, Optional.empty());
+    }
 
     @ThriftConstructor
     @JsonCreator
     public ClusterMemoryPoolInfo(
             @JsonProperty("memoryPoolInfo") MemoryPoolInfo memoryPoolInfo,
             int blockedNodes,
-            int assignedQueries)
+            int assignedQueries,
+            Optional<QueryId> largestMemoryQuery)
     {
         this.memoryPoolInfo = requireNonNull(memoryPoolInfo, "memoryPoolInfo is null");
         this.blockedNodes = blockedNodes;
         this.assignedQueries = assignedQueries;
+        this.largestMemoryQuery = largestMemoryQuery;
     }
 
     @ThriftField(1)
@@ -59,5 +73,12 @@ public final class ClusterMemoryPoolInfo
     public int getAssignedQueries()
     {
         return assignedQueries;
+    }
+
+    @ThriftField(4)
+    @JsonProperty
+    public Optional<QueryId> getLargestMemoryQuery()
+    {
+        return largestMemoryQuery;
     }
 }
