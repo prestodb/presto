3cb1ee2d8f412b82baf6b9d23ce0e56755ef16d5
Xinli shang <shangx@uber.com>
2021-01-05 09:21:39 -0800

    Skip reading Parquet pages using Column Indexes feature
    Port some code from parquet-mr repo https://github.com/apache/parquet-mr
    Co-authored-by: Gabor Szadovszky <gabor.szadovszky@cloudera.com>
    
    More details about Parquet Column Indexes feature:
    
    Column Indexes also named as page level indexes that have min/max values for each page in a given column chunk. When reading pages, a reader doesn't need to process the page header to determine whether the page could be skipped based on the statistics. More information about this feature can be found https://github.com/apache/parquet-format/blob/master/PageIndex.md

--- presto-parquet/src/main/java/com/facebook/presto/parquet/predicate/Predicate.java
+++ presto-parquet/src/main/java/com/facebook/presto/parquet/predicate/Predicate.java
@@ -17,8 +17,10 @@ import com.facebook.presto.parquet.ParquetCorruptionException;
 import com.facebook.presto.parquet.ParquetDataSourceId;
 import org.apache.parquet.column.ColumnDescriptor;
 import org.apache.parquet.column.statistics.Statistics;
+import org.apache.parquet.internal.filter2.columnindex.ColumnIndexStore;
 
 import java.util.Map;
+import java.util.Optional;
 
 public interface Predicate
 {
@@ -33,6 +35,12 @@ public interface Predicate
 
         @Override
         public boolean matches(DictionaryDescriptor dictionary)
+        {
+            return true;
+        }
+
+        @Override
+        public boolean matches(long numberOfRows, Optional<ColumnIndexStore> columnIndexStore)
         {
             return true;
         }
@@ -57,4 +65,13 @@ public interface Predicate
      * @param dictionary The single column dictionary
      */
     boolean matches(DictionaryDescriptor dictionary);
+
+    /**
+     * Should the Parquet Reader process a file section with the specified statistics.
+     *
+     * @param numberOfRows the number of rows in the segment; this can be used with
+     * Statistics to determine if a column is only null
+     * @param columnIndexStore column index (statistics) store
+     */
+    boolean matches(long numberOfRows, Optional<ColumnIndexStore> columnIndexStore);
 }
