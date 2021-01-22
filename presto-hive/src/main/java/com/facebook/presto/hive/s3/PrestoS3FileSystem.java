779aff2120720adb7f7cf73a53c2eaacc4ff0900
Ashish Tadose <ashishtadose@gmail.com>
2021-01-22 20:46:18 +0530

    Fix iam role session name for s3 access

--- presto-hive/src/main/java/com/facebook/presto/hive/s3/PrestoS3FileSystem.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/s3/PrestoS3FileSystem.java
@@ -106,6 +106,7 @@ import static com.facebook.presto.hive.s3.S3ConfigurationUpdater.S3_CREDENTIALS_
 import static com.facebook.presto.hive.s3.S3ConfigurationUpdater.S3_ENCRYPTION_MATERIALS_PROVIDER;
 import static com.facebook.presto.hive.s3.S3ConfigurationUpdater.S3_ENDPOINT;
 import static com.facebook.presto.hive.s3.S3ConfigurationUpdater.S3_IAM_ROLE;
+import static com.facebook.presto.hive.s3.S3ConfigurationUpdater.S3_IAM_ROLE_SESSION_NAME;
 import static com.facebook.presto.hive.s3.S3ConfigurationUpdater.S3_KMS_KEY_ID;
 import static com.facebook.presto.hive.s3.S3ConfigurationUpdater.S3_MAX_BACKOFF_TIME;
 import static com.facebook.presto.hive.s3.S3ConfigurationUpdater.S3_MAX_CLIENT_RETRIES;
@@ -176,6 +177,7 @@ public class PrestoS3FileSystem
     private Duration maxRetryTime;
     private boolean useInstanceCredentials;
     private String s3IamRole;
+    private String s3IamRoleSessionName;
     private boolean pinS3ClientToCurrentRegion;
     private boolean sseEnabled;
     private PrestoS3SseType sseType;
@@ -214,6 +216,7 @@ public class PrestoS3FileSystem
         this.useInstanceCredentials = conf.getBoolean(S3_USE_INSTANCE_CREDENTIALS, defaults.isS3UseInstanceCredentials());
         this.pinS3ClientToCurrentRegion = conf.getBoolean(S3_PIN_CLIENT_TO_CURRENT_REGION, defaults.isPinS3ClientToCurrentRegion());
         this.s3IamRole = conf.get(S3_IAM_ROLE, defaults.getS3IamRole());
+        this.s3IamRoleSessionName = conf.get(S3_IAM_ROLE_SESSION_NAME, defaults.getS3IamRoleSessionName());
         verify(!(useInstanceCredentials && conf.get(S3_IAM_ROLE) != null),
                 "Invalid configuration: either use instance credentials or specify an iam role");
         verify((pinS3ClientToCurrentRegion && conf.get(S3_ENDPOINT) == null) || !pinS3ClientToCurrentRegion,
@@ -801,7 +804,7 @@ public class PrestoS3FileSystem
         }
 
         if (s3IamRole != null) {
-            return new STSAssumeRoleSessionCredentialsProvider.Builder(s3IamRole, "presto-" + uri).build();
+            return new STSAssumeRoleSessionCredentialsProvider.Builder(s3IamRole, s3IamRoleSessionName).build();
         }
 
         String providerClass = conf.get(S3_CREDENTIALS_PROVIDER);
