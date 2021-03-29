prestodb/presto

commit a66d9124d698cdf7bb0c4107a6a89e70f35b1c8a

Enforce fixed distribution for output operator
Output operator in Spark does rows compaction. It has to accumulate enough
rows to ensure at least 1kb of data per partition is being written to the
shuffle service in a single call. Without a local exchange the output operator
will inherit the table scan driver lifecycle, effectively buffering only the
rows produced by a single split. If splits are small that results into very
tiny batched being flushed to the shuffle service

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/AddLocalExchanges.java
@@ -68,6 +68,7 @@ import static com.facebook.presto.SystemSessionProperties.getTaskConcurrency;
 import static com.facebook.presto.SystemSessionProperties.getTaskPartitionedWriterCount;
 import static com.facebook.presto.SystemSessionProperties.getTaskWriterCount;
 import static com.facebook.presto.SystemSessionProperties.isDistributedSortEnabled;
+import static com.facebook.presto.SystemSessionProperties.isEnforceFixedDistributionForOutputOperator;
 import static com.facebook.presto.SystemSessionProperties.isJoinSpillingEnabled;
 import static com.facebook.presto.SystemSessionProperties.isSpillEnabled;
 import static com.facebook.presto.SystemSessionProperties.isTableWriterMergeOperatorEnabled;
@@ -611,6 +612,9 @@ public class AddLocalExchanges
                         node,
                         any().withOrderSensitivity(),
                         any().withOrderSensitivity());
+            }
+            if (isEnforceFixedDistributionForOutputOperator(session)) {
+                return planAndEnforceChildren(node, fixedParallelism(), fixedParallelism());
             }
             return planAndEnforceChildren(node, any(), defaultParallelism(session));
         }

presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
@@ -173,6 +173,7 @@ public final class SystemSessionProperties
     public static final String CHECK_ACCESS_CONTROL_ON_UTILIZED_COLUMNS_ONLY = "check_access_control_on_utilized_columns_only";
     public static final String SKIP_REDUNDANT_SORT = "skip_redundant_sort";
     public static final String ALLOW_WINDOW_ORDER_BY_LITERALS = "allow_window_order_by_literals";
+    public static final String ENFORCE_FIXED_DISTRIBUTION_FOR_OUTPUT_OPERATOR = "enforce_fixed_distribution_for_output_operator";
 
     private final List<PropertyMetadata<?>> sessionProperties;
 
@@ -902,7 +903,12 @@ public final class SystemSessionProperties
                         ALLOW_WINDOW_ORDER_BY_LITERALS,
                         "Allow ORDER BY literals in window functions",
                         featuresConfig.isAllowWindowOrderByLiterals(),
-                        false));
+                        false),
+                booleanProperty(
+                        ENFORCE_FIXED_DISTRIBUTION_FOR_OUTPUT_OPERATOR,
+                        "Enforce fixed distribution for output operator",
+                        featuresConfig.isEnforceFixedDistributionForOutputOperator(),
+                        true));
     }
 
     public static boolean isSkipRedundantSort(Session session)
@@ -1527,5 +1533,10 @@ public final class SystemSessionProperties
     public static boolean isCheckAccessControlOnUtilizedColumnsOnly(Session session)
     {
         return session.getSystemProperty(CHECK_ACCESS_CONTROL_ON_UTILIZED_COLUMNS_ONLY, Boolean.class);
+    }
+
+    public static boolean isEnforceFixedDistributionForOutputOperator(Session session)
+    {
+        return session.getSystemProperty(ENFORCE_FIXED_DISTRIBUTION_FOR_OUTPUT_OPERATOR, Boolean.class);
     }
 }

presto-main/src/main/java/com/facebook/presto/sql/analyzer/FeaturesConfig.java
@@ -180,6 +180,8 @@ public class FeaturesConfig
 
     private PartitioningPrecisionStrategy partitioningPrecisionStrategy = PartitioningPrecisionStrategy.AUTOMATIC;
 
+    private boolean enforceFixedDistributionForOutputOperator;
+
     public enum PartitioningPrecisionStrategy
     {
         // Let Presto decide when to repartition
@@ -1514,6 +1516,18 @@ public class FeaturesConfig
     public FeaturesConfig setAllowWindowOrderByLiterals(boolean value)
     {
         this.isAllowWindowOrderByLiterals = value;
+        return this;
+    }
+
+    public boolean isEnforceFixedDistributionForOutputOperator()
+    {
+        return enforceFixedDistributionForOutputOperator;
+    }
+
+    @Config("enforce-fixed-distribution-for-output-operator")
+    public FeaturesConfig setEnforceFixedDistributionForOutputOperator(boolean enforceFixedDistributionForOutputOperator)
+    {
+        this.enforceFixedDistributionForOutputOperator = enforceFixedDistributionForOutputOperator;
         return this;
     }
 }

presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
@@ -154,7 +154,8 @@ public class TestFeaturesConfig
                 .setWarnOnNoTableLayoutFilter("")
                 .setInlineSqlFunctions(true)
                 .setCheckAccessControlOnUtilizedColumnsOnly(false)
-                .setAllowWindowOrderByLiterals(true));
+                .setAllowWindowOrderByLiterals(true)
+                .setEnforceFixedDistributionForOutputOperator(false));
     }
 
     @Test
@@ -262,6 +263,7 @@ public class TestFeaturesConfig
                 .put("check-access-control-on-utilized-columns-only", "true")
                 .put("optimizer.skip-redundant-sort", "false")
                 .put("is-allow-window-order-by-literals", "false")
+                .put("enforce-fixed-distribution-for-output-operator", "true")
                 .build();
 
         FeaturesConfig expected = new FeaturesConfig()
@@ -366,7 +368,8 @@ public class TestFeaturesConfig
                 .setInlineSqlFunctions(false)
                 .setCheckAccessControlOnUtilizedColumnsOnly(true)
                 .setSkipRedundantSort(false)
-                .setAllowWindowOrderByLiterals(false);
+                .setAllowWindowOrderByLiterals(false)
+                .setEnforceFixedDistributionForOutputOperator(true);
         assertFullMapping(properties, expected);
     }
 

presto-spark-base/src/main/java/com/facebook/presto/spark/PrestoSparkSettingsRequirements.java
@@ -89,6 +89,7 @@ public class PrestoSparkSettingsRequirements
         config.setPreferDistributedUnion(true);
         config.setForceSingleNodeOutput(false);
         config.setInlineSqlFunctions(true);
+        config.setEnforceFixedDistributionForOutputOperator(true);
     }
 
     public static void setDefaults(QueryManagerConfig config)

commit f9a602076607ad026d8f1513341d2b6cbc563e99

Do not propagate streaming preference from remote exchnage node
Setting streaming preference propagates it down the plan and results
into a local exchange node being introduced right after the TableScan
node. This changes the lifecycle of the partial aggregation (or partial
window function) operator from "per split" to "per task". Unfrotunately
some of the "partial" operators, such as partial TopNRowNumber does not
enforce memory limit for the "partial" buffer, resulting into premature
memory exceeding errors. While not enforcing a buffer size is a problem
by itself, this change doesn't try to address that, but instead fixes a
regression introduced by https://github.com/prestodb/presto/pull/15780

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/AddLocalExchanges.java
@@ -613,10 +613,10 @@ public class AddLocalExchanges
                         any().withOrderSensitivity(),
                         any().withOrderSensitivity());
             }
-            if (isEnforceFixedDistributionForOutputOperator(session)) {
-                return planAndEnforceChildren(node, fixedParallelism(), fixedParallelism());
-            }
-            return planAndEnforceChildren(node, any(), defaultParallelism(session));
+            return planAndEnforceChildren(
+                    node,
+                    isEnforceFixedDistributionForOutputOperator(session) ? fixedParallelism() : any(),
+                    defaultParallelism(session));
         }
 
         @Override

presto-main/src/test/java/com/facebook/presto/sql/planner/TestLogicalPlanner.java
@@ -47,11 +47,13 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import static com.facebook.presto.SystemSessionProperties.DISTRIBUTED_SORT;
+import static com.facebook.presto.SystemSessionProperties.ENFORCE_FIXED_DISTRIBUTION_FOR_OUTPUT_OPERATOR;
 import static com.facebook.presto.SystemSessionProperties.FORCE_SINGLE_NODE_OUTPUT;
 import static com.facebook.presto.SystemSessionProperties.JOIN_DISTRIBUTION_TYPE;
 import static com.facebook.presto.SystemSessionProperties.OPTIMIZE_HASH_GENERATION;
 import static com.facebook.presto.SystemSessionProperties.OPTIMIZE_JOINS_WITH_EMPTY_SOURCES;
 import static com.facebook.presto.SystemSessionProperties.OPTIMIZE_NULLS_IN_JOINS;
+import static com.facebook.presto.SystemSessionProperties.TASK_CONCURRENCY;
 import static com.facebook.presto.common.block.SortOrder.ASC_NULLS_LAST;
 import static com.facebook.presto.common.predicate.Domain.singleValue;
 import static com.facebook.presto.common.type.BigintType.BIGINT;
@@ -1227,5 +1229,53 @@ public class TestLogicalPlanner
                                                 "region",
                                                 ImmutableMap.of(
                                                         "REGION_REGIONKEY", "regionkey"))))));
+    }
+
+    @Test
+    public void testEnforceFixedDistributionForOutputOperator()
+    {
+        Session session = Session.builder(this.getQueryRunner().getDefaultSession())
+                // enable concurrency (default is 1)
+                .setSystemProperty(TASK_CONCURRENCY, "2")
+                .setSystemProperty(ENFORCE_FIXED_DISTRIBUTION_FOR_OUTPUT_OPERATOR, "true")
+                .build();
+
+        // simple group by
+        assertDistributedPlan(
+                "SELECT orderstatus, sum(totalprice) FROM orders GROUP BY orderstatus",
+                session,
+                anyTree(
+                        aggregation(
+                                ImmutableMap.of("final_sum", functionCall("sum", ImmutableList.of("partial_sum"))),
+                                FINAL,
+                                exchange(LOCAL, REPARTITION,
+                                        exchange(REMOTE_STREAMING, REPARTITION,
+                                                exchange(LOCAL, REPARTITION,
+                                                        aggregation(
+                                                                ImmutableMap.of("partial_sum", functionCall("sum", ImmutableList.of("totalprice"))),
+                                                                PARTIAL,
+                                                                project(tableScan("orders", ImmutableMap.of("totalprice", "totalprice"))))))))));
+
+        assertDistributedPlan(
+                "SELECT orderstatus FROM (SELECT orderstatus, row_number() OVER (PARTITION BY orderstatus ORDER BY custkey) n FROM orders) WHERE n = 1",
+                session,
+                anyTree(
+                        topNRowNumber(topNRowNumber -> topNRowNumber
+                                        .specification(
+                                                ImmutableList.of("orderstatus"),
+                                                ImmutableList.of("custkey"),
+                                                ImmutableMap.of("custkey", ASC_NULLS_LAST))
+                                        .partial(false),
+                                exchange(LOCAL, REPARTITION,
+                                        exchange(REMOTE_STREAMING, REPARTITION,
+                                                exchange(LOCAL, REPARTITION,
+                                                        topNRowNumber(topNRowNumber -> topNRowNumber
+                                                                        .specification(
+                                                                                ImmutableList.of("orderstatus"),
+                                                                                ImmutableList.of("custkey"),
+                                                                                ImmutableMap.of("custkey", ASC_NULLS_LAST))
+                                                                        .partial(true),
+
+                                                                project(tableScan("orders", ImmutableMap.of("orderstatus", "orderstatus", "custkey", "custkey"))))))))));
     }
 }