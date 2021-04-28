77989bf483be56098af47a662a1b549fea6d2874
v-jizhang <jzhang@openteams.com>
2021-04-28 08:57:10 -0700

    Add max error retry config option to glue client
    Cherry pick of https://github.com/trinodb/trino/pull/4611 and
    https://github.com/vincentpoon/prestosql/commit/c1ac9ac257bc5a07f32a359c7aac6735fd6ef69f#diff-824c0eed6933e963a65ba98d9bc3582e5f3f96fbd78982996327b10e54bd5c67
    
    Co-authored-by: Vincent Poon <vincent.poon@salesforce.com>
    Co-authored-by: Philippe Gagnon <pgagnon@users.noreply.github.com>

--- presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/glue/GlueHiveMetastoreConfig.java
+++ presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/glue/GlueHiveMetastoreConfig.java
@@ -26,6 +26,7 @@ public class GlueHiveMetastoreConfig
     private Optional<String> glueRegion = Optional.empty();
     private Optional<String> glueEndpointUrl = Optional.empty();
     private boolean pinGlueClientToCurrentRegion;
+    private int maxGlueErrorRetries = 10;
     private int maxGlueConnections = 5;
     private Optional<String> defaultWarehouseDir = Optional.empty();
     private Optional<String> catalogId = Optional.empty();
@@ -83,6 +84,19 @@ public class GlueHiveMetastoreConfig
     public GlueHiveMetastoreConfig setMaxGlueConnections(int maxGlueConnections)
     {
         this.maxGlueConnections = maxGlueConnections;
+        return this;
+    }
+
+    @Min(0)
+    public int getMaxGlueErrorRetries()
+    {
+        return maxGlueErrorRetries;
+    }
+
+    @Config("hive.metastore.glue.max-error-retries")
+    public GlueHiveMetastoreConfig setMaxGlueErrorRetries(int maxGlueErrorRetries)
+    {
+        this.maxGlueErrorRetries = maxGlueErrorRetries;
         return this;
     }
 
