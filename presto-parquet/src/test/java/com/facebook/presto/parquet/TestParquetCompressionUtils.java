b4bc82ea4584405917816d33abb33bd08d47f570
Xinli shang <shangx@uber.com>
2021-05-12 10:05:16 -0700

    Use static imports

--- presto-parquet/src/test/java/com/facebook/presto/parquet/TestParquetCompressionUtils.java
+++ presto-parquet/src/test/java/com/facebook/presto/parquet/TestParquetCompressionUtils.java
@@ -23,6 +23,8 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 import static io.airlift.slice.Slices.wrappedBuffer;
+import static org.apache.parquet.hadoop.metadata.CompressionCodecName.LZ4;
+import static org.apache.parquet.hadoop.metadata.CompressionCodecName.ZSTD;
 import static org.testng.Assert.assertEquals;
 
 public class TestParquetCompressionUtils
@@ -31,36 +33,36 @@ public class TestParquetCompressionUtils
     public void testDecompressZSTD()
             throws IOException
     {
-        performTest(CompressionCodecName.ZSTD, 0);
-        performTest(CompressionCodecName.ZSTD, 1);
-        performTest(CompressionCodecName.ZSTD, 100);
-        performTest(CompressionCodecName.ZSTD, 256);
-        performTest(CompressionCodecName.ZSTD, 512);
-        performTest(CompressionCodecName.ZSTD, 1024);
-        performTest(CompressionCodecName.ZSTD, 2048);
+        performTest(ZSTD, 0);
+        performTest(ZSTD, 1);
+        performTest(ZSTD, 100);
+        performTest(ZSTD, 256);
+        performTest(ZSTD, 512);
+        performTest(ZSTD, 1024);
+        performTest(ZSTD, 2048);
     }
 
     @Test
     public void testDecompressLZ4()
             throws IOException
     {
-        performTest(CompressionCodecName.LZ4, 0);
-        performTest(CompressionCodecName.LZ4, 1);
-        performTest(CompressionCodecName.LZ4, 100);
-        performTest(CompressionCodecName.LZ4, 256);
-        performTest(CompressionCodecName.LZ4, 512);
-        performTest(CompressionCodecName.LZ4, 1024);
-        performTest(CompressionCodecName.LZ4, 2048);
+        performTest(LZ4, 0);
+        performTest(LZ4, 1);
+        performTest(LZ4, 100);
+        performTest(LZ4, 256);
+        performTest(LZ4, 512);
+        performTest(LZ4, 1024);
+        performTest(LZ4, 2048);
     }
 
     private void performTest(CompressionCodecName codec, int inputLength)
             throws IOException
     {
         Compressor compressor = null;
-        if (codec.equals(CompressionCodecName.ZSTD)) {
+        if (codec.equals(ZSTD)) {
             compressor = new ZstdCompressor();
         }
-        else if (codec.equals(CompressionCodecName.LZ4)) {
+        else if (codec.equals(LZ4)) {
             compressor = new Lz4Compressor();
         }
 
