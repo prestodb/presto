b3fd4dbaab080f0e4f1b033693cd632b9d865e1b
Sergii Druzkin <sdruzkin@fb.com>
2021-05-21 18:24:39 -0700

    Add StripeMetadataSourceFactory to consume loaded DWRF stripe cache

--- presto-orc/src/main/java/com/facebook/presto/orc/metadata/DwrfStripeCacheMode.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/metadata/DwrfStripeCacheMode.java
@@ -19,14 +19,65 @@ package com.facebook.presto.orc.metadata;
 public enum DwrfStripeCacheMode
 {
     // no cache
-    NONE,
+    NONE(false, false),
 
     // cache contains stripe indexes
-    INDEX,
+    INDEX(true, false),
 
     // cache contains stripe footers
-    FOOTER,
+    FOOTER(false, true),
 
     // cache contains stripe indexes and footers
-    INDEX_AND_FOOTER;
+    INDEX_AND_FOOTER(true, true);
+
+    private final boolean hasIndex;
+    private final boolean hasFooter;
+
+    DwrfStripeCacheMode(boolean hasIndex, boolean hasFooter)
+    {
+        this.hasIndex = hasIndex;
+        this.hasFooter = hasFooter;
+    }
+
+    public boolean hasIndex()
+    {
+        return hasIndex;
+    }
+
+    public boolean hasFooter()
+    {
+        return hasFooter;
+    }
+
+    /**
+     * Get the position of an element in the cacheOffsets that can be used to
+     * get the offset for the index streams slice for a given stripe.
+     */
+    public int getIndexOffsetPosition(int stripeOrdinal)
+    {
+        switch (this) {
+            case INDEX:
+                return stripeOrdinal;
+            case INDEX_AND_FOOTER:
+                return stripeOrdinal * 2;
+            default:
+                throw new IllegalStateException("This mode doesn't support index cache: " + this);
+        }
+    }
+
+    /**
+     * Get the position of an element in the cacheOffsets that can be used to
+     * get the offset for the stripe footer slice for a given stripe.
+     */
+    public int getFooterOffsetPosition(int stripeOrdinal)
+    {
+        switch (this) {
+            case FOOTER:
+                return stripeOrdinal;
+            case INDEX_AND_FOOTER:
+                return stripeOrdinal * 2 + 1;
+            default:
+                throw new IllegalStateException("This mode doesn't support footer cache: " + this);
+        }
+    }
 }
