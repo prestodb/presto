c547575832176fe9365c98f4f5cf6d96f9edf0b1
Naveen Kumar Mahadevuni <nmahadevuni@gmail.com>
2021-07-02 18:42:44 +0530

    Hive integration test infra changes to run with version 3.x
    Cherry pick of following commits
    https://github.com/trinodb/trino/commit/f90708111f50fc48a661e30bda49cca7f3d1abe8
    https://github.com/trinodb/trino/commit/de0f2df5d2c592200477c3749ffcd4267d6dfe53
    https://github.com/trinodb/trino/commit/802e2732b10b182f33d9a68250bc31d748c83aa9
    https://github.com/trinodb/trino/commit/b8729fe4a117a65a273ca259844f9289e8d1700f
    https://github.com/trinodb/trino/commit/5bfde980654e51bd946b1f820cbe5ad5ba803e28
    
    Co-authored-by: Łukasz Osipiuk <lukasz@osipiuk.net>
    Co-authored-by: Piotr Findeisen <piotr.findeisen@gmail.com>

--- presto-hive-hadoop2/src/test/java/com/facebook/presto/hive/TestHiveClient.java
+++ presto-hive-hadoop2/src/test/java/com/facebook/presto/hive/TestHiveClient.java
@@ -14,17 +14,30 @@
 package com.facebook.presto.hive;
 
 import org.apache.hadoop.net.NetUtils;
+import org.testng.SkipException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
 import java.net.UnknownHostException;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class TestHiveClient
         extends AbstractTestHiveClient
 {
-    @Parameters({"hive.hadoop2.metastoreHost", "hive.hadoop2.metastorePort", "hive.hadoop2.databaseName", "hive.hadoop2.timeZone"})
+    private int hiveVersionMajor;
+
+    @Parameters({
+            "hive.hadoop2.metastoreHost",
+            "hive.hadoop2.metastorePort",
+            "hive.hadoop2.databaseName",
+            "hive.hadoop2.hiveVersionMajor",
+            "hive.hadoop2.timeZone",
+    })
     @BeforeClass
-    public void initialize(String host, int port, String databaseName, String timeZone)
+    public void initialize(String host, int port, String databaseName, int hiveVersionMajor, String timeZone)
             throws UnknownHostException
     {
         String hadoopMasterIp = System.getProperty("hadoop-master-ip");
@@ -36,5 +49,63 @@ public class TestHiveClient
         }
 
         setup(host, port, databaseName, timeZone);
+
+        checkArgument(hiveVersionMajor > 0, "Invalid hiveVersionMajor: %s", hiveVersionMajor);
+        this.hiveVersionMajor = hiveVersionMajor;
+    }
+
+    protected int getHiveVersionMajor()
+    {
+        checkState(hiveVersionMajor > 0, "hiveVersionMajor not set");
+        return hiveVersionMajor;
+    }
+
+    @Override
+    public void testGetPartitionSplitsTableOfflinePartition()
+    {
+        if (getHiveVersionMajor() >= 2) {
+            throw new SkipException("ALTER TABLE .. ENABLE OFFLINE was removed in Hive 2.0 and this is a prerequisite for this test");
+        }
+
+        super.testGetPartitionSplitsTableOfflinePartition();
+    }
+
+    @Override
+    public void testTypesRcBinary()
+            throws Exception
+    {
+        if (getHiveVersionMajor() >= 3) {
+            // TODO (https://github.com/prestosql/presto/issues/1218) requires https://issues.apache.org/jira/browse/HIVE-22167
+            assertThatThrownBy(super::testTypesRcBinary)
+                    .isInstanceOf(AssertionError.class)
+                    .hasMessage("expected [2011-05-06 01:23:09.123] but found [2011-05-06 07:08:09.123]");
+            return;
+        }
+        super.testTypesRcBinary();
+    }
+
+    @Override
+    public void testTypesParquet()
+            throws Exception
+    {
+        if (getHiveVersionMajor() >= 3) {
+            // TODO (https://github.com/prestosql/presto/issues/1218) requires https://issues.apache.org/jira/browse/HIVE-21002
+            assertThatThrownBy(super::testTypesParquet)
+                    .isInstanceOf(AssertionError.class)
+                    .hasMessage("expected [2011-05-06 01:23:09.123] but found [2011-05-06 07:08:09.123]");
+            return;
+        }
+        super.testTypesParquet();
+    }
+
+    @Override
+    public void testMismatchSchemaTable()
+            throws Exception
+    {
+        if (getHiveVersionMajor() >= 3) {
+            throw new SkipException("Mismatch schema test fails in Hive 3 and above. Disabling it");
+        }
+
+        super.testMismatchSchemaTable();
     }
 }
