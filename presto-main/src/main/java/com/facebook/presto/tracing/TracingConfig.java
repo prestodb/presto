338e56620ef90e87d82bacb78add487a546b0270
tanjialiang <jacob.jialiang.tan@gmail.com>
2021-09-09 15:57:23 -0700

    Add config based tracer provider selection

---
+++ presto-main/src/main/java/com/facebook/presto/tracing/TracingConfig.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tracing;
+
+import com.facebook.airlift.configuration.Config;
+
+public class TracingConfig
+{
+    public static class TracerType
+    {
+        public static final String NOOP = "noop";
+        public static final String TESTING = "testing";
+    }
+
+    private String tracerType;
+
+    public TracingConfig()
+    {
+        this.tracerType = TracerType.NOOP;
+    }
+
+    public String getTracerType()
+    {
+        return this.tracerType;
+    }
+
+    @Config("tracing.tracer-type")
+    public TracingConfig setTracerType(String tracerType)
+    {
+        this.tracerType = tracerType;
+        return this;
+    }
+}
