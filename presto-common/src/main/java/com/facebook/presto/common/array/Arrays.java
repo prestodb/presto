3541380ee7f32dc8dd91864dc3b00ab843d7b24c
Ying Su <yingsu00@outlook.com>
2021-09-10 23:22:20 -0700

    Reorder methods in Arrays by types

--- presto-common/src/main/java/com/facebook/presto/common/array/Arrays.java
+++ presto-common/src/main/java/com/facebook/presto/common/array/Arrays.java
@@ -26,6 +26,72 @@ public class Arrays
 {
     private Arrays() {}
 
+    public static boolean[] ensureCapacity(boolean[] buffer, int capacity)
+    {
+        if (buffer == null || buffer.length < capacity) {
+            return new boolean[(int) (capacity * SMALL.expansionFactor)];
+        }
+
+        return buffer;
+    }
+
+    public static byte[] ensureCapacity(byte[] buffer, int capacity)
+    {
+        if (buffer == null || buffer.length < capacity) {
+            return new byte[(int) (capacity * SMALL.expansionFactor)];
+        }
+
+        return buffer;
+    }
+
+    public static byte[] ensureCapacity(byte[] buffer, int capacity, ExpansionFactor expansionFactor, ExpansionOption expansionOption)
+    {
+        int newCapacity = (int) (capacity * expansionFactor.expansionFactor);
+
+        if (buffer == null) {
+            buffer = new byte[newCapacity];
+        }
+        else if (buffer.length < capacity) {
+            if (expansionOption == PRESERVE) {
+                buffer = java.util.Arrays.copyOf(buffer, newCapacity);
+            }
+            else {
+                buffer = new byte[newCapacity];
+            }
+        }
+        else if (expansionOption == INITIALIZE) {
+            java.util.Arrays.fill(buffer, (byte) 0);
+        }
+
+        return buffer;
+    }
+
+    public static byte[] ensureCapacity(byte[] buffer, int capacity, int estimatedMaxCapacity, ExpansionFactor expansionFactor, ExpansionOption expansionOption, ArrayAllocator allocator)
+    {
+        int newCapacity = max(capacity, min((int) (capacity * expansionFactor.expansionFactor), estimatedMaxCapacity));
+
+        byte[] newBuffer;
+        if (buffer == null) {
+            newBuffer = allocator.borrowByteArray(newCapacity);
+        }
+        else if (buffer.length < capacity) {
+            newBuffer = allocator.borrowByteArray(newCapacity);
+            if (expansionOption == PRESERVE) {
+                System.arraycopy(buffer, 0, newBuffer, 0, buffer.length);
+            }
+            allocator.returnArray(buffer);
+        }
+        else {
+            newBuffer = buffer;
+        }
+
+        if (expansionOption == INITIALIZE) {
+            java.util.Arrays.fill(newBuffer, (byte) 0);
+        }
+
+        return newBuffer;
+    }
+
     public static int[] ensureCapacity(int[] buffer, int capacity)
     {
         return ensureCapacity(buffer, capacity, SMALL, NONE);
@@ -88,50 +154,6 @@ public class Arrays
         return buffer;
     }
 
-    public static boolean[] ensureCapacity(boolean[] buffer, int capacity)
-    {
-        if (buffer == null || buffer.length < capacity) {
-            return new boolean[(int) (capacity * SMALL.expansionFactor)];
-        }
-
-        return buffer;
-    }
-
-    public static byte[] ensureCapacity(byte[] buffer, int capacity)
-    {
-        if (buffer == null || buffer.length < capacity) {
-            return new byte[(int) (capacity * SMALL.expansionFactor)];
-        }
-
-        return buffer;
-    }
-
-    public static byte[] ensureCapacity(byte[] buffer, int capacity, int estimatedMaxCapacity, ExpansionFactor expansionFactor, ExpansionOption expansionOption, ArrayAllocator allocator)
-    {
-        int newCapacity = max(capacity, min((int) (capacity * expansionFactor.expansionFactor), estimatedMaxCapacity));
-
-        byte[] newBuffer;
-        if (buffer == null) {
-            newBuffer = allocator.borrowByteArray(newCapacity);
-        }
-        else if (buffer.length < capacity) {
-            newBuffer = allocator.borrowByteArray(newCapacity);
-            if (expansionOption == PRESERVE) {
-                System.arraycopy(buffer, 0, newBuffer, 0, buffer.length);
-            }
-            allocator.returnArray(buffer);
-        }
-        else {
-            newBuffer = buffer;
-        }
-
-        if (expansionOption == INITIALIZE) {
-            java.util.Arrays.fill(newBuffer, (byte) 0);
-        }
-
-        return newBuffer;
-    }
-
     public static int[][] ensureCapacity(int[][] buffer, int capacity)
     {
         if (buffer == null || buffer.length < capacity) {
@@ -145,28 +167,6 @@ public class Arrays
     {
         if (buffer == null || buffer.length < capacity) {
             return new boolean[capacity][];
-        }
-
-        return buffer;
-    }
-
-    public static byte[] ensureCapacity(byte[] buffer, int capacity, ExpansionFactor expansionFactor, ExpansionOption expansionOption)
-    {
-        int newCapacity = (int) (capacity * expansionFactor.expansionFactor);
-
-        if (buffer == null) {
-            buffer = new byte[newCapacity];
-        }
-        else if (buffer.length < capacity) {
-            if (expansionOption == PRESERVE) {
-                buffer = java.util.Arrays.copyOf(buffer, newCapacity);
-            }
-            else {
-                buffer = new byte[newCapacity];
-            }
-        }
-        else if (expansionOption == INITIALIZE) {
-            java.util.Arrays.fill(buffer, (byte) 0);
         }
 
         return buffer;
