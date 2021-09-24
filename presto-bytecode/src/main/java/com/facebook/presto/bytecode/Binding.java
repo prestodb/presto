778ac47b41ea2357599ec1c48d0f352ceeaa4c7e
Pranjal Shankhdhar <pranjal.ssh@gmail.com>
2021-09-23 18:54:52 -0700

    Add support to write codegen scalar functions

--- presto-bytecode/src/main/java/com/facebook/presto/bytecode/Binding.java
+++ presto-bytecode/src/main/java/com/facebook/presto/bytecode/Binding.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.sql.gen;
+package com.facebook.presto.bytecode;
 
 import java.lang.invoke.MethodType;
 
