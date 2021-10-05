e5c7d90b71b5f29d7112699239ffb0f9965578d6
Arunachalam Thirupathi <athirupathi@fb.com>
2021-10-04 23:42:43 -0700

    Add Type specific ColumnStatistics
    ColumnStatistics contain all type specific statistics. But ColumnStatistics
    utmost can hold only one type specific statistics. This change refactors
    the class using inheritance and provides type specific column statistics.
    With compressedOops (32 bit object pointers) the size of the class went
    from 64 bytes to 40 bytes. On heaps larger than 32GB, the class will gain
    about 56 bytes per instance. For wider and large tables, this can be few
    Megabytes per writer/reader.

---
+++ presto-orc/src/main/java/com/facebook/presto/orc/metadata/statistics/DecimalColumnStatistics.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.orc.metadata.statistics;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.Objects;
+
+public class DecimalColumnStatistics
+        extends ColumnStatistics
+{
+    private static final int INSTANCE_SIZE = ClassLayout.parseClass(DecimalColumnStatistics.class).instanceSize();
+
+    private final DecimalStatistics decimalStatistics;
+
+    public DecimalColumnStatistics(
+            Long numberOfValues,
+            long minAverageValueSizeInBytes,
+            HiveBloomFilter bloomFilter,
+            DecimalStatistics decimalStatistics)
+    {
+        super(numberOfValues, minAverageValueSizeInBytes, bloomFilter);
+        this.decimalStatistics = decimalStatistics;
+    }
+
+    @Override
+    public DecimalStatistics getDecimalStatistics()
+    {
+        return decimalStatistics;
+    }
+
+    @Override
+    public ColumnStatistics withBloomFilter(HiveBloomFilter bloomFilter)
+    {
+        return new DecimalColumnStatistics(
+                getNumberOfValues(),
+                getMinAverageValueSizeInBytes(),
+                bloomFilter,
+                decimalStatistics);
+    }
+
+    @Override
+    public long getRetainedSizeInBytes()
+    {
+        long sizeInBytes = INSTANCE_SIZE + getMembersSizeInBytes();
+        if (decimalStatistics != null) {
+            return sizeInBytes + decimalStatistics.getRetainedSizeInBytes();
+        }
+        return sizeInBytes;
+    }
+
+    @Override
+    public void addHash(StatisticsHasher hasher)
+    {
+        super.addHash(hasher);
+        hasher.putOptionalHashable(decimalStatistics);
+    }
+
+    @Override
+    protected ToStringHelper getToStringHelper()
+    {
+        return super.getToStringHelper()
+                .add("decimalStatistics", decimalStatistics);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        DecimalColumnStatistics that = (DecimalColumnStatistics) o;
+        return equalsInternal(that) && Objects.equals(decimalStatistics, that.decimalStatistics);
+    }
+
+    public int hashCode()
+    {
+        return Objects.hash(super.hashCode(), decimalStatistics);
+    }
+}
---
+++ presto-orc/src/main/java/com/facebook/presto/orc/metadata/statistics/StringColumnStatistics.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.orc.metadata.statistics;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.Objects;
+
+public class StringColumnStatistics
+        extends ColumnStatistics
+{
+    private static final int INSTANCE_SIZE = ClassLayout.parseClass(StringColumnStatistics.class).instanceSize();
+
+    private final StringStatistics stringStatistics;
+
+    public StringColumnStatistics(
+            Long numberOfValues,
+            long minAverageValueSizeInBytes,
+            HiveBloomFilter bloomFilter,
+            StringStatistics stringStatistics)
+    {
+        super(numberOfValues, minAverageValueSizeInBytes, bloomFilter);
+        this.stringStatistics = stringStatistics;
+    }
+
+    @Override
+    public StringStatistics getStringStatistics()
+    {
+        return stringStatistics;
+    }
+
+    @Override
+    public ColumnStatistics withBloomFilter(HiveBloomFilter bloomFilter)
+    {
+        return new StringColumnStatistics(
+                getNumberOfValues(),
+                getMinAverageValueSizeInBytes(),
+                bloomFilter,
+                stringStatistics);
+    }
+
+    @Override
+    public long getRetainedSizeInBytes()
+    {
+        long sizeInBytes = INSTANCE_SIZE + getMembersSizeInBytes();
+        if (stringStatistics != null) {
+            return sizeInBytes + stringStatistics.getRetainedSizeInBytes();
+        }
+        return sizeInBytes;
+    }
+
+    @Override
+    public void addHash(StatisticsHasher hasher)
+    {
+        super.addHash(hasher);
+        hasher.putOptionalHashable(stringStatistics);
+    }
+
+    @Override
+    protected ToStringHelper getToStringHelper()
+    {
+        return super.getToStringHelper()
+                .add("stringStatistics", stringStatistics);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        StringColumnStatistics that = (StringColumnStatistics) o;
+        return equalsInternal(that) && Objects.equals(stringStatistics, that.stringStatistics);
+    }
+
+    public int hashCode()
+    {
+        return Objects.hash(super.hashCode(), stringStatistics);
+    }
+}
