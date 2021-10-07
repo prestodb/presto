76609938faca24d417322a4ae780bb5669926cf3
Marshall <akbar@odilov.de>
2021-10-06 22:23:33 -0700

    Replace std::unordered_map with F14FastMap (#328) (#357)
    Summary:
    Replace std::unordered_map with F14FastMap in velox/type/tz/TimeZoneMap.cpp
    
    Fixes https://github.com/facebookincubator/velox/issues/328
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/357
    
    Reviewed By: amitkdutta
    
    Differential Revision: D31437196
    
    Pulled By: mbasmanova
    
    fbshipit-source-id: 16897b72612a06c99c1d2d01a0d64a126221a8f9

--- velox/type/tz/TimeZoneMap.cpp
+++ velox/type/tz/TimeZoneMap.cpp
@@ -16,7 +16,7 @@
 
 #include "velox/type/tz/TimeZoneMap.h"
 #include <fmt/core.h>
-#include <unordered_map>
+#include <folly/container/F14Map.h>
 
 namespace facebook::velox::util {
 
@@ -34,9 +34,9 @@ std::string getTimeZoneName(int64_t timeZoneID) {
 }
 
 namespace {
-std::unordered_map<std::string_view, int64_t> makeReverseMap(
+folly::F14FastMap<std::string_view, int64_t> makeReverseMap(
     const std::unordered_map<int64_t, std::string>& map) {
-  std::unordered_map<std::string_view, int64_t> reversed;
+  folly::F14FastMap<std::string_view, int64_t> reversed;
   reversed.reserve(map.size());
   for (const auto& entry : map) {
     reversed.emplace(entry.second, entry.first);
@@ -46,7 +46,7 @@ std::unordered_map<std::string_view, int64_t> makeReverseMap(
 } // namespace
 
 int64_t getTimeZoneID(std::string_view timeZone) {
-  static std::unordered_map<std::string_view, int64_t> nameToIdMap =
+  static folly::F14FastMap<std::string_view, int64_t> nameToIdMap =
       makeReverseMap(getTimeZoneDB());
 
   auto it = nameToIdMap.find(timeZone);
