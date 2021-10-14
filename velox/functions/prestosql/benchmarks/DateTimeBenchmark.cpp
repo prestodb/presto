c6601272e449f18228404c62afdc15c5980d7462
Masha Basmanova <mbasmanova@fb.com>
2021-10-14 10:04:00 -0700

    Add benchmark for year, month, day, hour, minute, second Presto functions (#430)
    Summary:
    Confirm no performance difference between vector and simple
    implementations of the hour function.
    
    ```
    ============================================================================
    functions/prestosql/benchmarks/DateTimeBenchmark.cpprelative  time/iter  iters/s
    ============================================================================
    year                                                        61.59ms    16.24
    month                                                       61.77ms    16.19
    day                                                         61.16ms    16.35
    hour                                                        61.49ms    16.26
    hour_vector                                      102.22%    60.15ms    16.62
    minute                                                      61.52ms    16.25
    second                                                      61.40ms    16.29
    ============================================================================
    ```
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/430
    
    Reviewed By: pedroerp
    
    Differential Revision: D31645703
    
    Pulled By: mbasmanova
    
    fbshipit-source-id: 3d9a9c2c7e8d9b5ae29be15084845b978da3cf5b

---
+++ velox/functions/prestosql/benchmarks/DateTimeBenchmark.cpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <folly/Benchmark.h>
+#include <folly/init/Init.h>
+#include "velox/expression/VectorFunction.h"
+#include "velox/external/date/tz.h"
+#include "velox/functions/lib/benchmarks/FunctionBenchmarkBase.h"
+#include "velox/functions/prestosql/CoreFunctions.h"
+
+using namespace facebook::velox;
+using namespace facebook::velox::test;
+
+namespace {
+
+class HourFunction : public exec::VectorFunction {
+ public:
+  const date::time_zone* FOLLY_NULLABLE
+  getTimeZoneIfNeeded(const core::QueryConfig& config) const {
+    const date::time_zone* timeZone = nullptr;
+    if (config.adjustTimestampToTimezone()) {
+      auto sessionTzName = config.sessionTimezone();
+      if (!sessionTzName.empty()) {
+        timeZone = date::locate_zone(sessionTzName);
+      }
+    }
+    return timeZone;
+  }
+
+  void apply(
+      const SelectivityVector& rows,
+      std::vector<VectorPtr>& args,
+      exec::Expr* /*caller*/,
+      exec::EvalCtx* context,
+      VectorPtr* result) const override {
+    VELOX_CHECK_EQ(args.size(), 1);
+    const auto* timestamps =
+        static_cast<const Timestamp*>(args[0]->valuesAsVoid());
+
+    // Initialize flat results vector.
+    BaseVector::ensureWritable(rows, BIGINT(), context->pool(), result);
+    auto rawResults = (*result)->as<FlatVector<int64_t>>()->mutableRawValues();
+
+    // Check if we need to adjust the current UTC timestamps to
+    // the user provided session timezone.
+    const auto* timeZone =
+        getTimeZoneIfNeeded(context->execCtx()->queryCtx()->config());
+    if (timeZone != nullptr) {
+      rows.applyToSelected([&](int row) {
+        auto timestamp = timestamps[row];
+        timestamp.toTimezoneUTC(*timeZone);
+        int64_t seconds = timestamp.getSeconds();
+        std::tm dateTime;
+        gmtime_r((const time_t*)&seconds, &dateTime);
+        rawResults[row] = dateTime.tm_hour;
+      });
+    } else {
+      rows.applyToSelected([&](int row) {
+        int64_t seconds = timestamps[row].getSeconds();
+        std::tm dateTime;
+        gmtime_r((const time_t*)&seconds, &dateTime);
+        rawResults[row] = dateTime.tm_hour;
+      });
+    }
+  }
+
+  static std::vector<std::shared_ptr<exec::FunctionSignature>> signatures() {
+    // timestamp -> bigint
+    return {exec::FunctionSignatureBuilder()
+                .returnType("bigint")
+                .argumentType("timestamp")
+                .build()};
+  }
+};
+
+class DateTimeBenchmark : public functions::test::FunctionBenchmarkBase {
+ public:
+  DateTimeBenchmark() : FunctionBenchmarkBase() {
+    functions::registerFunctions();
+    registerVectorFunction(
+        "hour_vector",
+        HourFunction::signatures(),
+        std::make_unique<HourFunction>());
+  }
+
+  void run(const std::string& functionName) {
+    folly::BenchmarkSuspender suspender;
+    constexpr vector_size_t size = 10'000;
+
+    auto data = vectorMaker_.rowVector({vectorMaker_.flatVector<Timestamp>(
+        size,
+        [](auto row) { return Timestamp(1633046400 + row, row % 11); },
+        VectorMaker::nullEvery(5))});
+    auto exprSet =
+        compileExpression(fmt::format("{}(c0)", functionName), data->type());
+    suspender.dismiss();
+
+    doRun(exprSet, data);
+  }
+
+  void doRun(exec::ExprSet& exprSet, const RowVectorPtr& rowVector) {
+    int cnt = 0;
+    for (auto i = 0; i < 100; i++) {
+      cnt += evaluate(exprSet, rowVector)->size();
+    }
+    folly::doNotOptimizeAway(cnt);
+  }
+};
+
+BENCHMARK(year) {
+  DateTimeBenchmark benchmark;
+  benchmark.run("year");
+}
+
+BENCHMARK(month) {
+  DateTimeBenchmark benchmark;
+  benchmark.run("month");
+}
+
+BENCHMARK(day) {
+  DateTimeBenchmark benchmark;
+  benchmark.run("day");
+}
+
+BENCHMARK(hour) {
+  DateTimeBenchmark benchmark;
+  benchmark.run("hour");
+}
+
+BENCHMARK_RELATIVE(hour_vector) {
+  DateTimeBenchmark benchmark;
+  benchmark.run("hour_vector");
+}
+
+BENCHMARK(minute) {
+  DateTimeBenchmark benchmark;
+  benchmark.run("minute");
+}
+
+BENCHMARK(second) {
+  DateTimeBenchmark benchmark;
+  benchmark.run("second");
+}
+} // namespace
+
+int main(int /*argc*/, char** /*argv*/) {
+  folly::runBenchmarks();
+  return 0;
+}
