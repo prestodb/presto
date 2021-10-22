bc31bb19db6e2214ea608996656cdb802d7c278e
Venki Korukanti <venki.korukanti@gmail.com>
2021-10-22 08:34:16 -0700

    Support nested column project and filter pushdown in Delta reader

--- presto-delta/src/main/java/com/facebook/presto/delta/DeltaExpressionUtils.java
+++ presto-delta/src/main/java/com/facebook/presto/delta/DeltaExpressionUtils.java
@@ -30,7 +30,6 @@ import io.delta.standalone.data.CloseableIterator;
 import java.io.IOException;
 import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
