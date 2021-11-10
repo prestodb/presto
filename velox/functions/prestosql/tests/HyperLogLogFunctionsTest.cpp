7da69d6169bd1d4f971c2e7c4b78825b8cfa477f
Masha Basmanova <mbasmanova@fb.com>
2021-11-10 15:12:25 -0800

    Add more tests for cardinality HLL Presto function (#600)
    Summary:
    Add unit tests specifically for cardinality(hll) Presto function in addition to tests in velox/functions/prestosql/aggregates/tests/ApproxDistinctTest.cpp.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/600
    
    Reviewed By: amitkdutta
    
    Differential Revision: D32329478
    
    Pulled By: mbasmanova
    
    fbshipit-source-id: 7363cdc6d22f8ce188d05502380d48e5ebc87e5c

---
+++ velox/functions/prestosql/tests/HyperLogLogFunctionsTest.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "velox/functions/prestosql/HyperLogLogType.h"
+#include "velox/functions/prestosql/hyperloglog/SparseHll.h"
+#include "velox/functions/prestosql/tests/FunctionBaseTest.h"
+#define XXH_INLINE_ALL
+#include "velox/external/xxhash.h"
+
+using facebook::velox::aggregate::hll::DenseHll;
+using facebook::velox::aggregate::hll::SparseHll;
+
+namespace facebook::velox {
+namespace {
+
+template <typename T>
+uint64_t hashOne(T value) {
+  return XXH64(&value, sizeof(value), 0);
+}
+
+class HyperLogLogFunctionsTest : public functions::test::FunctionBaseTest {
+ protected:
+  static std::string serialize(
+      int8_t indexBitLength,
+      const SparseHll& sparseHll) {
+    std::string serialized;
+    serialized.resize(sparseHll.serializedSize());
+    sparseHll.serialize(indexBitLength, serialized.data());
+    return serialized;
+  }
+
+  static std::string serialize(DenseHll& denseHll) {
+    std::string serialized;
+    serialized.resize(denseHll.serializedSize());
+    denseHll.serialize(serialized.data());
+    return serialized;
+  }
+
+  exec::HashStringAllocator allocator_{memory::MappedMemory::getInstance()};
+};
+
+TEST_F(HyperLogLogFunctionsTest, cardinalitySparse) {
+  const auto cardinality = [&](const std::string& input) {
+    return evaluateOnce<int64_t, StringView>(
+        "cardinality(c0)", {StringView(input)}, {HYPERLOGLOG()});
+  };
+
+  SparseHll sparseHll{&allocator_};
+  for (int i = 0; i < 1'000; i++) {
+    sparseHll.insertHash(hashOne(i % 17));
+  }
+
+  auto serialized = serialize(11, sparseHll);
+  EXPECT_EQ(17, cardinality(serialized));
+}
+
+TEST_F(HyperLogLogFunctionsTest, cardinalityDense) {
+  const auto cardinality = [&](const std::string& input) {
+    return evaluateOnce<int64_t, StringView>(
+        "cardinality(c0)", {StringView(input)}, {HYPERLOGLOG()});
+  };
+
+  DenseHll denseHll{12, &allocator_};
+  for (int i = 0; i < 10'000'000; i++) {
+    denseHll.insertHash(hashOne(i));
+  }
+
+  auto serialized = serialize(denseHll);
+  EXPECT_EQ(denseHll.cardinality(), cardinality(serialized));
+}
+
+} // namespace
+} // namespace facebook::velox
