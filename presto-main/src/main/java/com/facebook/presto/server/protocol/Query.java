49ae2871736f1c783860596768652755e8159ebd
tanjialiang <jacob.jialiang.tan@gmail.com>
2021-11-18 11:12:44 -0800

    Adding tracing instrumentations in codesites

--- presto-main/src/main/java/com/facebook/presto/server/protocol/Query.java
+++ presto-main/src/main/java/com/facebook/presto/server/protocol/Query.java
@@ -39,6 +39,7 @@ import com.facebook.presto.spi.function.SqlInvokedFunction;
 import com.facebook.presto.spi.page.PagesSerde;
 import com.facebook.presto.spi.page.SerializedPage;
 import com.facebook.presto.spi.security.SelectedRole;
+import com.facebook.presto.spi.tracing.Tracer;
 import com.facebook.presto.transaction.TransactionId;
 import com.facebook.presto.transaction.TransactionInfo;
 import com.facebook.presto.transaction.TransactionManager;
@@ -178,7 +179,7 @@ class Query
 
         result.queryManager.addOutputInfoListener(result.getQueryId(), result::setQueryOutputInfo);
 
-        result.queryManager.addStateChangeListener(result.getQueryId(), state -> {
+        result.queryManager.addStateChangeListener(result.getQueryId(), (state) -> {
             if (state.isDone()) {
                 QueryInfo queryInfo = queryManager.getFullQueryInfo(result.getQueryId());
                 result.closeExchangeClientIfNecessary(queryInfo);
@@ -242,6 +243,13 @@ class Query
     public boolean isSlugValid(String slug)
     {
         return this.slug.equals(slug);
+    }
+
+    public Tracer getTracer()
+    {
+        Optional<Tracer> tracer = session.getTracer();
+        checkArgument(tracer.isPresent(), "tracer is not present");
+        return tracer.get();
     }
 
     public synchronized Optional<String> getSetCatalog()
