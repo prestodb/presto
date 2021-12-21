a4cac4ac7df747aa9db9e263b3659a6d460add56
Pedro Eugenio Rocha Pedreira <pedroerp@fb.com>
2021-12-21 10:34:09 -0800

    Refactor ArrayMin/ArrayMax simple function and registration (#787)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/787
    
    Refactor ArrayMin/ArrayMax simple function and registration
    
    Reviewed By: laithsakka
    
    Differential Revision: D33201733
    
    fbshipit-source-id: 4a925181c3bd5b3791184bb68929f032f5352236

--- velox/functions/prestosql/ArrayFunctions.h
+++ velox/functions/prestosql/ArrayFunctions.h
@@ -14,81 +14,89 @@
  * limitations under the License.
  */
 #pragma once
+
 #include "velox/functions/Udf.h"
 
 namespace facebook::velox::functions {
 
-template <bool isMax, typename VeloxType>
-VELOX_UDF_BEGIN(array_min_max)
+template <typename TExecCtx, bool isMax>
+struct ArrayMinMaxFunction {
+  VELOX_DEFINE_FUNCTION_TYPES(TExecCtx);
 
-template <typename T>
-void update(T& currentValue, const T& candidateValue) {
-  if constexpr (isMax) {
-    if (candidateValue > currentValue) {
-      currentValue = candidateValue;
-    }
-  } else {
-    if (candidateValue < currentValue) {
-      currentValue = candidateValue;
+  template <typename T>
+  void update(T& currentValue, const T& candidateValue) {
+    if constexpr (isMax) {
+      if (candidateValue > currentValue) {
+        currentValue = candidateValue;
+      }
+    } else {
+      if (candidateValue < currentValue) {
+        currentValue = candidateValue;
+      }
     }
   }
-}
 
-void assign(out_type<VeloxType>& out, const arg_type<VeloxType>& value) {
-  if constexpr (std::is_same<Varchar, VeloxType>::value) {
+  template <typename TReturn, typename TInput>
+  void assign(TReturn& out, const TInput& value) {
+    out = value;
+  }
+
+  void assign(out_type<Varchar>& out, const arg_type<Varchar>& value) {
     // TODO: reuse strings once support landed.
     out.resize(value.size());
     if (value.size() != 0) {
       std::memcpy(out.data(), value.data(), value.size());
     }
-  } else {
-    out = value;
-  }
-}
-
-FOLLY_ALWAYS_INLINE bool call(
-    out_type<VeloxType>& out,
-    const arg_type<Array<VeloxType>>& array) {
-  // Result is null if array is empty.
-  if (array.size() == 0) {
-    return false;
   }
 
-  if (!array.mayHaveNulls()) {
-    // Input array does not have nulls.
-    auto currentValue = *array[0];
-    for (const auto& item : array) {
-      update(currentValue, item.value());
+  template <typename TReturn, typename TInput>
+  FOLLY_ALWAYS_INLINE bool call(TReturn& out, const TInput& array) {
+    // Result is null if array is empty.
+    if (array.size() == 0) {
+      return false;
     }
-    assign(out, currentValue);
-    return true;
-  }
 
-  auto it = array.begin();
-  // Result is null if any element is null.
-  if (!it->has_value()) {
-    return false;
-  }
+    if (!array.mayHaveNulls()) {
+      // Input array does not have nulls.
+      auto currentValue = *array[0];
+      for (const auto& item : array) {
+        update(currentValue, item.value());
+      }
+      assign(out, currentValue);
+      return true;
+    }
 
-  auto currentValue = it->value();
-  it++;
-  while (it != array.end()) {
+    auto it = array.begin();
+    // Result is null if any element is null.
     if (!it->has_value()) {
       return false;
     }
-    update(currentValue, it->value());
+
+    auto currentValue = it->value();
     it++;
+    while (it != array.end()) {
+      if (!it->has_value()) {
+        return false;
+      }
+      update(currentValue, it->value());
+      it++;
+    }
+
+    assign(out, currentValue);
+    return true;
   }
+};
 
-  assign(out, currentValue);
-  return true;
-}
+template <typename TExecCtx>
+struct ArrayMinFunction : public ArrayMinMaxFunction<TExecCtx, false> {};
 
-VELOX_UDF_END()
+template <typename TExecCtx>
+struct ArrayMaxFunction : public ArrayMinMaxFunction<TExecCtx, true> {};
 
 template <typename T>
 inline void registerArrayMinMaxFunctions() {
-  registerFunction<udf_array_min_max<false, T>, T, Array<T>>({"array_min"});
-  registerFunction<udf_array_min_max<true, T>, T, Array<T>>({"array_max"});
+  registerFunction<ArrayMinFunction, T, Array<T>>({"array_min"});
+  registerFunction<ArrayMaxFunction, T, Array<T>>({"array_max"});
 }
+
 } // namespace facebook::velox::functions
