d8323f56f8b2828163a6161311115257beda2cc8
v-jizhang <jzhang@openteams.com>
2022-01-13 14:21:41 -0800

    Preserve quotedness for Identifier when formatting Query
    Cherry-pick of https://github.com/trinodb/trino/pull/80
    
    Presto doesn't maintain the quotedness of an identifier when
    using SqlQueryFormatter so it results in throwing parsing error
    when we run prepare query of the syntax
    [https://github.com/prestodb/presto/issues/10739].
    This PR solves that above issue.
    
    Co-authored-by: praveenkrishna <praveenkrishna@tutanota.com>

--- presto-parser/src/test/java/com/facebook/presto/sql/parser/TestSqlParser.java
+++ presto-parser/src/test/java/com/facebook/presto/sql/parser/TestSqlParser.java
@@ -157,6 +157,7 @@ import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.facebook.presto.sql.QueryUtil.identifier;
 import static com.facebook.presto.sql.QueryUtil.query;
@@ -2285,7 +2286,7 @@ public class TestSqlParser
         final String[] tableNames = {"t", "s.t", "c.s.t"};
 
         for (String fullName : tableNames) {
-            QualifiedName qualifiedName = QualifiedName.of(Arrays.asList(fullName.split("\\.")));
+            QualifiedName qualifiedName = makeQualifiedName(fullName);
             assertStatement(format("SHOW STATS FOR %s", qualifiedName), new ShowStats(new Table(qualifiedName)));
         }
     }
@@ -2296,7 +2297,7 @@ public class TestSqlParser
         final String[] tableNames = {"t", "s.t", "c.s.t"};
 
         for (String fullName : tableNames) {
-            QualifiedName qualifiedName = QualifiedName.of(Arrays.asList(fullName.split("\\.")));
+            QualifiedName qualifiedName = makeQualifiedName(fullName);
             assertStatement(format("SHOW STATS FOR (SELECT * FROM %s)", qualifiedName),
                     createShowStats(qualifiedName, ImmutableList.of(new AllColumns()), Optional.empty()));
             assertStatement(format("SHOW STATS FOR (SELECT * FROM %s WHERE field > 0)", qualifiedName),
@@ -2318,6 +2319,14 @@ public class TestSqlParser
                                                     new Identifier("field"),
                                                     new LongLiteral("0"))))));
         }
+    }
+
+    private QualifiedName makeQualifiedName(String tableName)
+    {
+        List<Identifier> parts = Arrays.stream(tableName.split("\\."))
+                .map(Identifier::new)
+                .collect(Collectors.toList());
+        return QualifiedName.of(parts);
     }
 
     private ShowStats createShowStats(QualifiedName name, List<SelectItem> selects, Optional<Expression> where)
