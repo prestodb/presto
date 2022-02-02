dd473194738c729c95d592f6d0f51caf020c8a78
Harsha Rastogi <hrastogi@fb.com>
2022-02-03 00:19:54 +0530

    Support optional row numbers in OrcSelectiveReader
    If appendRowNumber flag is set to true, every page returned by the
    reader will contain an additional block in the end that stores the
    row numbers. If a file has n rows, row numbers range from [0 to n-1]

--- presto-orc/src/main/java/com/facebook/presto/orc/OrcWriteValidation.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/OrcWriteValidation.java
@@ -490,7 +490,8 @@ public class OrcWriteValidation
         public void addPage(Page page)
         {
             requireNonNull(page, "page is null");
-            checkArgument(page.getChannelCount() == columnHashes.size(), "invalid page");
+            // When append row number is set to true, the page will have an additional block appended at the end
+            checkArgument(page.getChannelCount() >= columnHashes.size(), "invalid page");
 
             for (int channel = 0; channel < columnHashes.size(); channel++) {
                 Type type = types.get(channel);
