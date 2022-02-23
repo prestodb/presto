105dd326acdfd3925ca022ab85160eb0d91b4aed
v-jizhang <jzhang@openteams.com>
2022-02-23 13:48:19 -0800

    Add support for json type in MongoDB
    Cherry-pick of https://github.com/trinodb/trino/pull/8584
    
    Co-authored-by: Yuya Ebihara <ebyhry@gmail.com>

--- presto-mongodb/src/main/java/com/facebook/presto/mongodb/TypeUtils.java
+++ presto-mongodb/src/main/java/com/facebook/presto/mongodb/TypeUtils.java
@@ -19,6 +19,7 @@ import com.facebook.presto.common.type.Type;
 import java.util.function.Predicate;
 
 import static com.facebook.presto.common.type.DateType.DATE;
+import static com.facebook.presto.common.type.StandardTypes.JSON;
 import static com.facebook.presto.common.type.TimeType.TIME;
 import static com.facebook.presto.common.type.TimestampType.TIMESTAMP;
 import static com.facebook.presto.common.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
@@ -26,6 +27,11 @@ import static com.facebook.presto.common.type.TimestampWithTimeZoneType.TIMESTAM
 public final class TypeUtils
 {
     private TypeUtils() {}
+
+    public static boolean isJsonType(Type type)
+    {
+        return type.getTypeSignature().getBase().equals(JSON);
+    }
 
     public static boolean isArrayType(Type type)
     {
