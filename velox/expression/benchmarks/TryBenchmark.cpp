09aecddd7485b0da729a6b20ddebd46033db5827
Kevin Wilfong <kevinwilfong@fb.com>
2022-02-24 11:29:53 -0800

    Add benchmark for try expressions (#1015)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/1015
    
    A customer was concerned that using TRY expressions might introduce a performance regression in cases where no exceptions were thrown (obviously throwing and catching exceptions has overhead).
    
    I put together this benchmark to test the performance.
    
    It sums 50 integer columns to create a deep tree of expressions, and runs it with no Try expression, a single Try expression around the entire sum, and a Try around each individual addition.
    
    Also, just to measure the cost of exceptions, I compared dividing two columns, to dividing two columns where once the denominator was 0 (so one row threw an exception), to dividing a column by 0 (so every row threw an exception), all wrapped in TRY expressions.
    
    ## Results
    
    ```
    ============================================================================
    velox/expression/benchmarks/TryBenchmark.cpp    relative  time/iter  iters/s
    ============================================================================
    baseline                                                   277.71ns    3.60M
    singleOuterTry                                             262.28ns    3.81M
    nestedTries                                                280.72ns    3.56M
    divideNoExceptions                                          16.55ns   60.43M
    divideOneException                                          42.93ns   23.29M
    divideAllExceptions                                         42.40us   23.59K
    ============================================================================
    ```
    
    This shows no significant performance impact from the Try expressions, and the cost of a single exception is a 2x overhead (this is likely an upper bound given how simple the expression is), and >1000x worse when lots of exceptions are thrown (this seems to be dominated by the cost of C++'s exception handling, rather than any logic in the Try expression itself).
    
    It's also worth noting that we log to stderr on exceptions which adds significantly to the cost, without that the overhead of a single exception is closer to 50%.
    
    Reviewed By: laithsakka
    
    Differential Revision: D34119788
    
    fbshipit-source-id: 71eddeca2a798dc9e9c5e2ea3ae46044d99c01d4

---
+++ velox/expression/benchmarks/TryBenchmark.cpp
@@ -0,0 +1,190 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/Benchmark.h>
+#include <folly/init/Init.h>
+
+#include "velox/functions/lib/benchmarks/FunctionBenchmarkBase.h"
+#include "velox/functions/prestosql/registration/RegistrationFunctions.h"
+
+// This runs two sets of benchmarks:
+// 1) Benchmark executing expressions wrapped in try compared to executing the
+// same expressions without.
+// It sums 50 integer columns to create a deep tree of expressions, and runs
+// it with no Try expression, a single Try expression around the entire sum,
+// and a Try around each individual addition. No exceptions are thrown or
+// caught in these benchmarks.
+// 2) Benchmark the performance impact of throwing exceptions and catching them
+// using Try.
+// It divides two integers, using division by 0 to trigger an exception getting
+// thrown.
+// These benchmarks show that meerly adding a Try expression does not
+// significantly impact performance, and the performance cost of handling
+// exceptions scales linearly with the number of rows that saw exceptions.
+
+using namespace facebook::velox;
+
+namespace {
+class TryBenchmark : public functions::test::FunctionBenchmarkBase {
+ public:
+  TryBenchmark() : FunctionBenchmarkBase() {
+    functions::prestosql::registerAllScalarFunctions();
+  }
+
+  RowVectorPtr makeData(vector_size_t numArgs) {
+    const vector_size_t size = 1'000;
+
+    std::vector<VectorPtr> args;
+
+    for (vector_size_t i = 0; i < numArgs; ++i) {
+      args.emplace_back(vectorMaker_.flatVector<int32_t>(
+          // Add 1 so we don't accidentally end up with any 0s for the division
+          // test.
+          size,
+          [i](vector_size_t row) { return (row * i) + 1; }));
+    }
+
+    return vectorMaker_.rowVector(args);
+  }
+
+  // Baseline does not use any try.
+  size_t runBaseline(vector_size_t numArgs) {
+    folly::BenchmarkSuspender suspender;
+    auto rowVector = makeData(numArgs);
+
+    std::string expression = "c0 + c1";
+    for (vector_size_t i = 2; i < numArgs; ++i) {
+      expression += fmt::format(" + c{}", i);
+    }
+
+    auto exprSet = compileExpression(expression, rowVector->type());
+    suspender.dismiss();
+
+    return doRun(exprSet, rowVector);
+  }
+
+  size_t runWithOuterTry(vector_size_t numArgs) {
+    folly::BenchmarkSuspender suspender;
+    auto rowVector = makeData(numArgs);
+
+    std::string expression = "c0 + c1";
+    for (vector_size_t i = 2; i < numArgs; ++i) {
+      expression += fmt::format(" + c{}", i);
+    }
+    expression = fmt::format("try({})", expression);
+
+    auto exprSet = compileExpression(expression, rowVector->type());
+    suspender.dismiss();
+
+    return doRun(exprSet, rowVector);
+  }
+
+  size_t runWithNestedTries(vector_size_t numArgs) {
+    folly::BenchmarkSuspender suspender;
+    auto rowVector = makeData(numArgs);
+
+    std::string expression = "try(c0 + c1)";
+    for (vector_size_t i = 2; i < numArgs; ++i) {
+      expression = fmt::format("try({} + c{})", expression, i);
+    }
+
+    auto exprSet = compileExpression(expression, rowVector->type());
+    suspender.dismiss();
+
+    return doRun(exprSet, rowVector);
+  }
+
+  size_t runDivisionWithNoExceptions() {
+    folly::BenchmarkSuspender suspender;
+    auto rowVector = makeData(2);
+
+    auto exprSet = compileExpression("TRY(c0 / c1)", rowVector->type());
+    suspender.dismiss();
+
+    return doRun(exprSet, rowVector);
+  }
+
+  size_t runDivisionWithOneException() {
+    folly::BenchmarkSuspender suspender;
+    auto numerators = makeData(1)->childAt(0);
+    auto denominatorValues = std::vector<int32_t>(numerators->size(), 1);
+    denominatorValues[0] = 0; // NOLINT
+    auto denominators = vectorMaker_.flatVector(denominatorValues);
+    auto rowVector = vectorMaker_.rowVector({numerators, denominators});
+
+    auto exprSet = compileExpression("TRY(c0 / c1)", rowVector->type());
+    suspender.dismiss();
+
+    return doRun(exprSet, rowVector);
+  }
+
+  size_t runDivisionWithAllExceptions() {
+    folly::BenchmarkSuspender suspender;
+    auto numerators = makeData(1)->childAt(0);
+    auto denominatorValues = std::vector<int32_t>(numerators->size(), 0);
+    auto denominators = vectorMaker_.flatVector(denominatorValues);
+    auto rowVector = vectorMaker_.rowVector({numerators, denominators});
+
+    auto exprSet = compileExpression("TRY(c0 / c1)", rowVector->type());
+    suspender.dismiss();
+
+    return doRun(exprSet, rowVector);
+  }
+
+  size_t doRun(exec::ExprSet& exprSet, const RowVectorPtr& rowVector) {
+    int cnt = 0;
+    for (auto i = 0; i < 100; i++) {
+      cnt += evaluate(exprSet, rowVector)->size();
+    }
+    return cnt;
+  }
+};
+
+BENCHMARK_MULTI(baseline) {
+  TryBenchmark benchmark;
+  return benchmark.runBaseline(50);
+}
+
+BENCHMARK_MULTI(singleOuterTry) {
+  TryBenchmark benchmark;
+  return benchmark.runWithOuterTry(50);
+}
+
+BENCHMARK_MULTI(nestedTries) {
+  TryBenchmark benchmark;
+  return benchmark.runWithNestedTries(50);
+}
+
+BENCHMARK_MULTI(divideNoExceptions) {
+  TryBenchmark benchmark;
+  return benchmark.runDivisionWithNoExceptions();
+}
+
+BENCHMARK_MULTI(divideOneException) {
+  TryBenchmark benchmark;
+  return benchmark.runDivisionWithOneException();
+}
+
+BENCHMARK_MULTI(divideAllExceptions) {
+  TryBenchmark benchmark;
+  return benchmark.runDivisionWithAllExceptions();
+}
+} // namespace
+
+int main(int /*argc*/, char** /*argv*/) {
+  folly::runBenchmarks();
+  return 0;
+}
