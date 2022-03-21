3f60a5f06b7663c55ff5b4eee49ea828648b72db
Ajay George <ajaygeorge@fb.com>
2022-03-21 11:18:29 -0700

    Thriftify Buffer classes

--- presto-main/src/main/java/com/facebook/presto/execution/buffer/BufferState.java
+++ presto-main/src/main/java/com/facebook/presto/execution/buffer/BufferState.java
@@ -13,58 +13,64 @@
  */
 package com.facebook.presto.execution.buffer;
 
+import com.facebook.drift.annotations.ThriftEnum;
+import com.facebook.drift.annotations.ThriftEnumValue;
+
 import java.util.Set;
 import java.util.stream.Stream;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 
+@ThriftEnum
 public enum BufferState
 {
     /**
      * Additional buffers can be added.
      * Any next state is allowed.
      */
-    OPEN(true, true, false),
+    OPEN(true, true, false, 0),
     /**
      * No more buffers can be added.
      * Next state is {@link #FLUSHING}.
      */
-    NO_MORE_BUFFERS(true, false, false),
+    NO_MORE_BUFFERS(true, false, false, 1),
     /**
      * No more pages can be added.
      * Next state is {@link #FLUSHING}.
      */
-    NO_MORE_PAGES(false, true, false),
+    NO_MORE_PAGES(false, true, false, 2),
     /**
      * No more pages or buffers can be added, and buffer is waiting
      * for the final pages to be consumed.
      * Next state is {@link #FINISHED}.
      */
-    FLUSHING(false, false, false),
+    FLUSHING(false, false, false, 3),
     /**
      * No more buffers can be added and all pages have been consumed.
      * This is the terminal state.
      */
-    FINISHED(false, false, true),
+    FINISHED(false, false, true, 4),
     /**
      * Buffer has failed.  No more buffers or pages can be added.  Readers
      * will be blocked, as to not communicate a finished state.  It is
      * assumed that the reader will be cleaned up elsewhere.
      * This is the terminal state.
      */
-    FAILED(false, false, true);
+    FAILED(false, false, true, 5);
 
     public static final Set<BufferState> TERMINAL_BUFFER_STATES = Stream.of(BufferState.values()).filter(BufferState::isTerminal).collect(toImmutableSet());
 
     private final boolean newPagesAllowed;
     private final boolean newBuffersAllowed;
     private final boolean terminal;
+    private final int value;
 
-    BufferState(boolean newPagesAllowed, boolean newBuffersAllowed, boolean terminal)
+    BufferState(boolean newPagesAllowed, boolean newBuffersAllowed, boolean terminal, int value)
     {
         this.newPagesAllowed = newPagesAllowed;
         this.newBuffersAllowed = newBuffersAllowed;
         this.terminal = terminal;
+        this.value = value;
     }
 
     public boolean canAddPages()
@@ -80,5 +86,11 @@ public enum BufferState
     public boolean isTerminal()
     {
         return terminal;
+    }
+
+    @ThriftEnumValue
+    public int getValue()
+    {
+        return value;
     }
 }
