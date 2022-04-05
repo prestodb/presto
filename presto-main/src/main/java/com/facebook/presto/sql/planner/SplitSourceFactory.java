5589e6d741d36a3859be3bdb9d72da8f7ce0a987
Amit Adhikari <amitadhikari@fb.com>
2022-04-05 14:03:09 -0700

    Add missing MergeJoinNode visit methods

--- presto-main/src/main/java/com/facebook/presto/sql/planner/SplitSourceFactory.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/SplitSourceFactory.java
@@ -45,6 +45,7 @@ import com.facebook.presto.sql.planner.plan.GroupIdNode;
 import com.facebook.presto.sql.planner.plan.IndexJoinNode;
 import com.facebook.presto.sql.planner.plan.InternalPlanVisitor;
 import com.facebook.presto.sql.planner.plan.JoinNode;
+import com.facebook.presto.sql.planner.plan.MergeJoinNode;
 import com.facebook.presto.sql.planner.plan.MetadataDeleteNode;
 import com.facebook.presto.sql.planner.plan.OutputNode;
 import com.facebook.presto.sql.planner.plan.RemoteSourceNode;
@@ -167,6 +168,17 @@ public class SplitSourceFactory
 
         @Override
         public Map<PlanNodeId, SplitSource> visitJoin(JoinNode node, Context context)
+        {
+            Map<PlanNodeId, SplitSource> leftSplits = node.getLeft().accept(this, context);
+            Map<PlanNodeId, SplitSource> rightSplits = node.getRight().accept(this, context);
+            return ImmutableMap.<PlanNodeId, SplitSource>builder()
+                    .putAll(leftSplits)
+                    .putAll(rightSplits)
+                    .build();
+        }
+
+        @Override
+        public Map<PlanNodeId, SplitSource> visitMergeJoin(MergeJoinNode node, Context context)
         {
             Map<PlanNodeId, SplitSource> leftSplits = node.getLeft().accept(this, context);
             Map<PlanNodeId, SplitSource> rightSplits = node.getRight().accept(this, context);
