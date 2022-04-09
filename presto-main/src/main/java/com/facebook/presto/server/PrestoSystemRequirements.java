28a7524d99d9377215c735b3e4b72f89b3ce2a95
striverpan <616358337@qq.com>
2022-04-09 22:36:46 +0800

    add arm architecture support option

--- presto-main/src/main/java/com/facebook/presto/server/PrestoSystemRequirements.java
+++ presto-main/src/main/java/com/facebook/presto/server/PrestoSystemRequirements.java
@@ -70,11 +70,14 @@ public final class PrestoSystemRequirements
         String osName = StandardSystemProperty.OS_NAME.value();
         String osArch = StandardSystemProperty.OS_ARCH.value();
         if ("Linux".equals(osName)) {
-            if (!"amd64".equals(osArch) && !"ppc64le".equals(osArch)) {
-                failRequirement("Presto requires amd64 or ppc64le on Linux (found %s)", osArch);
+            if (!"amd64".equals(osArch) && !"ppc64le".equals(osArch) && !"aarch64".equals(osArch)) {
+                failRequirement("Presto requires amd64 or ppc64le or aarch64 on Linux (found %s)", osArch);
             }
             if ("ppc64le".equals(osArch)) {
                 warnRequirement("Support for the POWER architecture is experimental");
+            }
+            if ("aarch64".equals(osArch)) {
+                warnRequirement("Support for the ARM architecture is experimental");
             }
         }
         else if ("Mac OS X".equals(osName)) {
