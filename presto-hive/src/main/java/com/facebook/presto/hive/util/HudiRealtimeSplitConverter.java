e5492325c829ac208dad421210f4fd4313285144
Todd Gao <gaoguantao@bytedance.com>
2022-05-05 18:07:54 +0800

    Upgrade hudi to version 0.11.0 and fix breakings

--- presto-hive/src/main/java/com/facebook/presto/hive/util/HudiRealtimeSplitConverter.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/util/HudiRealtimeSplitConverter.java
@@ -73,6 +73,8 @@ public class HudiRealtimeSplitConverter
                     requireNonNull(customSplitInfo.get(HUDI_BASEPATH_KEY), "HUDI_BASEPATH_KEY is missing"),
                     deltaLogFiles,
                     requireNonNull(customSplitInfo.get(HUDI_MAX_COMMIT_TIME_KEY), "HUDI_MAX_COMMIT_TIME_KEY is missing"),
+                    // false as incremental query is not supported yet
+                    false,
                     Option.empty()));
         }
         return Optional.empty();
