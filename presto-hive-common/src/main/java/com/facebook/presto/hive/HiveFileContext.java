3429b87cd065154ed6622d8eb13f2d0243af7617
Arunachalam Thirupathi <athirupathi@fb.com>
2022-06-02 15:39:28 -0700

    Add read hints to ExtendedFileSystem open API
    Large files are stored in the blob storage. Each worker only processes
    a part of the large file. Before this change the open call, has no hint
    on what part of the file worker is going to work on. This change adds
    the startOffset and length hint, which can be used by a file system
    to open only the part of the file.

--- presto-hive-common/src/main/java/com/facebook/presto/hive/HiveFileContext.java
+++ presto-hive-common/src/main/java/com/facebook/presto/hive/HiveFileContext.java
@@ -17,29 +17,55 @@ import com.facebook.presto.common.RuntimeStats;
 import com.facebook.presto.common.RuntimeUnit;
 
 import java.util.Optional;
+import java.util.OptionalLong;
 
 import static com.facebook.presto.hive.CacheQuota.NO_CACHE_CONSTRAINTS;
 import static java.util.Objects.requireNonNull;
 
 public class HiveFileContext
 {
-    public static final HiveFileContext DEFAULT_HIVE_FILE_CONTEXT = new HiveFileContext(true, NO_CACHE_CONSTRAINTS, Optional.empty(), Optional.empty(), 0, false);
+    public static final HiveFileContext DEFAULT_HIVE_FILE_CONTEXT = new HiveFileContext(
+            true,
+            NO_CACHE_CONSTRAINTS,
+            Optional.empty(),
+            OptionalLong.empty(),
+            OptionalLong.empty(),
+            OptionalLong.empty(),
+            0,
+            false);
 
     private final boolean cacheable;
     private final CacheQuota cacheQuota;
     private final Optional<ExtraHiveFileInfo<?>> extraFileInfo;
-    private final Optional<Long> fileSize;
+    private final OptionalLong fileSize;
+    // HiveFileContext optionally contains startOffset and length that readers are going to read
+    // For large files, these hints will help in fetching only the required blocks from the storage
+    // Note: even when they are present, readers can read past this range like footer and stripe's
+    // start may be in this range, but end may be beyond this range. So this is a hint and readers
+    // should handle the read beyond the range gracefully.
+    private final OptionalLong startOffset;
+    private final OptionalLong length;
     private final long modificationTime;
     private final boolean verboseRuntimeStatsEnabled;
 
     private final RuntimeStats stats = new RuntimeStats();
 
-    public HiveFileContext(boolean cacheable, CacheQuota cacheQuota, Optional<ExtraHiveFileInfo<?>> extraFileInfo, Optional<Long> fileSize, long modificationTime, boolean verboseRuntimeStatsEnabled)
+    public HiveFileContext(
+            boolean cacheable,
+            CacheQuota cacheQuota,
+            Optional<ExtraHiveFileInfo<?>> extraFileInfo,
+            OptionalLong fileSize,
+            OptionalLong startOffset,
+            OptionalLong length,
+            long modificationTime,
+            boolean verboseRuntimeStatsEnabled)
     {
         this.cacheable = cacheable;
         this.cacheQuota = requireNonNull(cacheQuota, "cacheQuota is null");
         this.extraFileInfo = requireNonNull(extraFileInfo, "extraFileInfo is null");
         this.fileSize = requireNonNull(fileSize, "fileSize is null");
+        this.startOffset = requireNonNull(startOffset, "startOffset is null");
+        this.length = requireNonNull(length, "length is null");
         this.modificationTime = modificationTime;
         this.verboseRuntimeStatsEnabled = verboseRuntimeStatsEnabled;
     }
@@ -70,9 +96,19 @@ public class HiveFileContext
         return extraFileInfo;
     }
 
-    public Optional<Long> getFileSize()
+    public OptionalLong getFileSize()
     {
         return fileSize;
+    }
+
+    public OptionalLong getStartOffset()
+    {
+        return startOffset;
+    }
+
+    public OptionalLong getLength()
+    {
+        return length;
     }
 
     public interface ExtraHiveFileInfo<T>
