a6e2a86f2eb6d6ee99b577252cbc6b3e8a496deb
Deepak Majeti <deepak@ahana.io>
2022-07-07 11:51:12 -0700

    Add option to include results in TpchBenchmark output (#1967)
    Summary:
    Results are useful to verify correctness.
    
    Sample output
    ```
    velox_tpch_benchmark --data_format="dwrf" --data_path="/data/tpchdwrf10" --run_query_verbose=1 --include_results
    Results:
    ROW<l_returnflag:VARCHAR,l_linestatus:VARCHAR,a0:DOUBLE,a1:DOUBLE,a2:DOUBLE,a3:DOUBLE,a4:DOUBLE,a5:DOUBLE,a6:DOUBLE,a7:BIGINT>
    {A, F, 377518399, 566065727797.2493, 537759104278.056, 559276670892.1042, 25.500975103007097, 38237.1510089585, 0.05000657453995794, 14804077}
    {N, F, 9851614, 14767438399.170023, 14028805792.211483, 14590490998.366697, 25.522448302840946, 38257.8106600812, 0.04997336773768567, 385998}
    {N, O, 743124873, 1114302286901.8455, 1058580922144.9487, 1100937000170.6143, 25.498075870689316, 38233.90292348062, 0.05000081182194938, 29144351}
    {R, F, 377732830, 566431054976.0214, 538110922664.7752, 559634780885.104, 25.50838478968014, 38251.219273561204, 0.049996792313801046, 14808183}
    
    Execution time: 3.60s
    Splits total: 80, finished: 80
    .....
    ```
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/1967
    
    Reviewed By: laithsakka, kagamiori
    
    Differential Revision: D37691668
    
    Pulled By: mbasmanova
    
    fbshipit-source-id: cb2a46ae230f237518eace0629e8e20d36ebe543

--- velox/benchmarks/tpch/TpchBenchmark.cpp
+++ velox/benchmarks/tpch/TpchBenchmark.cpp
@@ -46,7 +46,7 @@ static bool validateDataFormat(const char* flagname, const std::string& value) {
   }
   std::cout
       << fmt::format(
-             "Invalid value for --{}: {}. Allowed values are [\"parquet\", \"orc\"]",
+             "Invalid value for --{}: {}. Allowed values are [\"parquet\", \"dwrf\"]",
              flagname,
              value)
       << std::endl;
@@ -56,6 +56,21 @@ static bool validateDataFormat(const char* flagname, const std::string& value) {
 void ensureTaskCompletion(exec::Task* task) {
   // ASSERT_TRUE requires a function with return type void.
   ASSERT_TRUE(waitForTaskCompletion(task));
+}
+
+void printResults(const std::vector<RowVectorPtr>& results) {
+  std::cout << "Results:" << std::endl;
+  bool printType = true;
+  for (const auto& vector : results) {
+    // Print RowType only once.
+    if (printType) {
+      std::cout << vector->type()->asRow().toString() << std::endl;
+      printType = false;
+    }
+    for (size_t i = 0; i < vector->size(); ++i) {
+      std::cout << vector->toString(i) << std::endl;
+    }
+  }
 }
 } // namespace
 
@@ -68,6 +83,7 @@ DEFINE_bool(
     include_custom_stats,
     false,
     "Include custom statistics along with execution statistics");
+DEFINE_bool(include_results, false, "Include results in the output");
 DEFINE_int32(num_drivers, 4, "Number of drivers");
 DEFINE_string(data_format, "parquet", "Data format");
 DEFINE_int32(num_splits_per_file, 10, "Number of splits per file");
@@ -90,7 +106,8 @@ class TpchBenchmark {
     connector::registerConnector(hiveConnector);
   }
 
-  std::shared_ptr<Task> run(const TpchPlan& tpchPlan) {
+  std::pair<std::unique_ptr<TaskCursor>, std::vector<RowVectorPtr>> run(
+      const TpchPlan& tpchPlan) {
     CursorParameters params;
     params.maxDrivers = FLAGS_num_drivers;
     params.planNode = tpchPlan.plan;
@@ -112,8 +129,7 @@ class TpchBenchmark {
       }
       noMoreSplits = true;
     };
-    auto [cursor, results] = readCursor(params, addSplits);
-    return cursor->task();
+    return readCursor(params, addSplits);
   }
 };
 
@@ -165,8 +181,13 @@ int main(int argc, char** argv) {
     folly::runBenchmarks();
   } else {
     const auto queryPlan = queryBuilder->getQueryPlan(FLAGS_run_query_verbose);
-    const auto task = benchmark.run(queryPlan);
+    const auto [cursor, actualResults] = benchmark.run(queryPlan);
+    auto task = cursor->task();
     ensureTaskCompletion(task.get());
+    if (FLAGS_include_results) {
+      printResults(actualResults);
+      std::cout << std::endl;
+    }
     const auto stats = task->taskStats();
     std::cout << fmt::format(
                      "Execution time: {}",
