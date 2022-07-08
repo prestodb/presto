0145056233b06846183b6d8c424df88d2667e3c0
Kimberly Yang <kimtyang@fb.com>
2022-07-08 14:37:47 -0700

    Started with the structure of reusing result in FlatMapColumnReader
    Summary: Separated assignment of result based on whether a new shared pointer needs to be created. If the result vector is not null and the type is consistent with the existing mapType, we can adjust the contents of the VectorPtr without creating a new shared pointer.
    
    Differential Revision: D37706526
    
    fbshipit-source-id: 9429c2491673f938dec57c319147487520715092

--- velox/dwio/dwrf/reader/FlatMapColumnReader.cpp
+++ velox/dwio/dwrf/reader/FlatMapColumnReader.cpp
@@ -453,16 +453,39 @@ void FlatMapColumnReader<T>::next(
       : MAP(keysVector->type(), valuesVector->type());
 
   // TODO Reuse
-  result = std::make_shared<MapVector>(
-      &memoryPool_,
-      mapType,
-      nulls,
-      numValues,
-      offsets,
-      lengths,
-      keysVector,
-      valuesVector,
-      nullCount);
+  if (result && result->type() == mapType) {
+    LOG(INFO) << "reusing flat map result" << std::endl;
+    // not assigning memoryPool_ and mapType for now
+    // figuring out how to assign variables instead of creating new shared ptr
+    // mapVector->nulls = nulls;
+    // mapVector->length = numValues;
+    // mapVector->offsets() = std::move(offsets); // const
+    // mapVector->sizes() = std::move(lengths); // const
+    // mapVector->mapKeys() = std::move(keysVector);
+    // mapVector->mapValues() = std::move(valuesVector);
+    // mapVector->nullCount
+    result = std::make_shared<MapVector>(
+        &memoryPool_,
+        mapType,
+        nulls,
+        numValues,
+        offsets,
+        lengths,
+        keysVector,
+        valuesVector,
+        nullCount);
+  } else {
+    result = std::make_shared<MapVector>(
+        &memoryPool_,
+        mapType,
+        nulls,
+        numValues,
+        offsets,
+        lengths,
+        keysVector,
+        valuesVector,
+        nullCount);
+  }
 }
 
 template <>
