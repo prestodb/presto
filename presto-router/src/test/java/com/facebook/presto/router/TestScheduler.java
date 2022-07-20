633e977a052e9829ca7a94c96124dc2616da2518
Chunxu Tang <chunxut@twitter.com>
2022-07-19 21:24:55 -0700

    Add user hash scheduler in the presto router

--- presto-router/src/test/java/com/facebook/presto/router/TestScheduler.java
+++ presto-router/src/test/java/com/facebook/presto/router/TestScheduler.java
@@ -1,13 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.facebook.presto.router;
 
 import com.facebook.presto.router.scheduler.RandomChoiceScheduler;
 import com.facebook.presto.router.scheduler.Scheduler;
+import com.facebook.presto.router.scheduler.UserHashScheduler;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.URI;
 import java.util.ArrayList;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class TestScheduler
@@ -33,5 +48,21 @@ public class TestScheduler
 
         URI target = scheduler.getDestination("test").orElse(new URI("invalid"));
         assertTrue(servers.contains(target));
+    }
+
+    @Test
+    public void testUserHashScheduler()
+            throws Exception
+    {
+        scheduler = new UserHashScheduler();
+        scheduler.setCandidates(servers);
+
+        URI target1 = scheduler.getDestination("test").orElse(new URI("invalid"));
+        assertTrue(servers.contains(target1));
+
+        URI target2 = scheduler.getDestination("test").orElse(new URI("invalid"));
+        assertTrue(servers.contains(target2));
+
+        assertEquals(target2, target1);
     }
 }
