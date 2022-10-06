df87ad89be9d46e778033be39f2df005047731e9
tanjialiang <jacob.jialiang.tan@gmail.com>
2022-10-05 19:50:40 -0700

    Add HttpNativeExecutionTaskInfoFetcher and disable some tests

--- presto-spark-base/src/main/java/com/facebook/presto/spark/execution/http/PrestoSparkHttpWorkerClient.java
+++ presto-spark-base/src/main/java/com/facebook/presto/spark/execution/http/PrestoSparkHttpWorkerClient.java
@@ -18,24 +18,39 @@ import com.facebook.airlift.http.client.HttpStatus;
 import com.facebook.airlift.http.client.Request;
 import com.facebook.airlift.http.client.Response;
 import com.facebook.airlift.http.client.ResponseHandler;
+import com.facebook.airlift.json.JsonCodec;
 import com.facebook.airlift.log.Logger;
+import com.facebook.presto.Session;
 import com.facebook.presto.execution.TaskId;
+import com.facebook.presto.execution.TaskInfo;
+import com.facebook.presto.execution.TaskSource;
+import com.facebook.presto.execution.buffer.OutputBuffers;
+import com.facebook.presto.execution.scheduler.TableWriteInfo;
 import com.facebook.presto.operator.HttpRpcShuffleClient;
 import com.facebook.presto.operator.PageBufferClient;
 import com.facebook.presto.operator.RpcShuffleClient;
+import com.facebook.presto.server.TaskUpdateRequest;
+import com.facebook.presto.server.smile.BaseResponse;
+import com.facebook.presto.sql.planner.PlanFragment;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.airlift.units.DataSize;
 
 import javax.annotation.concurrent.ThreadSafe;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 import static com.facebook.airlift.http.client.HttpStatus.familyForStatusCode;
 import static com.facebook.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
 import static com.facebook.airlift.http.client.Request.Builder.prepareDelete;
 import static com.facebook.airlift.http.client.Request.Builder.prepareGet;
+import static com.facebook.airlift.http.client.Request.Builder.preparePost;
+import static com.facebook.airlift.http.client.StaticBodyGenerator.createStaticBodyGenerator;
 import static com.facebook.airlift.http.client.StatusResponseHandler.createStatusResponseHandler;
 import static com.facebook.presto.client.PrestoHeaders.PRESTO_MAX_SIZE;
+import static com.facebook.presto.server.RequestHelpers.setContentTypeHeaders;
+import static com.facebook.presto.server.smile.AdaptingJsonResponseHandler.createAdaptingJsonResponseHandler;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -49,13 +64,23 @@ public class PrestoSparkHttpWorkerClient
 
     private final HttpClient httpClient;
     private final URI location;
+    private final URI taskUri;
     private final TaskId taskId;
+    private final JsonCodec<TaskInfo> taskInfoCodec;
+    private final JsonCodec<PlanFragment> planFragmentCodec;
+    private final JsonCodec<TaskUpdateRequest> taskUpdateRequestCodec;
 
     public PrestoSparkHttpWorkerClient(HttpClient httpClient, TaskId taskId, URI location)
     {
         this.httpClient = requireNonNull(httpClient, "httpClient is null");
         this.taskId = requireNonNull(taskId, "taskId is null");
         this.location = requireNonNull(location, "location is null");
+        this.taskInfoCodec = JsonCodec.jsonCodec(TaskInfo.class);
+        this.planFragmentCodec = JsonCodec.jsonCodec(PlanFragment.class);
+        this.taskUpdateRequestCodec = JsonCodec.jsonCodec(TaskUpdateRequest.class);
+        this.taskUri = uriBuilderFrom(location)
+                .appendPath(taskId.toString())
+                .build();
     }
 
     /**
@@ -126,6 +151,40 @@ public class PrestoSparkHttpWorkerClient
     public Throwable rewriteException(Throwable throwable)
     {
         return null;
+    }
+
+    public ListenableFuture<BaseResponse<TaskInfo>> getTaskInfo()
+    {
+        Request request = setContentTypeHeaders(false, prepareGet())
+                .setUri(taskUri)
+                .build();
+        ResponseHandler responseHandler = createAdaptingJsonResponseHandler(taskInfoCodec);
+        return httpClient.executeAsync(request, responseHandler);
+    }
+
+    public ListenableFuture<BaseResponse<TaskInfo>> updateTask(
+            List<TaskSource> sources,
+            PlanFragment planFragment,
+            TableWriteInfo tableWriteInfo,
+            Session session,
+            OutputBuffers outputBuffers)
+    {
+        Optional<byte[]> fragment = Optional.of(planFragment.toBytes(planFragmentCodec));
+        Optional<TableWriteInfo> writeInfo = Optional.of(tableWriteInfo);
+        TaskUpdateRequest updateRequest = new TaskUpdateRequest(
+                session.toSessionRepresentation(),
+                session.getIdentity().getExtraCredentials(),
+                fragment,
+                sources,
+                outputBuffers,
+                writeInfo);
+
+        return httpClient.executeAsync(
+                setContentTypeHeaders(false, preparePost())
+                        .setUri(taskUri)
+                        .setBodyGenerator(createStaticBodyGenerator(taskUpdateRequestCodec.toBytes(updateRequest)))
+                        .build(),
+                createAdaptingJsonResponseHandler(taskInfoCodec));
     }
 
     public URI getLocation()
