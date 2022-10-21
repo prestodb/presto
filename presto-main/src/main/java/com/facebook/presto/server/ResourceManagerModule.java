bc873b879841c7e054a213d40f6a149cdc2718e4
Rohit Jain <jainxrohit@gmail.com>
2022-10-20 21:11:58 -0700

    Rename QueryPreparer to BuiltInQueryPreparer

--- presto-main/src/main/java/com/facebook/presto/server/ResourceManagerModule.java
+++ presto-main/src/main/java/com/facebook/presto/server/ResourceManagerModule.java
@@ -32,7 +32,7 @@ import com.facebook.presto.resourcemanager.RatisServer;
 import com.facebook.presto.resourcemanager.ResourceManagerClusterStateProvider;
 import com.facebook.presto.resourcemanager.ResourceManagerProxy;
 import com.facebook.presto.resourcemanager.ResourceManagerServer;
-import com.facebook.presto.sql.analyzer.QueryPreparer;
+import com.facebook.presto.sql.analyzer.BuiltInQueryPreparer;
 import com.facebook.presto.transaction.NoOpTransactionManager;
 import com.facebook.presto.transaction.TransactionManager;
 import com.google.inject.Binder;
@@ -80,7 +80,7 @@ public class ResourceManagerModule
         binder.bind(QueryManager.class).to(NoOpQueryManager.class).in(Scopes.SINGLETON);
         jaxrsBinder(binder).bind(DistributedResourceGroupInfoResource.class);
         binder.bind(QueryIdGenerator.class).in(Scopes.SINGLETON);
-        binder.bind(QueryPreparer.class).in(Scopes.SINGLETON);
+        binder.bind(BuiltInQueryPreparer.class).in(Scopes.SINGLETON);
         binder.bind(SessionSupplier.class).to(QuerySessionSupplier.class).in(Scopes.SINGLETON);
 
         binder.bind(ResourceGroupManager.class).to(NoOpResourceGroupManager.class);
