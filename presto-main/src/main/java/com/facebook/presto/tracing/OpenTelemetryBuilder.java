245345549fc5cf1590b707c0c4764808fd8db700
Alex Chen <achen2289@gmail.com>
2022-10-26 01:11:53 +0000

    add back trace exporting

--- presto-main/src/main/java/com/facebook/presto/tracing/OpenTelemetryBuilder.java
+++ presto-main/src/main/java/com/facebook/presto/tracing/OpenTelemetryBuilder.java
@@ -17,9 +17,11 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
 public final class OpenTelemetryBuilder
@@ -35,10 +37,9 @@ public final class OpenTelemetryBuilder
                 .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "presto")));
 
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().build()).build())
                 .setResource(resource)
                 .build();
-
-//                .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().build()).build())
 
         OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
                 .setTracerProvider(sdkTracerProvider)
