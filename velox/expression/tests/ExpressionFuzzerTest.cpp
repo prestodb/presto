64ebc87f035fb40c0d14cecad0832755f321d6f4
Masha Basmanova <mbasmanova@fb.com>
2022-11-15 10:51:15 -0800

    Remove neq from Fuzzer's skip list (#3245)
    Summary:
    There is no GitHub issue attached to neq function in the skip list, hence, it is
    not clear why it was added in the first place. Removing the function from the
    skip list and running the fuzzer doesn't result in an error.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/3245
    
    Reviewed By: xiaoxmeng
    
    Differential Revision: D41308613
    
    Pulled By: mbasmanova
    
    fbshipit-source-id: 857e09528104fc21aa60374fce095bc8947fe7fe

--- velox/expression/tests/ExpressionFuzzerTest.cpp
+++ velox/expression/tests/ExpressionFuzzerTest.cpp
@@ -58,7 +58,7 @@ int main(int argc, char** argv) {
       // cardinality passing a VARBINARY (since HLL's implementation uses an
       // alias to VARBINARY).
       "cardinality",
-      "neq"};
+  };
   size_t initialSeed = FLAGS_seed == 0 ? std::time(nullptr) : FLAGS_seed;
   return FuzzerRunner::run(
       FLAGS_only, initialSeed, skipFunctions, FLAGS_special_forms);
