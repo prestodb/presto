9c970b6bd075b543e139611906fcac3fde6f4f20
Huameng Jiang <huamengjiang@meta.com>
2022-11-16 16:47:02 -0800

    More informative SimpleMemoryTracker exception message (#3278)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/3278
    
    onthetin
    
    Differential Revision: D41353026
    
    fbshipit-source-id: ebf618e67ba4c089eb6afba05971f7f9bdcfdb8d

--- velox/common/memory/MemoryUsageTracker.cpp
+++ velox/common/memory/MemoryUsageTracker.cpp
@@ -245,7 +245,10 @@ void SimpleMemoryTracker::update(int64_t size, bool mock) {
   int64_t previousUsage =
       totalUserMemory_.fetch_add(size, std::memory_order_relaxed);
   if (size > 0 && previousUsage + size > userMemoryQuota_) {
-    VELOX_MEM_CAP_EXCEEDED(userMemoryQuota_);
+    VELOX_MEM_CAP_EXCEEDED(fmt::format(
+        MEM_CAP_EXCEEDED_ERROR_FORMAT.data(),
+        succinctBytes(userMemoryQuota_),
+        succinctBytes(size)));
   }
 }
 
