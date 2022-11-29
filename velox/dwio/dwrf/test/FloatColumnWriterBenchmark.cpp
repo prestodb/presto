abb9de2233c770c1029845ce9920a417864df4e2
Laith Sakka <lsakka@meta.com>
2022-11-28 20:49:05 -0800

    Refactor FloatColumnWriter benchmark
    Summary:
    - mostly avoid using benchmark(_, n) since it does not guarantee consistency across runs.
    - Some clean up.
    
    ```
    ============================================================================
    [...]f/test/FloatColumnWriterBenchmark.cpp     relative  time/iter   iters/s
    ============================================================================
    FloatColumnWriterBenchmark2                                 7.10ms    140.88
    FloatColumnWriterBenchmark5                                 8.96ms    111.65
    FloatColumnWriterBenchmark10                                9.94ms    100.58
    FloatColumnWriterBenchmark25                               10.63ms     94.06
    FloatColumnWriterBenchmark50                                9.62ms    103.94
    FloatColumnWriterBenchmark100                               9.30ms    107.49
    FloatColumnWriterBenchmark500                               9.25ms    108.07
    FloatColumnWriterBenchmark1000                              9.39ms    106.44
    FloatColumnWriterBenchmark2000                              8.91ms    112.27
    FloatColumnWriterBenchmark5000                              8.75ms    114.33
    FloatColumnWriterBenchmark10000                             6.72ms    148.75
    
    ```
    
    Reviewed By: xiaoxmeng
    
    Differential Revision: D41568598
    
    fbshipit-source-id: 666ef4acca1c45fc9e6853d05794ef2dc01ca567

--- velox/dwio/dwrf/test/FloatColumnWriterBenchmark.cpp
+++ velox/dwio/dwrf/test/FloatColumnWriterBenchmark.cpp
@@ -26,151 +26,103 @@ using namespace facebook::velox::dwio::common;
 using namespace facebook::velox;
 using namespace facebook::velox::dwrf;
 
-bool isNotNull(int32_t pos, int32_t run_len) {
-  return pos == 0 || pos % run_len != 0;
-}
+constexpr vector_size_t kVectorSize = 10000;
+vector_size_t kNumIterations = 100;
 
 float genData(float pos) {
   return float(pos * (float)3.14);
 }
 
-int32_t size = 0;
-int32_t run_len = 1;
+bool isNotNull(int32_t pos, int32_t nullEvery) {
+  return pos == 0 || pos % nullEvery != 0;
+}
 
-void FloatColumnWriterBenchmarkbase() {
+void runBenchmark(int nullEvery) {
+  folly::BenchmarkSuspender braces;
+
   auto type = CppToType<float>::create();
   auto typeWithId = TypeWithId::create(type, 1);
-
   auto pool = memory::getDefaultMemoryPool();
-  folly::BenchmarkSuspender braces;
   VectorPtr vector;
+  // Prepare input
+  BufferPtr values = AlignedBuffer::allocate<float>(kVectorSize, pool.get());
+  auto valuesPtr = values->asMutable<float>();
 
-  // Generate sample data
-  {
-    // Prepare input
-    BufferPtr values = AlignedBuffer::allocate<float>(size, pool.get());
-    auto valuesPtr = values->asMutable<float>();
+  BufferPtr nulls =
+      AlignedBuffer::allocate<char>(bits::nbytes(kVectorSize), pool.get());
+  auto* nullsPtr = nulls->asMutable<uint64_t>();
 
-    BufferPtr nulls =
-        AlignedBuffer::allocate<char>(bits::nbytes(size), pool.get());
-    auto* nullsPtr = nulls->asMutable<uint64_t>();
-
-    uint32_t nullCount = 0;
-    for (size_t i = 0; i < size; ++i) {
-      bool isPresent = isNotNull(i, run_len);
-      bits::setNull(nullsPtr, i, !isPresent);
-      if (isPresent) {
-        valuesPtr[i] = genData(i);
-      } else {
-        ++nullCount;
-      }
+  uint32_t nullCount = 0;
+  for (size_t i = 0; i < kVectorSize; ++i) {
+    bool isPresent = isNotNull(i, nullEvery);
+    bits::setNull(nullsPtr, i, !isPresent);
+    if (isPresent) {
+      valuesPtr[i] = genData(i);
+    } else {
+      ++nullCount;
     }
-
-    vector = std::make_shared<FlatVector<float>>(
-        pool.get(),
-        nullCount == 0 ? nullptr : nulls,
-        size,
-        values,
-        std::vector<BufferPtr>{});
   }
 
-  // Writer
-  {
-    // write
+  vector = std::make_shared<FlatVector<float>>(
+      pool.get(),
+      nullCount == 0 ? nullptr : nulls,
+      kVectorSize,
+      values,
+      std::vector<BufferPtr>{});
+
+  // write
+  braces.dismiss();
+
+  for (auto i = 0; i < kNumIterations; i++) {
     auto config = std::make_shared<Config>();
     WriterContext context{config, memory::getDefaultMemoryPool()};
     auto writer = BaseColumnWriter::create(context, *typeWithId, 0);
-    braces.dismiss();
-    writer->write(vector, common::Ranges::of(0, size));
+    writer->write(vector, common::Ranges::of(0, kVectorSize));
   }
 }
 
-BENCHMARK(FloatColumnWriterBenchmark2, n) {
-  size = 10000;
-  run_len = 2;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark2) {
+  runBenchmark(2);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark5, n) {
-  size = 10000;
-  run_len = 5;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark5) {
+  runBenchmark(5);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark10, n) {
-  size = 10000;
-  run_len = 10;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark10) {
+  runBenchmark(10);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark25, n) {
-  size = 10000;
-  run_len = 25;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark25) {
+  runBenchmark(25);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark50, n) {
-  size = 10000;
-  run_len = 50;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark50) {
+  runBenchmark(50);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark100, n) {
-  size = 10000;
-  run_len = 100;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark100) {
+  runBenchmark(100);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark500, n) {
-  size = 10000;
-  run_len = 500;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark500) {
+  runBenchmark(500);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark1000, n) {
-  size = 10000;
-  run_len = 1000;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark1000) {
+  runBenchmark(1000);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark2000, n) {
-  size = 10000;
-  run_len = 2000;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark2000) {
+  runBenchmark(2000);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark5000, n) {
-  size = 10000;
-  run_len = 5000;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark5000) {
+  runBenchmark(5000);
 }
 
-BENCHMARK(FloatColumnWriterBenchmark10000, n) {
-  size = 10000;
-  run_len = 10000;
-  for (size_t i = 0; i < n; i++) {
-    FloatColumnWriterBenchmarkbase();
-  }
+BENCHMARK(FloatColumnWriterBenchmark10000) {
+  runBenchmark(10000);
 }
 
 int32_t main(int32_t argc, char* argv[]) {
