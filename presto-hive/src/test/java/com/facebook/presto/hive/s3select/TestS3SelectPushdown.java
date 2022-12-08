cd67349f9ae29721b032f6ded4e910642b5c08ab
dnnanuti <dnnanuti@amazon.com>
2022-12-08 16:45:23 +0000

    Refactor S3 tests
    Move common methods for Hive File System tests setup to utility class.
    Add more test cases for S3 Select Pushdown enabling.

--- presto-hive/src/test/java/com/facebook/presto/hive/s3select/TestS3SelectPushdown.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/s3select/TestS3SelectPushdown.java
@@ -13,24 +13,94 @@
  */
 package com.facebook.presto.hive.s3select;
 
+import com.facebook.presto.hive.metastore.Column;
+import com.facebook.presto.hive.metastore.Partition;
+import com.facebook.presto.hive.metastore.Storage;
+import com.facebook.presto.hive.metastore.StorageFormat;
+import com.facebook.presto.hive.metastore.Table;
+import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.testing.TestingConnectorSession;
+import com.google.common.collect.ImmutableMap;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.TextInputFormat;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Optional;
+import java.util.Properties;
+
+import static com.facebook.presto.hive.HiveStorageFormat.ORC;
+import static com.facebook.presto.hive.HiveStorageFormat.TEXTFILE;
+import static com.facebook.presto.hive.HiveType.HIVE_BINARY;
+import static com.facebook.presto.hive.HiveType.HIVE_BOOLEAN;
+import static com.facebook.presto.hive.metastore.PrestoTableType.EXTERNAL_TABLE;
+import static com.facebook.presto.hive.metastore.StorageFormat.fromHiveStorageFormat;
+import static com.facebook.presto.hive.s3select.S3SelectPushdown.shouldEnablePushdownForTable;
+import static com.facebook.presto.spi.session.PropertyMetadata.booleanProperty;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonList;
+import static org.apache.hadoop.hive.serde.serdeConstants.SERIALIZATION_LIB;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class TestS3SelectPushdown
 {
+    private static final String S3_SELECT_PUSHDOWN_ENABLED = "s3_select_pushdown_enabled";
+
     private TextInputFormat inputFormat;
+    private ConnectorSession session;
+    private Table table;
+    private Partition partition;
+    private Storage storage;
+    private Column column;
+    private Properties schema;
 
     @BeforeClass
     public void setUp()
     {
         inputFormat = new TextInputFormat();
         inputFormat.configure(new JobConf());
+
+        session = initTestingConnectorSession(true);
+
+        column = new Column("column", HIVE_BOOLEAN, Optional.empty(), Optional.empty());
+
+        storage = Storage.builder()
+                .setStorageFormat(fromHiveStorageFormat(TEXTFILE))
+                .setLocation("location")
+                .build();
+
+        partition = new Partition(
+                "db",
+                "table",
+                emptyList(),
+                storage,
+                singletonList(column),
+                emptyMap(),
+                Optional.empty(),
+                false,
+                false,
+                1234,
+                4567L);
+
+        table = new Table(
+                "db",
+                "table",
+                "owner",
+                EXTERNAL_TABLE,
+                storage,
+                singletonList(column),
+                emptyList(),
+                emptyMap(),
+                Optional.empty(),
+                Optional.empty());
+
+        schema = new Properties();
+        schema.setProperty(SERIALIZATION_LIB, LazySimpleSerDe.class.getName());
     }
 
     @Test
@@ -41,5 +111,139 @@ public class TestS3SelectPushdown
         assertFalse(S3SelectPushdown.isCompressionCodecSupported(inputFormat, new Path("s3://fakeBucket/fakeObject.lz4")));
         assertFalse(S3SelectPushdown.isCompressionCodecSupported(inputFormat, new Path("s3://fakeBucket/fakeObject.snappy")));
         assertTrue(S3SelectPushdown.isCompressionCodecSupported(inputFormat, new Path("s3://fakeBucket/fakeObject.bz2")));
+    }
+
+    @Test
+    public void testShouldEnableSelectPushdown()
+    {
+        assertTrue(shouldEnablePushdownForTable(session, table, "s3://fakeBucket/fakeObject", Optional.empty()));
+        assertTrue(shouldEnablePushdownForTable(session, table, "s3://fakeBucket/fakeObject", Optional.of(partition)));
+    }
+
+    @Test
+    public void testShouldNotEnableSelectPushdownWhenDisabledOnSession()
+    {
+        ConnectorSession testSession = initTestingConnectorSession(false);
+        assertFalse(shouldEnablePushdownForTable(testSession, table, "", Optional.empty()));
+    }
+
+    @Test
+    public void testShouldNotEnableSelectPushdownWhenIsNotS3StoragePath()
+    {
+        assertFalse(shouldEnablePushdownForTable(session, table, null, Optional.empty()));
+        assertFalse(shouldEnablePushdownForTable(session, table, "", Optional.empty()));
+        assertFalse(shouldEnablePushdownForTable(session, table, "s3:/invalid", Optional.empty()));
+        assertFalse(shouldEnablePushdownForTable(session, table, "s3:/invalid", Optional.of(partition)));
+    }
+
+    @Test
+    public void testShouldNotEnableSelectPushdownWhenIsNotSupportedSerde()
+    {
+        Storage newStorage = Storage.builder()
+                .setStorageFormat(fromHiveStorageFormat(ORC))
+                .setLocation("location")
+                .build();
+        Table newTable = new Table(
+                "db",
+                "table",
+                "owner",
+                EXTERNAL_TABLE,
+                newStorage,
+                singletonList(column),
+                emptyList(),
+                emptyMap(),
+                Optional.empty(),
+                Optional.empty());
+
+        assertFalse(shouldEnablePushdownForTable(session, newTable, "s3://fakeBucket/fakeObject", Optional.empty()));
+
+        Partition newPartition = new Partition(
+                "db",
+                "table",
+                emptyList(),
+                newStorage,
+                singletonList(column),
+                emptyMap(),
+                Optional.empty(),
+                false,
+                false,
+                1234,
+                4567L);
+        assertFalse(shouldEnablePushdownForTable(session, newTable, "s3://fakeBucket/fakeObject", Optional.of(newPartition)));
+    }
+
+    @Test
+    public void testShouldNotEnableSelectPushdownWhenInputFormatIsNotSupported()
+    {
+        Storage newStorage = Storage.builder()
+                .setStorageFormat(StorageFormat.create(LazySimpleSerDe.class.getName(), "inputFormat", "outputFormat"))
+                .setLocation("location")
+                .build();
+        Table newTable = new Table(
+                "db",
+                "table",
+                "owner",
+                EXTERNAL_TABLE,
+                newStorage,
+                singletonList(column),
+                emptyList(),
+                emptyMap(),
+                Optional.empty(),
+                Optional.empty());
+        assertFalse(shouldEnablePushdownForTable(session, newTable, "s3://fakeBucket/fakeObject", Optional.empty()));
+    }
+
+    @Test
+    public void testShouldNotEnableSelectPushdownWhenColumnTypesAreNotSupported()
+    {
+        Column newColumn = new Column("column", HIVE_BINARY, Optional.empty(), Optional.empty());
+        Table newTable = new Table(
+                "db",
+                "table",
+                "owner",
+                EXTERNAL_TABLE,
+                storage,
+                singletonList(newColumn),
+                emptyList(),
+                emptyMap(),
+                Optional.empty(),
+                Optional.empty());
+        assertFalse(shouldEnablePushdownForTable(session, newTable, "s3://fakeBucket/fakeObject", Optional.empty()));
+
+        Partition newPartition = new Partition(
+                "db",
+                "table",
+                emptyList(),
+                storage,
+                singletonList(column),
+                emptyMap(),
+                Optional.empty(),
+                false,
+                false,
+                1234,
+                4567L);
+        assertFalse(shouldEnablePushdownForTable(session, newTable, "s3://fakeBucket/fakeObject", Optional.of(newPartition)));
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown()
+    {
+        inputFormat = null;
+        session = null;
+        table = null;
+        partition = null;
+        storage = null;
+        column = null;
+        schema = null;
+    }
+
+    private TestingConnectorSession initTestingConnectorSession(boolean enableSelectPushdown)
+    {
+        return new TestingConnectorSession(singletonList(booleanProperty(
+                S3_SELECT_PUSHDOWN_ENABLED,
+                "S3 Select pushdown enabled",
+                true,
+                false)),
+                ImmutableMap.of(S3_SELECT_PUSHDOWN_ENABLED, enableSelectPushdown));
     }
 }
