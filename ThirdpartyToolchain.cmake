b06bf84f4df7ea21c1c54d1edb5e253ac765b3a5
Jacob Wujciak-Jens <jacob@wujciak.de>
2022-12-13 13:58:30 -0800

    Move fmt to use resolve_dependency (#3100)
    Summary:
    This PR moves fmt to use resolve_dependency and makes the dependency source (optionally) configurable via envvar. This enables the CI to test correctly with fmt from source even with system fmt installed.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/3100
    
    Reviewed By: Yuhta
    
    Differential Revision: D42003001
    
    Pulled By: kgpai
    
    fbshipit-source-id: 71465e4d58d3d28e7c4c9f6b6a564ec35982fc0c

--- ThirdpartyToolchain.cmake
+++ ThirdpartyToolchain.cmake
@@ -181,6 +181,31 @@ macro(build_pybind11)
 endmacro()
 
 # ================================ END PYBIND11 ================================
+# ================================ FMT =========================================
+if(DEFINED ENV{VELOX_FMT_URL})
+  set(VELOX_FMT_SOURCE_URL "$ENV{VELOX_FMT_URL}")
+else()
+  set(VELOX_FMT_VERSION 8.0.1)
+  set(VELOX_FMT_SOURCE_URL
+      "https://github.com/fmtlib/fmt/archive/${VELOX_FMT_VERSION}.tar.gz")
+  set(VELOX_FMT_BUILD_SHA256_CHECKSUM
+      b06ca3130158c625848f3fb7418f235155a4d389b2abc3a6245fb01cb0eb1e01)
+endif()
+
+macro(build_fmt)
+  message(STATUS "Building fmt from source")
+  FetchContent_Declare(
+    fmt
+    URL ${VELOX_FMT_SOURCE_URL}
+    URL_HASH SHA256=${VELOX_FMT_BUILD_SHA256_CHECKSUM})
+
+  # Force fmt to create fmt-config.cmake which can be found by other dependecies
+  # (e.g. folly)
+  set(FMT_INSTALL ON)
+  set(fmt_BUILD_TESTS OFF)
+  FetchContent_MakeAvailable(fmt)
+endmacro()
+# ================================ END FMT ================================
 
 macro(build_dependency DEPENDENCY_NAME)
   if("${DEPENDENCY_NAME}" STREQUAL "folly")
@@ -189,6 +214,8 @@ macro(build_dependency DEPENDENCY_NAME)
     build_protobuf()
   elseif("${DEPENDENCY_NAME}" STREQUAL "pybind11")
     build_pybind11()
+  elseif("${DEPENDENCY_NAME}" STREQUAL "fmt")
+    build_fmt()
   else()
     message(
       FATAL_ERROR "Unknown thirdparty dependency to build: ${DEPENDENCY_NAME}")
