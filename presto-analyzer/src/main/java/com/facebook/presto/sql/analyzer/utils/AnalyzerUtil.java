001dbd525f935c6d2f178c6eb84638efc9d53b79
Rohit Jain <jainxrohit@gmail.com>
2022-12-19 17:56:25 -0800

    Remove ParsingOptions from AnalyzerOptions
    Usage of the ParsingOptions creates a dependency on the presto-parser
    for analyzer interfaces. Presto analyzer interfaces would move to the
    presto-spi package and should be independent of the presto-parser.
    Hence removing ParsionOptions from the AnalyzerOptions.

---
+++ presto-analyzer/src/main/java/com/facebook/presto/sql/analyzer/utils/AnalyzerUtil.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.analyzer.utils;
+
+import com.facebook.presto.spi.PrestoWarning;
+import com.facebook.presto.sql.analyzer.AnalyzerOptions;
+import com.facebook.presto.sql.parser.ParsingOptions;
+
+import static com.facebook.presto.spi.StandardWarningCode.PARSER_WARNING;
+import static com.facebook.presto.sql.parser.ParsingOptions.DecimalLiteralTreatment.AS_DECIMAL;
+import static com.facebook.presto.sql.parser.ParsingOptions.DecimalLiteralTreatment.AS_DOUBLE;
+
+public class AnalyzerUtil
+{
+    private AnalyzerUtil() {}
+
+    public static ParsingOptions createParsingOptions(AnalyzerOptions analyzerOptions)
+    {
+        return ParsingOptions.builder()
+                .setDecimalLiteralTreatment(analyzerOptions.isParseDecimalLiteralsAsDouble() ? AS_DOUBLE : AS_DECIMAL)
+                .setWarningConsumer(warning -> analyzerOptions.getWarningCollector().add(new PrestoWarning(PARSER_WARNING, warning.getMessage())))
+                .build();
+    }
+}
