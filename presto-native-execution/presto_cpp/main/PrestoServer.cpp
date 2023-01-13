9e01c8146e9315e7bedbcc2e61aa18fe2428fbf7
Ge Gao <gaoge@fb.com>
2023-01-13 11:12:36 -0800

    Table writer 4: Refactor table writer API usage
    Deprecate the notion of write protocol, to adapt to the change
    in Velox. Clean up enum values that are not being used.

--- presto-native-execution/presto_cpp/main/PrestoServer.cpp
+++ presto-native-execution/presto_cpp/main/PrestoServer.cpp
@@ -31,7 +31,6 @@
 #include "presto_cpp/main/operators/ShuffleInterface.h"
 #include "presto_cpp/main/operators/UnsafeRowExchangeSource.h"
 #include "presto_cpp/presto_protocol/Connectors.h"
-#include "presto_cpp/presto_protocol/WriteProtocol.h"
 #include "presto_cpp/presto_protocol/presto_protocol.h"
 #include "velox/common/base/StatsReporter.h"
 #include "velox/common/caching/SsdCache.h"
@@ -152,8 +151,6 @@ void PrestoServer::run() {
   registerShuffleInterfaceFactories();
   protocol::registerHiveConnectors();
   protocol::registerTpchConnector();
-  protocol::HiveNoCommitWriteProtocol::registerProtocol();
-  protocol::HiveTaskCommitWriteProtocol::registerProtocol();
 
   auto executor = std::make_shared<folly::IOThreadPoolExecutor>(
       systemConfig->numIoThreads(),
