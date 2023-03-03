37f62f393fec6c017f94e6f0a5ef7fab3c1d0748
Masha Basmanova <mbasmanova@meta.com>
2023-03-03 12:23:17 -0800

    Remove deprecated ConstantTypedExpr constructor (#4189)
    Summary: Pull Request resolved: https://github.com/facebookincubator/velox/pull/4189
    
    Reviewed By: xiaoxmeng
    
    Differential Revision: D43773902
    
    fbshipit-source-id: bb05149e0540c6d23581f3adcc5d966c3233e544

--- velox/core/Expressions.h
+++ velox/core/Expressions.h
@@ -70,13 +70,6 @@ class InputTypedExpr : public ITypedExpr {
 
 class ConstantTypedExpr : public ITypedExpr {
  public:
-// TODO Remove this constructor after Prestissimo is updated.
-#ifdef VELOX_ENABLE_BACKWARD_COMPATIBILITY
-  // Creates constant expression of scalar type.
-  explicit ConstantTypedExpr(variant value)
-      : ITypedExpr{value.inferType()}, value_{std::move(value)} {}
-#endif
-
   // Creates constant expression. For complex types, only
   // variant::null() value is supported.
   ConstantTypedExpr(std::shared_ptr<const Type> type, variant value)
