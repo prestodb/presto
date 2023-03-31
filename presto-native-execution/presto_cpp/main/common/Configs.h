11b10232a3f8928d96dc495653124e46444ccd51
Ankur Pathela <arpa@fb.com>
2023-03-30 22:14:19 -0700

    Add test for native execution of functions registered via a json file
    The support for this feature was added via
    `JsonFileBasedFunctionNamespaceManager` in #18630.
    
    The introduced test fires a query `SELECT json.test_schema.eq(...) ...`
    and asserts successful registration of the external function in presto
    function register as well as successful registration+execution of the
    function in presto native(velox). On native side, the function is
    conditionally registered via the introduced config flag
    `register-test-functions` that's only true for testing.

--- presto-native-execution/presto_cpp/main/common/Configs.h
+++ presto-native-execution/presto_cpp/main/common/Configs.h
@@ -116,6 +116,8 @@ class SystemConfig : public ConfigBase {
       "http-server.enable-access-log"};
   static constexpr std::string_view kHttpEnableStatFilter{
       "http-server.enable-stats-filter"};
+  static constexpr std::string_view kRegisterTestFunctions{
+      "register-test-functions"};
   /// The options to configure the max quantized memory allocation size to store
   /// the received http response data.
   static constexpr std::string_view kHttpMaxAllocateBytes{
@@ -144,6 +146,7 @@ class SystemConfig : public ConfigBase {
   static constexpr bool kUseMmapAllocatorDefault{true};
   static constexpr bool kHttpEnableAccessLogDefault = false;
   static constexpr bool kHttpEnableStatsFilterDefault = false;
+  static constexpr bool kRegisterTestFunctionsDefault = false;
   static constexpr uint64_t kHttpMaxAllocateBytesDefault = 64 << 10;
 
   static SystemConfig* instance();
@@ -200,6 +203,8 @@ class SystemConfig : public ConfigBase {
   bool enableHttpAccessLog() const;
 
   bool enableHttpStatsFilter() const;
+
+  bool registerTestFunctions() const;
 
   uint64_t httpMaxAllocateBytes() const;
 };
