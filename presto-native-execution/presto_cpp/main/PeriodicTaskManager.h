b0e5a9bf961c74bada6326ce157481d6244a7458
Jialiang Tan <jacob.jialiang.tan@gmail.com>
2023-04-06 00:45:38 -0700

    [native] Refactor PeriodicTaskManager and expose additional API * Breaks down PeriodicTaskManager::start() to be smaller methods for better   readability. * Added addAdditionalPeriodicTasks() API exposure to external extenders of   PrestoServer for additional task scheduling. * Advanced Velox version

--- presto-native-execution/presto_cpp/main/PeriodicTaskManager.h
+++ presto-native-execution/presto_cpp/main/PeriodicTaskManager.h
@@ -25,44 +25,70 @@ namespace facebook::velox::connector {
 class Connector;
 }
 
+namespace facebook::velox::cache {
+class AsyncDataCache;
+}
+
 namespace facebook::presto {
 
 class TaskManager;
 
-// Manages a set of periodic tasks via folly::FunctionScheduler.
-// This is a place to add a new task or add more functionality to an existing
-// one.
+/// Manages a set of periodic tasks via folly::FunctionScheduler.
+/// This is a place to add a new task or add more functionality to an existing
+/// one.
 class PeriodicTaskManager {
  public:
   explicit PeriodicTaskManager(
-      folly::CPUThreadPoolExecutor* driverCPUExecutor,
-      folly::IOThreadPoolExecutor* httpExecutor,
-      TaskManager* taskManager,
-      std::vector<std::shared_ptr<velox::connector::Connector>> connectors);
+      folly::CPUThreadPoolExecutor* const driverCPUExecutor,
+      folly::IOThreadPoolExecutor* const httpExecutor,
+      TaskManager* const taskManager,
+      const velox::memory::MemoryAllocator* const memoryAllocator,
+      const velox::cache::AsyncDataCache* const asyncDataCache,
+      const std::unordered_map<
+          std::string,
+          std::shared_ptr<velox::connector::Connector>>& connectors);
+
   ~PeriodicTaskManager() {
     stop();
   }
 
-  // All the tasks will start here.
+  /// Invoked to start all registered, and fundamental periodic tasks running at
+  /// the background.
+  ///
+  /// NOTE: start() shall be called after everything in PrestoServer is
+  /// initialized because PeriodicTaskManager replies on proper initializations
+  /// of various entities in the system to work as expected.
   void start();
 
-  // Add a task to run periodically.
+  /// Add a task to run periodically.
   template <typename TFunc>
   void addTask(TFunc&& func, size_t periodMicros, const std::string& taskName) {
     scheduler_.addFunction(
         func, std::chrono::microseconds{periodMicros}, taskName);
   }
 
-  // Stops all periodic tasks. Returns only when everything is stopped.
+  /// Stops all periodic tasks. Returns only when everything is stopped.
   void stop();
 
  private:
+  void addExecutorStatsTask();
+  void addTaskStatsTask();
+  void addTaskCleanupTask();
+  void addMemoryAllocatorStatsTask();
+  void addPrestoExchangeSourceMemoryStatsTask();
+  void addAsyncDataCacheStatsTask();
+  void addConnectorStatsTask();
+  void addOperatingSystemStatsTask();
+
   folly::FunctionScheduler scheduler_;
-  folly::CPUThreadPoolExecutor* driverCPUExecutor_;
-  folly::IOThreadPoolExecutor* httpExecutor_;
-  TaskManager* taskManager_;
-  velox::memory::MemoryManager& memoryManager_;
-  std::vector<std::shared_ptr<velox::connector::Connector>> connectors_;
+  folly::CPUThreadPoolExecutor* const driverCPUExecutor_;
+  folly::IOThreadPoolExecutor* const httpExecutor_;
+  TaskManager* const taskManager_;
+  const velox::memory::MemoryAllocator* const memoryAllocator_;
+  const velox::cache::AsyncDataCache* const asyncDataCache_;
+  const std::unordered_map<
+      std::string,
+      std::shared_ptr<velox::connector::Connector>>& connectors_;
 };
 
 } // namespace facebook::presto
