eea73e2bb6c622a30007b7cbe987f3a9fe07f7ca
Sergey Pershin <spershin@hotmail.com>
2023-04-11 16:57:07 -0700

    [native] Adding "running*WallNanos" operator runtime metrics.

--- presto-native-execution/presto_cpp/main/PrestoTask.cpp
+++ presto-native-execution/presto_cpp/main/PrestoTask.cpp
@@ -93,7 +93,7 @@ void setTiming(
 static protocol::RuntimeMetric createProtocolRuntimeMetric(
     const std::string& name,
     int64_t value,
-    protocol::RuntimeUnit unit) {
+    protocol::RuntimeUnit unit = protocol::RuntimeUnit::NONE) {
   return protocol::RuntimeMetric{name, unit, value, 1, value, value};
 }
 
@@ -127,6 +127,71 @@ static void addRuntimeMetricIfNotZero(
         createVeloxRuntimeMetric(value, RuntimeCounter::Unit::kNone);
     addRuntimeMetric(runtimeMetrics, name, veloxMetric);
   }
+}
+
+// Add 'running time' metrics from Velox operator stats to Presto operator
+// stats.
+static void addRunningTimeOperatorMetrics(
+    protocol::OperatorStats& opOut,
+    protocol::TaskStats& prestoTaskStats,
+    const exec::OperatorStats& op) {
+  auto statName = fmt::format(
+      "{}.{}.runningAddInputWallNanos", op.operatorType, op.planNodeId);
+  auto prestoMetric =
+      createProtocolRuntimeMetric(statName, op.addInputTiming.wallNanos);
+  opOut.runtimeStats.emplace(statName, prestoMetric);
+  prestoTaskStats.runtimeStats[statName] = prestoMetric;
+
+  statName = fmt::format(
+      "{}.{}.runningGetOutputWallNanos", op.operatorType, op.planNodeId);
+  prestoMetric =
+      createProtocolRuntimeMetric(statName, op.getOutputTiming.wallNanos);
+  opOut.runtimeStats.emplace(statName, prestoMetric);
+  prestoTaskStats.runtimeStats[statName] = prestoMetric;
+
+  statName = fmt::format(
+      "{}.{}.runningFinishWallNanos", op.operatorType, op.planNodeId);
+  prestoMetric =
+      createProtocolRuntimeMetric(statName, op.finishTiming.wallNanos);
+  opOut.runtimeStats.emplace(statName, prestoMetric);
+  prestoTaskStats.runtimeStats[statName] = prestoMetric;
+
+  statName = fmt::format(
+      "{}.{}.runningTotalTimeWallNanos", op.operatorType, op.planNodeId);
+  const auto totalRunningWallNanos = op.addInputTiming.wallNanos +
+      op.getOutputTiming.wallNanos + op.finishTiming.wallNanos;
+  prestoMetric = createProtocolRuntimeMetric(statName, totalRunningWallNanos);
+  opOut.runtimeStats.emplace(statName, prestoMetric);
+  prestoTaskStats.runtimeStats[statName] = prestoMetric;
+}
+
+// Add 'spilling' metrics from Velox operator stats to Presto operator stats.
+static void addSpillingOperatorMetrics(
+    protocol::OperatorStats& opOut,
+    protocol::TaskStats& prestoTaskStats,
+    const exec::OperatorStats& op) {
+  std::string statName =
+      fmt::format("{}.{}.spilledBytes", op.operatorType, op.planNodeId);
+  auto prestoMetric = createProtocolRuntimeMetric(
+      statName, op.spilledBytes, protocol::RuntimeUnit::BYTE);
+  opOut.runtimeStats.emplace(statName, prestoMetric);
+  prestoTaskStats.runtimeStats[statName] = prestoMetric;
+
+  statName = fmt::format("{}.{}.spilledRows", op.operatorType, op.planNodeId);
+  prestoMetric = createProtocolRuntimeMetric(statName, op.spilledRows);
+  opOut.runtimeStats.emplace(statName, prestoMetric);
+  prestoTaskStats.runtimeStats[statName] = prestoMetric;
+
+  statName =
+      fmt::format("{}.{}.spilledPartitions", op.operatorType, op.planNodeId);
+  prestoMetric = createProtocolRuntimeMetric(statName, op.spilledPartitions);
+  opOut.runtimeStats.emplace(statName, prestoMetric);
+  prestoTaskStats.runtimeStats[statName] = prestoMetric;
+
+  statName = fmt::format("{}.{}.spilledFiles", op.operatorType, op.planNodeId);
+  prestoMetric = createProtocolRuntimeMetric(statName, op.spilledFiles);
+  opOut.runtimeStats.emplace(statName, prestoMetric);
+  prestoTaskStats.runtimeStats[statName] = prestoMetric;
 }
 
 } // namespace
@@ -395,49 +460,17 @@ protocol::TaskInfo PrestoTask::updateInfoLocked() {
         }
       }
       if (op.numSplits != 0) {
-        const auto statName = fmt::format(
-            "{}.{}.{}", op.operatorType, op.planNodeId, "numSplits");
+        const auto statName =
+            fmt::format("{}.{}.numSplits", op.operatorType, op.planNodeId);
         opOut.runtimeStats.emplace(
-            statName,
-            protocol::RuntimeMetric{
-                statName,
-                protocol::RuntimeUnit::NONE,
-                op.numSplits,
-                1,
-                op.numSplits,
-                op.numSplits});
+            statName, createProtocolRuntimeMetric(statName, op.numSplits));
       }
 
-      // If Velox's operator has spilling stats, then add them to the protocol
+      addRunningTimeOperatorMetrics(opOut, prestoTaskStats, op);
+      // If Velox operator has spilling stats, then add them to the Presto
       // operator stats and the task stats as runtime stats.
       if (op.spilledBytes > 0) {
-        std::string statName =
-            fmt::format("{}.{}.spilledBytes", op.operatorType, op.planNodeId);
-        auto protocolMetric = createProtocolRuntimeMetric(
-            statName, op.spilledBytes, protocol::RuntimeUnit::BYTE);
-        opOut.runtimeStats.emplace(statName, protocolMetric);
-        prestoTaskStats.runtimeStats[statName] = protocolMetric;
-
-        statName =
-            fmt::format("{}.{}.spilledRows", op.operatorType, op.planNodeId);
-        protocolMetric = createProtocolRuntimeMetric(
-            statName, op.spilledRows, protocol::RuntimeUnit::NONE);
-        opOut.runtimeStats.emplace(statName, protocolMetric);
-        prestoTaskStats.runtimeStats[statName] = protocolMetric;
-
-        statName = fmt::format(
-            "{}.{}.spilledPartitions", op.operatorType, op.planNodeId);
-        protocolMetric = createProtocolRuntimeMetric(
-            statName, op.spilledPartitions, protocol::RuntimeUnit::NONE);
-        opOut.runtimeStats.emplace(statName, protocolMetric);
-        prestoTaskStats.runtimeStats[statName] = protocolMetric;
-
-        statName =
-            fmt::format("{}.{}.spilledFiles", op.operatorType, op.planNodeId);
-        protocolMetric = createProtocolRuntimeMetric(
-            statName, op.spilledFiles, protocol::RuntimeUnit::NONE);
-        opOut.runtimeStats.emplace(statName, protocolMetric);
-        prestoTaskStats.runtimeStats[statName] = protocolMetric;
+        addSpillingOperatorMetrics(opOut, prestoTaskStats, op);
       }
 
       auto wallNanos = op.addInputTiming.wallNanos +
