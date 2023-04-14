c3f8c9f20b86a1e575955315e37945cab55467e4
Pramod <pramod@ahana.io>
2023-04-14 10:05:39 -0700

    Add Expression to SubfieldFilter support for Short Decimals (#4487)
    Summary: Pull Request resolved: https://github.com/facebookincubator/velox/pull/4487
    
    Reviewed By: kgpai
    
    Differential Revision: D44962508
    
    Pulled By: Yuhta
    
    fbshipit-source-id: be822cc783af5a274f3613039baa3d0b99a5981b

---
+++ velox/dwio/parquet/tests/reader/ParquetTableScanTest.cpp
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/init/Init.h>
+
+#include "velox/common/base/tests/GTestUtils.h"
+#include "velox/dwio/common/tests/utils/DataFiles.h"
+#include "velox/dwio/parquet/RegisterParquetReader.h"
+#include "velox/dwio/parquet/reader/ParquetReader.h"
+#include "velox/exec/tests/utils/HiveConnectorTestBase.h"
+#include "velox/exec/tests/utils/PlanBuilder.h"
+#include "velox/type/tests/SubfieldFiltersBuilder.h"
+
+using namespace facebook::velox;
+using namespace facebook::velox::exec;
+using namespace facebook::velox::exec::test;
+using namespace facebook::velox::parquet;
+
+class ParquetTableScanTest : public HiveConnectorTestBase {
+ protected:
+  using OperatorTestBase::assertQuery;
+
+  void SetUp() override {
+    HiveConnectorTestBase::SetUp();
+    registerParquetReaderFactory(
+        facebook::velox::parquet::ParquetReaderType::NATIVE);
+  }
+
+  void TearDown() override {
+    unregisterParquetReaderFactory();
+    HiveConnectorTestBase::TearDown();
+  }
+
+  void assertSelectWithFilter(
+      std::vector<std::string>&& outputColumnNames,
+      const std::vector<std::string>& subfieldFilters,
+      const std::string& remainingFilter,
+      const std::string& sql) {
+    auto rowType = getRowType(std::move(outputColumnNames));
+    parse::ParseOptions options;
+    options.parseDecimalAsDouble = false;
+
+    auto plan = PlanBuilder()
+                    .setParseOptions(options)
+                    .tableScan(rowType, subfieldFilters, remainingFilter)
+                    .planNode();
+
+    assertQuery(plan, splits_, sql);
+  }
+
+  void
+  loadData(const std::string& filePath, RowTypePtr rowType, RowVectorPtr data) {
+    splits_ = {makeSplit(filePath)};
+    rowType_ = rowType;
+    createDuckDbTable({data});
+  }
+
+  std::string getExampleFilePath(const std::string& fileName) {
+    return facebook::velox::test::getDataFilePath(
+        "velox/dwio/parquet/tests/reader", "../examples/" + fileName);
+  }
+
+  std::shared_ptr<connector::hive::HiveConnectorSplit> makeSplit(
+      const std::string& filePath) {
+    return makeHiveConnectorSplits(
+        filePath, 1, dwio::common::FileFormat::PARQUET)[0];
+  }
+
+ private:
+  RowTypePtr getRowType(std::vector<std::string>&& outputColumnNames) const {
+    std::vector<TypePtr> types;
+    for (auto colName : outputColumnNames) {
+      types.push_back(rowType_->findChild(colName));
+    }
+
+    return ROW(std::move(outputColumnNames), std::move(types));
+  }
+
+  RowTypePtr rowType_;
+  std::vector<std::shared_ptr<connector::ConnectorSplit>> splits_;
+};
+
+TEST_F(ParquetTableScanTest, decimalSubfieldFilter) {
+  // decimal.parquet holds two columns (a: DECIMAL(5, 2), b: DECIMAL(20, 5)) and
+  // 20 rows (10 rows per group). Data is in plain uncompressed format:
+  //   a: [100.01 .. 100.20]
+  //   b: [100000000000000.00001 .. 100000000000000.00020]
+  std::vector<int64_t> unscaledShortValues(20);
+  std::iota(unscaledShortValues.begin(), unscaledShortValues.end(), 10001);
+  loadData(
+      getExampleFilePath("decimal.parquet"),
+      ROW({"a"}, {DECIMAL(5, 2)}),
+      makeRowVector(
+          {"a"},
+          {
+              makeShortDecimalFlatVector(unscaledShortValues, DECIMAL(5, 2)),
+          }));
+
+  assertSelectWithFilter(
+      {"a"}, {"a < 100.07"}, "", "SELECT a FROM tmp WHERE a < 100.07");
+  assertSelectWithFilter(
+      {"a"}, {"a <= 100.07"}, "", "SELECT a FROM tmp WHERE a <= 100.07");
+  assertSelectWithFilter(
+      {"a"}, {"a > 100.07"}, "", "SELECT a FROM tmp WHERE a > 100.07");
+  assertSelectWithFilter(
+      {"a"}, {"a >= 100.07"}, "", "SELECT a FROM tmp WHERE a >= 100.07");
+  assertSelectWithFilter(
+      {"a"}, {"a = 100.07"}, "", "SELECT a FROM tmp WHERE a = 100.07");
+  assertSelectWithFilter(
+      {"a"},
+      {"a BETWEEN 100.07 AND 100.12"},
+      "",
+      "SELECT a FROM tmp WHERE a BETWEEN 100.07 AND 100.12");
+
+  VELOX_ASSERT_THROW(
+      assertSelectWithFilter(
+          {"a"}, {"a < 1000.7"}, "", "SELECT a FROM tmp WHERE a < 1000.7"),
+      "Scalar function signature is not supported: lt(DECIMAL(5,2), DECIMAL(5,1))");
+  VELOX_ASSERT_THROW(
+      assertSelectWithFilter(
+          {"a"}, {"a = 1000.7"}, "", "SELECT a FROM tmp WHERE a = 1000.7"),
+      "Scalar function signature is not supported: eq(DECIMAL(5,2), DECIMAL(5,1))");
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  folly::init(&argc, &argv, false);
+  return RUN_ALL_TESTS();
+}
--- velox/expression/ExprToSubfieldFilter.cpp
+++ velox/expression/ExprToSubfieldFilter.cpp
@@ -162,6 +162,9 @@ std::unique_ptr<common::Filter> makeLessThanOrEqualFilter(
       return lessThanOrEqual(singleValue<StringView>(upper));
     case TypeKind::DATE:
       return lessThanOrEqual(singleValue<Date>(upper).days());
+    case TypeKind::SHORT_DECIMAL:
+      return lessThanOrEqual(
+          singleValue<UnscaledShortDecimal>(upper).unscaledValue());
     default:
       return nullptr;
   }
@@ -191,6 +194,8 @@ std::unique_ptr<common::Filter> makeLessThanFilter(
       return lessThan(singleValue<StringView>(upper));
     case TypeKind::DATE:
       return lessThan(singleValue<Date>(upper).days());
+    case TypeKind::SHORT_DECIMAL:
+      return lessThan(singleValue<UnscaledShortDecimal>(upper).unscaledValue());
     default:
       return nullptr;
   }
@@ -220,6 +225,9 @@ std::unique_ptr<common::Filter> makeGreaterThanOrEqualFilter(
       return greaterThanOrEqual(singleValue<StringView>(lower));
     case TypeKind::DATE:
       return greaterThanOrEqual(singleValue<Date>(lower).days());
+    case TypeKind::SHORT_DECIMAL:
+      return greaterThanOrEqual(
+          singleValue<UnscaledShortDecimal>(lower).unscaledValue());
     default:
       return nullptr;
   }
@@ -249,6 +257,9 @@ std::unique_ptr<common::Filter> makeGreaterThanFilter(
       return greaterThan(singleValue<StringView>(lower));
     case TypeKind::DATE:
       return greaterThan(singleValue<Date>(lower).days());
+    case TypeKind::SHORT_DECIMAL:
+      return greaterThan(
+          singleValue<UnscaledShortDecimal>(lower).unscaledValue());
     default:
       return nullptr;
   }
@@ -276,6 +287,8 @@ std::unique_ptr<common::Filter> makeEqualFilter(
       return equal(singleValue<StringView>(value));
     case TypeKind::DATE:
       return equal(singleValue<Date>(value).days());
+    case TypeKind::SHORT_DECIMAL:
+      return equal(singleValue<UnscaledShortDecimal>(value).unscaledValue());
     default:
       return nullptr;
   }
@@ -400,6 +413,10 @@ std::unique_ptr<common::Filter> makeBetweenFilter(
     case TypeKind::VARCHAR:
       return between(
           singleValue<StringView>(lower), singleValue<StringView>(upper));
+    case TypeKind::SHORT_DECIMAL:
+      return between(
+          singleValue<UnscaledShortDecimal>(lower).unscaledValue(),
+          singleValue<UnscaledShortDecimal>(upper).unscaledValue());
     default:
       return nullptr;
   }
