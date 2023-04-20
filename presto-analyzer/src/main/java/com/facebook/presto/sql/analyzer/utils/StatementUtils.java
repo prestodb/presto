prestodb/presto

commit 8ac456f6b0e51fa118e5c711b29536716dd54fb0

Provide ddl support
Updates tests

Add missing changes

Add helper class for binding

Refactored data definition binding code

Fixing test failures

Refactor tasks to remove dependency on QueryStateMachine

Adding tests

Add CreateFunction Test

Remove unused code in PrestoSparkQueryExecutionFactory

Fix to build failure. Added SessionTransactionControlTask for tasks needing QueryStateMachine for transaction and session management

Remove sysout statements from code

Add provider for Session Control tasks

Fix to Build failure

Fix to build failures

Fix to preto-main checkstyle failure

Address review comments

Address review comments

Remove unused line

Remove Unused import

presto-main/src/main/java/com/facebook/presto/execution/CreateFunctionTask.java
@@ -13,12 +13,14 @@
  */
 package com.facebook.presto.execution;
 
+import com.facebook.presto.Session;
 import com.facebook.presto.common.QualifiedObjectName;
 import com.facebook.presto.common.type.Type;
 import com.facebook.presto.common.type.TypeSignature;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.security.AccessControl;
 import com.facebook.presto.spi.PrestoException;
+import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.function.Parameter;
 import com.facebook.presto.spi.function.RoutineCharacteristics;
 import com.facebook.presto.spi.function.SqlFunctionHandle;
@@ -35,16 +37,20 @@ import com.facebook.presto.sql.tree.ExpressionTreeRewriter;
 import com.facebook.presto.sql.tree.Return;
 import com.facebook.presto.sql.tree.RoutineBody;
 import com.facebook.presto.transaction.TransactionManager;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import javax.inject.Inject;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static com.facebook.presto.common.type.TypeSignature.parseTypeSignature;
 import static com.facebook.presto.metadata.FunctionAndTypeManager.qualifyObjectName;
 import static com.facebook.presto.metadata.SessionFunctionHandle.SESSION_NAMESPACE;
+import static com.facebook.presto.spi.StandardErrorCode.ALREADY_EXISTS;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.facebook.presto.spi.function.FunctionVersion.notVersioned;
 import static com.facebook.presto.sql.SqlFormatter.formatSql;
@@ -54,9 +60,10 @@ import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 public class CreateFunctionTask
-        implements DataDefinitionTask<CreateFunction>
+        implements DDLDefinitionTask<CreateFunction>
 {
     private final SqlParser sqlParser;
+    private final Map<SqlFunctionId, SqlInvokedFunction> addedSessionFunctions = new ConcurrentHashMap<>();
 
     @Inject
     public CreateFunctionTask(SqlParser sqlParser)
@@ -77,9 +84,9 @@ public class CreateFunctionTask
     }
 
     @Override
-    public ListenableFuture<?> execute(CreateFunction statement, TransactionManager transactionManager, Metadata metadata, AccessControl accessControl, QueryStateMachine stateMachine, List<Expression> parameters)
+    public ListenableFuture<?> execute(CreateFunction statement, TransactionManager transactionManager, Metadata metadata, AccessControl accessControl, Session session, List<Expression> parameters, WarningCollector warningCollector)
     {
-        Analyzer analyzer = new Analyzer(stateMachine.getSession(), metadata, sqlParser, accessControl, Optional.empty(), parameters, stateMachine.getWarningCollector());
+        Analyzer analyzer = new Analyzer(session, metadata, sqlParser, accessControl, Optional.empty(), parameters, warningCollector);
         Analysis analysis = analyzer.analyze(statement);
         if (analysis.getFunctionHandles().values().stream()
                 .anyMatch(SqlFunctionHandle.class::isInstance)) {
@@ -88,7 +95,7 @@ public class CreateFunctionTask
 
         SqlInvokedFunction function = createSqlInvokedFunction(statement, metadata, analysis);
         if (statement.isTemporary()) {
-            stateMachine.addSessionFunction(new SqlFunctionId(function.getSignature().getName(), function.getSignature().getArgumentTypes()), function);
+            addSessionFunction(session, new SqlFunctionId(function.getSignature().getName(), function.getSignature().getArgumentTypes()), function);
         }
         else {
             metadata.getFunctionAndTypeManager().createFunction(function, statement.isReplace());
@@ -154,5 +161,21 @@ public class CreateFunctionTask
                 routineCharacteristics,
                 formatSql(body, Optional.empty()),
                 notVersioned());
+    }
+
+    private void addSessionFunction(Session session, SqlFunctionId signature, SqlInvokedFunction function)
+    {
+        requireNonNull(signature, "signature is null");
+        requireNonNull(function, "function is null");
+
+        if (session.getSessionFunctions().containsKey(signature) || addedSessionFunctions.putIfAbsent(signature, function) != null) {
+            throw new PrestoException(ALREADY_EXISTS, format("Session function %s has already been defined", signature));
+        }
+    }
+
+    @VisibleForTesting
+    public Map<SqlFunctionId, SqlInvokedFunction> getAddedSessionFunctions()
+    {
+        return addedSessionFunctions;
     }
 }

presto-main/src/main/java/com/facebook/presto/execution/DropFunctionTask.java
@@ -13,25 +13,32 @@
  */
 package com.facebook.presto.execution;
 
+import com.facebook.presto.Session;
 import com.facebook.presto.common.QualifiedObjectName;
 import com.facebook.presto.common.type.TypeSignature;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.security.AccessControl;
+import com.facebook.presto.spi.PrestoException;
+import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.function.SqlFunctionId;
 import com.facebook.presto.sql.analyzer.Analyzer;
 import com.facebook.presto.sql.parser.SqlParser;
 import com.facebook.presto.sql.tree.DropFunction;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.transaction.TransactionManager;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import javax.inject.Inject;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static com.facebook.presto.metadata.FunctionAndTypeManager.qualifyObjectName;
 import static com.facebook.presto.metadata.SessionFunctionHandle.SESSION_NAMESPACE;
+import static com.facebook.presto.spi.StandardErrorCode.NOT_FOUND;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.util.concurrent.Futures.immediateFuture;
 import static java.lang.String.format;
@@ -39,9 +46,10 @@ import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 
 public class DropFunctionTask
-        implements DataDefinitionTask<DropFunction>
+        implements DDLDefinitionTask<DropFunction>
 {
     private final SqlParser sqlParser;
+    private final Set<SqlFunctionId> removedSessionFunctions = Sets.newConcurrentHashSet();
 
     @Inject
     public DropFunctionTask(SqlParser sqlParser)
@@ -62,14 +70,14 @@ public class DropFunctionTask
     }
 
     @Override
-    public ListenableFuture<?> execute(DropFunction statement, TransactionManager transactionManager, Metadata metadata, AccessControl accessControl, QueryStateMachine stateMachine, List<Expression> parameters)
+    public ListenableFuture<?> execute(DropFunction statement, TransactionManager transactionManager, Metadata metadata, AccessControl accessControl, Session session, List<Expression> parameters, WarningCollector warningCollector)
     {
-        Analyzer analyzer = new Analyzer(stateMachine.getSession(), metadata, sqlParser, accessControl, Optional.empty(), parameters, stateMachine.getWarningCollector());
+        Analyzer analyzer = new Analyzer(session, metadata, sqlParser, accessControl, Optional.empty(), parameters, warningCollector);
         analyzer.analyze(statement);
         Optional<List<TypeSignature>> parameterTypes = statement.getParameterTypes().map(types -> types.stream().map(TypeSignature::parseTypeSignature).collect(toImmutableList()));
 
         if (statement.isTemporary()) {
-            stateMachine.removeSessionFunction(
+            removeSessionFunction(session,
                     new SqlFunctionId(
                             QualifiedObjectName.valueOf(SESSION_NAMESPACE, statement.getFunctionName().getSuffix()),
                             parameterTypes.orElse(emptyList())),
@@ -83,5 +91,25 @@ public class DropFunctionTask
         }
 
         return immediateFuture(null);
+    }
+
+    private void removeSessionFunction(Session session, SqlFunctionId signature, boolean suppressNotFoundException)
+    {
+        requireNonNull(signature, "signature is null");
+
+        if (!session.getSessionFunctions().containsKey(signature)) {
+            if (!suppressNotFoundException) {
+                throw new PrestoException(NOT_FOUND, format("Session function %s not found", signature.getFunctionName()));
+            }
+        }
+        else {
+            removedSessionFunctions.add(signature);
+        }
+    }
+
+    @VisibleForTesting
+    public Set<SqlFunctionId> getRemovedSessionFunctions()
+    {
+        return removedSessionFunctions;
     }
 }

commit 0363c8a1f93518f2eb86d74b775479b587c4fafb

Fix CREATE/DROP TEMPORARY FUNCTION
This was broken by https://github.com/prestodb/presto/pull/16699 and has
not worked since release 0.268.  The CreateFunctionTask and
DropFunctionTask should be adding functions directly to the
QueryStateMachine rather than keeping a special map in the task. This
commit basically reverts the changes that #16699 introduced for create
and drop function.

presto-analyzer/src/main/java/com/facebook/presto/sql/analyzer/utils/StatementUtils.java
@@ -129,9 +129,9 @@ public final class StatementUtils
         builder.put(DropView.class, QueryType.DATA_DEFINITION);
         builder.put(CreateMaterializedView.class, QueryType.DATA_DEFINITION);
         builder.put(DropMaterializedView.class, QueryType.DATA_DEFINITION);
-        builder.put(CreateFunction.class, QueryType.DATA_DEFINITION);
+        builder.put(CreateFunction.class, QueryType.CONTROL);
         builder.put(AlterFunction.class, QueryType.DATA_DEFINITION);
-        builder.put(DropFunction.class, QueryType.DATA_DEFINITION);
+        builder.put(DropFunction.class, QueryType.CONTROL);
         builder.put(Use.class, QueryType.CONTROL);
         builder.put(SetSession.class, QueryType.CONTROL);
         builder.put(ResetSession.class, QueryType.CONTROL);

presto-main/src/main/java/com/facebook/presto/execution/CreateFunctionTask.java
@@ -19,7 +19,6 @@ import com.facebook.presto.common.type.Type;
 import com.facebook.presto.common.type.TypeSignature;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.spi.PrestoException;
-import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.function.Parameter;
 import com.facebook.presto.spi.function.RoutineCharacteristics;
 import com.facebook.presto.spi.function.SqlFunctionHandle;
@@ -38,7 +37,6 @@ import com.facebook.presto.sql.tree.NodeRef;
 import com.facebook.presto.sql.tree.Return;
 import com.facebook.presto.sql.tree.RoutineBody;
 import com.facebook.presto.transaction.TransactionManager;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import javax.inject.Inject;
@@ -46,12 +44,10 @@ import javax.inject.Inject;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static com.facebook.presto.common.type.TypeSignature.parseTypeSignature;
 import static com.facebook.presto.metadata.FunctionAndTypeManager.qualifyObjectName;
 import static com.facebook.presto.metadata.SessionFunctionHandle.SESSION_NAMESPACE;
-import static com.facebook.presto.spi.StandardErrorCode.ALREADY_EXISTS;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.facebook.presto.spi.function.FunctionVersion.notVersioned;
 import static com.facebook.presto.sql.SqlFormatter.formatSql;
@@ -62,10 +58,9 @@ import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 public class CreateFunctionTask
-        implements DDLDefinitionTask<CreateFunction>
+        implements SessionTransactionControlTask<CreateFunction>
 {
     private final SqlParser sqlParser;
-    private final Map<SqlFunctionId, SqlInvokedFunction> addedSessionFunctions = new ConcurrentHashMap<>();
 
     @Inject
     public CreateFunctionTask(SqlParser sqlParser)
@@ -86,10 +81,11 @@ public class CreateFunctionTask
     }
 
     @Override
-    public ListenableFuture<?> execute(CreateFunction statement, TransactionManager transactionManager, Metadata metadata, AccessControl accessControl, Session session, List<Expression> parameters, WarningCollector warningCollector)
+    public ListenableFuture<?> execute(CreateFunction statement, TransactionManager transactionManager, Metadata metadata, AccessControl accessControl, QueryStateMachine stateMachine, List<Expression> parameters)
     {
         Map<NodeRef<com.facebook.presto.sql.tree.Parameter>, Expression> parameterLookup = parameterExtractor(statement, parameters);
-        Analyzer analyzer = new Analyzer(session, metadata, sqlParser, accessControl, Optional.empty(), parameters, parameterLookup, warningCollector);
+        Session session = stateMachine.getSession();
+        Analyzer analyzer = new Analyzer(session, metadata, sqlParser, accessControl, Optional.empty(), parameters, parameterLookup, stateMachine.getWarningCollector());
         Analysis analysis = analyzer.analyze(statement);
         if (analysis.getFunctionHandles().values().stream()
                 .anyMatch(SqlFunctionHandle.class::isInstance)) {
@@ -98,7 +94,7 @@ public class CreateFunctionTask
 
         SqlInvokedFunction function = createSqlInvokedFunction(statement, metadata, analysis);
         if (statement.isTemporary()) {
-            addSessionFunction(session, new SqlFunctionId(function.getSignature().getName(), function.getSignature().getArgumentTypes()), function);
+            stateMachine.addSessionFunction(new SqlFunctionId(function.getSignature().getName(), function.getSignature().getArgumentTypes()), function);
         }
         else {
             metadata.getFunctionAndTypeManager().createFunction(function, statement.isReplace());
@@ -164,21 +160,5 @@ public class CreateFunctionTask
                 routineCharacteristics,
                 formatSql(body, Optional.empty()),
                 notVersioned());
-    }
-
-    private void addSessionFunction(Session session, SqlFunctionId signature, SqlInvokedFunction function)
-    {
-        requireNonNull(signature, "signature is null");
-        requireNonNull(function, "function is null");
-
-        if (session.getSessionFunctions().containsKey(signature) || addedSessionFunctions.putIfAbsent(signature, function) != null) {
-            throw new PrestoException(ALREADY_EXISTS, format("Session function %s has already been defined", signature));
-        }
-    }
-
-    @VisibleForTesting
-    public Map<SqlFunctionId, SqlInvokedFunction> getAddedSessionFunctions()
-    {
-        return addedSessionFunctions;
     }
 }

presto-main/src/main/java/com/facebook/presto/execution/DropFunctionTask.java
@@ -13,12 +13,9 @@
  */
 package com.facebook.presto.execution;
 
-import com.facebook.presto.Session;
 import com.facebook.presto.common.QualifiedObjectName;
 import com.facebook.presto.common.type.TypeSignature;
 import com.facebook.presto.metadata.Metadata;
-import com.facebook.presto.spi.PrestoException;
-import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.function.SqlFunctionId;
 import com.facebook.presto.spi.security.AccessControl;
 import com.facebook.presto.sql.analyzer.Analyzer;
@@ -28,8 +25,6 @@ import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.NodeRef;
 import com.facebook.presto.sql.tree.Parameter;
 import com.facebook.presto.transaction.TransactionManager;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import javax.inject.Inject;
@@ -37,11 +32,9 @@ import javax.inject.Inject;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 import static com.facebook.presto.metadata.FunctionAndTypeManager.qualifyObjectName;
 import static com.facebook.presto.metadata.SessionFunctionHandle.SESSION_NAMESPACE;
-import static com.facebook.presto.spi.StandardErrorCode.NOT_FOUND;
 import static com.facebook.presto.sql.analyzer.utils.ParameterUtils.parameterExtractor;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.util.concurrent.Futures.immediateFuture;
@@ -50,10 +43,9 @@ import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 
 public class DropFunctionTask
-        implements DDLDefinitionTask<DropFunction>
+        implements SessionTransactionControlTask<DropFunction>
 {
     private final SqlParser sqlParser;
-    private final Set<SqlFunctionId> removedSessionFunctions = Sets.newConcurrentHashSet();
 
     @Inject
     public DropFunctionTask(SqlParser sqlParser)
@@ -74,15 +66,15 @@ public class DropFunctionTask
     }
 
     @Override
-    public ListenableFuture<?> execute(DropFunction statement, TransactionManager transactionManager, Metadata metadata, AccessControl accessControl, Session session, List<Expression> parameters, WarningCollector warningCollector)
+    public ListenableFuture<?> execute(DropFunction statement, TransactionManager transactionManager, Metadata metadata, AccessControl accessControl, QueryStateMachine stateMachine, List<Expression> parameters)
     {
         Map<NodeRef<Parameter>, Expression> parameterLookup = parameterExtractor(statement, parameters);
-        Analyzer analyzer = new Analyzer(session, metadata, sqlParser, accessControl, Optional.empty(), parameters, parameterLookup, warningCollector);
+        Analyzer analyzer = new Analyzer(stateMachine.getSession(), metadata, sqlParser, accessControl, Optional.empty(), parameters, parameterLookup, stateMachine.getWarningCollector());
         analyzer.analyze(statement);
         Optional<List<TypeSignature>> parameterTypes = statement.getParameterTypes().map(types -> types.stream().map(TypeSignature::parseTypeSignature).collect(toImmutableList()));
 
         if (statement.isTemporary()) {
-            removeSessionFunction(session,
+            stateMachine.removeSessionFunction(
                     new SqlFunctionId(
                             QualifiedObjectName.valueOf(SESSION_NAMESPACE, statement.getFunctionName().getSuffix()),
                             parameterTypes.orElse(emptyList())),
@@ -96,25 +88,5 @@ public class DropFunctionTask
         }
 
         return immediateFuture(null);
-    }
-
-    private void removeSessionFunction(Session session, SqlFunctionId signature, boolean suppressNotFoundException)
-    {
-        requireNonNull(signature, "signature is null");
-
-        if (!session.getSessionFunctions().containsKey(signature)) {
-            if (!suppressNotFoundException) {
-                throw new PrestoException(NOT_FOUND, format("Session function %s not found", signature.getFunctionName()));
-            }
-        }
-        else {
-            removedSessionFunctions.add(signature);
-        }
-    }
-
-    @VisibleForTesting
-    public Set<SqlFunctionId> getRemovedSessionFunctions()
-    {
-        return removedSessionFunctions;
     }
 }

presto-main/src/main/java/com/facebook/presto/util/PrestoDataDefBindingHelper.java
@@ -121,9 +121,7 @@ public class PrestoDataDefBindingHelper
         dataDefBuilder.put(DropView.class, DropViewTask.class);
         dataDefBuilder.put(CreateMaterializedView.class, CreateMaterializedViewTask.class);
         dataDefBuilder.put(DropMaterializedView.class, DropMaterializedViewTask.class);
-        dataDefBuilder.put(CreateFunction.class, CreateFunctionTask.class);
         dataDefBuilder.put(AlterFunction.class, AlterFunctionTask.class);
-        dataDefBuilder.put(DropFunction.class, DropFunctionTask.class);
         dataDefBuilder.put(Call.class, CallTask.class);
         dataDefBuilder.put(CreateRole.class, CreateRoleTask.class);
         dataDefBuilder.put(DropRole.class, DropRoleTask.class);
@@ -141,6 +139,8 @@ public class PrestoDataDefBindingHelper
         transactionDefBuilder.put(SetRole.class, SetRoleTask.class);
         transactionDefBuilder.put(Prepare.class, PrepareTask.class);
         transactionDefBuilder.put(Deallocate.class, DeallocateTask.class);
+        transactionDefBuilder.put(CreateFunction.class, CreateFunctionTask.class);
+        transactionDefBuilder.put(DropFunction.class, DropFunctionTask.class);
 
         STATEMENT_TASK_TYPES = dataDefBuilder.build();
         TRANSACTION_CONTROL_TYPES = transactionDefBuilder.build();

presto-main/src/test/java/com/facebook/presto/execution/TestCreateFunctionTask.java
@@ -15,7 +15,6 @@ package com.facebook.presto.execution;
 
 import com.facebook.presto.metadata.MetadataManager;
 import com.facebook.presto.spi.PrestoException;
-import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.security.AllowAllAccessControl;
 import com.facebook.presto.sql.parser.ParsingOptions;
 import com.facebook.presto.sql.parser.SqlParser;
@@ -54,10 +53,8 @@ public class TestCreateFunctionTask
         CreateFunction statement = (CreateFunction) parser.createStatement(sqlString, ParsingOptions.builder().build());
         TransactionManager transactionManager = createTestTransactionManager();
         QueryStateMachine stateMachine = createQueryStateMachine(sqlString, TEST_SESSION, false, transactionManager, executorService, metadataManager);
-        WarningCollector warningCollector = stateMachine.getWarningCollector();
-        CreateFunctionTask createFunctionTask = new CreateFunctionTask(parser);
-        createFunctionTask.execute(statement, transactionManager, metadataManager, new AllowAllAccessControl(), TEST_SESSION, emptyList(), warningCollector);
-        assertEquals(createFunctionTask.getAddedSessionFunctions().size(), 1);
+        new CreateFunctionTask(parser).execute(statement, transactionManager, metadataManager, new AllowAllAccessControl(), stateMachine, emptyList());
+        assertEquals(stateMachine.getAddedSessionFunctions().size(), 1);
     }
 
     @Test(expectedExceptions = PrestoException.class, expectedExceptionsMessageRegExp = "Session function .* has already been defined")
@@ -68,9 +65,7 @@ public class TestCreateFunctionTask
         CreateFunction statement = (CreateFunction) parser.createStatement(sqlString, ParsingOptions.builder().build());
         TransactionManager transactionManager = createTestTransactionManager();
         QueryStateMachine stateMachine = createQueryStateMachine(sqlString, TEST_SESSION, false, transactionManager, executorService, metadataManager);
-        WarningCollector warningCollector = stateMachine.getWarningCollector();
-        CreateFunctionTask createFunctionTask = new CreateFunctionTask(parser);
-        createFunctionTask.execute(statement, transactionManager, metadataManager, new AllowAllAccessControl(), TEST_SESSION, emptyList(), warningCollector);
-        createFunctionTask.execute(statement, transactionManager, metadataManager, new AllowAllAccessControl(), TEST_SESSION, emptyList(), warningCollector);
+        new CreateFunctionTask(parser).execute(statement, transactionManager, metadataManager, new AllowAllAccessControl(), stateMachine, emptyList());
+        new CreateFunctionTask(parser).execute(statement, transactionManager, metadataManager, new AllowAllAccessControl(), stateMachine, emptyList());
     }
 }

presto-main/src/test/java/com/facebook/presto/execution/TestDropFunctionTask.java
@@ -17,7 +17,6 @@ import com.facebook.presto.Session;
 import com.facebook.presto.common.QualifiedObjectName;
 import com.facebook.presto.metadata.MetadataManager;
 import com.facebook.presto.spi.PrestoException;
-import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.function.SqlFunctionId;
 import com.facebook.presto.spi.security.AllowAllAccessControl;
 import com.facebook.presto.sql.parser.ParsingOptions;
@@ -86,9 +85,7 @@ public class TestDropFunctionTask
         DropFunction statement = (DropFunction) parser.createStatement(sqlString, ParsingOptions.builder().build());
         TransactionManager tm = createTestTransactionManager();
         QueryStateMachine stateMachine = createQueryStateMachine(sqlString, session, false, tm, executorService, metadataManager);
-        WarningCollector warningCollector = stateMachine.getWarningCollector();
-        DropFunctionTask dropFunctionTask = new DropFunctionTask(parser);
-        dropFunctionTask.execute(statement, tm, metadataManager, new AllowAllAccessControl(), session, emptyList(), warningCollector);
-        return dropFunctionTask.getRemovedSessionFunctions();
+        new DropFunctionTask(parser).execute(statement, tm, metadataManager, new AllowAllAccessControl(), stateMachine, emptyList());
+        return stateMachine.getRemovedSessionFunctions();
     }
 }

presto-spark-base/src/test/java/com/facebook/presto/spark/TestPrestoSparkQueryRunner.java
@@ -1303,16 +1303,6 @@ public class TestPrestoSparkQueryRunner
     }
 
     @Test
-    public void testCreateFunction()
-    {
-        assertQuerySucceeds("CREATE FUNCTION unittest.memory.tan (x int) RETURNS double COMMENT 'tangent trigonometric function' LANGUAGE SQL DETERMINISTIC CALLED ON NULL INPUT RETURN sin(x) / cos(x)");
-        MaterializedResult actual = computeActual("select unittest.memory.tan(5)");
-        assertEquals("-3.380515006246586", actual.getOnlyValue().toString());
-        //PoS currently supports one query per session. So temporary function created is not discoverable by another query.
-        assertQuerySucceeds("CREATE TEMPORARY FUNCTION foo() RETURNS int RETURN 1");
-    }
-
-    @Test
     public void testCreateType()
     {
         assertQuerySucceeds("CREATE TYPE unittest.memory.num AS integer");