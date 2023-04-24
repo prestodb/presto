4f99c7bf7b6da1936109923f10efcd03c7f4dedd
Jialiang Tan <jacob.jialiang.tan@gmail.com>
2023-04-24 16:46:32 -0700

    [spark] Enables Presto native integration tests for Spark
    * Disables the ones that are not currently supported by Presto-on-Spark
    * Reduces log levels during test runs to prevent log spamming

---
+++ presto-native-execution/src/test/java/com/facebook/presto/spark/TestPrestoSparkNativeAggregations.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.spark;
+
+import com.facebook.presto.nativeworker.AbstractTestNativeAggregations;
+import com.facebook.presto.testing.ExpectedQueryRunner;
+import com.facebook.presto.testing.QueryRunner;
+import org.testng.annotations.Ignore;
+
+public class TestPrestoSparkNativeAggregations
+        extends AbstractTestNativeAggregations
+{
+    @Override
+    protected QueryRunner createQueryRunner()
+    {
+        return PrestoSparkNativeQueryRunnerUtils.createPrestoSparkNativeQueryRunner();
+    }
+
+    @Override
+    protected ExpectedQueryRunner createExpectedQueryRunner()
+            throws Exception
+    {
+        return PrestoSparkNativeQueryRunnerUtils.createJavaQueryRunner();
+    }
+
+    @Override
+    protected void assertQuery(String sql)
+    {
+        super.assertQuery(sql);
+        PrestoSparkNativeQueryRunnerUtils.assertShuffleMetadata();
+    }
+
+    // TODO: Enable following Ignored tests after fixing (Tests can be enabled by removing the method)
+    @Override
+    @Ignore
+    public void testChecksum() {}
+
+    @Override
+    @Ignore
+    public void testMinMax() {}
+}
