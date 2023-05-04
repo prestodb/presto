17e2418667bb42f1b347361a1ff523d552d4b4fb
Ali Parsaei <aparsaei@meta.com>
2023-05-04 11:50:39 -0700

    Support loading all json files in a dir to json-function-namespace-manager
    Before this change, You can only provide a path to a single JSON file to feed into JsonFileBasedFunctionNamespaceManager; this change allows for loading multiple json files in a directory to JsonFileBasedFunctionNamespaceManager. This helps with modularity of function metadata.

--- presto-function-namespace-managers/src/main/java/com/facebook/presto/functionNamespace/json/JsonFileBasedFunctionDefinitionProvider.java
+++ presto-function-namespace-managers/src/main/java/com/facebook/presto/functionNamespace/json/JsonFileBasedFunctionDefinitionProvider.java
@@ -15,24 +15,54 @@ package com.facebook.presto.functionNamespace.json;
 
 import com.facebook.airlift.log.Logger;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.facebook.presto.plugin.base.JsonUtils.parseJson;
+import static java.util.Locale.ENGLISH;
 
 public class JsonFileBasedFunctionDefinitionProvider
         implements FunctionDefinitionProvider
 {
     private static final Logger log = Logger.get(JsonFileBasedFunctionDefinitionProvider.class);
+    private static final int MAX_DIRECTORY_DEPTH = 4;
+    private static final String JSON_FILE_EXTENSION = ".json";
 
     @Override
     public UdfFunctionSignatureMap getUdfDefinition(String filePath)
     {
+        Map<String, List<JsonBasedUdfFunctionMetadata>> udfSignatureMap = new HashMap<>();
+
+        // If any of the files is not a valid source, the function will return null;
         try {
-            return parseJson(Paths.get(filePath), UdfFunctionSignatureMap.class);
+            for (Path path : getFilesInPath(filePath, MAX_DIRECTORY_DEPTH)) {
+                // Note: If a function signature is duplicated, the last definition overrides prior definitions.
+                udfSignatureMap.putAll(parseJson(path, UdfFunctionSignatureMap.class).getUDFSignatureMap());
+            }
+            return new UdfFunctionSignatureMap(udfSignatureMap);
         }
         catch (Exception e) {
             log.info("Failed to load function definition for JsonFileBasedFunctionNamespaceManager " + e.getMessage());
         }
         return null;
+    }
+
+    // Returns the list containing the file if filePath is a single file or all json files inside filePath if it's a dir.
+    // Set maxDepth to 0 for file, 1 for match in a root-level dir, ... & N for match in n-level nested dirs.
+    private List<Path> getFilesInPath(String filePath, int maxDirectoryDepth) throws IOException
+    {
+        try (Stream<Path> stream = Files.find(
+                Paths.get(filePath),
+                maxDirectoryDepth,
+                (p, basicFileAttributes) -> p.getFileName().toString().toLowerCase(ENGLISH).endsWith(JSON_FILE_EXTENSION))) {
+            return stream.collect(Collectors.toList());
+        }
     }
 }
