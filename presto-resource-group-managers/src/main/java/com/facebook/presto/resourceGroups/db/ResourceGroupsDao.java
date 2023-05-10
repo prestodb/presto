ad1f7cf747a06abe46b20d3cb0bff0c501fb6b27
Swapnil Tailor <swapniltailor@fb.com>
2023-05-10 10:07:21 -0700

    Adding support for schema in resource group selector
    Today our Selector has no support to chose resource group based on schema.
    With this change we allow resource group selector to support schema in selectors.
    Also we allowing resource group id name to have schema variable support similar to how
    we support user.

--- presto-resource-group-managers/src/main/java/com/facebook/presto/resourceGroups/db/ResourceGroupsDao.java
+++ presto-resource-group-managers/src/main/java/com/facebook/presto/resourceGroups/db/ResourceGroupsDao.java
@@ -64,7 +64,7 @@ public interface ResourceGroupsDao
     @UseRowMapper(ResourceGroupSpecBuilder.Mapper.class)
     List<ResourceGroupSpecBuilder> getResourceGroups(@Bind("environment") String environment);
 
-    @SqlQuery("SELECT S.resource_group_id, S.priority, S.user_regex, S.source_regex, S.query_type, S.client_tags, S.selector_resource_estimate, S.client_info_regex\n" +
+    @SqlQuery("SELECT S.resource_group_id, S.priority, S.user_regex, S.source_regex, S.query_type, S.client_tags, S.selector_resource_estimate, S.client_info_regex, S.schema\n" +
             "FROM selectors S\n" +
             "JOIN resource_groups R ON (S.resource_group_id = R.resource_group_id)\n" +
             "WHERE R.environment = :environment\n" +
@@ -81,6 +81,7 @@ public interface ResourceGroupsDao
             "  client_tags VARCHAR(512),\n" +
             "  selector_resource_estimate VARCHAR(1024),\n" +
             "  client_info_regex VARCHAR(1024),\n" +
+            "  schema VARCHAR(1024),\n" +
             "  id BIGINT NOT NULL AUTO_INCREMENT,\n" +
             "  PRIMARY KEY (id),\n" +
             "  FOREIGN KEY (resource_group_id) REFERENCES resource_groups (resource_group_id)\n" +
