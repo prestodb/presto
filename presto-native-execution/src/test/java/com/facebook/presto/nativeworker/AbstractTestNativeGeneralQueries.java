bce4a7fa44e3f21e31e00fbe8f0738a974d5b84f
Ivan Sadikov <ivan@ahana.io>
2023-05-25 08:25:11 +1200

    [native] Support decimal range filters in Prestissimo

--- presto-native-execution/src/test/java/com/facebook/presto/nativeworker/AbstractTestNativeGeneralQueries.java
+++ presto-native-execution/src/test/java/com/facebook/presto/nativeworker/AbstractTestNativeGeneralQueries.java
@@ -947,6 +947,68 @@ public abstract class AbstractTestNativeGeneralQueries
         assertQuery("SELECT cast(row(nationkey, regionkey) as row(a bigint, b bigint)) FROM nation");
     }
 
+    @Test
+    public void testDecimalRangeFilters()
+    {
+        // Actual session is for the native query runner.
+        // It is required to have "parquet_pushdown_filter_enabled" enabled, that is the only supported mode.
+        Session currentSession = Session.builder(getSession())
+                .setCatalogSessionProperty("hive", "parquet_pushdown_filter_enabled", "true")
+                .build();
+
+        // Expected session is for the Java query runner.
+        // The Java runner does not support Parquet filter pushdown yet, so we have to explicitly disable it.
+        Session expectedSession = Session.builder(getSession())
+                .setCatalogSessionProperty("hive", "parquet_pushdown_filter_enabled", "false")
+                .build();
+
+        // Generate temporary table name.
+        String tmpTableName = generateRandomTableName();
+
+        String shortDecimalMin = "DECIMAL '-999999999999999999'";
+        String shortDecimalMax = "DECIMAL '999999999999999999'";
+        // Cannot convert from DECIMAL(38,0) to DECIMAL(38,2) so we keep the max values as DECIMAL(38,2).
+        String longDecimalMin = "DECIMAL '-999999999999999999999999999999999999.99'";
+        String longDecimalMax = "DECIMAL '999999999999999999999999999999999999.99'";
+
+        try {
+            // Create a Parquet table with decimal types and test data.
+            getExpectedQueryRunner().execute(expectedSession, String.format("CREATE TABLE %s (c0 DECIMAL(15,2), c1 DECIMAL(38,2)) WITH (format = 'PARQUET')", tmpTableName), ImmutableList.of());
+            getExpectedQueryRunner().execute(expectedSession, String.format("INSERT INTO %s VALUES (DECIMAL '0', DECIMAL '0'), (DECIMAL '1.2', DECIMAL '3.4'), (DECIMAL '1000000.12', DECIMAL '28239823232323.57'), (DECIMAL '-542392.89', DECIMAL '-6723982392109.29')", tmpTableName), ImmutableList.of());
+
+            String[] queries = {
+                    String.format("SELECT * FROM %s WHERE c0 > DECIMAL '1.1' and c1 < DECIMAL '5.2'", tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c0 >= DECIMAL '1.2' and c1 <= DECIMAL '5.2'", tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c0 = DECIMAL '1.2' and c1 = DECIMAL '3.4'", tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c0 > DECIMAL '-542392.89' and c1 <= DECIMAL '28239823232323.57'", tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c0 >= DECIMAL '1.2'", tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c1 < DECIMAL '5.2'", tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c1 <= DECIMAL '3.4'", tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c0 = DECIMAL '1.2'", tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c1 = DECIMAL '3.4'", tmpTableName),
+
+                    // Test short decimal min/max values.
+                    String.format("SELECT * FROM %s WHERE c0 > " + shortDecimalMin, tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c0 < " + shortDecimalMax, tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c0 >= " + shortDecimalMin, tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c0 <= " + shortDecimalMax, tmpTableName),
+
+                    // Test long decimal min/max values.
+                    String.format("SELECT * FROM %s WHERE c1 > " + longDecimalMin, tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c1 < " + longDecimalMax, tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c1 >= " + longDecimalMin, tmpTableName),
+                    String.format("SELECT * FROM %s WHERE c1 <= " + longDecimalMax, tmpTableName)
+            };
+
+            for (String query : queries) {
+                assertQuery(currentSession, query, expectedSession, query);
+            }
+        }
+        finally {
+            dropTableIfExists(tmpTableName);
+        }
+    }
+
     private void assertQueryResultCount(String sql, int expectedResultCount)
     {
         assertEquals(getQueryRunner().execute(sql).getRowCount(), expectedResultCount);
