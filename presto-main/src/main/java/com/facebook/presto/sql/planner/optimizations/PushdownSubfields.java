26a0e61762638fa513dcdc5c9d56743fd5ad80e7
Lyublena Antova <lyublena@fb.com>
2023-06-01 09:44:04 -0700

    Track applicable optimizations that were disabled with a flag

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PushdownSubfields.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PushdownSubfields.java
@@ -89,10 +89,23 @@ public class PushdownSubfields
         implements PlanOptimizer
 {
     private final Metadata metadata;
+    private boolean isEnabledForTesting;
 
     public PushdownSubfields(Metadata metadata)
     {
         this.metadata = requireNonNull(metadata, "metadata is null");
+    }
+
+    @Override
+    public void setEnabledForTesting(boolean isSet)
+    {
+        isEnabledForTesting = isSet;
+    }
+
+    @Override
+    public boolean isEnabled(Session session)
+    {
+        return isEnabledForTesting || isPushdownSubfieldsEnabled(session);
     }
 
     @Override
@@ -102,7 +115,7 @@ public class PushdownSubfields
         requireNonNull(session, "session is null");
         requireNonNull(types, "types is null");
 
-        if (!isPushdownSubfieldsEnabled(session)) {
+        if (!isEnabled(session)) {
             return plan;
         }
 
