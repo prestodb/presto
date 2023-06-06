f8dbd87620a9751e4168dd0ecad80b4f5e69d39d
Miaojiang (MJ) Deng <mjdeng@meta.com>
2023-06-06 16:51:02 -0700

    [native pos] Fail over spark executor if native process launch failed
     If the native process launch failed, it usually indicates the current host
     machine is overloaded, we need to throw a fatal error to let Spark shutdown
     current executor and fail over to another one.

--- presto-spark-base/src/main/java/com/facebook/presto/spark/PrestoSparkQueryExecutionFactory.java
+++ presto-spark-base/src/main/java/com/facebook/presto/spark/PrestoSparkQueryExecutionFactory.java
@@ -57,6 +57,7 @@ import com.facebook.presto.spark.classloader_interface.IPrestoSparkQueryExecutio
 import com.facebook.presto.spark.classloader_interface.IPrestoSparkQueryExecutionFactory;
 import com.facebook.presto.spark.classloader_interface.PrestoSparkConfInitializer;
 import com.facebook.presto.spark.classloader_interface.PrestoSparkExecutionException;
+import com.facebook.presto.spark.classloader_interface.PrestoSparkFatalException;
 import com.facebook.presto.spark.classloader_interface.PrestoSparkSession;
 import com.facebook.presto.spark.classloader_interface.PrestoSparkShuffleStats;
 import com.facebook.presto.spark.classloader_interface.PrestoSparkTaskExecutorFactoryProvider;
@@ -785,7 +786,18 @@ public class PrestoSparkQueryExecutionFactory
                 log.error(eventFailure, "Error publishing query immediate failure event");
             }
 
-            throw toPrestoSparkFailure(session, failureInfo.get());
+            if (isFatalException(executionFailure)) {
+                // Throw fatal error directly to allow spark fail over to other executors.
+                throw executionFailure;
+            }
+            else {
+                throw toPrestoSparkFailure(session, failureInfo.get());
+            }
         }
+    }
+
+    private boolean isFatalException(Throwable t)
+    {
+        return t instanceof PrestoSparkFatalException;
     }
 }
