389d0ea94f2ce85146f260d2313357c0f0b7de99
Daniel Munoz <munozdaniel@meta.com>
2023-06-08 10:00:23 -0700

    Test StreamLabels (#5188)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/5188
    
    Test the newly created class
    
    Reviewed By: Yuhta
    
    Differential Revision: D46537777
    
    fbshipit-source-id: 19e88f5c247a97ff21de3a408547fb6af033efa2

---
+++ velox/dwio/dwrf/test/StreamLabelsTests.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "velox/dwio/dwrf/reader/StreamLabels.h"
+
+using namespace ::testing;
+using facebook::velox::AllocationPool;
+using namespace facebook::velox::dwrf;
+
+TEST(StreamLabelsTest, E2E) {
+  auto pool = facebook::velox::memory::addDefaultLeafMemoryPool();
+  AllocationPool allocationPool(pool.get());
+  StreamLabels root(allocationPool);
+  auto c0 = root.append("c0");
+  auto c1 = root.append("c1");
+  auto c0_f0 = c0.append("f0");
+  auto c1_f0 = c1.append("f0");
+  EXPECT_EQ(root.label(), "/");
+  EXPECT_EQ(c0.label(), "/c0");
+  EXPECT_EQ(c1.label(), "/c1");
+  EXPECT_EQ(c0_f0.label(), "/c0/f0");
+  EXPECT_EQ(c1_f0.label(), "/c1/f0");
+}
