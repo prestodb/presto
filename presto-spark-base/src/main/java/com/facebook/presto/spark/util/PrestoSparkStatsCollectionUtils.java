f65fd092966c05c7eba3e6716236be2b442b4ca4
Shrinidhi Joshi <shrinidhijoshi@meta.com>
2023-06-08 11:30:03 -0700

    [native pos] Update SparkMetrics integration to work with NativeTaskExecutorFactory
    Current integration of metrics for presto-spark-native into spark
    metrics relies on extracting Operator level info from TaskInfo
    objects from Native Process.
    After the introduction of PrestoSparkNativeTaskExecutorFactory,
    we no longer use NativeExecutionOperator.
    So instead we can directly extract metrics from TaskInfo object.

--- presto-spark-base/src/main/java/com/facebook/presto/spark/util/PrestoSparkStatsCollectionUtils.java
+++ presto-spark-base/src/main/java/com/facebook/presto/spark/util/PrestoSparkStatsCollectionUtils.java
@@ -15,19 +15,12 @@ package com.facebook.presto.spark.util;
 
 import com.facebook.airlift.log.Logger;
 import com.facebook.presto.common.RuntimeMetric;
-import com.facebook.presto.common.RuntimeStats;
 import com.facebook.presto.common.RuntimeUnit;
 import com.facebook.presto.execution.TaskInfo;
-import com.facebook.presto.operator.NativeExecutionInfo;
-import com.facebook.presto.operator.OperatorStats;
-import com.facebook.presto.operator.PipelineStats;
-import com.facebook.presto.operator.TaskStats;
 import org.apache.commons.text.CaseUtils;
 import org.apache.spark.executor.TaskMetrics;
 import org.apache.spark.util.AccumulatorV2;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class PrestoSparkStatsCollectionUtils
@@ -41,38 +34,35 @@ public class PrestoSparkStatsCollectionUtils
 
     public static void collectMetrics(final TaskInfo taskInfo)
     {
-        int taskId = -1;
-        int stageId = -1;
+        if (taskInfo == null || taskInfo.getStats() == null) {
+            return;
+        }
+
         try {
-            taskId = taskInfo.getTaskId().getId();
-            stageId = taskInfo.getTaskId().getStageExecutionId().getStageId().getId();
-            Set<RuntimeStats> runtimeStatsSet = collectRuntimeStats(taskInfo);
-            collectMetrics(runtimeStatsSet);
+            taskInfo.getStats().getRuntimeStats().getMetrics()
+                    .forEach(PrestoSparkStatsCollectionUtils::incSparkInternalAccumulator);
         }
         catch (Exception e) {
-            log.error("An error occurred while processing taskId=%s  stageId=%s", taskId, stageId, e);
+            log.warn("An error occurred while updating Spark Internal metrics for task=%s", taskInfo, e);
         }
     }
 
-    public static void collectMetrics(Set<RuntimeStats> runtimeStatsSet)
+    static void incSparkInternalAccumulator(final String prestoKey, final RuntimeMetric metric)
     {
-        runtimeStatsSet.forEach(runStats ->
-        {
-            runStats.getMetrics().entrySet().forEach(entry -> {
-                String prestoKey = entry.getKey();
-                String sparkInternalAccumulatorKey = getSparkInternalAccumulatorKey(prestoKey);
-                collectMetric(sparkInternalAccumulatorKey, prestoKey, entry.getValue());
-            });
-        });
-    }
+        TaskMetrics sparkTaskMetrics = org.apache.spark.TaskContext.get().taskMetrics();
+        if (sparkTaskMetrics == null) {
+            return;
+        }
 
-    static void collectMetric(final String sparkInternalAccumulatorKey,
-            final String prestoKey,
-            final RuntimeMetric metric)
-    {
-        boolean isSparkUnitMs = sparkInternalAccumulatorKey.contains("Ms");
-        long metricVal = getMetricValue(metric, isSparkUnitMs);
-        incSparkInternalAccumulator(sparkInternalAccumulatorKey, prestoKey, metricVal);
+        String sparkInternalAccumulatorName = getSparkInternalAccumulatorKey(prestoKey);
+        scala.Option accumulatorV2Optional = sparkTaskMetrics.nameToAccums().get(sparkInternalAccumulatorName);
+        if (accumulatorV2Optional.isEmpty()) {
+            return;
+        }
+
+        AccumulatorV2<Object, Object> accumulatorV2 = (AccumulatorV2<Object, Object>) accumulatorV2Optional.get();
+        accumulatorV2.add(
+                getMetricLongValue(metric, sparkInternalAccumulatorName.contains("Ms")));
     }
 
     static String getSparkInternalAccumulatorKey(final String prestoKey)
@@ -81,12 +71,13 @@ public class PrestoSparkStatsCollectionUtils
             int index = prestoKey.indexOf(PRESTO_NATIVE_OPERATOR_STATS_SEP);
             return prestoKey.substring(index);
         }
-        String[] strs = prestoKey.split("\\.");
-        if (strs == null || strs.length < 2) {
+        String[] prestoKeyParts = prestoKey.split("\\.");
+        int prestoKeyPartsLength = prestoKeyParts.length;
+        if (prestoKeyPartsLength < 2) {
             log.debug("Fail to build spark internal key for %s format not supported", prestoKey);
             return "";
         }
-        String prestoNewKey = String.format("%1$s%2$s", strs[0], strs[strs.length - 1]);
+        String prestoNewKey = String.format("%1$s%2$s", prestoKeyParts[0], prestoKeyParts[prestoKeyPartsLength - 1]);
         if (prestoNewKey.contains("_")) {
             prestoNewKey = CaseUtils.toCamelCase(prestoKey, false, '_');
         }
@@ -94,56 +85,12 @@ public class PrestoSparkStatsCollectionUtils
                 PRESTO_NATIVE_OPERATOR_STATS_PREFIX, prestoNewKey);
     }
 
-    static Set<RuntimeStats> collectRuntimeStats(TaskInfo taskInfo)
-    {
-        Set<RuntimeStats> stats = new HashSet<>();
-        if (taskInfo.getStats() == null) {
-            return stats;
-        }
-        for (PipelineStats pipelineStats : taskInfo.getStats().getPipelines()) {
-            if (pipelineStats != null) {
-                for (OperatorStats operatorStats : pipelineStats.getOperatorSummaries()) {
-                    if (operatorStats != null) {
-                        if (operatorStats.getOperatorType().equals("NativeExecutionOperator")) {
-                            NativeExecutionInfo nativeExecutionInfo = (NativeExecutionInfo) operatorStats.getInfo();
-                            if (nativeExecutionInfo != null) {
-                                for (TaskStats taskStats : nativeExecutionInfo.getTaskStats()) {
-                                    if (taskStats != null) {
-                                        RuntimeStats runtimeStat = taskStats.getRuntimeStats();
-                                        stats.add(runtimeStat);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return stats;
-    }
-
-    static long getMetricValue(RuntimeMetric metric, boolean isSparkUnitMs)
+    static long getMetricLongValue(RuntimeMetric metric, boolean isSparkUnitMs)
     {
         long sum = metric.getSum();
         if (metric.getUnit().equals(RuntimeUnit.NANO) && isSparkUnitMs) {
             sum = TimeUnit.NANOSECONDS.toMillis(sum);
         }
         return sum;
-    }
-
-    static void incSparkInternalAccumulator(final String sparkInternalAccuName, final String prestoKey, final Object metric)
-    {
-        TaskMetrics tm = org.apache.spark.TaskContext.get().taskMetrics();
-        if (tm != null) {
-            scala.Option acc2 = tm.nameToAccums().get(sparkInternalAccuName);
-            if (!acc2.isEmpty()) {
-                AccumulatorV2<Object, Object> acc = (AccumulatorV2<Object, Object>) acc2.get();
-                acc.add(metric);
-            }
-            else {
-                log.debug("Fail to find spark internal accumulator matching key:" +
-                        " %s prestoKey = %s ", sparkInternalAccuName, prestoKey);
-            }
-        }
     }
 }
