65f7009a9c88e1a6e8cab82e876ee463b4e74174
Masha Basmanova <mbasmanova@fb.com>
2023-06-07 21:41:23 -0400

    [native pos] Fail fast on sorted aggregations
    Velox doesn't support sorted aggregations yet. Make sure to fail fast instead of
    returning incorrect results.

--- presto-native-execution/src/test/java/com/facebook/presto/nativeworker/AbstractTestNativeAggregations.java
+++ presto-native-execution/src/test/java/com/facebook/presto/nativeworker/AbstractTestNativeAggregations.java
@@ -279,6 +279,13 @@ public abstract class AbstractTestNativeAggregations
         assertQuerySucceeds("SELECT orderkey, arbitrary(tax_as_real) FROM lineitem GROUP BY 1");
     }
 
+    @Test
+    public void testUnsupported()
+    {
+        assertQueryFails("SELECT array_agg(nationkey ORDER BY name) FROM nation",
+                ".* Aggregations with ORDER BY are not supported yet.");
+    }
+
     private void assertQueryResultCount(String sql, int expectedResultCount)
     {
         assertEquals(getQueryRunner().execute(sql).getRowCount(), expectedResultCount);
