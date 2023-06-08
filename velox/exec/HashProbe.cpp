c91af62eae38baf2756da400411c5b4a7ed3ccb5
Orri Erling <oerling@fb.com>
2023-06-08 04:04:10 -0700

    Return nullptr from getOutput of finished HashProbe - Fix yield (#4851)
    Summary:
    Operators allow calling getOutput() repeatedly after being finished. They are expected to return nullptr. Add check to HashProbe for this. This behavior was broken when adding 'state_' and spilling.
    
    Tested by adding a requestYield() after addInput() and noMoreInput() call in Driver::runInternal().  Original error surfaced in timer-based yield on a cluster, where infrequently the time slice would hit after nomoreInput() to a HashProbe. The subsequent getOutput when the Driver came back on thread would trigger the error.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/4851
    
    Reviewed By: xiaoxmeng
    
    Differential Revision: D45618359
    
    Pulled By: gggrace14
    
    fbshipit-source-id: 656bf21b2abad1c2606d34eed07488ab8c032705

--- velox/exec/HashProbe.cpp
+++ velox/exec/HashProbe.cpp
@@ -825,6 +825,9 @@ void HashProbe::checkStateTransition(ProbeOperatorState state) {
 }
 
 RowVectorPtr HashProbe::getOutput() {
+  if (isFinished()) {
+    return nullptr;
+  }
   checkRunning();
 
   clearIdentityProjectedOutput();
