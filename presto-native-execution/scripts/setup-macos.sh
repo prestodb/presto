9d2d767dd731ab43a757f340d8246783e11da64a
xiaoxmeng <xiaoxmeng@fb.com>
2023-06-13 01:40:56 -0700

    [native]turn on bigint compile option for folly to support velox

--- presto-native-execution/scripts/setup-macos.sh
+++ presto-native-execution/scripts/setup-macos.sh
@@ -28,9 +28,8 @@ export PATH=$(brew --prefix bison)/bin:$PATH
 function install_folly {
   github_checkout facebook/folly "${FB_OS_VERSION}"
   OPENSSL_ROOT_DIR=$(brew --prefix openssl@1.1) \
-    cmake_install -DBUILD_TESTS=OFF
+   cmake_install -DBUILD_TESTS=OFF -DFOLLY_HAVE_INT128_T=ON
 }
-
 
 function install_fizz {
   github_checkout facebookincubator/fizz "${FB_OS_VERSION}"
