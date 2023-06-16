234be70ddb8c638608d52c483fdd53e56ae755b4
Mahadevuni Naveen Kumar <nmahadevuni@gmail.com>
2023-06-16 21:17:33 +0530

    Add JWT for internal communication
    Supports JWT for authentication of internal requests.
    This is required for secure internal communication,
    especially when used in conjunction with external
    user authentication such as PASSWORD, LDAP etc
    
    Cherry-pick of trinodb/trino#2032
    Cherry-pick of trinodb/trino#2090
    Cherry-pick of trinodb/trino#2093
    Cherry-pick of trinodb/trino#2202
    Cherry-pick of trinodb/trino#11944
    
    Co-authored-by: Dain Sundstrom <dain@iq80.com>

--- presto-main/src/main/java/com/facebook/presto/server/PrestoServer.java
+++ presto-main/src/main/java/com/facebook/presto/server/PrestoServer.java
@@ -19,7 +19,6 @@ import com.facebook.airlift.discovery.client.DiscoveryModule;
 import com.facebook.airlift.discovery.client.ServiceAnnouncement;
 import com.facebook.airlift.event.client.HttpEventModule;
 import com.facebook.airlift.event.client.JsonEventModule;
-import com.facebook.airlift.http.server.HttpServerModule;
 import com.facebook.airlift.jaxrs.JaxrsModule;
 import com.facebook.airlift.jmx.JmxHttpModule;
 import com.facebook.airlift.jmx.JmxModule;
