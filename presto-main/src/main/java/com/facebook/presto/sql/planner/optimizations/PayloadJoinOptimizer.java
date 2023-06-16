768820bd6abd248f2a5903961f92d890c87d47d1
Lyublena Antova <lyublena@fb.com>
2023-06-16 14:25:22 -0700

    Extend payload join optimization for cases without map columns + bug fixes
    Several fixes and improvements related to payload join optimization:
    - enable optimization even if the fact table doesn't have any map or array columns
    - abort rewrite if there are operators other than select/project/filter/join as it may introduce wrong results
    - abort rewrite if collected join keys contain a column from the RHS of the current join
    SELECT l.* FROM lineitem l left join orders o on (l.orderkey = o.orderkey) left join part p on (l.partkey=p.partkey) left join (select 1 as m) mm on p.partkey=mm.m
    
    The join key from the last join condition (p.partkey) cannot be pushed to the left-most table in the join (lineitem). This change takes care of only propagating those join keys that
    come from the LHS of the join.
    
    - other fixes in propagating optimization context across operators
    - additional tests

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PayloadJoinOptimizer.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PayloadJoinOptimizer.java
@@ -14,8 +14,6 @@
 package com.facebook.presto.sql.planner.optimizations;
 
 import com.facebook.presto.Session;
-import com.facebook.presto.common.type.ArrayType;
-import com.facebook.presto.common.type.MapType;
 import com.facebook.presto.common.type.Type;
 import com.facebook.presto.common.type.VarcharType;
 import com.facebook.presto.expressions.LogicalRowExpressions;
@@ -64,6 +62,9 @@ import static com.facebook.presto.sql.planner.PlannerUtils.clonePlanNode;
 import static com.facebook.presto.sql.planner.PlannerUtils.coalesce;
 import static com.facebook.presto.sql.planner.PlannerUtils.equalityPredicate;
 import static com.facebook.presto.sql.planner.PlannerUtils.isScanFilterProject;
+import static com.facebook.presto.sql.planner.PlannerUtils.restrictOutput;
+import static com.facebook.presto.sql.planner.plan.ChildReplacer.replaceChildren;
+import static com.facebook.presto.sql.planner.plan.JoinNode.Type.LEFT;
 import static com.facebook.presto.sql.relational.Expressions.constant;
 import static com.facebook.presto.sql.relational.Expressions.specialForm;
 import static com.google.common.base.Preconditions.checkArgument;
@@ -136,8 +137,7 @@ public class PayloadJoinOptimizer
     {
         FunctionAndTypeManager functionAndTypeManager = metadata.getFunctionAndTypeManager();
         if (isEnabled(session)) {
-            PlanNode result = SimplePlanRewriter.rewriteWith(new PayloadJoinOptimizer.Rewriter(session, this.metadata, types, functionAndTypeManager, idAllocator, variableAllocator), plan, new JoinContext());
-            return result;
+            return SimplePlanRewriter.rewriteWith(new PayloadJoinOptimizer.Rewriter(session, this.metadata, types, functionAndTypeManager, idAllocator, variableAllocator), plan, new JoinContext());
         }
         return plan;
     }
@@ -174,41 +174,45 @@ public class PayloadJoinOptimizer
             this.variableAllocator = requireNonNull(variableAllocator, "variableAllocator is null");
         }
 
-        private Set<VariableReferenceExpression> extractPayloadColumns(ImmutableSet<VariableReferenceExpression> columns)
-        {
-            return columns.stream().filter(var -> var.getType() instanceof MapType || var.getType() instanceof ArrayType).collect(toImmutableSet());
-        }
-
         @Override
-        public PlanNode visitPlan(PlanNode node, RewriteContext<JoinContext> context)
+        public PlanNode visitPlan(PlanNode planNode, RewriteContext<JoinContext> context)
         {
-            return context.defaultRewrite(node, context.get());
+            // do a default rewrite with a new context for each child to avoid lateral propagation of context information
+            List<PlanNode> newChildren = planNode.getSources().stream().map(childNode -> context.rewrite(childNode, new JoinContext())).collect(Collectors.toList());
+            return replaceChildren(planNode, newChildren);
         }
 
         @Override
         public PlanNode visitJoin(JoinNode joinNode, RewriteContext<JoinContext> context)
         {
             final JoinContext joinContext = context.get();
-            int numJoinKeys = joinContext.getJoinKeys().size();
+            Set<VariableReferenceExpression> inputJoinKeys = joinContext.getJoinKeys();
             PlanNode leftNode = joinNode.getLeft();
             PlanNode rightNode = joinNode.getRight();
-            boolean isTopJoin = numJoinKeys == 0;
+
+            boolean isTopJoin = joinContext.getJoinKeys().size() == 0;
 
             ImmutableSet<VariableReferenceExpression> leftColumns = leftNode.getOutputVariables().stream().collect(toImmutableSet());
+
+            // abort rewrite if some of the collected join keys are in the RHS of the current join
+            ImmutableSet<VariableReferenceExpression> rightJoinKeys = inputJoinKeys.stream().filter(key -> rightNode.getOutputVariables().contains(key)).collect(toImmutableSet());
             Set<VariableReferenceExpression> joinKeys = extractJoinKeys(joinNode.getFilter(), joinNode.getCriteria());
 
             ImmutableSet<VariableReferenceExpression> leftJoinKeys = intersection(joinKeys, leftColumns).immutableCopy();
-            if (!needsRewrite(joinNode.getType(), leftColumns, leftJoinKeys)) {
-                return context.defaultRewrite(joinNode, joinContext);
+            if (!rightJoinKeys.isEmpty() || !needsRewrite(joinNode.getType(), leftColumns, leftJoinKeys)) {
+                List<PlanNode> newChildren = joinNode.getSources().stream()
+                        .map(child -> defaultRewriteJoinChild(child, context, joinNode.isCrossJoin()))
+                        .collect(toImmutableList());
+                return replaceChildren(joinNode, newChildren);
             }
 
             joinContext.addKeys(leftJoinKeys);
             joinContext.incrementNumJoins();
 
             PlanNode newLeftNode = context.rewrite(leftNode, joinContext);
-            if (leftNode == newLeftNode) {
+            if (leftNode.equals(newLeftNode)) {
                 newLeftNode = context.rewrite(leftNode, new JoinContext());
-                return joinNode.replaceChildren(ImmutableList.of(newLeftNode, rightNode));
+                return replaceChildren(joinNode, ImmutableList.of(newLeftNode, rightNode));
             }
 
             List<VariableReferenceExpression> leftCols = newLeftNode.getOutputVariables();
@@ -231,25 +235,32 @@ public class PayloadJoinOptimizer
 
             if (isTopJoin && context.get().needsPayloadRejoin()) {
                 PlanNode payloadJoin = transformJoin(newJoinNode, joinContext);
+                // reset payload node as it has been reattached to the plan node
+                context.get().setPayloadNode(null);
+
                 // do a final check that the rewrite didn't lose any columns (can happen if there are intermediate projections on non-join keys that get hidden because of the DISTINCT keys computation)
-                if (!payloadJoin.getOutputVariables().containsAll(joinNode.getOutputVariables())) {
+                List<VariableReferenceExpression> outputVariables = joinNode.getOutputVariables();
+                if (!payloadJoin.getOutputVariables().containsAll(outputVariables)) {
                     return joinNode;
                 }
-                return payloadJoin;
+                return restrictOutput(payloadJoin, planNodeIdAllocator, outputVariables);
             }
 
             return newJoinNode;
         }
 
+        private PlanNode defaultRewriteJoinChild(PlanNode child, RewriteContext<JoinContext> context, boolean isCrossJoin)
+        {
+            PlanNode newChild = context.rewrite(child, new JoinContext());
+            if (isCrossJoin && child.getOutputVariables() != newChild.getOutputVariables()) {
+                return restrictOutput(newChild, planNodeIdAllocator, child.getOutputVariables());
+            }
+            return newChild;
+        }
+
         private boolean needsRewrite(JoinNode.Type joinType, ImmutableSet<VariableReferenceExpression> leftColumns, Set<VariableReferenceExpression> joinKeys)
         {
-            if (joinType == JoinNode.Type.LEFT && supportedJoinKeyTypes(joinKeys)) {
-                Set<VariableReferenceExpression> leftPayloadColumns = extractPayloadColumns(leftColumns);
-
-                return !leftPayloadColumns.isEmpty() && !leftPayloadColumns.containsAll(joinKeys);
-            }
-
-            return false;
+            return joinType == LEFT && supportedJoinKeyTypes(joinKeys) && leftColumns.stream().anyMatch(var -> !joinKeys.contains(var));
         }
 
         @Override
@@ -261,24 +272,17 @@ public class PayloadJoinOptimizer
 
             PlanNode child = projectNode.getSource();
 
-            Set<VariableReferenceExpression> joinKeys = context.get().getJoinKeys();
-            Assignments newAssignments = projectNode.getAssignments();
-            if (!child.getOutputVariables().containsAll(joinKeys)) {
-                Assignments.Builder assignments = Assignments.builder();
+            Set<VariableReferenceExpression> inputJoinKeys = context.get().getJoinKeys();
+            if (!child.getOutputVariables().containsAll(inputJoinKeys)) {
                 Map<VariableReferenceExpression, RowExpression> pushableExpressions = new HashMap<>();
 
                 projectNode.getAssignments().forEach((var, expr) -> {
-                    if (joinKeys.contains(var) && !var.equals(expr)) {
+                    if (inputJoinKeys.contains(var) && !var.equals(expr)) {
                         // join key computed in this projection: need to push down
-                        assignments.put(var, var);
                         pushableExpressions.put(var, expr);
-                    }
-                    else {
-                        assignments.put(var, expr);
                     }
                 });
 
-                newAssignments = assignments.build();
                 context.get().addProjectionsToPush(pushableExpressions);
             }
 
@@ -286,6 +290,26 @@ public class PayloadJoinOptimizer
 
             if (child.equals(newChild)) {
                 return projectNode;
+            }
+
+            // remove assignments that were pushed down
+            Set<VariableReferenceExpression> joinKeys = context.get().getJoinKeys();
+
+            Assignments newAssignments = projectNode.getAssignments();
+            if (context.get().needsPayloadRejoin() && !child.getOutputVariables().containsAll(joinKeys)) {
+                Assignments.Builder assignments = Assignments.builder();
+
+                projectNode.getAssignments().forEach((var, expr) -> {
+                    if (joinKeys.contains(var) && !var.equals(expr)) {
+                        // join key computed in this projection: need to push down
+                        assignments.put(var, var);
+                    }
+                    else {
+                        assignments.put(var, expr);
+                    }
+                });
+
+                newAssignments = assignments.build();
             }
 
             Set<VariableReferenceExpression> newChildOutputVarSet = newChild.getOutputVariables().stream().collect(toImmutableSet());
@@ -300,18 +324,11 @@ public class PayloadJoinOptimizer
         @Override
         public PlanNode visitFilter(FilterNode filterNode, RewriteContext<JoinContext> context)
         {
-            Set<VariableReferenceExpression> joinKeys = context.get().getJoinKeys();
-            List<VariableReferenceExpression> predicateVars = VariablesExtractor.extractAll(filterNode.getPredicate());
-
             if (isScanFilterProject(filterNode)) {
                 return rewriteScanFilterProject(filterNode, context);
             }
 
-            if (joinKeys.isEmpty() || joinKeys.containsAll(predicateVars)) {
-                return context.defaultRewrite(filterNode, context.get());
-            }
-
-            return filterNode;
+            return context.defaultRewrite(filterNode, new JoinContext());
         }
 
         @Override
@@ -495,7 +512,7 @@ public class PayloadJoinOptimizer
 
     private static class JoinContext
     {
-        private final Set<VariableReferenceExpression> joinKeys = new HashSet<>();
+        private Set<VariableReferenceExpression> joinKeys = new HashSet<>();
         private Map<VariableReferenceExpression, VariableReferenceExpression> joinKeyMap;
         private Map<VariableReferenceExpression, RowExpression> projectionsToPush = new HashMap<>();
 
@@ -544,6 +561,14 @@ public class PayloadJoinOptimizer
             this.payloadNode = payloadNode;
         }
 
+        public void reset()
+        {
+            joinKeys = new HashSet<>();
+            projectionsToPush = new HashMap<>();
+            joinKeyMap = null;
+            numJoins = 0;
+            payloadNode = null;
+        }
         public int getNumJoins()
         {
             return numJoins;
--- presto-singlestore/src/test/java/com/facebook/presto/plugin/singlestore/TestSingleStoreDistributedQueries.java
+++ presto-singlestore/src/test/java/com/facebook/presto/plugin/singlestore/TestSingleStoreDistributedQueries.java
@@ -195,7 +195,13 @@ public class TestSingleStoreDistributedQueries
     }
 
     @Override
-    public void testPayloadJoins()
+    public void testPayloadJoinApplicability()
+    {
+        // no op -- test not supported due to lack of support for array types.
+    }
+
+    @Override
+    public void testPayloadJoinCorrectness()
     {
         // no op -- test not supported due to lack of support for array types.
     }
