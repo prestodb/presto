8ebd6f11265c16999d0fa45744816d52077825ff
Vivek <vbharathan2008@gmail.com>
2023-07-17 15:56:21 -0700

    Add table constraint properties "enforced" and "enabled"
    Impose ordering on table columns in constraint to conform with Hive spec

--- presto-spi/src/main/java/com/facebook/presto/spi/constraints/PrimaryKeyConstraint.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/constraints/PrimaryKeyConstraint.java
@@ -16,26 +16,28 @@ package com.facebook.presto.spi.constraints;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toCollection;
 
 public class PrimaryKeyConstraint<T>
         extends UniqueConstraint<T>
 {
     @JsonCreator
     public PrimaryKeyConstraint(@JsonProperty("name") String name,
-                                @JsonProperty("columns")Set<T> columnNames,
-                                @JsonProperty("enforced") boolean enabled,
-                                @JsonProperty("rely") boolean rely)
+            @JsonProperty("columns") LinkedHashSet<T> columnNames,
+            @JsonProperty("enforced") boolean enabled,
+            @JsonProperty("rely") boolean rely,
+            @JsonProperty("enforced") boolean enforced)
     {
-        this(Optional.of(name), columnNames, enabled, rely);
+        this(Optional.of(name), columnNames, enabled, rely, enforced);
     }
 
-    private PrimaryKeyConstraint(Optional<String> name, Set<T> columnNames, boolean enabled, boolean rely)
+    private PrimaryKeyConstraint(Optional<String> name, LinkedHashSet<T> columnNames, boolean enabled, boolean rely, boolean enforced)
     {
-        super(name, columnNames, enabled, rely);
+        super(name, columnNames, enabled, rely, enforced);
     }
 
     @Override
@@ -43,9 +45,10 @@ public class PrimaryKeyConstraint<T>
     {
         if (this.getColumns().stream().allMatch(assignments::containsKey)) {
             return Optional.of(new PrimaryKeyConstraint<R>(this.getName(),
-                    this.getColumns().stream().map(assignments::get).collect(Collectors.toSet()),
-                    this.isEnforced(),
-                    this.isRely()));
+                    this.getColumns().stream().map(assignments::get).collect(toCollection(LinkedHashSet::new)),
+                    this.isEnabled(),
+                    this.isRely(),
+                    this.isEnforced()));
         }
         else {
             return Optional.empty();
