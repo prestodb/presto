2347775fcca61381d546a090bf1caadda10b8833
Zac <zacw@meta.com>
2023-07-17 08:09:32 -0700

    [native] Remove unnecessary prefix in e2e test

--- presto-native-execution/src/test/java/com/facebook/presto/nativeworker/AbstractTestNativeArrayFunctionQueries.java
+++ presto-native-execution/src/test/java/com/facebook/presto/nativeworker/AbstractTestNativeArrayFunctionQueries.java
@@ -34,45 +34,45 @@ public abstract class AbstractTestNativeArrayFunctionQueries
     @Test
     public void testRepeat()
     {
-        this.assertQuery("SELECT repeat(orderkey, linenumber) FROM lineitem");
-        this.assertQuery("SELECT repeat(orderkey, 3) FROM lineitem");
-        this.assertQuery("SELECT repeat(orderkey, NULL) FROM lineitem");
-        this.assertQuery("SELECT try(repeat(orderkey, -2)) FROM lineitem");
-        this.assertQuery("SELECT try(repeat(orderkey, 10001)) FROM lineitem");
-        this.assertQueryFails("SELECT repeat(orderkey, -2) FROM lineitem",
+        assertQuery("SELECT repeat(orderkey, linenumber) FROM lineitem");
+        assertQuery("SELECT repeat(orderkey, 3) FROM lineitem");
+        assertQuery("SELECT repeat(orderkey, NULL) FROM lineitem");
+        assertQuery("SELECT try(repeat(orderkey, -2)) FROM lineitem");
+        assertQuery("SELECT try(repeat(orderkey, 10001)) FROM lineitem");
+        assertQueryFails("SELECT repeat(orderkey, -2) FROM lineitem",
                 ".*Count argument of repeat function must be greater than or equal to 0.*");
-        this.assertQueryFails("SELECT repeat(orderkey, 10001) FROM lineitem",
+        assertQueryFails("SELECT repeat(orderkey, 10001) FROM lineitem",
                 ".*Count argument of repeat function must be less than or equal to 10000.*");
     }
 
     @Test
     public void testShuffle()
     {
-        this.assertQuerySucceeds("SELECT shuffle(quantities) FROM orders_ex");
-        this.assertQuerySucceeds("SELECT shuffle(array_sort(quantities)) FROM orders_ex");
-        this.assertQuery("SELECT array_sort(shuffle(quantities)) FROM orders_ex");
+        assertQuerySucceeds("SELECT shuffle(quantities) FROM orders_ex");
+        assertQuerySucceeds("SELECT shuffle(array_sort(quantities)) FROM orders_ex");
+        assertQuery("SELECT array_sort(shuffle(quantities)) FROM orders_ex");
     }
 
     @Test
     public void testArrayMatch()
     {
         // test all_match
-        this.assertQuery("SELECT all_match(quantities, x -> ((10 / x) > 2)) FROM orders_ex");
-        this.assertQuery("SELECT all_match(quantities, x -> TRY(((10 / x) > 2))) FROM orders_ex");
-        this.assertQuery("SELECT TRY(all_match(quantities, x -> ((10 / x) > 2))) FROM orders_ex");
-        this.assertQuery("SELECT all_match(shuffle(quantities), x -> (x > 500.0)) FROM orders_ex");
+        assertQuery("SELECT all_match(quantities, x -> ((10 / x) > 2)) FROM orders_ex");
+        assertQuery("SELECT all_match(quantities, x -> TRY(((10 / x) > 2))) FROM orders_ex");
+        assertQuery("SELECT TRY(all_match(quantities, x -> ((10 / x) > 2))) FROM orders_ex");
+        assertQuery("SELECT all_match(shuffle(quantities), x -> (x > 500.0)) FROM orders_ex");
 
         // test any_match
-        this.assertQuery("SELECT any_match(quantities, x -> ((10 / x) > 2)) FROM orders_ex");
-        this.assertQuery("SELECT any_match(quantities, x -> TRY(((10 / x) > 2))) FROM orders_ex");
-        this.assertQuery("SELECT TRY(any_match(quantities, x -> ((10 / x) > 2))) FROM orders_ex");
-        this.assertQuery("SELECT any_match(shuffle(quantities), x -> (x > 500.0)) FROM orders_ex");
+        assertQuery("SELECT any_match(quantities, x -> ((10 / x) > 2)) FROM orders_ex");
+        assertQuery("SELECT any_match(quantities, x -> TRY(((10 / x) > 2))) FROM orders_ex");
+        assertQuery("SELECT TRY(any_match(quantities, x -> ((10 / x) > 2))) FROM orders_ex");
+        assertQuery("SELECT any_match(shuffle(quantities), x -> (x > 500.0)) FROM orders_ex");
 
         // test none_match
-        this.assertQuery("SELECT none_match(quantities, x -> ((10 / x) > 2)) FROM orders_ex");
-        this.assertQuery("SELECT none_match(quantities, x -> TRY(((10 / x) > 2))) FROM orders_ex");
-        this.assertQuery("SELECT TRY(none_match(quantities, x -> ((10 / x) > 2))) FROM orders_ex");
-        this.assertQuery("SELECT none_match(shuffle(quantities), x -> (x > 500.0)) FROM orders_ex");
+        assertQuery("SELECT none_match(quantities, x -> ((10 / x) > 2)) FROM orders_ex");
+        assertQuery("SELECT none_match(quantities, x -> TRY(((10 / x) > 2))) FROM orders_ex");
+        assertQuery("SELECT TRY(none_match(quantities, x -> ((10 / x) > 2))) FROM orders_ex");
+        assertQuery("SELECT none_match(shuffle(quantities), x -> (x > 500.0)) FROM orders_ex");
     }
 
     @Test
@@ -86,42 +86,42 @@ public abstract class AbstractTestNativeArrayFunctionQueries
     @Test
     public void testArrayTrim()
     {
-        this.assertQuery("SELECT trim_array(quantities, 0) FROM orders_ex");
-        this.assertQuery("SELECT trim_array(quantities, 1) FROM orders_ex where cardinality(quantities) > 5");
-        this.assertQuery("SELECT trim_array(quantities, 2) FROM orders_ex where cardinality(quantities) > 5");
-        this.assertQuery("SELECT trim_array(quantities, 3) FROM orders_ex where cardinality(quantities) > 5");
-        this.assertQueryFails("SELECT trim_array(quantities, 3) FROM orders_ex where cardinality(quantities) = 2", ".*size must not exceed array cardinality.*");
+        assertQuery("SELECT trim_array(quantities, 0) FROM orders_ex");
+        assertQuery("SELECT trim_array(quantities, 1) FROM orders_ex where cardinality(quantities) > 5");
+        assertQuery("SELECT trim_array(quantities, 2) FROM orders_ex where cardinality(quantities) > 5");
+        assertQuery("SELECT trim_array(quantities, 3) FROM orders_ex where cardinality(quantities) > 5");
+        assertQueryFails("SELECT trim_array(quantities, 3) FROM orders_ex where cardinality(quantities) = 2", ".*size must not exceed array cardinality.*");
     }
 
     @Test
     public void testArrayConcat()
     {
         // Concatenate two integer arrays.
-        this.assertQuery("SELECT concat(ARRAY[linenumber], ARRAY[orderkey, partkey]) FROM lineitem");
-        this.assertQuery("SELECT ARRAY[linenumber] || ARRAY[orderkey, partkey] FROM lineitem");
+        assertQuery("SELECT concat(ARRAY[linenumber], ARRAY[orderkey, partkey]) FROM lineitem");
+        assertQuery("SELECT ARRAY[linenumber] || ARRAY[orderkey, partkey] FROM lineitem");
         // Concatenate two integer arrays with null.
-        this.assertQuery("SELECT concat(ARRAY[linenumber, NULL], ARRAY[orderkey, partkey]) FROM lineitem");
-        this.assertQuery("SELECT concat(ARRAY[linenumber], NULL, ARRAY[orderkey, partkey]) FROM lineitem");
-        this.assertQuery("SELECT ARRAY[linenumber, NULL] || ARRAY[orderkey, partkey] FROM lineitem");
+        assertQuery("SELECT concat(ARRAY[linenumber, NULL], ARRAY[orderkey, partkey]) FROM lineitem");
+        assertQuery("SELECT concat(ARRAY[linenumber], NULL, ARRAY[orderkey, partkey]) FROM lineitem");
+        assertQuery("SELECT ARRAY[linenumber, NULL] || ARRAY[orderkey, partkey] FROM lineitem");
         // Concatenate more than two arrays.
-        this.assertQuery("SELECT concat(ARRAY[linenumber], ARRAY[partkey], ARRAY[orderkey, partkey], ARRAY[123, 456], ARRAY[quantity]) FROM lineitem");
-        this.assertQuery("SELECT ARRAY[linenumber] || ARRAY[partkey] || ARRAY[orderkey, partkey] || ARRAY[123, 456] || ARRAY[quantity] FROM lineitem");
+        assertQuery("SELECT concat(ARRAY[linenumber], ARRAY[partkey], ARRAY[orderkey, partkey], ARRAY[123, 456], ARRAY[quantity]) FROM lineitem");
+        assertQuery("SELECT ARRAY[linenumber] || ARRAY[partkey] || ARRAY[orderkey, partkey] || ARRAY[123, 456] || ARRAY[quantity] FROM lineitem");
         // Concatenate complex types.
-        this.assertQuery("SELECT concat(ARRAY[ARRAY[linenumber], ARRAY[suppkey, orderkey]], ARRAY[ARRAY[orderkey, partkey]]) FROM lineitem");
-        this.assertQuery("SELECT ARRAY[ARRAY[linenumber], ARRAY[suppkey, orderkey]] || ARRAY[ARRAY[orderkey, partkey]] FROM lineitem");
+        assertQuery("SELECT concat(ARRAY[ARRAY[linenumber], ARRAY[suppkey, orderkey]], ARRAY[ARRAY[orderkey, partkey]]) FROM lineitem");
+        assertQuery("SELECT ARRAY[ARRAY[linenumber], ARRAY[suppkey, orderkey]] || ARRAY[ARRAY[orderkey, partkey]] FROM lineitem");
         // Concatenate array with a single element.
-        this.assertQuery("SELECT concat(linenumber, ARRAY[orderkey, partkey]) FROM lineitem");
-        this.assertQuery("SELECT concat(ARRAY[orderkey, partkey], linenumber) FROM lineitem");
-        this.assertQuery("SELECT linenumber || ARRAY[orderkey, partkey] FROM lineitem");
-        this.assertQuery("SELECT ARRAY[orderkey, partkey] || linenumber FROM lineitem");
+        assertQuery("SELECT concat(linenumber, ARRAY[orderkey, partkey]) FROM lineitem");
+        assertQuery("SELECT concat(ARRAY[orderkey, partkey], linenumber) FROM lineitem");
+        assertQuery("SELECT linenumber || ARRAY[orderkey, partkey] FROM lineitem");
+        assertQuery("SELECT ARRAY[orderkey, partkey] || linenumber FROM lineitem");
         // Concatenate array with a null.
-        this.assertQuery("SELECT concat(CAST(NULL AS INTEGER), ARRAY[orderkey, partkey]) FROM lineitem");
-        this.assertQuery("SELECT concat(ARRAY[orderkey, partkey], CAST(NULL AS INTEGER)) FROM lineitem");
-        this.assertQuery("SELECT CAST(NULL AS INTEGER) || ARRAY[orderkey, partkey] FROM lineitem");
-        this.assertQuery("SELECT ARRAY[orderkey, partkey] || CAST(NULL AS INTEGER) FROM lineitem");
+        assertQuery("SELECT concat(CAST(NULL AS INTEGER), ARRAY[orderkey, partkey]) FROM lineitem");
+        assertQuery("SELECT concat(ARRAY[orderkey, partkey], CAST(NULL AS INTEGER)) FROM lineitem");
+        assertQuery("SELECT CAST(NULL AS INTEGER) || ARRAY[orderkey, partkey] FROM lineitem");
+        assertQuery("SELECT ARRAY[orderkey, partkey] || CAST(NULL AS INTEGER) FROM lineitem");
         // Test nested concatenation.
-        this.assertQuery("SELECT concat(linenumber, concat(orderkey, ARRAY[suppkey, partkey])) FROM lineitem");
-        this.assertQuery("SELECT linenumber || concat(orderkey, ARRAY[suppkey, partkey]) FROM lineitem");
-        this.assertQuery("SELECT concat(linenumber, orderkey || ARRAY[suppkey, partkey]) FROM lineitem");
+        assertQuery("SELECT concat(linenumber, concat(orderkey, ARRAY[suppkey, partkey])) FROM lineitem");
+        assertQuery("SELECT linenumber || concat(orderkey, ARRAY[suppkey, partkey]) FROM lineitem");
+        assertQuery("SELECT concat(linenumber, orderkey || ARRAY[suppkey, partkey]) FROM lineitem");
     }
 }
