ebee09c0e188ed54f883d86f96f8eb2a8d3a4d53
Amit Dutta <adutta@fb.com>
2023-07-23 08:20:12 -0700

    [native] Make node optional in spill path.

--- presto-native-execution/presto_cpp/main/tests/TaskManagerTest.cpp
+++ presto-native-execution/presto_cpp/main/tests/TaskManagerTest.cpp
@@ -946,11 +946,29 @@ TEST_F(TaskManagerTest, buildTaskSpillDirectoryPath) {
   EXPECT_EQ(
       "fs::/base/192.168.10.2_19/2022-12-20/presto_native/20221220-Q/Task1/",
       TaskManager::buildTaskSpillDirectoryPath(
-          "fs::/base", "192.168.10.2", "19", "20221220-Q", "Task1"));
+          "fs::/base", "192.168.10.2", "19", "20221220-Q", "Task1", true));
   EXPECT_EQ(
       "fsx::/root/192.16.10.2_sample_node_id/1970-01-01/presto_native/Q100/Task22/",
       TaskManager::buildTaskSpillDirectoryPath(
-          "fsx::/root", "192.16.10.2", "sample_node_id", "Q100", "Task22"));
+          "fsx::/root",
+          "192.16.10.2",
+          "sample_node_id",
+          "Q100",
+          "Task22",
+          true));
+  EXPECT_EQ(
+      "fs::/base/2022-12-20/presto_native/20221220-Q/Task1/",
+      TaskManager::buildTaskSpillDirectoryPath(
+          "fs::/base", "192.168.10.2", "19", "20221220-Q", "Task1", false));
+  EXPECT_EQ(
+      "fsx::/root/1970-01-01/presto_native/Q100/Task22/",
+      TaskManager::buildTaskSpillDirectoryPath(
+          "fsx::/root",
+          "192.16.10.2",
+          "sample_node_id",
+          "Q100",
+          "Task22",
+          false));
 }
 
 TEST_F(TaskManagerTest, getDataOnAbortedTask) {
