56d04d4d140bc6984937c27f470eb33a2a1e80f3
Pedro Eugenio Rocha Pedreira <pedroerp@meta.com>
2023-07-27 22:25:24 -0700

    [native] Adding RemoteFunctionRegisterer (#20335)
    Summary:
    Pull Request resolved: https://github.com/prestodb/presto/pull/20335
    
    Adding RemoteFunctionRegisterer to recursively read json files from
    a specified path, and register them as remote functions. The next PR will add a
    config parameter to close this entire loop.
    
    Differential Revision: D47564255
    
    fbshipit-source-id: 74302d61663192cb7eba8dd6ad030499be826975

---
+++ presto-native-execution/presto_cpp/main/RemoteFunctionRegisterer.h
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <folly/SocketAddress.h>
+
+namespace facebook::presto {
+
+/// Register remote functions from json files found at `path`.
+///
+/// If `path` is a file, open it and parse the json signature using
+/// JsonSignatureParser. If `path` is a directory, recursively visit all
+/// directories and read/register all json files in the directory tree.
+///
+/// `location` specifies where the remote function should forward connections
+/// to.
+///
+/// Returns the number of signatures registered.
+size_t registerRemoteFuctions(
+    const std::string& inputPath,
+    const folly::SocketAddress& location);
+
+} // namespace facebook::presto
