03af1f090f0fe5313e46d1d612d7806cfc8d25fb
xiaoxmeng <xiaoxmeng@fb.com>
2023-08-01 07:42:53 -0700

    [native]Advance velox version and update spill configs

--- presto-native-execution/presto_cpp/main/types/tests/PlanConverterTest.cpp
+++ presto-native-execution/presto_cpp/main/types/tests/PlanConverterTest.cpp
@@ -24,7 +24,7 @@
 #include "presto_cpp/main/types/PrestoToVeloxQueryPlan.h"
 #include "presto_cpp/presto_protocol/Connectors.h"
 #include "presto_cpp/presto_protocol/presto_protocol.h"
-#include "velox/connectors/hive/HiveConnector.h"
+#include "velox/connectors/hive/TableHandle.h"
 #include "velox/exec/tests/utils/TempDirectoryPath.h"
 
 namespace fs = boost::filesystem;
