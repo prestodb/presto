fb2829ad55c3d719bf43178a9eea34792398a878
Jimmy Lu <jimmylu@meta.com>
2023-08-04 17:34:29 -0700

    Add missing cast in NEON SIMD to fix build (#5986)
    Summary: Pull Request resolved: https://github.com/facebookincubator/velox/pull/5986
    
    Reviewed By: tanjialiang
    
    Differential Revision: D48032335
    
    fbshipit-source-id: e52abdd29562f1644a5b661302cb918bac33c0d4

--- velox/common/base/SimdUtil-inl.h
+++ velox/common/base/SimdUtil-inl.h
@@ -1207,6 +1207,25 @@ struct ReinterpretBatch<int32_t, uint32_t, A> {
 };
 
 template <typename A>
+struct ReinterpretBatch<uint64_t, uint32_t, A> {
+#if XSIMD_WITH_NEON
+  static xsimd::batch<uint64_t, A> apply(
+      xsimd::batch<uint32_t, A> data,
+      const xsimd::neon&) {
+    return vreinterpret_u64_u32(data.data);
+  }
+#endif
+
+#if XSIMD_WITH_NEON64
+  static xsimd::batch<uint64_t, A> apply(
+      xsimd::batch<uint32_t, A> data,
+      const xsimd::neon64&) {
+    return vreinterpretq_u64_u32(data.data);
+  }
+#endif
+};
+
+template <typename A>
 struct ReinterpretBatch<uint64_t, int64_t, A> {
 #if XSIMD_WITH_NEON
   static xsimd::batch<uint64_t, A> apply(
@@ -1226,6 +1245,25 @@ struct ReinterpretBatch<uint64_t, int64_t, A> {
 };
 
 template <typename A>
+struct ReinterpretBatch<uint32_t, int64_t, A> {
+#if XSIMD_WITH_NEON
+  static xsimd::batch<uint32_t, A> apply(
+      xsimd::batch<int64_t, A> data,
+      const xsimd::neon&) {
+    return vreinterpret_u32_s64(data.data);
+  }
+#endif
+
+#if XSIMD_WITH_NEON64
+  static xsimd::batch<uint32_t, A> apply(
+      xsimd::batch<int64_t, A> data,
+      const xsimd::neon64&) {
+    return vreinterpretq_u32_s64(data.data);
+  }
+#endif
+};
+
+template <typename A>
 struct ReinterpretBatch<int64_t, uint64_t, A> {
 #if XSIMD_WITH_NEON
   static xsimd::batch<int64_t, A> apply(
@@ -1240,6 +1278,25 @@ struct ReinterpretBatch<int64_t, uint64_t, A> {
       xsimd::batch<uint64_t, A> data,
       const xsimd::neon64&) {
     return vreinterpretq_s64_u64(data.data);
+  }
+#endif
+};
+
+template <typename A>
+struct ReinterpretBatch<uint32_t, uint64_t, A> {
+#if XSIMD_WITH_NEON
+  static xsimd::batch<uint32_t, A> apply(
+      xsimd::batch<uint64_t, A> data,
+      const xsimd::neon&) {
+    return vreinterpret_u32_u64(data.data);
+  }
+#endif
+
+#if XSIMD_WITH_NEON64
+  static xsimd::batch<uint32_t, A> apply(
+      xsimd::batch<uint64_t, A> data,
+      const xsimd::neon64&) {
+    return vreinterpretq_u32_u64(data.data);
   }
 #endif
 };
