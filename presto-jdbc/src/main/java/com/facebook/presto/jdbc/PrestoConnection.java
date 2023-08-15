b896f129d0e34afcd41cc87613cf3ee93b481145
Ivan Millan <ing.imillan@gmail.com>
2023-08-15 18:16:39 -0500

    Refactor PrestoConnection newConnectionWithSessionProperties
    - Removing queryParams from prestoUrl during connection creation to prevent infinite query execution loops for cases where queryInterceptors property is part of the connection url query parameters
    
    - Added copySessionProperties method as part of the refactoring process and exposed it publicly to enable the copy of session properties from one connection to another without exposing getSessionProperties method from PrestoConnection class

--- presto-jdbc/src/main/java/com/facebook/presto/jdbc/PrestoConnection.java
+++ presto-jdbc/src/main/java/com/facebook/presto/jdbc/PrestoConnection.java
@@ -128,18 +128,24 @@ public class PrestoConnection
     public static PrestoConnection newConnectionWithSessionProperties(PrestoConnection connectionWithSessionProperties, Properties connectionProperties)
             throws SQLException
     {
-        if (connectionWithSessionProperties != null) {
-            Map<String, String> map = connectionWithSessionProperties.getSessionProperties();
-            if (map != null) {
-                PrestoDriverUri uri = new PrestoDriverUri(connectionWithSessionProperties.getMetaData().getURL(), connectionProperties);
-                PrestoConnection prestoConnection = new PrestoConnection(uri, connectionWithSessionProperties.queryExecutor);
+        requireNonNull(connectionWithSessionProperties, "connectionWithSessionProperties is null");
 
-                map.forEach(prestoConnection::setSessionProperty);
-                return prestoConnection;
-            }
-        }
+        URI connectionWithSessionPropertiesURI = connectionWithSessionProperties.getURI();
+        String prestoUrl = format("%s://%s:%s%s", connectionWithSessionPropertiesURI.getScheme(), connectionWithSessionPropertiesURI.getHost(), connectionWithSessionPropertiesURI.getPort(), connectionWithSessionPropertiesURI.getPath());
+        PrestoDriverUri uri = new PrestoDriverUri(prestoUrl, connectionProperties);
+        PrestoConnection prestoConnection = new PrestoConnection(uri, connectionWithSessionProperties.queryExecutor);
+        copySessionProperties(connectionWithSessionProperties, prestoConnection);
 
-        return connectionWithSessionProperties;
+        return prestoConnection;
+    }
+
+    public static void copySessionProperties(PrestoConnection src, PrestoConnection dst)
+    {
+        requireNonNull(src, "src is null");
+        requireNonNull(dst, "dst is null");
+
+        Map<String, String> map = src.getSessionProperties();
+        map.forEach(dst::setSessionProperty);
     }
 
     @Override
