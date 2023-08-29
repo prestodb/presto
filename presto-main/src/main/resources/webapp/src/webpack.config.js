86f6a64911f72abc6e1e9b8812d7cc686c9f63f2
Yihong Wang <yh.wang@ibm.com>
2023-08-28 17:07:03 -0700

    dashboard: update webpack to v5
    Node v16 is going to sunset on Sep 11 2023. Although
    we only use it to compile the jsx files, it's still good to
    switch to Node v18. Therefore, I also updated the
    followings to newer versions:
    - webpack and webpack-cli
    - babel
    - react
    
    The extra benefit is:
    The size of the new js files is reduced to almost 1/2.
    
    While verifying the change, I also fixed some minor issues in
    StageDetail.jsx and timeline.html.
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- presto-main/src/main/resources/webapp/src/webpack.config.js
+++ presto-main/src/main/resources/webapp/src/webpack.config.js
@@ -1,33 +1,57 @@
 /* global __dirname */
 
-const process = require('process');
+const path = require('node:path');
+const TerserPlugin = require("terser-webpack-plugin");
 
-const mode = process.argv.includes('-p') ? 'production' : 'development';
-
-module.exports = {
-    entry: {
-        'index': `${__dirname}/index.jsx`,
-        'query': `${__dirname}/query.jsx`,
-        'plan': `${__dirname}/plan.jsx`,
-        'embedded_plan': `${__dirname}/embedded_plan.jsx`,
-        'stage': `${__dirname}/stage.jsx`,
-        'worker': `${__dirname}/worker.jsx`,
-    },
-    mode,
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: __dirname + '/../dist',
-        filename: '[name].js'
-    }
+module.exports = (env) => {
+    return {
+        entry: {
+            'index': path.join(__dirname, 'index.jsx'),
+            'query': path.join(__dirname, 'query.jsx'),
+            'plan': path.join(__dirname, 'plan.jsx'),
+            'embedded_plan': path.join(__dirname, 'embedded_plan.jsx'),
+            'stage': path.join(__dirname, 'stage.jsx'),
+            'worker': path.join(__dirname, 'worker.jsx'),
+        },
+        mode: env.production ? 'production' : 'development',
+        module: {
+            rules: [
+                {
+                    test: /\.(?:js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', { targets: "defaults" }],
+                                ['@babel/preset-react'],
+                                ['@babel/preset-flow']
+                            ]
+                        }
+                    }
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx']
+        },
+        output: {
+            path: __dirname + '/../dist',
+            filename: '[name].js'
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [
+              new TerserPlugin({
+                // do not genreate *.LICENSE.txt files
+                terserOptions: {
+                  format: {
+                    comments: false,
+                  },
+                },
+                extractComments: false,
+              }),
+            , '...'],
+          },
+    };
 };
