bed0f848fa2d1709c9a02dc955181e20d3e6092c
oyeliseiev <oyeliseiev-ua@singlestore.com>
2023-09-05 13:18:00 +0300

    Added SingleStore connector

---
+++ presto-singlestore/src/test/java/com/facebook/presto/plugin/singlestore/TestSingleStoreIntegrationSmokeTest.java
@@ -0,0 +1,219 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.plugin.singlestore;
+
+import com.facebook.presto.testing.MaterializedResult;
+import com.facebook.presto.testing.QueryRunner;
+import com.facebook.presto.tests.AbstractTestIntegrationSmokeTest;
+import com.facebook.presto.tests.DistributedQueryRunner;
+import com.google.common.collect.ImmutableMap;
+import org.intellij.lang.annotations.Language;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
+
+import static com.facebook.presto.common.type.VarcharType.VARCHAR;
+import static com.facebook.presto.testing.assertions.Assert.assertEquals;
+import static io.airlift.tpch.TpchTable.ORDERS;
+import static java.lang.String.format;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+@Test
+public class TestSingleStoreIntegrationSmokeTest
+        extends AbstractTestIntegrationSmokeTest
+{
+    private final DockerizedSingleStoreServer singleStoreServer;
+
+    public TestSingleStoreIntegrationSmokeTest()
+    {
+        this.singleStoreServer = new DockerizedSingleStoreServer();
+    }
+
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
+    {
+        return SingleStoreQueryRunner.createSingleStoreQueryRunner(singleStoreServer, ORDERS);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public final void destroy()
+    {
+        singleStoreServer.close();
+    }
+
+    @Override
+    public void testDescribeTable()
+    {
+        // we need specific implementation of this tests due to specific Presto<->SingleStore varchar length mapping.
+        MaterializedResult actualColumns = computeActual("DESC ORDERS").toTestTypes();
+
+        MaterializedResult expectedColumns = MaterializedResult.resultBuilder(getQueryRunner().getDefaultSession(), VARCHAR, VARCHAR, VARCHAR, VARCHAR)
+                .row("orderkey", "bigint", "", "")
+                .row("custkey", "bigint", "", "")
+                .row("orderstatus", "varchar(85)", "", "")//utf-8
+                .row("totalprice", "double", "", "")
+                .row("orderdate", "date", "", "")
+                .row("orderpriority", "varchar(85)", "", "")
+                .row("clerk", "varchar(85)", "", "")
+                .row("shippriority", "integer", "", "")
+                .row("comment", "varchar(85)", "", "")
+                .build();
+        assertEquals(actualColumns, expectedColumns);
+    }
+
+    @Test
+    public void testDropTable()
+    {
+        assertUpdate("CREATE TABLE test_drop AS SELECT 123 x", 1);
+        assertTrue(getQueryRunner().tableExists(getSession(), "test_drop"));
+
+        assertUpdate("DROP TABLE test_drop");
+        assertFalse(getQueryRunner().tableExists(getSession(), "test_drop"));
+    }
+
+    @Test
+    public void testViews()
+            throws SQLException
+    {
+        execute("CREATE VIEW tpch.test_view AS SELECT * FROM tpch.orders");
+        assertQuery("SELECT orderkey FROM test_view", "SELECT orderkey FROM orders");
+        execute("DROP VIEW IF EXISTS tpch.test_view");
+    }
+
+    @Test
+    public void testInsert()
+            throws Exception
+    {
+        execute("CREATE TABLE tpch.test_insert (x bigint, y varchar(100))");
+        assertUpdate("INSERT INTO test_insert VALUES (123, 'test')", 1);
+        assertQuery("SELECT * FROM test_insert", "SELECT 123 x, 'test' y");
+        assertUpdate("DROP TABLE test_insert");
+    }
+
+    @Test
+    public void testCharTrailingSpace()
+            throws Exception
+    {
+        execute("CREATE TABLE tpch.char_trailing_space (x char(10))");
+        assertUpdate("INSERT INTO char_trailing_space VALUES ('test')", 1);
+
+        assertQuery("SELECT * FROM char_trailing_space WHERE x = char 'test'", "VALUES 'test'");
+        assertQuery("SELECT * FROM char_trailing_space WHERE x = char 'test  '", "VALUES 'test'");
+        assertQuery("SELECT * FROM char_trailing_space WHERE x = char 'test        '", "VALUES 'test'");
+
+        assertEquals(getQueryRunner().execute("SELECT * FROM char_trailing_space WHERE x = char ' test'").getRowCount(), 0);
+
+        Map<String, String> properties = ImmutableMap.of("deprecated.legacy-char-to-varchar-coercion", "true");
+        Map<String, String> connectorProperties = ImmutableMap.of("connection-url", singleStoreServer.getJdbcUrl());
+
+        try (QueryRunner queryRunner = new DistributedQueryRunner(getSession(), 3, properties);) {
+            queryRunner.installPlugin(new SingleStorePlugin());
+            queryRunner.createCatalog("singlestore", "singlestore", connectorProperties);
+
+            assertEquals(queryRunner.execute("SELECT * FROM char_trailing_space WHERE x = char 'test'").getRowCount(), 0);
+            assertEquals(queryRunner.execute("SELECT * FROM char_trailing_space WHERE x = char 'test  '").getRowCount(), 0);
+            assertEquals(queryRunner.execute("SELECT * FROM char_trailing_space WHERE x = char 'test       '").getRowCount(), 0);
+
+            MaterializedResult result = queryRunner.execute("SELECT * FROM char_trailing_space WHERE x = char 'test      '");
+            assertEquals(result.getRowCount(), 1);
+            assertEquals(result.getMaterializedRows().get(0).getField(0), "test      ");
+        }
+
+        assertUpdate("DROP TABLE char_trailing_space");
+    }
+
+    @Test
+    public void testInsertIntoNotNullColumn()
+    {
+        String createTableFormat = "CREATE TABLE %s.tpch.test_insert_not_null (\n" +
+                "   %s date,\n" +
+                "   %s date NOT NULL\n" +
+                ")";
+        @Language("SQL") String createTableSql = format(
+                createTableFormat,
+                getSession().getCatalog().get(),
+                "column_a",
+                "column_b");
+        @Language("SQL") String expectedCreateTableSql = format(
+                createTableFormat,
+                getSession().getCatalog().get(),
+                "\"column_a\"",
+                "\"column_b\"");
+        assertUpdate(createTableSql);
+        assertEquals(computeScalar("SHOW CREATE TABLE test_insert_not_null"), expectedCreateTableSql);
+
+        assertQueryFails("INSERT INTO test_insert_not_null (column_a) VALUES (date '2012-12-31')", "NULL value not allowed for NOT NULL column: column_b");
+        assertQueryFails("INSERT INTO test_insert_not_null (column_a, column_b) VALUES (date '2012-12-31', null)", "NULL value not allowed for NOT NULL column: column_b");
+
+        assertUpdate("ALTER TABLE test_insert_not_null ADD COLUMN column_c BIGINT NOT NULL");
+
+        createTableFormat = "CREATE TABLE %s.tpch.test_insert_not_null (\n" +
+                "   %s date,\n" +
+                "   %s date NOT NULL,\n" +
+                "   %s bigint NOT NULL\n" +
+                ")";
+        createTableSql = format(
+                createTableFormat,
+                getSession().getCatalog().get(),
+                "column_a",
+                "column_b",
+                "column_c");
+        expectedCreateTableSql = format(
+                createTableFormat,
+                getSession().getCatalog().get(),
+                "\"column_a\"",
+                "\"column_b\"",
+                "\"column_c\"");
+        assertEquals(computeScalar("SHOW CREATE TABLE test_insert_not_null"), expectedCreateTableSql);
+
+        assertQueryFails("INSERT INTO test_insert_not_null (column_b) VALUES (date '2012-12-31')", "NULL value not allowed for NOT NULL column: column_c");
+        assertQueryFails("INSERT INTO test_insert_not_null (column_b, column_c) VALUES (date '2012-12-31', null)", "NULL value not allowed for NOT NULL column: column_c");
+
+        assertUpdate("INSERT INTO test_insert_not_null (column_b, column_c) VALUES (date '2012-12-31', 1)", 1);
+        assertUpdate("INSERT INTO test_insert_not_null (column_a, column_b, column_c) VALUES (date '2013-01-01', date '2013-01-02', 2)", 1);
+        assertQuery(
+                "SELECT * FROM test_insert_not_null",
+                "VALUES (NULL, CAST('2012-12-31' AS DATE), 1), (CAST('2013-01-01' AS DATE), CAST('2013-01-02' AS DATE), 2)");
+
+        assertUpdate("DROP TABLE test_insert_not_null");
+    }
+
+    @Test
+    public void testColumnComment()
+            throws Exception
+    {
+        execute("create table tpch.test_column_comment (column_a char(3) comment 'first field', column_b int comment '', column_c int)");
+        assertQuery(
+                "SELECT column_name, comment FROM information_schema.columns WHERE table_schema = 'tpch' AND table_name = 'test_column_comment'",
+                "VALUES ('column_a', 'first field'), ('column_b', null), ('column_c', null)");
+
+        assertUpdate("DROP TABLE test_column_comment");
+    }
+
+    private void execute(String sql)
+            throws SQLException
+    {
+        try (Connection connection = DriverManager.getConnection(singleStoreServer.getJdbcUrl());
+                Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+    }
+}
