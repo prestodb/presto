bee5dd7430826304a72eac1ce4299288ddabe16f
feilong-liu <feilongliu@meta.com>
2023-09-13 15:25:19 -0700

    Add optimizer information to QueryInfo for Presto On Spark

--- presto-spark-base/src/main/java/com/facebook/presto/spark/PrestoSparkQueryExecutionFactory.java
+++ presto-spark-base/src/main/java/com/facebook/presto/spark/PrestoSparkQueryExecutionFactory.java
@@ -372,7 +372,7 @@ public class PrestoSparkQueryExecutionFactory
                 ImmutableMap.of(),
                 ImmutableSet.of(),
                 planAndMore.map(PlanAndMore::getPlan).map(Plan::getStatsAndCosts).orElseGet(StatsAndCosts::empty),
-                ImmutableList.of(),
+                session.getOptimizerInformationCollector().getOptimizationInfo(),
                 ImmutableList.of(),
                 planAndMore.map(PlanAndMore::getInvokedScalarFunctions).orElseGet(ImmutableSet::of),
                 planAndMore.map(PlanAndMore::getInvokedAggregateFunctions).orElseGet(ImmutableSet::of),
