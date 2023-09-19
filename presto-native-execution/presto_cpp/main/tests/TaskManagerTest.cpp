8c38dc738fd2fac946ac68b7338cb48e6cf62d5a
Amit Dutta <amit.kolorob@gmail.com>
2023-09-18 18:41:21 -0700

    [native] Use presto_native as prefix for spill path.
    Instead of putting date as prefix of spill path, using presto-native as its easier to manager for remote storage.

--- presto-native-execution/presto_cpp/main/tests/TaskManagerTest.cpp
+++ presto-native-execution/presto_cpp/main/tests/TaskManagerTest.cpp
@@ -937,11 +937,11 @@ TEST_F(TaskManagerTest, aggregationSpill) {
 
 TEST_F(TaskManagerTest, buildTaskSpillDirectoryPath) {
   EXPECT_EQ(
-      "fs::/base/192.168.10.2_19/2022-12-20/presto_native/20221220-Q/Task1/",
+      "fs::/base/presto_native/192.168.10.2_19/2022-12-20/20221220-Q/Task1/",
       TaskManager::buildTaskSpillDirectoryPath(
           "fs::/base", "192.168.10.2", "19", "20221220-Q", "Task1", true));
   EXPECT_EQ(
-      "fsx::/root/192.16.10.2_sample_node_id/1970-01-01/presto_native/Q100/Task22/",
+      "fsx::/root/presto_native/192.16.10.2_sample_node_id/1970-01-01/Q100/Task22/",
       TaskManager::buildTaskSpillDirectoryPath(
           "fsx::/root",
           "192.16.10.2",
@@ -950,11 +950,11 @@ TEST_F(TaskManagerTest, buildTaskSpillDirectoryPath) {
           "Task22",
           true));
   EXPECT_EQ(
-      "fs::/base/2022-12-20/presto_native/20221220-Q/Task1/",
+      "fs::/base/presto_native/2022-12-20/20221220-Q/Task1/",
       TaskManager::buildTaskSpillDirectoryPath(
           "fs::/base", "192.168.10.2", "19", "20221220-Q", "Task1", false));
   EXPECT_EQ(
-      "fsx::/root/1970-01-01/presto_native/Q100/Task22/",
+      "fsx::/root/presto_native/1970-01-01/Q100/Task22/",
       TaskManager::buildTaskSpillDirectoryPath(
           "fsx::/root",
           "192.16.10.2",
