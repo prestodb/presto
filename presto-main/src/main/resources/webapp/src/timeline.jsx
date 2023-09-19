9b58a78327d9507ff4aad1a33dbc8fd253b88c6f
Yihong Wang <yh.wang@ibm.com>
2023-09-19 16:59:55 -0700

    Add the resource group page
    Add the `Resource Groups` tab on the index page to
    show the resource groups' information. Also, update
    the QueryList to have the group id as links which
    points to the new resource groups page.
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- presto-main/src/main/resources/webapp/src/timeline.jsx
+++ presto-main/src/main/resources/webapp/src/timeline.jsx
@@ -3,7 +3,7 @@ import { PageTitle } from "./components/PageTitle";
 import { createRoot } from 'react-dom/client';
 
 const title = createRoot(document.getElementById('title'));
-title.render(<PageTitle title="Timeline" />);
+title.render(<PageTitle titles={["Timeline"]} />);
 
 const timeline = createRoot(document.getElementById('timeline'));
 timeline.render(<Splits />);
