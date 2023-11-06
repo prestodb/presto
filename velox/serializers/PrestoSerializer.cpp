9dbf761a8ce205ef2c6aa204064a6152b4176f2a
Jake Jung <jakej@meta.com>
2023-11-06 08:06:52 -0800

    Remove string copy (#7287)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/7287
    
    - Remove unnecessary copy of `std::string`
    - Remove unnecessary copy of a `shared_ptr`
    
    Reviewed By: rukshanperera, abhash09, Sullivan-Patrick
    
    Differential Revision: D50755272
    
    fbshipit-source-id: c016af3d423893be86c1a80d6a2704e8541dc3ec

--- velox/serializers/PrestoSerializer.cpp
+++ velox/serializers/PrestoSerializer.cpp
@@ -831,7 +831,7 @@ std::string readLengthPrefixedString(ByteStream* source) {
   return value;
 }
 
-void checkTypeEncoding(std::string encoding, TypePtr type) {
+void checkTypeEncoding(std::string_view encoding, const TypePtr& type) {
   auto kindEncoding = typeToEncodingName(type);
   VELOX_CHECK(
       encoding == kindEncoding,
