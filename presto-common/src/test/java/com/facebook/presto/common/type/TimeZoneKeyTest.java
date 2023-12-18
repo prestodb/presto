d894e7ede334c59a428b5d600db890d3bcfb6695
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2023-12-18 20:13:32 +0100

    Replaced the left curly bracket with a new line

--- presto-common/src/test/java/com/facebook/presto/common/type/TimeZoneKeyTest.java
+++ presto-common/src/test/java/com/facebook/presto/common/type/TimeZoneKeyTest.java
@@ -24,7 +24,8 @@ import static org.assertj.core.api.Fail.fail;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class TimeZoneKeyTest {
+public class TimeZoneKeyTest
+{
     TimeZoneKey timeZoneKey = new TimeZoneKey("1", (short) 1);
     Class<?> clazz = TimeZoneKey.class;
     Method normalizeZoneId;
