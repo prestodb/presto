5f3d38ec8bb52c0316a0514f195e68ed860a2943
Pramod Satya <pramod.satya@ibm.com>
2023-12-20 06:21:44 -0800

    [native] Move presto to velox property mapping to SessionProperties
    Co-authored-by: Abe Varghese Kodiyan <abe.varghese@ibm.com>
    Co-authored-by: Joe Abraham <joe.abraham@ibm.com>
    Co-authored-by: Deepthy Davis <deepthy.davis@ibm.com>

--- presto-native-execution/presto_cpp/main/QueryContextManager.cpp
+++ presto-native-execution/presto_cpp/main/QueryContextManager.cpp
@@ -14,6 +14,7 @@
 
 #include "presto_cpp/main/QueryContextManager.h"
 #include <folly/executors/IOThreadPoolExecutor.h>
+#include "presto_cpp/main/SessionProperties.h"
 #include "presto_cpp/main/common/Configs.h"
 #include "presto_cpp/main/common/Counters.h"
 #include "velox/common/base/StatsReporter.h"
@@ -27,36 +28,6 @@ using facebook::presto::protocol::TaskId;
 
 namespace facebook::presto {
 namespace {
-// Utility function to translate a config name in Presto to its equivalent in
-// Velox. Returns 'name' as is if there is no mapping.
-std::string toVeloxConfig(const std::string& name) {
-  using velox::core::QueryConfig;
-  static const folly::F14FastMap<std::string, std::string>
-      kPrestoToVeloxMapping = {
-          {"native_simplified_expression_evaluation_enabled",
-           QueryConfig::kExprEvalSimplified},
-          {"native_max_spill_level", QueryConfig::kMaxSpillLevel},
-          {"native_max_spill_file_size", QueryConfig::kMaxSpillFileSize},
-          {"native_spill_compression_codec",
-           QueryConfig::kSpillCompressionKind},
-          {"native_spill_write_buffer_size",
-           QueryConfig::kSpillWriteBufferSize},
-          {"native_spill_file_create_config",
-           QueryConfig::kSpillFileCreateConfig},
-          {"native_join_spill_enabled", QueryConfig::kJoinSpillEnabled},
-          {"native_window_spill_enabled", QueryConfig::kWindowSpillEnabled},
-          {"native_writer_spill_enabled", QueryConfig::kWriterSpillEnabled},
-          {"native_row_number_spill_enabled",
-           QueryConfig::kRowNumberSpillEnabled},
-          {"native_spiller_num_partition_bits",
-           QueryConfig::kSpillNumPartitionBits},
-          {"native_topn_row_number_spill_enabled",
-           QueryConfig::kTopNRowNumberSpillEnabled},
-          {"native_debug_validate_output_from_operators",
-           QueryConfig::kValidateOutputFromOperators}};
-  auto it = kPrestoToVeloxMapping.find(name);
-  return it == kPrestoToVeloxMapping.end() ? name : it->second;
-}
 
 // Update passed in query session configs with system configs. For any pairing
 // system/session configs if session config is present, it overrides system
@@ -89,12 +60,13 @@ void updateFromSystemConfigs(
 }
 
 std::unordered_map<std::string, std::string> toVeloxConfigs(
-    const protocol::SessionRepresentation& session) {
+    const protocol::SessionRepresentation& session,
+    const std::shared_ptr<SessionProperties>& sessionProperties) {
   // Use base velox query config as the starting point and add Presto session
   // properties on top of it.
   auto configs = BaseVeloxQueryConfig::instance()->values();
   for (const auto& it : session.systemProperties) {
-    configs[toVeloxConfig(it.first)] = it.second;
+    configs[sessionProperties->toVeloxConfig(it.first)] = it.second;
   }
 
   // If there's a timeZoneKey, convert to timezone name and add to the
@@ -142,19 +114,24 @@ void updateVeloxConfigs(
         core::QueryConfig::kDriverCpuTimeSliceLimitMs, "1000");
   }
 }
+
 } // namespace
 
 QueryContextManager::QueryContextManager(
     folly::Executor* driverExecutor,
     folly::Executor* spillerExecutor)
-    : driverExecutor_(driverExecutor), spillerExecutor_(spillerExecutor) {}
+    : driverExecutor_(driverExecutor),
+      spillerExecutor_(spillerExecutor),
+      sessionProperties_(std::make_shared<SessionProperties>()) {}
 
 std::shared_ptr<velox::core::QueryCtx>
 QueryContextManager::findOrCreateQueryCtx(
     const protocol::TaskId& taskId,
     const protocol::SessionRepresentation& session) {
   return findOrCreateQueryCtx(
-      taskId, toVeloxConfigs(session), toConnectorConfigs(session));
+      taskId,
+      toVeloxConfigs(session, sessionProperties_),
+      toConnectorConfigs(session));
 }
 
 std::shared_ptr<core::QueryCtx> QueryContextManager::findOrCreateQueryCtx(
