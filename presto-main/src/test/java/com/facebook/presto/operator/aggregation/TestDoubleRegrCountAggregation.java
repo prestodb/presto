d0b658e483a63914177a5e643b5e60dc6c649b4d
8dukongjian <2580053843@qq.com>
2024-01-04 14:49:01 +0800

    Add additional linear regression functions
    Add REGR_AVGX, REGR_AVGY, REGR_COUNT, REGR_R2, REGR_SXX, REGR_SXY, and REGR_SYY.

---
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestDoubleRegrCountAggregation.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator.aggregation;
+
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
+import static com.facebook.presto.block.BlockAssertions.createDoublesBlock;
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class TestDoubleRegrCountAggregation
+        extends AbstractTestDoubleRegrAggregationFunction
+{
+    @Override
+    protected String getFunctionName()
+    {
+        return "regr_count";
+    }
+
+    @Override
+    public Object getExpectedValue(int start, int length)
+    {
+        if (length <= 1) {
+            return (double) length;
+        }
+        else {
+            SimpleRegression regression = new SimpleRegression();
+            for (int i = start; i < start + length; i++) {
+                regression.addData(i + 2, i);
+            }
+            return (double) regression.getN();
+        }
+    }
+
+    @Override
+    protected void testNonTrivialAggregation(Double[] y, Double[] x)
+    {
+        SimpleRegression regression = new SimpleRegression();
+        for (int i = 0; i < x.length; i++) {
+            regression.addData(x[i], y[i]);
+        }
+        double expected = (double) regression.getN();
+        checkArgument(Double.isFinite(expected) && expected != 0.0f, "Expected result is trivial");
+        testAggregation(expected, createDoublesBlock(y), createDoublesBlock(x));
+    }
+}
