ebefc6a8e904e653b966e9c848a0e6ea021eba98
Masha Basmanova <mbasmanova@meta.com>
2024-01-09 18:19:59 -0800

    Run null-aware right semi project join single-threaded in Fuzzer (#8317)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/8317
    
    Null-aware right semi project join doesn't support multi-threaded execution.
    Update JoinFuzzer to run such joins single-threaded.
    
    Fixes https://github.com/facebookincubator/velox/issues/8018
    
    Reviewed By: xiaoxmeng
    
    Differential Revision: D52645117
    
    fbshipit-source-id: 99f34fe530a7ce36143c1f282c87d0ef2e2640a2

--- velox/exec/tests/JoinFuzzer.cpp
+++ velox/exec/tests/JoinFuzzer.cpp
@@ -277,9 +277,22 @@ std::vector<RowVectorPtr> flatten(const std::vector<RowVectorPtr>& vectors) {
   return flatVectors;
 }
 
+bool isNullAwareRightSemiProjectJoin(const core::PlanNodePtr& plan) {
+  if (auto joinNode = dynamic_cast<const core::HashJoinNode*>(plan.get())) {
+    return joinNode->isNullAware() &&
+        joinNode->joinType() == core::JoinType::kRightSemiProject;
+  }
+
+  return false;
+}
+
 RowVectorPtr JoinFuzzer::execute(const PlanWithSplits& plan, bool injectSpill) {
   LOG(INFO) << "Executing query plan: " << std::endl
             << plan.plan->toString(true, true);
+
+  // Null-aware right semi project join doesn't support multi-threaded
+  // execution.
+  const int maxDrivers = isNullAwareRightSemiProjectJoin(plan.plan) ? 1 : 2;
 
   AssertQueryBuilder builder(plan.plan);
   for (const auto& [nodeId, nodeSplits] : plan.splits) {
@@ -295,7 +308,7 @@ RowVectorPtr JoinFuzzer::execute(const PlanWithSplits& plan, bool injectSpill) {
         .spillDirectory(spillDirectory->path);
   }
 
-  auto result = builder.maxDrivers(2).copyResults(pool_.get());
+  auto result = builder.maxDrivers(maxDrivers).copyResults(pool_.get());
   LOG(INFO) << "Results: " << result->toString();
   if (VLOG_IS_ON(1)) {
     VLOG(1) << std::endl << result->toString(0, result->size());
