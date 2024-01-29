f3b91e36aff65a9e4a4c4f917a98ba84cb396582
Yedidya Feldblum <yfeldblum@meta.com>
2024-01-29 08:06:49 -0800

    migrate from FOLLY_FALLTHROUGH to [[fallthrough]] (assorted) (#8558)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/8558
    
    The attribute `[[fallthrough]]` is standard C++17. We no longer need to wrap it.
    
    Reviewed By: Orvid
    
    Differential Revision: D53034074
    
    fbshipit-source-id: 19b1e8314e70f3e08882e76b93f7ded4df7ab9f2

--- velox/exec/HashBuild.cpp
+++ velox/exec/HashBuild.cpp
@@ -986,7 +986,7 @@ BlockingReason HashBuild::isBlocked(ContinueFuture* future) {
       }
       break;
     case State::kWaitForBuild:
-      FOLLY_FALLTHROUGH;
+      [[fallthrough]];
     case State::kWaitForProbe:
       if (!future_.valid()) {
         setRunning();
@@ -1036,11 +1036,11 @@ void HashBuild::checkStateTransition(State state) {
       }
       break;
     case State::kWaitForBuild:
-      FOLLY_FALLTHROUGH;
+      [[fallthrough]];
     case State::kWaitForSpill:
-      FOLLY_FALLTHROUGH;
+      [[fallthrough]];
     case State::kWaitForProbe:
-      FOLLY_FALLTHROUGH;
+      [[fallthrough]];
     case State::kFinish:
       VELOX_CHECK_EQ(state_, State::kRunning);
       break;
