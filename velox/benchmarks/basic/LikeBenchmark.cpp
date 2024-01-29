c1964974f7b1a47263df1733b25e857ae043cd86
Masha Basmanova <mbasmanova@meta.com>
2024-01-29 15:45:43 -0800

    Back out "Optimize LIKE for more relaxed patterns" (#8585)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/8585
    
    See https://github.com/facebookincubator/velox/pull/8050?fbclid=IwAR09vWcGL1a4gg08AJyWhbAiu5qgCxPGXeArXRlZug-hRT1T0AOG-Qc7lVA#issuecomment-1915373228
    
    Original commit changeset: c307f3f93949
    
    Original Phabricator Diff: D52312645
    
    Reviewed By: xiaoxmeng, bikramSingh91
    
    Differential Revision: D53190666
    
    fbshipit-source-id: 3b6b741b812cc3aa0f08584fde7d3106d4e2ba28

--- velox/benchmarks/basic/LikeBenchmark.cpp
+++ velox/benchmarks/basic/LikeBenchmark.cpp
@@ -35,87 +35,59 @@ int main(int argc, char** argv) {
   // Register the scalar functions.
   prestosql::registerAllScalarFunctions("");
 
+  // exec::register
   ExpressionBenchmarkBuilder benchmarkBuilder;
   const vector_size_t vectorSize = 1000;
   auto vectorMaker = benchmarkBuilder.vectorMaker();
 
-  auto makeInput = [&](vector_size_t vectorSize,
-                       bool padAtHead,
-                       bool padAtTail,
-                       std::string content = "a_b_c",
-                       std::string paddingStr = "xxx") {
-    return vectorMaker.flatVector<std::string>(vectorSize, [&](auto row) {
-      // Strings in even rows contain/start with/end with a_b_c depends on
-      // value of padAtHead && padAtTail.
-
-      // Calculates the padding.
-      std::ostringstream os;
-      for (auto i = 0; i < row / 2 + 1; ++i) {
-        os << paddingStr;
-      }
-      auto padding = os.str();
-
-      if (row % 2 == 0) {
-        if (padAtHead && padAtTail) {
-          return fmt::format("{}{}{}", padding, content, padding);
-        } else if (padAtHead) {
-          return fmt::format("{}{}", padding, content);
-        } else if (padAtTail) {
-          return fmt::format("{}{}", content, padding);
-        } else {
-          return content;
-        }
-      } else {
-        // Yes, two padding concatenated, since we have a '/2' above.
-        return padding + padding;
-      }
-    });
-  };
+  auto makeInput =
+      [&](vector_size_t vectorSize, bool padAtHead, bool padAtTail) {
+        return vectorMaker.flatVector<std::string>(vectorSize, [&](auto row) {
+          // Strings in even rows contain/start with/end with a_b_c depends on
+          // value of padAtHead && padAtTail.
+          if (row % 2 == 0) {
+            auto padding = std::string(row / 2 + 1, 'x');
+            if (padAtHead && padAtTail) {
+              return fmt::format("{}a_b_c{}", padding, padding);
+            } else if (padAtHead) {
+              return fmt::format("{}a_b_c", padding);
+            } else if (padAtTail) {
+              return fmt::format("a_b_c{}", padding);
+            } else {
+              return std::string("a_b_c");
+            }
+          } else {
+            return std::string(row, 'x');
+          }
+        });
+      };
 
   auto substringInput = makeInput(vectorSize, true, true);
   auto prefixInput = makeInput(vectorSize, false, true);
-  auto prefixUnicodeInput = makeInput(vectorSize, false, true, "你_好_啊");
   auto suffixInput = makeInput(vectorSize, true, false);
-  auto suffixUnicodeInput = makeInput(vectorSize, true, false, "你_好_啊");
 
   benchmarkBuilder
       .addBenchmarkSet(
-          "substring", vectorMaker.rowVector({"col0"}, {substringInput}))
-      .addExpression("substring", R"(like(col0, '%a\_b\_c%', '\'))")
+          "like_substring", vectorMaker.rowVector({"col0"}, {substringInput}))
+      .addExpression("like_substring", R"(like(col0, '%a\_b\_c%', '\'))")
       .addExpression("strpos", R"(strpos(col0, 'a_b_c') > 0)");
 
   benchmarkBuilder
       .addBenchmarkSet(
-          "prefix",
-          vectorMaker.rowVector(
-              {"col0", "col1"}, {prefixInput, prefixUnicodeInput}))
-      .addExpression("prefix", R"(like(col0, 'a\_b\_c%', '\'))")
-      .addExpression("relaxed_prefix_1", R"(like(col0, 'a\__\_c%', '\'))")
-      .addExpression("relaxed_prefix_2", R"(like(col0, '_\__\_c%', '\'))")
-      .addExpression(
-          "relaxed_prefix_unicode_1", R"(like(col1, '你\__\_啊%', '\'))")
-      .addExpression(
-          "relaxed_prefix_unicode_2", R"(like(col1, '_\__\_啊%', '\'))")
+          "like_prefix", vectorMaker.rowVector({"col0"}, {prefixInput}))
+      .addExpression("like_prefix", R"(like(col0, 'a\_b\_c%', '\'))")
       .addExpression("starts_with", R"(starts_with(col0, 'a_b_c'))");
 
   benchmarkBuilder
       .addBenchmarkSet(
-          "suffix",
-          vectorMaker.rowVector(
-              {"col0", "col1"}, {suffixInput, suffixUnicodeInput}))
-      .addExpression("suffix", R"(like(col0, '%a\_b\_c', '\'))")
-      .addExpression("relaxed_suffix_1", R"(like(col0, '%a\__\_c', '\'))")
-      .addExpression("relaxed_suffix_2", R"(like(col0, '%_\__\_c', '\'))")
-      .addExpression(
-          "relaxed_suffix_unicode_1", R"(like(col1, '%你\__\_啊', '\'))")
-      .addExpression(
-          "relaxed_suffix_unicode_2", R"(like(col1, '%_\__\_啊', '\'))")
+          "like_suffix", vectorMaker.rowVector({"col0"}, {suffixInput}))
+      .addExpression("like_suffix", R"(like(col0, '%a\_b\_c', '\'))")
       .addExpression("ends_with", R"(ends_with(col0, 'a_b_c'))");
 
   benchmarkBuilder
       .addBenchmarkSet(
-          "generic", vectorMaker.rowVector({"col0"}, {substringInput}))
-      .addExpression("generic", R"(like(col0, '%a%b%c'))");
+          "like_generic", vectorMaker.rowVector({"col0"}, {substringInput}))
+      .addExpression("like_generic", R"(like(col0, '%a%b%c'))");
 
   benchmarkBuilder.registerBenchmarks();
   benchmarkBuilder.testBenchmarks();
