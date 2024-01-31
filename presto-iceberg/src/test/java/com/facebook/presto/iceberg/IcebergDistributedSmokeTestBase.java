4688b92e796e8856332740c9733f8e8a66f832e7
Deepa Mariam George <121156476+dmariamgeorge@users.noreply.github.com>
2024-01-31 18:31:20 +0530

    Added fix insert into statement error for column having white spaces
    Added fix insert into statement error for column having white spaces in Iceberg Connector
    
    Added unit testcases for the fix
    
    Fixed failing unit testcase

--- presto-iceberg/src/test/java/com/facebook/presto/iceberg/IcebergDistributedSmokeTestBase.java
+++ presto-iceberg/src/test/java/com/facebook/presto/iceberg/IcebergDistributedSmokeTestBase.java
@@ -1232,4 +1232,14 @@ public class IcebergDistributedSmokeTestBase
         assertUpdate("CALL system.unregister_table('" + schemaName + "', '" + tableName + "')");
         assertFalse(getQueryRunner().tableExists(getSession(), tableName));
     }
+
+    @Test
+    public void testColumnNameWithSpace()
+    {
+        String tableName = "test_column_name_with_space";
+        assertUpdate(format("CREATE TABLE %s ('column a' CHAR(5))", tableName));
+        assertUpdate(format("INSERT INTO %s VALUES ('abcde'), ('12345')", tableName), 2);
+        assertQuery(format("SELECT * FROM %s", tableName), "VALUES ('abcde'), ('12345')");
+        dropTable(getSession(), tableName);
+    }
 }
