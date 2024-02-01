efd604aa7dd00fb7faa7f0b8d1474777abc3828b
Ke <ke.404notfound@gmail.com>
2024-01-31 16:48:58 -0800

    Add spill related session property for native engine

--- presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
+++ presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
@@ -328,6 +328,7 @@ public final class SystemSessionProperties
     public static final String NATIVE_WRITER_SPILL_ENABLED = "native_writer_spill_enabled";
     public static final String NATIVE_ROW_NUMBER_SPILL_ENABLED = "native_row_number_spill_enabled";
     public static final String NATIVE_TOPN_ROW_NUMBER_SPILL_ENABLED = "native_topn_row_number_spill_enabled";
+    public static final String NATIVE_JOIN_SPILLER_PARTITION_BITS = "native_join_spiller_partition_bits";
     public static final String NATIVE_EXECUTION_ENABLED = "native_execution_enabled";
     public static final String NATIVE_EXECUTION_EXECUTABLE_PATH = "native_execution_executable_path";
     public static final String NATIVE_EXECUTION_PROGRAM_ARGUMENTS = "native_execution_program_arguments";
@@ -1634,6 +1635,12 @@ public final class SystemSessionProperties
                         NATIVE_TOPN_ROW_NUMBER_SPILL_ENABLED,
                         "Native Execution only. Enable topN row number spilling on native engine",
                         false,
+                        false),
+                integerProperty(
+                        NATIVE_JOIN_SPILLER_PARTITION_BITS,
+                        "Native Execution only. The number of bits (N) used to calculate the " +
+                                "spilling partition number for hash join and RowNumber: 2 ^ N",
+                        2,
                         false),
                 booleanProperty(
                         NATIVE_EXECUTION_ENABLED,
