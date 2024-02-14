fb8f04dca391362c1f998476f46aaf3f7631febd
Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>
2024-02-14 17:53:00 +0530

    Refactor base lakehouse connector classes in presto-hive-common
    As we no longer need to serialise the BaseHiveTableLayoutHandle, BaseHiveColumnHandle, BaseHiveTableHandle objects, this commit removes the JSON constructors from the mentioned classes

--- presto-hive-common/src/main/java/com/facebook/presto/hive/BaseHiveTableHandle.java
+++ presto-hive-common/src/main/java/com/facebook/presto/hive/BaseHiveTableHandle.java
@@ -15,7 +15,6 @@ package com.facebook.presto.hive;
 
 import com.facebook.presto.spi.ConnectorTableHandle;
 import com.facebook.presto.spi.SchemaTableName;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import static java.util.Objects.requireNonNull;
@@ -26,10 +25,7 @@ public class BaseHiveTableHandle
     private final String schemaName;
     private final String tableName;
 
-    @JsonCreator
-    public BaseHiveTableHandle(
-            @JsonProperty("schemaName") String schemaName,
-            @JsonProperty("tableName") String tableName)
+    public BaseHiveTableHandle(String schemaName, String tableName)
     {
         this.schemaName = requireNonNull(schemaName, "schemaName is null");
         this.tableName = requireNonNull(tableName, "tableName is null");
