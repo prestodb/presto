325f63089c23b9f8707e26de47ca2ab8fb51febd
Deepak Majeti <deepak.majeti@ibm.com>
2024-02-21 14:09:03 +0530

    [native] Remove bison dependency from setup-macos.sh
    bison is installed in the velox setup script

--- presto-native-execution/scripts/setup-macos.sh
+++ presto-native-execution/scripts/setup-macos.sh
@@ -16,9 +16,7 @@ set -eufx -o pipefail
 # Run the velox setup script first.
 source "$(dirname "${BASH_SOURCE}")/../velox/scripts/setup-macos.sh"
 
-MACOS_DEPS="${MACOS_DEPS} bison"
 export FB_OS_VERSION=v2023.12.04.00
-
 export PATH=$(brew --prefix bison)/bin:$PATH
 
 function install_proxygen {
