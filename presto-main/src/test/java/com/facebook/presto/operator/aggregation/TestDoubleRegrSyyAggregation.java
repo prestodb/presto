d7e1f54c54565593ae66857ded889d992c3c7d3f
8dukongjian <2580053843@qq.com>
2024-03-08 14:31:39 +0800

    Fix regr functions result to be null when the input data is null

--- presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestDoubleRegrSyyAggregation.java
+++ presto-main/src/test/java/com/facebook/presto/operator/aggregation/TestDoubleRegrSyyAggregation.java
@@ -30,7 +30,10 @@ public class TestDoubleRegrSyyAggregation
     @Override
     public Object getExpectedValue(int start, int length)
     {
-        if (length <= 1) {
+        if (length == 0) {
+            return null;
+        }
+        else if (length <= 1) {
             return (double) 0;
         }
         else {
