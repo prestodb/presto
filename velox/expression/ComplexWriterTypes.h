0e1b903894c3d61d5825acd5fdbbd89527d46866
zky.zhoukeyong <zky.zhoukeyong@alibaba-inc.com>
2024-03-13 16:25:34 -0700

    Remove unnecessary check in provide_std_interface in ComplexWriterTypes.h (#8669)
    Summary:
    Since `SimpleTypeTrait<Generic<T, comparable, orderable>>::isPrimitiveType` is false, then `!std::is_same_v<Any, V>` is unnecessary. Same for `!isOpaqueType<current_t>::value`.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/8669
    
    Reviewed By: Yuhta
    
    Differential Revision: D54841382
    
    Pulled By: mbasmanova
    
    fbshipit-source-id: c76cc8efd79fb136febc4c89d8bd19d8dbb63cc9

--- velox/expression/ComplexWriterTypes.h
+++ velox/expression/ComplexWriterTypes.h
@@ -91,9 +91,8 @@ struct PrimitiveWriter {
 };
 
 template <typename V>
-bool constexpr provide_std_interface =
-    SimpleTypeTrait<V>::isPrimitiveType && !std::is_same_v<Varchar, V> &&
-    !std::is_same_v<Varbinary, V> && !std::is_same_v<Any, V>;
+bool constexpr provide_std_interface = SimpleTypeTrait<V>::isPrimitiveType &&
+    !std::is_same_v<Varchar, V> && !std::is_same_v<Varbinary, V>;
 
 // bool is an exception, it requires commit but also provides std::interface.
 template <typename V>
@@ -981,9 +980,7 @@ class RowWriter {
     (
         [&]() {
           using current_t = std::tuple_element_t<Is, children_types>;
-          if constexpr (
-              !provide_std_interface<current_t> &&
-              !isOpaqueType<current_t>::value) {
+          if constexpr (!provide_std_interface<current_t>) {
             if (UNLIKELY(std::get<Is>(needCommit_))) {
               std::get<Is>(childrenWriters_).finalizeNull();
               std::get<Is>(needCommit_) = false;
