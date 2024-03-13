ab53e61d26b27752ea2e5cb27a77711f2db0c77a
Zac Blanco <zac@ibm.com>
2024-03-12 22:08:40 -0700

    Add JSON output format to CLI
    Cherry-pick of https://github.com/trinodb/trino/commit/daf5ce670ee70d14b8534c3e41a09c4a365b6fd8
    Co-authored-by: Yuya Ebihara <ebyhry@gmail.com>

--- presto-cli/src/test/java/com/facebook/presto/cli/TestJsonPrinter.java
+++ presto-cli/src/test/java/com/facebook/presto/cli/TestJsonPrinter.java
@@ -24,82 +24,58 @@ import static com.facebook.presto.cli.TestAlignedTablePrinter.row;
 import static com.facebook.presto.cli.TestAlignedTablePrinter.rows;
 import static org.testng.Assert.assertEquals;
 
-public class TestTsvPrinter
+public class TestJsonPrinter
 {
     @Test
-    public void testTsvPrinting()
+    public void testJsonPrinting()
             throws Exception
     {
         StringWriter writer = new StringWriter();
         List<String> fieldNames = ImmutableList.of("first", "last", "quantity");
-        OutputPrinter printer = new TsvPrinter(fieldNames, writer, true);
+        OutputPrinter printer = new JsonPrinter(fieldNames, writer);
 
         printer.printRows(rows(
                 row("hello", "world", 123),
                 row("a", null, 4.5),
                 row("some long\ntext\tdone", "more\ntext", 4567),
-                row("bye", "done", -15),
-                row("oops\0a\nb\rc\bd\fe\tf\\g\1done", "escape", 9)),
+                row("bye", "done", -15)),
                 true);
         printer.finish();
 
         String expected = "" +
-                "first\tlast\tquantity\n" +
-                "hello\tworld\t123\n" +
-                "a\t\t4.5\n" +
-                "some long\\ntext\\tdone\tmore\\ntext\t4567\n" +
-                "bye\tdone\t-15\n" +
-                "oops\\0a\\nb\\rc\\bd\\fe\\tf\\\\g\1done\tescape\t9\n";
+                "{\"first\":\"hello\",\"last\":\"world\",\"quantity\":123}\n" +
+                "{\"first\":\"a\",\"last\":null,\"quantity\":4.5}\n" +
+                "{\"first\":\"some long\\ntext\\tdone\",\"last\":\"more\\ntext\",\"quantity\":4567}\n" +
+                "{\"first\":\"bye\",\"last\":\"done\",\"quantity\":-15}\n";
 
         assertEquals(writer.getBuffer().toString(), expected);
     }
 
     @Test
-    public void testTsvPrintingNoRows()
+    public void testJsonPrintingNoRows()
             throws Exception
     {
         StringWriter writer = new StringWriter();
         List<String> fieldNames = ImmutableList.of("first", "last");
-        OutputPrinter printer = new TsvPrinter(fieldNames, writer, true);
+        OutputPrinter printer = new JsonPrinter(fieldNames, writer);
 
         printer.finish();
 
-        assertEquals(writer.getBuffer().toString(), "first\tlast\n");
+        assertEquals(writer.getBuffer().toString(), "");
     }
 
     @Test
-    public void testTsvPrintingNoHeader()
-            throws Exception
-    {
-        StringWriter writer = new StringWriter();
-        List<String> fieldNames = ImmutableList.of("first", "last", "quantity");
-        OutputPrinter printer = new TsvPrinter(fieldNames, writer, false);
-
-        printer.printRows(rows(
-                row("hello", "world", 123),
-                row("a", null, 4.5)),
-                true);
-        printer.finish();
-
-        String expected = "" +
-                "hello\tworld\t123\n" +
-                "a\t\t4.5\n";
-
-        assertEquals(writer.getBuffer().toString(), expected);
-    }
-
-    @Test
-    public void testTsvVarbinaryPrinting()
+    public void testJsonVarbinaryPrinting()
             throws IOException
     {
         StringWriter writer = new StringWriter();
         List<String> fieldNames = ImmutableList.of("first", "last", "quantity");
-        OutputPrinter printer = new TsvPrinter(fieldNames, writer, false);
+        OutputPrinter printer = new JsonPrinter(fieldNames, writer);
 
         printer.printRows(rows(row("hello".getBytes(), null, 123)), true);
         printer.finish();
 
-        String expected = "68 65 6c 6c 6f\t\t123\n";
+        String expected = "{\"first\":\"68 65 6c 6c 6f\",\"last\":null,\"quantity\":123}\n";
 
         assertEquals(writer.getBuffer().toString(), expected);
     }
