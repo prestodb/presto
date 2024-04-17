0f443ef707503a6c6408db7e08b05c96d5f3963d
Sergey Pershin <spershin@hotmail.com>
2024-04-17 14:53:20 -0700

    [client] Add support for SphericalGeography type in the client.
    Currently in the Presto client the following query would immediately
    yield "" error.
    SELECT TO_SPHERICAL_GEOGRAPHY(ST_POINT(-71.138715, 42.337546));
    This PR fixes it by adding the SphericalGeography type to the
    FixJsonDataUtils::fixValue().

--- presto-common/src/main/java/com/facebook/presto/common/type/StandardTypes.java
+++ presto-common/src/main/java/com/facebook/presto/common/type/StandardTypes.java
@@ -52,6 +52,7 @@ public final class StandardTypes
     public static final String IPADDRESS = "ipaddress";
     public static final String IPPREFIX = "ipprefix";
     public static final String GEOMETRY = "Geometry";
+    public static final String SPHERICAL_GEOGRAPHY = "SphericalGeography";
     public static final String BING_TILE = "BingTile";
     public static final String BIGINT_ENUM = "BigintEnum";
     public static final String VARCHAR_ENUM = "VarcharEnum";
