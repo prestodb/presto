8c2f94e20eb636ea548feddfa91e66ed01223db7
Naveen-Nitturu <Naveen.Nitturu@ibm.com>
2024-04-29 23:28:39 +0530

    Revert "Remove unused copy pasta"
    This reverts commit 9226d682b39e4460c0e6b3cad69f2d9a408344b0.

--- presto-main/src/test/java/com/facebook/presto/operator/TestTopNOperator.java
+++ presto-main/src/test/java/com/facebook/presto/operator/TestTopNOperator.java
@@ -169,6 +169,8 @@ public class TestTopNOperator
     public void testLimitZero()
             throws Exception
     {
+        List<Page> input = rowPagesBuilder(BIGINT).row(1L).build();
+
         TopNOperatorFactory factory = new TopNOperatorFactory(
                 0,
                 new PlanNodeId("test"),
