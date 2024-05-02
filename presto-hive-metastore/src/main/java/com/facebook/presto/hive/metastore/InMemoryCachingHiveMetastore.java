e0a0d83596bfc7f579bdca3b968ebd71400739c1
Swapnil Tailor <swapniltailor@fb.com>
2024-05-02 13:22:45 -0700

    Revert "Streamline Partition and PartitionStatistics cache invalidation"
    This reverts commit c93927273371b5a8276f9539d3b1505d82969d0d.

--- presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/InMemoryCachingHiveMetastore.java
+++ presto-hive-metastore/src/main/java/com/facebook/presto/hive/metastore/InMemoryCachingHiveMetastore.java
@@ -591,7 +591,9 @@ public class InMemoryCachingHiveMetastore
             Map<Column, Domain> partitionPredicates)
     {
         if (partitionVersioningEnabled) {
-            return getPartitionNamesWithVersionByFilter(metastoreContext, databaseName, tableName, partitionPredicates);
+            List<PartitionNameWithVersion> partitionNamesWithVersion = getPartitionNamesWithVersionByFilter(metastoreContext, databaseName, tableName, partitionPredicates);
+            invalidateStalePartitions(partitionNamesWithVersion, databaseName, tableName, metastoreContext);
+            return partitionNamesWithVersion;
         }
         return get(partitionFilterCache, getCachingKey(metastoreContext, partitionFilter(databaseName, tableName, partitionPredicates)));
     }
@@ -695,9 +697,6 @@ public class InMemoryCachingHiveMetastore
 
     private Optional<Partition> loadPartitionByName(KeyAndContext<HivePartitionName> partitionName)
     {
-        //Invalidate Partition Statistics Cache on a partition cache miss.
-        partitionStatisticsCache.invalidate(getCachingKey(partitionName.getContext(), partitionName.getKey()));
-
         return delegate.getPartition(
                 partitionName.getContext(),
                 partitionName.getKey().getHiveTableName().getDatabaseName(),
@@ -709,9 +708,6 @@ public class InMemoryCachingHiveMetastore
     {
         requireNonNull(partitionNamesKey, "partitionNames is null");
         checkArgument(!Iterables.isEmpty(partitionNamesKey), "partitionNames is empty");
-
-        //Invalidate Partition Statistics Cache on a partition cache miss.
-        partitionStatisticsCache.invalidateAll(transform(partitionNamesKey, partitionNameKey -> getCachingKey(partitionNameKey.getContext(), partitionNameKey.getKey())));
 
         KeyAndContext<HivePartitionName> firstPartitionKey = Iterables.get(partitionNamesKey, 0);
 
