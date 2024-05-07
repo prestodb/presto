35ed111455bdee87d7c2d4d908fa345001f2773c
Joe Abraham <Joe.Abraham@ibm.com>
2024-05-07 10:50:56 +0530

    Update DiscoveryNodeManager to use ServerConfig
    x

--- presto-main/src/test/java/com/facebook/presto/metadata/TestDiscoveryNodeManagerWithSidecar.java
+++ presto-main/src/test/java/com/facebook/presto/metadata/TestDiscoveryNodeManagerWithSidecar.java
@@ -39,8 +39,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 import static com.facebook.airlift.discovery.client.ServiceDescriptor.serviceDescriptor;
 import static com.facebook.airlift.discovery.client.ServiceSelectorConfig.DEFAULT_POOL;
@@ -52,24 +50,18 @@ import static com.facebook.presto.spi.NodeState.INACTIVE;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotSame;
 
-@Test(singleThreaded = true)
-public class TestDiscoveryNodeManager
+public class TestDiscoveryNodeManagerWithSidecar
 {
     private final NodeInfo workerNodeInfo = new NodeInfo("test");
-    private final NodeInfo coordinatorNodeInfo = new NodeInfo("test");
-    private final NodeInfo resourceManagerNodeInfo = new NodeInfo("test");
-    private final NodeInfo catalogServerNodeInfo = new NodeInfo("test");
+    private final NodeInfo coordinatorSidecarNodeInfo = new NodeInfo("test");
+
     private final InternalCommunicationConfig internalCommunicationConfig = new InternalCommunicationConfig();
     private NodeVersion expectedVersion;
     private Set<InternalNode> activeNodes;
     private Set<InternalNode> workerNodes;
     private Set<InternalNode> inactiveNodes;
-    private InternalNode coordinator;
-    private InternalNode inActiveCoordinator;
-    private InternalNode inActiveResourceManager;
-    private InternalNode resourceManager;
-    private InternalNode catalogServer;
-    private InternalNode inActiveCatalogServer;
+    private InternalNode coordinatorSidecar;
+    private InternalNode inActiveCoordinatorSidecar;
     private InternalNode workerNode1;
     private final PrestoNodeServiceSelector selector = new PrestoNodeServiceSelector();
     private HttpClient testHttpClient;
@@ -77,7 +69,7 @@ public class TestDiscoveryNodeManager
     private InternalNode workerNode3;
     private InternalNode inActiveWorkerNode1;
     private InternalNode inActiveWorkerNode2;
-    private final ServerConfig serverConfig = new ServerConfig().setNativeExecutionEnabled(false);
+    private final ServerConfig serverConfig = new ServerConfig().setNativeExecutionEnabled(true);
 
     @BeforeMethod
     public void setup()
@@ -85,53 +77,36 @@ public class TestDiscoveryNodeManager
         testHttpClient = new TestingHttpClient(input -> new TestingResponse(OK, ArrayListMultimap.create(), ACTIVE.name().getBytes()));
 
         expectedVersion = new NodeVersion("1");
-        coordinator = new InternalNode(coordinatorNodeInfo.getNodeId(), URI.create("https://192.0.2.8"), expectedVersion, true);
-        resourceManager = new InternalNode(
-                resourceManagerNodeInfo.getNodeId(),
-                URI.create("https://192.0.2.9"),
-                expectedVersion,
-                false,
-                true,
-                false);
-        catalogServer = new InternalNode(
-                catalogServerNodeInfo.getNodeId(),
+        coordinatorSidecar = new InternalNode(
+                coordinatorSidecarNodeInfo.getNodeId(),
                 URI.create("https://192.0.3.1"),
                 expectedVersion,
+                false,
                 false,
                 false,
                 true);
         workerNode1 = new InternalNode(workerNodeInfo.getNodeId(), URI.create("http://192.0.1.1"), expectedVersion, false);
         workerNode2 = new InternalNode(UUID.randomUUID().toString(), URI.create("http://192.0.2.1:8080"), expectedVersion, false);
         workerNode3 = new InternalNode(UUID.randomUUID().toString(), URI.create("http://192.0.2.3"), expectedVersion, false);
-        inActiveResourceManager = new InternalNode(
-                resourceManagerNodeInfo.getNodeId(),
-                URI.create("https://192.0.2.9"),
-                new NodeVersion("2"),
-                false,
-                true,
-                false);
-        inActiveCatalogServer = new InternalNode(
-                catalogServerNodeInfo.getNodeId(),
+        inActiveCoordinatorSidecar = new InternalNode(
+                coordinatorSidecarNodeInfo.getNodeId(),
                 URI.create("https://192.0.3.2"),
                 new NodeVersion("2"),
                 false,
                 false,
+                false,
                 true);
-        inActiveCoordinator = new InternalNode(UUID.randomUUID().toString(), URI.create("https://192.0.3.1"), new NodeVersion("2"), true);
         inActiveWorkerNode1 = new InternalNode(UUID.randomUUID().toString(), URI.create("https://192.0.3.9"), NodeVersion.UNKNOWN, false);
         inActiveWorkerNode2 = new InternalNode(UUID.randomUUID().toString(), URI.create("https://192.0.4.9"), new NodeVersion("2"), false);
 
         workerNodes = ImmutableSet.of(workerNode1, workerNode2, workerNode3);
         activeNodes = ImmutableSet.<InternalNode>builder()
                 .addAll(workerNodes)
-                .add(coordinator)
-                .add(resourceManager)
-                .add(catalogServer)
+                .add(coordinatorSidecar)
                 .build();
+
         inactiveNodes = ImmutableSet.of(
-                inActiveCoordinator,
-                inActiveResourceManager,
-                inActiveCatalogServer,
+                inActiveCoordinatorSidecar,
                 inActiveWorkerNode1,
                 inActiveWorkerNode2);
 
@@ -141,13 +116,12 @@ public class TestDiscoveryNodeManager
     @Test
     public void testGetAllNodesForWorkerNode()
     {
-
         DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, workerNodeInfo, new NoOpFailureDetector(), Optional.empty(), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
         try {
             AllNodes allNodes = manager.getAllNodes();
 
             Set<InternalNode> activeNodes = allNodes.getActiveNodes();
-            assertEqualsIgnoreOrder(activeNodes, ImmutableSet.of(resourceManager, catalogServer));
+            assertEqualsIgnoreOrder(activeNodes, ImmutableSet.of(coordinatorSidecar));
 
             for (InternalNode actual : activeNodes) {
                 for (InternalNode expected : this.activeNodes) {
@@ -158,7 +132,7 @@ public class TestDiscoveryNodeManager
             assertEqualsIgnoreOrder(activeNodes, manager.getNodes(ACTIVE));
 
             Set<InternalNode> inactiveNodes = allNodes.getInactiveNodes();
-            assertEqualsIgnoreOrder(inactiveNodes, ImmutableSet.of(inActiveResourceManager, inActiveCatalogServer));
+            assertEqualsIgnoreOrder(inactiveNodes, ImmutableSet.of(inActiveCoordinatorSidecar));
 
             for (InternalNode actual : inactiveNodes) {
                 for (InternalNode expected : this.inactiveNodes) {
@@ -174,9 +148,9 @@ public class TestDiscoveryNodeManager
     }
 
     @Test
-    public void testGetAllNodesForCoordinator()
+    public void testGetAllNodesForCoordinatorSidecar()
     {
-        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, coordinatorNodeInfo, new NoOpFailureDetector(), Optional.empty(), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
+        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, coordinatorSidecarNodeInfo, new NoOpFailureDetector(), Optional.empty(), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
         try {
             AllNodes allNodes = manager.getAllNodes();
 
@@ -208,121 +182,11 @@ public class TestDiscoveryNodeManager
     }
 
     @Test
-    public void testGetAllNodesForResourceManager()
+    public void testGetCoordinatorSidecar()
     {
-        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, resourceManagerNodeInfo, new NoOpFailureDetector(), Optional.empty(), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
+        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, coordinatorSidecarNodeInfo, new NoOpFailureDetector(), Optional.of(host -> false), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
         try {
-            AllNodes allNodes = manager.getAllNodes();
-
-            Set<InternalNode> activeNodes = allNodes.getActiveNodes();
-            assertEqualsIgnoreOrder(activeNodes, this.activeNodes);
-
-            for (InternalNode actual : activeNodes) {
-                for (InternalNode expected : this.activeNodes) {
-                    assertNotSame(actual, expected);
-                }
-            }
-
-            assertEqualsIgnoreOrder(activeNodes, manager.getNodes(ACTIVE));
-
-            Set<InternalNode> inactiveNodes = allNodes.getInactiveNodes();
-            assertEqualsIgnoreOrder(inactiveNodes, this.inactiveNodes);
-
-            for (InternalNode actual : inactiveNodes) {
-                for (InternalNode expected : this.inactiveNodes) {
-                    assertNotSame(actual, expected);
-                }
-            }
-
-            assertEqualsIgnoreOrder(inactiveNodes, manager.getNodes(INACTIVE));
-        }
-        finally {
-            manager.stop();
-        }
-    }
-
-    @Test
-    public void testGetCurrentNode()
-    {
-
-        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, workerNodeInfo, new NoOpFailureDetector(), Optional.empty(), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
-        try {
-            assertEquals(manager.getCurrentNode(), workerNode1);
-        }
-        finally {
-            manager.stop();
-        }
-    }
-
-    @Test
-    public void testGetCoordinators()
-    {
-
-        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, resourceManagerNodeInfo, new NoOpFailureDetector(), Optional.empty(), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
-        try {
-            assertEquals(manager.getCoordinators(), ImmutableSet.of(coordinator));
-        }
-        finally {
-            manager.stop();
-        }
-    }
-
-    @Test
-    public void testGetResourceManagers()
-    {
-
-        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, workerNodeInfo, new NoOpFailureDetector(), Optional.of(host -> false), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
-        try {
-            assertEquals(manager.getResourceManagers(), ImmutableSet.of(resourceManager));
-        }
-        finally {
-            manager.stop();
-        }
-    }
-
-    @Test
-    public void testGetCatalogServers()
-    {
-
-        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, workerNodeInfo, new NoOpFailureDetector(), Optional.of(host -> false), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
-        try {
-            assertEquals(manager.getCatalogServers(), ImmutableSet.of(catalogServer));
-        }
-        finally {
-            manager.stop();
-        }
-    }
-
-    @SuppressWarnings("ResultOfObjectAllocationIgnored")
-    @Test(expectedExceptions = IllegalStateException.class, expectedExceptionsMessageRegExp = ".* current node not returned .*")
-    public void testGetCurrentNodeRequired()
-    {
-        new DiscoveryNodeManager(selector, new NodeInfo("test"), new NoOpFailureDetector(), Optional.empty(), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
-    }
-
-    @Test(timeOut = 60000)
-    public void testNodeChangeListener()
-            throws Exception
-    {
-        DiscoveryNodeManager manager = new DiscoveryNodeManager(selector, coordinatorNodeInfo, new NoOpFailureDetector(), Optional.empty(), expectedVersion, testHttpClient, new TestingDriftClient<>(), internalCommunicationConfig, serverConfig);
-        try {
-            manager.startPollingNodeStates();
-
-            BlockingQueue<AllNodes> notifications = new ArrayBlockingQueue<>(100);
-            manager.addNodeChangeListener(notifications::add);
-            AllNodes allNodes = notifications.take();
-            assertEquals(allNodes.getActiveNodes(), activeNodes);
-            assertEquals(allNodes.getInactiveNodes(), inactiveNodes);
-
-            selector.announceNodes(ImmutableSet.of(workerNode1), ImmutableSet.of(coordinator));
-            allNodes = notifications.take();
-            assertEquals(allNodes.getActiveNodes(), ImmutableSet.of(workerNode1, coordinator));
-            assertEquals(allNodes.getActiveCoordinators(), ImmutableSet.of(coordinator));
-
-            selector.announceNodes(activeNodes, inactiveNodes);
-            allNodes = notifications.take();
-            assertEquals(allNodes.getActiveNodes(), activeNodes);
-            assertEquals(allNodes.getInactiveNodes(), inactiveNodes);
+            assertEquals(manager.getCoordinatorSidecars(), ImmutableSet.of(coordinatorSidecar));
         }
         finally {
             manager.stop();
@@ -343,9 +207,7 @@ public class TestDiscoveryNodeManager
                         .setNodeId(node.getNodeIdentifier())
                         .addProperty("http", node.getInternalUri().toString())
                         .addProperty("node_version", node.getNodeVersion().toString())
-                        .addProperty("coordinator", String.valueOf(node.isCoordinator()))
-                        .addProperty("resource_manager", String.valueOf(node.isResourceManager()))
-                        .addProperty("catalog_server", String.valueOf(node.isCatalogServer()))
+                        .addProperty("sidecar", String.valueOf(node.isCoordinatorSidecar()))
                         .addProperty(POOL_TYPE, node.getPoolType().name())
                         .build());
             }
