d217c7490dc255c0d9f60ffde2aec807cbb3e375
prithvip <prithvi.pandian@gmail.com>
2024-05-29 17:47:00 -0700

    Extract interface from FailedDispatchQueryFactory

--- presto-main/src/main/java/com/facebook/presto/dispatcher/FailedDispatchQueryFactory.java
+++ presto-main/src/main/java/com/facebook/presto/dispatcher/FailedDispatchQueryFactory.java
@@ -14,50 +14,11 @@
 package com.facebook.presto.dispatcher;
 
 import com.facebook.presto.Session;
-import com.facebook.presto.event.QueryMonitor;
-import com.facebook.presto.execution.ExecutionFailureInfo;
-import com.facebook.presto.execution.LocationFactory;
-import com.facebook.presto.server.BasicQueryInfo;
 import com.facebook.presto.spi.resourceGroups.ResourceGroupId;
 
-import javax.inject.Inject;
-
 import java.util.Optional;
-import java.util.concurrent.ExecutorService;
 
-import static com.facebook.presto.util.Failures.toFailure;
-import static java.util.Objects.requireNonNull;
-
-public class FailedDispatchQueryFactory
+public interface FailedDispatchQueryFactory
 {
-    private final QueryMonitor queryMonitor;
-    private final LocationFactory locationFactory;
-    private final ExecutorService executor;
-
-    @Inject
-    public FailedDispatchQueryFactory(QueryMonitor queryMonitor, LocationFactory locationFactory, DispatchExecutor dispatchExecutor)
-    {
-        this.queryMonitor = requireNonNull(queryMonitor, "queryMonitor is null");
-        this.locationFactory = requireNonNull(locationFactory, "locationFactory is null");
-        this.executor = requireNonNull(dispatchExecutor, "dispatchExecutor is null").getExecutor();
-    }
-
-    public FailedDispatchQuery createFailedDispatchQuery(Session session, String query, Optional<ResourceGroupId> resourceGroup, Throwable throwable)
-    {
-        ExecutionFailureInfo failure = toFailure(throwable);
-        FailedDispatchQuery failedDispatchQuery = new FailedDispatchQuery(
-                session,
-                query,
-                locationFactory.createQueryLocation(session.getQueryId()),
-                resourceGroup,
-                failure,
-                executor);
-
-        BasicQueryInfo queryInfo = failedDispatchQuery.getBasicQueryInfo();
-
-        queryMonitor.queryCreatedEvent(queryInfo);
-        queryMonitor.queryImmediateFailureEvent(queryInfo, failure);
-
-        return failedDispatchQuery;
-    }
+    FailedDispatchQuery createFailedDispatchQuery(Session session, String query, Optional<ResourceGroupId> resourceGroup, Throwable throwable);
 }
