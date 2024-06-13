ffc9e1ccbc79c664f77210670b6f273908e990ab
abhinavmuk04 <69162586+abhinavmuk04@users.noreply.github.com>
2024-06-13 13:03:24 -0700

    Implement array_sum function in Java from SQL

--- presto-main/src/test/java/com/facebook/presto/operator/scalar/sql/TestArraySqlFunctions.java
+++ presto-main/src/test/java/com/facebook/presto/operator/scalar/sql/TestArraySqlFunctions.java
@@ -39,36 +39,6 @@ public class TestArraySqlFunctions
         extends AbstractTestFunctions
 {
     @Test
-    public void testArraySum()
-    {
-        assertFunction("array_sum(array[BIGINT '1', BIGINT '2'])", BIGINT, 3L);
-        assertFunction("array_sum(array[INTEGER '1', INTEGER '2'])", BIGINT, 3L);
-        assertFunction("array_sum(array[SMALLINT '1', SMALLINT '2'])", BIGINT, 3L);
-        assertFunction("array_sum(array[TINYINT '1', TINYINT '2'])", BIGINT, 3L);
-
-        assertFunction("array_sum(array[BIGINT '1', INTEGER '2'])", BIGINT, 3L);
-        assertFunction("array_sum(array[INTEGER '1', SMALLINT '2'])", BIGINT, 3L);
-        assertFunction("array_sum(array[SMALLINT '1', TINYINT '2'])", BIGINT, 3L);
-
-        assertFunctionWithError("array_sum(array[DOUBLE '-2.0', DOUBLE '5.3'])", DOUBLE, 3.3);
-        assertFunctionWithError("array_sum(array[DOUBLE '-2.0', REAL '5.3'])", DOUBLE, 3.3);
-        assertFunctionWithError("array_sum(array[DOUBLE '-2.0', DECIMAL '5.3'])", DOUBLE, 3.3);
-        assertFunctionWithError("array_sum(array[REAL '-2.0', DECIMAL '5.3'])", DOUBLE, 3.3);
-
-        assertFunctionWithError("array_sum(array[BIGINT '-2', DOUBLE '5.3'])", DOUBLE, 3.3);
-        assertFunctionWithError("array_sum(array[INTEGER '-2', REAL '5.3'])", DOUBLE, 3.3);
-        assertFunctionWithError("array_sum(array[SMALLINT '-2', DECIMAL '5.3'])", DOUBLE, 3.3);
-        assertFunctionWithError("array_sum(array[TINYINT '-2', DOUBLE '5.3'])", DOUBLE, 3.3);
-
-        assertFunction("array_sum(null)", BIGINT, null);
-        assertFunction("array_sum(array[])", BIGINT, 0L);
-        assertFunction("array_sum(array[NULL])", BIGINT, 0L);
-        assertFunction("array_sum(array[NULL, NULL, NULL])", BIGINT, 0L);
-        assertFunction("array_sum(array[3, NULL, 5])", BIGINT, 8L);
-        assertFunctionWithError("array_sum(array[NULL, double '1.2', double '2.3', NULL, -3])", DOUBLE, 0.5);
-    }
-
-    @Test
     public void testArrayAverage()
     {
         assertFunctionWithError("array_average(array[1, 2])", DOUBLE, 1.5);
