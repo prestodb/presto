79c00d5f71d880f6a0c28ac38d148bbc8ba9d58f
sai bhaskar reddy <sai.bhaskar.reddy.sabbasani1@ibm.com>
2024-07-09 10:55:10 +0530

    Adding Arrow Flight connector template
    This adds the Arrow Flight base module as a template to build connectors
    that use a Flight service to transfer data with Presto in Arrow format.
    A concrete Arrow Flight connector implementation will extend the
    BaseArrowFlightClientHandler that can connect to a Flight service that
    handles the required RPC calls from the client handler. An example
    connector implementation is provided in the unit testing.
    
    RFC: https://github.com/prestodb/rfcs/blob/main/RFC-0004-arrow-flight-connector.md
    
    Co-authored-by: sai bhaskar reddy <sai.bhaskar.reddy.sabbasani1@ibm.com>
    Co-authored-by: SthuthiGhosh9400 <Sthuthi.Ghosh@ibm.com>
    Co-authored-by: lithinwxd <Lithin.Purushothaman@ibm.com>
    Co-authored-by: Steve Burnett <burnett@pobox.com>
    Co-authored-by: elbinpallimalilibm <elbin.pallimalil@ibm.com>
    Co-authored-by: Tim Meehan <tim@timdmeehan.com>

---
+++ presto-base-arrow-flight/src/test/java/com/facebook/plugin/arrow/ArrowMetadataUtil.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.plugin.arrow;
+
+import com.facebook.airlift.json.JsonCodec;
+import com.facebook.airlift.json.JsonCodecFactory;
+import com.facebook.airlift.json.JsonObjectMapperProvider;
+import com.facebook.presto.common.type.StandardTypes;
+import com.facebook.presto.common.type.Type;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+import static com.facebook.presto.common.type.BigintType.BIGINT;
+import static com.facebook.presto.common.type.VarcharType.VARCHAR;
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Locale.ENGLISH;
+import static org.testng.Assert.assertEquals;
+
+final class ArrowMetadataUtil
+{
+    private ArrowMetadataUtil() {}
+
+    public static final JsonCodec<ArrowColumnHandle> COLUMN_CODEC;
+    public static final JsonCodec<ArrowTableHandle> TABLE_CODEC;
+
+    static {
+        JsonObjectMapperProvider provider = new JsonObjectMapperProvider();
+        provider.setJsonDeserializers(ImmutableMap.of(Type.class, new TestingTypeDeserializer()));
+        JsonCodecFactory codecFactory = new JsonCodecFactory(provider);
+        COLUMN_CODEC = codecFactory.jsonCodec(ArrowColumnHandle.class);
+        TABLE_CODEC = codecFactory.jsonCodec(ArrowTableHandle.class);
+    }
+
+    public static final class TestingTypeDeserializer
+            extends FromStringDeserializer<Type>
+    {
+        private final Map<String, Type> types = ImmutableMap.of(
+                StandardTypes.BIGINT, BIGINT,
+                StandardTypes.VARCHAR, VARCHAR);
+
+        public TestingTypeDeserializer()
+        {
+            super(Type.class);
+        }
+
+        @Override
+        protected Type _deserialize(String value, DeserializationContext context)
+        {
+            Type type = types.get(value.toLowerCase(ENGLISH));
+            checkArgument(type != null, "Unknown type %s", value);
+            return type;
+        }
+    }
+
+    public static <T> void assertJsonRoundTrip(JsonCodec<T> codec, T object)
+    {
+        String json = codec.toJson(object);
+        T copy = codec.fromJson(json);
+        assertEquals(copy, object);
+    }
+}
