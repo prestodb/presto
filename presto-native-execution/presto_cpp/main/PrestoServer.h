c75087f144ccba5e1c8f642f2eaa4331bcb51afb
Deepak Majeti <deepak.majeti@ibm.com>
2024-07-10 08:25:21 -0400

    [native] Add Velox factory registrations
    Velox library will be removing the factory registrations for connectors,
    readers, sinks, etc...
    We now need to register the factories in Prestissimo.

--- presto-native-execution/presto_cpp/main/PrestoServer.h
+++ presto-native-execution/presto_cpp/main/PrestoServer.h
@@ -136,7 +136,13 @@ class PrestoServer {
 
   /// Invoked to register the required dwio data sinks which are used by
   /// connectors.
-  virtual void registerFileSinks() {}
+  virtual void registerFileSinks();
+
+  virtual void registerFileReadersAndWriters();
+
+  virtual void unregisterFileReadersAndWriters();
+
+  virtual void registerConnectorFactories();
 
   /// Invoked by presto shutdown procedure to unregister connectors.
   virtual void unregisterConnectors();
@@ -152,6 +158,8 @@ class PrestoServer {
   virtual void registerVectorSerdes();
 
   virtual void registerFileSystems();
+
+  virtual void unregisterFileSystems();
 
   virtual void registerMemoryArbitrators();
 
