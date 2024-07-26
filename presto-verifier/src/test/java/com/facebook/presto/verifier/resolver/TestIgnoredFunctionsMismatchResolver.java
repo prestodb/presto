eb9cb95e09d1af854597ec85a81df6b8f950a99e
Ge Gao <gaoge@fb.com>
2024-07-26 13:50:24 -0700

    Allow Verifier to validate varchar as floating point
    Treat varchar column as double and apply floating point column
    validation, if it is detected that the varchar column is derived
    from casting floating points. This applies to simple varchar column,
    array(varchar) and map(varchar, varchar). More nested varchars is
    not supported.
    
    Add verifier config --validate-string-as-double.

--- presto-verifier/src/test/java/com/facebook/presto/verifier/resolver/TestIgnoredFunctionsMismatchResolver.java
+++ presto-verifier/src/test/java/com/facebook/presto/verifier/resolver/TestIgnoredFunctionsMismatchResolver.java
@@ -45,7 +45,10 @@ public class TestIgnoredFunctionsMismatchResolver
     @Test
     public void testDefault()
     {
-        ColumnMatchResult<?> mismatchedColumn = createMismatchedColumn(VARCHAR, new SimpleColumnChecksum(binary(0xa)), new SimpleColumnChecksum(binary(0xb)));
+        ColumnMatchResult<?> mismatchedColumn = createMismatchedColumn(
+                VARCHAR,
+                new SimpleColumnChecksum(binary(0xa), Optional.empty()),
+                new SimpleColumnChecksum(binary(0xb), Optional.empty()));
 
         // resolved
         assertResolved(createBundle("CREATE TABLE test AS SELECT rand() x FROM source"), mismatchedColumn);
