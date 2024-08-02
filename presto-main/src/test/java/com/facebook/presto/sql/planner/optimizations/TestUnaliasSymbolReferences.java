prestodb/presto

commit f6c22523a6e007460d74cffd2ddb6c2f81f939d9

Canonicalize equi join conditions for inner joins
This reduces number of unique symbols in query plan
and allows other optimizations to be applied
(e.g: running multiple joins in the same stage which
operate on same partitions after canonicalization)

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/UnaliasSymbolReferences.java
@@ -82,6 +82,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
+import static com.facebook.presto.sql.planner.plan.JoinNode.Type.INNER;
 import static com.facebook.presto.util.ImmutableCollectors.toImmutableList;
 import static com.facebook.presto.util.ImmutableCollectors.toImmutableSet;
 import static com.google.common.base.Preconditions.checkState;
@@ -110,13 +111,19 @@ public class UnaliasSymbolReferences
         requireNonNull(symbolAllocator, "symbolAllocator is null");
         requireNonNull(idAllocator, "idAllocator is null");
 
-        return SimplePlanRewriter.rewriteWith(new Rewriter(), plan);
+        return SimplePlanRewriter.rewriteWith(new Rewriter(types), plan);
     }
 
     private static class Rewriter
             extends SimplePlanRewriter<Void>
     {
         private final Map<Symbol, Symbol> mapping = new HashMap<>();
+        private final Map<Symbol, Type> types;
+
+        private Rewriter(Map<Symbol, Type> types)
+        {
+            this.types = types;
+        }
 
         @Override
         public PlanNode visitAggregation(AggregationNode node, RewriteContext<Void> context)
@@ -533,7 +540,18 @@ public class UnaliasSymbolReferences
             PlanNode left = context.rewrite(node.getLeft());
             PlanNode right = context.rewrite(node.getRight());
 
-            return new JoinNode(node.getId(), node.getType(), left, right, canonicalizeJoinCriteria(node.getCriteria()), node.getFilter().map(this::canonicalize), canonicalize(node.getLeftHashSymbol()), canonicalize(node.getRightHashSymbol()));
+            List<JoinNode.EquiJoinClause> canonicalCriteria = canonicalizeJoinCriteria(node.getCriteria());
+            Optional<Expression> canonicalFilter = node.getFilter().map(this::canonicalize);
+            Optional<Symbol> canonicalLeftHashSymbol = canonicalize(node.getLeftHashSymbol());
+            Optional<Symbol> canonicalRightHashSymbol = canonicalize(node.getRightHashSymbol());
+
+            if (node.getType().equals(INNER)) {
+                canonicalCriteria.stream()
+                        .filter(clause -> types.get(clause.getLeft()).equals(types.get(clause.getRight())))
+                        .forEach(clause -> map(clause.getRight(), clause.getLeft()));
+            }
+
+            return new JoinNode(node.getId(), node.getType(), left, right, canonicalCriteria, canonicalFilter, canonicalLeftHashSymbol, canonicalRightHashSymbol);
         }
 
         @Override

commit 9651f49cafa297f68460b04ad41b5d02695ac664

Fix assignments for equijoin on TimestampWithTimeZone
TimestampWithTimeZone can be represented in multiple equivalant ways.
This breaks and optimization introduced by f6c2252 because while
the values may be semantically equivalent, the contents may still
differ.  To support this, and future types that may end up needing this
support (such as JSON), a new flag is introduced to the type to indicate
whether or not the values of the type may support more than one
representation.

presto-common/src/main/java/com/facebook/presto/common/type/TimestampWithTimeZoneType.java
@@ -29,6 +29,19 @@ public final class TimestampWithTimeZoneType
         super(parseTypeSignature(StandardTypes.TIMESTAMP_WITH_TIME_ZONE));
     }
 
+    /**
+     * Timestamp with time zone represents a single point in time.  Multiple timestamps with timezones may
+     * each refer to the same point in time.  For example, 9:00am in New York is the same point in time as
+     * 2:00pm in London.  While those two timestamps may be encoded differently, they each refer to the same
+     * point in time.  Therefore, it's possible encode multiple timestamps which each represent the same
+     * point in time, and hence it's not safe to use equality as a proxy for identity.
+     */
+    @Override
+    public boolean equalValuesAreIdentical()
+    {
+        return false;
+    }
+
     @Override
     public Object getObjectValue(SqlFunctionProperties properties, Block block, int position)
     {

presto-common/src/main/java/com/facebook/presto/common/type/Type.java
@@ -48,6 +48,14 @@ public interface Type
     boolean isOrderable();
 
     /**
+     * True if there can only be one canonical representation of data corresponding to this type.
+     */
+    default boolean equalValuesAreIdentical()
+    {
+        return true;
+    }
+
+    /**
      * Gets the Java class type used to represent this value on the stack during
      * expression execution.
      * <p>

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/UnaliasSymbolReferences.java
@@ -578,7 +578,7 @@ public class UnaliasSymbolReferences
 
             if (node.getType().equals(INNER)) {
                 canonicalCriteria.stream()
-                        .filter(clause -> clause.getLeft().getType().equals(clause.getRight().getType()))
+                        .filter(clause -> clause.getLeft().getType().equals(clause.getRight().getType()) && clause.getLeft().getType().equalValuesAreIdentical())
                         .filter(clause -> node.getOutputVariables().contains(clause.getLeft()))
                         .forEach(clause -> map(clause.getRight(), clause.getLeft()));
             }

presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestUnaliasSymbolReferences.java
@@ -1,0 +1,71 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.planner.optimizations;
+
+import com.facebook.presto.sql.planner.assertions.BasePlanTest;
+import com.google.common.collect.ImmutableList;
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.spi.plan.JoinType.INNER;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.anyTree;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.equiJoinClause;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.join;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.values;
+
+public class TestUnaliasSymbolReferences
+        extends BasePlanTest
+{
+    @Test
+    public void testJoinTimestampsWithTimezonesPreservesAssignments()
+    {
+        // Although the two timestamps are the same point in time, they are in different timezones,
+        // so the optimizer should not collapse the two assignments.
+        assertPlan("WITH source AS (" +
+                        "SELECT * FROM (" +
+                        "    VALUES" +
+                        "        (TIMESTAMP '2001-08-22 03:04:05.321 America/Los_Angeles')," +
+                        "        (TIMESTAMP '2001-08-22 06:04:05.321 America/New_York')" +
+                        ") AS tbl (tstz)" +
+                        ")" +
+                        "SELECT * FROM source a JOIN source b ON a.tstz = b.tstz",
+                anyTree(
+                        join(INNER, ImmutableList.of(equiJoinClause("LEFT_BAR", "RIGHT_BAR")),
+                                anyTree(values("LEFT_BAR")),
+                                anyTree(values("RIGHT_BAR")))
+                                .withNumberOfOutputColumns(2)
+                                .withExactOutputs("LEFT_BAR", "RIGHT_BAR")));
+    }
+
+    @Test
+    public void testIdenticalValuesCollapseAssignments()
+    {
+        // We take the same table as in #testJoinTimestampsWithTimezonesPreservesAssignments but convert the
+        // values to UTC epoch time as a double.  The optimizer should collapse the two identical values into
+        // a single assignment, because doubles are ensured to be represented only a single way.
+        assertPlan("WITH source AS (" +
+                        "SELECT * FROM (" +
+                        "    VALUES" +
+                        "        (to_unixtime(TIMESTAMP '2001-08-22 03:04:05.321 America/Los_Angeles'))," +
+                        "        (to_unixtime(TIMESTAMP '2001-08-22 06:04:05.321 America/New_York'))" +
+                        ") AS tbl (tstz)" +
+                        ")" +
+                        "SELECT * FROM source a JOIN source b ON a.tstz = b.tstz",
+                anyTree(
+                        join(INNER, ImmutableList.of(equiJoinClause("LEFT_BAR", "RIGHT_BAR")),
+                                anyTree(values("LEFT_BAR")),
+                                anyTree(values("RIGHT_BAR")))
+                                .withNumberOfOutputColumns(1)
+                                .withExactOutputs("LEFT_BAR")));
+    }
+}

presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -120,6 +120,7 @@ import static com.facebook.presto.tests.QueryTemplate.parameter;
 import static com.facebook.presto.tests.QueryTemplate.queryTemplate;
 import static com.facebook.presto.tests.StatefulSleepingSum.STATEFUL_SLEEPING_SUM;
 import static com.facebook.presto.tests.StructuralTestUtil.mapType;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.common.collect.Iterables.transform;
 import static java.lang.String.format;
@@ -7450,6 +7451,30 @@ public abstract class AbstractTestQueries
         assertQuery(session, "select r.custkey, r.orderkey, r.name, n.nationkey from (select o.custkey, o.orderkey, c.name from orders o join customer c on cast(o.custkey as varchar) = cast(c.custkey as varchar)) r, nation n");
         // Do not trigger optimization
         assertQuery(session, "select * from customer c join orders o on cast(acctbal as varchar) = cast(totalprice as varchar)");
+    }
+
+    @Test
+    public void testPreserveAssignmentsInJoin()
+    {
+        // The following two timestamps represent the same point in time but with different time zones
+        String timestampLosAngeles = "2001-08-22 03:04:05.321 America/Los_Angeles";
+        String timestampNewYork = "2001-08-22 06:04:05.321 America/New_York";
+        Set<List<Object>> rows = computeActual("WITH source AS (" +
+                "SELECT * FROM (" +
+                "    VALUES" +
+                "        (TIMESTAMP '" + timestampLosAngeles + "')," +
+                "        (TIMESTAMP '" + timestampNewYork + "')" +
+                ") AS tbl (tstz)" +
+                ")" +
+                "SELECT * FROM source a JOIN source b ON a.tstz = b.tstz").getMaterializedRows().stream()
+                .map(MaterializedRow::getFields)
+                .collect(toImmutableSet());
+        assertEquals(rows,
+                ImmutableSet.of(
+                        ImmutableList.of(zonedDateTime(timestampLosAngeles), zonedDateTime(timestampLosAngeles)),
+                        ImmutableList.of(zonedDateTime(timestampLosAngeles), zonedDateTime(timestampNewYork)),
+                        ImmutableList.of(zonedDateTime(timestampNewYork), zonedDateTime(timestampLosAngeles)),
+                        ImmutableList.of(zonedDateTime(timestampNewYork), zonedDateTime(timestampNewYork))));
     }
 
     @Test