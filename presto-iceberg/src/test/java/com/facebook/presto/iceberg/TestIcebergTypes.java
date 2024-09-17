d7b012559a305ec6db83d6aad67b8c08a2c70042
auden-woolfson <auden.woolfson@ibm.com>
2024-09-17 10:46:39 -0700

    parameterize test with query runner

--- presto-iceberg/src/test/java/com/facebook/presto/iceberg/TestIcebergTypes.java
+++ presto-iceberg/src/test/java/com/facebook/presto/iceberg/TestIcebergTypes.java
@@ -19,69 +19,105 @@ import com.facebook.presto.common.type.Type;
 import com.facebook.presto.testing.MaterializedResult;
 import com.facebook.presto.testing.QueryRunner;
 import com.facebook.presto.tests.AbstractTestQueryFramework;
+import com.facebook.presto.tests.DistributedQueryRunner;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
 
+import static com.facebook.presto.hive.HiveCommonSessionProperties.PARQUET_BATCH_READ_OPTIMIZATION_ENABLED;
 import static com.facebook.presto.iceberg.IcebergQueryRunner.createIcebergQueryRunner;
+import static com.google.common.base.Preconditions.checkState;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class TestIcebergTypes
         extends AbstractTestQueryFramework
 {
+    private QueryRunner batchReaderEnabledQueryRunner;
+
     @Override
     protected QueryRunner createQueryRunner() throws Exception
     {
+        this.batchReaderEnabledQueryRunner = createIcebergQueryRunner(
+                ImmutableMap.of(),
+                ImmutableMap.of(),
+                ImmutableMap.of(PARQUET_BATCH_READ_OPTIMIZATION_ENABLED, "true"),
+                new IcebergConfig().getFileFormat(),
+                true,
+                false,
+                OptionalInt.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                false);
         return createIcebergQueryRunner(ImmutableMap.of(), ImmutableMap.of());
     }
 
-    @Test
-    public void testTimestampWithTimezone()
+    @DataProvider(name = "testTimestampWithTimezone")
+    public Object[][] createTestTimestampWithTimezoneData()
+    {
+        return new Object[][] {
+                {getBatchReaderEnabledQueryRunner()},
+                {getQueryRunner()}
+        };
+    }
+
+    @Test(dataProvider = "testTimestampWithTimezone")
+    public void testTimestampWithTimezone(QueryRunner runner)
     {
         String timestamptz = "TIMESTAMP '1984-12-08 00:10:00 America/Los_Angeles'";
         String timestamp = "TIMESTAMP '1984-12-08 00:10:00'";
 
-        getQueryRunner().execute("CREATE TABLE test_timestamptz(a TIMESTAMP WITH TIME ZONE, b TIMESTAMP, c TIMESTAMP WITH TIME ZONE)");
+        runner.execute("CREATE TABLE test_timestamptz(a TIMESTAMP WITH TIME ZONE, b TIMESTAMP, c TIMESTAMP WITH TIME ZONE)");
         String row = "(" + timestamptz + ", " + timestamp + ", " + timestamptz + ")";
         for (int i = 0; i < 10; i++) {
-            getQueryRunner().execute("INSERT INTO test_timestamptz values " + row);
+            runner.execute("INSERT INTO test_timestamptz values " + row);
         }
 
-        MaterializedResult initialRows = getQueryRunner().execute("SELECT * FROM test_timestamptz");
+        MaterializedResult initialRows = runner.execute("SELECT * FROM test_timestamptz");
         List<Type> types = initialRows.getTypes();
 
         assertTrue(types.get(0) instanceof TimestampWithTimeZoneType);
         assertTrue(types.get(1) instanceof TimestampType);
 
-        getQueryRunner().execute("CREATE TABLE test_timestamptz_partition(a TIMESTAMP WITH TIME ZONE, b TIMESTAMP, c TIMESTAMP WITH TIME ZONE) " +
+        runner.execute("CREATE TABLE test_timestamptz_partition(a TIMESTAMP WITH TIME ZONE, b TIMESTAMP, c TIMESTAMP WITH TIME ZONE) " +
                 "WITH (PARTITIONING = ARRAY['b'])");
-        getQueryRunner().execute("INSERT INTO test_timestamptz_partition (a, b, c) SELECT a, b, c FROM test_timestamptz");
+        runner.execute("INSERT INTO test_timestamptz_partition (a, b, c) SELECT a, b, c FROM test_timestamptz");
 
-        MaterializedResult partitionRows = getQueryRunner().execute("SELECT * FROM test_timestamptz");
+        MaterializedResult partitionRows = runner.execute("SELECT * FROM test_timestamptz");
         List<Type> partitionTypes = partitionRows.getTypes();
 
         assertTrue(partitionTypes.get(0) instanceof TimestampWithTimeZoneType);
         assertTrue(partitionTypes.get(1) instanceof TimestampType);
 
         String earlyTimestamptz = "TIMESTAMP '1980-12-08 00:10:00 America/Los_Angeles'";
-        getQueryRunner().execute("CREATE TABLE test_timestamptz_filter(a TIMESTAMP WITH TIME ZONE)");
+        runner.execute("CREATE TABLE test_timestamptz_filter(a TIMESTAMP WITH TIME ZONE)");
 
         for (int i = 0; i < 5; i++) {
-            getQueryRunner().execute("INSERT INTO test_timestamptz_filter VALUES (" + earlyTimestamptz + ")");
+            runner.execute("INSERT INTO test_timestamptz_filter VALUES (" + earlyTimestamptz + ")");
         }
         for (int i = 0; i < 5; i++) {
-            getQueryRunner().execute("INSERT INTO test_timestamptz_filter VALUES (" + timestamptz + ")");
+            runner.execute("INSERT INTO test_timestamptz_filter VALUES (" + timestamptz + ")");
         }
 
-        MaterializedResult lateRows = getQueryRunner().execute("SELECT a FROM test_timestamptz_filter WHERE a > " + earlyTimestamptz);
+        MaterializedResult lateRows = runner.execute("SELECT a FROM test_timestamptz_filter WHERE a > " + earlyTimestamptz);
         assertEquals(lateRows.getMaterializedRows().size(), 5);
 
-        MaterializedResult rows = getQueryRunner().execute("SELECT * FROM test_timestamptz_filter");
+        MaterializedResult rows = runner.execute("SELECT * FROM test_timestamptz_filter");
         System.out.println(rows.toString());
 
-        MaterializedResult earlyRows = getQueryRunner().execute("SELECT a FROM test_timestamptz_filter WHERE a <= " + earlyTimestamptz);
+        MaterializedResult earlyRows = runner.execute("SELECT a FROM test_timestamptz_filter WHERE a <= " + earlyTimestamptz);
         assertEquals(earlyRows.getMaterializedRows().size(), 5);
+    }
+
+    private QueryRunner getBatchReaderEnabledQueryRunner()
+    {
+        checkState(batchReaderEnabledQueryRunner != null, "batchReaderEnabledQueryRunner not set");
+        return batchReaderEnabledQueryRunner;
     }
 }
