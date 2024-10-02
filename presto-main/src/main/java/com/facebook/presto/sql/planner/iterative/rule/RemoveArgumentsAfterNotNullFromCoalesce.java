61f1a2251b7936388bb6cad5004c74ffd31462b4
auden-woolfson <auden.woolfson@ibm.com>
2024-10-02 15:34:30 -0700

    add instance vars

--- presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/RemoveArgumentsAfterNotNullFromCoalesce.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/RemoveArgumentsAfterNotNullFromCoalesce.java
@@ -1,20 +1,30 @@
 package com.facebook.presto.sql.planner.iterative.rule;
 
+import com.facebook.presto.matching.Capture;
 import com.facebook.presto.matching.Captures;
 import com.facebook.presto.matching.Pattern;
+import com.facebook.presto.metadata.FunctionAndTypeManager;
 import com.facebook.presto.spi.plan.PlanNode;
 import com.facebook.presto.spi.plan.ProjectNode;
 import com.facebook.presto.sql.planner.iterative.Rule;
+import com.facebook.presto.sql.planner.plan.JoinNode;
 
+import static com.facebook.presto.matching.Capture.newCapture;
+import static com.facebook.presto.sql.planner.plan.Patterns.join;
 import static com.facebook.presto.sql.planner.plan.Patterns.project;
+import static com.facebook.presto.sql.planner.plan.Patterns.source;
 
 public class RemoveArgumentsAfterNotNullFromCoalesce
     implements Rule<ProjectNode>
 {
+    private static final Capture<JoinNode> JOIN = newCapture();
+
+    private static final Pattern<ProjectNode> PATTERN = project()
+            .with(source().matching(join().capturedAs(JOIN)));
 
     @Override
     public Pattern<ProjectNode> getPattern() {
-        return project();
+        return PATTERN;
     }
 
     @Override
