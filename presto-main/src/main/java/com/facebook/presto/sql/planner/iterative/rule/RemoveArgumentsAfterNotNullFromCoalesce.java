0d8ddcf6d02682d6109b90646c59aa48e6675364
auden-woolfson <auden.woolfson@ibm.com>
2024-10-02 16:43:03 -0700

    add header

--- presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/RemoveArgumentsAfterNotNullFromCoalesce.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/RemoveArgumentsAfterNotNullFromCoalesce.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.facebook.presto.sql.planner.iterative.rule;
 
 import com.facebook.presto.matching.Capture;
@@ -10,7 +23,6 @@ import com.facebook.presto.spi.relation.VariableReferenceExpression;
 import com.facebook.presto.sql.planner.iterative.Rule;
 import com.facebook.presto.sql.planner.plan.JoinNode;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 
 import java.util.List;
 import java.util.Map;
