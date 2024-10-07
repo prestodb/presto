4652269b44edd9c7ca83b9873e917fcc71c53247
wonderfulvamsi <wonderfulvamsi@gmail.com>
2024-10-07 20:17:06 +0530

    using docker image gchq/accumulo

--- presto-accumulo/src/test/java/com/facebook/presto/accumulo/TestingAccumuloServer.java
+++ presto-accumulo/src/test/java/com/facebook/presto/accumulo/TestingAccumuloServer.java
@@ -36,7 +36,7 @@ public class TestingAccumuloServer
     }
     private TestingAccumuloServer()
     {
-        accumuloContainer = new FixedHostPortGenericContainer<>("prestodev/accumulo:35");
+        accumuloContainer = new FixedHostPortGenericContainer<>("gchq/accumulo");
         accumuloContainer.withFixedExposedPort(ACCUMULO_MASTER_PORT, ACCUMULO_MASTER_PORT);
         accumuloContainer.withFixedExposedPort(ACCUMULO_TSERVER_PORT, ACCUMULO_TSERVER_PORT);
         accumuloContainer.withExposedPorts(ZOOKEEPER_PORT);
