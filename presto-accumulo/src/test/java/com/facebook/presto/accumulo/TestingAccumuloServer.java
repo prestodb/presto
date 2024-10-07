d463ce094c0cc92e4432df16315ffd3d75fe62e5
wonderfulvamsi <wonderfulvamsi@gmail.com>
2024-10-07 21:12:17 +0530

    using docker image version 35

--- presto-accumulo/src/test/java/com/facebook/presto/accumulo/TestingAccumuloServer.java
+++ presto-accumulo/src/test/java/com/facebook/presto/accumulo/TestingAccumuloServer.java
@@ -36,7 +36,7 @@ public class TestingAccumuloServer
     }
     private TestingAccumuloServer()
     {
-        accumuloContainer = new FixedHostPortGenericContainer<>("ghcr.io/trinodb/testing/accumulo");
+        accumuloContainer = new FixedHostPortGenericContainer<>("ghcr.io/trinodb/testing/accumulo:35");
         accumuloContainer.withFixedExposedPort(ACCUMULO_MASTER_PORT, ACCUMULO_MASTER_PORT);
         accumuloContainer.withFixedExposedPort(ACCUMULO_TSERVER_PORT, ACCUMULO_TSERVER_PORT);
         accumuloContainer.withExposedPorts(ZOOKEEPER_PORT);
