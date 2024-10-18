45831797d91316dd51b7d4cbc2f477db1c5d82ba
Bryan Cutler <cutlerb@gmail.com>
2024-10-18 09:39:56 -0700

    Remove bucketToPartition null check in native

--- presto-native-execution/presto_cpp/main/types/PrestoToVeloxQueryPlan.cpp
+++ presto-native-execution/presto_cpp/main/types/PrestoToVeloxQueryPlan.cpp
@@ -1856,7 +1856,7 @@ core::PlanFragment VeloxQueryPlanConverterBase::toVeloxQueryPlan(
       case protocol::SystemPartitioning::FIXED: {
         switch (systemPartitioningHandle->function) {
           case protocol::SystemPartitionFunction::ROUND_ROBIN: {
-            auto numPartitions = partitioningScheme.bucketToPartition ? partitioningScheme.bucketToPartition->size() : 1;
+            auto numPartitions = partitioningScheme.bucketToPartition->size();
 
             if (numPartitions == 1) {
               planFragment.planNode = core::PartitionedOutputNode::single(
@@ -1876,7 +1876,7 @@ core::PlanFragment VeloxQueryPlanConverterBase::toVeloxQueryPlan(
             return planFragment;
           }
           case protocol::SystemPartitionFunction::HASH: {
-            auto numPartitions = partitioningScheme.bucketToPartition ? partitioningScheme.bucketToPartition->size() : 1;
+            auto numPartitions = partitioningScheme.bucketToPartition->size();
 
             if (numPartitions == 1) {
               planFragment.planNode = core::PartitionedOutputNode::single(
