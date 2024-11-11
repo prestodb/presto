d1e7b5e5275c0fd03971746ea6f1b89b65718575
Ke <ke.404notfound@gmail.com>
2024-11-11 14:08:45 -0800

    [native] Add e2e table format test with native worker
    This is followup e2e test for https://github.com/prestodb/presto/pull/23923
    
    For a partitioned table, when respect_table_format is set to false,
    and table_format and hive_storage_format is different, the newly
    added partition should use hive_storage_format.

--- presto-native-execution/src/test/java/com/facebook/presto/nativeworker/TestPrestoNativeWriter.java
+++ presto-native-execution/src/test/java/com/facebook/presto/nativeworker/TestPrestoNativeWriter.java
@@ -88,6 +88,34 @@ public class TestPrestoNativeWriter
     }
 
     @Test
+    public void testTableFormats()
+    {
+        Session session = Session.builder(getSession())
+                .setSystemProperty("scale_writers", "true")
+                .setSystemProperty("table_writer_merge_operator_enabled", "true")
+                .setSystemProperty("task_writer_count", "1")
+                .setSystemProperty("task_partitioned_writer_count", "2")
+                .setCatalogSessionProperty("hive", "collect_column_statistics_on_write", "true")
+                .setCatalogSessionProperty("hive", "orc_compression_codec", "ZSTD")
+                .setCatalogSessionProperty("hive", "compression_codec", "NONE")
+                .setCatalogSessionProperty("hive", "hive_storage_format", "PARQUET")
+                .setCatalogSessionProperty("hive", "respect_table_format", "false").build();
+        String tmpTableName = generateRandomTableName();
+
+        for (String tableFormat : TABLE_FORMATS) {
+            try {
+                getQueryRunner().execute(session, String.format("CREATE TABLE %s (name VARCHAR, regionkey BIGINT, nationkey BIGINT) WITH (format = '%s', partitioned_by = ARRAY['regionkey','nationkey'])", tmpTableName, tableFormat));
+                // With different storage_format for partition than table format
+                getQueryRunner().execute(session, String.format("INSERT INTO %s SELECT name, regionkey, nationkey FROM nation", tmpTableName));
+                // TODO add support for presto to query each partition's format then verify written format is correct
+            }
+            finally {
+                dropTableIfExists(tmpTableName);
+            }
+        }
+    }
+
+    @Test
     public void testCreateUnpartitionedTableAsSelect()
     {
         // Generate temporary table name.
