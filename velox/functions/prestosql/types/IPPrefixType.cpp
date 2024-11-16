3eb2fa4e7f74b5f60bbbec96242c1fd64706f274
Yenda Li <yuandali@meta.com>
2024-11-16 13:08:28 -0800

    Back out "misc(revert ipaddress cast): Add ipprefix cast operators for varchar [1/n]" (#11561)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/11561
    
    Original commit changeset: 2b87c8942309
    
    Original Phabricator Diff: D65449935
    
    Reviewed By: amitkdutta
    
    Differential Revision: D66058188
    
    fbshipit-source-id: 3b530980114518e70c2e51af046f99a6c008b520

--- velox/functions/prestosql/types/IPPrefixType.cpp
+++ velox/functions/prestosql/types/IPPrefixType.cpp
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include <folly/small_vector.h>
 
 #include "velox/expression/CastExpr.h"
-#include "velox/expression/VectorWriters.h"
 #include "velox/functions/prestosql/types/IPPrefixType.h"
 
 namespace facebook::velox {
@@ -26,21 +26,11 @@ namespace {
 class IPPrefixCastOperator : public exec::CastOperator {
  public:
   bool isSupportedFromType(const TypePtr& other) const override {
-    switch (other->kind()) {
-      case TypeKind::VARCHAR:
-        return true;
-      default:
-        return false;
-    }
+    return false;
   }
 
   bool isSupportedToType(const TypePtr& other) const override {
-    switch (other->kind()) {
-      case TypeKind::VARCHAR:
-        return true;
-      default:
-        return false;
-    }
+    return false;
   }
 
   void castTo(
@@ -50,14 +40,8 @@ class IPPrefixCastOperator : public exec::CastOperator {
       const TypePtr& resultType,
       VectorPtr& result) const override {
     context.ensureWritable(rows, resultType, result);
-    switch (input.typeKind()) {
-      case TypeKind::VARCHAR:
-        return castFromString(input, context, rows, *result);
-      default:
-        VELOX_NYI(
-            "Cast from {} to IPPrefix not yet supported",
-            input.type()->toString());
-    }
+    VELOX_NYI(
+        "Cast from {} to IPPrefix not yet supported", input.type()->toString());
   }
 
   void castFrom(
@@ -67,83 +51,8 @@ class IPPrefixCastOperator : public exec::CastOperator {
       const TypePtr& resultType,
       VectorPtr& result) const override {
     context.ensureWritable(rows, resultType, result);
-    switch (resultType->kind()) {
-      case TypeKind::VARCHAR:
-        return castToString(input, context, rows, *result);
-      default:
-        VELOX_NYI(
-            "Cast from IPPrefix to {} not yet supported",
-            resultType->toString());
-    }
-  }
-
- private:
-  static void castToString(
-      const BaseVector& input,
-      exec::EvalCtx& context,
-      const SelectivityVector& rows,
-      BaseVector& result) {
-    auto* flatResult = result.as<FlatVector<StringView>>();
-    auto rowVector = input.as<RowVector>();
-    auto rowType = rowVector->type();
-    const auto* ipaddr = rowVector->childAt(ipaddress::kIpRowIndex)
-                             ->as<SimpleVector<int128_t>>();
-    const auto* prefix = rowVector->childAt(ipaddress::kIpPrefixRowIndex)
-                             ->as<SimpleVector<int8_t>>();
-    context.applyToSelectedNoThrow(rows, [&](auto row) {
-      const auto ipAddrVal = ipaddr->valueAt(row);
-      // The string representation of the last byte needs
-      // to be unsigned
-      const uint8_t prefixVal = prefix->valueAt(row);
-
-      // Copy the first 16 bytes into a ByteArray16.
-      folly::ByteArray16 addrBytes;
-      memcpy(&addrBytes, &ipAddrVal, ipaddress::kIPAddressBytes);
-      // Reverse the bytes to get the correct order. Similar to
-      // IPAddressType. We assume we're ALWAYS on a little endian machine.
-      // Note: for big endian, we should not reverse the bytes.
-      std::reverse(addrBytes.begin(), addrBytes.end());
-      // // Construct a V6 address from the ByteArray16.
-      folly::IPAddressV6 v6Addr(addrBytes);
-
-      // Inline func to get string for ipv4 or ipv6 string
-      const auto ipString =
-          (v6Addr.isIPv4Mapped()) ? v6Addr.createIPv4().str() : v6Addr.str();
-
-      // Format of string is {ipString}/{mask}
-      auto stringRet = fmt::format("{}/{}", ipString, prefixVal);
-
-      // Write the string to the result vector
-      exec::StringWriter<false> result(flatResult, row);
-      result.append(stringRet);
-      result.finalize();
-    });
-  }
-
-  static void castFromString(
-      const BaseVector& input,
-      exec::EvalCtx& context,
-      const SelectivityVector& rows,
-      BaseVector& result) {
-    auto* rowVectorResult = result.as<RowVector>();
-    const auto* ipPrefixStrings = input.as<SimpleVector<StringView>>();
-
-    context.applyToSelectedNoThrow(rows, [&](auto row) {
-      auto ipAddressStringView = ipPrefixStrings->valueAt(row);
-      auto tryIpPrefix = ipaddress::tryParseIpPrefixString(ipAddressStringView);
-      if (tryIpPrefix.hasError()) {
-        context.setStatus(row, std::move(tryIpPrefix.error()));
-      }
-
-      const auto& ipPrefix = tryIpPrefix.value();
-      auto writer = exec::VectorWriter<Row<int128_t, int8_t>>();
-      writer.init(*rowVectorResult);
-      writer.setOffset(row);
-      auto& rowWriter = writer.current();
-      rowWriter.get_writer_at<0>() = ipPrefix.first;
-      rowWriter.get_writer_at<1>() = ipPrefix.second;
-      writer.commit();
-    });
+    VELOX_NYI(
+        "Cast from IPPrefix to {} not yet supported", resultType->toString());
   }
 };
 
