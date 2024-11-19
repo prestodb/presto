479a5e80a893f33ec1119eab939d5c941a017bc9
Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>
2024-11-19 19:18:14 +0530

    modernizer for presto-redshift

--- presto-redshift/src/test/java/com/facebook/presto/plugin/redshift/TestRedshiftPlugin.java
+++ presto-redshift/src/test/java/com/facebook/presto/plugin/redshift/TestRedshiftPlugin.java
@@ -19,7 +19,9 @@ import com.facebook.presto.testing.TestingConnectorContext;
 import com.google.common.collect.ImmutableMap;
 import org.testng.annotations.Test;
 
-import static com.google.common.collect.Iterables.getOnlyElement;
+import java.util.stream.StreamSupport;
+
+import static com.google.common.collect.MoreCollectors.onlyElement;
 
 public class TestRedshiftPlugin
 {
@@ -27,7 +29,7 @@ public class TestRedshiftPlugin
     public void testCreateConnector()
     {
         Plugin plugin = new RedshiftPlugin();
-        ConnectorFactory factory = getOnlyElement(plugin.getConnectorFactories());
+        ConnectorFactory factory = StreamSupport.stream(plugin.getConnectorFactories().spliterator(), false).collect(onlyElement());
         factory.create("test", ImmutableMap.of("connection-url", "test"), new TestingConnectorContext());
     }
 }
