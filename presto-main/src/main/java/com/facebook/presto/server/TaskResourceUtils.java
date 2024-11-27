102a04af14491713da24a810c5c041138a9268ef
Shang Ma <shangma@meta.com>
2024-11-27 12:45:36 -0800

    add runtime stats for task create/update on the worker side

--- presto-main/src/main/java/com/facebook/presto/server/TaskResourceUtils.java
+++ presto-main/src/main/java/com/facebook/presto/server/TaskResourceUtils.java
@@ -13,6 +13,9 @@
  */
 package com.facebook.presto.server;
 
+import com.facebook.presto.common.RuntimeMetricName;
+import com.facebook.presto.common.RuntimeStats;
+import com.facebook.presto.common.RuntimeUnit;
 import com.facebook.presto.connector.ConnectorTypeSerdeManager;
 import com.facebook.presto.execution.TaskInfo;
 import com.facebook.presto.metadata.HandleResolver;
@@ -32,6 +35,7 @@ import java.util.List;
 
 import static com.facebook.presto.operator.OperatorInfoUnion.convertToOperatorInfo;
 import static com.facebook.presto.operator.OperatorInfoUnion.convertToOperatorInfoUnion;
+import static java.lang.Math.max;
 import static java.util.stream.Collectors.toList;
 
 public class TaskResourceUtils
@@ -522,5 +526,17 @@ public class TaskResourceUtils
         return metadataUpdateHandles.stream()
                 .map(e -> connectorTypeSerde.deserialize(handleResolver.getMetadataUpdateHandleClass(e.getId()), e.getBytes()))
                 .collect(toList());
+    }
+
+    public static void recordTaskUpdateReceivedTimeNanos(RuntimeStats runtimeStats, long cpuTimeNanos, long wallTimeNanos)
+    {
+        runtimeStats.addMetricValue(RuntimeMetricName.TASK_UPDATE_RECEIVED_CPU_TIME_NANOS, RuntimeUnit.NANO, max(0, cpuTimeNanos));
+        runtimeStats.addMetricValue(RuntimeMetricName.TASK_UPDATE_RECEIVED_WALL_TIME_NANOS, RuntimeUnit.NANO, max(0, wallTimeNanos));
+    }
+
+    public static void recordTaskExecutionCreationTimeNanos(RuntimeStats runtimeStats, long cpuTimeNanos, long wallTimeNanos)
+    {
+        runtimeStats.addMetricValue(RuntimeMetricName.TASK_EXECUTION_CREATION_CPU_TIME_NANOS, RuntimeUnit.NANO, max(0, cpuTimeNanos));
+        runtimeStats.addMetricValue(RuntimeMetricName.TASK_EXECUTION_CREATION_WALL_TIME_NANOS, RuntimeUnit.NANO, max(0, wallTimeNanos));
     }
 }
