f8ee46b97c512290ad60a73969598f56c0c0913a
auden-woolfson <auden.woolfson@ibm.com>
2025-01-09 15:13:24 -0800

    working on creating valid parquet files

--- presto-hive/src/test/java/com/facebook/presto/hive/benchmark/StoragePartitionLoaderBenchmark.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/benchmark/StoragePartitionLoaderBenchmark.java
@@ -15,6 +15,8 @@ package com.facebook.presto.hive.benchmark;
 
 import com.facebook.presto.benchmark.AbstractSqlBenchmark;
 import com.facebook.presto.benchmark.SimpleLineBenchmarkResultWriter;
+import com.facebook.presto.common.PageBuilder;
+import com.facebook.presto.common.type.Type;
 import com.facebook.presto.hive.HiveConnectorFactory;
 import com.facebook.presto.hive.HiveType;
 import com.facebook.presto.hive.metastore.Column;
@@ -23,23 +25,43 @@ import com.facebook.presto.hive.metastore.PrincipalPrivileges;
 import com.facebook.presto.hive.metastore.Storage;
 import com.facebook.presto.hive.metastore.StorageFormat;
 import com.facebook.presto.hive.metastore.Table;
+import com.facebook.presto.parquet.writer.ParquetWriter;
+import com.facebook.presto.parquet.writer.ParquetWriterOptions;
 import com.facebook.presto.testing.LocalQueryRunner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.io.Files;
+import io.airlift.units.DataSize;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat;
 import org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat;
 import org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe;
+import org.apache.parquet.example.data.Group;
+import org.apache.parquet.example.data.simple.SimpleGroupFactory;
+import org.apache.parquet.hadoop.ParquetFileWriter;
+import org.apache.parquet.hadoop.example.ExampleParquetWriter;
+import org.apache.parquet.hadoop.example.GroupWriteSupport;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.MessageTypeParser;
+import org.testcontainers.shaded.com.google.common.base.Charsets;
 
 import java.io.File;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import static com.facebook.presto.benchmark.BenchmarkQueryRunner.createLocalQueryRunner;
+import static com.facebook.presto.common.type.BigintType.BIGINT;
+import static com.facebook.presto.common.type.BooleanType.BOOLEAN;
+import static com.facebook.presto.common.type.IntegerType.INTEGER;
+import static com.facebook.presto.common.type.VarcharType.VARCHAR;
 import static com.facebook.presto.hive.HiveQueryRunner.METASTORE_CONTEXT;
 import static com.facebook.presto.hive.TestHiveUtil.createTestingFileHiveMetastore;
 import static com.facebook.presto.hive.metastore.PrestoTableType.EXTERNAL_TABLE;
+import static java.util.UUID.randomUUID;
+import static org.testng.Assert.fail;
 
 public class StoragePartitionLoaderBenchmark
         extends AbstractSqlBenchmark
@@ -100,16 +122,17 @@ public class StoragePartitionLoaderBenchmark
         File symlinkFile = new File(location, "symlink.txt");
         try {
             symlinkFile.createNewFile();
-            //Files.asCharSink(symlinkFile, Charsets.UTF_8)
-            //        .write(String.format("file:%s/datafile1.parquet\nfile:%s/datafile2.parquet\n", location, location));
+            Files.asCharSink(symlinkFile, Charsets.UTF_8)
+                    .write(String.format("file:%s/datafile1.parquet\nfile:%s/datafile2.parquet\n", location, location));
         }
         catch (Exception e) {
             throw new RuntimeException("Failed to create symlink file at: " + symlinkFile.getAbsolutePath(), e);
         }
 
         try {
-            new File(location, "datafile1.parquet").createNewFile();
-            new File(location, "datafile2.parquet").createNewFile();
+            //new File(location, "datafile1.parquet").createNewFile();
+            //new File(location, "datafile2.parquet").createNewFile();
+            createValidParquetFiles(location);
         }
         catch (Exception e) {
             throw new RuntimeException("Failed to create data files in: " + location, e);
@@ -137,5 +160,40 @@ public class StoragePartitionLoaderBenchmark
                 ImmutableMap.of(),
                 Optional.empty(),
                 Optional.empty());
+    }
+
+    public static void createValidParquetFiles(File location)
+            throws Exception
+    {
+        File df1 = new File(location, "datafile1.parquet");
+        File df2 = new File(location, "datafile2.parquet");
+
+        for (File df : ImmutableList.of(df1, df2)) {
+            List<Type> types = ImmutableList.of(BIGINT, INTEGER, VARCHAR, BOOLEAN);
+            List<String> names = ImmutableList.of("col_1", "col_2", "col_3", "col_4");
+            ParquetWriterOptions parquetWriterOptions = ParquetWriterOptions.builder()
+                    .setMaxPageSize(DataSize.succinctBytes(1000))
+                    .setMaxBlockSize(DataSize.succinctBytes(15000))
+                    .setMaxDictionaryPageSize(DataSize.succinctBytes(1000))
+                    .build();
+            try (com.facebook.presto.parquet.writer.ParquetWriter parquetWriter = new ParquetWriter()) {
+                Random rand = new Random();
+                for (int pageIdx = 0; pageIdx < 10; pageIdx++) {
+                    int pageRowCount = 100;
+                    PageBuilder pageBuilder = new PageBuilder(pageRowCount, types);
+                    for (int rowIdx = 0; rowIdx < pageRowCount; rowIdx++) {
+                        // maintain col_1's dictionary size approximately half of raw data
+                        BIGINT.writeLong(pageBuilder.getBlockBuilder(0), pageIdx * 100 + rand.nextInt(50));
+                        INTEGER.writeLong(pageBuilder.getBlockBuilder(1), rand.nextInt(100000000));
+                        VARCHAR.writeString(pageBuilder.getBlockBuilder(2), randomUUID().toString());
+                        BOOLEAN.writeBoolean(pageBuilder.getBlockBuilder(3), rand.nextBoolean());
+                        pageBuilder.declarePosition();
+                    }
+                    parquetWriter.write(pageBuilder.build());
+                }
+            } catch (Exception e) {
+                fail("Should not fail, but throw an exception as follows:", e);
+            }
+        }
     }
 }
