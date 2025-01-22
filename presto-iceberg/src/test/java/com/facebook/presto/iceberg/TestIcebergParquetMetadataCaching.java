7fd56185b9479b8b82136ade6ee217c20f0dac47
Mahadevuni Naveen Kumar <nmahadevuni@gmail.com>
2025-01-22 12:13:38 +0530

    Fix flaky Iceberg parquet metadata cache test

--- presto-iceberg/src/test/java/com/facebook/presto/iceberg/TestIcebergParquetMetadataCaching.java
+++ presto-iceberg/src/test/java/com/facebook/presto/iceberg/TestIcebergParquetMetadataCaching.java
@@ -45,7 +45,7 @@ public class TestIcebergParquetMetadataCaching
                 PARQUET,
                 false,
                 true,
-                OptionalInt.of(2),
+                OptionalInt.of(1),
                 Optional.empty());
     }
 
