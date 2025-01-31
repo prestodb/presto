dcc7dc9011c2abf082d440a631050bef7d261b8b
Pratik Joseph Dabre <pdabre12@gmail.com>
2025-01-31 15:18:36 -0800

    [native] Integrate SPI with sidecar and add e2e native function validation tests

--- presto-native-sidecar-plugin/src/main/java/com/facebook/presto/sidecar/functionNamespace/NativeFunctionDefinitionProvider.java
+++ presto-native-sidecar-plugin/src/main/java/com/facebook/presto/sidecar/functionNamespace/NativeFunctionDefinitionProvider.java
@@ -13,17 +13,26 @@
  */
 package com.facebook.presto.sidecar.functionNamespace;
 
+import com.facebook.airlift.http.client.HttpClient;
+import com.facebook.airlift.http.client.HttpUriBuilder;
+import com.facebook.airlift.http.client.Request;
 import com.facebook.airlift.json.JsonCodec;
 import com.facebook.airlift.log.Logger;
 import com.facebook.presto.functionNamespace.JsonBasedUdfFunctionMetadata;
 import com.facebook.presto.functionNamespace.UdfFunctionSignatureMap;
+import com.facebook.presto.sidecar.ForSidecarInfo;
+import com.facebook.presto.spi.Node;
 import com.facebook.presto.spi.NodeManager;
 import com.facebook.presto.spi.PrestoException;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
+import static com.facebook.airlift.http.client.JsonResponseHandler.createJsonResponseHandler;
+import static com.facebook.airlift.http.client.Request.Builder.prepareGet;
 import static com.facebook.presto.spi.StandardErrorCode.INVALID_ARGUMENTS;
 import static java.util.Objects.requireNonNull;
 
@@ -31,23 +40,44 @@ public class NativeFunctionDefinitionProvider
         implements FunctionDefinitionProvider
 {
     private static final Logger log = Logger.get(NativeFunctionDefinitionProvider.class);
-
     private final JsonCodec<Map<String, List<JsonBasedUdfFunctionMetadata>>> nativeFunctionSignatureMapJsonCodec;
+    private final NodeManager nodeManager;
+    private final HttpClient httpClient;
+    private static final String FUNCTION_SIGNATURES_ENDPOINT = "/v1/functions";
 
     @Inject
-    public NativeFunctionDefinitionProvider(JsonCodec<Map<String, List<JsonBasedUdfFunctionMetadata>>> nativeFunctionSignatureMapJsonCodec)
+    public NativeFunctionDefinitionProvider(
+            @ForSidecarInfo HttpClient httpClient,
+            JsonCodec<Map<String, List<JsonBasedUdfFunctionMetadata>>> nativeFunctionSignatureMapJsonCodec,
+            NodeManager nodeManager)
     {
-        this.nativeFunctionSignatureMapJsonCodec = requireNonNull(nativeFunctionSignatureMapJsonCodec, "nativeFunctionSignatureMapJsonCodec is null");
+        this.nativeFunctionSignatureMapJsonCodec =
+                requireNonNull(nativeFunctionSignatureMapJsonCodec, "nativeFunctionSignatureMapJsonCodec is null");
+        this.nodeManager = requireNonNull(nodeManager, "nodeManager is null");
+        this.httpClient = requireNonNull(httpClient, "typeManager is null");
     }
 
     @Override
     public UdfFunctionSignatureMap getUdfDefinition(NodeManager nodeManager)
     {
         try {
-            throw new UnsupportedOperationException();
+            Request request = prepareGet().setUri(getSidecarLocation()).build();
+            Map<String, List<JsonBasedUdfFunctionMetadata>> nativeFunctionSignatureMap = httpClient.execute(request, createJsonResponseHandler(nativeFunctionSignatureMapJsonCodec));
+            return new UdfFunctionSignatureMap(ImmutableMap.copyOf(nativeFunctionSignatureMap));
         }
         catch (Exception e) {
             throw new PrestoException(INVALID_ARGUMENTS, "Failed to get functions from sidecar.", e);
         }
+    }
+
+    private URI getSidecarLocation()
+    {
+        Node sidecarNode = nodeManager.getSidecarNode();
+        return HttpUriBuilder.uriBuilder()
+                .scheme("http")
+                .host(sidecarNode.getHost())
+                .port(sidecarNode.getHostAndPort().getPort())
+                .appendPath(FUNCTION_SIGNATURES_ENDPOINT)
+                .build();
     }
 }
