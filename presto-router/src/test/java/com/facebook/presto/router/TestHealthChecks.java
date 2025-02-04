1aec6719a6cef10bb69ad6b363f85a73d070509d
auden-woolfson <auden.woolfson@ibm.com>
2025-02-04 15:29:53 -0800

    fix checkstyle;

--- presto-router/src/test/java/com/facebook/presto/router/TestHealthChecks.java
+++ presto-router/src/test/java/com/facebook/presto/router/TestHealthChecks.java
@@ -33,6 +33,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import javax.servlet.http.HttpServletRequest;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.net.URI;
@@ -46,79 +47,79 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.testng.Assert.assertFalse;
 
 public class TestHealthChecks
-    {
-        private List<TestingPrestoServer> prestoServers;
-        private ClusterManager clusterManager;
+{
+    private List<TestingPrestoServer> prestoServers;
+    private ClusterManager clusterManager;
 
-        @BeforeClass
-        public void setup()
+    @BeforeClass
+    public void setup()
             throws Exception
-        {
-            String configTemplate = new String(
-                    Files.readAllBytes(Paths.get(this.getClass().getClassLoader().getResource("health-check-test-router-template.json").getPath())));
+    {
+        String configTemplate = new String(
+                Files.readAllBytes(Paths.get(this.getClass().getClassLoader().getResource("health-check-test-router-template.json").getPath())));
 
-            File configFile = File.createTempFile("router", "json");
+        File configFile = File.createTempFile("router", "json");
 
-            Logging.initialize();
+        Logging.initialize();
 
-            // set up server
-            ImmutableList.Builder<TestingPrestoServer> builder = ImmutableList.builder();
-            for (int i = 0; i < 3; ++i) {
-                TestingPrestoServer server = new TestingPrestoServer();
-                server.installPlugin(new TpchPlugin());
-                server.createCatalog("tpch", "tpch");
-                server.refreshNodes();
-                builder.add(server);
-            }
-
-            prestoServers = builder.build();
-
-            for (TestingPrestoServer s : prestoServers) {
-                configTemplate = configTemplate.replaceFirst("\\$\\{SERVERS}", String.format("[ \"%s\" ]", s.getBaseUrl().toString()));
-            }
-
-            FileOutputStream fileOutputStream = new FileOutputStream(configFile);
-            fileOutputStream.write(configTemplate.getBytes(UTF_8));
-
-            fileOutputStream.close();
-
-            Bootstrap app = new Bootstrap(
-                    new TestingNodeModule("test"),
-                    new TestingHttpServerModule(), new JsonModule(),
-                    new JaxrsModule(true),
-                    new ServerSecurityModule(),
-                    new RouterModule());
-
-            Injector injector = app.doNotInitializeLogging().setRequiredConfigurationProperty("router.config-file", configFile.getAbsolutePath()).initialize();
-            clusterManager = injector.getInstance(ClusterManager.class);
+        // set up server
+        ImmutableList.Builder<TestingPrestoServer> builder = ImmutableList.builder();
+        for (int i = 0; i < 3; ++i) {
+            TestingPrestoServer server = new TestingPrestoServer();
+            server.installPlugin(new TpchPlugin());
+            server.createCatalog("tpch", "tpch");
+            server.refreshNodes();
+            builder.add(server);
         }
 
-        @AfterClass(alwaysRun = true)
-        public void tearDownServer()
-                throws Exception
-        {
-            for (TestingPrestoServer prestoServer : prestoServers) {
-                prestoServer.close();
-            }
+        prestoServers = builder.build();
+
+        for (TestingPrestoServer s : prestoServers) {
+            configTemplate = configTemplate.replaceFirst("\\$\\{SERVERS}", String.format("[ \"%s\" ]", s.getBaseUrl().toString()));
         }
 
-        @Test(enabled = false)
-        public void testHealthChecks()
-        {
-            prestoServers.get(0).stopResponding();
-            List<URI> destinations = new ArrayList();
-            for (int i = 0; i < 3; i++) {
-               destinations.add(getDestinationWrapper().orElse(URI.create("null")));
-            }
-            assertFalse(destinations.contains(prestoServers.get(0).getBaseUrl()));
-        }
+        FileOutputStream fileOutputStream = new FileOutputStream(configFile);
+        fileOutputStream.write(configTemplate.getBytes(UTF_8));
 
-        private Optional<URI> getDestinationWrapper()
-        {
-            HttpServletRequest request = new MockRouterHttpServletRequest(
-                    ImmutableListMultimap.of(),
-                    "testRemote",
-                    ImmutableMap.of());
-            return clusterManager.getDestination(new RequestInfo(request, ""));
+        fileOutputStream.close();
+
+        Bootstrap app = new Bootstrap(
+                new TestingNodeModule("test"),
+                new TestingHttpServerModule(), new JsonModule(),
+                new JaxrsModule(true),
+                new ServerSecurityModule(),
+                new RouterModule());
+
+        Injector injector = app.doNotInitializeLogging().setRequiredConfigurationProperty("router.config-file", configFile.getAbsolutePath()).initialize();
+        clusterManager = injector.getInstance(ClusterManager.class);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownServer()
+            throws Exception
+    {
+        for (TestingPrestoServer prestoServer : prestoServers) {
+            prestoServer.close();
         }
+    }
+
+    @Test(enabled = false)
+    public void testHealthChecks()
+    {
+        prestoServers.get(0).stopResponding();
+        List<URI> destinations = new ArrayList();
+        for (int i = 0; i < 3; i++) {
+            destinations.add(getDestinationWrapper().orElse(URI.create("null")));
+        }
+        assertFalse(destinations.contains(prestoServers.get(0).getBaseUrl()));
+    }
+
+    private Optional<URI> getDestinationWrapper()
+    {
+        HttpServletRequest request = new MockRouterHttpServletRequest(
+                ImmutableListMultimap.of(),
+                "testRemote",
+                ImmutableMap.of());
+        return clusterManager.getDestination(new RequestInfo(request, ""));
+    }
 }
