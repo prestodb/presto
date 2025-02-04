71f75b5db753bff8b6963cde580c9296a34c0238
sureshbabu-areekara <sureshbabu.areekara@ibm.com>
2025-02-04 23:05:45 +0530

    Dummy commit

--- presto-open-telemetry/src/main/java/com/facebook/presto/opentelemetry/OpenTelemetryBuilder.java
+++ presto-open-telemetry/src/main/java/com/facebook/presto/opentelemetry/OpenTelemetryBuilder.java
@@ -26,6 +26,7 @@ import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
+//Dummy commit
 public final class OpenTelemetryBuilder
 {
     private OpenTelemetryBuilder()
