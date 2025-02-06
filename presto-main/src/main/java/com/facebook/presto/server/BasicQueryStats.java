c6ba59f8ca2b8e4aacd6e9405a25354c73e5fd70
Shakyan Kushwaha <Shakyan.Kushwah@ibm.com>
2025-02-06 13:54:28 +0530

    [native] Differentiate number of drivers and number of splits in stats reporting (Part-1)

--- presto-main/src/main/java/com/facebook/presto/server/BasicQueryStats.java
+++ presto-main/src/main/java/com/facebook/presto/server/BasicQueryStats.java
@@ -61,6 +61,11 @@ public class BasicQueryStats
     private final int runningDrivers;
     private final int completedDrivers;
 
+    private final int totalSplits;
+    private final int queuedSplits;
+    private final int runningSplits;
+    private final int completedSplits;
+
     private final DataSize rawInputDataSize;
     private final long rawInputPositions;
 
@@ -98,6 +103,10 @@ public class BasicQueryStats
             @JsonProperty("queuedDrivers") int queuedDrivers,
             @JsonProperty("runningDrivers") int runningDrivers,
             @JsonProperty("completedDrivers") int completedDrivers,
+            @JsonProperty("totalSplits") int totalSplits,
+            @JsonProperty("queuedSplits") int queuedSplits,
+            @JsonProperty("runningSplits") int runningSplits,
+            @JsonProperty("completedSplits") int completedSplits,
             @JsonProperty("rawInputDataSize") DataSize rawInputDataSize,
             @JsonProperty("rawInputPositions") long rawInputPositions,
             @JsonProperty("cumulativeUserMemory") double cumulativeUserMemory,
@@ -123,6 +132,7 @@ public class BasicQueryStats
         this.elapsedTime = requireNonNull(elapsedTime, "elapsedTime is null");
         this.executionTime = requireNonNull(executionTime, "executionTime is null");
         this.analysisTime = requireNonNull(analysisTime, "analysisTime is null");
+
         this.runningTasks = runningTasks;
         this.peakRunningTasks = peakRunningTasks;
 
@@ -134,6 +144,14 @@ public class BasicQueryStats
         this.runningDrivers = runningDrivers;
         checkArgument(completedDrivers >= 0, "completedDrivers is negative");
         this.completedDrivers = completedDrivers;
+        checkArgument(totalSplits >= 0, "totalSplits is negative");
+        this.totalSplits = totalSplits;
+        checkArgument(queuedSplits >= 0, "queuedSplits is negative");
+        this.queuedSplits = queuedSplits;
+        checkArgument(runningSplits >= 0, "runningSplits is negative");
+        this.runningSplits = runningSplits;
+        checkArgument(completedSplits >= 0, "completedSplits is negative");
+        this.completedSplits = completedSplits;
 
         this.rawInputDataSize = requireNonNull(rawInputDataSize);
         this.rawInputPositions = rawInputPositions;
@@ -172,6 +190,10 @@ public class BasicQueryStats
                 queryStats.getQueuedDrivers(),
                 queryStats.getRunningDrivers(),
                 queryStats.getCompletedDrivers(),
+                queryStats.getTotalSplits(),
+                queryStats.getQueuedSplits(),
+                queryStats.getRunningSplits(),
+                queryStats.getCompletedSplits(),
                 succinctBytes(queryStats.getRawInputDataSizeInBytes()),
                 queryStats.getRawInputPositions(),
                 queryStats.getCumulativeUserMemory(),
@@ -201,6 +223,10 @@ public class BasicQueryStats
                 new Duration(0, MILLISECONDS),
                 new Duration(0, MILLISECONDS),
                 new Duration(0, MILLISECONDS),
+                0,
+                0,
+                0,
+                0,
                 0,
                 0,
                 0,
@@ -425,5 +451,33 @@ public class BasicQueryStats
     public Duration getAnalysisTime()
     {
         return analysisTime;
+    }
+
+    @ThriftField(30)
+    @JsonProperty
+    public int getTotalSplits()
+    {
+        return totalSplits;
+    }
+
+    @ThriftField(31)
+    @JsonProperty
+    public int getQueuedSplits()
+    {
+        return queuedSplits;
+    }
+
+    @ThriftField(32)
+    @JsonProperty
+    public int getRunningSplits()
+    {
+        return runningSplits;
+    }
+
+    @ThriftField(33)
+    @JsonProperty
+    public int getCompletedSplits()
+    {
+        return completedSplits;
     }
 }
