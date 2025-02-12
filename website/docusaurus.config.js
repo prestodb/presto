796c7171a1a76a1307313204540c315e4e9f81c3
Christian Zentgraf <kitgocz@gmail.com>
2025-02-11 18:41:48 -0800

    misc(website): Update dependencies (#11895)
    Summary:
    Ran "npm update --save" to update the dependencies. The dependencies were out of date with security vulnerabilities so it is best to update them to the latest versions.
    The commit also fixes syntax errors in markdown that caused build failures with the new versions.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/11895
    
    Reviewed By: kagamiori
    
    Differential Revision: D69496721
    
    Pulled By: kgpai
    
    fbshipit-source-id: 597e844ac30d5f270b16bfdd982bd70dd5bd0110

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -34,13 +34,10 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          onUntruncatedBlogPosts: 'ignore',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
