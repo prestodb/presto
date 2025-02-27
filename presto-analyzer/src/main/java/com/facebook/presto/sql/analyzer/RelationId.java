74baaa1eef9f6220631a4083f90c6f31a5d8a253
Rebecca Schlussel <rschlussel@fb.com>
2025-02-27 16:11:26 -0500

    Fix check_access_control_on_utilized_columns_only for CTE
    Previously we weren't associating the with query with the columns used
    from it in the main part of the query, so we wouldn't consider any
    columns as used.  When the usage within the cte or outside of it was
    just an identifier, we would have the utilized column collected anyway
    because the column collected in the main query kept the source table
    information. However, if there was an expression in between, we would
    lose that information, and we wouldn't check access control for the
    column that was used in an expression in the cte.
    
    Example:
    
    For the following query
    ```
    with cte as (select  x as c1, z + 1 as c2 from t13) select c1, c2 from (select * from cte)
    ```
    
    Previously we would only check access permissions on t13.x, but not
    t13.z.  With this change we will check column access on both 13.x and
    t13.z.
    
    Fix empty relations case
    
    Add error message to utilized columns warning.
    
    handle ctes used multiple times
    
    fixup >1 reference support
    
    Add tests for multiple cte with same name
    
    Co-authored-by: Kevin Tang <tangk@meta.com>
    
    Add comments clarification and test for multiple same name for multiple columns

--- presto-analyzer/src/main/java/com/facebook/presto/sql/analyzer/RelationId.java
+++ presto-analyzer/src/main/java/com/facebook/presto/sql/analyzer/RelationId.java
@@ -53,6 +53,11 @@ public class RelationId
         return sourceNode == null;
     }
 
+    public Node getSourceNode()
+    {
+        return sourceNode;
+    }
+
     @Override
     public boolean equals(Object o)
     {
