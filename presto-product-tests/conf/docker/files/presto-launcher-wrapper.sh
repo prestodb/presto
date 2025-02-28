a63ccfa3cfec5605262a3bdd1c4256b8d3fc19a3
Anant Aneja <1797669+aaneja@users.noreply.github.com>
2025-02-28 14:00:28 +0530

    Tweak launcher to choose the right JVM properties
    Reenabled tests for JDK 8

--- presto-product-tests/conf/docker/files/presto-launcher-wrapper.sh
+++ presto-product-tests/conf/docker/files/presto-launcher-wrapper.sh
@@ -24,9 +24,19 @@ fi
 echo "Starting Presto with java set to :"
 java -version
 
-/docker/volumes/presto-server/bin/launcher \
+# Check if Java version is 17
+# This relies on the version string adhering to the format "17.x.xx"
+JVM_CONFIG="${PRESTO_CONFIG_DIRECTORY}/jvm.config"
+JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d'.' -f1)
+if [ "$JAVA_VERSION" == "17" ]; then
+    echo "Java version is 17, setting custom JVM config"
+    JVM_CONFIG="${PRESTO_CONFIG_DIRECTORY}/jvm17.config"
+fi
+
+/docker/volumes/presto-server/bin/launcher  \
   -Dnode.id="${HOSTNAME}" \
   --etc-dir="${PRESTO_CONFIG_DIRECTORY}" \
+  --jvm-config="${JVM_CONFIG}" \
   --config="${PRESTO_CONFIG_DIRECTORY}/${CONFIG}".properties \
   --data-dir=/var/presto \
   "$@"
