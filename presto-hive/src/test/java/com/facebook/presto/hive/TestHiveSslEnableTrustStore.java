a0de629e2f8184c391cf93c911661ac47c61eafc
Shijin <bibith4@gmail.com>
2025-03-14 15:35:29 +0530

    Changes to fix product test failures

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveSslEnableTrustStore.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveSslEnableTrustStore.java
@@ -52,6 +52,10 @@ public class TestHiveSslEnableTrustStore
                 ImmutableMap.<String, String>builder()
                         // This is required when connecting to ssl enabled hms
                         .put("hive.metastore.thrift.client.tls.enabled", "true")
+                        .put("hive.s3.path-style-access", "true")
+                        .put("hive.non-managed-table-writes-enabled", "true")
+//                        .put("hive.metastore.thrift.client.tls.keystore.path", this.getClass().getClassLoader().getResource("hive_ssl_enable/hive-metastore.jks").getPath())
+//                        .put("hive.metastore.thrift.client.tls.keystore.password", "123456")
                         .put("hive.metastore.thrift.client.tls.truststore.path", this.getClass().getClassLoader().getResource("hive_ssl_enable/hive-metastore-truststore.jks").getPath())
                         .put("hive.metastore.thrift.client.tls.truststore.password", "123456")
                         .build(),
