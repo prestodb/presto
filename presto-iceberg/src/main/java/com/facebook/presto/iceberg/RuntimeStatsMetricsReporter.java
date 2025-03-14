4905d6a98f7490614c7a48f5007d33f02a8da7f8
j-sund <joshua.sundararaman@gmail.com>
2025-03-14 16:37:23 -0400

    More method stubs for MetricsReporter

--- presto-iceberg/src/main/java/com/facebook/presto/iceberg/RuntimeStatsMetricsReporter.java
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/RuntimeStatsMetricsReporter.java
@@ -1,12 +1,26 @@
 package com.facebook.presto.iceberg;
 
 import com.facebook.presto.common.RuntimeStats;
+import org.apache.iceberg.metrics.MetricsReport;
 import org.apache.iceberg.metrics.MetricsReporter;
 
 public class RuntimeStatsMetricsReporter implements MetricsReporter {
 
     private final RuntimeStats runtimeStats;
 
+    public RuntimeStatsMetricsReporter(RuntimeStats runtimeStats) {
+        this.runtimeStats = runtimeStats;
+    }
 
+    public void reportMetrics(MetricsReport report) {
+
+        // for (Metric metric in report) {
+        //  runtimeStats.update(...)
+        // }
+    }
+
+    @Override
+    public void report(MetricsReport metricsReport) {
+
+    }
 }
-
