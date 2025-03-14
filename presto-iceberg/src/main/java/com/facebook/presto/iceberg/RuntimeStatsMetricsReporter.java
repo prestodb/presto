cc7f0af1e91e0d9fa6b4149ccc667356db845afd
j-sund <joshua.sundararaman@gmail.com>
2025-03-14 16:20:23 -0400

    Initial template for RuntimeStatsMetricsReporter

---
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/RuntimeStatsMetricsReporter.java
@@ -0,0 +1,12 @@
+package com.facebook.presto.iceberg;
+
+import com.facebook.presto.common.RuntimeStats;
+import org.apache.iceberg.metrics.MetricsReporter;
+
+public class RuntimeStatsMetricsReporter implements MetricsReporter {
+
+    private final RuntimeStats runtimeStats;
+
+
+}
+
