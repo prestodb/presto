4d294671ca3916634859eac8ab79270e83fcee0e
j-sund <joshua.sundararaman@gmail.com>
2025-03-14 18:32:51 -0400

    Added totalDuration metric tracker

--- presto-iceberg/src/main/java/com/facebook/presto/iceberg/RuntimeStatsMetricsReporter.java
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/RuntimeStatsMetricsReporter.java
@@ -1,8 +1,14 @@
 package com.facebook.presto.iceberg;
 
 import com.facebook.presto.common.RuntimeStats;
+import com.facebook.presto.common.RuntimeUnit;
+import io.grpc.services.MetricReport;
 import org.apache.iceberg.metrics.MetricsReport;
 import org.apache.iceberg.metrics.MetricsReporter;
+import org.apache.iceberg.metrics.ScanReport;
+
+import java.util.Objects;
+//com.facebook.presto.tests.statistics;
 
 public class RuntimeStatsMetricsReporter implements MetricsReporter {
 
@@ -12,15 +18,41 @@ public class RuntimeStatsMetricsReporter implements MetricsReporter {
         this.runtimeStats = runtimeStats;
     }
 
-    public void reportMetrics(MetricsReport report) {
-
-        // for (Metric metric in report) {
-        //  runtimeStats.update(...)
-        // }
-    }
-
     @Override
-    public void report(MetricsReport metricsReport) {
+    public void report(MetricsReport report) {
+
+        if(!(report instanceof ScanReport)) {
+            return;
+        }
+
+        ScanReport scanReport = (ScanReport)report;
+
+        //tablename-nameofmetrics
+        //tablename-totalPlanningDuration
+
+        //
+
+        String table_name = scanReport.tableName();
+        // check null
+        if(scanReport.scanMetrics().totalPlanningDuration() != null && Objects.requireNonNull(scanReport.scanMetrics().totalPlanningDuration()).totalDuration() != null)
+        {
+            runtimeStats.addMetricValue(table_name + "-totalPlanningDuration", RuntimeUnit.NANO,
+                    scanReport.scanMetrics().totalPlanningDuration().totalDuration().toNanos() );
+        }
+
+        // do rest for ScanReport
+
+        // can test by run Query on iCeberg Connector
+        // check webUi in runtimestats
+
+
+
+
+
+
+
 
     }
+
+
 }
