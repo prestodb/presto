0878d3c21e843cf068bdfd1d673ca5590344bfa5
mohsaka <135669458+mohsaka@users.noreply.github.com>
2025-03-24 13:15:56 -0700

    Add requiredColumns field to TableFunctionAnalysis The new field allows the table function to declare during analysis which columns from the input tables are necessary to execute the function.
    The required columns can be then validated by the analyzer.
    This declaration can be also used by the optimizer to prune
    any input columns that are not used by the table function.
    
    Changes adapted from trino/PR#15256
    Original commit: c77995bde37f79c44c9d74336f57b32a2b142570
    Author: kasiafi
    
    Co-authored-by: kasiafi <30203062+kasiafi@users.noreply.github.com>

--- presto-spi/src/main/java/com/facebook/presto/spi/function/table/TableFunctionAnalysis.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/function/table/TableFunctionAnalysis.java
@@ -13,7 +13,12 @@
  */
 package com.facebook.presto.spi.function.table;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.facebook.presto.spi.function.table.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
@@ -32,19 +37,32 @@ import static java.util.Objects.requireNonNull;
  */
 public final class TableFunctionAnalysis
 {
+    // a map from table argument name to list of column indexes for all columns required from the table argument
+    private final Map<String, List<Integer>> requiredColumns;
+
     private final Optional<Descriptor> returnedType;
     private final ConnectorTableFunctionHandle handle;
 
-    private TableFunctionAnalysis(Optional<Descriptor> returnedType, ConnectorTableFunctionHandle handle)
+    private TableFunctionAnalysis(Optional<Descriptor> returnedType, Map<String, List<Integer>> requiredColumns, ConnectorTableFunctionHandle handle)
     {
         this.returnedType = requireNonNull(returnedType, "returnedType is null");
         returnedType.ifPresent(descriptor -> checkArgument(descriptor.isTyped(), "field types not specified"));
+        this.requiredColumns = Collections.unmodifiableMap(
+                requiredColumns.entrySet().stream()
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry -> Collections.unmodifiableList(entry.getValue()))));
         this.handle = requireNonNull(handle, "handle is null");
     }
 
     public Optional<Descriptor> getReturnedType()
     {
         return returnedType;
+    }
+
+    public Map<String, List<Integer>> getRequiredColumns()
+    {
+        return requiredColumns;
     }
 
     public ConnectorTableFunctionHandle getHandle()
@@ -60,6 +78,7 @@ public final class TableFunctionAnalysis
     public static final class Builder
     {
         private Descriptor returnedType;
+        private final Map<String, List<Integer>> requiredColumns = new HashMap<>();
         private ConnectorTableFunctionHandle handle = new ConnectorTableFunctionHandle() {};
 
         private Builder() {}
@@ -67,6 +86,12 @@ public final class TableFunctionAnalysis
         public Builder returnedType(Descriptor returnedType)
         {
             this.returnedType = returnedType;
+            return this;
+        }
+
+        public Builder requiredColumns(String tableArgument, List<Integer> columns)
+        {
+            this.requiredColumns.put(tableArgument, columns);
             return this;
         }
 
@@ -78,7 +103,7 @@ public final class TableFunctionAnalysis
 
         public TableFunctionAnalysis build()
         {
-            return new TableFunctionAnalysis(Optional.ofNullable(returnedType), handle);
+            return new TableFunctionAnalysis(Optional.ofNullable(returnedType), requiredColumns, handle);
         }
     }
 }
