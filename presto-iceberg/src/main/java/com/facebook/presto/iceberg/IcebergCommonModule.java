agrawalreetika/prestodb

Add Iceberg StatisticsFile cache invalidation procedure

presto-iceberg/src/main/java/com/facebook/presto/iceberg/IcebergCommonModule.java
@@ -46,6 +46,7 @@ import com.facebook.presto.iceberg.nessie.IcebergNessieConfig;
 import com.facebook.presto.iceberg.optimizer.IcebergPlanOptimizerProvider;
 import com.facebook.presto.iceberg.procedure.ExpireSnapshotsProcedure;
 import com.facebook.presto.iceberg.procedure.FastForwardBranchProcedure;
+import com.facebook.presto.iceberg.procedure.IcebergStatisticsFileCacheInvalidationProcedure;
 import com.facebook.presto.iceberg.procedure.RegisterTableProcedure;
 import com.facebook.presto.iceberg.procedure.RemoveOrphanFiles;
 import com.facebook.presto.iceberg.procedure.RollbackToSnapshotProcedure;
@@ -183,6 +184,7 @@ public class IcebergCommonModule
         procedures.addBinding().toProvider(FastForwardBranchProcedure.class).in(Scopes.SINGLETON);
         procedures.addBinding().toProvider(SetCurrentSnapshotProcedure.class).in(Scopes.SINGLETON);
         procedures.addBinding().toProvider(SetTablePropertyProcedure.class).in(Scopes.SINGLETON);
+        procedures.addBinding().toProvider(IcebergStatisticsFileCacheInvalidationProcedure.class).in(Scopes.SINGLETON);
 
         if (buildConfigObject(MetastoreClientConfig.class).isInvalidateMetastoreCacheProcedureEnabled()) {
             procedures.addBinding().toProvider(InvalidateMetastoreCacheProcedure.class).in(Scopes.SINGLETON);

presto-iceberg/src/test/java/com/facebook/presto/iceberg/IcebergDistributedTestBase.java
@@ -2744,6 +2744,42 @@ public abstract class IcebergDistributedTestBase
         assertQueryFails(query, Pattern.quote(format("Unable to parse sort field: [%s]", sortField)));
     }
 
+    public void testStatisticsFileCacheProcedure()
+    {
+        assertQuerySucceeds("CREATE TABLE test_statistics_file_cache_procedure(i int)");
+        assertUpdate("INSERT INTO test_statistics_file_cache_procedure VALUES 1, 2, 3, 4, 5", 5);
+        assertQuerySucceeds("ANALYZE test_statistics_file_cache_procedure");
+        for (int i = 0; i < 3; i++) {
+            assertQuerySucceeds("SHOW STATS FOR test_statistics_file_cache_procedure");
+        }
+
+        String jmxMetricsQuery = format("SELECT sum(\"cachestats.hitcount\"), sum(\"cachestats.size\"), sum(\"cachestats.misscount\") " +
+                "from jmx.current.\"com.facebook.presto.iceberg.statistics:name=%s,type=statisticsfilecache\"", getSession().getCatalog().get());
+
+        MaterializedResult result = computeActual(jmxMetricsQuery);
+        long afterHitCount = (long) result.getMaterializedRows().get(0).getField(0);
+        long afterCacheSize = (long) result.getMaterializedRows().get(0).getField(1);
+        long afterMissCount = (long) result.getMaterializedRows().get(0).getField(2);
+        assertTrue(afterHitCount > 0);
+        assertTrue(afterCacheSize > 0);
+
+        //test invalidate_iceberg_statistics_file_cache procedure
+        assertQuerySucceeds(format("CALL %s.system.invalidate_iceberg_statistics_file_cache()", getSession().getCatalog().get()));
+        MaterializedResult resultAfterProcedure = computeActual(jmxMetricsQuery);
+        long afterProcedureCacheSize = (long) resultAfterProcedure.getMaterializedRows().get(0).getField(1);
+        assertTrue(afterProcedureCacheSize == 0);
+
+        assertQuerySucceeds("SHOW STATS FOR test_statistics_file_cache_procedure");
+
+        MaterializedResult resultAfter = computeActual(jmxMetricsQuery);
+        long newCacheSize = (long) resultAfter.getMaterializedRows().get(0).getField(1);
+        long newMissCount = (long) resultAfter.getMaterializedRows().get(0).getField(2);
+        assertTrue(newCacheSize > 0);
+        assertTrue(afterMissCount < newMissCount);
+
+        getQueryRunner().execute("DROP TABLE test_statistics_file_cache_procedure");
+    }
+
     @DataProvider(name = "sortedTableWithSortTransform")
     public static Object[][] sortedTableWithSortTransform()
     {

prestodb/presto

presto-iceberg/src/main/java/com/facebook/presto/iceberg/IcebergCommonModule.java
@@ -52,6 +52,7 @@ import com.facebook.presto.iceberg.procedure.RollbackToSnapshotProcedure;
 import com.facebook.presto.iceberg.procedure.RollbackToTimestampProcedure;
 import com.facebook.presto.iceberg.procedure.SetCurrentSnapshotProcedure;
 import com.facebook.presto.iceberg.procedure.SetTablePropertyProcedure;
+import com.facebook.presto.iceberg.procedure.StatisticsFileCacheInvalidationProcedure;
 import com.facebook.presto.iceberg.procedure.UnregisterTableProcedure;
 import com.facebook.presto.iceberg.statistics.StatisticsFileCache;
 import com.facebook.presto.iceberg.statistics.StatisticsFileCacheKey;
@@ -183,6 +184,7 @@ public class IcebergCommonModule
         procedures.addBinding().toProvider(FastForwardBranchProcedure.class).in(Scopes.SINGLETON);
         procedures.addBinding().toProvider(SetCurrentSnapshotProcedure.class).in(Scopes.SINGLETON);
         procedures.addBinding().toProvider(SetTablePropertyProcedure.class).in(Scopes.SINGLETON);
+        procedures.addBinding().toProvider(StatisticsFileCacheInvalidationProcedure.class).in(Scopes.SINGLETON);
 
         if (buildConfigObject(MetastoreClientConfig.class).isInvalidateMetastoreCacheProcedureEnabled()) {
             procedures.addBinding().toProvider(InvalidateMetastoreCacheProcedure.class).in(Scopes.SINGLETON);

presto-iceberg/src/test/java/com/facebook/presto/iceberg/IcebergDistributedTestBase.java
@@ -2744,6 +2744,42 @@ public abstract class IcebergDistributedTestBase
         assertQueryFails(query, Pattern.quote(format("Unable to parse sort field: [%s]", sortField)));
     }
 
+    public void testStatisticsFileCacheInvalidationProcedure()
+    {
+        assertQuerySucceeds("CREATE TABLE test_statistics_file_cache_procedure(i int)");
+        assertUpdate("INSERT INTO test_statistics_file_cache_procedure VALUES 1, 2, 3, 4, 5", 5);
+        assertQuerySucceeds("ANALYZE test_statistics_file_cache_procedure");
+        for (int i = 0; i < 3; i++) {
+            assertQuerySucceeds("SHOW STATS FOR test_statistics_file_cache_procedure");
+        }
+
+        String jmxMetricsQuery = format("SELECT sum(\"cachestats.hitcount\"), sum(\"cachestats.size\"), sum(\"cachestats.misscount\") " +
+                "from jmx.current.\"com.facebook.presto.iceberg.statistics:name=%s,type=statisticsfilecache\"", getSession().getCatalog().get());
+
+        MaterializedResult result = computeActual(jmxMetricsQuery);
+        long afterHitCount = (long) result.getMaterializedRows().get(0).getField(0);
+        long afterCacheSize = (long) result.getMaterializedRows().get(0).getField(1);
+        long afterMissCount = (long) result.getMaterializedRows().get(0).getField(2);
+        assertTrue(afterHitCount > 0);
+        assertTrue(afterCacheSize > 0);
+
+        //test invalidate_statistics_file_cache procedure
+        assertQuerySucceeds(format("CALL %s.system.invalidate_statistics_file_cache()", getSession().getCatalog().get()));
+        MaterializedResult resultAfterProcedure = computeActual(jmxMetricsQuery);
+        long afterProcedureCacheSize = (long) resultAfterProcedure.getMaterializedRows().get(0).getField(1);
+        assertTrue(afterProcedureCacheSize == 0);
+
+        assertQuerySucceeds("SHOW STATS FOR test_statistics_file_cache_procedure");
+
+        MaterializedResult resultAfter = computeActual(jmxMetricsQuery);
+        long newCacheSize = (long) resultAfter.getMaterializedRows().get(0).getField(1);
+        long newMissCount = (long) resultAfter.getMaterializedRows().get(0).getField(2);
+        assertTrue(newCacheSize > 0);
+        assertTrue(afterMissCount < newMissCount);
+
+        getQueryRunner().execute("DROP TABLE test_statistics_file_cache_procedure");
+    }
+
     @DataProvider(name = "sortedTableWithSortTransform")
     public static Object[][] sortedTableWithSortTransform()
     {