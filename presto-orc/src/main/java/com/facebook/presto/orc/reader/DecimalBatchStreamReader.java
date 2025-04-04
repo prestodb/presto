2eb132ccd4359a84cff1db61d65d81034a0b58e7
Zac Blanco <zac@ibm.com>
2025-04-03 20:47:34 -0700

    [presto-orc] jakarta + dependency

--- presto-orc/src/main/java/com/facebook/presto/orc/reader/DecimalBatchStreamReader.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/reader/DecimalBatchStreamReader.java
@@ -29,9 +29,8 @@ import com.facebook.presto.orc.stream.InputStreamSource;
 import com.facebook.presto.orc.stream.InputStreamSources;
 import com.facebook.presto.orc.stream.LongInputStream;
 import io.airlift.slice.Slice;
+import jakarta.annotation.Nullable;
 import org.openjdk.jol.info.ClassLayout;
-
-import javax.annotation.Nullable;
 
 import java.io.IOException;
 
