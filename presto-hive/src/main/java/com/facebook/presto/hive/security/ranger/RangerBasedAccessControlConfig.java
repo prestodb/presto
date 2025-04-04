f565d4d0dc8b50ac5c381eec9f5579516ce9eca7
Zac Blanco <zac@ibm.com>
2025-04-03 20:35:47 -0700

    [presto-hive] jakarta + dependency + enforcer

--- presto-hive/src/main/java/com/facebook/presto/hive/security/ranger/RangerBasedAccessControlConfig.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/security/ranger/RangerBasedAccessControlConfig.java
@@ -16,8 +16,7 @@ package com.facebook.presto.hive.security.ranger;
 import com.facebook.airlift.configuration.Config;
 import io.airlift.units.Duration;
 import io.airlift.units.MinDuration;
-
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
