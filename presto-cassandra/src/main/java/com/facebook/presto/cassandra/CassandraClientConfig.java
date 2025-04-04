cee4acb6d247e7debde8f486205ff75cda37a25d
Zac Blanco <zac@ibm.com>
2025-04-03 20:29:21 -0700

    [presto-cassandra] jakarta + dependency

--- presto-cassandra/src/main/java/com/facebook/presto/cassandra/CassandraClientConfig.java
+++ presto-cassandra/src/main/java/com/facebook/presto/cassandra/CassandraClientConfig.java
@@ -25,10 +25,9 @@ import com.google.common.collect.ImmutableList;
 import io.airlift.units.Duration;
 import io.airlift.units.MaxDuration;
 import io.airlift.units.MinDuration;
-
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.io.File;
 import java.util.Arrays;
