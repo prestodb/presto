add92df6dc5cc790a8859dd1cbd7b46a4d79813c
Zac Blanco <zac@ibm.com>
2025-04-03 20:46:33 -0700

    [presto-native-sidecar-plugin] jakarta + dependency + enforcer

--- presto-native-sidecar-plugin/src/main/java/com/facebook/presto/sidecar/nativechecker/PlanConversionFailureInfo.java
+++ presto-native-sidecar-plugin/src/main/java/com/facebook/presto/sidecar/nativechecker/PlanConversionFailureInfo.java
@@ -17,9 +17,8 @@ import com.facebook.presto.common.ErrorCode;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
-
-import javax.annotation.Nullable;
-import javax.annotation.concurrent.Immutable;
+import com.google.errorprone.annotations.Immutable;
+import jakarta.annotation.Nullable;
 
 import java.util.List;
 import java.util.regex.Matcher;
