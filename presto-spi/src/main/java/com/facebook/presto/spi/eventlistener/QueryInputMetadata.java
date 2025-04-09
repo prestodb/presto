0076bcbdfe889d1f2087c468c996da0e905d3980
Nidhin Varghese <Nidhin.Varghese1@ibm.com>
2025-04-09 17:06:44 +0530

    Add additional column metadata to event listener QueryOutputMetadata

--- presto-spi/src/main/java/com/facebook/presto/spi/eventlistener/QueryInputMetadata.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/eventlistener/QueryInputMetadata.java
@@ -16,8 +16,10 @@ package com.facebook.presto.spi.eventlistener;
 import com.facebook.presto.spi.statistics.TableStatistics;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.requireNonNull;
 
@@ -26,17 +28,17 @@ public class QueryInputMetadata
     private final String catalogName;
     private final String schema;
     private final String table;
-    private final List<String> columns;
+    private final List<Column> columnObjects;
     private final Optional<Object> connectorInfo;
     private final Optional<TableStatistics> statistics;
     private final String serializedCommitOutput;
 
-    public QueryInputMetadata(String catalogName, String schema, String table, List<String> columns, Optional<Object> connectorInfo, Optional<TableStatistics> statistics, String serializedCommitOutput)
+    public QueryInputMetadata(String catalogName, String schema, String table, List<Column> columnObjects, Optional<Object> connectorInfo, Optional<TableStatistics> statistics, String serializedCommitOutput)
     {
         this.catalogName = requireNonNull(catalogName, "catalogName is null");
         this.schema = requireNonNull(schema, "schema is null");
         this.table = requireNonNull(table, "table is null");
-        this.columns = requireNonNull(columns, "columns is null");
+        this.columnObjects = requireNonNull(columnObjects, "columns is null");
         this.connectorInfo = requireNonNull(connectorInfo, "connectorInfo is null");
         this.statistics = requireNonNull(statistics, "table statistics is null");
         this.serializedCommitOutput = requireNonNull(serializedCommitOutput, "serializedCommitOutput is null");
@@ -63,7 +65,13 @@ public class QueryInputMetadata
     @JsonProperty
     public List<String> getColumns()
     {
-        return columns;
+        return Collections.unmodifiableList(columnObjects.stream().map(Column::getName).collect(Collectors.toList()));
+    }
+
+    @JsonProperty
+    public List<Column> getColumnObjects()
+    {
+        return columnObjects;
     }
 
     @JsonProperty
