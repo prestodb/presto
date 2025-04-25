4e1fd23d7cc6a7cf45549c9493026ccd755f7084
unidevel <unidevel@hotmail.com>
2025-04-25 11:24:53 +0100

    Add JVM_OPTS for release-notes.sh

--- src/release/release-notes.sh
+++ src/release/release-notes.sh
@@ -7,4 +7,5 @@ fi
 
 curl -L -o /tmp/presto_release "https://oss.sonatype.org/service/local/artifact/maven/redirect?g=com.facebook.presto&a=presto-release-tools&v=RELEASE&r=releases&c=executable&e=jar"
 chmod 755 /tmp/presto_release
-/tmp/presto_release release-notes --github-user $1 --github-access-token $2
+echo "java ${JVM_OPTS} -jar /tmp/presto_release release-notes --github-user **** --github-access-token ****"
+java ${JVM_OPTS} -jar /tmp/presto_release release-notes --github-user $1 --github-access-token $2
