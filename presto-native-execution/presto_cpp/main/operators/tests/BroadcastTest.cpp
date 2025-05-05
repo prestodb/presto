bea77f400e63401d855957073eab422bcff450ab
Xiaoxuan Meng <xiaoxmeng@meta.com>
2025-05-05 13:04:45 -0700

    [native]Update task cursor usage and advance velox version

--- presto-native-execution/presto_cpp/main/operators/tests/BroadcastTest.cpp
+++ presto-native-execution/presto_cpp/main/operators/tests/BroadcastTest.cpp
@@ -85,8 +85,7 @@ class BroadcastTest : public exec::test::OperatorTestBase {
 
     exec::CursorParameters params;
     params.planNode = writerPlan;
-    auto [taskCursor, results] =
-        exec::test::readCursor(params, [](auto /*task*/) {});
+    auto [taskCursor, results] = exec::test::readCursor(params);
 
     std::vector<std::string> broadcastFilePaths;
     for (auto result : results) {
@@ -115,23 +114,27 @@ class BroadcastTest : public exec::test::OperatorTestBase {
           fmt::format(kBroadcastFileInfoFormat, broadcastFilePath));
     }
 
-    uint8_t splitIndex = 0;
     // Read back result using BroadcastExchangeSource.
-    return exec::test::readCursor(broadcastReadParams, [&](auto* task) {
-      if (splitIndex >= broadcastFilePaths.size()) {
-        task->noMoreSplits("0");
-        return;
-      }
-
-      auto split = exec::Split(
-          std::make_shared<exec::RemoteConnectorSplit>(fmt::format(
-              "batch://task?broadcastInfo={}",
-              fmt::format(
-                  kBroadcastFileInfoFormat, broadcastFilePaths[splitIndex]))),
-          -1);
-      task->addSplit("0", std::move(split));
-      ++splitIndex;
-    });
+    return exec::test::readCursor(
+        broadcastReadParams, [&](exec::TaskCursor* taskCursor) {
+          if (taskCursor->noMoreSplits()) {
+            return;
+          }
+          auto& task = taskCursor->task();
+          for (int splitIndex = 0; splitIndex < broadcastFilePaths.size();
+               ++splitIndex) {
+            auto split = exec::Split(
+                std::make_shared<exec::RemoteConnectorSplit>(fmt::format(
+                    "batch://task?broadcastInfo={}",
+                    fmt::format(
+                        kBroadcastFileInfoFormat,
+                        broadcastFilePaths[splitIndex]))),
+                -1);
+            task->addSplit("0", std::move(split));
+          }
+          task->noMoreSplits("0");
+          taskCursor->setNoMoreSplits();
+        });
   }
 
   std::vector<RowVectorPtr> reorderColumns(
@@ -368,7 +371,10 @@ TEST_F(BroadcastTest, malformedBroadcastInfoJson) {
   VELOX_ASSERT_THROW(
       exec::test::readCursor(
           broadcastReadParams,
-          [&](auto* task) {
+          [&](exec::TaskCursor* taskCursor) {
+            if (taskCursor->noMoreSplits()) {
+              return;
+            }
             auto fileInfos =
                 fmt::format(kBroadcastFileInfoFormat, invalidBroadcastFilePath);
             auto split = exec::Split(
@@ -379,8 +385,10 @@ TEST_F(BroadcastTest, malformedBroadcastInfoJson) {
                     basePath,
                     fileInfos)),
                 -1);
+            auto& task = taskCursor->task();
             task->addSplit("0", std::move(split));
             task->noMoreSplits("0");
+            taskCursor->setNoMoreSplits();
           }),
       "BroadcastInfo deserialization failed");
 }
