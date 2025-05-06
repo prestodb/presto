b8ef2383554781e2934421a270192cc6010cdabb
Jialiang Tan <jtan6@meta.com>
2025-05-06 13:52:43 -0700

    misc: Temporarily block mixed group mode join fuzzer (#13246)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/13246
    
    Currently mixed grouped mode join fuzzer is failing. Temporarily block this join mode and unblock when the issue is fixed.
    
    Reviewed By: xiaoxmeng
    
    Differential Revision: D74261923
    
    fbshipit-source-id: e1c7c1511bf42dd30065938bb918e2e6c2783da4

--- velox/exec/fuzzer/JoinFuzzer.cpp
+++ velox/exec/fuzzer/JoinFuzzer.cpp
@@ -764,12 +764,13 @@ void JoinFuzzer::verify(core::JoinType joinType) {
         numGroups, false, JoinMaker::JoinOrder::NATURAL));
 
     // Use mixed mode grouped execution.
-    if (!needRightSideJoin(joinType)) {
-      // Mixed grouped mode join does not support types that needs right side
-      // join.
-      altPlans.push_back(joinMaker.makeHashJoinWithTableScan(
-          numGroups, true, JoinMaker::JoinOrder::NATURAL));
-    }
+    // TODO(jtan6): Unblock mixed mode after fix fuzzer.
+    // if (!needRightSideJoin(joinType)) {
+    //   // Mixed grouped mode join does not support types that needs right side
+    //   // join.
+    //   altPlans.push_back(joinMaker.makeHashJoinWithTableScan(
+    //       numGroups, true, JoinMaker::JoinOrder::NATURAL));
+    // }
 
     if (joinMaker.supportsFlippingHashJoin()) {
       // Use ungrouped execution.
@@ -781,12 +782,14 @@ void JoinFuzzer::verify(core::JoinType joinType) {
           numGroups, false, JoinMaker::JoinOrder::FLIPPED));
 
       // Use mixed mode grouped execution.
-      if (!needRightSideJoin(flipJoinType(joinType))) {
-        // Mixed grouped mode join does not support types that needs right side
-        // join.
-        altPlans.push_back(joinMaker.makeHashJoinWithTableScan(
-            numGroups, true, JoinMaker::JoinOrder::FLIPPED));
-      }
+      // TODO(jtan6): Unblock mixed mode after fix fuzzer.
+      // if (!needRightSideJoin(flipJoinType(joinType))) {
+      //   // Mixed grouped mode join does not support types that needs right
+      //   side
+      //   // join.
+      //   altPlans.push_back(joinMaker.makeHashJoinWithTableScan(
+      //       numGroups, true, JoinMaker::JoinOrder::FLIPPED));
+      // }
     }
 
     if (joinMaker.supportsMergeJoin()) {
