1c63fd2cd6a0e42cd9fb8a0f44bf39c97c501436
Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>
2025-05-07 13:39:54 +0530

    Align Tempto with default Hive user configuration
    Presto’s default configuration interacts with Hive/Hadoop using the hive user (-DHADOOP_USER_NAME=hive). To ensure consistency and avoid permission issues, Tempto should also use the same hive user for data setup and as the default user for executing queries.
    
    Also, modify temporary storage location in hdfs where user hive has access

--- presto-product-tests/src/main/resources/sql-tests/testcases/csv_table_with_custom_parameters.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/csv_table_with_custom_parameters.sql
@@ -11,10 +11,10 @@ c_varchar|varchar|||
 --! name: show create csv table
 SHOW CREATE TABLE csv_table_with_custom_parameters
 --!
-CREATE TABLE hive.default.csv_table_with_custom_parameters (\n   "c_bigint" varchar,\n   "c_varchar" varchar\n)\nWITH (\n   csv_escape = 'E',\n   csv_quote = 'Q',\n   csv_separator = 'S',\n   external_location = 'hdfs://hadoop-master:9000/product-test/datasets/csv_table_with_custom_parameters',\n   format = 'CSV'\n)
+CREATE TABLE hive.default.csv_table_with_custom_parameters (\n   "c_bigint" varchar,\n   "c_varchar" varchar\n)\nWITH (\n   csv_escape = 'E',\n   csv_quote = 'Q',\n   csv_separator = 'S',\n   external_location = 'hdfs://hadoop-master:9000/tmp/product-test/datasets/csv_table_with_custom_parameters',\n   format = 'CSV'\n)
 --! name: create table like
 DROP TABLE IF EXISTS like_csv_table;
 CREATE TABLE like_csv_table (LIKE csv_table_with_custom_parameters INCLUDING PROPERTIES);
 SHOW CREATE TABLE like_csv_table
 --!
-CREATE TABLE hive.default.like_csv_table (\n   "c_bigint" varchar,\n   "c_varchar" varchar\n)\nWITH (\n   csv_escape = 'E',\n   csv_quote = 'Q',\n   csv_separator = 'S',\n   external_location = 'hdfs://hadoop-master:9000/product-test/datasets/csv_table_with_custom_parameters',\n   format = 'CSV'\n)
+CREATE TABLE hive.default.like_csv_table (\n   "c_bigint" varchar,\n   "c_varchar" varchar\n)\nWITH (\n   csv_escape = 'E',\n   csv_quote = 'Q',\n   csv_separator = 'S',\n   external_location = 'hdfs://hadoop-master:9000/tmp/product-test/datasets/csv_table_with_custom_parameters',\n   format = 'CSV'\n)
