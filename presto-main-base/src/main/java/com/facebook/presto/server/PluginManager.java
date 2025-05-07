16d65415f9fe0c1479414a938e4e281ca88c5fb2
auden-woolfson <auden.woolfson@ibm.com>
2025-05-07 15:29:19 -0700

    Refactor PluginManager(s) to use PluginManagerUtil

--- presto-main-base/src/main/java/com/facebook/presto/server/PluginManager.java
+++ presto-main-base/src/main/java/com/facebook/presto/server/PluginManager.java
@@ -34,7 +34,6 @@ import com.facebook.presto.spi.CoordinatorPlugin;
 import com.facebook.presto.spi.Plugin;
 import com.facebook.presto.spi.analyzer.AnalyzerProvider;
 import com.facebook.presto.spi.analyzer.QueryPreparerProvider;
-import com.facebook.presto.spi.classloader.ThreadContextClassLoader;
 import com.facebook.presto.spi.connector.ConnectorFactory;
 import com.facebook.presto.spi.eventlistener.EventListenerFactory;
 import com.facebook.presto.spi.function.FunctionNamespaceManagerFactory;
@@ -64,56 +63,22 @@ import com.facebook.presto.ttl.clusterttlprovidermanagers.ClusterTtlProviderMana
 import com.facebook.presto.ttl.nodettlfetchermanagers.NodeTtlFetcherManager;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Ordering;
 import io.airlift.resolver.ArtifactResolver;
-import io.airlift.resolver.DefaultArtifact;
-import org.sonatype.aether.artifact.Artifact;
 
 import javax.annotation.concurrent.ThreadSafe;
 import javax.inject.Inject;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.ServiceLoader;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.facebook.presto.metadata.FunctionExtractor.extractFunctions;
-import static com.facebook.presto.server.PluginDiscovery.discoverPlugins;
-import static com.facebook.presto.server.PluginDiscovery.writePluginServices;
+import static com.facebook.presto.server.PluginManagerUtil.SPI_PACKAGES;
 import static java.util.Objects.requireNonNull;
 
 @ThreadSafe
 public class PluginManager
 {
-    // When generating code the AfterBurner module loads classes with *some* classloader.
-    // When the AfterBurner module is configured not to use the value classloader
-    // (e.g., AfterBurner().setUseValueClassLoader(false)) AppClassLoader is used for loading those
-    // classes. Otherwise, the PluginClassLoader is used, which is the default behavior.
-    // Therefore, in the former case Afterburner won't be able to load the connector classes
-    // as AppClassLoader doesn't see them, and in the latter case the PluginClassLoader won't be
-    // able to load the AfterBurner classes themselves. So, our solution is to use the PluginClassLoader
-    // and whitelist the AfterBurner classes here, so that the PluginClassLoader can load the
-    // AfterBurner classes.
-    private static final ImmutableList<String> SPI_PACKAGES = ImmutableList.<String>builder()
-            .add("com.facebook.presto.spi.")
-            .add("com.fasterxml.jackson.annotation.")
-            .add("com.fasterxml.jackson.module.afterburner.")
-            .add("io.airlift.slice.")
-            .add("io.airlift.units.")
-            .add("org.openjdk.jol.")
-            .add("com.facebook.presto.common")
-            .add("com.facebook.drift.annotations.")
-            .add("com.facebook.drift.TException")
-            .add("com.facebook.drift.TApplicationException")
-            .build();
-
     //  TODO: To make CoordinatorPlugin loading compulsory when native execution is enabled.
     private static final String COORDINATOR_PLUGIN_SERVICES_FILE = "META-INF/services/" + CoordinatorPlugin.class.getName();
     private static final String PLUGIN_SERVICES_FILE = "META-INF/services/" + Plugin.class.getName();
@@ -145,6 +110,7 @@ public class PluginManager
     private final ClientRequestFilterManager clientRequestFilterManager;
     private final PlanCheckerProviderManager planCheckerProviderManager;
     private final ExpressionOptimizerManager expressionOptimizerManager;
+    private final PluginInstaller pluginInstaller;
 
     @Inject
     public PluginManager(
@@ -206,63 +172,24 @@ public class PluginManager
         this.clientRequestFilterManager = requireNonNull(clientRequestFilterManager, "clientRequestFilterManager is null");
         this.planCheckerProviderManager = requireNonNull(planCheckerProviderManager, "planCheckerProviderManager is null");
         this.expressionOptimizerManager = requireNonNull(expressionOptimizerManager, "expressionManager is null");
+        this.pluginInstaller = new MainPluginInstaller(this);
     }
 
     public void loadPlugins()
             throws Exception
     {
-        if (!pluginsLoading.compareAndSet(false, true)) {
-            return;
-        }
-
-        for (File file : listFiles(installedPluginsDir)) {
-            if (file.isDirectory()) {
-                loadPlugin(file.getAbsolutePath());
-            }
-        }
-
-        for (String plugin : plugins) {
-            loadPlugin(plugin);
-        }
-
-        metadata.verifyComparableOrderableContract();
-
-        pluginsLoaded.set(true);
-    }
-
-    private void loadPlugin(String plugin)
-            throws Exception
-    {
-        log.info("-- Loading plugin %s --", plugin);
-        URLClassLoader pluginClassLoader = buildClassLoader(plugin);
-        try (ThreadContextClassLoader ignored = new ThreadContextClassLoader(pluginClassLoader)) {
-            loadPlugin(pluginClassLoader, CoordinatorPlugin.class);
-            loadPlugin(pluginClassLoader, Plugin.class);
-        }
-        log.info("-- Finished loading plugin %s --", plugin);
-    }
-
-    private void loadPlugin(URLClassLoader pluginClassLoader, Class<?> clazz)
-    {
-        ServiceLoader<?> serviceLoader = ServiceLoader.load(clazz, pluginClassLoader);
-        List<?> plugins = ImmutableList.copyOf(serviceLoader);
-
-        if (plugins.isEmpty()) {
-            log.warn("No service providers of type %s", clazz.getName());
-        }
-
-        for (Object plugin : plugins) {
-            log.info("Installing %s", plugin.getClass().getName());
-            if (plugin instanceof Plugin) {
-                installPlugin((Plugin) plugin);
-            }
-            else if (plugin instanceof CoordinatorPlugin) {
-                installCoordinatorPlugin((CoordinatorPlugin) plugin);
-            }
-            else {
-                log.warn("Unknown plugin type: %s", plugin.getClass().getName());
-            }
-        }
+        PluginManagerUtil.loadPlugins(
+                pluginsLoading,
+                pluginsLoaded,
+                installedPluginsDir,
+                plugins,
+                metadata,
+                resolver,
+                SPI_PACKAGES,
+                COORDINATOR_PLUGIN_SERVICES_FILE,
+                PLUGIN_SERVICES_FILE,
+                pluginInstaller,
+                getClass().getClassLoader());
     }
 
     public void installPlugin(Plugin plugin)
@@ -410,100 +337,26 @@ public class PluginManager
         }
     }
 
-    private URLClassLoader buildClassLoader(String plugin)
-            throws Exception
+    private class MainPluginInstaller
+            implements PluginInstaller
     {
-        File file = new File(plugin);
-        if (file.isFile() && (file.getName().equals("pom.xml") || file.getName().endsWith(".pom"))) {
-            return buildClassLoaderFromPom(file);
+        private final PluginManager pluginManager;
+
+        public MainPluginInstaller(PluginManager pluginManager)
+        {
+            this.pluginManager = pluginManager;
         }
-        if (file.isDirectory()) {
-            return buildClassLoaderFromDirectory(file);
+
+        @Override
+        public void installPlugin(Plugin plugin)
+        {
+            pluginManager.installPlugin(plugin);
         }
-        return buildClassLoaderFromCoordinates(plugin);
-    }
 
-    private URLClassLoader buildClassLoaderFromPom(File pomFile)
-            throws Exception
-    {
-        List<Artifact> artifacts = resolver.resolvePom(pomFile);
-        URLClassLoader classLoader = createClassLoader(artifacts, pomFile.getPath());
-
-        Artifact artifact = artifacts.get(0);
-
-        processPlugins(artifact, classLoader, COORDINATOR_PLUGIN_SERVICES_FILE, CoordinatorPlugin.class.getName());
-        processPlugins(artifact, classLoader, PLUGIN_SERVICES_FILE, Plugin.class.getName());
-
-        return classLoader;
-    }
-
-    private URLClassLoader buildClassLoaderFromDirectory(File dir)
-            throws Exception
-    {
-        log.debug("Classpath for %s:", dir.getName());
-        List<URL> urls = new ArrayList<>();
-        for (File file : listFiles(dir)) {
-            log.debug("    %s", file);
-            urls.add(file.toURI().toURL());
-        }
-        return createClassLoader(urls);
-    }
-
-    private URLClassLoader buildClassLoaderFromCoordinates(String coordinates)
-            throws Exception
-    {
-        Artifact rootArtifact = new DefaultArtifact(coordinates);
-        List<Artifact> artifacts = resolver.resolveArtifacts(rootArtifact);
-        return createClassLoader(artifacts, rootArtifact.toString());
-    }
-
-    private URLClassLoader createClassLoader(List<Artifact> artifacts, String name)
-            throws IOException
-    {
-        log.debug("Classpath for %s:", name);
-        List<URL> urls = new ArrayList<>();
-        for (Artifact artifact : sortedArtifacts(artifacts)) {
-            if (artifact.getFile() == null) {
-                throw new RuntimeException("Could not resolve artifact: " + artifact);
-            }
-            File file = artifact.getFile().getCanonicalFile();
-            log.debug("    %s", file);
-            urls.add(file.toURI().toURL());
-        }
-        return createClassLoader(urls);
-    }
-
-    private URLClassLoader createClassLoader(List<URL> urls)
-    {
-        ClassLoader parent = getClass().getClassLoader();
-        return new PluginClassLoader(urls, parent, SPI_PACKAGES);
-    }
-
-    private static List<File> listFiles(File installedPluginsDir)
-    {
-        if (installedPluginsDir != null && installedPluginsDir.isDirectory()) {
-            File[] files = installedPluginsDir.listFiles();
-            if (files != null) {
-                Arrays.sort(files);
-                return ImmutableList.copyOf(files);
-            }
-        }
-        return ImmutableList.of();
-    }
-
-    private static List<Artifact> sortedArtifacts(List<Artifact> artifacts)
-    {
-        List<Artifact> list = new ArrayList<>(artifacts);
-        Collections.sort(list, Ordering.natural().nullsLast().onResultOf(Artifact::getFile));
-        return list;
-    }
-
-    private void processPlugins(Artifact artifact, ClassLoader classLoader, String servicesFile, String className)
-            throws IOException
-    {
-        Set<String> plugins = discoverPlugins(artifact, classLoader, servicesFile, className);
-        if (!plugins.isEmpty()) {
-            writePluginServices(plugins, artifact.getFile(), servicesFile);
+        @Override
+        public void installCoordinatorPlugin(CoordinatorPlugin plugin)
+        {
+            pluginManager.installCoordinatorPlugin(plugin);
         }
     }
 }
