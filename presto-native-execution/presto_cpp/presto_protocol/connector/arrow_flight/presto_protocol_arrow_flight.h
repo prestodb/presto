09175f3244f038dd725eb2c4ff2c31cbe76063a5
Bryan Cutler <cutlerb@gmail.com>
2025-05-09 16:20:13 -0700

    Fix protocol generation to properly remove license
    The java-to-struct-json.py script was supposed to remove the license
    header from special classes ending in .inc so they can be inserted in
    protocol definition sources. It was incorrectly using the Velox license
    header, which is slightly different, and never removed the license.
    This uses the correct license.header file from presto_cpp so it is
    removed from special classes when generating the presto protocol.

--- presto-native-execution/presto_cpp/presto_protocol/connector/arrow_flight/presto_protocol_arrow_flight.h
+++ presto-native-execution/presto_cpp/presto_protocol/connector/arrow_flight/presto_protocol_arrow_flight.h
@@ -38,20 +38,6 @@ void to_json(json& j, const ArrowTransactionHandle& p);
 void from_json(const json& j, ArrowTransactionHandle& p);
 
 } // namespace facebook::presto::protocol::arrow_flight
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // ArrowColumnHandle is special since it needs an implementation of
 // operator<().
 
