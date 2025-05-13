f2eba024053c5bad614b9947ace3d88468536bb3
Reetika Agrawal <agrawal.reetika786@gmail.com>
2025-05-13 11:01:38 +0530

    Enable case-sensitive matching support for all the Jdbc connectors

--- presto-redshift/src/main/java/com/facebook/presto/plugin/redshift/RedshiftClient.java
+++ presto-redshift/src/main/java/com/facebook/presto/plugin/redshift/RedshiftClient.java
@@ -31,6 +31,7 @@ import java.sql.SQLException;
 
 import static com.facebook.presto.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
 import static java.lang.String.format;
+import static java.util.Locale.ENGLISH;
 
 public class RedshiftClient
         extends BaseJdbcClient
@@ -65,5 +66,11 @@ public class RedshiftClient
         catch (SQLException e) {
             throw new PrestoException(JDBC_ERROR, e);
         }
+    }
+
+    @Override
+    public String normalizeIdentifier(ConnectorSession session, String identifier)
+    {
+        return caseSensitiveNameMatchingEnabled ? identifier : identifier.toLowerCase(ENGLISH);
     }
 }
