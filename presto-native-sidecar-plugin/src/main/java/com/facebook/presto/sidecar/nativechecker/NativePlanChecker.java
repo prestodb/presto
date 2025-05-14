8594d73ac3bcbd69eb6939738a5957ac599c8cb0
Tim Meehan <tim@timdmeehan.com>
2025-05-14 16:43:05 -0400

    Fix native plan checker for CTAS and insert queries
    Previously, these queries would always fail because the root of the plan
    was TableWriterMerge.  Now, the plan is modified to swap out a dummy project
    node for the TableWriterNode.  This is because TableWriterNode relies on
    information that is not known until the scheduling phase.

--- presto-native-sidecar-plugin/src/main/java/com/facebook/presto/sidecar/nativechecker/NativePlanChecker.java
+++ presto-native-sidecar-plugin/src/main/java/com/facebook/presto/sidecar/nativechecker/NativePlanChecker.java
@@ -21,11 +21,17 @@ import com.facebook.presto.spi.NodeManager;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.TableHandle;
 import com.facebook.presto.spi.WarningCollector;
+import com.facebook.presto.spi.plan.Assignments;
 import com.facebook.presto.spi.plan.PlanChecker;
 import com.facebook.presto.spi.plan.PlanNode;
 import com.facebook.presto.spi.plan.PlanVisitor;
+import com.facebook.presto.spi.plan.ProjectNode;
 import com.facebook.presto.spi.plan.SimplePlanFragment;
 import com.facebook.presto.spi.plan.TableScanNode;
+import com.facebook.presto.spi.plan.TableWriterNode;
+import com.facebook.presto.spi.relation.ConstantExpression;
+import com.facebook.presto.spi.relation.RowExpression;
+import com.facebook.presto.spi.relation.VariableReferenceExpression;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -33,11 +39,18 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
+import static com.facebook.presto.common.type.BigintType.BIGINT;
+import static com.facebook.presto.common.type.VarcharType.VARCHAR;
 import static com.facebook.presto.sidecar.nativechecker.NativePlanCheckerErrorCode.NATIVEPLANCHECKER_CONNECTION_ERROR;
 import static com.facebook.presto.sidecar.nativechecker.NativePlanCheckerErrorCode.NATIVEPLANCHECKER_UNKNOWN_CONVERSION_FAILURE;
 import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static io.airlift.slice.Slices.utf8Slice;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Uses the native sidecar to check verify a plan can be run on a native worker.
@@ -70,12 +83,34 @@ public final class NativePlanChecker
     @Override
     public void validateFragment(SimplePlanFragment planFragment, WarningCollector warningCollector, ConnectorSession session)
     {
-        if (!planFragment.getPartitioning().isCoordinatorOnly() && !isInternalSystemConnector(planFragment.getRoot())) {
-            runValidation(planFragment);
-        }
-        else {
+        if (planFragment.getPartitioning().isCoordinatorOnly()
+                || isInternalSystemConnector(planFragment.getRoot())) {
             LOG.debug("Skipping native plan validation [fragment: %s, root: %s]", planFragment.getId(), planFragment.getRoot().getId());
+            return;
         }
+        runValidation(removeTableWriter(planFragment));
+    }
+
+    /**
+     * HACK: Replace TableWriterNode from the plan fragment with a ProjectNode because validating a TableWriterNode
+     * is unsupported by the native sidecar.  They are unsupported because they contain information only determined
+     * during scheduling.
+     */
+    private SimplePlanFragment removeTableWriter(SimplePlanFragment planFragment)
+    {
+        // Remove TableWriterNode from the plan fragment
+        PlanNode root = planFragment.getRoot().accept(new TableWriterNodeReplacer(), null);
+        requireNonNull(root, "TableWriterNode removal resulted in null root");
+
+        return new SimplePlanFragment(
+                planFragment.getId(),
+                root,
+                planFragment.getVariables(),
+                planFragment.getPartitioning(),
+                planFragment.getTableScanSchedulingOrder(),
+                planFragment.getPartitioningScheme(),
+                planFragment.getStageExecutionDescriptor(),
+                planFragment.isOutputTableWriterFragment());
     }
 
     private boolean isInternalSystemConnector(PlanNode planNode)
@@ -150,6 +185,42 @@ public final class NativePlanChecker
                 }
             }
             return false;
+        }
+    }
+
+    private static class TableWriterNodeReplacer
+            extends PlanVisitor<PlanNode, Void>
+    {
+        @Override
+        public PlanNode visitTableWriter(TableWriterNode tableWriter, Void context)
+        {
+            // Create dummy assignments for the ProjectNode
+            Map<VariableReferenceExpression, RowExpression> assignmentsMap = tableWriter.getOutputVariables()
+                    .subList(3, tableWriter.getOutputVariables().size())
+                    .stream()
+                    .collect(toMap(i -> i, i -> i));
+            assignmentsMap.put(tableWriter.getRowCountVariable(), new ConstantExpression(0L, BIGINT));
+            assignmentsMap.put(tableWriter.getFragmentVariable(), new ConstantExpression(utf8Slice(""), VARCHAR));
+            assignmentsMap.put(tableWriter.getTableCommitContextVariable(), new ConstantExpression(utf8Slice(""), VARCHAR));
+            Assignments assignments = Assignments.builder().putAll(assignmentsMap).build();
+
+            // Replace TableWriterNode with a ProjectNode
+            return new ProjectNode(
+                    tableWriter.getId(),
+                    tableWriter.getSource(),
+                    Assignments.builder().putAll(assignmentsMap).build());
+        }
+
+        @Override
+        public PlanNode visitPlan(PlanNode node, Void context)
+        {
+            // Recursively process child nodes
+            List<PlanNode> prunedChildren = node.getSources().stream()
+                    .map(child -> child.accept(this, context))
+                    .collect(toImmutableList());
+
+            // Replace the current node's children with the pruned children
+            return node.replaceChildren(prunedChildren);
         }
     }
 }
