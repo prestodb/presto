1113fb21a3c55d7b0a8a317f3b76152056a03101
Tim Meehan <tim@timdmeehan.com>
2025-05-16 15:48:58 -0400

    Always evaluate built-in functions using the presto.default namespace
    If the user has no specified a custom expression optimizer, expressions should be
    presumed to be in the presto.default namespace (which consists exclusively of Java
    functions).

--- presto-main-base/src/main/java/com/facebook/presto/sql/relational/RowExpressionOptimizer.java
+++ presto-main-base/src/main/java/com/facebook/presto/sql/relational/RowExpressionOptimizer.java
@@ -15,9 +15,12 @@ package com.facebook.presto.sql.relational;
 
 import com.facebook.presto.common.CatalogSchemaName;
 import com.facebook.presto.common.QualifiedObjectName;
+import com.facebook.presto.metadata.BuiltInFunctionHandle;
 import com.facebook.presto.metadata.FunctionAndTypeManager;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.function.FunctionHandle;
+import com.facebook.presto.spi.function.FunctionMetadata;
 import com.facebook.presto.spi.relation.CallExpression;
 import com.facebook.presto.spi.relation.ExpressionOptimizer;
 import com.facebook.presto.spi.relation.RowExpression;
@@ -26,21 +29,23 @@ import com.facebook.presto.spi.relation.VariableReferenceExpression;
 import com.facebook.presto.sql.analyzer.TypeSignatureProvider;
 import com.facebook.presto.sql.planner.RowExpressionInterpreter;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.function.Function;
 
+import static com.facebook.presto.common.Utils.checkState;
 import static com.facebook.presto.metadata.BuiltInTypeAndFunctionNamespaceManager.JAVA_BUILTIN_NAMESPACE;
 import static com.facebook.presto.spi.relation.ExpressionOptimizer.Level.OPTIMIZED;
 import static com.facebook.presto.sql.planner.LiteralEncoder.toRowExpression;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
-import static java.util.function.UnaryOperator.identity;
 
 public final class RowExpressionOptimizer
         implements ExpressionOptimizer
 {
     private final FunctionAndTypeManager functionAndTypeManager;
     private final CatalogSchemaName defaultNamespace;
-    private final Function<RowExpression, RowExpression> normalizeRowExpression;
 
     public RowExpressionOptimizer(Metadata metadata)
     {
@@ -51,24 +56,22 @@ public final class RowExpressionOptimizer
     {
         this.defaultNamespace = requireNonNull(functionAndTypeManager, "functionMetadataManager is null").getDefaultNamespace();
         this.functionAndTypeManager = requireNonNull(functionAndTypeManager, "functionMetadataManager is null");
-        if (!defaultNamespace.equals(JAVA_BUILTIN_NAMESPACE)) {
-            this.normalizeRowExpression = rowExpression -> rowExpression.accept(new BuiltInFunctionNamespaceOverride(), null);
-        }
-        else {
-            this.normalizeRowExpression = identity();
-        }
     }
 
     @Override
     public RowExpression optimize(RowExpression rowExpression, Level level, ConnectorSession session)
     {
         if (level.ordinal() <= OPTIMIZED.ordinal()) {
+            BuiltInNamespaceRewriter override = new BuiltInNamespaceRewriter();
             RowExpressionInterpreter rowExpressionInterpreter = new RowExpressionInterpreter(
-                    normalizeRowExpression.apply(rowExpression),
+                    override.convertToInterpreterNamespace(rowExpression),
                     functionAndTypeManager,
                     session,
                     level);
-            return toRowExpression(rowExpression.getSourceLocation(), rowExpressionInterpreter.optimize(), rowExpression.getType());
+            return override.restoreOriginalNamespaces(toRowExpression(
+                    rowExpression.getSourceLocation(),
+                    rowExpressionInterpreter.optimize(),
+                    rowExpression.getType()));
         }
         throw new IllegalArgumentException("Not supported optimization level: " + level);
     }
@@ -76,12 +79,16 @@ public final class RowExpressionOptimizer
     @Override
     public RowExpression optimize(RowExpression expression, Level level, ConnectorSession session, Function<VariableReferenceExpression, Object> variableResolver)
     {
+        BuiltInNamespaceRewriter visitor = new BuiltInNamespaceRewriter();
         RowExpressionInterpreter interpreter = new RowExpressionInterpreter(
-                normalizeRowExpression.apply(expression),
+                visitor.convertToInterpreterNamespace(expression),
                 functionAndTypeManager,
                 session,
                 level);
-        return toRowExpression(expression.getSourceLocation(), interpreter.optimize(variableResolver::apply), expression.getType());
+        return visitor.restoreOriginalNamespaces(toRowExpression(
+                expression.getSourceLocation(),
+                interpreter.optimize(variableResolver::apply),
+                expression.getType()));
     }
 
     /**
@@ -90,32 +97,90 @@ public final class RowExpressionOptimizer
      * function namespace has been switched (for example, to native.default. in the case of native functions).  This will no longer be needed
      * when the native sidecar is capable of providing its own expression optimizer.
      */
-    private class BuiltInFunctionNamespaceOverride
-            implements RowExpressionVisitor<RowExpression, Void>
+    private class BuiltInNamespaceRewriter
     {
-        @Override
-        public RowExpression visitExpression(RowExpression expression, Void context)
+        private Map<FunctionHandle, FunctionHandle> map = new IdentityHashMap<>();
+
+        public RowExpression convertToInterpreterNamespace(RowExpression expression)
         {
-            return expression;
+            if (defaultNamespace.equals(JAVA_BUILTIN_NAMESPACE)) {
+                // No need to replace built-in namespaces if the default namespace is already the Java built-in namespace
+                return expression;
+            }
+            return expression.accept(new ReplaceBuiltInNamespaces(), null);
         }
 
-        @Override
-        public RowExpression visitCall(CallExpression call, Void context)
+        public RowExpression restoreOriginalNamespaces(RowExpression expression)
         {
-            if (call.getFunctionHandle().getCatalogSchemaName().equals(defaultNamespace)) {
-                call = new CallExpression(
-                        call.getSourceLocation(),
-                        call.getDisplayName(),
-                        functionAndTypeManager.lookupFunction(
-                                QualifiedObjectName.valueOf(JAVA_BUILTIN_NAMESPACE, call.getDisplayName()),
-                                call.getArguments().stream()
-                                        .map(RowExpression::getType)
-                                        .map(x -> new TypeSignatureProvider(x.getTypeSignature()))
-                                        .collect(toImmutableList())),
-                        call.getType(),
-                        call.getArguments());
+            if (map.isEmpty()) {
+                return expression;
             }
-            return call;
+            return expression.accept(new ReplaceOriginalNamespaces(), null);
+        }
+
+        private class ReplaceBuiltInNamespaces
+                implements RowExpressionVisitor<RowExpression, Void>
+        {
+            @Override
+            public RowExpression visitExpression(RowExpression expression, Void context)
+            {
+                return expression;
+            }
+
+            @Override
+            public RowExpression visitCall(CallExpression call, Void context)
+            {
+                FunctionHandle functionHandle = call.getFunctionHandle();
+                FunctionMetadata functionMetadata = functionAndTypeManager.getFunctionMetadata(functionHandle);
+                if (!functionMetadata.getImplementationType().canBeEvaluatedInCoordinator() && functionHandle instanceof BuiltInFunctionHandle) {
+                    BuiltInFunctionHandle builtInFunctionHandle = (BuiltInFunctionHandle) functionHandle;
+
+                    checkState(!functionHandle.getCatalogSchemaName().equals(JAVA_BUILTIN_NAMESPACE),
+                            format("FunctionHandle %s is already in the Java built-in namespace (%s), yet is marked as ineligible to be evaluated in the coordinator", functionHandle, builtInFunctionHandle.getCatalogSchemaName()));
+
+                    // Replace the namespace with the Java built-in namespace
+                    FunctionHandle javaNamespaceFunctionHandle = functionAndTypeManager.lookupFunction(
+                            QualifiedObjectName.valueOf(JAVA_BUILTIN_NAMESPACE, call.getDisplayName()),
+                            builtInFunctionHandle.getArgumentTypes().stream().map(TypeSignatureProvider::new).collect(toImmutableList()));
+
+                    checkState(functionAndTypeManager.getFunctionMetadata(javaNamespaceFunctionHandle).getImplementationType().canBeEvaluatedInCoordinator(),
+                            format("FunctionHandle %s in the Java built-in namespace (%s) is not eligible to be evaluated in the coordinator", javaNamespaceFunctionHandle, JAVA_BUILTIN_NAMESPACE));
+
+                    map.put(javaNamespaceFunctionHandle, functionHandle);
+                    return new CallExpression(
+                            call.getSourceLocation(),
+                            call.getDisplayName(),
+                            javaNamespaceFunctionHandle,
+                            call.getType(),
+                            call.getArguments());
+                }
+                return call;
+            }
+        }
+
+        private class ReplaceOriginalNamespaces
+                implements RowExpressionVisitor<RowExpression, Void>
+        {
+            @Override
+            public RowExpression visitExpression(RowExpression expression, Void context)
+            {
+                return expression;
+            }
+
+            @Override
+            public RowExpression visitCall(CallExpression call, Void context)
+            {
+                if (map.containsKey(call.getFunctionHandle())) {
+                    FunctionHandle originalFunctionHandle = map.get(call.getFunctionHandle());
+                    return new CallExpression(
+                            call.getSourceLocation(),
+                            call.getDisplayName(),
+                            originalFunctionHandle,
+                            call.getType(),
+                            call.getArguments());
+                }
+                return call;
+            }
         }
     }
 }
