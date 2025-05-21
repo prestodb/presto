0979ea72a35195680fc7b90f3322da977ea96628
Li Zhou <unidevel@hotmail.com>
2025-05-21 06:47:03 +0100

    Release action fixes and improvements (#24957)
    ## Description
    
    By the release 0.292, I noticed there are some issues
    
    ### Prepare release action 
    
    1. the action may failed when creating release PR(not really failed, the
    PR created, but the command exit with non-zero code because it failed to
    parse the warnings)
    2. CLA issue for the `prestodb-ci` account - **fixed by signing the CLA
    for prestodb-ci account**
    
    ### publish release action
    1. When there are issues, the release-tag need to be reset manually
    after patch commit pushed into the release branch, the publish release
    tag can not re-run without reset release tag - **by allowing the commit
    empty, recreate the tag based on the release branch**
    2. ~~There are 5 jobs in the release action, each job requires approval
    by the github action environment~~ (seems not possible)
    3. Variables like java version/distribution, ci user/email can be
    centrialized with org level variables
    
    ### Patch release
    Need action to create patch release when there are issues after
    publishing
    
    ## Motivation and Context
    <!---Why is this change required? What problem does it solve?-->
    <!---If it fixes an open issue, please link to the issue here.-->
    
    ## Impact
    
    Presto stable and patch release
    
    ## Test Plan
    
    In the test repos and next release
    
    ## Contributor checklist
    
    - [ ] Please make sure your submission complies with our [contributing
    guide](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md),
    in particular [code
    style](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md#code-style)
    and [commit
    standards](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md#commit-standards).
    - [ ] PR description addresses the issue accurately and concisely. If
    the change is non-trivial, a GitHub Issue is referenced.
    - [ ] Documented new properties (with its default value), SQL syntax,
    functions, or other functionality.
    - [ ] If release notes are required, they follow the [release notes
    guidelines](https://github.com/prestodb/presto/wiki/Release-Notes-Guidelines).
    - [ ] Adequate tests were added if applicable.
    - [ ] CI passed.
    
    ## Release Notes
    
    ```
    == NO RELEASE NOTE ==
    ```

--- src/release/release-notes.sh
+++ src/release/release-notes.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 if [[ "$#" -ne 2 ]]; then
     echo "Usage: $0 <github_user> <github_access_token>"
@@ -7,4 +7,4 @@ fi
 
 curl -L -o /tmp/presto_release "https://oss.sonatype.org/service/local/artifact/maven/redirect?g=com.facebook.presto&a=presto-release-tools&v=RELEASE&r=releases&c=executable&e=jar"
 chmod 755 /tmp/presto_release
-/tmp/presto_release release-notes --github-user $1 --github-access-token $2
+java ${JVM_OPTS} -jar /tmp/presto_release release-notes --github-user $1 --github-access-token $2
