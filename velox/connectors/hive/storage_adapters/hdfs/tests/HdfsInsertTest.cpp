8fd1e6cde2bfd83a1d92036193e03a574a64d7b8
Ke Jia <ke.a.jia@intel.com>
2025-05-27 22:19:17 -0700

    refactor: Refactor the HdfsInsertTest to use test_common/InsertTest (#13417)
    Summary:
    Utilize [test_common/InsertTest](https://github.com/facebookincubator/velox/blob/main/velox/connectors/hive/storage_adapters/test_common/InsertTest.h) in `HdfsInsertTest`, as demonstrated in [GcsInsertTest ](https://github.com/facebookincubator/velox/blob/main/velox/connectors/hive/storage_adapters/gcs/tests/GcsInsertTest.cpp)and [S3InsertTest](https://github.com/facebookincubator/velox/blob/main/velox/connectors/hive/storage_adapters/s3fs/tests/S3InsertTest.cpp), to minimize code duplication.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/13417
    
    Reviewed By: kgpai
    
    Differential Revision: D75459745
    
    Pulled By: bikramSingh91
    
    fbshipit-source-id: 104c14a397e99f5e49c34fe01d06843b18cf3841

---
+++ velox/connectors/hive/storage_adapters/hdfs/tests/HdfsInsertTest.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+
+#include "velox/common/memory/Memory.h"
+#include "velox/connectors/hive/storage_adapters/hdfs/RegisterHdfsFileSystem.h"
+#include "velox/connectors/hive/storage_adapters/hdfs/tests/HdfsMiniCluster.h"
+#include "velox/connectors/hive/storage_adapters/test_common/InsertTest.h"
+
+using namespace facebook::velox;
+using namespace facebook::velox::exec::test;
+using namespace facebook::velox::connector;
+using namespace facebook::velox::test;
+
+class HdfsInsertTest : public testing::Test, public InsertTest {
+ public:
+  static void SetUpTestSuite() {
+    memory::MemoryManager::testingSetInstance(memory::MemoryManager::Options{});
+  }
+
+  void SetUp() override {
+    filesystems::registerHdfsFileSystem();
+    if (miniCluster == nullptr) {
+      miniCluster = std::make_shared<filesystems::test::HdfsMiniCluster>();
+      miniCluster->start();
+    }
+    ioExecutor_ = std::make_unique<folly::IOThreadPoolExecutor>(3);
+
+    InsertTest::SetUp(
+        std::make_shared<const config::ConfigBase>(
+            std::unordered_map<std::string, std::string>()),
+        ioExecutor_.get());
+  }
+
+  void TearDown() override {
+    InsertTest::TearDown();
+    miniCluster->stop();
+  }
+
+  static std::shared_ptr<filesystems::test::HdfsMiniCluster> miniCluster;
+  std::unique_ptr<folly::IOThreadPoolExecutor> ioExecutor_;
+};
+
+std::shared_ptr<filesystems::test::HdfsMiniCluster>
+    HdfsInsertTest::miniCluster = nullptr;
+
+TEST_F(HdfsInsertTest, hdfsInsertTest) {
+  const int64_t kExpectedRows = 1'000;
+  runInsertTest(fmt::format("{}/", miniCluster->url()), kExpectedRows, pool());
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  folly::Init init{&argc, &argv, false};
+  return RUN_ALL_TESTS();
+}
