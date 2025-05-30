818f23d30465244398198876e7c370caefd41a01
Yi Cheng Lee <yclee03@meta.com>
2025-05-30 11:07:32 -0700

    feat: Add skeleton code for TextRowReader (#13501)
    Summary: Pull Request resolved: https://github.com/facebookincubator/velox/pull/13501
    
    Reviewed By: Yuhta, zacw7
    
    Differential Revision: D75468154
    
    fbshipit-source-id: 21cfc0735246118125ae077ab2ec9eb6cfdaef58

--- velox/dwio/text/TextReader.cpp
+++ velox/dwio/text/TextReader.cpp
@@ -17,25 +17,74 @@
 #include "velox/dwio/text/TextReader.h"
 
 using namespace facebook::velox::dwio::common;
-using namespace facebook::velox::common;
 
 namespace facebook::velox::text {
 
+namespace {
+
+class TextRowReader : public RowReader {
+ public:
+  /// TODO: Add implementation
+  explicit TextRowReader(
+      std::unique_ptr<RowReader> /*rowReader*/,
+      memory::MemoryPool& pool,
+      const std::shared_ptr<common::ScanSpec>& /*scanSpec*/)
+      : pool_(pool) {}
+
+  /// TODO: Add implementation
+  int64_t nextRowNumber() override {
+    return 0;
+  }
+
+  /// TODO: Add implementation
+  int64_t nextReadSize(uint64_t /*size*/) override {
+    return 0;
+  }
+
+  /// TODO: Add implementation
+  uint64_t next(
+      uint64_t /*size*/,
+      VectorPtr& /*result*/,
+      const Mutation* /*mutation*/) override {
+    return 0;
+  }
+
+  void updateRuntimeStats(RuntimeStatistics& /*stats*/) const override {
+    // No-op for non-selective reader.
+    return;
+  }
+
+  void resetFilterCaches() override {
+    // No-op for non-selective reader.
+    return;
+  }
+
+  std::optional<size_t> estimatedRowSize() const override {
+    return std::nullopt;
+  }
+
+ private:
+  std::unique_ptr<RowReader> rowReader_;
+  memory::MemoryPool& pool_;
+  std::unique_ptr<BaseVector> batch_;
+  std::shared_ptr<common::ScanSpec> scanSpec_;
+};
+
+} // namespace
+
 /// TODO: Add implementation
 std::unique_ptr<Reader> TextReaderFactory::createReader(
-    std::unique_ptr<BufferedInput> input,
-    const velox::dwio::common::ReaderOptions& options) {
+    std::unique_ptr<BufferedInput> /*input*/,
+    const dwio::common::ReaderOptions& /*options*/) {
   return nullptr;
 }
 
 void registerTextReaderFactory() {
-  velox::dwio::common::registerReaderFactory(
-      std::make_shared<TextReaderFactory>());
+  registerReaderFactory(std::make_shared<TextReaderFactory>());
 }
 
 void unregisterTextReaderFactory() {
-  velox::dwio::common::unregisterReaderFactory(
-      velox::dwio::common::FileFormat::TEXT);
+  unregisterReaderFactory(FileFormat::TEXT);
 }
 
 } // namespace facebook::velox::text
