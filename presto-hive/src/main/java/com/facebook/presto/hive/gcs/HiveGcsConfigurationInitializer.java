0824184b16f21051e4acaa15f339e23b9ceef4bc
Jalpreet Singh Nanda (:imjalpreet) <jalpreet.nanda@ibm.com>
2025-06-05 13:59:45 +0530

    Add support for GCS Key encryption
    Co-authored-by: Ramesh Kanna S <rameshkannas@ibm.com>
    Co-authored-by: Anthony Zhang <zuyu.zhang@ibm.com>
    
    Alchemy-item: [Add support for GCS Key encryption](https://github.ibm.com/lakehouse/presto/pull/3410#issuecomment-116487070) commit 1/1 - 7dd56e49e4c9202f9c41efde0589132e0ba7c853

--- presto-hive/src/main/java/com/facebook/presto/hive/gcs/HiveGcsConfigurationInitializer.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/gcs/HiveGcsConfigurationInitializer.java
@@ -13,18 +13,31 @@
  */
 package com.facebook.presto.hive.gcs;
 
+import com.facebook.airlift.log.Logger;
+import com.facebook.presto.common.security.CryptoLibrary;
+import com.facebook.presto.spi.PrestoException;
 import com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem;
 import jakarta.inject.Inject;
 import org.apache.hadoop.conf.Configuration;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+import static com.facebook.presto.common.util.LocalFileWriter.writeTextToFile;
+import static com.facebook.presto.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
 import static com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystemBase.AUTHENTICATION_PREFIX;
 import static com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystemConfiguration.AUTH_SERVICE_ACCOUNT_ENABLE;
 import static com.google.cloud.hadoop.util.AccessTokenProviderClassFromConfigFactory.ACCESS_TOKEN_PROVIDER_IMPL_SUFFIX;
 import static com.google.cloud.hadoop.util.EntriesCredentialConfiguration.JSON_KEYFILE_SUFFIX;
+import static com.google.common.base.Preconditions.checkArgument;
 
 public class HiveGcsConfigurationInitializer
         implements GcsConfigurationInitializer
 {
+    private static final Logger log = Logger.get(HiveGcsConfigurationInitializer.class);
+    private static final int BUFFER_SIZE = 4096;
     private final boolean useGcsAccessToken;
     private final String jsonKeyFilePath;
 
@@ -47,7 +60,47 @@ public class HiveGcsConfigurationInitializer
         else if (jsonKeyFilePath != null) {
             // use service account key file
             config.set(AUTH_SERVICE_ACCOUNT_ENABLE.getKey(), "true");
-            config.set(AUTHENTICATION_PREFIX + JSON_KEYFILE_SUFFIX, jsonKeyFilePath);
+
+            String encryptedJsonKey = readFileContents(jsonKeyFilePath);
+            String decryptedJsonKeyFilePath = constructDecryptedFilePath(jsonKeyFilePath);
+            decryptProperties(encryptedJsonKey, decryptedJsonKeyFilePath);
+            config.set(AUTHENTICATION_PREFIX + JSON_KEYFILE_SUFFIX, decryptedJsonKeyFilePath);
+        }
+    }
+
+    private String constructDecryptedFilePath(String jsonKeyFilePath)
+    {
+        return jsonKeyFilePath.substring(0, jsonKeyFilePath.lastIndexOf('/') + 1) + "decryptedFile.json";
+    }
+
+    private void decryptProperties(String encryptedString, String decryptedJsonKeyFilePath)
+    {
+        // Decrypt the encrypted JSON string and then decode it to retrieve the Key.
+        try {
+            final int outPlaintextLength = 2 * BUFFER_SIZE;
+            byte[] outPlaintext = new byte[outPlaintextLength];
+            int decResp = CryptoLibrary.INSTANCE.do_decrypt_string(encryptedString, outPlaintext, outPlaintextLength);
+            byte[] decodedBytes = Base64.getDecoder().decode(new String(outPlaintext, 0, decResp));
+            String decryptedKey = new String(decodedBytes, StandardCharsets.UTF_8);
+            writeTextToFile(decryptedKey, decryptedJsonKeyFilePath);
+        }
+        catch (Exception e) {
+            log.error("Error occurred while decrypting json string", e);
+            throw new PrestoException(GENERIC_INTERNAL_ERROR, "Unable to decrypt JsonKeyfile");
+        }
+    }
+
+    private String readFileContents(String fileLocation)
+    {
+        //Read file content in encrypted file.
+        checkArgument(fileLocation != null && !fileLocation.isEmpty(), "File path is null or empty");
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(fileLocation));
+            return new String(bytes, StandardCharsets.UTF_8);
+        }
+        catch (Exception exc) {
+            log.error("Error occurred while loading encrypted properties from jsonKey file");
+            throw new PrestoException(GENERIC_INTERNAL_ERROR, "Unable to load encrypted file properties");
         }
     }
 }
