d6705fcec4b4af3f9f3ea35c23d538209c95b057
Shelton Cai <shelton408@meta.com>
2025-06-06 17:09:11 -0700

    Revert Move Bootstrap.class to presto-bytecode to avoid duplicate definitions [D75970190] (#25260)
    Summary:
    
    This diff reverts D75970190
    Reverts https://github.com/prestodb/presto/pull/25245?fbclid=IwY2xjawKwI6BleHRuA2FlbQIxMQBicmlkETBoYW9yclAzdjR2bW9rU1ZlAR5T6z9TdGaufXbSNH8Y7s5HI1NhghLSwgqbKWZLniAvsDvnZshdKowS2qj08A_aem_CLbW4rp6V1haCMjCgdqPDg
    Causing issues in our facebook internal verifier. Not sure if issue is with facebook internal, or oss spi. Temporary revert so we can continue with release while we attempt to fix the issue.
    
    Depends on D75970190
    
    Differential Revision: D76145724

--- presto-main-base/src/main/java/com/facebook/presto/sql/gen/Bootstrap.java
+++ presto-main-base/src/main/java/com/facebook/presto/sql/gen/Bootstrap.java
@@ -11,7 +11,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.bytecode;
+package com.facebook.presto.sql.gen;
+
+import com.facebook.presto.bytecode.DynamicClassLoader;
 
 import java.lang.invoke.CallSite;
 import java.lang.invoke.ConstantCallSite;
