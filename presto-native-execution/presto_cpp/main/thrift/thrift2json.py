30f6642255f3a3789ddaab59ef6fa8d26499f636
Vivian Hsu <vivianhsu@meta.com>
2025-06-12 15:30:07 -0700

    [native] changes in ProtocolToThrift files based on IDL change (#25308)
    Summary:
    Pull Request resolved: https://github.com/prestodb/presto/pull/25308
    
    IDL changes for TimeZoneKey and HostAddress in https://github.com/prestodb/presto/pull/25292 to address https://github.com/prestodb/presto/issues/25287
    
    Rollback Plan:
    
    Differential Revision: D76545874

--- presto-native-execution/presto_cpp/main/thrift/thrift2json.py
+++ presto-native-execution/presto_cpp/main/thrift/thrift2json.py
@@ -1,4 +1,4 @@
-#!/Library/Developer/CommandLineTools/usr/bin/python3
+#!/usr/bin/env python3
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
