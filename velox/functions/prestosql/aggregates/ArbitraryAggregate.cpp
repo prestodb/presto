778beb89c20d8101bfc9f1f012cdbb823087f9bf
Xiaoxuan Meng <xiaoxmeng@meta.com>
2025-06-24 16:49:16 -0700

    fix: Back out "[velox] misc: Optimize arbitrary streaming aggregation data copy" (#13869)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/13869
    
    Original commit changeset: 05b0c3a64a12
    
    Original Phabricator Diff: D77003688
    
    Reviewed By: spershin, amitkdutta
    
    Differential Revision: D77253608
    
    fbshipit-source-id: 707d7e473ec88692ff1609d0a01de914fee1dbc7

--- velox/functions/prestosql/aggregates/ArbitraryAggregate.cpp
+++ velox/functions/prestosql/aggregates/ArbitraryAggregate.cpp
@@ -176,11 +176,12 @@ class NonNumericArbitrary : public exec::Aggregate {
 
   void extractValues(char** groups, int32_t numGroups, VectorPtr* result)
       override {
-    VELOX_CHECK_NOT_NULL(result);
+    VELOX_CHECK(result);
     (*result)->resize(numGroups);
 
+    auto* rawNulls = exec::Aggregate::getRawNulls(result->get());
+
     if (clusteredInput_) {
-      bool singleSource{true};
       VectorPtr* currentSource = nullptr;
       VELOX_DCHECK(copyRanges_.empty());
       for (vector_size_t i = 0; i < numGroups; ++i) {
@@ -189,9 +190,8 @@ class NonNumericArbitrary : public exec::Aggregate {
           (*result)->setNull(i, true);
           continue;
         }
-        if ((currentSource != nullptr) &&
-            (currentSource->get() != accumulator->vector.get())) {
-          singleSource = false;
+        if (currentSource &&
+            currentSource->get() != accumulator->vector.get()) {
           result->get()->copyRanges(currentSource->get(), copyRanges_);
           copyRanges_.clear();
         }
@@ -203,33 +203,11 @@ class NonNumericArbitrary : public exec::Aggregate {
           copyRanges_.push_back(range);
         }
       }
-      if (currentSource != nullptr) {
-        if (!singleSource) {
-          result->get()->copyRanges(currentSource->get(), copyRanges_);
-        } else {
-          if (copyRanges_.size() == 1) {
-            *result = currentSource->get()->slice(
-                copyRanges_[0].sourceIndex, copyRanges_[0].count);
-          } else {
-            prepareGroupIndices(numGroups, result->get()->pool());
-            applyToEachRange(
-                copyRanges_,
-                [&](auto targetIndex, auto sourceIndex, auto count) {
-                  for (auto i = 0; i < count; ++i) {
-                    rawGroupIndices_[targetIndex + i] = sourceIndex + i;
-                  }
-                });
-            *result = BaseVector::wrapInDictionary(
-                result->get()->nulls(),
-                groupIndices_,
-                numGroups,
-                *currentSource);
-          }
-        }
+      if (currentSource) {
+        result->get()->copyRanges(currentSource->get(), copyRanges_);
         copyRanges_.clear();
       }
     } else {
-      auto* rawNulls = exec::Aggregate::getRawNulls(result->get());
       for (int32_t i = 0; i < numGroups; ++i) {
         auto* accumulator = value<SingleValueAccumulator>(groups[i]);
         if (!accumulator->hasValue()) {
@@ -240,16 +218,6 @@ class NonNumericArbitrary : public exec::Aggregate {
         }
       }
     }
-  }
-
-  void prepareGroupIndices(size_t numGroups, memory::MemoryPool* pool) {
-    const auto groupByteSize = numGroups * sizeof(vector_size_t);
-    if ((groupIndices_ == nullptr) || !groupIndices_->unique() ||
-        (groupIndices_->capacity() < groupByteSize)) {
-      groupIndices_ = allocateIndices(numGroups, pool);
-      rawGroupIndices_ = groupIndices_->asMutable<vector_size_t>();
-    }
-    groupIndices_->setSize(groupByteSize);
   }
 
   void extractAccumulators(char** groups, int32_t numGroups, VectorPtr* result)
@@ -404,10 +372,6 @@ class NonNumericArbitrary : public exec::Aggregate {
 
   // Copy ranges used when extracting from ClusteredNonNumericAccumulator.
   std::vector<BaseVector::CopyRange> copyRanges_;
-
-  // Reusable buffers.
-  BufferPtr groupIndices_;
-  vector_size_t* rawGroupIndices_;
 };
 
 } // namespace
