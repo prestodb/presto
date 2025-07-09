c60851e0d92d423d731c7ac7db92d005d530033a
Peter Enescu <enescu@meta.com>
2025-07-09 12:00:23 -0700

    feat: Add Github Fuzzer job for Presto SOT Expression Evaluation (#13977)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/13977
    
    Add a new Github job to the fuzzer job suite to run Presto expression fuzzer using Presto as a source of truth (comparing Velox expression evaluation with Presto queries).
    
    Reviewed By: kagamiori
    
    Differential Revision: D77628147
    
    fbshipit-source-id: 0956104c497e74dd19422f1213ba912a90bc860b

--- velox/expression/fuzzer/ExpressionFuzzerTest.cpp
+++ velox/expression/fuzzer/ExpressionFuzzerTest.cpp
@@ -87,18 +87,23 @@ int main(int argc, char** argv) {
   std::unordered_set<std::string> skipFunctions = {
       "element_at",
       "width_bucket",
-      // Fuzzer and the underlying engine are confused about TDigest output
+      // Fuzzer and the underlying engine are confused about TDigest functions
       // (since TDigest is a user defined type), and tries to pass a
       // VARBINARY (since TDigest's implementation uses an
       // alias to VARBINARY).
+      "value_at_quantile",
+      "values_at_quantiles",
       "merge_tdigest",
-      "construct_tdigest",
+      "scale_tdigest",
+      "quantile_at_value",
       "quantiles_at_values",
-      "values_at_quantiles", // Skip until
-                             // https://github.com/prestodb/presto/pull/25291 is
-                             // released
+      "construct_tdigest",
+      "destructure_tdigest",
+      "trimmed_mean",
       // Fuzzer cannot generate valid 'comparator' lambda.
       "array_sort(array(T),constant function(T,T,bigint)) -> array(T)",
+      "array_sort(array(T),constant function(T,U)) -> array(T)",
+      "array_sort_desc(array(T),constant function(T,U)) -> array(T)",
       "split_to_map(varchar,varchar,varchar,function(varchar,varchar,varchar,varchar)) -> map(varchar,varchar)",
       // https://github.com/facebookincubator/velox/issues/8919
       "plus(date,interval year to month) -> date",
@@ -112,13 +117,19 @@ int main(int argc, char** argv) {
       "regexp_like",
       "regexp_replace",
       "regexp_split",
-      // date_format and format_datetime throw VeloxRuntimeError when input
-      // timestamp is out of the supported range.
-      "date_format",
-      "format_datetime",
-      // from_unixtime can generate timestamps out of the supported range that
-      // make other functions throw VeloxRuntimeErrors.
-      "from_unixtime",
+      "map_top_n", // https://github.com/facebookincubator/velox/issues/9497
+      // These can take 2 random int32s and makes an NxM matrix. Naturally this
+      // matrix can be very large taking a long time to populate and OOM'ing the
+      // host.
+      "bootstrap_metrics",
+      "bootstrap_metrics_list",
+      // This function invokes a Boost library function with a known issue where
+      // it hits integer overflows when the arguments are very large.
+      // https://github.com/facebookincubator/velox/issues/11802
+      "inverse_beta_cdf",
+      // https://github.com/facebookincubator/velox/issues/13047
+      "inverse_poisson_cdf",
+      "map_subset", // https://github.com/facebookincubator/velox/issues/12654
       // JSON not supported, Real doesn't match exactly, etc.
       "array_join(array(json),varchar) -> varchar",
       "array_join(array(json),varchar,varchar) -> varchar",
@@ -126,9 +137,9 @@ int main(int argc, char** argv) {
       "array_join(array(real),varchar,varchar) -> varchar",
       "array_join(array(double),varchar) -> varchar",
       "array_join(array(double),varchar,varchar) -> varchar",
-      // https://github.com/facebookincubator/velox/issues/13047
-      "inverse_poisson_cdf",
-      "map_subset", // https://github.com/facebookincubator/velox/issues/12654
+      // Distance functions
+      "cosine_similarity", // T224131333
+      "l2_squared", // T228562088
       // Geometry functions don't yet have a ValuesGenerator
       "st_geometryfromtext",
       "st_geomfrombinary",
@@ -161,14 +172,14 @@ int main(int argc, char** argv) {
       "st_point",
       "st_x",
       "st_y",
-      "st_xmin",
-      "st_ymin",
-      "st_xmax",
-      "st_ymax",
       "st_isvalid",
       "st_issimple",
       "geometry_invalid_reason",
       "simplify_geometry",
+      "st_xmax",
+      "st_xmin",
+      "st_ymax",
+      "st_ymin",
   };
   size_t initialSeed = FLAGS_seed == 0 ? std::time(nullptr) : FLAGS_seed;
 
@@ -217,9 +228,16 @@ int main(int argc, char** argv) {
     // Add additional functions to skip since we are now querying Presto
     // directly and are aware of certain failures.
     skipFunctions.insert({
-        "cast(real) -> varchar", // https://github.com/facebookincubator/velox/issues/11034
+        // https://github.com/facebookincubator/velox/issues/11034
+        "cast(real) -> varchar",
+        "cast(row(real)) -> row(varchar)",
+        "cast(double) -> varchar",
+        "cast(row(double)) -> row(varchar)",
+        "cast(array(double)) -> array(varchar)",
+        "cast(array(real)) -> array(varchar)",
+        "cast(map(varchar,double)) -> map(varchar,varchar)",
+        "cast(map(varchar,real)) -> map(varchar,varchar)",
         "round", // https://github.com/facebookincubator/velox/issues/10634
-        "json_size", // https://github.com/facebookincubator/velox/issues/10700
         "bitwise_right_shift_arithmetic", // https://github.com/facebookincubator/velox/issues/10841
         "map_size_with", // https://github.com/facebookincubator/velox/issues/10964
         "binomial_cdf", // https://github.com/facebookincubator/velox/issues/10702
@@ -227,8 +245,6 @@ int main(int argc, char** argv) {
         "gamma_cdf", // https://github.com/facebookincubator/velox/issues/10749
         "inverse_normal_cdf", // https://github.com/facebookincubator/velox/issues/10836
         "regr_avgx", // https://github.com/facebookincubator/velox/issues/10610
-        "is_json_scalar", // https://github.com/facebookincubator/velox/issues/10748
-        "json_extract_scalar", // https://github.com/facebookincubator/velox/issues/10698
         "rtrim", // https://github.com/facebookincubator/velox/issues/10973
         "split", // https://github.com/facebookincubator/velox/issues/10867
         "array_intersect", // https://github.com/facebookincubator/velox/issues/10740
@@ -261,30 +277,89 @@ int main(int argc, char** argv) {
         "bitwise_right_shift", // https://github.com/facebookincubator/velox/issues/12339
         "word_stem", // https://github.com/facebookincubator/velox/issues/12341
         "rpad", // https://github.com/facebookincubator/velox/issues/12343
-        "json_extract", // https://github.com/facebookincubator/velox/issues/12344
         "to_ieee754_32", // https://github.com/facebookincubator/velox/issues/12345
         "beta_cdf", // https://github.com/facebookincubator/velox/issues/12346
         "wilson_interval_upper", // https://github.com/facebookincubator/velox/issues/12347
-        "json_parse", // https://github.com/facebookincubator/velox/issues/12371
         "to_ieee754_64", // https://github.com/facebookincubator/velox/issues/12372
-        // No default Hive type provided for unsupported Hive type: json
+        "inverse_weibull_cdf", // https://github.com/facebookincubator/velox/issues/12550
+        "from_utf8", // https://github.com/facebookincubator/velox/issues/12554
+        // https://github.com/facebookincubator/velox/issues/12555
+        "year",
+        "year_of_week",
+        "day_of_year",
+        "week_of_year",
+        "doy",
+        "minute",
+        "quarter",
+        "yow",
+        "day",
+        "day_of_month",
+        "dow",
+        "week",
+        "cast(timestamp) -> varchar", // https://github.com/facebookincubator/velox/issues/12803
+        "from_iso8601_timestamp", // https://github.com/facebookincubator/velox/issues/13605
+        "typeof", // https://github.com/facebookincubator/velox/issues/12804
+        "escape", // https://github.com/facebookincubator/velox/issues/12558
+        "from_base64url", // https://github.com/facebookincubator/velox/issues/12562
+        "array_top_n", // https://github.com/prestodb/presto/issues/24700
+        "codepoint", // https://github.com/facebookincubator/velox/issues/12598
+        "in", // https://github.com/facebookincubator/velox/issues/12597
+        "multimap_from_entries", // https://github.com/facebookincubator/velox/issues/12628
+        "map_zip_with", // https://github.com/facebookincubator/velox/issues/10964
+        "transform_keys", // https://github.com/facebookincubator/velox/issues/12992
+        "arrays_overlap", // https://github.com/facebookincubator/velox/issues/12651
+        "array_normalize", // https://github.com/facebookincubator/velox/issues/12623
+        "array_distinct", // https://github.com/prestodb/presto/issues/24719
+        "power", // https://github.com/facebookincubator/velox/issues/12557
+        "like", // https://github.com/prestodb/presto/issues/24747
+        "slice", // https://github.com/facebookincubator/velox/issues/12782
+        "inverse_gamma_cdf", // https://github.com/facebookincubator/velox/issues/12918
+        "atan2", // T219988962
+        "cosine_similarity", // https://github.com/facebookincubator/velox/issues/12915
+        "l2_squared", // https://github.com/facebookincubator/velox/issues/12915
+        "subscript", // https://github.com/facebookincubator/velox/issues/12959
+        "reduce", // T220595632
+        "inverse_binomial_cdf", // https://github.com/facebookincubator/velox/issues/12981
+        "inverse_poisson_cdf", // https://github.com/facebookincubator/velox/issues/12982
+        // https://github.com/facebookincubator/velox/issues/13002
+        "ceiling",
+        "ceil",
+        // https://github.com/facebookincubator/velox/issues/13132
+        "normalize",
+        "concat",
+        "trim", // https://github.com/facebookincubator/velox/issues/13176
+        "map_key_exists", // https://github.com/facebookincubator/velox/issues/13190
+        "map_from_entries", // https://github.com/facebookincubator/velox/issues/13191
+        "switch", // https://github.com/prestodb/presto/issues/25077
+        "degrees", // https://github.com/facebookincubator/velox/issues/13608
+        "substring", // T225620908
+        "json_extract", // https://github.com/facebookincubator/velox/issues/12344
+        "to_milliseconds", // https://github.com/prestodb/presto/issues/25275
+        "parse_duration", // https://github.com/prestodb/presto/issues/25340
         "json_format",
-        "json_array_length",
-        "json_array_get",
-        "json_array_contains",
+        "json_array_length", // https://github.com/facebookincubator/velox/issues/13356
+        "is_json_scalar", // https://github.com/facebookincubator/velox/issues/10748
+        "json_size", // https://github.com/facebookincubator/velox/issues/12371
+        "json_extract_scalar", // https://github.com/facebookincubator/velox/issues/10698
+        "json_array_contains", // https://github.com/facebookincubator/velox/issues/13685
         "clamp", // Function clamp not registered
         "current_date", // Non-deterministic
         "xxhash64_internal",
         "combine_hash_internal",
-        "map_keys_by_top_n_values", // requires
-                                    // https://github.com/prestodb/presto/pull/24570
-        "inverse_gamma_cdf", // https://github.com/facebookincubator/velox/issues/12918
-        "inverse_binomial_cdf", // https://github.com/facebookincubator/velox/issues/12981
-        "inverse_poisson_cdf", // https://github.com/facebookincubator/velox/issues/12982
+        "noisy_avg_gaussian", // Non-deterministic
+        "noisy_count_if_gaussian", // Non-deterministic
+        "noisy_count_gaussian", // Non-deterministic
+        "noisy_sum_gaussian", // Non-deterministic
+        "second", // https://github.com/prestodb/presto/pull/25090
         "inverse_f_cdf", // https://github.com/facebookincubator/velox/issues/13715
+        // https://github.com/facebookincubator/velox/issues/13767
+        "array_max(array(__user_T1)) -> __user_T1",
+        "array_min(array(__user_T1)) -> __user_T1",
         "inverse_chi_squared_cdf", // https://github.com/facebookincubator/velox/issues/13788
         "bing_tile_children", // Velox limits the max zoom shift
                               // https://github.com/facebookincubator/velox/pull/13604
+        // Not registered
+        "array_sum_propagate_element_null",
     });
 
     referenceQueryRunner = std::make_shared<PrestoQueryRunner>(
