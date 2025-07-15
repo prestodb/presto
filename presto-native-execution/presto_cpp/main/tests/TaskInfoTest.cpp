17a99a7188fb0df6ba21f13e2835ce016cfa7fd4
vhsu14 <167463318+vhsu14@users.noreply.github.com>
2025-07-15 09:54:22 -0700

    [native] [presto] Expand connector specific fields (#25474)
    ## Description
    - Native changes to expand connector specific fields for thrift
    migration
    - Depends on https://github.com/prestodb/presto/pull/25242
    
    ## Motivation and Context
    https://github.com/prestodb/rfcs/pull/38
    
    ## Test Plan
    Build package and test in verifier: 230498
    
    ## Release Notes
    Please follow [release notes
    guidelines](https://github.com/prestodb/presto/wiki/Release-Notes-Guidelines)
    and fill in the release notes below.
    
    ```
    == RELEASE NOTES ==
    Prestissimo (Native Execution) Changes
    * Update thrift IDL to expand connector specific fields 
    
    ```

--- presto-native-execution/presto_cpp/main/tests/TaskInfoTest.cpp
+++ presto-native-execution/presto_cpp/main/tests/TaskInfoTest.cpp
@@ -18,40 +18,45 @@
 #include "presto_cpp/main/common/tests/test_json.h"
 #include "presto_cpp/main/connectors/PrestoToVeloxConnector.h"
 
-using namespace facebook;
-using namespace facebook::presto::protocol;
+using namespace facebook::presto;
 
-class TaskInfoTest : public ::testing::Test {};
+class TaskInfoTest : public ::testing::Test {
+  protected:
+  void SetUp() override {
+    registerPrestoToVeloxConnector(
+        std::make_unique<facebook::presto::HivePrestoToVeloxConnector>("hive"));
+  }
+
+  void TearDown() override {
+    unregisterPrestoToVeloxConnector("hive");
+  }
+};
 
 const std::string BASE_DATA_PATH = "/github/presto-trunk/presto-native-execution/presto_cpp/main/tests/data/";
 
 TEST_F(TaskInfoTest, duration) {
   double thrift = 0;
-  facebook::presto::thrift::toThrift(Duration(123, TimeUnit::MILLISECONDS), thrift);
+  thrift::toThrift(protocol::Duration(123, protocol::TimeUnit::MILLISECONDS), thrift);
   ASSERT_EQ(thrift, 123);
 }
 
 TEST_F(TaskInfoTest, binaryMetadataUpdates) {
   std::string str = slurp(getDataPath(BASE_DATA_PATH, "MetadataUpdates.json"));
   json j = json::parse(str);
-  registerPrestoToVeloxConnector(std::make_unique<facebook::presto::HivePrestoToVeloxConnector>("hive"));
-  MetadataUpdates metadataUpdates = j;
+  protocol::MetadataUpdates metadataUpdates = j;
   std::unique_ptr<std::string> thriftMetadataUpdates = std::make_unique<std::string>();
-  facebook::presto::thrift::toThrift(metadataUpdates, *thriftMetadataUpdates);
+  thrift::toThrift(metadataUpdates, *thriftMetadataUpdates);
 
   json thriftJson = json::parse(*thriftMetadataUpdates);
   ASSERT_EQ(j, thriftJson);
-
-  presto::unregisterPrestoToVeloxConnector("hive");
 }
 
 TEST_F(TaskInfoTest, taskInfo) {
   std::string str = slurp(getDataPath(BASE_DATA_PATH, "TaskInfo.json"));
   json j = json::parse(str);
-  registerPrestoToVeloxConnector(std::make_unique<facebook::presto::HivePrestoToVeloxConnector>("hive"));
-  TaskInfo taskInfo = j;
-  facebook::presto::thrift::TaskInfo thriftTaskInfo;
-  facebook::presto::thrift::toThrift(taskInfo, thriftTaskInfo);
+  protocol::TaskInfo taskInfo = j;
+  thrift::TaskInfo thriftTaskInfo;
+  thrift::toThrift(taskInfo, thriftTaskInfo);
   
   json thriftJson = json::parse(*thriftTaskInfo.metadataUpdates()->metadataUpdates());
   ASSERT_EQ(taskInfo.metadataUpdates, thriftJson);
@@ -59,18 +64,16 @@ TEST_F(TaskInfoTest, taskInfo) {
   ASSERT_EQ(thriftTaskInfo.outputBuffers()->buffers()->size(), 2);
   ASSERT_EQ(thriftTaskInfo.outputBuffers()->buffers()[0].bufferId()->id(), 100);
   ASSERT_EQ(thriftTaskInfo.outputBuffers()->buffers()[1].bufferId()->id(), 200);
-  ASSERT_EQ(thriftTaskInfo.stats()->blockedReasons()->count(facebook::presto::thrift::BlockedReason::WAITING_FOR_MEMORY), 1);
+  ASSERT_EQ(thriftTaskInfo.stats()->blockedReasons()->count(thrift::BlockedReason::WAITING_FOR_MEMORY), 1);
   ASSERT_EQ(thriftTaskInfo.stats()->runtimeStats()->metrics()->size(), 2);
   ASSERT_EQ(thriftTaskInfo.stats()->runtimeStats()->metrics()["test_metric1"].sum(), 123);
   ASSERT_EQ(thriftTaskInfo.stats()->runtimeStats()->metrics()["test_metric2"].name(), "test_metric2");
-
-  presto::unregisterPrestoToVeloxConnector("hive");
 }
 
 TEST_F(TaskInfoTest, taskId) {
-  TaskId taskId = "queryId.1.2.3.4";
-  facebook::presto::thrift::TaskId thriftTaskId;
-  facebook::presto::thrift::toThrift(taskId, thriftTaskId);
+  protocol::TaskId taskId = "queryId.1.2.3.4";
+  thrift::TaskId thriftTaskId;
+  thrift::toThrift(taskId, thriftTaskId);
   
   ASSERT_EQ(thriftTaskId.stageExecutionId()->stageId()->queryId(), "queryId");
   ASSERT_EQ(thriftTaskId.stageExecutionId()->stageId()->id(), 1);
@@ -83,9 +86,9 @@ TEST_F(TaskInfoTest, taskId) {
 TEST_F(TaskInfoTest, operatorStatsEmptyBlockedReason) {
   std::string str = slurp(getDataPath(BASE_DATA_PATH, "OperatorStatsEmptyBlockedReason.json"));
   json j = json::parse(str);
-  OperatorStats operatorStats = j;
-  facebook::presto::thrift::OperatorStats thriftOperatorStats;
-  facebook::presto::thrift::toThrift(operatorStats, thriftOperatorStats);
+  protocol::OperatorStats operatorStats = j;
+  thrift::OperatorStats thriftOperatorStats;
+  thrift::toThrift(operatorStats, thriftOperatorStats);
   
   ASSERT_EQ(thriftOperatorStats.blockedReason().has_value(), false);
   ASSERT_EQ(thriftOperatorStats.blockedWall(), 80);
@@ -95,9 +98,9 @@ TEST_F(TaskInfoTest, operatorStatsEmptyBlockedReason) {
 TEST_F(TaskInfoTest, operatorStats) {
   std::string str = slurp(getDataPath(BASE_DATA_PATH, "OperatorStats.json"));
   json j = json::parse(str);
-  OperatorStats operatorStats = j;
-  facebook::presto::thrift::OperatorStats thriftOperatorStats;
-  facebook::presto::thrift::toThrift(operatorStats, thriftOperatorStats);
+  protocol::OperatorStats operatorStats = j;
+  thrift::OperatorStats thriftOperatorStats;
+  thrift::toThrift(operatorStats, thriftOperatorStats);
   
-  ASSERT_EQ(thriftOperatorStats.blockedReason(), facebook::presto::thrift::BlockedReason::WAITING_FOR_MEMORY);
+  ASSERT_EQ(thriftOperatorStats.blockedReason(), thrift::BlockedReason::WAITING_FOR_MEMORY);
 }
