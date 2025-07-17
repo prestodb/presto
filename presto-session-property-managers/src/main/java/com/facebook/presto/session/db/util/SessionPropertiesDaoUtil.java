77c666dbd30fa41babf63c305198ece9e88cf082
Hazmi <ialhazmim@gmail.com>
2025-07-17 18:24:43 +0300

    Add a DB based session property manager
    Co-authored-by: Ashish Tadose <ashishtadose@gmail.com>
    Co-authored-by: Ariel Weisberg <aweisberg@fb.com>
    Co-authored-by: Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>

--- presto-session-property-managers/src/main/java/com/facebook/presto/session/db/util/SessionPropertiesDaoUtil.java
+++ presto-session-property-managers/src/main/java/com/facebook/presto/session/db/util/SessionPropertiesDaoUtil.java
@@ -11,28 +11,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.session;
+package com.facebook.presto.session.db.util;
 
-import com.facebook.airlift.configuration.Config;
-
-import javax.validation.constraints.NotNull;
-
-import java.io.File;
-
-public class FileSessionPropertyManagerConfig
+public final class SessionPropertiesDaoUtil
 {
-    private File configFile;
+    private SessionPropertiesDaoUtil() {}
 
-    @NotNull
-    public File getConfigFile()
-    {
-        return configFile;
-    }
-
-    @Config("session-property-manager.config-file")
-    public FileSessionPropertyManagerConfig setConfigFile(File configFile)
-    {
-        this.configFile = configFile;
-        return this;
-    }
+    public static final String SESSION_SPECS_TABLE = "session_specs";
+    public static final String CLIENT_TAGS_TABLE = "session_client_tags";
+    public static final String PROPERTIES_TABLE = "session_property_values";
 }
