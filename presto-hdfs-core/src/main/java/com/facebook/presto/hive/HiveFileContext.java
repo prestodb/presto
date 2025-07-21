11383c22463691a4b03077c1e2b02666fb7d3180
Nikhil Collooru <nikhilcollooru@meta.com>
2025-07-21 10:41:07 -0700

    Add new fields to HiveFileContext

--- presto-hdfs-core/src/main/java/com/facebook/presto/hive/HiveFileContext.java
+++ presto-hdfs-core/src/main/java/com/facebook/presto/hive/HiveFileContext.java
@@ -15,9 +15,12 @@ package com.facebook.presto.hive;
 
 import com.facebook.presto.common.RuntimeStats;
 import com.facebook.presto.common.RuntimeUnit;
+import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.security.ConnectorIdentity;
 
 import java.util.Optional;
 import java.util.OptionalLong;
+import java.util.Set;
 
 import static com.facebook.presto.hive.CacheQuota.NO_CACHE_CONSTRAINTS;
 import static java.util.Objects.requireNonNull;
@@ -47,6 +50,12 @@ public class HiveFileContext
     private final OptionalLong length;
     private final long modificationTime;
     private final boolean verboseRuntimeStatsEnabled;
+    private final Optional<String> source;
+    private final Optional<ConnectorIdentity> identity;
+    private final Optional<String> queryId;
+    private final Optional<String> schema;
+    private final Optional<String> clientInfo;
+    private final Optional<Set<String>> clientTags;
 
     private final RuntimeStats stats;
 
@@ -74,6 +83,71 @@ public class HiveFileContext
             boolean verboseRuntimeStatsEnabled,
             RuntimeStats runtimeStats)
     {
+        this(
+                cacheable,
+                cacheQuota,
+                extraFileInfo,
+                fileSize,
+                startOffset,
+                length,
+                modificationTime,
+                verboseRuntimeStatsEnabled,
+                runtimeStats,
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty());
+    }
+
+    public HiveFileContext(
+            boolean cacheable,
+            CacheQuota cacheQuota,
+            Optional<ExtraHiveFileInfo<?>> extraFileInfo,
+            OptionalLong fileSize,
+            OptionalLong startOffset,
+            OptionalLong length,
+            long modificationTime,
+            boolean verboseRuntimeStatsEnabled,
+            RuntimeStats runtimeStats,
+            ConnectorSession session)
+    {
+        this(
+                cacheable,
+                cacheQuota,
+                extraFileInfo,
+                fileSize,
+                startOffset,
+                length,
+                modificationTime,
+                verboseRuntimeStatsEnabled,
+                runtimeStats,
+                session.getSource(),
+                Optional.of(session.getIdentity()),
+                Optional.of(session.getQueryId()),
+                session.getSchema(),
+                session.getClientInfo(),
+                Optional.of(session.getClientTags()));
+    }
+
+    public HiveFileContext(
+            boolean cacheable,
+            CacheQuota cacheQuota,
+            Optional<ExtraHiveFileInfo<?>> extraFileInfo,
+            OptionalLong fileSize,
+            OptionalLong startOffset,
+            OptionalLong length,
+            long modificationTime,
+            boolean verboseRuntimeStatsEnabled,
+            RuntimeStats runtimeStats,
+            Optional<String> source,
+            Optional<ConnectorIdentity> identity,
+            Optional<String> queryId,
+            Optional<String> schema,
+            Optional<String> clientInfo,
+            Optional<Set<String>> clientTags)
+    {
         this.cacheable = cacheable;
         this.cacheQuota = requireNonNull(cacheQuota, "cacheQuota is null");
         this.extraFileInfo = requireNonNull(extraFileInfo, "extraFileInfo is null");
@@ -83,6 +157,12 @@ public class HiveFileContext
         this.modificationTime = modificationTime;
         this.verboseRuntimeStatsEnabled = verboseRuntimeStatsEnabled;
         this.stats = requireNonNull(runtimeStats, "runtimeStats is null");
+        this.source = requireNonNull(source, "source is null");
+        this.identity = requireNonNull(identity, "identity is null");
+        this.queryId = requireNonNull(queryId, "queryId is null");
+        this.schema = requireNonNull(schema, "schema is null");
+        this.clientInfo = requireNonNull(clientInfo, "clientInfo is null");
+        this.clientTags = requireNonNull(clientTags, "clientTags is null");
     }
 
     /**
@@ -141,5 +221,35 @@ public class HiveFileContext
     public RuntimeStats getStats()
     {
         return stats;
+    }
+
+    public Optional<String> getSource()
+    {
+        return source;
+    }
+
+    public Optional<ConnectorIdentity> getIdentity()
+    {
+        return identity;
+    }
+
+    public Optional<String> getQueryId()
+    {
+        return queryId;
+    }
+
+    public Optional<String> getSchema()
+    {
+        return schema;
+    }
+
+    public Optional<String> getClientInfo()
+    {
+        return clientInfo;
+    }
+
+    public Optional<Set<String>> getClientTags()
+    {
+        return clientTags;
     }
 }
