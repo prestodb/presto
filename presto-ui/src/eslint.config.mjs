4542150f44a3085fbbb341eec86edfa9dd0ee27b
Joe O'Hallaron <johallaron@nvidia.com>
2025-08-06 15:07:31 -0600

    Fix flow, move globals to own block

--- presto-ui/src/eslint.config.mjs
+++ presto-ui/src/eslint.config.mjs
@@ -2,21 +2,28 @@ import ftFlow from "eslint-plugin-ft-flow";
 import js from "@eslint/js";
 import hermes from "hermes-eslint";
 import globals from "globals";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import prettierEslint from "eslint-plugin-prettier/recommended";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
   js.configs.recommended,
-  eslintPluginPrettierRecommended,
+  prettierEslint,
   {
-    // Note: there should be no other properties in this object
     ignores: [
       "**/vendor/**",
       "**/node_modules/**",
       "**/sql-parser/**",
       "webpack.config.js",
     ],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jquery,
+      },
+    },
   },
   // Flow
   {
@@ -37,19 +44,14 @@ export default [
   },
   // React
   {
-    files: ["**/*.jsx"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.jquery,
-      },
-    },
+    files: ["**/*.jsx", "**/use*.js"],
     plugins: {
       react,
       "react-hooks": reactHooks,
     },
     rules: {
       ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
       ["react/prop-types"]: "warn",
       ["react/no-deprecated"]: "warn",
       ["no-prototype-builtins"]: "warn",
