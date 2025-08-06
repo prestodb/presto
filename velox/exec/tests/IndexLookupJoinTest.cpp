45855c35f323b5d2d14031bb14496eb1a606ab06
Xiaoxuan Meng <xiaoxmeng@meta.com>
2025-08-06 13:15:41 -0700

    misc: Remove lazy code option and make it default for ss client (#14343)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/14343
    
    Make lazy decode the default and prevent the potential race condition of concurrent nimble decode which might cause data corruption with prefetch enabled.
    
    Reviewed By: wenqiwooo
    
    Differential Revision: D79705068
    
    fbshipit-source-id: 89044cae0f407b97427a949ac5db9201a6bb9f13

--- velox/exec/tests/IndexLookupJoinTest.cpp
+++ velox/exec/tests/IndexLookupJoinTest.cpp
@@ -2360,9 +2360,6 @@ DEBUG_ONLY_TEST_P(IndexLookupJoinTest, runtimeStats) {
   ASSERT_EQ(runtimeStats.count(IndexLookupJoin::kClientResultProcessTime), 0);
   ASSERT_EQ(runtimeStats.count(IndexLookupJoin::kClientLookupResultSize), 0);
   ASSERT_EQ(runtimeStats.count(IndexLookupJoin::kClientLookupResultRawSize), 0);
-  ASSERT_EQ(
-      runtimeStats.count(IndexLookupJoin::kClientNumLazyDecodedResultBatches),
-      0);
   ASSERT_THAT(
       operatorStats.toString(true, true),
       testing::MatchesRegex(".*Runtime stats.*connectorLookupWallNanos:.*"));
