de532f2615bf62e062bc1f85bfad4522c8080a23
Chris Matzenbach <cmatzenbach@nvidia.com>
2025-08-06 18:47:13 -0500

    Convert PageTitle and subcomponent into function components

--- presto-ui/src/components/PageTitle.jsx
+++ presto-ui/src/components/PageTitle.jsx
@@ -12,7 +12,7 @@
  * limitations under the License.
  */
 //@flow
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 type Props = {
     titles: string[],
@@ -30,7 +30,7 @@ type State = {
     errorText: ?string,
 }
 
-function ClusterResourceGroupNavBar({titles, urls, current = 0} : Props) {
+const ClusterResourceGroupNavBar = ({titles, urls, current = 0} : Props) => {
     const classNames = ['navbar-brand inactive', 'navbar-brand'];
     const navBarItems = titles.map( (title, index) => {
         const classNameIdx = (current === index || !urls?.length) ? 0 : 1;
@@ -42,157 +42,169 @@ function ClusterResourceGroupNavBar({titles, urls, current = 0} : Props) {
             </td>
         );
     });
+
     return (
         <>{navBarItems}</>
     );
-}
+};
 
-function isOffline() {
+const isOffline = () => {
     return window.location.protocol === 'file:';
-}
+};
 
-export class PageTitle extends React.Component<Props, State> {
-    timeoutId: TimeoutID;
+export const PageTitle = (props: Props) => {
+    const [state, setState] = useState<State>({
+        noConnection: false,
+        lightShown: false,
+        info: null,
+        lastSuccess: Date.now(),
+        modalShown: false,
+        errorText: null,
+    });
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            noConnection: false,
-            lightShown: false,
-            info: null,
-            lastSuccess: Date.now(),
-            modalShown: false,
-            errorText: null,
-        };
-    }
+    const timeoutId = useRef<number | null>(null);
 
-    refreshLoop: () => void = () => {
-        clearTimeout(this.timeoutId);
+    const refreshLoop = () => {
+        clearTimeout(timeoutId.current);
         fetch("/v1/info")
             .then(response => response.json())
             .then(info => {
-                this.setState({
+                setState(prevState => ({
+                    ...prevState,
                     info: info,
                     noConnection: false,
                     lastSuccess: Date.now(),
                     modalShown: false,
-                });
+                }));
                 //$FlowFixMe$ Bootstrap 5 plugin
                 $('#no-connection-modal').hide();
-                this.resetTimer();
+                timeoutId.current = setTimeout(refreshLoop, 1000);
             })
             .catch(error => {
-                this.setState({
-                    noConnection: true,
-                    lightShown: !this.state.lightShown,
-                    errorText: error
+                setState(prevState => {
+                    const noConnection = true;
+                    const lightShown = !prevState.lightShown;
+                    const errorText = error;
+                    const shouldShowModal = !prevState.modalShown && (error || (Date.now() - prevState.lastSuccess) > 30 * 1000);
+                    
+                    if (shouldShowModal) {
+                        //$FlowFixMe$ Bootstrap 5 plugin
+                        $('#no-connection-modal').modal('show');
+                    }
+
+                    timeoutId.current = setTimeout(refreshLoop, 1000);
+                    
+                    return {
+                        ...prevState,
+                        noConnection,
+                        lightShown,
+                        errorText,
+                        modalShown: shouldShowModal || prevState.modalShown
+                    };
                 });
-                this.resetTimer();
+            });
+    };
 
-                if (!this.state.modalShown && (error || (Date.now() - this.state.lastSuccess) > 30 * 1000)) {
-                    //$FlowFixMe$ Bootstrap 5 plugin
-                    $('#no-connection-modal').modal();
-                    this.setState({modalShown: true});
-                }
-        });
-    }
-
-    resetTimer() {
-        clearTimeout(this.timeoutId);
-        this.timeoutId = setTimeout(this.refreshLoop.bind(this), 1000);
-    }
-
-    componentDidMount() {
-        if ( isOffline() ) {
-            this.setState({
+    useEffect(() => {
+        if (isOffline()) {
+            setState(prevState => ({
+                ...prevState,
                 noConnection: true,
                 lightShown: true,
-            });
+            }));
+        } else {
+            refreshLoop();
         }
-        else {
-            this.refreshLoop();
-        }
-    }
 
-    renderStatusLight(): any {
-        if (this.state.noConnection) {
-            if (this.state.lightShown) {
+        return () => {
+            clearTimeout(timeoutId.current);
+        };
+    }, []);
+
+    useEffect(() => {
+        //$FlowFixMe$ Bootstrap 5 plugin
+        $('[data-bs-toggle="tooltip"]')?.tooltip?.();
+    });
+
+    const renderStatusLight = () => {
+        if (state.noConnection) {
+            if (state.lightShown) {
                 return <span className="status-light status-light-red" id="status-indicator"/>;
+            } else {
+                return <span className="status-light" id="status-indicator"/>;
             }
-            else {
-                return <span className="status-light" id="status-indicator"/>
-            }
+        } else {
+            return <span className="status-light status-light-green" id="status-indicator"/>;
         }
-        return <span className="status-light status-light-green" id="status-indicator"/>;
+    };
+
+    const info = state.info;
+    if (!isOffline() && !info) {
+        return null;
     }
 
-    render(): any {
-        const info = this.state.info;
-        if (!isOffline() && !info) {
-            return null;
-        }
-
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <table>
-                                <tbody>
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <table>
+                            <tbody>
                                 <tr>
                                     <td>
-                                        <a href="/ui/"><img src={`${this.props.path ? this.props.path : '.'}/assets/logo.png`}/></a>
+                                        <a href="/ui/"><img src={`${props.path ? props.path : '.'}/assets/logo.png`}/></a>
                                     </td>
-                                    <ClusterResourceGroupNavBar titles={this.props.titles} urls={this.props.urls} current={this.props.current} />
+                                    <ClusterResourceGroupNavBar titles={props.titles} urls={props.urls} current={props.current} />
                                 </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div id="navbar" className="navbar-collapse collapse">
-                            <ul className="nav navbar-nav navbar-right ms-auto">
-                                <li>
-                                    <span className="navbar-cluster-info">
-                                        <span className="uppercase">Version</span><br/>
-                                        <span className="text" id="version-number">{isOffline() ? 'N/A' : info?.nodeVersion?.version}</span>
-                                    </span>
-                                </li>
-                                <li>
-                                    <span className="navbar-cluster-info">
-                                        <span className="uppercase">Environment</span><br/>
-                                        <span className="text" id="environment">{isOffline() ? 'N/A' : info?.environment}</span>
-                                    </span>
-                                </li>
-                                <li>
-                                    <span className="navbar-cluster-info">
-                                        <span className="uppercase">Uptime</span><br/>
-                                        <span data-bs-toggle="tooltip" data-bs-placement="bottom" title="Connection status">
-                                        {this.renderStatusLight()}
-                                         </span>
-                                        &nbsp;
-                                        <span className="text" id="uptime">{isOffline() ? 'Offline' : info?.uptime}</span>
-                                    </span>
-                                </li>
-                            </ul>
-                        </div>
+                            </tbody>
+                        </table>
                     </div>
-                </nav>
-                <div id="no-connection-modal" className="modal" tabIndex="-1" role="dialog">
-                    <div className="modal-dialog modal-sm" role="document">
-                        <div className="modal-content">
-                            <div className="row error-message">
-                                <div className="col-12">
-                                    <br />
-                                    <h4>Unable to connect to server</h4>
-                                    <p>{this.state.errorText ? "Error: " + this.state.errorText : null}</p>
-                                </div>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div id="navbar" className="navbar-collapse collapse">
+                        <ul className="nav navbar-nav navbar-right ms-auto">
+                            <li>
+                                <span className="navbar-cluster-info">
+                                    <span className="uppercase">Version</span><br/>
+                                    <span className="text" id="version-number">{isOffline() ? 'N/A' : info?.nodeVersion?.version}</span>
+                                </span>
+                            </li>
+                            <li>
+                                <span className="navbar-cluster-info">
+                                    <span className="uppercase">Environment</span><br/>
+                                    <span className="text" id="environment">{isOffline() ? 'N/A' : info?.environment}</span>
+                                </span>
+                            </li>
+                            <li>
+                                <span className="navbar-cluster-info">
+                                    <span className="uppercase">Uptime</span><br/>
+                                    <span data-bs-toggle="tooltip" data-bs-placement="bottom" title="Connection status">
+                                        {renderStatusLight()}
+                                    </span>
+                                    &nbsp;
+                                    <span className="text" id="uptime">{isOffline() ? 'Offline' : info?.uptime}</span>
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div id="no-connection-modal" className="modal" tabIndex="-1" role="dialog">
+                <div className="modal-dialog modal-sm" role="document">
+                    <div className="modal-content">
+                        <div className="row error-message">
+                            <div className="col-12">
+                                <br />
+                                <h4>Unable to connect to server</h4>
+                                <p>{state.errorText ? "Error: " + state.errorText : null}</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default PageTitle;
