7b07b3ec6b596acbb16edec786c322babddc0d88
Ping Liu <lpingbj@cn.ibm.com>
2025-08-06 16:00:38 +0100

    Support insert data into iceberg table. Co-authored-by lpingbj@cn.ibm.com

--- presto-native-execution/presto_cpp/presto_protocol/connector/iceberg/IcebergConnectorProtocol.h
+++ presto-native-execution/presto_cpp/presto_protocol/connector/iceberg/IcebergConnectorProtocol.h
@@ -22,8 +22,15 @@ using IcebergConnectorProtocol = ConnectorProtocolTemplate<
     IcebergTableHandle,
     IcebergTableLayoutHandle,
     IcebergColumnHandle,
-    NotImplemented,
-    NotImplemented,
+
+#ifdef PRESTO_ENABLE_ICEBERG_NATIVE_INSERTION
+    IcebergInsertTableHandle,
+    IcebergOutputTableHandle,
+#else
+    // NotImplemented,
+    // NotImplemented,
+#endif
+
     IcebergSplit,
     NotImplemented,
     hive::HiveTransactionHandle,
