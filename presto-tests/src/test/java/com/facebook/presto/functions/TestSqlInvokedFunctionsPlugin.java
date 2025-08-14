25eb7ac7c05f5d9939d459da739b8ef22b5ff286
Pratik Joseph Dabre <pdabre12@gmail.com>
2025-08-14 11:38:19 -0700

    Add tests for getSqlInvokedFunctions() SPI
    Co-Authored-by: Naveen Mahadevuni <Naveen.mahadevuni@ibm.com>

---
+++ presto-tests/src/test/java/com/facebook/presto/functions/TestSqlInvokedFunctionsPlugin.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.functions;
+
+import com.facebook.presto.spi.function.Description;
+import com.facebook.presto.spi.function.SqlInvokedScalarFunction;
+import com.facebook.presto.spi.function.SqlParameter;
+import com.facebook.presto.spi.function.SqlParameters;
+import com.facebook.presto.spi.function.SqlType;
+
+public final class TestSqlInvokedFunctionsPlugin
+{
+    private TestSqlInvokedFunctionsPlugin()
+    {}
+
+    @SqlInvokedScalarFunction(value = "custom_square", deterministic = true, calledOnNullInput = false)
+    @Description("Custom SQL to test NULLIF in Functions")
+    @SqlParameters({@SqlParameter(name = "x", type = "integer"), @SqlParameter(name = "y", type = "integer")})
+    @SqlType("integer")
+    public static String customSquare()
+    {
+        return "RETURN IF(NULLIF(x, y) IS NOT NULL, x * x, y * y)";
+    }
+}
