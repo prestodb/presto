facebookincubator/velox

commit bb0be012df6deeeda7065db33819a22bd71f7816

feat: Support timestamp as hive PartitionId (#13494)
Summary:

https://fburl.com/scuba/presto_queries/c8e9g59t
We need to support queries failing for timestamp partition exec::VectorHasher::typeKindSupportsValueIds( inputType->childAt(channel)->kind()) Unsupported partition type: TIMESTAMP.

Differential Revision: D75156810

velox/exec/VectorHasher.h
@@ -293,6 +293,7 @@ class VectorHasher {
       case TypeKind::BIGINT:
       case TypeKind::VARCHAR:
       case TypeKind::VARBINARY:
+      case TypeKind::TIMESTAMP:
         return true;
       default:
         return false;
@@ -330,6 +331,10 @@ class VectorHasher {
   template <typename T>
   inline int64_t toInt64(T value) const {
     return value;
+  }
+
+  inline int64_t toInt64(Timestamp timestamp) const {
+    return timestamp.toMillis();
   }
 
   // Sets the data statistics from 'other'. Does not set the mapping mode.
@@ -662,8 +667,33 @@ inline uint64_t VectorHasher::lookupValueId(StringView value) const {
 }
 
 template <>
+inline uint64_t VectorHasher::lookupValueId(Timestamp timestamp) const {
+  auto int64Value = toInt64(timestamp);
+  if (isRange_) {
+    if (int64Value > max_ || int64Value < min_) {
+      return kUnmappable;
+    }
+    return int64Value - min_ + 1;
+  }
+  UniqueValue unique(timestamp.toMillis());
+  auto iter = uniqueValues_.find(unique);
+  if (iter != uniqueValues_.end()) {
+    return iter->id();
+  }
+  return kUnmappable;
+}
+
+template <>
 inline uint64_t VectorHasher::valueId(bool value) {
   return value ? 2 : 1;
+}
+
+template <>
+inline uint64_t VectorHasher::valueId(Timestamp value) {
+  if (value.getNanos() % 1'000'000 != 0) {
+    return kUnmappable;
+  }
+  return valueId(static_cast<int64_t>(value.toMillis()));
 }
 
 template <>

velox/connectors/hive/HivePartitionUtil.cpp
@@ -28,6 +28,7 @@ namespace facebook::velox::connector::hive {
       case TypeKind::BIGINT:                                                \
       case TypeKind::VARCHAR:                                               \
       case TypeKind::VARBINARY:                                             \
+      case TypeKind::TIMESTAMP:                                             \
         return VELOX_DYNAMIC_SCALAR_TYPE_DISPATCH(                          \
             TEMPLATE_FUNC, typeKind, __VA_ARGS__);                          \
       default:                                                              \
@@ -45,6 +46,15 @@ inline std::string makePartitionValueString(T value) {
 template <>
 inline std::string makePartitionValueString(bool value) {
   return value ? "true" : "false";
+}
+
+template <>
+inline std::string makePartitionValueString(Timestamp value) {
+  value.toTimezone(Timestamp::defaultTimezone());
+  TimestampToStringOptions options;
+  options.dateTimeSeparator = ' ';
+  options.precision = TimestampPrecision::kMilliseconds;
+  return value.toString(options);
 }
 
 template <TypeKind Kind>

velox/connectors/hive/tests/HivePartitionUtilTest.cpp
@@ -29,6 +29,10 @@ using namespace facebook::velox::dwio::catalog::fbhive;
 class HivePartitionUtilTest : public ::testing::Test,
                               public velox::test::VectorTestBase {
  protected:
+  static void SetUpTestCase() {
+    memory::MemoryManager::testingSetInstance(memory::MemoryManager::Options{});
+  }
+
   template <typename T>
   VectorPtr makeDictionary(const std::vector<T>& data) {
     auto base = makeFlatVector(data);
@@ -69,14 +73,17 @@ TEST_F(HivePartitionUtilTest, partitionName) {
          "flat_int_col",
          "flat_bigint_col",
          "dict_string_col",
-         "const_date_col"},
+         "const_date_col",
+         "flat_timestamp_col"},
         {makeFlatVector<bool>(std::vector<bool>{false}),
          makeFlatVector<int8_t>(std::vector<int8_t>{10}),
          makeFlatVector<int16_t>(std::vector<int16_t>{100}),
          makeFlatVector<int32_t>(std::vector<int32_t>{1000}),
          makeFlatVector<int64_t>(std::vector<int64_t>{10000}),
          makeDictionary<StringView>(std::vector<StringView>{"str1000"}),
-         makeConstant<int32_t>(10000, 1, DATE())});
+         makeConstant<int32_t>(10000, 1, DATE()),
+         makeFlatVector<Timestamp>(
+             std::vector<Timestamp>{Timestamp::fromMillis(1577836800000)})});
 
     std::vector<std::string> expectedPartitionKeyValues{
         "flat_bool_col=false",
@@ -85,7 +92,8 @@ TEST_F(HivePartitionUtilTest, partitionName) {
         "flat_int_col=1000",
         "flat_bigint_col=10000",
         "dict_string_col=str1000",
-        "const_date_col=1997-05-19"};
+        "const_date_col=1997-05-19",
+        "flat_timestamp_col=2019-12-31 16%3A00%3A00.000"};
 
     std::vector<column_index_t> partitionChannels;
     for (auto i = 1; i <= expectedPartitionKeyValues.size(); i++) {
@@ -131,7 +139,8 @@ TEST_F(HivePartitionUtilTest, partitionNameForNull) {
       "flat_int_col",
       "flat_bigint_col",
       "flat_string_col",
-      "const_date_col"};
+      "const_date_col",
+      "flat_timestamp_col"};
 
   RowVectorPtr input = makeRowVector(
       partitionColumnNames,
@@ -141,7 +150,8 @@ TEST_F(HivePartitionUtilTest, partitionNameForNull) {
        makeNullableFlatVector<int32_t>({std::nullopt}),
        makeNullableFlatVector<int64_t>({std::nullopt}),
        makeNullableFlatVector<StringView>({std::nullopt}),
-       makeConstant<int32_t>(std::nullopt, 1, DATE())});
+       makeConstant<int32_t>(std::nullopt, 1, DATE()),
+       makeNullableFlatVector<Timestamp>({std::nullopt})});
 
   for (auto i = 0; i < partitionColumnNames.size(); i++) {
     std::vector<column_index_t> partitionChannels = {(column_index_t)i};

velox/connectors/hive/tests/PartitionIdGeneratorTest.cpp
@@ -16,6 +16,7 @@
 
 #include "velox/connectors/hive/PartitionIdGenerator.h"
 #include "velox/common/base/tests/GTestUtils.h"
+#include "velox/type/TimestampConversion.h"
 #include "velox/vector/tests/utils/VectorTestBase.h"
 
 #include "gtest/gtest.h"
@@ -236,6 +237,78 @@ TEST_F(PartitionIdGeneratorTest, limitOfPartitionNumber) {
       fmt::format("Exceeded limit of {} distinct partitions.", maxPartitions));
 }
 
+TEST_F(PartitionIdGeneratorTest, timestampPartitionKeyComparasion) {
+  PartitionIdGenerator idGenerator(
+      ROW({"timestamp_col"}, {TIMESTAMP()}), {0}, 100, pool(), true);
+  auto timestampResult = util::fromTimestampString(
+      "2025-01-02 00:00:00", util::TimestampParseMode::kPrestoCast);
+  auto input = makeRowVector({
+      makeFlatVector<Timestamp>({timestampResult.value()}),
+  });
+  raw_vector<uint64_t> testTimeIds;
+  idGenerator.run(input, testTimeIds);
+  EXPECT_EQ(
+      idGenerator.partitionName(testTimeIds[0]),
+      "timestamp_col=2025-01-01 16%3A00%3A00.000");
+}
+
+TEST_F(PartitionIdGeneratorTest, timestampPartitionKey) {
+  PartitionIdGenerator idGenerator(ROW({TIMESTAMP()}), {0}, 100, pool(), true);
+
+  auto numPartitions = 50;
+  auto input = makeRowVector({
+      makeFlatVector<Timestamp>(
+          numPartitions,
+          [](auto row) {
+            return Timestamp::fromMillis(
+                1639426440000 + static_cast<int64_t>(row) * 100);
+          }),
+  });
+
+  raw_vector<uint64_t> firstTimeIds;
+  idGenerator.run(input, firstTimeIds);
+
+  std::unordered_set<uint64_t> distinctIds(
+      firstTimeIds.begin(), firstTimeIds.end());
+  EXPECT_EQ(distinctIds.size(), numPartitions);
+  EXPECT_EQ(*std::min_element(distinctIds.begin(), distinctIds.end()), 0);
+  EXPECT_EQ(
+      *std::max_element(distinctIds.begin(), distinctIds.end()),
+      numPartitions - 1);
+
+  raw_vector<uint64_t> secondTimeIds;
+  idGenerator.run(input, secondTimeIds);
+
+  for (auto i = 0; i < input->size(); ++i) {
+    EXPECT_EQ(firstTimeIds[i], secondTimeIds[i]) << "at " << i;
+  }
+
+  auto otherNumPartitions = 30;
+  auto otherInput = makeRowVector({
+      makeFlatVector<Timestamp>(
+          otherNumPartitions,
+          [](auto row) {
+            return Timestamp::fromMillis(
+                1639426440000 + static_cast<int64_t>(row) * 500);
+          }),
+  });
+
+  raw_vector<uint64_t> otherIds;
+  idGenerator.run(otherInput, otherIds);
+
+  std::unordered_set<uint64_t> otherDistinctIds(
+      otherIds.begin(), otherIds.end());
+  EXPECT_EQ(otherDistinctIds.size(), otherNumPartitions);
+
+  // Run the original input again and verify stable IDs
+  raw_vector<uint64_t> thirdTimeIds;
+  idGenerator.run(input, thirdTimeIds);
+
+  for (auto i = 0; i < input->size(); ++i) {
+    EXPECT_EQ(firstTimeIds[i], thirdTimeIds[i]) << "at " << i;
+  }
+}
+
 TEST_F(PartitionIdGeneratorTest, supportedPartitionKeyTypes) {
   // Test on supported key types.
   {
@@ -248,23 +321,27 @@ TEST_F(PartitionIdGeneratorTest, supportedPartitionKeyTypes) {
             SMALLINT(),
             INTEGER(),
             BIGINT(),
+            TIMESTAMP(),
         }),
-        {0, 1, 2, 3, 4, 5, 6},
+        {0, 1, 2, 3, 4, 5, 6, 7},
         100,
         pool(),
         true);
 
-    auto input = makeRowVector({
-        makeNullableFlatVector<StringView>(
-            {"Left", std::nullopt, "Right"}, VARCHAR()),
-        makeNullableFlatVector<bool>({true, false, std::nullopt}),
-        makeFlatVector<StringView>(
-            {"proton", "neutron", "electron"}, VARBINARY()),
-        makeNullableFlatVector<int8_t>({1, 2, std::nullopt}),
-        makeNullableFlatVector<int16_t>({1, 2, std::nullopt}),
-        makeNullableFlatVector<int32_t>({1, std::nullopt, 2}),
-        makeNullableFlatVector<int64_t>({std::nullopt, 1, 2}),
-    });
+    auto input = makeRowVector(
+        {makeNullableFlatVector<StringView>(
+             {"Left", std::nullopt, "Right"}, VARCHAR()),
+         makeNullableFlatVector<bool>({true, false, std::nullopt}),
+         makeFlatVector<StringView>(
+             {"proton", "neutron", "electron"}, VARBINARY()),
+         makeNullableFlatVector<int8_t>({1, 2, std::nullopt}),
+         makeNullableFlatVector<int16_t>({1, 2, std::nullopt}),
+         makeNullableFlatVector<int32_t>({1, std::nullopt, 2}),
+         makeNullableFlatVector<int64_t>({std::nullopt, 1, 2}),
+         makeNullableFlatVector<Timestamp>(
+             {std::nullopt,
+              Timestamp::fromMillis(1639426440001),
+              Timestamp::fromMillis(1639426440002)})});
 
     raw_vector<uint64_t> ids;
     idGenerator.run(input, ids);
@@ -279,7 +356,6 @@ TEST_F(PartitionIdGeneratorTest, supportedPartitionKeyTypes) {
     auto input = makeRowVector({
         makeConstant<float>(1.0, 1),
         makeConstant<double>(1.0, 1),
-        makeConstant<Timestamp>(Timestamp::fromMillis(1639426440000), 1),
         makeArrayVector<int32_t>({{1, 2, 3}}),
         makeMapVector<int16_t, int16_t>({{{1, 2}}}),
     });

velox/exec/VectorHasher.cpp
@@ -45,6 +45,9 @@ namespace facebook::velox::exec {
       case TypeKind::VARBINARY: {                                        \
         return TEMPLATE_FUNC<TypeKind::VARCHAR>(__VA_ARGS__);            \
       }                                                                  \
+      case TypeKind::TIMESTAMP: {                                        \
+        return TEMPLATE_FUNC<TypeKind::TIMESTAMP>(__VA_ARGS__);          \
+      }                                                                  \
       default:                                                           \
         VELOX_UNREACHABLE(                                               \
             "Unsupported value ID type: ", mapTypeKindToName(typeKind)); \
@@ -736,6 +739,7 @@ void extendRange(
     case TypeKind::BIGINT:
     case TypeKind::VARCHAR:
     case TypeKind::VARBINARY:
+    case TypeKind::TIMESTAMP:
       extendRange<int64_t>(reserve, min, max);
       break;
 

prestodb/presto

commit 2e4a0835c9147651db68e90c45955523f092a93b

fix: Fix crash by stack overflow when grouping by nano-timestamp key (#14383)
Summary:
Fixes https://github.com/facebookincubator/velox/issues/14413.

Caused by https://github.com/facebookincubator/velox/pull/13494.

Pull Request resolved: https://github.com/facebookincubator/velox/pull/14383

Reviewed By: kKPulla

Differential Revision: D80256380

Pulled By: mbasmanova

fbshipit-source-id: 37763d5bb050e840d70b0669eb80bb0ed9b04acb

velox/exec/VectorHasher.h
@@ -677,11 +677,17 @@ template <>
 inline uint64_t VectorHasher::valueId(bool value) {
   return value ? 2 : 1;
 }
-
 template <>
 inline uint64_t VectorHasher::valueId(Timestamp value) {
-  return value.getNanos() % 1'000'000 != 0 ? kUnmappable
-                                           : valueId(value.toMillis());
+  if (FOLLY_UNLIKELY(
+          value.getNanos() % Timestamp::kNanosecondsInMillisecond != 0)) {
+    // The timestamp is in nanosecond or microsecond precision. The values are
+    // not mappable to milliseconds without precision loss.
+    setRangeOverflow();
+    setDistinctOverflow();
+    return kUnmappable;
+  }
+  return valueId(value.toMillis());
 }
 
 template <>

velox/exec/tests/AggregationTest.cpp
@@ -384,14 +384,15 @@ class AggregationTest : public OperatorTestBase {
   }
 
   RowTypePtr rowType_{
-      ROW({"c0", "c1", "c2", "c3", "c4", "c5", "c6"},
+      ROW({"c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7"},
           {BIGINT(),
            SMALLINT(),
            INTEGER(),
            BIGINT(),
            REAL(),
            DOUBLE(),
-           VARCHAR()})};
+           VARCHAR(),
+           TIMESTAMP()})};
   folly::Random::DefaultGenerator rng_;
   memory::MemoryReclaimer::Stats reclaimerStats_;
   VectorFuzzer::Options fuzzerOpts_{
@@ -399,6 +400,8 @@ class AggregationTest : public OperatorTestBase {
       .nullRatio = 0,
       .stringLength = 1024,
       .stringVariableLength = false,
+      .timestampPrecision =
+          VectorFuzzer::Options::TimestampPrecision::kMicroSeconds,
       .allowLazyVector = false};
 };
 
@@ -709,6 +712,20 @@ TEST_F(AggregationTest, singleStringKeyDistinct) {
   createDuckDbTable(vectors);
   testSingleKey<StringView>(vectors, "c6", false, true);
   testSingleKey<StringView>(vectors, "c6", true, true);
+}
+
+TEST_F(AggregationTest, singleTimestampKey) {
+  auto vectors = createVectors(100, rowType_, fuzzerOpts_);
+  createDuckDbTable(vectors);
+  testSingleKey<StringView>(vectors, "c7", false, false);
+  testSingleKey<StringView>(vectors, "c7", true, false);
+}
+
+TEST_F(AggregationTest, singleTimestampKeyDistinct) {
+  auto vectors = createVectors(100, rowType_, fuzzerOpts_);
+  createDuckDbTable(vectors);
+  testSingleKey<StringView>(vectors, "c7", false, true);
+  testSingleKey<StringView>(vectors, "c7", true, true);
 }
 
 TEST_F(AggregationTest, multiKey) {