1c50a19d1323a6b53f9b7792e84cb31716772a9c
Chris Matzenbach <cmatzenbach@nvidia.com>
2025-08-25 17:01:48 -0500

    Fixed polling bugs; fixed code highlighting bug

--- presto-ui/src/components/QueryDetail.jsx
+++ presto-ui/src/components/QueryDetail.jsx
@@ -1104,6 +1104,10 @@ const QueryDetail = () => {
     }
 
     const updateCharts = () => {
+        // don't attempt to highlight or throttle before query content is rendered
+        if (state.query === null) {
+            return;
+        }
         // prevent multiple calls to componentDidUpdate (resulting from calls to setState or otherwise) within the refresh interval from re-rendering sparklines/charts
         if (state.lastRender === null || (Date.now() - state.lastRender) >= 1000) {
             const renderTimestamp = Date.now();
@@ -1140,11 +1144,7 @@ const QueryDetail = () => {
         // if (window.ClipboardJS) { new window.ClipboardJS('.copy-button'); }
 
     useEffect(() => {
-        if (!timerIdRef.current) {
-            timerIdRef.current = setTimeout(fetchData, 1000);
-        } else {
-            updateCharts();
-        }
+        fetchData();
 
         return () => {
             clearTimeout(timerIdRef.current);
