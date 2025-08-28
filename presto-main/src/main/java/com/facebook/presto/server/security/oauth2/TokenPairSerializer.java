1e03eea172dd1d7df5b5e73f6a01cb56c1925f81
auden-woolfson <auden.woolfson@ibm.com>
2025-08-28 15:54:01 -0700

    Remove unnecessary deps

--- presto-main/src/main/java/com/facebook/presto/server/security/oauth2/TokenPairSerializer.java
+++ presto-main/src/main/java/com/facebook/presto/server/security/oauth2/TokenPairSerializer.java
@@ -15,8 +15,7 @@
 package com.facebook.presto.server.security.oauth2;
 
 import com.facebook.presto.server.security.oauth2.OAuth2Client.Response;
-
-import javax.annotation.Nullable;
+import jakarta.annotation.Nullable;
 
 import java.util.Date;
 import java.util.Optional;
