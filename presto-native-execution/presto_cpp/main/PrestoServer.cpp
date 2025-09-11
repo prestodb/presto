601e73fcf76c1973fe75879955b66b0fe4d47503
Xin Zhang <desertsxin@gmail.com>
2025-09-11 22:03:01 +0100

    feat(native): Add TextReader

--- presto-native-execution/presto_cpp/main/PrestoServer.cpp
+++ presto-native-execution/presto_cpp/main/PrestoServer.cpp
@@ -62,6 +62,7 @@
 #include "velox/dwio/orc/reader/OrcReader.h"
 #include "velox/dwio/parquet/RegisterParquetReader.h"
 #include "velox/dwio/parquet/RegisterParquetWriter.h"
+#include "velox/dwio/text/RegisterTextReader.h"
 #include "velox/dwio/text/RegisterTextWriter.h"
 #include "velox/exec/OutputBufferManager.h"
 #include "velox/exec/TraceUtil.h"
@@ -1452,6 +1453,9 @@ void PrestoServer::registerFileReadersAndWriters() {
   if (SystemConfig::instance()->textWriterEnabled()) {
     velox::text::registerTextWriterFactory();
   }
+  if (SystemConfig::instance()->textReaderEnabled()) {
+    velox::text::registerTextReaderFactory();
+  }
 }
 
 void PrestoServer::unregisterFileReadersAndWriters() {
@@ -1461,6 +1465,9 @@ void PrestoServer::unregisterFileReadersAndWriters() {
   velox::parquet::unregisterParquetWriterFactory();
   if (SystemConfig::instance()->textWriterEnabled()) {
     velox::text::unregisterTextWriterFactory();
+  }
+  if (SystemConfig::instance()->textReaderEnabled()) {
+    velox::text::unregisterTextReaderFactory();
   }
 }
 
