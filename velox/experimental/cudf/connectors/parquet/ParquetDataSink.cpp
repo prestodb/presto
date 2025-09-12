07a4e287e88dca9ae918c8d9562c506a895b3b20
Rui Mo <rui@apache.org>
2025-09-12 16:24:10 -0700

    fix(cuDF): Compilation failure (#14799)
    Summary:
    Fixes the below compilation error.
    
    ```
    velox/experimental/cudf/connectors/parquet/ParquetDataSink.cpp:145:7: error: 'commitStrategyToString' was not declared in this scope; did you mean 'commitStrategy_'?
      145 |       commitStrategyToString(commitStrategy_));
          |       ^~~~~~~~~~~~~~~~~~~~~~
    ```
    
    Follow-up for https://github.com/facebookincubator/velox/commit/99fe06adaf59aae3828c401070c5b13bfac25b5a.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/14799
    
    Reviewed By: kagamiori
    
    Differential Revision: D82029874
    
    Pulled By: Yuhta
    
    fbshipit-source-id: ea537a07e8d2625ebc0b5fddea2dea8b0b098de8

--- velox/experimental/cudf/connectors/parquet/ParquetDataSink.cpp
+++ velox/experimental/cudf/connectors/parquet/ParquetDataSink.cpp
@@ -142,7 +142,7 @@ ParquetDataSink::ParquetDataSink(
       (commitStrategy_ == CommitStrategy::kNoCommit) ||
           (commitStrategy_ == CommitStrategy::kTaskCommit),
       "Unsupported commit strategy: {}",
-      commitStrategyToString(commitStrategy_));
+      CommitStrategyName::toName(commitStrategy_));
 
   const auto& writerOptions = dynamic_cast<ParquetWriterOptions*>(
       insertTableHandle_->writerOptions().get());
