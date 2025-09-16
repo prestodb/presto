0726c362c8a4129cae2c2a1b2097a5df9dc0c8b8
Zac Wen <zacw@meta.com>
2025-09-16 10:18:58 -0700

    Add ThriftFilterPushdown for native execution
    Index join doesn't support FilterNode in native execution. This change adds a connector optimizer to push down the FilterNode into table layout, which can be evaluated as remaining filter.

--- presto-thrift-connector/src/main/java/com/facebook/presto/connector/thrift/ThriftTableLayoutHandle.java
+++ presto-thrift-connector/src/main/java/com/facebook/presto/connector/thrift/ThriftTableLayoutHandle.java
@@ -16,6 +16,7 @@ package com.facebook.presto.connector.thrift;
 import com.facebook.presto.common.predicate.TupleDomain;
 import com.facebook.presto.spi.ColumnHandle;
 import com.facebook.presto.spi.ConnectorTableLayoutHandle;
+import com.facebook.presto.spi.relation.RowExpression;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableSet;
@@ -24,6 +25,8 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
+import static com.facebook.presto.expressions.LogicalRowExpressions.TRUE_CONSTANT;
+import static com.google.common.base.MoreObjects.toStringHelper;
 import static java.util.Objects.requireNonNull;
 
 public class ThriftTableLayoutHandle
@@ -33,18 +36,21 @@ public class ThriftTableLayoutHandle
     private final String tableName;
     private final Optional<Set<ColumnHandle>> columns;
     private final TupleDomain<ColumnHandle> constraint;
+    private final RowExpression remainingPredicate;
 
     @JsonCreator
     public ThriftTableLayoutHandle(
             @JsonProperty("schemaName") String schemaName,
             @JsonProperty("tableName") String tableName,
             @JsonProperty("columns") Optional<Set<ColumnHandle>> columns,
-            @JsonProperty("constraint") TupleDomain<ColumnHandle> constraint)
+            @JsonProperty("constraint") TupleDomain<ColumnHandle> constraint,
+            @JsonProperty("remainingPredicate") RowExpression remainingPredicate)
     {
         this.schemaName = requireNonNull(schemaName, "schemaName is null");
         this.tableName = requireNonNull(tableName, "tableName is null");
         this.columns = requireNonNull(columns, "columns is null").map(ImmutableSet::copyOf);
         this.constraint = requireNonNull(constraint, "constraint is null");
+        this.remainingPredicate = requireNonNull(remainingPredicate, "remainingPredicate is null");
     }
 
     @JsonProperty
@@ -71,6 +77,12 @@ public class ThriftTableLayoutHandle
         return constraint;
     }
 
+    @JsonProperty
+    public RowExpression getRemainingPredicate()
+    {
+        return remainingPredicate;
+    }
+
     @Override
     public boolean equals(Object o)
     {
@@ -84,18 +96,23 @@ public class ThriftTableLayoutHandle
         return schemaName.equals(other.schemaName)
                 && tableName.equals(other.tableName)
                 && columns.equals(other.columns)
-                && constraint.equals(other.constraint);
+                && constraint.equals(other.constraint)
+                && remainingPredicate.equals(other.remainingPredicate);
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hash(schemaName, tableName, columns, constraint);
+        return Objects.hash(schemaName, tableName, columns, constraint, remainingPredicate);
     }
 
     @Override
     public String toString()
     {
-        return schemaName + ":" + tableName;
+        return toStringHelper(this)
+                .add("schemaName", schemaName)
+                .add("tableName", tableName)
+                .add("remainingPredicate", TRUE_CONSTANT.equals(remainingPredicate) ? null : remainingPredicate.toString())
+                .toString();
     }
 }
