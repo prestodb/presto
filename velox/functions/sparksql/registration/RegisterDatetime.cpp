6ca62726653d170e3ecce86f73526b70ce6bbd66
Zhen Li <zhli@microsoft.com>
2025-09-19 18:15:20 -0700

    misc: Remove doy alias for Spark dayofyear function (#14898)
    Summary:
    In Spark, `dayofyear` function does not have a `doy` alias, so remove it.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/14898
    
    Reviewed By: shrinidhijoshi
    
    Differential Revision: D82830551
    
    Pulled By: xiaoxmeng
    
    fbshipit-source-id: 072f7f18bf72bc4b70ec5a71d1c25fe4533d2b22

--- velox/functions/sparksql/registration/RegisterDatetime.cpp
+++ velox/functions/sparksql/registration/RegisterDatetime.cpp
@@ -65,8 +65,7 @@ void registerDatetimeFunctions(const std::string& prefix) {
   registerFunction<DateSubFunction, Date, Date, int32_t>({prefix + "date_sub"});
   registerFunction<DayFunction, int32_t, Date>(
       {prefix + "day", prefix + "dayofmonth"});
-  registerFunction<DayOfYearFunction, int32_t, Date>(
-      {prefix + "doy", prefix + "dayofyear"});
+  registerFunction<DayOfYearFunction, int32_t, Date>({prefix + "dayofyear"});
   registerFunction<DayOfWeekFunction, int32_t, Date>({prefix + "dayofweek"});
   registerFunction<WeekdayFunction, int32_t, Date>({prefix + "weekday"});
   registerFunction<QuarterFunction, int32_t, Date>({prefix + "quarter"});
