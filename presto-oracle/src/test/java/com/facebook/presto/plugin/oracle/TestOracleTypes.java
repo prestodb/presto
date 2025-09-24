55cf5621871a0fa191d9d0f942909d6b91f6deb5
Sumi Mathew <sumimathew90@gmail.com>
2025-09-24 14:13:11 +0530

    Upgrade org.testcontainers:oracle-xe versio to 1.21.3

--- presto-oracle/src/test/java/com/facebook/presto/plugin/oracle/TestOracleTypes.java
+++ presto-oracle/src/test/java/com/facebook/presto/plugin/oracle/TestOracleTypes.java
@@ -42,7 +42,7 @@ public class TestOracleTypes
     private final OracleServerTester oracleServer;
     private final QueryRunner queryRunner;
 
-    private TestOracleTypes()
+    public TestOracleTypes()
             throws Exception
     {
         this.oracleServer = new OracleServerTester();
