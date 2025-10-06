987118e01ee4beb26a52222e45fae4e60e1874a2
wangd <mingwbd@gmail.com>
2025-10-07 01:08:12 +0800

    Support and handle call distributed procedure statement in preparer

--- presto-analyzer/src/test/java/com/facebook/presto/sql/analyzer/TestBuiltInQueryPreparer.java
+++ presto-analyzer/src/test/java/com/facebook/presto/sql/analyzer/TestBuiltInQueryPreparer.java
@@ -13,47 +13,108 @@
  */
 package com.facebook.presto.sql.analyzer;
 
+import com.facebook.presto.common.resourceGroups.QueryType;
+import com.facebook.presto.spi.ConnectorId;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.analyzer.AnalyzerOptions;
+import com.facebook.presto.spi.procedure.DistributedProcedure;
+import com.facebook.presto.spi.procedure.IProcedureRegistry;
+import com.facebook.presto.spi.procedure.Procedure;
+import com.facebook.presto.spi.procedure.TestProcedureRegistry;
 import com.facebook.presto.sql.analyzer.BuiltInQueryPreparer.BuiltInPreparedQuery;
 import com.facebook.presto.sql.parser.SqlParser;
 import com.facebook.presto.sql.tree.AllColumns;
+import com.facebook.presto.sql.tree.Call;
+import com.facebook.presto.sql.tree.CallArgument;
 import com.facebook.presto.sql.tree.QualifiedName;
+import com.facebook.presto.sql.tree.StringLiteral;
 import com.google.common.collect.ImmutableMap;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import static com.facebook.presto.common.type.StandardTypes.VARCHAR;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_FOUND;
+import static com.facebook.presto.spi.procedure.DistributedProcedure.SCHEMA;
+import static com.facebook.presto.spi.procedure.DistributedProcedure.TABLE_NAME;
 import static com.facebook.presto.sql.QueryUtil.selectList;
 import static com.facebook.presto.sql.QueryUtil.simpleQuery;
 import static com.facebook.presto.sql.QueryUtil.table;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.INVALID_PARAMETER_USAGE;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
 public class TestBuiltInQueryPreparer
 {
     private static final SqlParser SQL_PARSER = new SqlParser();
-    private static final BuiltInQueryPreparer QUERY_PREPARER = new BuiltInQueryPreparer(SQL_PARSER);
     private static final Map<String, String> emptyPreparedStatements = ImmutableMap.of();
     private static final AnalyzerOptions testAnalyzerOptions = AnalyzerOptions.builder().build();
+    private static IProcedureRegistry procedureRegistry;
+    private static BuiltInQueryPreparer queryPreparer;
+
+    @BeforeClass
+    public void setup()
+    {
+        procedureRegistry = new TestProcedureRegistry();
+        List<Procedure.Argument> arguments = new ArrayList<>();
+        arguments.add(new Procedure.Argument(SCHEMA, VARCHAR));
+        arguments.add(new Procedure.Argument(TABLE_NAME, VARCHAR));
+
+        List<Procedure> procedures = new ArrayList<>();
+        procedures.add(new Procedure("system", "fun", arguments));
+        procedures.add(new DistributedProcedure("system", "distributed_fun",
+                arguments,
+                (session, transactionContext, procedureHandle, fragments) -> null,
+                (transactionContext, procedureHandle, fragments) -> {}));
+        procedureRegistry.addProcedures(new ConnectorId("test"), procedures);
+        queryPreparer = new BuiltInQueryPreparer(SQL_PARSER, procedureRegistry);
+    }
 
     @Test
     public void testSelectStatement()
     {
-        BuiltInPreparedQuery preparedQuery = QUERY_PREPARER.prepareQuery(testAnalyzerOptions, "SELECT * FROM foo", emptyPreparedStatements, WarningCollector.NOOP);
+        BuiltInPreparedQuery preparedQuery = queryPreparer.prepareQuery(testAnalyzerOptions, "SELECT * FROM foo", emptyPreparedStatements, WarningCollector.NOOP);
         assertEquals(preparedQuery.getStatement(),
                 simpleQuery(selectList(new AllColumns()), table(QualifiedName.of("foo"))));
+    }
+
+    @Test
+    public void testCallProcedureStatement()
+    {
+        BuiltInPreparedQuery preparedQuery = queryPreparer.prepareQuery(testAnalyzerOptions, "call test.system.fun('a', 'b')", emptyPreparedStatements, WarningCollector.NOOP);
+        List<CallArgument> arguments = new ArrayList<>();
+        arguments.add(new CallArgument(new StringLiteral("a")));
+        arguments.add(new CallArgument(new StringLiteral("b")));
+        assertEquals(preparedQuery.getStatement(),
+                new Call(QualifiedName.of("test", "system", "fun"), arguments));
+        assertTrue(preparedQuery.getQueryType().isPresent());
+        assertEquals(preparedQuery.getQueryType().get(), QueryType.DATA_DEFINITION);
+    }
+
+    @Test
+    public void testCallDistributedProcedureStatement()
+    {
+        BuiltInPreparedQuery preparedQuery = queryPreparer.prepareQuery(testAnalyzerOptions, "call test.system.distributed_fun('a', 'b')", emptyPreparedStatements, WarningCollector.NOOP);
+        List<CallArgument> arguments = new ArrayList<>();
+        arguments.add(new CallArgument(new StringLiteral("a")));
+        arguments.add(new CallArgument(new StringLiteral("b")));
+        assertEquals(preparedQuery.getStatement(),
+                new Call(QualifiedName.of("test", "system", "distributed_fun"), arguments));
+        assertTrue(preparedQuery.getQueryType().isPresent());
+        assertEquals(preparedQuery.getQueryType().get(), QueryType.CALL_DISTRIBUTED_PROCEDURE);
     }
 
     @Test
     public void testExecuteStatement()
     {
         Map<String, String> preparedStatements = ImmutableMap.of("my_query", "SELECT * FROM foo");
-        BuiltInPreparedQuery preparedQuery = QUERY_PREPARER.prepareQuery(testAnalyzerOptions, "EXECUTE my_query", preparedStatements, WarningCollector.NOOP);
+        BuiltInPreparedQuery preparedQuery = queryPreparer.prepareQuery(testAnalyzerOptions, "EXECUTE my_query", preparedStatements, WarningCollector.NOOP);
         assertEquals(preparedQuery.getStatement(),
                 simpleQuery(selectList(new AllColumns()), table(QualifiedName.of("foo"))));
     }
@@ -62,7 +123,7 @@ public class TestBuiltInQueryPreparer
     public void testExecuteStatementDoesNotExist()
     {
         try {
-            QUERY_PREPARER.prepareQuery(testAnalyzerOptions, "execute my_query", emptyPreparedStatements, WarningCollector.NOOP);
+            queryPreparer.prepareQuery(testAnalyzerOptions, "execute my_query", emptyPreparedStatements, WarningCollector.NOOP);
             fail("expected exception");
         }
         catch (PrestoException e) {
@@ -75,7 +136,7 @@ public class TestBuiltInQueryPreparer
     {
         try {
             Map<String, String> preparedStatements = ImmutableMap.of("my_query", "SELECT * FROM foo where col1 = ?");
-            QUERY_PREPARER.prepareQuery(testAnalyzerOptions, "EXECUTE my_query USING 1,2", preparedStatements, WarningCollector.NOOP);
+            queryPreparer.prepareQuery(testAnalyzerOptions, "EXECUTE my_query USING 1,2", preparedStatements, WarningCollector.NOOP);
             fail("expected exception");
         }
         catch (SemanticException e) {
@@ -88,7 +149,7 @@ public class TestBuiltInQueryPreparer
     {
         try {
             Map<String, String> preparedStatements = ImmutableMap.of("my_query", "SELECT ? FROM foo where col1 = ?");
-            QUERY_PREPARER.prepareQuery(testAnalyzerOptions, "EXECUTE my_query USING 1", preparedStatements, WarningCollector.NOOP);
+            queryPreparer.prepareQuery(testAnalyzerOptions, "EXECUTE my_query USING 1", preparedStatements, WarningCollector.NOOP);
             fail("expected exception");
         }
         catch (SemanticException e) {
@@ -100,7 +161,7 @@ public class TestBuiltInQueryPreparer
     public void testFormattedQuery()
     {
         AnalyzerOptions analyzerOptions = AnalyzerOptions.builder().setLogFormattedQueryEnabled(true).build();
-        BuiltInPreparedQuery preparedQuery = QUERY_PREPARER.prepareQuery(
+        BuiltInPreparedQuery preparedQuery = queryPreparer.prepareQuery(
                 analyzerOptions,
                 "PREPARE test FROM SELECT * FROM foo where col1 = ?",
                 emptyPreparedStatements,
@@ -112,7 +173,7 @@ public class TestBuiltInQueryPreparer
                 "     foo\n" +
                 "   WHERE (col1 = ?)\n"));
 
-        preparedQuery = QUERY_PREPARER.prepareQuery(
+        preparedQuery = queryPreparer.prepareQuery(
                 analyzerOptions,
                 "PREPARE test FROM SELECT * FROM foo",
                 emptyPreparedStatements,
