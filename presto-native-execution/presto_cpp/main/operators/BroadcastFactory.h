fbf6d22e299754cb777fb029dee9b3b8f8601c57
Jialiang Tan <jtan6@meta.com>
2025-10-10 20:18:47 -0700

    [sv] Make broadcast reader/writer buffered for memory/latency efficiency (#26041)
    Summary:
    
    Make broadcast writes buffered, to avoid too many calls to file flush. Leverage SpillWriter to achieve this functionality.
    This makes broadcast writes __**5x more efficient**__
    
    before: 20250911_071257_00000_9spc3 
    after: 20250915_171512_00000_eabhd
    
    Reviewed By: xiaoxmeng, singcha
    
    Differential Revision: D82384431

--- presto-native-execution/presto_cpp/main/operators/BroadcastFactory.h
+++ presto-native-execution/presto_cpp/main/operators/BroadcastFactory.h
@@ -13,19 +13,20 @@
  */
 #pragma once
 
+#include "velox/common/file/FileInputStream.h"
 #include "velox/common/file/FileSystems.h"
 #include "velox/common/memory/MemoryPool.h"
+#include "velox/serializers/SerializedPageFile.h"
 #include "velox/vector/ComplexVector.h"
-#include "velox/vector/VectorStream.h"
 
 namespace facebook::presto::operators {
 
-/// Struct for single broadcast file info.]
-// This struct is a 1:1 strict API mapping to
-// presto-spark-base/src/main/java/com/facebook/presto/spark/execution/BroadcastFileInfo.java
-// Please refrain from making changes to this API class. If any changes have to
-// be made to this struct, one should make sure to make corresponding changes in
-// the above Java classes and its corresponding serde functionalities.
+/// Struct for single broadcast file info.
+/// This struct is a 1:1 strict API mapping to
+/// presto-spark-base/src/main/java/com/facebook/presto/spark/execution/BroadcastFileInfo.java
+/// Please refrain from making changes to this API class. If any changes have to
+/// be made to this struct, one should make sure to make corresponding changes
+/// in the above Java classes and its corresponding serde functionalities.
 struct BroadcastFileInfo {
   std::string filePath_;
   // TODO: Add additional stats including checksum, num rows, size.
@@ -34,19 +35,20 @@ struct BroadcastFileInfo {
       const std::string& info);
 };
 
-/// Writes broadcast data to a file.
-class BroadcastFileWriter {
+/// Writes broadcast data to a single file.
+class BroadcastFileWriter : velox::serializer::SerializedPageFileWriter {
  public:
   BroadcastFileWriter(
-      std::string_view filename,
-      const velox::RowTypePtr& inputType,
-      std::shared_ptr<velox::filesystems::FileSystem> fileSystem,
+      const std::string& pathPrefix,
+      uint64_t writeBufferSize,
+      std::unique_ptr<velox::VectorSerde::Options> serdeOptions,
       velox::memory::MemoryPool* pool);
 
   virtual ~BroadcastFileWriter() = default;
 
-  /// Write to file.
-  void collect(const velox::RowVectorPtr& input);
+  /// Serializes input rowVector using PrestoVectorSerde and
+  /// writes serialized data to file using buffered writes.
+  void write(const velox::RowVectorPtr& rowVector);
 
   /// Flush the data.
   void noMoreData();
@@ -56,21 +58,21 @@ class BroadcastFileWriter {
   velox::RowVectorPtr fileStats();
 
  private:
-  /// Initializes write file.
-  void initializeWriteFile();
+  uint64_t flush() override;
 
-  /// Serializes input rowVector using PrestoVectorSerde and
-  /// writes serialized data to file.
-  void write(const velox::RowVectorPtr& rowVector);
+  void closeFile() override;
 
-  std::unique_ptr<velox::WriteFile> writeFile_;
-  std::shared_ptr<velox::filesystems::FileSystem> fileSystem_;
-  std::string filename_;
-  int64_t numRows_;
-  int64_t maxSerializedSize_;
-  velox::memory::MemoryPool* pool_;
-  std::unique_ptr<velox::VectorSerde> serde_;
-  const velox::RowTypePtr& inputType_;
+  // Writes a footer at the end of the file containing metadata about all pages.
+  // The reader needs to know all page sizes ahead of time for exchange client's
+  // memory planning.
+  //
+  // Footer structure using thrift serialization:
+  // [serialized-thrift-footer][footer_size(8)]
+  void writeFooter();
+
+  int64_t numRows_{0};
+  std::vector<int64_t> pageSizes_;
+  velox::RowVectorPtr fileStats_{nullptr};
 };
 
 /// Reads broadcast data back from files.
@@ -86,30 +88,40 @@ class BroadcastFileReader {
   /// Return true if more data is available.
   bool hasNext();
 
-  /// Read next block of data.
+  /// Read next page of data. Returns nullptr when no more pages.
   velox::BufferPtr next();
 
-  /// Reader stats - number of bytes.
+  /// Reader stats - number of bytes and pages.
   folly::F14FastMap<std::string, int64_t> stats();
 
+  /// Get page sizes for pages that haven't been read yet.
+  std::vector<int64_t> remainingPageSizes() const;
+
  private:
-  std::unique_ptr<BroadcastFileInfo> broadcastFileInfo_;
-  std::shared_ptr<velox::filesystems::FileSystem> fileSystem_;
-  bool hasData_;
-  int64_t numBytes_;
-  velox::memory::MemoryPool* pool_;
+  // Read the footer to get all page sizes
+  void readFooter(velox::ReadFile* readFile);
+
+  velox::memory::MemoryPool* const pool_;
+  const std::unique_ptr<BroadcastFileInfo> broadcastFileInfo_;
+
+  std::unique_ptr<velox::common::FileInputStream> inputStream_;
+  int64_t numBytes_{0};
+  uint32_t numPagesRead_{0};
+  std::vector<int64_t> pageSizes_;
 };
 
 /// Factory to create Writers & Reader for file based broadcast.
 class BroadcastFactory {
  public:
+  /// Create FileBroadcast to write files under specified basePath.
   BroadcastFactory(const std::string& basePath);
 
   virtual ~BroadcastFactory() = default;
 
   std::unique_ptr<BroadcastFileWriter> createWriter(
+      uint64_t writeBufferSize,
       velox::memory::MemoryPool* pool,
-      const velox::RowTypePtr& inputType);
+      std::unique_ptr<velox::VectorSerde::Options> serdeOptions);
 
   std::shared_ptr<BroadcastFileReader> createReader(
       const std::unique_ptr<BroadcastFileInfo> fileInfo,
