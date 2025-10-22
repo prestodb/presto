3f6da5228b67d83d6cefe29c6927a6cca3c2f285
Christian Zentgraf <kitgocz@gmail.com>
2025-10-22 11:32:22 -0700

    build(native): Refactor setup scripts
    A recent change in Velox changed what the current
    directory is after github_checkout. This breaks
    building the PrestoC++ dependencies.
    
    The refactor removes github cloning and instead
    pulls released tar.gz images of the source code.
    This also increases the download speed.

--- presto-native-execution/scripts/setup-macos.sh
+++ presto-native-execution/scripts/setup-macos.sh
@@ -26,30 +26,20 @@ source "$(dirname "${BASH_SOURCE}")/../velox/scripts/setup-macos.sh"
 GPERF_VERSION="3.1"
 
 function install_proxygen {
-  github_checkout facebook/proxygen "${FB_OS_VERSION}" --depth 1
+  wget_and_untar https://github.com/facebook/proxygen/archive/refs/tags/${FB_OS_VERSION}.tar.gz proxygen
   # Folly Portability.h being used to decide whether or not support coroutines
   # causes issues (build, lin) if the selection is not consistent across users of folly.
   EXTRA_PKG_CXXFLAGS=" -DFOLLY_CFG_NO_COROUTINES"
-  cmake_install -DBUILD_TESTS=OFF
+  cmake_install_dir proxygen -DBUILD_TESTS=OFF
 }
 
 function install_gperf {
-  mkdir -p "${DEPENDENCY_DIR}"
-  cd "${DEPENDENCY_DIR}"
-  if [ -d gperf ]; then
-      if prompt "gperf already exists. Delete?"; then
-        ${SUDO} rm -rf gperf
-      else
-        return
-      fi
-  fi
-  (curl ${CURL_OPTIONS:+${CURL_OPTIONS}} -L https://ftp.gnu.org/pub/gnu/gperf/gperf-${GPERF_VERSION}.tar.gz -o gperf-${GPERF_VERSION}.tar.gz ||
-   curl ${CURL_OPTIONS:+${CURL_OPTIONS}} -L https://mirrors.ocf.berkeley.edu/gnu/gperf/gperf-${GPERF_VERSION}.tar.gz -o gperf-${GPERF_VERSION}.tar.gz) &&
-  mkdir -p gperf &&
-  tar -xz --strip-components=1 --no-same-owner -f gperf-${GPERF_VERSION}.tar.gz -C gperf &&
-  cd gperf &&
-  ./configure --prefix=${INSTALL_PREFIX} &&
-  make install
+  wget_and_untar https://mirrors.ocf.berkeley.edu/gnu/gperf/gperf-${GPERF_VERSION}.tar.gz gperf
+  (
+    cd ${DEPENDENCY_DIR}/gperf || exit &&
+    ./configure --prefix=${INSTALL_PREFIX} &&
+    make install
+  )
 }
 
 function install_presto_deps {
