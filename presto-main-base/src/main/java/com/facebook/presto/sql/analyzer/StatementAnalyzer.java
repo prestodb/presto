4ef4152a3c4829efadb1b26ce4414c538a3e6fcd
Timothy Meehan <tim@timdmeehan.com>
2025-10-28 14:10:14 -0400

    feat(plugin-memory): Add support for materialized views in memory connector (#26405)
    ## Description
    Per title. Also, REFRESH with WHERE clause does not make sense for
    memory connector (which only has unpartitioned tables), so making it
    optional with a new session property.
    
    ## Motivation and Context
    Add tests for materialized views
    
    ## Impact
    N/A
    
    ## Test Plan
    Included tests.
    
    ## Contributor checklist
    
    - [ ] Please make sure your submission complies with our [contributing
    guide](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md),
    in particular [code
    style](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md#code-style)
    and [commit
    standards](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md#commit-standards).
    - [ ] PR description addresses the issue accurately and concisely. If
    the change is non-trivial, a GitHub Issue is referenced.
    - [ ] Documented new properties (with its default value), SQL syntax,
    functions, or other functionality.
    - [ ] If release notes are required, they follow the [release notes
    guidelines](https://github.com/prestodb/presto/wiki/Release-Notes-Guidelines).
    - [ ] Adequate tests were added if applicable.
    - [ ] CI passed.
    - [ ] If adding new dependencies, verified they have an [OpenSSF
    Scorecard](https://securityscorecards.dev/#the-checks) score of 5.0 or
    higher (or obtained explicit TSC approval for lower scores).
    
    ## Release Notes
    ```
    == NO RELEASE NOTE ==
    ```

--- presto-main-base/src/main/java/com/facebook/presto/sql/analyzer/StatementAnalyzer.java
+++ presto-main-base/src/main/java/com/facebook/presto/sql/analyzer/StatementAnalyzer.java
@@ -228,6 +228,7 @@ import java.util.stream.Stream;
 
 import static com.facebook.presto.SystemSessionProperties.getMaxGroupingSets;
 import static com.facebook.presto.SystemSessionProperties.isAllowWindowOrderByLiterals;
+import static com.facebook.presto.SystemSessionProperties.isLegacyMaterializedViews;
 import static com.facebook.presto.SystemSessionProperties.isMaterializedViewDataConsistencyEnabled;
 import static com.facebook.presto.SystemSessionProperties.isMaterializedViewPartitionFilteringEnabled;
 import static com.facebook.presto.common.RuntimeMetricName.SKIP_READING_FROM_MATERIALIZED_VIEW_COUNT;
@@ -864,10 +865,9 @@ class StatementAnalyzer
             // Use AllowAllAccessControl; otherwise Analyzer will check SELECT permission on the materialized view, which is not necessary.
             StatementAnalyzer viewAnalyzer = new StatementAnalyzer(analysis, metadata, sqlParser, new AllowAllAccessControl(), session, warningCollector);
             Scope viewScope = viewAnalyzer.analyze(node.getTarget(), scope);
-            if (!node.getWhere().isPresent()) {
-                throw new SemanticException(NOT_SUPPORTED, node, "Refresh Materialized View without predicates is not supported.");
-            }
-            Map<SchemaTableName, Expression> tablePredicates = extractTablePredicates(viewName, node.getWhere().get(), viewScope, metadata, session);
+
+            Map<SchemaTableName, Expression> tablePredicates = getTablePredicatesForMaterializedViewRefresh(
+                    session, node, viewName, viewScope, metadata);
 
             Query viewQuery = parseView(view.getOriginalSql(), viewName, node);
             Query refreshQuery = tablePredicates.containsKey(toSchemaTableName(viewName)) ?
@@ -910,10 +910,9 @@ class StatementAnalyzer
             // Use AllowAllAccessControl; otherwise Analyzer will check SELECT permission on the materialized view, which is not necessary.
             StatementAnalyzer viewAnalyzer = new StatementAnalyzer(analysis, metadata, sqlParser, new AllowAllAccessControl(), session, warningCollector);
             Scope viewScope = viewAnalyzer.analyze(refreshMaterializedView.getTarget(), scope);
-            if (!refreshMaterializedView.getWhere().isPresent()) {
-                throw new SemanticException(NOT_SUPPORTED, "Refresh Materialized View without predicates is not supported.");
-            }
-            Map<SchemaTableName, Expression> tablePredicates = extractTablePredicates(viewName, refreshMaterializedView.getWhere().get(), viewScope, metadata, session);
+
+            Map<SchemaTableName, Expression> tablePredicates = getTablePredicatesForMaterializedViewRefresh(
+                    session, refreshMaterializedView, viewName, viewScope, metadata);
 
             SchemaTableName baseTableName = toSchemaTableName(createQualifiedObjectName(session, baseTable, baseTable.getName(), metadata));
             if (tablePredicates.containsKey(baseTableName)) {
@@ -926,6 +925,28 @@ class StatementAnalyzer
             }
 
             return Optional.empty();
+        }
+
+        private Map<SchemaTableName, Expression> getTablePredicatesForMaterializedViewRefresh(
+                Session session,
+                RefreshMaterializedView node,
+                QualifiedObjectName viewName,
+                Scope viewScope,
+                Metadata metadata)
+        {
+            if (isLegacyMaterializedViews(session)) {
+                if (!node.getWhere().isPresent()) {
+                    throw new SemanticException(NOT_SUPPORTED, node, "Refresh Materialized View without predicates is not supported.");
+                }
+                return extractTablePredicates(viewName, node.getWhere().get(), viewScope, metadata, session);
+            }
+            else {
+                if (node.getWhere().isPresent()) {
+                    throw new SemanticException(NOT_SUPPORTED, node, "WHERE clause in REFRESH MATERIALIZED VIEW is not supported. " +
+                            "Connectors automatically determine which data needs refreshing based on staleness detection.");
+                }
+                return ImmutableMap.of();
+            }
         }
 
         private Query buildQueryWithPredicate(Table table, Expression predicate)
