1f7fa8714fc90febf755da9a7aed7ba8a1711a3d
Natasha Sehgal <nasehgal@meta.com>
2025-10-29 14:11:32 -0700

    feat: Add TW feature flag for native report builder function
    Summary: Add TW flag to enable native report builder function
    
    Differential Revision: D85263402

--- presto-native-execution/presto_cpp/main/common/Configs.h
+++ presto-native-execution/presto_cpp/main/common/Configs.h
@@ -602,6 +602,8 @@ class SystemConfig : public ConfigBase {
       "enable_velox_task_logging"};
   static constexpr std::string_view kEnableVeloxExprSetLogging{
       "enable_velox_expression_logging"};
+  static constexpr std::string_view kEnableFbRemoteReportBuilderFunction{
+      "enable_fb_remote_report_builder_function"};
   static constexpr std::string_view kLocalShuffleMaxPartitionBytes{
       "shuffle.local.max-partition-bytes"};
   static constexpr std::string_view kShuffleName{"shuffle.name"};
@@ -975,6 +977,8 @@ class SystemConfig : public ConfigBase {
   bool enableVeloxTaskLogging() const;
 
   bool enableVeloxExprSetLogging() const;
+
+  bool enableFbRemoteReportBuilderFunction() const;
 
   bool useMmapAllocator() const;
 
