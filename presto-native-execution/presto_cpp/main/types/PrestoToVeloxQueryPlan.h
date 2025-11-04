1c38ff0d1ee3f84b967f23a1205666e6bfd38914
Zoltan Arnold Nagy <nag@zurich.ibm.com>
2025-11-05 00:09:53 +0100

    revert the old rootFragment fix
    (cherry picked from commit 5f7e352b88112cd9e451f429ee7d57c054618634)

--- presto-native-execution/presto_cpp/main/types/PrestoToVeloxQueryPlan.h
+++ presto-native-execution/presto_cpp/main/types/PrestoToVeloxQueryPlan.h
@@ -58,8 +58,7 @@ class VeloxQueryPlanConverterBase {
   velox::core::PlanNodePtr toVeloxQueryPlan(
       const std::shared_ptr<const protocol::OutputNode>& node,
       const std::shared_ptr<protocol::TableWriteInfo>& tableWriteInfo,
-      const protocol::TaskId& taskId,
-      const bool isRootFragment = false);
+      const protocol::TaskId& taskId);
 
   velox::core::PlanNodePtr toVeloxQueryPlan(
       const std::shared_ptr<const protocol::ExchangeNode>& node,
