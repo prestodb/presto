38c089cd82e0fd407380d82ea1a7d76e1f341e80
Nishitha-Bhaskaran <nishithakbhaskaran@gmail.com>
2025-11-05 14:19:33 +0530

    update image name 25.7.7.68

--- presto-clickhouse/src/test/java/com/facebook/presto/plugin/clickhouse/TestingClickHouseServer.java
+++ presto-clickhouse/src/test/java/com/facebook/presto/plugin/clickhouse/TestingClickHouseServer.java
@@ -27,7 +27,7 @@ import static org.testcontainers.utility.MountableFile.forClasspathResource;
 public class TestingClickHouseServer
         implements Closeable
 {
-    private static final String CLICKHOUSE_IMAGE = "clickhouse/clickhouse-server:25.3.6.56";
+    private static final String CLICKHOUSE_IMAGE = "clickhouse/clickhouse-server:25.7.7.68";
     private final ClickHouseContainer dockerContainer;
 
     public TestingClickHouseServer()
