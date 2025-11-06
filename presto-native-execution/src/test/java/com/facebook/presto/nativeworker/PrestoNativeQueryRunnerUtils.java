0a4e2ad109d7a61a27734c36df664ec3eb4b4422
Aditi Pandit <87670623+aditi-pandit@users.noreply.github.com>
2025-11-06 09:01:39 -0800

    misc(native): Add native-execution-enabled in HiveExternalWorkerQueryRunner (#26548)
    ```
    == NO RELEASE NOTE ==
    ```

--- presto-native-execution/src/test/java/com/facebook/presto/nativeworker/PrestoNativeQueryRunnerUtils.java
+++ presto-native-execution/src/test/java/com/facebook/presto/nativeworker/PrestoNativeQueryRunnerUtils.java
@@ -503,6 +503,7 @@ public class PrestoNativeQueryRunnerUtils
                         // Write config file - use an ephemeral port for the worker.
                         String configProperties = format("discovery.uri=%s%n" +
                                 "presto.version=testversion%n" +
+                                "native-execution-enabled=true%n" +
                                 "system-memory-gb=4%n" +
                                 "http-server.http.port=0%n", discoveryUri);
 
