1c80a577543227e9560edfd981cad8e4b22c0239
Joe Abraham <53977252+Joe-Abraham@users.noreply.github.com>
2025-11-14 03:34:33 +0530

    refactor(native): Move function to extract function name parts to utilities (#26605)
    ```
    == NO RELEASE NOTE ==
    ```

--- presto-native-execution/presto_cpp/main/common/Utils.cpp
+++ presto-native-execution/presto_cpp/main/common/Utils.cpp
@@ -136,4 +136,14 @@ std::string decompressMessageBody(
         e.what());
   }
 }
+
+const std::vector<std::string> getFunctionNameParts(
+    const std::string& registeredFunction) {
+  std::vector<std::string> parts;
+  folly::split('.', registeredFunction, parts, true);
+  VELOX_USER_CHECK(
+      parts.size() == 3,
+      fmt::format("Prefix missing for function {}", registeredFunction));
+  return parts;
+}
 } // namespace facebook::presto::util
