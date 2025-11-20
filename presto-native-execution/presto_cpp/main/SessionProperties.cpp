fb32a0401d70555d35cab8c618fc6bdedb86af00
yingsu00 <yingsu00@outlook.com>
2025-11-19 20:36:29 -0800

    feat: Add native_pushdown_integer_upcasts_to_source session properties
    This commit adds a new native session property native_pushdown_integer_
    upcasts_to_scan, which when set to true, would rewrite the local plan
    and push down integer upcasts to the source operaters.

--- presto-native-execution/presto_cpp/main/SessionProperties.cpp
+++ presto-native-execution/presto_cpp/main/SessionProperties.cpp
@@ -586,6 +586,14 @@ SessionProperties::SessionProperties() {
       false,
       std::nullopt,
       "true");
+
+  addSessionProperty(
+      kPushdownIntegerUpcastsToSource,
+      "Enable pushdown of integer upcasts to the source operators.",
+      BOOLEAN(),
+      false,
+      QueryConfig::kPushdownIntegerUpcastsToSource,
+      boolToString(c.pushdownIntegerUpcastsToSource()));
 }
 
 const std::string SessionProperties::toVeloxConfig(
