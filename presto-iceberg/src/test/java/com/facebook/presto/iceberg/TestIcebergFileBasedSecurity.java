c031abc0706d96dafc4d6669390891161e3d2a82
wangd <mingwbd@gmail.com>
2025-12-18 15:29:15 +0800

    Add row filters and column masks access check for distributed procedure

--- presto-iceberg/src/test/java/com/facebook/presto/iceberg/TestIcebergFileBasedSecurity.java
+++ presto-iceberg/src/test/java/com/facebook/presto/iceberg/TestIcebergFileBasedSecurity.java
@@ -14,8 +14,12 @@
 package com.facebook.presto.iceberg;
 
 import com.facebook.presto.Session;
+import com.facebook.presto.common.QualifiedObjectName;
 import com.facebook.presto.spi.security.Identity;
+import com.facebook.presto.spi.security.ViewExpression;
+import com.facebook.presto.sql.query.QueryAssertions;
 import com.facebook.presto.testing.QueryRunner;
+import com.facebook.presto.testing.TestingAccessControlManager;
 import com.facebook.presto.tests.AbstractTestQueryFramework;
 import com.facebook.presto.tests.DistributedQueryRunner;
 import com.google.common.collect.ImmutableMap;
@@ -33,9 +37,13 @@ import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.testng.Assert.ThrowingRunnable;
 
+@Test(singleThreaded = true)
 public class TestIcebergFileBasedSecurity
         extends AbstractTestQueryFramework
 {
+    private QueryAssertions assertions;
+    private TestingAccessControlManager accessControl;
+
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
@@ -60,11 +68,35 @@ public class TestIcebergFileBasedSecurity
 
         queryRunner.createCatalog(ICEBERG_CATALOG, "iceberg", icebergProperties);
 
+        assertions = new QueryAssertions(queryRunner);
+        accessControl = assertions.getQueryRunner().getAccessControl();
+
         return queryRunner;
     }
 
     @Test
-    public void testCallProcedures()
+    public void testCallNormalProcedures()
+    {
+        Session icebergAdmin = Session.builder(getSession("iceberg"))
+                .build();
+        Session alice = Session.builder(getSession("alice"))
+                .build();
+        Session bob = Session.builder(getSession("bob"))
+                .build();
+        Session joe = Session.builder(getSession("joe"))
+                .build();
+
+        // `icebergAdmin`, `alice`, and `bob` have the permission to execute `iceberg.system.invalidate_statistics_file_cache`
+        assertUpdate(icebergAdmin, "call system.invalidate_statistics_file_cache()");
+        assertUpdate(alice, "call system.invalidate_statistics_file_cache()");
+        assertUpdate(bob, "call system.invalidate_statistics_file_cache()");
+        // `joe` do not have the permission to execute `iceberg.system.invalidate_statistics_file_cache`
+        assertDenied(() -> assertUpdate(joe, "call iceberg.system.invalidate_statistics_file_cache()"),
+                "Access Denied: Cannot call procedure system.invalidate_statistics_file_cache");
+    }
+
+    @Test
+    public void testCallDistributedProceduresWithInsertDeletePermission()
     {
         Session icebergAdmin = Session.builder(getSession("iceberg"))
                 .build();
@@ -84,14 +116,6 @@ public class TestIcebergFileBasedSecurity
             assertUpdate(icebergAdmin, "insert into " + tableName + " values(1, '1001')", 1);
             assertUpdate(icebergAdmin, "insert into " + tableName + " values(2, '1002')", 1);
 
-            // `icebergAdmin`, `alice`, and `bob` have the permission to execute `iceberg.system.invalidate_statistics_file_cache`
-            assertUpdate(icebergAdmin, "call system.invalidate_statistics_file_cache()");
-            assertUpdate(alice, "call system.invalidate_statistics_file_cache()");
-            assertUpdate(bob, "call system.invalidate_statistics_file_cache()");
-            // `joe` do not have the permission to execute `iceberg.system.invalidate_statistics_file_cache`
-            assertDenied(() -> assertUpdate(joe, "call iceberg.system.invalidate_statistics_file_cache()"),
-                    "Access Denied: Cannot call procedure system.invalidate_statistics_file_cache");
-
             // `icebergAdmin` has permission to execute `iceberg.system.rewrite_data_files` and
             // perform INSERT/DELETE operations on the target table involved in the procedure
             assertUpdate(icebergAdmin, format("call system.rewrite_data_files('%s', '%s')", schema, tableName), 2);
@@ -104,6 +128,50 @@ public class TestIcebergFileBasedSecurity
             // `joe` do not have the permission to execute `iceberg.system.rewrite_data_files`
             assertDenied(() -> assertUpdate(joe, format("call system.rewrite_data_files('%s', '%s')", schema, tableName)),
                     "Access Denied: Cannot call procedure system.rewrite_data_files");
+        }
+        finally {
+            assertUpdate(icebergAdmin, "drop table if exists " + tableName);
+            assertUpdate(icebergAdmin, "drop schema if exists " + schema);
+        }
+    }
+
+    @Test
+    public void testCallDistributedProceduresWithRowFiltersAndColumnMasks()
+    {
+        Session icebergAdmin = Session.builder(getSession("iceberg"))
+                .build();
+
+        String schema = getSession().getSchema().get();
+        String tableName = "test_rewrite_table";
+
+        try {
+            assertUpdate(icebergAdmin, "create schema if not exists " + schema);
+            assertUpdate(icebergAdmin, "create table " + tableName + " (a int, b varchar)");
+            assertUpdate(icebergAdmin, "insert into " + tableName + " values(1, '1001')", 1);
+            assertUpdate(icebergAdmin, "insert into " + tableName + " values(2, '1002')", 1);
+
+            // `icebergAdmin` has permission to execute `iceberg.system.rewrite_data_files` and
+            // perform INSERT/DELETE operations on the target table involved in the procedure
+            assertUpdate(icebergAdmin, format("call system.rewrite_data_files('%s', '%s')", schema, tableName), 2);
+
+            assertions.executeExclusively(() -> {
+                accessControl.reset();
+                accessControl.rowFilter(
+                        new QualifiedObjectName("iceberg", schema, tableName),
+                        "iceberg",
+                        new ViewExpression("iceberg", Optional.empty(), Optional.empty(), "a < 2"));
+                assertions.assertQuery(icebergAdmin, "SELECT count(*) FROM " + tableName, "VALUES BIGINT '1'");
+                assertions.assertFails(icebergAdmin, format("call system.rewrite_data_files('%s', '%s')", schema, tableName),
+                        "Access Denied: Full data access is restricted by row filters and column masks");
+            });
+
+            assertions.executeExclusively(() -> {
+                accessControl.reset();
+                accessControl.columnMask(new QualifiedObjectName("iceberg", schema, tableName), "b", "iceberg",
+                                new ViewExpression("iceberg", Optional.empty(), Optional.empty(), "'noop'"));
+                assertions.assertFails(icebergAdmin, format("call system.rewrite_data_files('%s', '%s')", schema, tableName),
+                        "Access Denied: Full data access is restricted by row filters and column masks");
+            });
         }
         finally {
             assertUpdate(icebergAdmin, "drop table if exists " + tableName);
