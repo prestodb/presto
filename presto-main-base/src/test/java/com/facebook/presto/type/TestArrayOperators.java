d7cd45278732d89a2622fb14016785f33d3ebdc0
Yabin Ma <mayabin@gmail.com>
2025-12-19 21:39:38 +0100

    fix: Change fieldNamesInJsonCastEnabled default value to true

--- presto-main-base/src/test/java/com/facebook/presto/type/TestArrayOperators.java
+++ presto-main-base/src/test/java/com/facebook/presto/type/TestArrayOperators.java
@@ -100,6 +100,7 @@ public class TestArrayOperators
         extends AbstractTestFunctions
 {
     private static FunctionAssertions fieldNameInJsonCastEnabled;
+    private static FunctionAssertions fieldNameInJsonCastDisabled;
 
     public TestArrayOperators() {}
 
@@ -112,6 +113,11 @@ public class TestArrayOperators
                         .setSystemProperty(FIELD_NAMES_IN_JSON_CAST_ENABLED, "true")
                         .build(),
                 new FeaturesConfig());
+        fieldNameInJsonCastDisabled = new FunctionAssertions(
+                Session.builder(session)
+                        .setSystemProperty(FIELD_NAMES_IN_JSON_CAST_ENABLED, "false")
+                        .build(),
+                new FeaturesConfig());
     }
 
     @AfterClass(alwaysRun = true)
@@ -119,6 +125,8 @@ public class TestArrayOperators
     {
         fieldNameInJsonCastEnabled.close();
         fieldNameInJsonCastEnabled = null;
+        fieldNameInJsonCastDisabled.close();
+        fieldNameInJsonCastDisabled = null;
     }
 
     @ScalarFunction
@@ -257,51 +265,51 @@ public class TestArrayOperators
     @Test
     public void testArrayToJsonNoFieldNames()
     {
-        assertFunction("cast(cast (null as ARRAY<BIGINT>) AS JSON)", JSON, null);
-        assertFunction("cast(ARRAY[] AS JSON)", JSON, "[]");
-        assertFunction("cast(ARRAY[null, null] AS JSON)", JSON, "[null,null]");
+        fieldNameInJsonCastDisabled.assertFunction("cast(cast (null as ARRAY<BIGINT>) AS JSON)", JSON, null);
+        fieldNameInJsonCastDisabled.assertFunction("cast(ARRAY[] AS JSON)", JSON, "[]");
+        fieldNameInJsonCastDisabled.assertFunction("cast(ARRAY[null, null] AS JSON)", JSON, "[null,null]");
 
-        assertFunction("cast(ARRAY[true, false, null] AS JSON)", JSON, "[true,false,null]");
+        fieldNameInJsonCastDisabled.assertFunction("cast(ARRAY[true, false, null] AS JSON)", JSON, "[true,false,null]");
 
-        assertFunction("cast(cast(ARRAY[1, 2, null] AS ARRAY<TINYINT>) AS JSON)", JSON, "[1,2,null]");
-        assertFunction("cast(cast(ARRAY[12345, -12345, null] AS ARRAY<SMALLINT>) AS JSON)", JSON, "[12345,-12345,null]");
-        assertFunction("cast(cast(ARRAY[123456789, -123456789, null] AS ARRAY<INTEGER>) AS JSON)", JSON, "[123456789,-123456789,null]");
-        assertFunction("cast(cast(ARRAY[1234567890123456789, -1234567890123456789, null] AS ARRAY<BIGINT>) AS JSON)", JSON, "[1234567890123456789,-1234567890123456789,null]");
+        fieldNameInJsonCastDisabled.assertFunction("cast(cast(ARRAY[1, 2, null] AS ARRAY<TINYINT>) AS JSON)", JSON, "[1,2,null]");
+        fieldNameInJsonCastDisabled.assertFunction("cast(cast(ARRAY[12345, -12345, null] AS ARRAY<SMALLINT>) AS JSON)", JSON, "[12345,-12345,null]");
+        fieldNameInJsonCastDisabled.assertFunction("cast(cast(ARRAY[123456789, -123456789, null] AS ARRAY<INTEGER>) AS JSON)", JSON, "[123456789,-123456789,null]");
+        fieldNameInJsonCastDisabled.assertFunction("cast(cast(ARRAY[1234567890123456789, -1234567890123456789, null] AS ARRAY<BIGINT>) AS JSON)", JSON, "[1234567890123456789,-1234567890123456789,null]");
 
-        assertFunction("CAST(CAST(ARRAY[3.14E0, nan(), infinity(), -infinity(), null] AS ARRAY<REAL>) AS JSON)", JSON, "[3.14,\"NaN\",\"Infinity\",\"-Infinity\",null]");
-        assertFunction("CAST(ARRAY[3.14E0, 1e-323, 1e308, nan(), infinity(), -infinity(), null] AS JSON)", JSON, "[3.14,1.0E-323,1.0E308,\"NaN\",\"Infinity\",\"-Infinity\",null]");
-        assertFunction("CAST(ARRAY[DECIMAL '3.14', null] AS JSON)", JSON, "[3.14,null]");
-        assertFunction("CAST(ARRAY[DECIMAL '12345678901234567890.123456789012345678', null] AS JSON)", JSON, "[12345678901234567890.123456789012345678,null]");
+        fieldNameInJsonCastDisabled.assertFunction("CAST(CAST(ARRAY[3.14E0, nan(), infinity(), -infinity(), null] AS ARRAY<REAL>) AS JSON)", JSON, "[3.14,\"NaN\",\"Infinity\",\"-Infinity\",null]");
+        fieldNameInJsonCastDisabled.assertFunction("CAST(ARRAY[3.14E0, 1e-323, 1e308, nan(), infinity(), -infinity(), null] AS JSON)", JSON, "[3.14,1.0E-323,1.0E308,\"NaN\",\"Infinity\",\"-Infinity\",null]");
+        fieldNameInJsonCastDisabled.assertFunction("CAST(ARRAY[DECIMAL '3.14', null] AS JSON)", JSON, "[3.14,null]");
+        fieldNameInJsonCastDisabled.assertFunction("CAST(ARRAY[DECIMAL '12345678901234567890.123456789012345678', null] AS JSON)", JSON, "[12345678901234567890.123456789012345678,null]");
 
-        assertFunction("cast(ARRAY['a', 'bb', null] AS JSON)", JSON, "[\"a\",\"bb\",null]");
-        assertFunction(
+        fieldNameInJsonCastDisabled.assertFunction("cast(ARRAY['a', 'bb', null] AS JSON)", JSON, "[\"a\",\"bb\",null]");
+        fieldNameInJsonCastDisabled.assertFunction(
                 "cast(ARRAY[JSON '123', JSON '3.14', JSON 'false', JSON '\"abc\"', JSON '[1, \"a\", null]', JSON '{\"a\": 1, \"b\": \"str\", \"c\": null}', JSON 'null', null] AS JSON)",
                 JSON,
                 "[123,3.14,false,\"abc\",[1,\"a\",null],{\"a\":1,\"b\":\"str\",\"c\":null},null,null]");
 
-        assertFunction(
+        fieldNameInJsonCastDisabled.assertFunction(
                 "CAST(ARRAY[TIMESTAMP '1970-01-01 00:00:01', null] AS JSON)",
                 JSON,
                 format("[\"%s\",null]", sqlTimestampOf(1970, 1, 1, 0, 0, 1, 0, TEST_SESSION)));
-        assertFunction(
+        fieldNameInJsonCastDisabled.assertFunction(
                 "CAST(ARRAY[DATE '2001-08-22', DATE '2001-08-23', null] AS JSON)",
                 JSON,
                 "[\"2001-08-22\",\"2001-08-23\",null]");
 
-        assertFunction(
+        fieldNameInJsonCastDisabled.assertFunction(
                 "cast(ARRAY[ARRAY[1, 2], ARRAY[3, null], ARRAY[], ARRAY[null, null], null] AS JSON)",
                 JSON,
                 "[[1,2],[3,null],[],[null,null],null]");
-        assertFunction(
+        fieldNameInJsonCastDisabled.assertFunction(
                 "cast(ARRAY[MAP(ARRAY['b', 'a'], ARRAY[2, 1]), MAP(ARRAY['three', 'none'], ARRAY[3, null]), MAP(), MAP(ARRAY['h2', 'h1'], ARRAY[null, null]), null] AS JSON)",
                 JSON,
                 "[{\"a\":1,\"b\":2},{\"none\":null,\"three\":3},{},{\"h1\":null,\"h2\":null},null]");
-        assertFunction(
+        fieldNameInJsonCastDisabled.assertFunction(
                 "cast(ARRAY[ROW(1, 2), ROW(3, CAST(null as INTEGER)), CAST(ROW(null, null) AS ROW(INTEGER, INTEGER)), null] AS JSON)",
                 JSON,
                 "[[1,2],[3,null],[null,null],null]");
-        assertFunction("CAST(ARRAY [12345.12345, 12345.12345, 3.0] AS JSON)", JSON, "[12345.12345,12345.12345,3.00000]");
-        assertFunction(
+        fieldNameInJsonCastDisabled.assertFunction("CAST(ARRAY [12345.12345, 12345.12345, 3.0] AS JSON)", JSON, "[12345.12345,12345.12345,3.00000]");
+        fieldNameInJsonCastDisabled.assertFunction(
                 "CAST(ARRAY [123456789012345678901234567890.87654321, 123456789012345678901234567890.12345678] AS JSON)",
                 JSON,
                 "[123456789012345678901234567890.87654321,123456789012345678901234567890.12345678]");
