7e89f829267d57213ebf22dc13b5ca319af8330c
mohsaka <135669458+mohsaka@users.noreply.github.com>
2026-01-06 09:58:54 -0800

    Refactor DeltaQueryRunner and add native support
    Co-authored-by: unidevel <unidevel@hotmail.com>

--- presto-delta/src/test/java/com/facebook/presto/delta/TestUppercasePartitionColumns.java
+++ presto-delta/src/test/java/com/facebook/presto/delta/TestUppercasePartitionColumns.java
@@ -13,22 +13,15 @@
  */
 package com.facebook.presto.delta;
 
-import com.facebook.presto.Session;
 import com.facebook.presto.common.type.TimeZoneKey;
-import com.facebook.presto.hive.HivePlugin;
 import com.facebook.presto.testing.MaterializedResult;
 import com.facebook.presto.testing.QueryRunner;
-import com.facebook.presto.tests.DistributedQueryRunner;
-import com.facebook.presto.tpch.TpchPlugin;
 import com.google.common.collect.ImmutableMap;
 import org.testng.annotations.Test;
 
-import java.nio.file.Path;
 import java.util.Map;
 
-import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
 import static java.lang.String.format;
-import static java.util.Locale.US;
 import static org.testng.Assert.assertEquals;
 
 public class TestUppercasePartitionColumns
@@ -38,54 +31,16 @@ public class TestUppercasePartitionColumns
     protected QueryRunner createQueryRunner()
             throws Exception
     {
-        return createDeltaQueryRunner(ImmutableMap.of(
+        Map<String, String> extraProperties = ImmutableMap.of(
                 "experimental.pushdown-subfields-enabled", "true",
-                "experimental.pushdown-dereference-enabled", "true"));
-    }
+                "experimental.pushdown-dereference-enabled", "true");
 
-    private static DistributedQueryRunner createDeltaQueryRunner(Map<String, String> extraProperties)
-            throws Exception
-    {
-        Session session = testSessionBuilder()
-                .setCatalog(DELTA_CATALOG)
-                .setSchema(DELTA_SCHEMA.toLowerCase(US))
+        return DeltaQueryRunner.builder()
                 .setTimeZoneKey(TimeZoneKey.getTimeZoneKey("Europe/Madrid"))
-                .build();
-
-        DistributedQueryRunner queryRunner = DistributedQueryRunner.builder(session)
                 .setExtraProperties(extraProperties)
-                .build();
-
-        // Install the TPCH plugin for test data (not in Delta format)
-        queryRunner.installPlugin(new TpchPlugin());
-        queryRunner.createCatalog("tpch", "tpch");
-
-        Path dataDirectory = queryRunner.getCoordinator().getDataDirectory().resolve("delta_metadata");
-        Path catalogDirectory = dataDirectory.getParent().resolve("catalog");
-
-        // Install a Delta connector catalog
-        queryRunner.installPlugin(new DeltaPlugin());
-        Map<String, String> deltaProperties = ImmutableMap.<String, String>builder()
-                .put("hive.metastore", "file")
-                .put("hive.metastore.catalog.dir", catalogDirectory.toFile().toURI().toString())
-                .put("delta.case-sensitive-partitions-enabled", "true")
-                .build();
-        queryRunner.createCatalog(DELTA_CATALOG, "delta", deltaProperties);
-
-        // Install a Hive connector catalog that uses the same metastore as Delta
-        // This catalog will be used to create tables in metastore as the Delta connector doesn't
-        // support creating tables yet.
-        queryRunner.installPlugin(new HivePlugin("hive"));
-        Map<String, String> hiveProperties = ImmutableMap.<String, String>builder()
-                .put("hive.metastore", "file")
-                .put("hive.metastore.catalog.dir", catalogDirectory.toFile().toURI().toString())
-                .put("hive.allow-drop-table", "true")
-                .put("hive.security", "legacy")
-                .build();
-        queryRunner.createCatalog(HIVE_CATALOG, "hive", hiveProperties);
-        queryRunner.execute(format("CREATE SCHEMA %s.%s", HIVE_CATALOG, DELTA_SCHEMA));
-
-        return queryRunner;
+                .caseSensitivePartitions()
+                .build()
+                .getQueryRunner();
     }
 
     @Test(dataProvider = "deltaReaderVersions")
