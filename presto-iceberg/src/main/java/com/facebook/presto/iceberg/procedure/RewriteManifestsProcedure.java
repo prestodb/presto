2ba4faf8224024023d8bf435f2166964bebeea39
Reetika Agrawal <agrawal.reetika786@gmail.com>
2026-01-06 10:06:02 +0530

    feat(plugin-iceberg): Support rewrite_manifests procedure for iceberg (#26888)
    ## Description
    Support rewrite_manifests procedure for iceberg
    
    ## Motivation and Context
    New procedure to rewrite the manifest files of an Iceberg table to
    optimize table metadata.
    
    ## Impact
    New procedure to rewrite the manifest files of an Iceberg table to
    optimize table metadata.
    
    ## Test Plan
    Added tests
    
    ## Contributor checklist
    
    - [ ] Please make sure your submission complies with our [contributing
    guide](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md),
    in particular [code
    style](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md#code-style)
    and [commit
    standards](https://github.com/prestodb/presto/blob/master/CONTRIBUTING.md#commit-standards).
    - [ ] PR description addresses the issue accurately and concisely. If
    the change is non-trivial, a GitHub Issue is referenced.
    - [ ] Documented new properties (with its default value), SQL syntax,
    functions, or other functionality.
    - [ ] If release notes are required, they follow the [release notes
    guidelines](https://github.com/prestodb/presto/wiki/Release-Notes-Guidelines).
    - [ ] Adequate tests were added if applicable.
    - [ ] CI passed.
    - [ ] If adding new dependencies, verified they have an [OpenSSF
    Scorecard](https://securityscorecards.dev/#the-checks) score of 5.0 or
    higher (or obtained explicit TSC approval for lower scores).
    
    ## Release Notes
    Please follow [release notes
    guidelines](https://github.com/prestodb/presto/wiki/Release-Notes-Guidelines)
    and fill in the release notes below.
    
    ```
    == RELEASE NOTES ==
    
    Iceberg Connector Changes
    * Add rewrite_manifests procedure for iceberg
    ```

---
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/procedure/RewriteManifestsProcedure.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.iceberg.procedure;
+
+import com.facebook.presto.iceberg.IcebergMetadataFactory;
+import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.PrestoException;
+import com.facebook.presto.spi.SchemaTableName;
+import com.facebook.presto.spi.classloader.ThreadContextClassLoader;
+import com.facebook.presto.spi.connector.ConnectorMetadata;
+import com.facebook.presto.spi.procedure.Procedure;
+import com.facebook.presto.spi.procedure.Procedure.Argument;
+import com.google.common.collect.ImmutableList;
+import jakarta.inject.Inject;
+import org.apache.iceberg.RewriteManifests;
+import org.apache.iceberg.Table;
+
+import javax.inject.Provider;
+
+import java.lang.invoke.MethodHandle;
+
+import static com.facebook.presto.common.block.MethodHandleUtil.methodHandle;
+import static com.facebook.presto.common.type.StandardTypes.INTEGER;
+import static com.facebook.presto.common.type.StandardTypes.VARCHAR;
+import static com.facebook.presto.iceberg.IcebergErrorCode.ICEBERG_INVALID_SPEC_ID;
+import static com.facebook.presto.iceberg.IcebergUtil.getIcebergTable;
+import static java.util.Objects.requireNonNull;
+
+public class RewriteManifestsProcedure
+        implements Provider<Procedure>
+{
+    private static final MethodHandle REWRITE_MANIFESTS = methodHandle(
+            RewriteManifestsProcedure.class,
+            "rewriteManifests",
+            ConnectorSession.class,
+            String.class,
+            String.class,
+            Integer.class);
+
+    private final IcebergMetadataFactory metadataFactory;
+
+    @Inject
+    public RewriteManifestsProcedure(IcebergMetadataFactory metadataFactory)
+    {
+        this.metadataFactory = requireNonNull(metadataFactory, "metadataFactory is null");
+    }
+
+    @Override
+    public Procedure get()
+    {
+        return new Procedure(
+                "system",
+                "rewrite_manifests",
+                ImmutableList.of(
+                        new Argument("schema", VARCHAR),
+                        new Argument("table_name", VARCHAR),
+                        new Argument("spec_id", INTEGER, false, null)),
+                REWRITE_MANIFESTS.bindTo(this));
+    }
+
+    public void rewriteManifests(ConnectorSession clientSession, String schemaName, String tableName, Integer specId)
+    {
+        try (ThreadContextClassLoader ignored = new ThreadContextClassLoader(getClass().getClassLoader())) {
+            SchemaTableName schemaTableName = new SchemaTableName(schemaName, tableName);
+            ConnectorMetadata metadata = metadataFactory.create();
+            Table icebergTable = getIcebergTable(metadata, clientSession, schemaTableName);
+            RewriteManifests rewriteManifests = icebergTable.rewriteManifests().clusterBy(file -> "file");
+            int targetSpecId;
+            if (specId != null) {
+                if (!icebergTable.specs().containsKey(specId)) {
+                    throw new PrestoException(ICEBERG_INVALID_SPEC_ID, "Given spec id does not exist: " + specId);
+                }
+                targetSpecId = specId;
+            }
+            else {
+                targetSpecId = icebergTable.spec().specId();
+            }
+            rewriteManifests.rewriteIf(manifest -> manifest.partitionSpecId() == targetSpecId).commit();
+        }
+    }
+}
