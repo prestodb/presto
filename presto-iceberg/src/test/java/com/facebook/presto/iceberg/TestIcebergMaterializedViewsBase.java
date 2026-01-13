8dc268c7100e862e561e2f4e6c55f3fb64e6280c
Tim Meehan <tim@timdmeehan.com>
2026-01-13 16:09:41 -0500

    Add support for materialized views with Hive catalog

--- presto-iceberg/src/test/java/com/facebook/presto/iceberg/TestIcebergMaterializedViewsBase.java
+++ presto-iceberg/src/test/java/com/facebook/presto/iceberg/TestIcebergMaterializedViewsBase.java
@@ -13,71 +13,18 @@
  */
 package com.facebook.presto.iceberg;
 
-import com.facebook.airlift.http.server.testing.TestingHttpServer;
 import com.facebook.presto.Session;
-import com.facebook.presto.testing.QueryRunner;
 import com.facebook.presto.tests.AbstractTestQueryFramework;
-import com.google.common.collect.ImmutableMap;
-import org.assertj.core.util.Files;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.util.Optional;
-
-import static com.facebook.presto.iceberg.CatalogType.REST;
-import static com.facebook.presto.iceberg.rest.IcebergRestTestUtil.getRestServer;
-import static com.facebook.presto.iceberg.rest.IcebergRestTestUtil.restConnectorProperties;
-import static com.google.common.io.MoreFiles.deleteRecursively;
-import static com.google.common.io.RecursiveDeleteOption.ALLOW_INSECURE;
 
 @Test(singleThreaded = true)
-public class TestIcebergMaterializedViews
+public abstract class TestIcebergMaterializedViewsBase
         extends AbstractTestQueryFramework
 {
-    private File warehouseLocation;
-    private TestingHttpServer restServer;
-    private String serverUri;
-
-    @BeforeClass
-    @Override
-    public void init()
-            throws Exception
-    {
-        warehouseLocation = Files.newTemporaryFolder();
-
-        restServer = getRestServer(warehouseLocation.getAbsolutePath());
-        restServer.start();
-
-        serverUri = restServer.getBaseUrl().toString();
-        super.init();
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDown()
-            throws Exception
-    {
-        if (restServer != null) {
-            restServer.stop();
-        }
-        deleteRecursively(warehouseLocation.toPath(), ALLOW_INSECURE);
-    }
-
-    @Override
-    protected QueryRunner createQueryRunner()
-            throws Exception
-    {
-        return IcebergQueryRunner.builder()
-                .setCatalogType(REST)
-                .setExtraConnectorProperties(restConnectorProperties(serverUri))
-                .setDataDirectory(Optional.of(warehouseLocation.toPath()))
-                .setSchemaName("test_schema")
-                .setCreateTpchTables(false)
-                .setExtraProperties(ImmutableMap.of("experimental.legacy-materialized-views", "false"))
-                .build().getQueryRunner();
-    }
+    protected File warehouseLocation;
 
     @Test
     public void testCreateMaterializedView()
