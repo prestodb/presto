05360d2366731a0e2ffb76ebdef93baa560c0ff5
auden-woolfson <auden.woolfson@ibm.com>
2026-01-22 14:48:08 -0800

    Add support for views in MySql connector

---
+++ presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlMetadataFactory.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.plugin.mysql;
+
+import com.facebook.presto.plugin.jdbc.JdbcMetadata;
+import com.facebook.presto.plugin.jdbc.JdbcMetadataCache;
+import com.facebook.presto.plugin.jdbc.JdbcMetadataConfig;
+import com.facebook.presto.plugin.jdbc.JdbcMetadataFactory;
+import com.facebook.presto.plugin.jdbc.TableLocationProvider;
+import jakarta.inject.Inject;
+
+import static java.util.Objects.requireNonNull;
+
+public class MySqlMetadataFactory
+        extends JdbcMetadataFactory
+{
+    private final JdbcMetadataCache jdbcMetadataCache;
+    private final MySqlClient mySqlClient;
+    private final boolean allowDropTable;
+    private final TableLocationProvider tableLocationProvider;
+
+    @Inject
+    public MySqlMetadataFactory(JdbcMetadataCache jdbcMetadataCache, MySqlClient mySqlClient, JdbcMetadataConfig config, TableLocationProvider tableLocationProvider)
+    {
+        super(jdbcMetadataCache, mySqlClient, config, tableLocationProvider);
+        this.jdbcMetadataCache = requireNonNull(jdbcMetadataCache, "jdbcMetadataCache is null");
+        this.mySqlClient = requireNonNull(mySqlClient, "mySqlClient is null");
+        requireNonNull(config, "config is null");
+        this.allowDropTable = config.isAllowDropTable();
+        this.tableLocationProvider = requireNonNull(tableLocationProvider, "tableLocationProvider is null");
+    }
+
+    @Override
+    public JdbcMetadata create()
+    {
+        return new MySqlMetadata(
+                jdbcMetadataCache,
+                mySqlClient,
+                allowDropTable,
+                tableLocationProvider);
+    }
+}
