021694331bc38b4ffe842e4b1253b8ea985d30c3
auden-woolfson <auden.woolfson@ibm.com>
2026-01-22 14:56:01 -0800

    Compiles

--- presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlMetadata.java
+++ presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlMetadata.java
@@ -1,6 +1,18 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.facebook.presto.plugin.mysql;
 
-import com.facebook.presto.plugin.jdbc.JdbcClient;
 import com.facebook.presto.plugin.jdbc.JdbcMetadata;
 import com.facebook.presto.plugin.jdbc.JdbcMetadataCache;
 import com.facebook.presto.plugin.jdbc.TableLocationProvider;
@@ -43,7 +55,8 @@ public class MySqlMetadata
                 tableNames = listViews(session, Optional.of(prefix.getSchemaName()));
             }
             else {
-                tableNames = mySqlClient.listSchemasForViews(session);
+                //tableNames = mySqlClient.listSchemasForViews(session);
+                tableNames = ImmutableList.of();
             }
         }
 
