45b4084db4788c25c0ba2d6742122812c88dc3fb
xiuwenzheng <xiuwen.zheng@ibm.com>
2026-01-23 10:28:27 -0800

    fix code style

--- presto-iceberg/src/main/java/com/facebook/presto/iceberg/IcebergTableHandle.java
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/IcebergTableHandle.java
@@ -19,9 +19,6 @@ import com.facebook.presto.spi.ConnectorTableHandle;
 import com.facebook.presto.spi.HboCanonicalizableTableHandle;
 import com.facebook.presto.spi.HboUnpublishableTableHandle;
 import com.facebook.presto.spi.SchemaTableName;
-import com.facebook.presto.spi.ConnectorTableHandle;
-import com.facebook.presto.spi.HboCanonicalizableTableHandle;
-import com.facebook.presto.spi.HboUnpublishableTableHandle;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
@@ -50,18 +47,7 @@ public class IcebergTableHandle
     private final Optional<SchemaTableName> materializedViewName;
 
     @JsonCreator
-    public IcebergTableHandle(
-            @JsonProperty("schemaName") String schemaName,
-            @JsonProperty("icebergTableName") IcebergTableName icebergTableName,
-            @JsonProperty("snapshotSpecified") boolean snapshotSpecified,
-            @JsonProperty("outputPath") Optional<String> outputPath,
-            @JsonProperty("storageProperties") Optional<Map<String, String>> storageProperties,
-            @JsonProperty("tableSchemaJson") Optional<String> tableSchemaJson,
-            @JsonProperty("partitionFieldIds") Optional<Set<Integer>> partitionFieldIds,
-            @JsonProperty("equalityFieldIds") Optional<Set<Integer>> equalityFieldIds,
-            @JsonProperty("sortOrder") List<SortField> sortOrder,
-            @JsonProperty("updatedColumns") List<IcebergColumnHandle> updatedColumns,
-            @JsonProperty("materializedViewName") Optional<SchemaTableName> materializedViewName)
+    public IcebergTableHandle(@JsonProperty("schemaName") String schemaName, @JsonProperty("icebergTableName") IcebergTableName icebergTableName, @JsonProperty("snapshotSpecified") boolean snapshotSpecified, @JsonProperty("outputPath") Optional<String> outputPath, @JsonProperty("storageProperties") Optional<Map<String, String>> storageProperties, @JsonProperty("tableSchemaJson") Optional<String> tableSchemaJson, @JsonProperty("partitionFieldIds") Optional<Set<Integer>> partitionFieldIds, @JsonProperty("equalityFieldIds") Optional<Set<Integer>> equalityFieldIds, @JsonProperty("sortOrder") List<SortField> sortOrder, @JsonProperty("updatedColumns") List<IcebergColumnHandle> updatedColumns, @JsonProperty("materializedViewName") Optional<SchemaTableName> materializedViewName)
 
     {
         super(schemaName, icebergTableName.getTableName());
@@ -140,18 +126,7 @@ public class IcebergTableHandle
 
     public IcebergTableHandle withUpdatedColumns(List<IcebergColumnHandle> updatedColumns)
     {
-        return new IcebergTableHandle(
-                getSchemaName(),
-                icebergTableName,
-                snapshotSpecified,
-                outputPath,
-                storageProperties,
-                tableSchemaJson,
-                partitionFieldIds,
-                equalityFieldIds,
-                sortOrder,
-                updatedColumns,
-                materializedViewName);
+        return new IcebergTableHandle(getSchemaName(), icebergTableName, snapshotSpecified, outputPath, storageProperties, tableSchemaJson, partitionFieldIds, equalityFieldIds, sortOrder, updatedColumns, materializedViewName);
     }
 
     @Override
@@ -165,13 +140,7 @@ public class IcebergTableHandle
         }
 
         IcebergTableHandle that = (IcebergTableHandle) o;
-        return Objects.equals(getSchemaName(), that.getSchemaName()) &&
-                Objects.equals(icebergTableName, that.icebergTableName) &&
-                snapshotSpecified == that.snapshotSpecified &&
-                Objects.equals(sortOrder, that.sortOrder) &&
-                Objects.equals(tableSchemaJson, that.tableSchemaJson) &&
-                Objects.equals(equalityFieldIds, that.equalityFieldIds) &&
-                Objects.equals(materializedViewName, that.materializedViewName);
+        return Objects.equals(getSchemaName(), that.getSchemaName()) && Objects.equals(icebergTableName, that.icebergTableName) && snapshotSpecified == that.snapshotSpecified && Objects.equals(sortOrder, that.sortOrder) && Objects.equals(tableSchemaJson, that.tableSchemaJson) && Objects.equals(equalityFieldIds, that.equalityFieldIds) && Objects.equals(materializedViewName, that.materializedViewName);
     }
 
     @Override
