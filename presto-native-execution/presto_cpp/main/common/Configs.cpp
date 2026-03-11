d7822ef7e45ca91fc875b583aa26bff3c44187f5
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2026-03-11 17:59:27 -0500

    plan-dump-dir feature

--- presto-native-execution/presto_cpp/main/common/Configs.cpp
+++ presto-native-execution/presto_cpp/main/common/Configs.cpp
@@ -1278,6 +1278,10 @@ bool SystemConfig::planConsistencyCheckEnabled() const {
   return optionalProperty<bool>(kPlanConsistencyCheckEnabled).value();
 }
 
+folly::Optional<std::string> SystemConfig::planDumpDir() const {
+  return optionalProperty<std::string>(kPlanDumpDir);
+}
+
 NodeConfig::NodeConfig() {
   registeredProps_ =
       std::unordered_map<std::string, folly::Optional<std::string>>{
