ae8740ff3f7f92acc23db83d7ac3d83740438387
wangd <mingwbd@gmail.com>
2026-03-16 00:25:20 +0800

    refactor(plugin-iceberg): Defer the start of transaction for procedure
    This way, the distributed procedure can decide for itself which table's
    transaction to start. In some scenarios, the specific procedure may
    want to start and write to a target table that is not necessarily the
    one specified by the first two parameters (which might only serve as a
    source table).

--- presto-iceberg/src/main/java/com/facebook/presto/iceberg/IcebergAbstractMetadata.java
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/IcebergAbstractMetadata.java
@@ -1455,7 +1455,7 @@ public abstract class IcebergAbstractMetadata
                         procedureName.getSchemaName(),
                         procedureName.getObjectName()));
         verify(procedure instanceof DistributedProcedure, "procedure must be DistributedProcedure");
-        procedureContext = Optional.of((IcebergProcedureContext) ((DistributedProcedure) procedure).createContext(icebergTable, icebergTable.newTransaction()));
+        procedureContext = Optional.of((IcebergProcedureContext) ((DistributedProcedure) procedure).createContext(icebergTable, this));
         return ((DistributedProcedure) procedure).begin(session, procedureContext.get(), tableLayoutHandle, arguments);
     }
 
