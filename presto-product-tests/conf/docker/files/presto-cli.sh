d324999a44cf8848bb0ef98ab64568ecbee7a6b0
Shelton Cai <shelton408@meta.com>
2026-03-24 13:15:00 -0700

    Revert airbase 109 upgrade and Java 8 bytecode fixes on release-0.297-edge20
      Reverts the following commits:
      - 1ddef8b8f7 chore(deps): Updated airlift base and checkstyle to work with Java 17 (#27130)
      - 61bf930ddd [presto][diff_train] Fix OSS Spark modules to produce Java 8 bytecode
      - 0bce74d8ac Add Java 8 target for remaining Spark classloader modules
      - e1dc53663a Add Java 8 target for presto-spark-base, presto-spark, presto-spark-package
    
      The airbase 109 upgrade (D96822883) changed the default compiler target
      to Java 17, breaking all Spark modules that must run on Java 8 executors.
      Reverting airbase to 108 on this release branch.

--- presto-product-tests/conf/docker/files/presto-cli.sh
+++ presto-product-tests/conf/docker/files/presto-cli.sh
@@ -2,9 +2,4 @@
 
 set -euxo pipefail
 
-echo "java --version"
-echo $(java --version)
-export JAVA_HOME=/docker/volumes/overridejdk
-echo "/docker/volumes/overridejdk/bin/java --version"
-echo $(/docker/volumes/overridejdk/bin/java --version)
-/docker/volumes/overridejdk/bin/java -jar /docker/volumes/presto-cli/presto-cli-executable.jar ${CLI_ARGUMENTS} "$@"
+java -jar /docker/volumes/presto-cli/presto-cli-executable.jar ${CLI_ARGUMENTS} "$@"
