b0b83a4c4d7bc3fdddc6669e474ac1fb1210e23e
inf <ialhazmim@gmail.com>
2026-03-27 21:47:26 +0300

    feat(server): Add http support for internal resource manager communication (#26635)

--- presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
+++ presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
@@ -147,6 +147,7 @@ import com.facebook.presto.resourcemanager.ClusterMemoryManagerService;
 import com.facebook.presto.resourcemanager.ClusterQueryTrackerService;
 import com.facebook.presto.resourcemanager.ClusterStatusSender;
 import com.facebook.presto.resourcemanager.ForResourceManager;
+import com.facebook.presto.resourcemanager.HttpResourceManagerClient;
 import com.facebook.presto.resourcemanager.NoopResourceGroupService;
 import com.facebook.presto.resourcemanager.RaftConfig;
 import com.facebook.presto.resourcemanager.RandomResourceManagerAddressSelector;
@@ -475,7 +476,7 @@ public class ServerMainModule
 
         binder.bind(RandomResourceManagerAddressSelector.class).in(Scopes.SINGLETON);
         driftClientBinder(binder)
-                .bindDriftClient(ResourceManagerClient.class, ForResourceManager.class)
+                .bindDriftClient(com.facebook.presto.resourcemanager.thrift.ResourceManagerClient.class, ForResourceManager.class)
                 .withAddressSelector((addressSelectorBinder, annotation, prefix) ->
                         addressSelectorBinder.bind(AddressSelector.class).annotatedWith(annotation).to(RandomResourceManagerAddressSelector.class))
                 .withExceptionClassifier(throwable -> {
@@ -501,12 +502,14 @@ public class ServerMainModule
                     @Override
                     public void configure(Binder moduleBinder)
                     {
+                        binder.bind(ResourceManagerClient.class).to(HttpResourceManagerClient.class).in(Scopes.SINGLETON);
                         configBinder(moduleBinder).bindConfig(ResourceManagerConfig.class);
-                        // HTTP endpoint for some of ResourceManagerServer methods.
                         ResourceManagerConfig resourceManagerConfig = buildConfigObject(ResourceManagerConfig.class);
-                        if (resourceManagerConfig.getHeartbeatHttpEnabled()) {
-                            jaxrsBinder(moduleBinder).bind(ResourceManagerHeartbeatResource.class);
+
+                        if (serverConfig.isResourceManager() && resourceManagerConfig.getHttpServerEnabled()) {
+                            jaxrsBinder(moduleBinder).bind(ResourceManagerResource.class);
                         }
+
                         moduleBinder.bind(ClusterStatusSender.class).to(ResourceManagerClusterStatusSender.class).in(Scopes.SINGLETON);
                         if (serverConfig.isCoordinator()) {
                             moduleBinder.bind(ClusterMemoryManagerService.class).in(Scopes.SINGLETON);
