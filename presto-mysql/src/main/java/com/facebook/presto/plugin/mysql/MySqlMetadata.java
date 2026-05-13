78a6f823637cee11ba45c97e7811fec49472bde6
auden-woolfson <auden.woolfson@ibm.com>
2026-05-13 10:18:44 -0700

    Simplify getViews

--- presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlMetadata.java
+++ presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlMetadata.java
@@ -68,18 +68,7 @@ public class MySqlMetadata
     public Map<SchemaTableName, ConnectorViewDefinition> getViews(ConnectorSession session, SchemaTablePrefix prefix)
     {
         List<SchemaTableName> tableNames;
-
-        if (prefix.getTableName() != null) {
-            tableNames = ImmutableList.of(
-                    new SchemaTableName(prefix.getSchemaName(), prefix.getTableName()));
-        }
-        else if (prefix.getSchemaName() != null) {
-            tableNames = mySqlClient.listViews(session, Optional.of(prefix.getSchemaName()));
-        }
-        else {
-            tableNames = mySqlClient.listViews(session, Optional.empty());
-        }
-
+        tableNames = ImmutableList.of(new SchemaTableName(prefix.getSchemaName(), prefix.getTableName()));
         return mySqlClient.getViews(session, tableNames);
     }
 }
