e140a307043cd987b513b5758dcb730c7eea6931
Daniel Bauer <dnb@zurich.ibm.com>
2026-06-05 06:54:20 +0000

    ucx-exchange Presto hooks
    Squashed application of perfleap/presto PR #24 (11 commits):
    - cudf-exchange Presto hooks
    - guard ucx server startup
    - fix ucx communicator startup
    - fix ucx server startup integration
    - Fix shutdown of velox UCX communicator thread in PrestoServer.cpp
    - Remove unnecessary cudf header import
    - Minor from review
    - revert the old rootFragment fix
    - Update shutdownServer API
    - Rename cudf-exchange namespace to ucx-exchange in PrestoServer
    - Move ucx exchange startup message to actual startup and log error if it doesn't

--- presto-native-execution/presto_cpp/main/common/Configs.h
+++ presto-native-execution/presto_cpp/main/common/Configs.h
@@ -777,6 +777,9 @@ class SystemConfig : public ConfigBase {
   static constexpr std::string_view kHttpClientConnectionReuseCounterEnabled{
       "http-client.connection-reuse-counter-enabled"};
 
+  static constexpr std::string_view kCudfServerPort{
+      "cudf.exchange.server.port"};
+
   static constexpr std::string_view kExchangeMaxErrorDuration{
       "exchange.max-error-duration"};
 
@@ -972,6 +975,8 @@ class SystemConfig : public ConfigBase {
   static SystemConfig* instance();
 
   int httpServerHttpPort() const;
+
+  int cudfServerPort() const;
 
   bool httpServerReusePort() const;
 
