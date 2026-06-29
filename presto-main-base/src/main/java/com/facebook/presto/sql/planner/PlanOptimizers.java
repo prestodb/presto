8f2f7194e396ac5a1b9bced920adba1d37250cb3
wangd <mingwbd@gmail.com>
2026-06-29 10:15:29 +0800

    fix: Address update failure when using partitioned semi-join

--- presto-main-base/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
+++ presto-main-base/src/main/java/com/facebook/presto/sql/planner/PlanOptimizers.java
@@ -215,7 +215,7 @@ import com.facebook.presto.sql.planner.optimizations.PushdownSubfields;
 import com.facebook.presto.sql.planner.optimizations.RandomizeNullKeyInOuterJoin;
 import com.facebook.presto.sql.planner.optimizations.RemoveRedundantDistinctAggregation;
 import com.facebook.presto.sql.planner.optimizations.ReplaceConstantVariableReferencesWithConstants;
-import com.facebook.presto.sql.planner.optimizations.ReplicateSemiJoinInDelete;
+import com.facebook.presto.sql.planner.optimizations.ReplicateSemiJoinInDeleteOrUpdate;
 import com.facebook.presto.sql.planner.optimizations.RewriteIfOverAggregation;
 import com.facebook.presto.sql.planner.optimizations.RewriteWriterTarget;
 import com.facebook.presto.sql.planner.optimizations.RpcExecutionPolicy;
@@ -1045,7 +1045,7 @@ public class PlanOptimizers
                         ImmutableSet.of(new ScaledWriterRule())));
 
         if (!noExchange) {
-            builder.add(new ReplicateSemiJoinInDelete()); // Must run before AddExchanges
+            builder.add(new ReplicateSemiJoinInDeleteOrUpdate()); // Must run before AddExchanges
 
             builder.add(new IterativeOptimizer(
                     metadata,
