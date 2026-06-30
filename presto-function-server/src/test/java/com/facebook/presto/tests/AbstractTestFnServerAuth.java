c55977e266c02de59bf52901fb45dd217190c6c3
Saiteja Goruganthu <saiteja.goruganthu121@gmail.com>
2026-06-29 21:40:57 -0700

    removed unused import statement

--- presto-function-server/src/test/java/com/facebook/presto/tests/AbstractTestFnServerAuth.java
+++ presto-function-server/src/test/java/com/facebook/presto/tests/AbstractTestFnServerAuth.java
@@ -21,7 +21,6 @@ import org.testng.annotations.Test;
 
 import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
 /**
