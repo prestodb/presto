aaneja/presto2

Derived column support
Based on RFC-0025.

Parser changes:
1. Extended SqlBase.g4 to support GENERATED ALWAYS AS <expression> PERSISTENT|VIRTUAL syntax in column definitions

SPI changes:

1. DerivedColumnSpec: Stores column name, expression, and type (PERSISTENT/VIRTUAL)
2. Extended ColumnMetadata with optional DerivedColumnSpec

Iceberg connector changes:

1. IcebergTableProperties: Added 'presto.derived-columns.spec.json' table property
2. IcebergAbstractMetadata: Modified to propagate derived column specs through table creation/alteration

presto-iceberg/src/main/java/com/facebook/presto/iceberg/IcebergUtil.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.iceberg;
 
+import com.facebook.airlift.json.JsonCodec;
 import com.facebook.airlift.log.Logger;
 import com.facebook.airlift.units.DataSize;
 import com.facebook.presto.common.GenericInternalException;
@@ -53,6 +54,8 @@ import com.facebook.presto.spi.SchemaTableName;
 import com.facebook.presto.spi.TableNotFoundException;
 import com.facebook.presto.spi.connector.ConnectorMetadata;
 import com.facebook.presto.spi.connector.ConnectorTableVersion.VersionOperator;
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnSpec;
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnSpecList;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -162,6 +165,7 @@ import static com.facebook.presto.iceberg.IcebergMetadataColumn.isMetadataColumn
 import static com.facebook.presto.iceberg.IcebergPartitionType.IDENTITY;
 import static com.facebook.presto.iceberg.IcebergSessionProperties.getCompressionCodec;
 import static com.facebook.presto.iceberg.IcebergSessionProperties.isMergeOnReadModeEnabled;
+import static com.facebook.presto.iceberg.IcebergTableProperties.DERIVED_COLUMN_EXPRESSION_SPEC;
 import static com.facebook.presto.iceberg.IcebergTableProperties.getWriteDataLocation;
 import static com.facebook.presto.iceberg.IcebergTableProperties.isHiveLocksEnabled;
 import static com.facebook.presto.iceberg.TypeConverter.toIcebergType;
@@ -170,6 +174,7 @@ import static com.facebook.presto.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_FOUND;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -255,6 +260,8 @@ public final class IcebergUtil
     public static final int REAL_NEGATIVE_INFINITE = 0xff800000;
 
     protected static final String VIEW_OWNER = "view_owner";
+    public static final JsonCodec<DerivedColumnSpecList> DERIVED_COLUMN_SPEC_JSON_CODEC = JsonCodec.jsonCodec(DerivedColumnSpecList.class);
+    public static final String DERIVED_COL_EMPTY_SPEC = DERIVED_COLUMN_SPEC_JSON_CODEC.toJson(new DerivedColumnSpecList(ImmutableList.of()));
 
     public static final int DEFAULT_MIN_INPUT_FILES = 5;
 
@@ -1363,10 +1370,30 @@ public final class IcebergUtil
         }
     }
 
-    public static Map<String, String> populateTableProperties(IcebergAbstractMetadata metadata, ConnectorTableMetadata tableMetadata, IcebergTableProperties tableProperties, FileFormat fileFormat, ConnectorSession session)
+    public static Map<String, String> populateTableProperties(
+            IcebergAbstractMetadata metadata,
+            ConnectorTableMetadata tableMetadata,
+            IcebergTableProperties tableProperties,
+            FileFormat fileFormat,
+            ConnectorSession session,
+            Schema schema)
     {
-        ImmutableMap.Builder<String, String> propertiesBuilder = ImmutableMap.builderWithExpectedSize(5);
+        ImmutableMap.Builder<String, String> propertiesBuilder = ImmutableMap.builderWithExpectedSize(10);
+        checkArgument(IcebergTableProperties.getDerivedColumnSpec(tableMetadata.getProperties()).getDerivedColumnSpecs().isEmpty(),
+                "property %s is not user configurable", DERIVED_COLUMN_EXPRESSION_SPEC);
+        List<DerivedColumnSpec> derivedColumnSpecs = tableMetadata.getColumns().stream()
+                .filter(columnMetadata -> columnMetadata.getDerivedColumnSpec().isPresent())
+                .map(columnMetadata -> columnMetadata.getDerivedColumnSpec().get())
+                .map(derivedColumnSpec ->
+                        DerivedColumnSpec.buildFrom(derivedColumnSpec).setDerivedColumnFieldId(schema.findField(derivedColumnSpec.getDerivedColumnName()).fieldId()).build())
+                .collect(toImmutableList());
 
+        DerivedColumnSpecList derivedColumnSpecList = new DerivedColumnSpecList(derivedColumnSpecs);
+        checkState(derivedColumnSpecList.validateFieldIds(), format("derived column spec has invalid fieldIds. %s", DERIVED_COLUMN_SPEC_JSON_CODEC.toJson(derivedColumnSpecList)));
+        if (!derivedColumnSpecList.getDerivedColumnSpecs().isEmpty()) {
+            // Following property is updated automatically via create/alter table, user overrides are not permitted.
+            propertiesBuilder.put(DERIVED_COLUMN_EXPRESSION_SPEC, DERIVED_COLUMN_SPEC_JSON_CODEC.toJson(derivedColumnSpecList));
+        }
         String writeDataLocation = getWriteDataLocation(tableMetadata.getProperties());
         if (!isNullOrEmpty(writeDataLocation)) {
             propertiesBuilder.put(WRITE_DATA_LOCATION, writeDataLocation);
@@ -1448,6 +1475,11 @@ public final class IcebergUtil
         return RowLevelOperationMode.fromName(table.properties()
                 .getOrDefault(DELETE_MODE, DELETE_MODE_DEFAULT)
                 .toUpperCase(Locale.ENGLISH));
+    }
+
+    public static DerivedColumnSpecList getDerivedColumnUDFSpec(Table table)
+    {
+        return DERIVED_COLUMN_SPEC_JSON_CODEC.fromJson(table.properties().getOrDefault(DERIVED_COLUMN_EXPRESSION_SPEC, DERIVED_COL_EMPTY_SPEC));
     }
 
     public static RowLevelOperationMode getUpdateMode(Table table)

presto-main-base/src/main/java/com/facebook/presto/execution/CreateTableTask.java
@@ -26,6 +26,7 @@ import com.facebook.presto.spi.TableMetadata;
 import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.constraints.PrimaryKeyConstraint;
 import com.facebook.presto.spi.constraints.TableConstraint;
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnSpec;
 import com.facebook.presto.spi.security.AccessControl;
 import com.facebook.presto.spi.type.UnknownTypeException;
 import com.facebook.presto.sql.analyzer.SemanticException;
@@ -141,7 +142,9 @@ public class CreateTableTask
                 if (!column.isNullable() && !metadata.getConnectorCapabilities(session, connectorId).contains(NOT_NULL_COLUMN_CONSTRAINT)) {
                     throw new SemanticException(NOT_SUPPORTED, column, "Catalog '%s' does not support non-null column for column name '%s'", connectorId.getCatalogName(), column.getName());
                 }
-
+                if (column.getDefaultExpression().isPresent() && column.getDerivedColumnSpec().isPresent()) {
+                    throw new SemanticException(NOT_SUPPORTED, column, "Both default expression and derived column expression cannot be set on the same column %s.", column.getName());
+                }
                 Map<String, Expression> sqlProperties = mapFromProperties(column.getProperties());
                 Map<String, Object> columnProperties = metadata.getColumnPropertyManager().getProperties(
                         connectorId,
@@ -150,13 +153,17 @@ public class CreateTableTask
                         session,
                         metadata,
                         parameterLookup);
+                ImmutableMap.Builder<String, Object> propertiesMapBuilder = ImmutableMap.<String, Object>builder()
+                        .putAll(columnProperties);
 
+                Optional<DerivedColumnSpec> derivedColumnSpec = normalizeDerivedColumnSpec(column, name);
                 columns.put(name, ColumnMetadata.builder()
                         .setName(name)
                         .setType(type)
                         .setNullable(column.isNullable())
                         .setComment(column.getComment().orElse(null))
-                        .setProperties(columnProperties)
+                        .setProperties(propertiesMapBuilder.build())
+                        .setDerivedColumnSpec(derivedColumnSpec.orElse(null))
                         .build());
             }
             else if (element instanceof LikeClause) {
@@ -238,6 +245,16 @@ public class CreateTableTask
             }
         }
         return immediateFuture(null);
+    }
+
+    static Optional<DerivedColumnSpec> normalizeDerivedColumnSpec(ColumnDefinition column, String name)
+    {
+        // Normalize the values for column name and Type
+        return column.getDerivedColumnSpec().map(derivedColumnSpec1 -> DerivedColumnSpec
+                .buildFrom(derivedColumnSpec1)
+                .setDerivedColumnName(name)
+                .setDerivedColumnReturnType(parseTypeSignature(derivedColumnSpec1.getDerivedColumnReturnType()).toString())
+                .build());
     }
 
     private static Map<String, Object> combineProperties(Set<String> specifiedPropertyKeys, Map<String, Object> defaultProperties, Map<String, Object> inheritedProperties)

presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -13,7 +13,10 @@
  */
 package com.facebook.presto.sql.parser;
 
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnSpec;
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnType;
 import com.facebook.presto.spi.security.ViewSecurity;
+import com.facebook.presto.sql.ExpressionFormatter;
 import com.facebook.presto.sql.tree.AddColumn;
 import com.facebook.presto.sql.tree.AddConstraint;
 import com.facebook.presto.sql.tree.AliasedRelation;
@@ -2353,12 +2356,50 @@ class AstBuilder
 
         Optional<Expression> defaultExpression = Optional.empty();
         if (context.DEFAULT() != null && context.expression() != null) {
-            defaultExpression = Optional.of((Expression) visit(context.expression()));
+            defaultExpression = Optional.of((Expression) visit(context.expression().get(0)));
         }
 
+        if (context.DEFAULT() != null && (context.GENERATED_ALWAYS() != null || context.AS() != null)) {
+            throw new ParsingException("Setting a default expression on GENERATED column is not currently supported.");
+        }
+
+        Optional<DerivedColumnSpec> derivedColumnExpressionSpec = Optional.empty();
+        Identifier columnIdentifier = (Identifier) visit(context.identifier());
+        Optional<Expression> derivedColumnExpression = Optional.empty();
+        if (context.AS() != null && !context.expression().isEmpty()) {
+            SqlBaseParser.ExpressionContext tree = context.expression().get(0);
+            check(tree != null, "expression is null ", context);
+            Expression validatedDerivedColumnExpression = (Expression) visit(tree);
+            DerivedColumnType derivedColumnType = DerivedColumnType.PERSISTENT;
+            if (context.GENERATED_ALWAYS() != null) {
+                derivedColumnType = DerivedColumnType.GENERATED_ALWAYS_PERSISTENT;
+            }
+            if (context.VIRTUAL() != null) {
+                derivedColumnType = DerivedColumnType.VIRTUAL;
+            }
+            derivedColumnExpression = Optional.of(validatedDerivedColumnExpression);
+            // tree.getText() gets the expression as text, but messes it up by removing spaces. e.g. (CAST(c1 AS decimal) * DECIMAL '10.5') becomes (CAST(c1ASdecimal)*DECIMAL'10.5')
+            derivedColumnExpressionSpec = Optional.of(
+                    new DerivedColumnSpec(derivedColumnType,
+                            ExpressionFormatter.formatExpression(validatedDerivedColumnExpression, Optional.empty()),
+                            columnIdentifier.getValue(),
+                            -1,
+                            getType(context.type())));
+        }
+
+        if (derivedColumnExpressionSpec.isPresent()) {
+            return new ColumnDefinition(Optional.of(getLocation(context)),
+                    columnIdentifier,
+                    getType(context.type()),
+                    nullable,
+                    properties,
+                    comment,
+                    derivedColumnExpression,
+                    derivedColumnExpressionSpec);
+        }
         return new ColumnDefinition(
                 getLocation(context),
-                (Identifier) visit(context.identifier()),
+                columnIdentifier,
                 getType(context.type()),
                 nullable, properties,
                 comment,

prestodb/presto

presto-iceberg/src/main/java/com/facebook/presto/iceberg/IcebergUtil.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.iceberg;
 
+import com.facebook.airlift.json.JsonCodec;
 import com.facebook.airlift.log.Logger;
 import com.facebook.airlift.units.DataSize;
 import com.facebook.presto.common.GenericInternalException;
@@ -53,6 +54,8 @@ import com.facebook.presto.spi.SchemaTableName;
 import com.facebook.presto.spi.TableNotFoundException;
 import com.facebook.presto.spi.connector.ConnectorMetadata;
 import com.facebook.presto.spi.connector.ConnectorTableVersion.VersionOperator;
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnSpec;
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnSpecList;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -162,6 +165,7 @@ import static com.facebook.presto.iceberg.IcebergMetadataColumn.isMetadataColumn
 import static com.facebook.presto.iceberg.IcebergPartitionType.IDENTITY;
 import static com.facebook.presto.iceberg.IcebergSessionProperties.getCompressionCodec;
 import static com.facebook.presto.iceberg.IcebergSessionProperties.isMergeOnReadModeEnabled;
+import static com.facebook.presto.iceberg.IcebergTableProperties.DERIVED_COLUMN_EXPRESSION_SPEC;
 import static com.facebook.presto.iceberg.IcebergTableProperties.getWriteDataLocation;
 import static com.facebook.presto.iceberg.IcebergTableProperties.isHiveLocksEnabled;
 import static com.facebook.presto.iceberg.TypeConverter.toIcebergType;
@@ -170,6 +174,7 @@ import static com.facebook.presto.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_FOUND;
 import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -255,6 +260,8 @@ public final class IcebergUtil
     public static final int REAL_NEGATIVE_INFINITE = 0xff800000;
 
     protected static final String VIEW_OWNER = "view_owner";
+    public static final JsonCodec<DerivedColumnSpecList> DERIVED_COLUMN_SPEC_JSON_CODEC = JsonCodec.jsonCodec(DerivedColumnSpecList.class);
+    public static final String DERIVED_COL_EMPTY_SPEC = DERIVED_COLUMN_SPEC_JSON_CODEC.toJson(new DerivedColumnSpecList(ImmutableList.of()));
 
     public static final int DEFAULT_MIN_INPUT_FILES = 5;
 
@@ -1363,10 +1370,30 @@ public final class IcebergUtil
         }
     }
 
-    public static Map<String, String> populateTableProperties(IcebergAbstractMetadata metadata, ConnectorTableMetadata tableMetadata, IcebergTableProperties tableProperties, FileFormat fileFormat, ConnectorSession session)
+    public static Map<String, String> populateTableProperties(
+            IcebergAbstractMetadata metadata,
+            ConnectorTableMetadata tableMetadata,
+            IcebergTableProperties tableProperties,
+            FileFormat fileFormat,
+            ConnectorSession session,
+            Schema schema)
     {
-        ImmutableMap.Builder<String, String> propertiesBuilder = ImmutableMap.builderWithExpectedSize(5);
+        ImmutableMap.Builder<String, String> propertiesBuilder = ImmutableMap.builderWithExpectedSize(10);
+        checkArgument(IcebergTableProperties.getDerivedColumnSpec(tableMetadata.getProperties()).getDerivedColumnSpecs().isEmpty(),
+                "property %s is not user configurable", DERIVED_COLUMN_EXPRESSION_SPEC);
+        List<DerivedColumnSpec> derivedColumnSpecs = tableMetadata.getColumns().stream()
+                .filter(columnMetadata -> columnMetadata.getDerivedColumnSpec().isPresent())
+                .map(columnMetadata -> columnMetadata.getDerivedColumnSpec().get())
+                .map(derivedColumnSpec ->
+                        DerivedColumnSpec.buildFrom(derivedColumnSpec).setDerivedColumnFieldId(schema.findField(derivedColumnSpec.getDerivedColumnName()).fieldId()).build())
+                .collect(toImmutableList());
 
+        DerivedColumnSpecList derivedColumnSpecList = new DerivedColumnSpecList(derivedColumnSpecs);
+        checkState(derivedColumnSpecList.validateFieldIds(), format("derived column spec has invalid fieldIds. %s", DERIVED_COLUMN_SPEC_JSON_CODEC.toJson(derivedColumnSpecList)));
+        if (!derivedColumnSpecList.getDerivedColumnSpecs().isEmpty()) {
+            // Following property is updated automatically via create/alter table, user overrides are not permitted.
+            propertiesBuilder.put(DERIVED_COLUMN_EXPRESSION_SPEC, DERIVED_COLUMN_SPEC_JSON_CODEC.toJson(derivedColumnSpecList));
+        }
         String writeDataLocation = getWriteDataLocation(tableMetadata.getProperties());
         if (!isNullOrEmpty(writeDataLocation)) {
             propertiesBuilder.put(WRITE_DATA_LOCATION, writeDataLocation);
@@ -1448,6 +1475,11 @@ public final class IcebergUtil
         return RowLevelOperationMode.fromName(table.properties()
                 .getOrDefault(DELETE_MODE, DELETE_MODE_DEFAULT)
                 .toUpperCase(Locale.ENGLISH));
+    }
+
+    public static DerivedColumnSpecList getDerivedColumnSpec(Table table)
+    {
+        return DERIVED_COLUMN_SPEC_JSON_CODEC.fromJson(table.properties().getOrDefault(DERIVED_COLUMN_EXPRESSION_SPEC, DERIVED_COL_EMPTY_SPEC));
     }
 
     public static RowLevelOperationMode getUpdateMode(Table table)

presto-main-base/src/main/java/com/facebook/presto/execution/CreateTableTask.java
@@ -26,6 +26,7 @@ import com.facebook.presto.spi.TableMetadata;
 import com.facebook.presto.spi.WarningCollector;
 import com.facebook.presto.spi.constraints.PrimaryKeyConstraint;
 import com.facebook.presto.spi.constraints.TableConstraint;
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnSpec;
 import com.facebook.presto.spi.security.AccessControl;
 import com.facebook.presto.spi.type.UnknownTypeException;
 import com.facebook.presto.sql.analyzer.SemanticException;
@@ -141,7 +142,9 @@ public class CreateTableTask
                 if (!column.isNullable() && !metadata.getConnectorCapabilities(session, connectorId).contains(NOT_NULL_COLUMN_CONSTRAINT)) {
                     throw new SemanticException(NOT_SUPPORTED, column, "Catalog '%s' does not support non-null column for column name '%s'", connectorId.getCatalogName(), column.getName());
                 }
-
+                if (column.getDefaultExpression().isPresent() && column.getDerivedColumnSpec().isPresent()) {
+                    throw new SemanticException(NOT_SUPPORTED, column, "Both default expression and derived column expression cannot be set on the same column %s.", column.getName());
+                }
                 Map<String, Expression> sqlProperties = mapFromProperties(column.getProperties());
                 Map<String, Object> columnProperties = metadata.getColumnPropertyManager().getProperties(
                         connectorId,
@@ -150,13 +153,17 @@ public class CreateTableTask
                         session,
                         metadata,
                         parameterLookup);
+                ImmutableMap.Builder<String, Object> propertiesMapBuilder = ImmutableMap.<String, Object>builder()
+                        .putAll(columnProperties);
 
+                Optional<DerivedColumnSpec> derivedColumnSpec = normalizeDerivedColumnSpec(column, name);
                 columns.put(name, ColumnMetadata.builder()
                         .setName(name)
                         .setType(type)
                         .setNullable(column.isNullable())
                         .setComment(column.getComment().orElse(null))
-                        .setProperties(columnProperties)
+                        .setProperties(propertiesMapBuilder.build())
+                        .setDerivedColumnSpec(derivedColumnSpec)
                         .build());
             }
             else if (element instanceof LikeClause) {
@@ -238,6 +245,16 @@ public class CreateTableTask
             }
         }
         return immediateFuture(null);
+    }
+
+    static Optional<DerivedColumnSpec> normalizeDerivedColumnSpec(ColumnDefinition column, String name)
+    {
+        // Normalize the values for column name and Type
+        return column.getDerivedColumnSpec().map(derivedColumnSpec -> DerivedColumnSpec
+                .buildFrom(derivedColumnSpec)
+                .setDerivedColumnName(name)
+                .setDerivedColumnReturnType(parseTypeSignature(derivedColumnSpec.getDerivedColumnReturnType()).toString())
+                .build());
     }
 
     private static Map<String, Object> combineProperties(Set<String> specifiedPropertyKeys, Map<String, Object> defaultProperties, Map<String, Object> inheritedProperties)

presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -13,7 +13,10 @@
  */
 package com.facebook.presto.sql.parser;
 
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnSpec;
+import com.facebook.presto.spi.derivedcolumns.DerivedColumnType;
 import com.facebook.presto.spi.security.ViewSecurity;
+import com.facebook.presto.sql.ExpressionFormatter;
 import com.facebook.presto.sql.tree.AddColumn;
 import com.facebook.presto.sql.tree.AddConstraint;
 import com.facebook.presto.sql.tree.AliasedRelation;
@@ -2353,12 +2356,50 @@ class AstBuilder
 
         Optional<Expression> defaultExpression = Optional.empty();
         if (context.DEFAULT() != null && context.expression() != null) {
-            defaultExpression = Optional.of((Expression) visit(context.expression()));
+            defaultExpression = Optional.of((Expression) visit(context.expression().get(0)));
         }
 
+        if (context.DEFAULT() != null && (context.GENERATED() != null || context.AS() != null)) {
+            throw new ParsingException("Setting a default expression on a derived column is not supported.");
+        }
+
+        Optional<DerivedColumnSpec> derivedColumnExpressionSpec = Optional.empty();
+        Identifier columnIdentifier = (Identifier) visit(context.identifier());
+        Optional<Expression> derivedColumnExpression = Optional.empty();
+        if (context.AS() != null && !context.expression().isEmpty()) {
+            SqlBaseParser.ExpressionContext tree = context.expression().get(0);
+            check(tree != null, "expression is null ", context);
+            Expression validatedDerivedColumnExpression = (Expression) visit(tree);
+            DerivedColumnType derivedColumnType = DerivedColumnType.PERSISTENT;
+            if (context.GENERATED() != null && context.ALWAYS() != null) {
+                derivedColumnType = DerivedColumnType.GENERATED_ALWAYS_PERSISTENT;
+            }
+            if (context.VIRTUAL() != null) {
+                derivedColumnType = DerivedColumnType.VIRTUAL;
+            }
+            derivedColumnExpression = Optional.of(validatedDerivedColumnExpression);
+            // tree.getText() gets the expression as text, but messes it up by removing spaces. e.g. (CAST(c1 AS decimal) * DECIMAL '10.5') becomes (CAST(c1ASdecimal)*DECIMAL'10.5')
+            derivedColumnExpressionSpec = Optional.of(
+                    new DerivedColumnSpec(derivedColumnType,
+                            ExpressionFormatter.formatExpression(validatedDerivedColumnExpression, Optional.empty()),
+                            columnIdentifier.getValue(),
+                            -1,
+                            getType(context.type())));
+        }
+
+        if (derivedColumnExpressionSpec.isPresent()) {
+            return new ColumnDefinition(Optional.of(getLocation(context)),
+                    columnIdentifier,
+                    getType(context.type()),
+                    nullable,
+                    properties,
+                    comment,
+                    derivedColumnExpression,
+                    derivedColumnExpressionSpec);
+        }
         return new ColumnDefinition(
                 getLocation(context),
-                (Identifier) visit(context.identifier()),
+                columnIdentifier,
                 getType(context.type()),
                 nullable, properties,
                 comment,